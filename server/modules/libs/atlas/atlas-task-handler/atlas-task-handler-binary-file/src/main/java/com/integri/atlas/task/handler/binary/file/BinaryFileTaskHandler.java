f952d54cc588566f09099c25cf154cab83ecf42c
Ivica Cardic <ivica.cardic@liferay.com>
2021-12-02 11:11:51 +0100

    ISSUE-8 - Add BinaryFile task support

---
+++ server/modules/libs/atlas/atlas-task-handler/atlas-task-handler-binary-file/src/main/java/com/integri/atlas/task/handler/binary/file/BinaryFileTaskHandler.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2016-2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Modifications copyright (C) 2021 <your company/name>
+ */
+
+package com.integri.atlas.task.handler.binary.file;
+
+import com.integri.atlas.engine.core.task.TaskExecution;
+import com.integri.atlas.engine.worker.task.handler.TaskHandler;
+import com.integri.atlas.json.item.BinaryItem;
+import com.integri.atlas.json.item.BinaryItemHelper;
+import com.integri.atlas.json.item.JSONItem;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import org.json.JSONObject;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Ivica Cardic
+ */
+@Component("binaryFile")
+public class BinaryFileTaskHandler implements TaskHandler<JSONObject> {
+
+    private enum Operation {
+        READ,
+        WRITE,
+    }
+
+    private final BinaryItemHelper binaryNodeHelper;
+
+    public BinaryFileTaskHandler(BinaryItemHelper binaryNodeHelper) {
+        this.binaryNodeHelper = binaryNodeHelper;
+    }
+
+    @Override
+    public JSONObject handle(TaskExecution taskExecution) throws Exception {
+        JSONObject jsonObject;
+
+        String fileName = taskExecution.getRequired("fileName");
+        Operation operation = Operation.valueOf(taskExecution.getRequired("operation"));
+
+        if (operation == Operation.READ) {
+            try (InputStream inputStream = new FileInputStream(fileName)) {
+                jsonObject = binaryNodeHelper.writeBinaryData(fileName, inputStream);
+            }
+        } else {
+            BinaryItem binaryItem = taskExecution.getRequired("binaryItem", BinaryItem.class);
+
+            try (InputStream inputStream = binaryNodeHelper.openDataInputStream(binaryItem)) {
+                jsonObject =
+                    JSONItem.of(
+                        "bytes",
+                        Files.copy(inputStream, Path.of(fileName), StandardCopyOption.REPLACE_EXISTING)
+                    );
+            }
+        }
+
+        return jsonObject;
+    }
+}
---
+++ server/modules/libs/atlas/atlas-task-handler/atlas-task-handler-binary-file/src/test/java/com/integri/atlas/task/handler/binary/file/BinaryFileTaskDescriptorTest.java
@@ -0,0 +1,117 @@
+/*
+ * Copyright 2016-2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Modifications copyright (C) 2021 <your company/name>
+ */
+
+package com.integri.atlas.task.handler.binary.file;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.json.JSONObject;
+import org.junit.jupiter.api.Test;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.skyscreamer.jsonassert.JSONParser;
+
+/**
+ * @author Ivica Cardic
+ */
+public class BinaryFileTaskDescriptorTest {
+
+    private final ObjectMapper objectMapper = new ObjectMapper() {
+        {
+            setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        }
+    };
+
+    @Test
+    public void testBinaryFileTaskDescription() throws JsonProcessingException {
+        JSONAssert.assertEquals(
+            """
+            {
+                "description":"Reads or writes a binary file from/toto disk",
+                "displayName":"Binary File",
+                "name":"binaryFile",
+                "properties":[
+                    {
+                        "defaultValue":"read",
+                        "description":"The operation to perform.",
+                        "displayName":"Operation",
+                        "name":"operation",
+                        "required":true,
+                        "type":"SELECT",
+                        "options":[
+                            {
+                                "name":"Read to file",
+                                "value":"READ"
+                            },
+                            {
+                                "name":"Write from file",
+                                "value":"WRITE"
+                            }
+                        ]
+                    },
+                    {
+                        "defaultValue":"",
+                        "description":"The path of the file to read.",
+                        "displayName":"File Name",
+                        "displayOption":{
+                            "show":{
+                                "operation":["READ"]
+                            }
+                        },
+                        "name":"fileName",
+                        "required":true,
+                        "type":"STRING",
+                        "placeholder":"/data/your_file.pdf"
+                    },
+                    {
+                        "defaultValue":"",
+                        "description":"The path to which the file should be written.",
+                        "displayName":"File Name",
+                        "displayOption":{
+                            "show":{
+                                "operation":["WRITE"]
+                            }
+                        },
+                        "name":"fileName",
+                        "required":true,
+                        "type":"STRING",
+                        "placeholder":"/data/your_file.pdf"
+                    },
+                   {
+                        "description":"The Binary Item property which contains the data for the file to be written.",
+                        "displayName":"Binary Item",
+                        "displayOption":{
+                            "show":{
+                                "operation":["WRITE"]
+                            }
+                        },
+                        "name":"binaryItem",
+                        "required":true,
+                        "type":"BINARY"
+                    }
+                ],
+                "version":1.0
+                }
+            """,
+            (JSONObject) JSONParser.parseJSON(
+                objectMapper.writeValueAsString(BinaryFileTaskDescriptor.TASK_DESCRIPTION)
+            ),
+            true
+        );
+    }
+}
