9304c6d774a2ad0888f74d13ee93243389a9be08
Ivica Cardic <ivica.cardic@liferay.com>
2021-12-02 11:24:40 +0100

    ISSUE-12 - Add test for TaskController

---
+++ server/modules/libs/atlas/atlas-rest/src/test/java/com/integri/atlas/workflow/web/TaskControllerIntTest.java
@@ -0,0 +1,101 @@
+/*
+ * Copyright 2016-2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Modifications copyright (C) 2021 <your company/name>
+ */
+
+package com.integri.atlas.workflow.web;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+/**
+ * @author Ivica Cardic
+ */
+@AutoConfigureMockMvc
+@SpringBootTest
+public class TaskControllerIntTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testGetTaskDescriptor() throws Exception {
+        mockMvc
+            .perform(get("/tasks/task1").accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andExpect(
+                content()
+                    .json(
+                        """
+                {
+                    "authentication":null,
+                    "description":null,
+                    "displayName":null,
+                    "name":"task1",
+                    "icon":null,
+                    "properties":null,
+                    "subtitle":null,
+                    "version":1.0
+                }
+                """
+                    )
+            );
+    }
+
+    @Test
+    public void testGetTaskDescriptors() throws Exception {
+        mockMvc
+            .perform(get("/tasks").accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andExpect(
+                content()
+                    .json(
+                        """
+                [
+                    {
+                        "authentication":null,
+                        "description":null,
+                        "displayName":null,
+                        "name":"task1",
+                        "icon":null,
+                        "properties":null,
+                        "subtitle":null,
+                        "version":1.0
+                    },
+                    {
+                        "authentication":null,
+                        "description":null,
+                        "displayName":null,
+                        "name":"task2",
+                        "icon":null,
+                        "properties":null,
+                        "subtitle":null,
+                        "version":1.0
+                    }
+                ]
+                """
+                    )
+            );
+    }
+}
