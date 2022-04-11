adcd65b06f54db1810d5933503c2208b003f1ea0
Ivica Cardic <ivica.cardic@liferay.com>
2022-04-11 11:22:22 +0200

    ISSUE-52 - Add XMLHelper

---
+++ server/modules/libs/atlas/engine/engine-core-api/src/main/java/com/integri/atlas/engine/core/xml/XMLHelper.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2021 <your company/name>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.integri.atlas.engine.core.xml;
+
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import java.util.Map;
+
+/**
+ * @author Ivica Cardic
+ */
+public class XMLHelper {
+
+    private static final XmlMapper xmlMapper = new XmlMapper();
+
+    @SuppressWarnings("unchecked")
+    public Map<String, ?> deserialize(String xml) {
+        try {
+            return xmlMapper.convertValue(xmlMapper.readTree(xml), Map.class);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public <T> T deserialize(String xml, Class<T> clazz) {
+        try {
+            return xmlMapper.readValue(xml, clazz);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String serialize(Object value) {
+        return serialize(value, "root");
+    }
+
+    public String serialize(Object value, String rootName) {
+        try {
+            return xmlMapper.writer().withRootName(rootName).writeValueAsString(value);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
