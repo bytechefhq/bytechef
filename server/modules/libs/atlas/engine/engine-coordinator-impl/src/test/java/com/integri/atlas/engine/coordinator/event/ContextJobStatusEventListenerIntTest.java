06410303963acb865e0bee93deeec33a5c1ca321
Ivica Cardic <ivica.cardic@liferay.com>
2022-04-16 08:03:37 +0200

    ISSUE-60 - Add ContextJobStatusEventListenerIntTest

---
+++ server/modules/libs/atlas/engine/engine-coordinator-impl/src/test/java/com/integri/atlas/engine/coordinator/event/ContextJobStatusEventListenerIntTest.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2021 <your company/name>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.integri.atlas.engine.coordinator.event;
+
+import com.integri.atlas.engine.coordinator.job.Job;
+import com.integri.atlas.engine.coordinator.job.repository.JobRepository;
+import com.integri.atlas.engine.core.context.repository.ContextRepository;
+import com.integri.atlas.engine.worker.task.handler.TaskHandler;
+import com.integri.atlas.task.handler.BaseTaskIntTest;
+import com.integri.atlas.task.handler.io.Print;
+import com.integri.atlas.task.handler.random.RandomInt;
+import com.integri.atlas.task.handler.time.Sleep;
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * @author Ivica Cardic
+ */
+@SpringBootTest
+public class ContextJobStatusEventListenerIntTest extends BaseTaskIntTest {
+
+    @Autowired
+    private ContextRepository contextRepository;
+
+    @Test
+    public void testJobStatusEventHandler() {
+        Job job = startJob("samples/hello.json", Map.of("yourName", "me"));
+
+        Assertions.assertNull(contextRepository.peek(job.getId()));
+    }
+
+    @Override
+    protected Map<String, TaskHandler<?>> getTaskHandlerResolverMap() {
+        Map<String, TaskHandler<?>> handlers = new HashMap<>();
+        handlers.put("io/print", new Print());
+        handlers.put("random/int", new RandomInt());
+        handlers.put("time/sleep", new Sleep());
+
+        return handlers;
+    }
+
+    @TestConfiguration
+    public static class CoordinatorTestConfiguration {
+
+        @Bean
+        ContextJobStatusEventListener contextJobStatusEventHandler(
+            ContextService contextService,
+            JobRepository jobRepository
+        ) {
+            return new ContextJobStatusEventListener(contextService, jobRepository);
+        }
+    }
+}
