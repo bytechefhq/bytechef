4816928cf1abd27545a27dc02d14c898a0ee8867
Ivica Cardic <ivica.cardic@liferay.com>
2022-04-18 09:09:52 +0200

    ISSUE-62 - Define ContextService

---
+++ server/modules/libs/atlas/engine/engine-coordinator-impl/src/main/java/com/integri/atlas/engine/coordinator/event/ContextService.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2021 <your company/name>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.integri.atlas.engine.coordinator.event;
+
+import com.integri.atlas.engine.core.context.repository.ContextRepository;
+import com.integri.atlas.engine.core.task.TaskExecution;
+import com.integri.atlas.engine.core.task.repository.TaskExecutionRepository;
+import java.util.List;
+import org.springframework.transaction.annotation.Transactional;
+
+public class ContextService {
+
+    private final ContextRepository contextRepository;
+    private final TaskExecutionRepository taskExecutionRepository;
+
+    public ContextService(ContextRepository contextRepository, TaskExecutionRepository taskExecutionRepository) {
+        this.contextRepository = contextRepository;
+        this.taskExecutionRepository = taskExecutionRepository;
+    }
+
+    @Transactional
+    public void deleteJobContext(String jobId) {
+        List<TaskExecution> taskExecutions = taskExecutionRepository.findByJobId(jobId);
+
+        for (TaskExecution taskExecution : taskExecutions) {
+            deleteTaskExecutionContext(taskExecution);
+        }
+
+        contextRepository.delete(jobId);
+    }
+
+    private void deleteTaskExecutionContext(TaskExecution taskExecution) {
+        List<TaskExecution> taskExecutions = taskExecutionRepository.findByParentId(taskExecution.getId());
+
+        for (TaskExecution curTaskExecution : taskExecutions) {
+            deleteTaskExecutionContext(curTaskExecution);
+        }
+
+        contextRepository.delete(taskExecution.getId());
+    }
+}
