30b709c7fb546caa7a707ea7e17d029f8e2195f2
Igor Beslic <igor.beslic@liferay.com>
2022-08-24 17:18:02 +0200

    ISSUE-19 TEST - update after spotbugs initiated updates

--- server/modules/libs/task/task-handler/task-handler-io/src/test/java/com/bytechef/task/handler/io/MkdirTest.java
+++ server/modules/libs/task/task-handler/task-handler-io/src/test/java/com/bytechef/task/handler/io/MkdirTest.java
@@ -17,8 +17,8 @@
 package com.bytechef.task.handler.io;
 
 import com.bytechef.atlas.task.execution.domain.SimpleTaskExecution;
+import com.bytechef.atlas.worker.task.exception.TaskExecutionException;
 import java.io.File;
-import java.nio.file.FileSystemException;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -37,7 +37,7 @@ public class MkdirTest {
 
     @Test
     public void test2() {
-        Assertions.assertThrows(FileSystemException.class, () -> {
+        Assertions.assertThrows(TaskExecutionException.class, () -> {
             Mkdir mkdir = new Mkdir();
             SimpleTaskExecution task = new SimpleTaskExecution();
             task.set("path", "/no/such/thing");
