33af77fea2a9035e3fe345970b160959190716c5
Ivica Cardic <ivica.cardic@liferay.com>
2022-10-01 11:33:31 +0200

    ISSUE-76 - coordinator-app - Move coordinator app to the services dir, add Consul and property support for service discovery

--- server/apps/services/coordinator-service-app/src/main/java/com/bytechef/config/CorsConfiguration.java
+++ server/apps/services/coordinator-service-app/src/main/java/com/bytechef/config/CorsConfiguration.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016-2018 the original author or authors.
+ * Copyright 2021 <your company/name>.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,19 +12,24 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
- * Modifications copyright (C) 2021 <your company/name>
  */
 
 package com.bytechef.config;
 
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
 
 /**
- * @author Arik Cohen
  * @author Ivica Cardic
  */
 @Configuration
-@EnableCaching
-public class CacheConfiguration {}
+@Profile("dev")
+public class CorsConfiguration implements WebFluxConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+        corsRegistry.addMapping("/**");
+    }
+}
