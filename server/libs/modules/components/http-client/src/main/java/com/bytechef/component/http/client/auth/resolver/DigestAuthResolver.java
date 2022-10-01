aa2f5326ce54a29030f5f54dc2baa60e3b408b56
Ivica Cardic <ivica.cardic@liferay.com>
2022-10-01 20:43:11 +0200

    ISSUE-76 - http-client - Adjust http client compponent

---
+++ server/libs/modules/components/http-client/src/main/java/com/bytechef/component/http/client/auth/resolver/DigestAuthResolver.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2021 <your company/name>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef.component.http.client.auth.resolver;
+
+import com.bytechef.component.http.client.constants.HttpClientConstants;
+import com.bytechef.hermes.component.ConnectionParameters;
+import com.github.mizosoft.methanol.Methanol;
+import java.net.Authenticator;
+import java.net.PasswordAuthentication;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author Ivica Cardic
+ */
+public class DigestAuthResolver implements AuthResolver {
+
+    @Override
+    public void apply(
+            Methanol.Builder builder,
+            Map<String, List<String>> headers,
+            Map<String, List<String>> queryParams,
+            ConnectionParameters connectionParameters) {
+
+        builder.authenticator(new Authenticator() {
+
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(
+                        connectionParameters.getParameter(HttpClientConstants.USERNAME),
+                        ((String) connectionParameters.getParameter(HttpClientConstants.PASSWORD)).toCharArray());
+            }
+        });
+    }
+}
