d3af9c9bd95e0e3853987c12d9fde116bdaeb8c3
Ivica Čardić <ivica.cardic@gmail.com>
2022-10-05 21:07:47 +0200

    ISSUE-76 - rest-impl - Add exception translator

--- server/libs/core/rest/rest-api/src/main/java/com/bytechef/rest/error/ErrorConstants.java
+++ server/libs/core/rest/rest-api/src/main/java/com/bytechef/rest/error/ErrorConstants.java
@@ -25,7 +25,7 @@ public final class ErrorConstants {
 
     public static final String ERR_CONCURRENCY_FAILURE = "error.concurrencyFailure";
     public static final String ERR_VALIDATION = "error.validation";
-    public static final String PROBLEM_BASE_URL = "https://www.lomvo.com/problem";
+    public static final String PROBLEM_BASE_URL = "https://www.bytechef.io/problem";
     public static final URI DEFAULT_TYPE = URI.create(PROBLEM_BASE_URL + "/problem-with-message");
     public static final URI CONSTRAINT_VIOLATION_TYPE = URI.create(PROBLEM_BASE_URL + "/constraint-violation");
 
