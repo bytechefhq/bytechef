e97640c74725b068315e656f361e962afa208370
Ivica Čardić <ivica.cardic@gmail.com>
2022-10-19 16:57:56 +0200

    ISSUE-76 - hermes-task-dispatcher-rest - Generate

---
+++ server/libs/hermes/hermes-task-dispatcher/hermes-task-dispatcher-rest/generated/src/main/java/com/bytechef/hermes/task/dispatcher/web/rest/model/PropertyOptionValueModel.java
@@ -0,0 +1,20 @@
+package com.bytechef.hermes.task.dispatcher.web.rest.model;
+
+import java.net.URI;
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import org.openapitools.jackson.nullable.JsonNullable;
+import java.time.OffsetDateTime;
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+
+import java.util.*;
+import javax.annotation.Generated;
+
+
+@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2022-10-27T16:28:51.543539+02:00[Europe/Zagreb]")
+public interface PropertyOptionValueModel {
+}
