d27c54ca7c20a5ab0de0bbe6523613b700739f36
Ivica Čardić <ivica.cardic@gmail.com>
2022-10-30 10:39:11 +0100

    ISSUE-76 - coordinator-service-app - Remove duplicated classes

--- server/apps/services/coordinator-service-app/src/main/java/com/bytechef/coordinator/config/DiscoveryClientConfiguration.java
+++ server/apps/services/coordinator-service-app/src/main/java/com/bytechef/coordinator/config/DiscoveryClientConfiguration.java
@@ -16,13 +16,10 @@
 
 package com.bytechef.coordinator.config;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
+import com.bytechef.autoconfigure.property.DiscoveryClientPropertyProperties;
+import com.bytechef.autoconfigure.property.PropertyDiscoveryClient;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.cloud.client.DefaultServiceInstance;
-import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
@@ -48,37 +45,7 @@ public class DiscoveryClientConfiguration {
 
         @Bean
         DiscoveryClient discoveryClient(DiscoveryClientPropertyProperties discoveryClientPropertyProperties) {
-            Map<String, List<ServiceInstance>> instances = Map.of(
-                    PLATFORM_SERVICE_APP,
-                    discoveryClientPropertyProperties.getPlatformServiceApp().getInstances().stream()
-                            .map(instance -> (ServiceInstance) new DefaultServiceInstance(
-                                    PLATFORM_SERVICE_APP + Objects.hash(instance.getHost(), instance.getPort()),
-                                    PLATFORM_SERVICE_APP,
-                                    instance.getHost(),
-                                    instance.getPort(),
-                                    false))
-                            .toList());
-
-            return new DiscoveryClient() {
-
-                @Override
-                public String description() {
-                    return "Coordinator Discovery Client";
-                }
-
-                @Override
-                public List<ServiceInstance> getInstances(String serviceId) {
-                    return instances.get(serviceId);
-                }
-
-                @Override
-                public List<String> getServices() {
-                    return discoveryClientPropertyProperties.getPlatformServiceApp().getInstances().stream()
-                            .map(instance ->
-                                    PLATFORM_SERVICE_APP + Objects.hash(instance.getHost(), instance.getPort()))
-                            .toList();
-                }
-            };
+            return new PropertyDiscoveryClient(discoveryClientPropertyProperties, PLATFORM_SERVICE_APP);
         }
     }
 }
