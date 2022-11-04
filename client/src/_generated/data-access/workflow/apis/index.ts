68480e397765efa694be252c28c6fa9215e14a08
Ivica Čardić <ivica.cardic@gmail.com>
2022-11-04 12:11:22 +0100

    ISSUE-76 - client - Generate rest clients

--- client/src/_generated/data-access/workflow/apis/index.ts
+++
@@ -1,6 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-export * from './CacheControllerApi';
-export * from './JobControllerApi';
-export * from './TaskExecutionControllerApi';
-export * from './WorkflowControllerApi';
---
+++ client/src/data-access/workflow/apis/index.ts
@@ -0,0 +1,6 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './CachesApi';
+export * from './JobsApi';
+export * from './TaskExecutionsApi';
+export * from './WorkflowsApi';
