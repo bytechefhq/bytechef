4e870de63fa2a195940964f0a63476cb3cd39c51
Ivica Čardić <ivica.cardic@gmail.com>
2022-11-12 08:10:03 +0100

    ISSUE-76 - hermes-connection-service - Update test infra

--- server/libs/hermes/hermes-connection/hermes-connection-service/src/test/java/com/bytechef/hermes/connection/repository/ConnectionRepositoryIntTest.java
+++ server/libs/hermes/hermes-connection/hermes-connection-service/src/test/java/com/bytechef/hermes/connection/repository/ConnectionRepositoryIntTest.java
@@ -16,30 +16,26 @@
 
 package com.bytechef.hermes.connection.repository;
 
+import com.bytechef.hermes.connection.config.ConnectionIntTestConfiguration;
 import com.bytechef.hermes.connection.domain.Connection;
+import com.bytechef.test.extension.PostgresTestContainerExtension;
 import java.util.Map;
 import java.util.stream.StreamSupport;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 /**
  * @author Ivica Cardic
  */
-@SpringBootTest
+@ExtendWith(PostgresTestContainerExtension.class)
+@SpringBootTest(classes = ConnectionIntTestConfiguration.class)
 public class ConnectionRepositoryIntTest {
 
     @Autowired
     private ConnectionRepository connectionRepository;
-
-    @BeforeEach
-    public void beforeEach() {
-        for (Connection connection : connectionRepository.findAll()) {
-            connectionRepository.deleteById(connection.getId());
-        }
-    }
 
     @Test
     public void testCreate() {
