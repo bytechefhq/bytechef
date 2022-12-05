0c8bb3cdd03948bd14dccfa5445034ceea0b075f
Marta Jurcevic <marta.raptavi997@gmail.com>
2022-12-05 14:29:36 +0100

    reslove issue

--- client/src/pages/integrations/Integrations.tsx
+++ client/src/pages/integrations/Integrations.tsx
@@ -1,14 +1,15 @@
 import {SidebarContentLayout} from '../../components/Layouts/SidebarContentLayout';
 import {PageHeader} from 'components/PageHeader/PageHeader';
+import {IntegrationList} from './IntegrationList';
 
 export const Integrations = (): JSX.Element => {
-	return (
-		<SidebarContentLayout title={'Integrations'}>
-			<PageHeader
-				subTitle="All Integrations"
-				buttonTitle="New Integration"
-			/>
-			{/* <IntegrationList /> */}
-		</SidebarContentLayout>
-	);
+    return (
+        <SidebarContentLayout title={'Integrations'}>
+            <PageHeader
+                subTitle="All Integrations"
+                buttonTitle="New Integration"
+            />
+            <IntegrationList />
+        </SidebarContentLayout>
+    );
 };
