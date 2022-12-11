48058fc5bba0f8042b525081a3a06519f8ed399a
Ivica Čardić <ivica.cardic@gmail.com>
2022-12-11 17:08:15 +0100

    ISSUE-76 - test-support - Reorganize test support

---
+++ server/libs/hermes/hermes-task-dispatcher/hermes-task-dispatcher-test-int-support/src/main/java/com/bytechef/hermes/task/dispatcher/test/config/TaskDispatcherIntTestConfiguration.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2021 <your company/name>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef.hermes.task.dispatcher.test.config;
+
+import com.bytechef.atlas.repository.config.WorkflowRepositoryConfiguration;
+import com.bytechef.atlas.repository.resource.config.ResourceWorkflowRepositoryConfiguration;
+import com.bytechef.atlas.sync.executor.config.WorkflowExecutorConfiguration;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
+
+/**
+ * @author Ivica Cardic
+ */
+@ComponentScan("com.bytechef.hermes.task.dispatcher")
+@EnableAutoConfiguration(
+        exclude = {DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class})
+@Import({
+    ResourceWorkflowRepositoryConfiguration.class,
+    WorkflowExecutorConfiguration.class,
+    WorkflowRepositoryConfiguration.class
+})
+@SpringBootConfiguration
+public class TaskDispatcherIntTestConfiguration {
+
+    @EnableCaching
+    @TestConfiguration
+    public static class CacheConfiguration {}
+}
