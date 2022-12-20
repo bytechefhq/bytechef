db826b864e675cda42056057bb0e1c99a065f606
Krešimir Čoko <kresimircoko@gmail.com>
2022-12-20 23:00:39 +0100

    ISSUE-94 Add Modal component and use it

--- client/src/pages/integrations/Integrations.tsx
+++ client/src/pages/integrations/Integrations.tsx
@@ -1,14 +1,79 @@
+/* eslint-disable tailwindcss/no-custom-classname */
+
+import PageHeader from 'components/PageHeader/PageHeader';
+import Modal from 'components/Modal/Modal';
+
 import {SidebarContentLayout} from '../../components/Layouts/SidebarContentLayout';
 import {IntegrationList} from './IntegrationList';
 import React from 'react';
-import Button from '../../components/Button/Button';
+import Select from 'react-select';
+
+const options = [
+    {value: 'chocolate', label: 'Chocolate'},
+    {value: 'strawberry', label: 'Strawberry'},
+    {value: 'vanilla', label: 'Vanilla'},
+];
 
 const Integrations: React.FC = () => (
-    <SidebarContentLayout
-        title={'Integrations'}
-        subTitle="All Integrations"
-        topRight={<Button label="New Integration" />}
-    >
+    <SidebarContentLayout title={'Integrations'} subTitle={''}>
+        <PageHeader
+            title="All Integrations"
+            buttonLabel="New Integration"
+            right={
+                <Modal
+                    cancelButtonLabel="Cancel"
+                    confirmButtonLabel="Create"
+                    description="Use this to create your integration which will contain related workflows"
+                    triggerLabel="Create Integration"
+                    title="Create Integration"
+                >
+                    <fieldset className="Fieldset">
+                        <label className="Label" htmlFor="name">
+                            Name
+                        </label>
+
+                        <input
+                            className="Input"
+                            id="name"
+                            placeholder="John Doe"
+                        />
+                    </fieldset>
+
+                    <fieldset className="Fieldset">
+                        <label className="Label" htmlFor="username">
+                            Description
+                        </label>
+
+                        <textarea
+                            className="Input"
+                            id="username"
+                            placeholder="Cute description of your integration"
+                        />
+                    </fieldset>
+
+                    <fieldset className="Fieldset">
+                        <label htmlFor="category" className="Label">
+                            Category
+                        </label>
+
+                        <input
+                            className="Input"
+                            id="category"
+                            placeholder="Marketing, Sales, Social Media..."
+                        />
+                    </fieldset>
+
+                    <fieldset className="Fieldset">
+                        <label htmlFor="category" className="Label">
+                            Tags
+                        </label>
+
+                        <Select options={options} isMulti={true} />
+                    </fieldset>
+                </Modal>
+            }
+        />
+
         <IntegrationList />
     </SidebarContentLayout>
 );
