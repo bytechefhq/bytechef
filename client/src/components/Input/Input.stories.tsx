1ea3245263fcc810e1b877a681e85027bf83dabc
Krešimir Čoko <kresimircoko@gmail.com>
2022-12-30 12:37:00 +0100

    ISSUE-94 Fix failing CI

--- client/src/components/Input/Input.stories.tsx
+++ client/src/components/Input/Input.stories.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {ComponentStory, ComponentMeta} from '@storybook/react';
 
-import {Input} from './Input';
+import Input from './Input';
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
