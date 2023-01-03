038b554e1249fed7bd33d297b9a4b6301846e4ec
Krešimir Čoko <kresimircoko@gmail.com>
2023-01-03 19:00:36 +0100

    ISSUE-144 Add Button stories

--- client/src/components/Button/Button.stories.tsx
+++ client/src/components/Button/Button.stories.tsx
@@ -1,41 +1,57 @@
 import React from 'react';
 import {ComponentStory, ComponentMeta} from '@storybook/react';
+import Button from './Button';
+import {MixIcon} from '@radix-ui/react-icons';
 
-import {Button} from './Button';
-
-// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-    title: 'Example/Button',
+    title: 'Button',
     component: Button,
-    // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-    argTypes: {
-        backgroundColor: {control: 'color'},
-    },
 } as ComponentMeta<typeof Button>;
 
-// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
 
 export const Primary = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
+
 Primary.args = {
-    primary: true,
     label: 'Button',
 };
 
 export const Secondary = Template.bind({});
+
 Secondary.args = {
     label: 'Button',
+    displayType: 'secondary',
+};
+
+export const Danger = Template.bind({});
+
+Danger.args = {
+    label: 'Button',
+    displayType: 'danger',
+};
+export const Unstyled = Template.bind({});
+
+Unstyled.args = {
+    label: 'Button',
+    displayType: 'unstyled',
 };
 
 export const Large = Template.bind({});
+
 Large.args = {
     size: 'large',
     label: 'Button',
 };
 
 export const Small = Template.bind({});
+
 Small.args = {
     size: 'small',
     label: 'Button',
+};
+export const Icon = Template.bind({});
+
+Icon.storyName = 'Button with an icon';
+Icon.args = {
+    icon: <MixIcon />,
 };
