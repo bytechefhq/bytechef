1c00298e5d339788b346e474586cb3c15431ecf1
Ivica Čardić <ivica.cardic@gmail.com>
2023-01-07 17:50:12 +0100

    ISSUE-76 - atlas-sync-executor - Reorganize

--- server/libs/atlas/atlas-sync-executor/src/main/java/com/bytechef/atlas/sync/executor/config/WorkflowExecutorConfiguration.java
+++ server/libs/atlas/atlas-sync-executor/src/main/java/com/bytechef/atlas/sync/executor/config/WorkflowExecutorConfiguration.java
@@ -20,9 +20,6 @@ package com.bytechef.atlas.sync.executor.config;
 import com.bytechef.atlas.coordinator.event.EventListener;
 import com.bytechef.atlas.coordinator.event.EventListenerChain;
 import com.bytechef.atlas.event.EventPublisher;
-import com.bytechef.atlas.message.broker.MessageBroker;
-import com.bytechef.atlas.message.broker.sync.SyncMessageBroker;
-import com.bytechef.atlas.repository.WorkflowCrudRepository;
 import com.bytechef.atlas.repository.WorkflowRepository;
 import com.bytechef.atlas.repository.memory.InMemoryContextRepository;
 import com.bytechef.atlas.repository.memory.InMemoryCounterRepository;
@@ -41,16 +38,18 @@ import com.bytechef.atlas.service.impl.WorkflowServiceImpl;
 import com.bytechef.atlas.sync.executor.WorkflowExecutor;
 import com.bytechef.atlas.worker.task.handler.TaskHandler;
 import com.bytechef.atlas.worker.task.handler.TaskHandlerRegistrar;
-import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
-import org.springframework.cache.CacheManager;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class WorkflowExecutorConfiguration implements InitializingBean {
@@ -94,52 +93,29 @@ public class WorkflowExecutorConfiguration implements InitializingBean {
     }
 
     @Bean
-    WorkflowService workflowService(
-        CacheManager cacheManager, List<WorkflowCrudRepository> workflowCrudRepositories,
-        List<WorkflowRepository> workflowRepositories) {
-        return new WorkflowServiceImpl(cacheManager, workflowCrudRepositories, workflowRepositories);
+    WorkflowService workflowService(List<WorkflowRepository> workflowRepositories) {
+        return new WorkflowServiceImpl(new ConcurrentMapCacheManager(), Collections.emptyList(), workflowRepositories);
     }
 
     @Bean
     WorkflowExecutor workflowExecutor(
-        ContextService contextService,
-        CounterService counterService,
-        JobService jobService,
-        EventPublisher eventPublisher,
-        TaskExecutionService taskExecutionService,
-        @Autowired(required = false) Map<String, TaskHandler<?>> taskHandlerMap,
-        WorkflowService workflowService) {
+        ContextService contextService, CounterService counterService, EventPublisher eventPublisher,
+        JobService jobService, TaskExecutionService taskExecutionService,
+        @Autowired(required = false) Map<String, TaskHandler<?>> taskHandlerMap, WorkflowService workflowService) {
+
         return new WorkflowExecutor(
-            contextService,
-            counterService,
-            jobService,
-            eventPublisher,
-            taskExecutionService,
-            taskHandlerMap == null ? Map.of() : taskHandlerMap,
-            workflowService);
+            contextService, counterService, jobService, eventPublisher, taskExecutionService,
+            taskHandlerMap == null ? Map.of() : taskHandlerMap, workflowService);
     }
 
     @Configuration
     public static class EventConfiguration {
 
         @Bean
-        @Primary
-        EventListenerChain eventListener(List<EventListener> eventListeners) {
-            return new EventListenerChain(eventListeners);
-        }
+        EventPublisher eventPublisher(List<EventListener> eventListeners) {
+            EventListener eventListener = new EventListenerChain(eventListeners);
 
-        @Bean
-        EventPublisher eventPublisher(EventListener eventListener) {
             return eventListener::onApplicationEvent;
-        }
-    }
-
-    @Configuration
-    public static class MessageBrokerConfiguration {
-
-        @Bean
-        MessageBroker messageBroker() {
-            return new SyncMessageBroker();
         }
     }
 }
