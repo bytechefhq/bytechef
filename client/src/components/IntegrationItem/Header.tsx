f320f4cdf3329a1d33331a3808a9ac94b2c2d598
Marta Jurcevic <marta.raptavi997@gmail.com>
2023-01-13 10:27:28 +0100

    #129 deleting dots

--- client/src/components/IntegrationItem/Header.tsx
+++ client/src/components/IntegrationItem/Header.tsx
@@ -10,13 +10,13 @@ const Header: React.FC<{
 }> = ({id, name, status}) => {
     return (
         <div className="grid grid-cols-3 gap-4">
-            <span className="... col-span-2 flex flex-row space-x-4 ">
+            <span className="col-span-2 flex flex-row space-x-4 ">
                 <Name name={name} />
 
                 <Status status={status} />
             </span>
 
-            <div className="... grid justify-items-end">
+            <div className="grid justify-items-end">
                 <Dropdown id={id} />
             </div>
         </div>
