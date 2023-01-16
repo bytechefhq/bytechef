110a286b7c5920dbb16c806f5cd055acc10ab74c
Ivica Čardić <ivica.cardic@gmail.com>
2023-01-16 13:22:19 +0100

    #182 - hermes-component - Apply services

--- server/libs/hermes/hermes-component/hermes-component-rest/src/main/java/com/bytechef/hermes/component/web/rest/ComponentDefinitionController.java
+++ server/libs/hermes/hermes-component/hermes-component-rest/src/main/java/com/bytechef/hermes/component/web/rest/ComponentDefinitionController.java
@@ -18,8 +18,9 @@
 package com.bytechef.hermes.component.web.rest;
 
 import com.bytechef.autoconfigure.annotation.ConditionalOnApi;
-import com.bytechef.hermes.component.ComponentDefinitionFactory;
-import com.bytechef.hermes.component.definition.ComponentDefinition;
+import com.bytechef.hermes.component.service.ComponentDefinitionService;
+import com.bytechef.hermes.component.web.rest.model.GetComponentDefinitionRequestModel;
+import com.bytechef.hermes.component.web.rest.model.GetComponentDefinitionsRequestModel;
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -27,9 +28,12 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import java.util.List;
+
 import java.util.stream.Collectors;
+
+import org.springframework.core.convert.ConversionService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -47,21 +51,24 @@ import reactor.core.publisher.Mono;
 @Tag(name = "component-definitions")
 public class ComponentDefinitionController {
 
-    private final List<ComponentDefinitionFactory> componentDefinitionFactories;
+    private final ConversionService conversionService;
+    private final ComponentDefinitionService componentDefinitionService;
 
-    public ComponentDefinitionController(List<ComponentDefinitionFactory> componentDefinitionFactories) {
-        this.componentDefinitionFactories = componentDefinitionFactories;
+    public ComponentDefinitionController(ConversionService conversionService,
+        ComponentDefinitionService componentDefinitionService) {
+        this.conversionService = conversionService;
+        this.componentDefinitionService = componentDefinitionService;
     }
 
     /**
-     * GET /definitions/components Returns all component definitions
+     * GET /definitions/components Get all component definitions
      *
      * @return OK (status code 200)
      */
     @Operation(
-        description = "Returns all component definitions.",
+        description = "Get all component definitions.",
         operationId = "getComponentDefinitions",
-        summary = "Returns all component definitions.",
+        summary = "Get all component definitions.",
         tags = {
             "component-definitions"
         },
@@ -72,7 +79,7 @@ public class ComponentDefinitionController {
                 content = {
                     @Content(
                         mediaType = "application/json",
-                        schema = @Schema(implementation = ComponentDefinition.class))
+                        schema = @Schema(implementation = GetComponentDefinitionsRequestModel.class))
                 })
         })
     @RequestMapping(
@@ -81,10 +88,52 @@ public class ComponentDefinitionController {
         produces = {
             "application/json"
         })
-    public Mono<ResponseEntity<Flux<ComponentDefinition>>> getComponentDefinitions(
+    public Mono<ResponseEntity<Flux<GetComponentDefinitionsRequestModel>>> getComponentDefinitions(
         @Parameter(hidden = true) final ServerWebExchange exchange) {
-        return Mono.just(ResponseEntity.ok(Flux.fromIterable(componentDefinitionFactories.stream()
-            .map(ComponentDefinitionFactory::getDefinition)
-            .collect(Collectors.toList()))));
+        return Mono.just(
+            ResponseEntity.ok(
+                componentDefinitionService.getComponentDefinitions()
+                    .collectList()
+                    .flatMapMany(Flux::fromIterable)
+                    .map(componentDefinition -> conversionService.convert(componentDefinition,
+                        GetComponentDefinitionsRequestModel.class))
+                    .collect(Collectors.toList())
+                    .flatMapMany(Flux::fromIterable)));
+    }
+
+    /**
+     * GET /definitions/components/{name} Returns a component definition by name
+     *
+     * @return OK (status code 200)
+     */
+    @Operation(
+        description = "Get a component definition by name.",
+        operationId = "getComponentDefinition",
+        summary = "Get a component definition by name.",
+        tags = {
+            "component-definitions"
+        },
+        responses = {
+            @ApiResponse(responseCode = "200", description = "OK", content = {
+                @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = GetComponentDefinitionRequestModel.class))
+            })
+        })
+    @RequestMapping(
+        method = RequestMethod.GET,
+        value = "/definitions/components/{name}",
+        produces = {
+            "application/json"
+        })
+    public Mono<ResponseEntity<GetComponentDefinitionRequestModel>> getComponentDefinition(
+        @Parameter(
+            name = "name", description = "The name of the component to get.",
+            required = true) @PathVariable("name") String name,
+        @Parameter(hidden = true) final ServerWebExchange exchange) {
+        return componentDefinitionService.getComponentDefinition(name)
+            .map(componentDefinition -> conversionService.convert(componentDefinition,
+                GetComponentDefinitionRequestModel.class))
+            .map(ResponseEntity::ok);
     }
 }
