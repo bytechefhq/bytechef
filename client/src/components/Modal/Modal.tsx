c3ca9a0c7ba1077611f08fa4797bb3f76594d7e2
Krešimir Čoko <kresimircoko@gmail.com>
2023-01-16 11:20:40 +0100

    #125 Add frontend validation to the form that creates the integration

--- client/src/components/Modal/Modal.tsx
+++ client/src/components/Modal/Modal.tsx
@@ -13,13 +13,16 @@ import {
 } from '@radix-ui/react-dialog';
 import {Cross1Icon} from '@radix-ui/react-icons';
 import Button from '../Button/Button';
-import {Fragment, useState} from 'react';
+import {Dispatch, Fragment, SetStateAction} from 'react';
 
 const Modal: React.FC<{
     children: React.ReactNode;
     confirmButtonLabel: string;
     description: string;
     handleConfirmButtonClick: () => void;
+    form: boolean;
+    isOpen: boolean;
+    setIsOpen: Dispatch<SetStateAction<boolean>>;
     title: string;
     triggerLabel: string;
 }> = ({
@@ -27,57 +30,58 @@ const Modal: React.FC<{
     confirmButtonLabel,
     description,
     handleConfirmButtonClick,
+    form,
+    isOpen,
+    setIsOpen,
     title,
     triggerLabel,
-}) => {
-    const [isOpen, setIsOpen] = useState(false);
+}) => (
+    <Root open={isOpen} onOpenChange={setIsOpen}>
+        <Trigger asChild>
+            <Button label={triggerLabel} />
+        </Trigger>
 
-    return (
-        <Root open={isOpen} onOpenChange={setIsOpen}>
-            <Trigger asChild>
-                <Button label={triggerLabel} />
-            </Trigger>
+        <Portal forceMount>
+            <Transition.Root show={isOpen}>
+                <Transition.Child
+                    as={Fragment}
+                    enter="ease-out duration-300"
+                    enterFrom="opacity-0"
+                    enterTo="opacity-100"
+                    leave="ease-in duration-200"
+                    leaveFrom="opacity-100"
+                    leaveTo="opacity-0"
+                >
+                    <Overlay
+                        forceMount
+                        className="fixed inset-0 z-20 bg-black/50"
+                    />
+                </Transition.Child>
 
-            <Portal forceMount>
-                <Transition.Root show={isOpen}>
-                    <Transition.Child
-                        as={Fragment}
-                        enter="ease-out duration-300"
-                        enterFrom="opacity-0"
-                        enterTo="opacity-100"
-                        leave="ease-in duration-200"
-                        leaveFrom="opacity-100"
-                        leaveTo="opacity-0"
+                <Transition.Child
+                    as={Fragment}
+                    enter="ease-out duration-300"
+                    enterFrom="opacity-0 scale-95"
+                    enterTo="opacity-100 scale-100"
+                    leave="ease-in duration-200"
+                    leaveFrom="opacity-100 scale-100"
+                    leaveTo="opacity-0 scale-95"
+                >
+                    <Content
+                        forceMount
+                        className="fixed top-[50%] left-[50%] z-50 w-[95vw] max-w-md translate-x-[-50%] translate-y-[-50%] rounded-lg bg-white p-4 focus:outline-none focus-visible:ring focus-visible:ring-blue-500/75 dark:bg-gray-800 md:w-full"
                     >
-                        <Overlay
-                            forceMount
-                            className="fixed inset-0 z-20 bg-black/50"
-                        />
-                    </Transition.Child>
+                        <Title className="text-sm font-medium text-gray-900 dark:text-gray-100">
+                            {title}
+                        </Title>
 
-                    <Transition.Child
-                        as={Fragment}
-                        enter="ease-out duration-300"
-                        enterFrom="opacity-0 scale-95"
-                        enterTo="opacity-100 scale-100"
-                        leave="ease-in duration-200"
-                        leaveFrom="opacity-100 scale-100"
-                        leaveTo="opacity-0 scale-95"
-                    >
-                        <Content
-                            forceMount
-                            className="fixed top-[50%] left-[50%] z-50 w-[95vw] max-w-md translate-x-[-50%] translate-y-[-50%] rounded-lg bg-white p-4 focus:outline-none focus-visible:ring focus-visible:ring-blue-500/75 dark:bg-gray-800 md:w-full"
-                        >
-                            <Title className="text-sm font-medium text-gray-900 dark:text-gray-100">
-                                {title}
-                            </Title>
+                        <Description className="mt-2 text-sm font-normal text-gray-700 dark:text-gray-400">
+                            {description}
+                        </Description>
 
-                            <Description className="mt-2 text-sm font-normal text-gray-700 dark:text-gray-400">
-                                {description}
-                            </Description>
+                        {children}
 
-                            <form className="mt-2 space-y-2">{children}</form>
-
+                        {!form && (
                             <div className="mt-4 flex justify-end">
                                 <Close asChild>
                                     <Button
@@ -86,16 +90,16 @@ const Modal: React.FC<{
                                     />
                                 </Close>
                             </div>
+                        )}
 
-                            <Close className="absolute top-3.5 right-3.5 inline-flex items-center justify-center rounded-full p-1 focus:outline-none focus-visible:ring focus-visible:ring-blue-500/75">
-                                <Cross1Icon className="h-4 w-4 text-gray-500 hover:text-gray-700 dark:text-gray-500 dark:hover:text-gray-400" />
-                            </Close>
-                        </Content>
-                    </Transition.Child>
-                </Transition.Root>
-            </Portal>
-        </Root>
-    );
-};
+                        <Close className="absolute top-3.5 right-3.5 inline-flex items-center justify-center rounded-full p-1 focus:outline-none focus-visible:ring focus-visible:ring-blue-500/75">
+                            <Cross1Icon className="h-4 w-4 text-gray-500 hover:text-gray-700 dark:text-gray-500 dark:hover:text-gray-400" />
+                        </Close>
+                    </Content>
+                </Transition.Child>
+            </Transition.Root>
+        </Portal>
+    </Root>
+);
 
 export default Modal;
--- client/src/components/MultiSelect/MultiSelect.tsx
+++ client/src/components/MultiSelect/MultiSelect.tsx
@@ -1,5 +1,6 @@
 import {ExclamationCircleIcon} from '@heroicons/react/24/outline';
 import cx from 'classnames';
+import React, {forwardRef} from 'react';
 import CreatableSelect from 'react-select/creatable';
 import './MultiSelect.css';
 
@@ -12,49 +13,53 @@ type MultiSelectProps = {
     /* eslint-disable @typescript-eslint/no-explicit-any */
     onChange?: (value: any) => void;
     onCreateOption?: (value: string) => void;
-};
+} & CreatableSelect;
 
-const MultiSelect = ({error, label, name, ...props}: MultiSelectProps) => (
-    <fieldset>
-        <label
-            htmlFor={name}
-            className="text-xs font-medium text-gray-700 dark:text-gray-400"
-        >
-            {label}
-        </label>
+const MultiSelect = forwardRef<CreatableSelect, MultiSelectProps>(
+    ({error, label, name, ...props}) => (
+        <fieldset>
+            <label
+                htmlFor={name}
+                className="text-xs font-medium text-gray-700 dark:text-gray-400"
+            >
+                {label}
+            </label>
 
-        <div
-            className={cx([
-                'mt-1 ',
-                error ? 'relative rounded-md shadow-sm' : null,
-            ])}
-        >
-            <CreatableSelect
-                classNamePrefix="react-select"
-                isMulti
-                {...props}
-            />
+            <div
+                className={cx([
+                    'mt-1 ',
+                    error ? 'relative rounded-md shadow-sm' : null,
+                ])}
+            >
+                <CreatableSelect
+                    classNamePrefix="react-select"
+                    isMulti
+                    {...props}
+                />
+
+                {error && (
+                    <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
+                        <ExclamationCircleIcon
+                            className="h-5 w-5 text-red-500"
+                            aria-hidden="true"
+                        />
+                    </div>
+                )}
+            </div>
 
             {error && (
-                <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
-                    <ExclamationCircleIcon
-                        className="h-5 w-5 text-red-500"
-                        aria-hidden="true"
-                    />
-                </div>
+                <p
+                    role="alert"
+                    className="mt-2 text-sm text-red-600"
+                    id={name + '-error'}
+                >
+                    This field is required
+                </p>
             )}
-        </div>
-
-        {error && (
-            <p
-                role="alert"
-                className="mt-2 text-sm text-red-600"
-                id={name + '-error'}
-            >
-                {error}
-            </p>
-        )}
-    </fieldset>
+        </fieldset>
+    )
 );
+
+MultiSelect.displayName = 'MultiSelect';
 
 export default MultiSelect;
