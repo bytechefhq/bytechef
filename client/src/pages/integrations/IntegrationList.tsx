f927fb74fd9915e548cdb9b59b402a93d850a590
Krešimir Čoko <kresimircoko@gmail.com>
2023-01-18 08:35:47 +0100

    #125 SF

--- client/src/pages/integrations/IntegrationList.tsx
+++ client/src/pages/integrations/IntegrationList.tsx
@@ -6,32 +6,28 @@ export const IntegrationList: React.FC = () => {
     const {isLoading, error, data: items} = useGetIntegrations();
 
     return (
-        <>
-            <div>
-                {isLoading && 'Loading...'}
+        <div>
+            {isLoading && 'Loading...'}
 
-                {error &&
-                    !isLoading &&
-                    `An error has occurred: ${error.message}`}
+            {error && !isLoading && `An error has occurred: ${error.message}`}
 
-                {!isLoading &&
-                    !error &&
-                    items &&
-                    items.map((integration) => (
-                        <IntegrationItem
-                            key={integration.id}
-                            category={integration.category}
-                            id={integration.id}
-                            name={integration.name}
-                            description={integration.description}
-                            tags={integration.tags}
-                            workflowIds={integration.workflowIds}
-                            date={integration.lastModifiedDate}
-                            status={false} // missing api
-                            button={''}
-                        />
-                    ))}
-            </div>
-        </>
+            {!isLoading &&
+                !error &&
+                items &&
+                items.map((integration) => (
+                    <IntegrationItem
+                        key={integration.id}
+                        category={integration.category}
+                        id={integration.id}
+                        name={integration.name}
+                        description={integration.description}
+                        tags={integration.tags}
+                        workflowIds={integration.workflowIds}
+                        date={integration.lastModifiedDate}
+                        status={false} // missing api
+                        button={''}
+                    />
+                ))}
+        </div>
     );
 };
