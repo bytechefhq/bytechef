c639c4273902efdc3f9b6e5880e294d9832651ed
Ivica Čardić <ivica.cardic@gmail.com>
2023-01-21 08:29:19 +0100

    #188 - client - generate

--- client/src/data-access/integration/models/index.ts
+++ client/src/data-access/integration/models/index.ts
@@ -4,3 +4,6 @@ export * from './CategoryModel';
 export * from './IntegrationModel';
 export * from './PostIntegrationWorkflowRequestModel';
 export * from './TagModel';
+export * from './WorkflowFormatModel';
+export * from './WorkflowModel';
+export * from './WorkflowTaskModel';
