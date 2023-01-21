fa38322958550fcee0f2ae5cde120aebe2cedcfb
Ivica Čardić <ivica.cardic@gmail.com>
2023-01-21 16:45:02 +0100

    #186 - hermes-integration-rest - Generate

--- server/libs/hermes/hermes-integration/hermes-integration-rest/generated/src/main/java/com/bytechef/hermes/integration/web/rest/model/CategoryModel.java
+++ server/libs/hermes/hermes-integration/hermes-integration-rest/generated/src/main/java/com/bytechef/hermes/integration/web/rest/model/CategoryModel.java
@@ -23,7 +23,7 @@ import jakarta.annotation.Generated;
 
 @Schema(name = "Category", description = "An integration category.")
 @JsonTypeName("Category")
-@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2023-01-18T09:51:05.328763+01:00[Europe/Zagreb]")
+@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2023-01-20T23:05:10.694831+01:00[Europe/Zagreb]")
 public class CategoryModel {
 
   @JsonProperty("createdBy")
