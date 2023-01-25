55fa24e352b1d0c004781f6b7d89e95d44f0dda0
Ivica Čardić <ivica.cardic@gmail.com>
2023-01-25 07:29:12 +0100

    #68 - cli - Generate

--- server/libs/modules/components/pipedrive/src/main/java/com/bytechef/component/pipedrive/AbstractPipedriveComponentHandler.java
+++ server/libs/modules/components/pipedrive/src/main/java/com/bytechef/component/pipedrive/AbstractPipedriveComponentHandler.java
@@ -92,7 +92,7 @@ public abstract class AbstractPipedriveComponentHandler implements RestComponent
                         .toLowerCase(),
                     AuthorizationType.OAUTH2_AUTHORIZATION_CODE)
                         .display(
-                            display("OAuth2 Authorization code"))
+                            display("Authorization Code"))
                         .properties(
                             string(CLIENT_ID)
                                 .label("Client Id")
