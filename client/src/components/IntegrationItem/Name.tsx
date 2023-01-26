41e838909da445588322dab66530ff10e0199a37
Marta Jurcevic <marta.raptavi997@gmail.com>
2023-01-26 17:58:30 +0100

    #129 using HoverCard for description

--- client/src/components/IntegrationItem/Name.tsx
+++ client/src/components/IntegrationItem/Name.tsx
@@ -1,5 +1,40 @@
 import React from 'react';
+import * as HoverCardPrimitive from '@radix-ui/react-hover-card';
+import {clsx} from 'clsx';
 
-export const Name: React.FC<{name: string}> = ({name}) => {
-    return <span>{name}</span>;
+export const Name: React.FC<{name: string; description: string}> = ({
+    name,
+    description,
+}) => {
+    return (
+        <HoverCardPrimitive.Root>
+            <HoverCardPrimitive.Trigger asChild>
+                <span
+                    className={clsx(
+                        'items-center justify-center rounded-full bg-white dark:bg-gray-900'
+                    )}
+                >
+                    {name}
+                </span>
+            </HoverCardPrimitive.Trigger>
+
+            <HoverCardPrimitive.Content
+                align="center"
+                sideOffset={4}
+                className={clsx(
+                    'max-w-md rounded-lg p-4 md:w-full',
+                    'bg-white dark:bg-gray-800',
+                    'border'
+                )}
+            >
+                <div className="flex h-full w-full space-x-4">
+                    <div>
+                        <p className="mt-1 text-sm font-normal text-gray-700 dark:text-gray-400">
+                            {description}
+                        </p>
+                    </div>
+                </div>
+            </HoverCardPrimitive.Content>
+        </HoverCardPrimitive.Root>
+    );
 };
