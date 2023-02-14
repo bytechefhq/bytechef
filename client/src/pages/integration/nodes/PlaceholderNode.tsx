426e30e7b2f4dcf12c0a845c1e874d998b1dd2c8
Ivica Čardić <ivica.cardic@gmail.com>
2023-02-14 12:53:29 +0100

    #28 - client Initial commit for replacing ContextualMenu with PopoverMenu

--- client/src/pages/integration/nodes/PlaceholderNode.tsx
+++ client/src/pages/integration/nodes/PlaceholderNode.tsx
@@ -3,6 +3,7 @@ import {Handle, Position, NodeProps} from 'reactflow';
 
 import styles from './NodeTypes.module.css';
 import usePlaceholderClick from '../hooks/usePlaceholderClick';
+import PopoverMenu from '../PopoverMenu';
 
 const PlaceholderNode = ({id, data}: NodeProps) => {
     // see the hook implementation for details of the click handler
@@ -10,27 +11,29 @@ const PlaceholderNode = ({id, data}: NodeProps) => {
     const onClick = usePlaceholderClick(id);
 
     return (
-        <div
-            onClick={onClick}
-            className="mx-[26px] flex h-[20px] w-[20px] cursor-pointer items-center justify-center rounded-md border border-gray-300 bg-gray-300 font-bold text-white shadow-none hover:bg-gray-500"
-            title="Click to add a component"
-        >
-            {data.label}
+        <PopoverMenu>
+            <div
+                onClick={onClick}
+                className="mx-[26px] flex h-[20px] w-[20px] cursor-pointer items-center justify-center rounded-md border border-gray-300 bg-gray-300 font-bold text-white shadow-none hover:bg-gray-500"
+                title="Click to add a component"
+            >
+                {data.label}
 
-            <Handle
-                className={styles.handle}
-                type="target"
-                position={Position.Top}
-                isConnectable={false}
-            />
+                <Handle
+                    className={styles.handle}
+                    type="target"
+                    position={Position.Top}
+                    isConnectable={false}
+                />
 
-            <Handle
-                className={styles.handle}
-                type="source"
-                position={Position.Bottom}
-                isConnectable={false}
-            />
-        </div>
+                <Handle
+                    className={styles.handle}
+                    type="source"
+                    position={Position.Bottom}
+                    isConnectable={false}
+                />
+            </div>
+        </PopoverMenu>
     );
 };
 
