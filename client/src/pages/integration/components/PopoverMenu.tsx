de3021004ecce0df7f12b84f43691b8b33e99672
Krešimir Čoko <kresimircoko@gmail.com>
2023-02-14 21:10:01 +0100

    #28 Reorganize the integration/ folder

---
+++ client/src/pages/integration/components/PopoverMenu.tsx
@@ -0,0 +1,45 @@
+import React, {ReactNode} from 'react';
+import {Arrow, Content, Root, Trigger} from '@radix-ui/react-popover';
+import {
+    useGetComponentsQuery,
+    useGetFlowControlsQuery,
+} from 'queries/integration.queries';
+import ContextualMenu from '../nodes/ContextualMenu';
+
+interface PopoverMenuProps {
+    children: ReactNode;
+    id?: string;
+}
+
+const PopoverMenu = ({children, id}: PopoverMenuProps) => {
+    const {data: components} = useGetComponentsQuery();
+
+    const {data: flowControls} = useGetFlowControlsQuery();
+
+    return (
+        <div className="relative inline-block text-left">
+            <Root>
+                <Trigger asChild>{children}</Trigger>
+
+                <Content
+                    align="center"
+                    sideOffset={4}
+                    // eslint-disable-next-line tailwindcss/no-custom-classname
+                    className="nowheel radix-side-bottom:animate-slide-down radix-side-top:animate-slide-up z-50 w-48 rounded-lg bg-white shadow-md dark:bg-gray-800 md:w-56"
+                >
+                    <Arrow className="fill-current text-white dark:text-gray-800" />
+
+                    {id && (
+                        <ContextualMenu
+                            id={id}
+                            components={components}
+                            flowControls={flowControls}
+                        />
+                    )}
+                </Content>
+            </Root>
+        </div>
+    );
+};
+
+export default PopoverMenu;
