eead6e8c4bd337f059229337803706886a65879e
Ivica Čardić <ivica.cardic@gmail.com>
2023-02-16 17:52:34 +0100

    #222 - db-migration - Update ddl

--- server/libs/db-migration/src/main/resources/db/migration/V1_2__hermes_connection_init.sql
+++ server/libs/db-migration/src/main/resources/db/migration/V1_2__hermes_connection_init.sql
@@ -13,3 +13,12 @@ CREATE TABLE IF NOT EXISTS connection (
 );
 
 ALTER TABLE connection ADD CONSTRAINT uk_connection_name UNIQUE (name);
+
+CREATE TABLE IF NOT EXISTS connection_tag (
+    id                       BIGSERIAL    NOT NULL PRIMARY KEY,
+    connection_id            BIGSERIAL    NOT NULL,
+    tag_id                   BIGSERIAL    NOT NULL
+);
+
+ALTER TABLE connection_tag ADD CONSTRAINT fk_connection_tag_connection FOREIGN KEY (connection_id) REFERENCES connection (id);
+ALTER TABLE connection_tag ADD CONSTRAINT fk_connection_tag_tag FOREIGN KEY (tag_id) REFERENCES tag (id);
