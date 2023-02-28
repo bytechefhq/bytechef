824f892b65f5987c971e8abbc21f461fb803b8b6
Ivica Čardić <ivica.cardic@gmail.com>
2023-02-28 08:46:36 +0100

    #222 - client - Add OAuth2 support, originates from https://github.com/tasoskakour/react-use-oauth2

---
+++ client/src/pages/connections/oauth2/OAuthPopup.tsx
@@ -0,0 +1,57 @@
+import {useEffect} from 'react';
+import {OAUTH_RESPONSE, OAUTH_STATE_KEY} from './constants';
+import {queryToObject} from './tools';
+
+const checkState = (receivedState: string) => {
+    const state = sessionStorage.getItem(OAUTH_STATE_KEY);
+    return state === receivedState;
+};
+
+type Props = {
+    Component?: React.ReactElement;
+};
+
+const OAuthPopup = (props: Props) => {
+    const {
+        Component = (
+            <div style={{margin: '12px'}} data-testid="popup-loading">
+                Loading...
+            </div>
+        ),
+    } = props;
+
+    // On mount
+    useEffect(() => {
+        const payload = {
+            ...queryToObject(window.location.search.split('?')[1]),
+            ...queryToObject(window.location.hash.split('#')[1]),
+        };
+        const state = payload?.state;
+        const error = payload?.error;
+
+        if (!window.opener) {
+            throw new Error('No window opener');
+        }
+
+        if (error) {
+            window.opener.postMessage({
+                type: OAUTH_RESPONSE,
+                error: decodeURI(error) || 'OAuth error: An error has occured.',
+            });
+        } else if (state && checkState(state)) {
+            window.opener.postMessage({
+                type: OAUTH_RESPONSE,
+                payload,
+            });
+        } else {
+            window.opener.postMessage({
+                type: OAUTH_RESPONSE,
+                error: 'OAuth error: State mismatch.',
+            });
+        }
+    }, []);
+
+    return Component;
+};
+
+export default OAuthPopup;
