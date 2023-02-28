bd6d64c743bdefea605f2ceba25d74d7bf49d8d0
Ivica Čardić <ivica.cardic@gmail.com>
2023-02-28 08:45:54 +0100

    #222 - client - Make Properties more usable with react-hook-forms

--- client/src/components/Properties/Properties.tsx
+++ client/src/components/Properties/Properties.tsx
@@ -1,19 +1,40 @@
 import {PropertyModel} from '../../middleware/definition-registry';
-import {UseFormRegister} from 'react-hook-form/dist/types/form';
+import {FormState, UseFormRegister} from 'react-hook-form/dist/types/form';
 import Input from '../Input/Input';
+import {Path, FieldValues} from 'react-hook-form/dist/types';
 
-interface PropertiesProps<TProperty extends PropertyModel> {
+interface PropertiesProps<
+    TProperty extends PropertyModel,
+    TFieldValues extends FieldValues = FieldValues
+> {
+    formState: FormState<TFieldValues>;
     path?: string;
     properties?: TProperty[];
-    /* eslint-disable @typescript-eslint/no-explicit-any */
-    register: UseFormRegister<any>;
+    register: UseFormRegister<TFieldValues>;
 }
 
-const Properties = <TProperty extends PropertyModel>({
+const Properties = <
+    TProperty extends PropertyModel,
+    TFieldValues extends FieldValues = FieldValues
+>({
+    formState: {errors, touchedFields},
     path = 'parameters',
     properties,
     register,
-}: PropertiesProps<TProperty>): JSX.Element => {
+}: PropertiesProps<TProperty, TFieldValues>): JSX.Element => {
+    function isError(propertyName: string) {
+        if (
+            touchedFields[path] &&
+            touchedFields[path]![propertyName] &&
+            errors[path] &&
+            (errors[path] as never)[propertyName]
+        ) {
+            return true;
+        }
+
+        return false;
+    }
+
     return (
         <>
             {properties &&
@@ -22,10 +43,16 @@ const Properties = <TProperty extends PropertyModel>({
                         return (
                             <Input
                                 key={property.name}
+                                error={isError(property.name!)}
                                 label={property.label}
-                                {...register(path + '.' + property.name!, {
-                                    required: property.required!,
-                                })}
+                                {...register(
+                                    (path +
+                                        '.' +
+                                        property.name!) as Path<TFieldValues>,
+                                    {
+                                        required: property.required!,
+                                    }
+                                )}
                             />
                         );
                     }
