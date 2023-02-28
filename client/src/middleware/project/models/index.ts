2ec25745deb7ddbe49eed245099b7054fa89b902
Ivica Čardić <ivica.cardic@gmail.com>
2023-02-28 12:10:32 +0100

    #241 - client - Generate

---
+++ client/src/middleware/project/models/index.ts
@@ -0,0 +1,10 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './CategoryModel';
+export * from './PostProjectWorkflowRequestModel';
+export * from './ProjectModel';
+export * from './PutProjectTagsRequestModel';
+export * from './TagModel';
+export * from './WorkflowFormatModel';
+export * from './WorkflowModel';
+export * from './WorkflowTaskModel';
