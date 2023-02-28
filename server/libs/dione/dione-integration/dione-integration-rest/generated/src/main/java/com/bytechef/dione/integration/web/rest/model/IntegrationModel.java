ca9fe90bc53c33f9cf6ca808692b5333f14943e1
Ivica Čardić <ivica.cardic@gmail.com>
2023-02-28 20:44:01 +0100

    #241 - Move integration modules under “dione” (codename for embedded)

---
+++ server/libs/dione/dione-integration/dione-integration-rest/generated/src/main/java/com/bytechef/dione/integration/web/rest/model/IntegrationModel.java
@@ -0,0 +1,346 @@
+package com.bytechef.dione.integration.web.rest.model;
+
+import java.util.Objects;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonTypeName;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.format.annotation.DateTimeFormat;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.*;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+
+import jakarta.annotation.Generated;
+
+/**
+ * A group of workflows that make one logical integration.
+ */
+
+@Schema(name = "Integration", description = "A group of workflows that make one logical integration.")
+@JsonTypeName("Integration")
+@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2023-02-28T11:24:35.597268+01:00[Europe/Zagreb]")
+public class IntegrationModel {
+
+  @JsonProperty("category")
+  private CategoryModel category;
+
+  @JsonProperty("createdBy")
+  private String createdBy;
+
+  @JsonProperty("createdDate")
+  @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+  private LocalDateTime createdDate;
+
+  @JsonProperty("id")
+  private Long id;
+
+  @JsonProperty("name")
+  private String name;
+
+  @JsonProperty("description")
+  private String description;
+
+  @JsonProperty("lastModifiedBy")
+  private String lastModifiedBy;
+
+  @JsonProperty("lastModifiedDate")
+  @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+  private LocalDateTime lastModifiedDate;
+
+  @JsonProperty("tags")
+  @Valid
+  private List<TagModel> tags = null;
+
+  @JsonProperty("version")
+  private Integer version;
+
+  @JsonProperty("workflowIds")
+  @Valid
+  private List<String> workflowIds = null;
+
+  public IntegrationModel category(CategoryModel category) {
+    this.category = category;
+    return this;
+  }
+
+  /**
+   * Get category
+   * @return category
+  */
+  @Valid
+  @Schema(name = "category", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+  public CategoryModel getCategory() {
+    return category;
+  }
+
+  public void setCategory(CategoryModel category) {
+    this.category = category;
+  }
+
+  public IntegrationModel createdBy(String createdBy) {
+    this.createdBy = createdBy;
+    return this;
+  }
+
+  /**
+   * The created by.
+   * @return createdBy
+  */
+
+  @Schema(name = "createdBy", accessMode = Schema.AccessMode.READ_ONLY, description = "The created by.", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+  public String getCreatedBy() {
+    return createdBy;
+  }
+
+  public void setCreatedBy(String createdBy) {
+    this.createdBy = createdBy;
+  }
+
+  public IntegrationModel createdDate(LocalDateTime createdDate) {
+    this.createdDate = createdDate;
+    return this;
+  }
+
+  /**
+   * The created date.
+   * @return createdDate
+  */
+  @Valid
+  @Schema(name = "createdDate", accessMode = Schema.AccessMode.READ_ONLY, description = "The created date.", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+  public LocalDateTime getCreatedDate() {
+    return createdDate;
+  }
+
+  public void setCreatedDate(LocalDateTime createdDate) {
+    this.createdDate = createdDate;
+  }
+
+  public IntegrationModel id(Long id) {
+    this.id = id;
+    return this;
+  }
+
+  /**
+   * The id of an integration.
+   * @return id
+  */
+
+  @Schema(name = "id", accessMode = Schema.AccessMode.READ_ONLY, description = "The id of an integration.", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public IntegrationModel name(String name) {
+    this.name = name;
+    return this;
+  }
+
+  /**
+   * The name of the integration.
+   * @return name
+  */
+  @NotNull
+  @Schema(name = "name", description = "The name of the integration.", requiredMode = Schema.RequiredMode.REQUIRED)
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public IntegrationModel description(String description) {
+    this.description = description;
+    return this;
+  }
+
+  /**
+   * The description of the integration.
+   * @return description
+  */
+
+  @Schema(name = "description", description = "The description of the integration.", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public IntegrationModel lastModifiedBy(String lastModifiedBy) {
+    this.lastModifiedBy = lastModifiedBy;
+    return this;
+  }
+
+  /**
+   * The last modified by.
+   * @return lastModifiedBy
+  */
+
+  @Schema(name = "lastModifiedBy", accessMode = Schema.AccessMode.READ_ONLY, description = "The last modified by.", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+  public String getLastModifiedBy() {
+    return lastModifiedBy;
+  }
+
+  public void setLastModifiedBy(String lastModifiedBy) {
+    this.lastModifiedBy = lastModifiedBy;
+  }
+
+  public IntegrationModel lastModifiedDate(LocalDateTime lastModifiedDate) {
+    this.lastModifiedDate = lastModifiedDate;
+    return this;
+  }
+
+  /**
+   * The last modified date.
+   * @return lastModifiedDate
+  */
+  @Valid
+  @Schema(name = "lastModifiedDate", accessMode = Schema.AccessMode.READ_ONLY, description = "The last modified date.", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+  public LocalDateTime getLastModifiedDate() {
+    return lastModifiedDate;
+  }
+
+  public void setLastModifiedDate(LocalDateTime lastModifiedDate) {
+    this.lastModifiedDate = lastModifiedDate;
+  }
+
+  public IntegrationModel tags(List<TagModel> tags) {
+    this.tags = tags;
+    return this;
+  }
+
+  public IntegrationModel addTagsItem(TagModel tagsItem) {
+    if (this.tags == null) {
+      this.tags = new ArrayList<>();
+    }
+    this.tags.add(tagsItem);
+    return this;
+  }
+
+  /**
+   * Get tags
+   * @return tags
+  */
+  @Valid
+  @Schema(name = "tags", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+  public List<TagModel> getTags() {
+    return tags;
+  }
+
+  public void setTags(List<TagModel> tags) {
+    this.tags = tags;
+  }
+
+  public IntegrationModel version(Integer version) {
+    this.version = version;
+    return this;
+  }
+
+  /**
+   * Get version
+   * @return version
+  */
+
+  @Schema(name = "version", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+  public Integer getVersion() {
+    return version;
+  }
+
+  public void setVersion(Integer version) {
+    this.version = version;
+  }
+
+  public IntegrationModel workflowIds(List<String> workflowIds) {
+    this.workflowIds = workflowIds;
+    return this;
+  }
+
+  public IntegrationModel addWorkflowIdsItem(String workflowIdsItem) {
+    if (this.workflowIds == null) {
+      this.workflowIds = new ArrayList<>();
+    }
+    this.workflowIds.add(workflowIdsItem);
+    return this;
+  }
+
+  /**
+   * The workflow ids belonging to this integration.
+   * @return workflowIds
+  */
+
+  @Schema(name = "workflowIds", description = "The workflow ids belonging to this integration.", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+  public List<String> getWorkflowIds() {
+    return workflowIds;
+  }
+
+  public void setWorkflowIds(List<String> workflowIds) {
+    this.workflowIds = workflowIds;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    IntegrationModel integration = (IntegrationModel) o;
+    return Objects.equals(this.category, integration.category) &&
+        Objects.equals(this.createdBy, integration.createdBy) &&
+        Objects.equals(this.createdDate, integration.createdDate) &&
+        Objects.equals(this.id, integration.id) &&
+        Objects.equals(this.name, integration.name) &&
+        Objects.equals(this.description, integration.description) &&
+        Objects.equals(this.lastModifiedBy, integration.lastModifiedBy) &&
+        Objects.equals(this.lastModifiedDate, integration.lastModifiedDate) &&
+        Objects.equals(this.tags, integration.tags) &&
+        Objects.equals(this.version, integration.version) &&
+        Objects.equals(this.workflowIds, integration.workflowIds);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(category, createdBy, createdDate, id, name, description, lastModifiedBy, lastModifiedDate, tags, version, workflowIds);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class IntegrationModel {\n");
+    sb.append("    category: ").append(toIndentedString(category)).append("\n");
+    sb.append("    createdBy: ").append(toIndentedString(createdBy)).append("\n");
+    sb.append("    createdDate: ").append(toIndentedString(createdDate)).append("\n");
+    sb.append("    id: ").append(toIndentedString(id)).append("\n");
+    sb.append("    name: ").append(toIndentedString(name)).append("\n");
+    sb.append("    description: ").append(toIndentedString(description)).append("\n");
+    sb.append("    lastModifiedBy: ").append(toIndentedString(lastModifiedBy)).append("\n");
+    sb.append("    lastModifiedDate: ").append(toIndentedString(lastModifiedDate)).append("\n");
+    sb.append("    tags: ").append(toIndentedString(tags)).append("\n");
+    sb.append("    version: ").append(toIndentedString(version)).append("\n");
+    sb.append("    workflowIds: ").append(toIndentedString(workflowIds)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
+
---
+++ server/libs/dione/dione-integration/dione-integration-service/src/test/java/com/bytechef/dione/integration/repository/IntegrationRepositoryIntTest.java
@@ -0,0 +1,128 @@
+
+/*
+ * Copyright 2021 <your company/name>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef.dione.integration.repository;
+
+import com.bytechef.dione.repository.IntegrationRepository;
+import com.bytechef.dione.integration.config.IntegrationIntTestConfiguration;
+import com.bytechef.dione.integration.domain.Integration;
+import com.bytechef.test.annotation.EmbeddedSql;
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author Ivica Cardic
+ */
+@EmbeddedSql
+@SpringBootTest(classes = IntegrationIntTestConfiguration.class)
+public class IntegrationRepositoryIntTest {
+
+    @Autowired
+    private IntegrationRepository integrationRepository;
+
+    @BeforeEach
+    public void beforeEach() {
+        integrationRepository.deleteAll();
+    }
+
+    @Test
+    @SuppressFBWarnings("NP")
+    public void testCreate() {
+        Integration integration = integrationRepository.save(getIntegration(Collections.emptyList()));
+
+        assertThat(integration).isEqualTo(integrationRepository.findById(integration.getId())
+            .get());
+    }
+
+    @Test
+    @SuppressFBWarnings("NP")
+    public void testDelete() {
+        Integration integration = integrationRepository.save(getIntegration(Collections.emptyList()));
+
+        Integration resultIntegration = integrationRepository.findById(integration.getId())
+            .orElseThrow();
+
+        assertThat(resultIntegration).isEqualTo(integration);
+
+        integrationRepository.deleteById(resultIntegration.getId());
+
+        assertThat(integrationRepository.findById(integration.getId())).isEmpty();
+    }
+
+    @Test
+    @SuppressFBWarnings("NP")
+    public void testFindById() {
+        Integration integration = integrationRepository.save(getIntegration(Collections.emptyList()));
+
+        Integration resultIntegration = integrationRepository.findById(integration.getId())
+            .orElseThrow();
+
+        assertThat(resultIntegration).isEqualTo(integration);
+
+        integrationRepository.deleteById(integration.getId());
+
+        integration = getIntegration(List.of("workflowId"));
+
+        integration = integrationRepository.save(integration);
+
+        resultIntegration = integrationRepository.findById(integration.getId())
+            .orElseThrow();
+
+        assertThat(resultIntegration.getWorkflowIds()).isEqualTo(integration.getWorkflowIds());
+
+        resultIntegration.removeWorkflow("workflowId");
+
+        integrationRepository.save(resultIntegration);
+
+        resultIntegration = integrationRepository.findById(integration.getId())
+            .orElseThrow();
+
+        assertThat(resultIntegration.getWorkflowIds()).isEmpty();
+    }
+
+    @Test
+    @SuppressFBWarnings("NP")
+    public void testUpdate() {
+        Integration integration = integrationRepository.save(getIntegration(List.of("workflow1")));
+
+        integration.addWorkflow("workflow2");
+        integration.setName("name2");
+
+        integrationRepository.save(integration);
+
+        assertThat(integrationRepository.findById(integration.getId())).hasValue(integration);
+    }
+
+    private static Integration getIntegration(List<String> workflowIds) {
+        Integration integration = new Integration();
+
+        integration.setWorkflowIds(workflowIds);
+
+        integration.setDescription("name");
+        integration.setName("name");
+
+        return integration;
+    }
+}
