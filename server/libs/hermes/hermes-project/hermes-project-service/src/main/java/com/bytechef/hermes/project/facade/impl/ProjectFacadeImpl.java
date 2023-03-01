e39396b2456ac060f9209b7db8588f862a07b77a
Ivica Čardić <ivica.cardic@gmail.com>
2023-03-01 08:33:17 +0100

    #241 - Rename “hermes-integration” module to “hermes-project”

---
+++ server/libs/hermes/hermes-project/hermes-project-service/src/main/java/com/bytechef/hermes/project/facade/impl/ProjectFacadeImpl.java
@@ -0,0 +1,197 @@
+
+/*
+ * Copyright 2021 <your company/name>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef.hermes.project.facade.impl;
+
+import com.bytechef.atlas.domain.Workflow;
+import com.bytechef.atlas.service.WorkflowService;
+import com.bytechef.category.domain.Category;
+import com.bytechef.hermes.project.domain.Project;
+import com.bytechef.hermes.project.facade.ProjectFacade;
+import com.bytechef.category.servicee.CategoryService;
+import com.bytechef.hermes.project.service.ProjectService;
+import com.bytechef.tag.domain.Tag;
+import com.bytechef.tag.service.TagService;
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.CollectionUtils;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * @author Ivica Cardic
+ */
+@Service
+@Transactional
+public class ProjectFacadeImpl implements ProjectFacade {
+
+    private final CategoryService categoryService;
+    private final ProjectService projectService;
+    private final TagService tagService;
+    private final WorkflowService workflowService;
+
+    @SuppressFBWarnings("EI2")
+    public ProjectFacadeImpl(
+        CategoryService categoryService, ProjectService projectService, TagService tagService,
+        WorkflowService workflowService) {
+
+        this.categoryService = categoryService;
+        this.projectService = projectService;
+        this.tagService = tagService;
+        this.workflowService = workflowService;
+    }
+
+    @Override
+    public Project addWorkflow(long id, String name, String description, String definition) {
+        if (definition == null) {
+            definition = "{\"label\": \"%s\", \"description\": \"%s\", \"tasks\": []}"
+                .formatted(name, description);
+        }
+
+        Workflow workflow = workflowService.create(definition, Workflow.Format.JSON, Workflow.SourceType.JDBC);
+
+        return projectService.addWorkflow(id, workflow.getId());
+    }
+
+    @Override
+    @SuppressFBWarnings("NP")
+    public Project create(Project project) {
+        if (project.getCategory() != null) {
+            Category category = project.getCategory();
+
+            project.setCategory(categoryService.save(category));
+        }
+
+        if (CollectionUtils.isEmpty(project.getWorkflowIds())) {
+            Workflow workflow = workflowService.create(null, Workflow.Format.JSON, Workflow.SourceType.JDBC);
+
+            project.setWorkflowIds(List.of(workflow.getId()));
+        }
+
+        if (!CollectionUtils.isEmpty(project.getTags())) {
+            project.setTags(tagService.save(project.getTags()));
+        }
+
+        return projectService.create(project);
+    }
+
+    @Override
+    public void delete(Long id) {
+//        Project project = projectService.getProject(id);
+
+        projectService.delete(id);
+
+// TODO find a way to delete ll tags not referenced anymore
+//        project.getTagIds()
+//            .forEach(tagService::delete);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Project getProject(Long id) {
+        Project project = projectService.getProject(id);
+
+        if (project.getCategoryId() != null) {
+            categoryService.fetchCategory(project.getCategoryId())
+                .ifPresent(project::setCategory);
+        }
+
+        project.setTags(tagService.getTags(project.getTagIds()));
+
+        return project;
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Project> getProjects(List<Long> categoryIds, List<Long> tagIds) {
+        List<Project> projects = projectService.getProjects(categoryIds, tagIds);
+
+        List<Category> categories = categoryService.getCategories(projects.stream()
+            .map(Project::getCategoryId)
+            .filter(Objects::nonNull)
+            .toList());
+
+        for (Category category : categories) {
+            projects.stream()
+                .filter(project -> Objects.equals(project.getCategoryId(), category.getId()))
+                .forEach(project -> project.setCategory(category));
+        }
+
+        List<Tag> tags = tagService.getTags(projects.stream()
+            .flatMap(project -> project.getTagIds()
+                .stream())
+            .filter(Objects::nonNull)
+            .toList());
+
+        for (Project project : projects) {
+            project.setTags(
+                tags.stream()
+                    .filter(tag -> {
+                        List<Long> curTagIds = project.getTagIds();
+
+                        return curTagIds.contains(tag.getId());
+                    })
+                    .toList());
+        }
+
+        return projects;
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Tag> getProjectTags() {
+        List<Project> projects = projectService.getProjects(null, null);
+
+        List<Long> tagIds = projects.stream()
+            .map(Project::getTagIds)
+            .flatMap(Collection::stream)
+            .toList();
+
+        return tagService.getTags(tagIds);
+    }
+
+    @Override
+    public List<Workflow> getProjectWorkflows(Long id) {
+        Project project = projectService.getProject(id);
+
+        return workflowService.getWorkflows(project.getWorkflowIds());
+    }
+
+    @Override
+    public Project update(Long id, List<Tag> tags) {
+        tags = CollectionUtils.isEmpty(tags) ? Collections.emptyList() : tagService.save(tags);
+
+        return projectService.update(id, tags);
+    }
+
+    @Override
+    public Project update(Project project) {
+        project
+            .setCategory(project.getCategory() == null ? null : categoryService.save(project.getCategory()));
+        project
+            .setTags(
+                CollectionUtils.isEmpty(project.getTags())
+                    ? Collections.emptyList()
+                    : tagService.save(project.getTags()));
+
+        return projectService.update(project);
+    }
+}
---
+++ server/libs/hermes/hermes-project/hermes-project-service/src/test/java/com/bytechef/hermes/project/service/ProjectServiceIntTest.java
@@ -0,0 +1,206 @@
+
+/*
+ * Copyright 2021 <your company/name>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef.hermes.project.service;
+
+import com.bytechef.hermes.project.config.ProjectIntTestConfiguration;
+import com.bytechef.category.domain.Category;
+import com.bytechef.hermes.project.domain.Project;
+import com.bytechef.category.repository.CategoryRepository;
+import com.bytechef.hermes.project.repository.ProjectRepository;
+import com.bytechef.tag.domain.Tag;
+import com.bytechef.tag.repository.TagRepository;
+import com.bytechef.test.annotation.EmbeddedSql;
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatException;
+
+/**
+ * @author Ivica Cardic
+ */
+@EmbeddedSql
+@SpringBootTest(classes = ProjectIntTestConfiguration.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class ProjectServiceIntTest {
+
+    private Category category;
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Autowired
+    private ProjectService projectService;
+
+    @Autowired
+    private ProjectRepository projectRepository;
+
+    @Autowired
+    private TagRepository tagRepository;
+
+    @BeforeAll
+    @SuppressFBWarnings("NP")
+    public void beforeAll() {
+        categoryRepository.deleteAll();
+
+        category = categoryRepository.save(new Category("name"));
+    }
+
+    @BeforeEach
+    @SuppressFBWarnings("NP")
+    public void beforeEach() {
+        projectRepository.deleteAll();
+        tagRepository.deleteAll();
+    }
+
+    @Test
+    @SuppressFBWarnings("NP")
+    public void testAddWorkflow() {
+        Project project = projectRepository.save(getProject());
+
+        project = projectService.addWorkflow(project.getId(), "workflow2");
+
+        assertThat(project.getWorkflowIds()).contains("workflow2");
+    }
+
+    @Test
+    public void testCreate() {
+        Project project = getProject();
+
+        Tag tag = tagRepository.save(new Tag("tag1"));
+
+        project.setTags(List.of(tag));
+
+        project = projectService.create(project);
+
+        assertThat(project)
+            .hasFieldOrPropertyWithValue("category", category)
+            .hasFieldOrPropertyWithValue("description", "description")
+            .hasFieldOrPropertyWithValue("name", "name")
+            .hasFieldOrPropertyWithValue("tagIds", List.of(tag.getId()))
+            .hasFieldOrPropertyWithValue("workflowIds", List.of("workflow1"));
+    }
+
+    @Test
+    @SuppressFBWarnings("NP")
+    public void testDelete() {
+        Project project = projectRepository.save(getProject());
+
+        projectService.delete(project.getId());
+
+        assertThat(projectRepository.findById(project.getId())).isNotPresent();
+    }
+
+    @Test
+    @SuppressFBWarnings("NP")
+    public void testGetProject() {
+        Project project = projectRepository.save(getProject());
+
+        assertThat(project).isEqualTo(projectService.getProject(project.getId()));
+    }
+
+    @Test
+    @SuppressFBWarnings("NP")
+    public void testGetProjects() {
+        Project project = projectRepository.save(getProject());
+
+        assertThat(projectService.getProjects(null, null)).hasSize(1);
+
+        Category category = new Category("category1");
+
+        category = categoryRepository.save(category);
+
+        project.setCategory(category);
+
+        project = projectRepository.save(project);
+
+        assertThat(projectService.getProjects(List.of(category.getId()), null)).hasSize(1);
+
+        assertThat(projectService.getProjects(List.of(Long.MAX_VALUE), null)).hasSize(0);
+
+        Tag tag = new Tag("tag1");
+
+        tag = tagRepository.save(tag);
+
+        project.setTags(List.of(tag));
+
+        projectRepository.save(project);
+
+        assertThat(projectService.getProjects(null, List.of(tag.getId()))).hasSize(1);
+
+        assertThat(projectService.getProjects(null, List.of(Long.MAX_VALUE))).hasSize(0);
+
+        assertThatException()
+            .isThrownBy(() -> projectService.getProjects(List.of(Long.MAX_VALUE), List.of(Long.MAX_VALUE)));
+    }
+
+    @Test
+    @SuppressFBWarnings("NP")
+    public void testUpdate() {
+        Project project = projectRepository.save(getProject());
+
+        project = projectService.update(project);
+
+        assertThat(project)
+            .hasFieldOrPropertyWithValue("category", category)
+            .hasFieldOrPropertyWithValue("description", "description")
+            .hasFieldOrPropertyWithValue("name", "name")
+            .hasFieldOrPropertyWithValue("tags", Collections.emptyList())
+            .hasFieldOrPropertyWithValue("workflowIds", List.of("workflow1"));
+
+        Tag tag = tagRepository.save(new Tag("tag2"));
+
+        project.setDescription("description2");
+        project.setName("name2");
+        project.setTags(List.of(tag));
+        project.setWorkflowIds(List.of("workflow2"));
+
+        Category category2 = categoryRepository.save(new Category("name2"));
+
+        project.setCategory(category2);
+
+        project = projectService.update(project);
+
+        assertThat(project)
+            .hasFieldOrPropertyWithValue("category", category2)
+            .hasFieldOrPropertyWithValue("description", "description2")
+            .hasFieldOrPropertyWithValue("name", "name2")
+            .hasFieldOrPropertyWithValue("tags", List.of(tag))
+            .hasFieldOrPropertyWithValue("workflowIds", List.of("workflow2"));
+    }
+
+    private Project getProject() {
+        Project project = new Project();
+
+        project.addWorkflow("workflow1");
+        project.setCategory(category);
+        project.setDescription("description");
+        project.setName("name");
+
+        return project;
+    }
+}
