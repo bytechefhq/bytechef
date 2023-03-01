ea1bc7262b2adddaee077d7f558e3b44da1170b2
Ivica Čardić <ivica.cardic@gmail.com>
2023-03-01 08:33:17 +0100

    #241 - Rename “hermes-integration” module to “hermes-project”

---
+++ server/libs/hermes/hermes-project/hermes-project-api/src/main/java/com/bytechef/hermes/project/service/ProjectService.java
@@ -0,0 +1,43 @@
+
+/*
+ * Copyright 2021 <your company/name>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef.hermes.project.service;
+
+import com.bytechef.hermes.project.domain.Project;
+import com.bytechef.tag.domain.Tag;
+
+import java.util.List;
+
+/**
+ * @author Ivica Cardic
+ */
+public interface ProjectService {
+
+    Project addWorkflow(long id, String workflowId);
+
+    Project create(Project project);
+
+    void delete(long id);
+
+    Project getProject(long id);
+
+    List<Project> getProjects(List<Long> categoryIds, List<Long> tagIds);
+
+    Project update(long id, List<Tag> tags);
+
+    Project update(Project project);
+}
---
+++ server/libs/hermes/hermes-project/hermes-project-service/src/main/java/com/bytechef/hermes/project/repository/ProjectRepository.java
@@ -0,0 +1,53 @@
+
+/*
+ * Copyright 2021 <your company/name>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef.hermes.project.repository;
+
+import com.bytechef.hermes.project.domain.Project;
+import org.springframework.data.jdbc.repository.query.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * @author Ivica Cardic
+ */
+@Repository
+public interface ProjectRepository
+    extends PagingAndSortingRepository<Project, Long>, CrudRepository<Project, Long> {
+
+    Iterable<Project> findByCategoryIdIn(List<Long> categoryIds);
+
+    @Query("""
+            SELECT project.* FROM project
+            JOIN project_tag ON project.id = project_tag.project_id
+            WHERE project_tag.tag_id in (:tagIds)
+        """)
+    Iterable<Project> findByTagIdIn(@Param("tagIds") List<Long> tagIds);
+
+    @Query("""
+            SELECT project.* FROM project
+            JOIN project_tag ON project.id = project_tag.project_id
+            WHERE project.category_id IN (:categoryIds)
+            AND project_tag.tag_id IN (:tagId)
+        """)
+    Iterable<Project> findByCategoryIdsAndTagIds(
+        @Param("categoryIds") List<Long> categoryIds, @Param("tagIds") List<Long> tagIds);
+}
