f08c8a58eb20ad1b622c33949ab3812b804b5cbe
Ivica Čardić <ivica.cardic@gmail.com>
2023-03-03 08:48:22 +0100

    #222 - atlas - Generate

--- server/libs/atlas/atlas-rest/atlas-rest-api/generated/src/main/java/com/bytechef/atlas/web/rest/model/WorkflowFormatModel.java
+++ server/libs/atlas/atlas-rest/atlas-rest-api/generated/src/main/java/com/bytechef/atlas/web/rest/model/WorkflowFormatModel.java
@@ -20,7 +20,7 @@ import com.fasterxml.jackson.annotation.JsonValue;
  * Gets or Sets WorkflowFormat
  */
 
-@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2023-03-01T09:06:53.695754+01:00[Europe/Zagreb]")
+@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2023-03-02T19:53:04.302675+01:00[Europe/Zagreb]")
 public enum WorkflowFormatModel {
   
   JSON("JSON"),
