6b1467c8fb23e6fbbaf3efe8c1be38a9f420b7a1
Ivica Čardić <ivica.cardic@gmail.com>
2023-03-03 16:07:01 +0100

    #222 - Apply the same reorganization like in Intgration/s components

--- client/src/pages/connections/ConnectionItem.tsx
+++ client/src/pages/connections/ConnectionItem.tsx
@@ -1,98 +1,24 @@
-import React, {useState} from 'react';
-import {
-    Dropdown,
-    DropdownMenuItemType,
-} from '../../components/Dropdown/Dropdown';
-import {TagModel} from '../../middleware/connection';
+import React from 'react';
+import DropdownMenu, {
+    DropdownMenuItemProps,
+} from '../../components/DropdownMenu/DropdownMenu';
+import {ConnectionModel, TagModel} from '../../middleware/connection';
 import {useQueryClient} from '@tanstack/react-query';
-import CreatableSelect, {
-    SelectOption,
-} from '../../components/CreatableSelect/CreatableSelect';
-import Button from 'components/Button/Button';
-import {PlusIcon, XMarkIcon} from '@heroicons/react/24/outline';
-import {ChevronDownIcon, Component1Icon} from '@radix-ui/react-icons';
+import {Component1Icon} from '@radix-ui/react-icons';
 import {ConnectionKeys} from '../../queries/connections';
-import {
-    useConnectionDeleteMutation,
-    useConnectionTagsMutation,
-} from '../../mutations/connections.mutations';
+import {useDeleteConnectionMutation} from '../../mutations/connections.mutations';
 import {ComponentDefinitionKeys} from '../../queries/componentDefinitions';
-import {SingleValue} from 'react-select';
+import TagList from './components/TagList';
 
 interface ConnectionItemProps {
-    id?: number;
-    lastModifiedDate?: Date;
-    name: string;
+    connection: ConnectionModel;
     remainingTags?: TagModel[];
-    tags?: TagModel[];
-    version?: number;
 }
 
-interface DateProps {
-    lastModifiedDate?: Date;
-}
-
-interface FooterProps {
-    tags?: TagModel[];
-    remainingTags?: TagModel[];
-    onAddTag: (newTag: TagModel) => void;
-    onDeleteTag: (deletedTag: TagModel) => void;
-}
-
-interface HeaderProps {
-    name: string;
-}
-
-interface NameProps {
-    name: string;
-}
-
-const Date = ({lastModifiedDate}: DateProps) => {
-    return (
-        <span className="mr-4 text-center text-sm text-gray-500">
-            Last Modified{' '}
-            {`${lastModifiedDate?.toLocaleDateString()} ${lastModifiedDate?.toLocaleTimeString()}`}
-        </span>
-    );
-};
-
-const Footer = ({tags, remainingTags, onAddTag, onDeleteTag}: FooterProps) => {
-    return (
-        <div
-            className="flex h-[38px] items-center"
-            onClick={(event) => event.preventDefault()}
-        >
-            {tags && (
-                <TagList
-                    tags={tags}
-                    remainingTags={remainingTags}
-                    onAddTag={onAddTag}
-                    onDeleteTag={onDeleteTag}
-                />
-            )}
-        </div>
-    );
-};
-
-const Header = ({name}: HeaderProps) => {
-    return (
-        <div className="relative mb-2 flex items-center">
-            <Component1Icon className="mr-1 h-5 w-5 flex-none" />{' '}
-            <Name name={name} />
-        </div>
-    );
-};
-
-const ConnectionItem = ({
-    id,
-    lastModifiedDate,
-    name,
-    tags,
-    remainingTags,
-}: ConnectionItemProps) => {
+const ConnectionItem = ({connection, remainingTags}: ConnectionItemProps) => {
     const queryClient = useQueryClient();
 
-    const connectionDeleteMutation = useConnectionDeleteMutation({
+    const connectionDeleteMutation = useDeleteConnectionMutation({
         onSuccess: () => {
             queryClient.invalidateQueries(
                 ComponentDefinitionKeys.componentDefinitions({
@@ -104,7 +30,7 @@ const ConnectionItem = ({
         },
     });
 
-    const menuItems: DropdownMenuItemType[] = [
+    const menuItems: DropdownMenuItemProps[] = [
         {
             label: 'Edit',
         },
@@ -112,6 +38,7 @@ const ConnectionItem = ({
             separator: true,
         },
         {
+            danger: true,
             label: 'Delete',
             onClick: (id?: number) => {
                 if (id) {
@@ -124,30 +51,35 @@ const ConnectionItem = ({
     return (
         <div className="flex items-center justify-between">
             <div>
-                <Header name={name} />
+                <div className="relative mb-2 flex items-center">
+                    <Component1Icon className="mr-1 h-5 w-5 flex-none" />{' '}
+                    <span className="mr-2 text-base font-semibold text-gray-900">
+                        {connection.name}
+                    </span>
+                </div>
 
-                <Footer
-                    tags={tags}
-                    remainingTags={remainingTags}
-                    onAddTag={handleOnAddTag}
-                    onDeleteTag={handleOnDeleteTag}
-                />
+                <div
+                    className="flex h-[38px] items-center"
+                    onClick={(event) => event.preventDefault()}
+                >
+                    {connection.tags && (
+                        <TagList
+                            connectionId={connection.id!}
+                            remainingTags={remainingTags}
+                            tags={connection.tags}
+                        />
+                    )}
+                </div>
             </div>
 
             <div className="flex items-center">
-                <Date lastModifiedDate={lastModifiedDate} />
+                <span className="mr-4 text-center text-sm text-gray-500">
+                    {`Last Modified ${connection.lastModifiedDate?.toLocaleDateString()} ${connection.lastModifiedDate?.toLocaleTimeString()}`}
+                </span>
 
-                <Dropdown id={id} menuItems={menuItems} />
+                <DropdownMenu id={connection.id} menuItems={menuItems} />
             </div>
         </div>
-    );
-};
-
-const Name = ({name}: NameProps) => {
-    return (
-        <span className="mr-2 text-base font-semibold text-gray-900">
-            {name}
-        </span>
     );
 };
 
