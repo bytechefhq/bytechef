a2ade0b0f8b206d03ac40ad8944e133aec54a3df
Ivica Čardić <ivica.cardic@gmail.com>
2023-03-04 06:52:21 +0100

    #241 - client - Rename interation/s to project/s, move them under “automation” dir

---
+++ client/src/queries/projects.ts
@@ -0,0 +1,69 @@
+import {useQuery} from '@tanstack/react-query';
+import {
+    CategoryModel,
+    ProjectModel,
+    ProjectsApi,
+    TagModel,
+} from 'middleware/project';
+import {WorkflowModel} from '../middleware/workflow';
+
+export const ProjectKeys = {
+    project: (id: number) => ['project', id],
+    projectCategories: ['projectCategories'] as const,
+    projectList: (filters: {categoryIds?: number[]; tagIds?: number[]}) => [
+        ...ProjectKeys.projects,
+        filters,
+    ],
+    projectTags: ['projectTags'] as const,
+    projectWorkflows: (id: number) => ['projectWorkflows', id],
+    projects: ['projects'] as const,
+};
+
+export const useGetProjectTagsQuery = () =>
+    useQuery<TagModel[], Error>(
+        ProjectKeys.projectTags,
+        () => new ProjectsApi().getProjectTags(),
+        {
+            staleTime: 1 * 60 * 1000,
+        }
+    );
+
+export const useGetProjectQuery = (id: number, initialData?: ProjectModel) =>
+    useQuery<ProjectModel, Error>(
+        ProjectKeys.project(id),
+        () => new ProjectsApi().getProject({id}),
+        {
+            staleTime: 1 * 60 * 1000,
+            initialData,
+        }
+    );
+
+export const useGetProjectsQuery = (filters: {
+    categoryIds?: number[];
+    tagIds?: number[];
+}) =>
+    useQuery<ProjectModel[], Error>(
+        ProjectKeys.projectList(filters),
+        () => new ProjectsApi().getProjects(filters),
+        {
+            staleTime: 1 * 60 * 1000,
+        }
+    );
+
+export const useGetProjectCategoriesQuery = () =>
+    useQuery<CategoryModel[], Error>(
+        ProjectKeys.projectCategories,
+        () => new ProjectsApi().getProjectCategories(),
+        {
+            staleTime: 1 * 60 * 1000,
+        }
+    );
+
+export const useGetProjectWorkflowsQuery = (id: number) =>
+    useQuery<WorkflowModel[], Error>(
+        ProjectKeys.projectWorkflows(id),
+        () => new ProjectsApi().getProjectWorkflows({id}),
+        {
+            staleTime: 1 * 60 * 1000,
+        }
+    );
