effae8515421e926ff2b22d4844e1ff8cd263f33
Krešimir Čoko <kresimircoko@gmail.com>
2023-03-10 15:43:38 +0100

    #28 Add a dropdown menu for editing and deleting a node (wip)

---
+++ client/src/pages/automation/project/components/EditNodeDialog.tsx
@@ -0,0 +1,55 @@
+import {useState} from 'react';
+import Dialog from 'components/Dialog/Dialog';
+import Input from 'components/Input/Input';
+import {Close} from '@radix-ui/react-dialog';
+import Button from 'components/Button/Button';
+
+type EditNodeDialogProps = {
+    visible?: boolean;
+    onClose?: () => void;
+};
+
+const EditNodeDialog = ({visible = false, onClose}: EditNodeDialogProps) => {
+    const [isOpen, setIsOpen] = useState(visible);
+
+    console.log('isOpen: ', isOpen);
+
+    function closeDialog() {
+        setIsOpen(false);
+
+        if (onClose) {
+            onClose();
+        }
+    }
+
+    return (
+        <Dialog
+            description="Use this to edit a workflow node."
+            isOpen={isOpen}
+            onOpenChange={(isOpen) => {
+                if (isOpen) {
+                    setIsOpen(isOpen);
+                } else {
+                    closeDialog();
+                }
+            }}
+            title="Edit Workflow Node"
+        >
+            <Input label="Name" name="node" />
+
+            <div className="mt-4 flex justify-end space-x-1">
+                <Close asChild>
+                    <Button
+                        displayType="lightBorder"
+                        label="Cancel"
+                        type="button"
+                    />
+                </Close>
+
+                <Button label="Save" type="submit" />
+            </div>
+        </Dialog>
+    );
+};
+
+export default EditNodeDialog;
--- client/src/pages/automation/project/nodes/WorkflowNode.tsx
+++ client/src/pages/automation/project/nodes/WorkflowNode.tsx
@@ -1,29 +1,66 @@
-import React, {memo} from 'react';
+import {memo, useState} from 'react';
 import {Handle, Position, NodeProps} from 'reactflow';
-import {twMerge} from 'tailwind-merge';
-
 import styles from './NodeTypes.module.css';
 import useNodeClickHandler from '../hooks/useNodeClick';
+import Button from 'components/Button/Button';
+import DropdownMenu from 'components/DropdownMenu/DropdownMenu';
+import EditNodeDialog from '../components/EditNodeDialog';
 
 const WorkflowNode = ({id, data}: NodeProps) => {
-    const onClick = useNodeClickHandler(data, id);
+    const [showEditNodeDialog, setShowEditNodeDialog] = useState(false);
+    const handleNodeClick = useNodeClickHandler(data, id);
+
+    const handleDeleteNodeClick = () => {
+        console.log('delete a node');
+    };
+
+    const handleEditNodeClick = () => {
+        setShowEditNodeDialog(true);
+    };
 
     return (
         <div
-            onClick={onClick}
-            className={twMerge(
-                styles.node,
-                'relative flex h-[72px] w-[72px] cursor-pointer items-center justify-center rounded-md border-2 border-gray-300 bg-white shadow hover:bg-gray-200'
-            )}
+            className="relative flex cursor-pointer items-center justify-center rounded-md border-2 border-gray-300 bg-white shadow hover:bg-gray-200"
             title="Click to add a child node"
         >
-            <div>{data.icon}</div>
+            <div className="absolute left-[-32px]">
+                <DropdownMenu
+                    menuItems={[
+                        {
+                            label: 'Edit',
+                            onClick: handleEditNodeClick,
+                        },
+                        {
+                            separator: true,
+                        },
+                        {
+                            danger: true,
+                            label: 'Delete',
+                            onClick: handleDeleteNodeClick,
+                        },
+                    ]}
+                />
+            </div>
+
+            <Button
+                className="p-4"
+                displayType="icon"
+                icon={data.icon}
+                onClick={handleNodeClick}
+            />
 
             <div className="absolute left-[80px] flex w-full min-w-max flex-col items-start">
                 <span className="text-sm text-gray-900">{data.label}</span>
 
                 <span className="text-xs text-gray-500">{data.name}</span>
             </div>
+
+            {showEditNodeDialog && (
+                <EditNodeDialog
+                    visible={showEditNodeDialog}
+                    onClose={() => setShowEditNodeDialog(false)}
+                />
+            )}
 
             <Handle
                 className={styles.handle}
