7f2bed6abad75e0a9d7963d9d02f9c5087bd21f5
Krešimir Čoko <kresimircoko@gmail.com>
2023-03-10 10:49:48 +0100

    #198 SF

--- client/src/layouts/LeftSidebarMenu/LeftSidebarMenuItem.tsx
+++ client/src/layouts/LeftSidebarMenu/LeftSidebarMenuItem.tsx
@@ -1,38 +1,38 @@
-import React, {ReactNode} from 'react';
+import {ReactNode} from 'react';
 import {twMerge} from 'tailwind-merge';
 import {Link} from 'react-router-dom';
 
-interface LeftSidebarItemProps {
+type LeftSidebarItemProps = {
     item: {
-        id?: number | string;
-        name: string;
         current: boolean;
+        name: string;
         onItemClick: (id?: number | string) => void;
+        id?: number | string;
     };
-    icon?: ReactNode;
     toLink: string;
-}
+    icon?: ReactNode;
+};
 
 const LeftSidebarMenuItem = ({
-    item: {id, name, current, onItemClick},
     icon,
+    item: {id, name, current, onItemClick},
     toLink,
-}: LeftSidebarItemProps) => {
-    return (
-        <Link
-            to={toLink}
-            className={twMerge(
-                current
-                    ? 'bg-gray-200 text-gray-900'
-                    : 'text-gray-600 hover:bg-gray-50 hover:text-gray-900 dark:text-gray-400 dark:hover:bg-gray-600',
-                'flex items-center rounded-md px-2 py-2 text-sm font-medium'
-            )}
-            aria-current={current ? 'page' : undefined}
-            onClick={() => onItemClick(id)}
-        >
-            {icon} <span className="truncate">{name}</span>
-        </Link>
-    );
-};
+}: LeftSidebarItemProps) => (
+    <Link
+        to={toLink}
+        className={twMerge(
+            current
+                ? 'bg-gray-200 text-gray-900'
+                : 'text-gray-600 hover:bg-gray-50 hover:text-gray-900 dark:text-gray-400 dark:hover:bg-gray-600',
+            'flex items-center rounded-md px-2 py-2 text-sm font-medium'
+        )}
+        aria-current={current ? 'page' : undefined}
+        onClick={() => onItemClick(id)}
+    >
+        {icon}
+
+        <span className="truncate">{name}</span>
+    </Link>
+);
 
 export default LeftSidebarMenuItem;
