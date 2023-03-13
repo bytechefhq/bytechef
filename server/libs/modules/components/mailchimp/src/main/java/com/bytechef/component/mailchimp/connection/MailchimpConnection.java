a3bb41377d62993fb712dc017672822892bb9ac8
Ivica Čardić <ivica.cardic@gmail.com>
2023-03-13 17:59:25 +0100

    222 - mailchimp - Add initial comit for Mailchimp component

---
+++ server/libs/modules/components/mailchimp/src/main/java/com/bytechef/component/mailchimp/connection/MailchimpConnection.java
@@ -0,0 +1,55 @@
+
+/*
+ * Copyright 2021 <your company/name>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef.component.mailchimp.connection;
+
+import static com.bytechef.hermes.component.constant.ComponentConstants.CLIENT_ID;
+import static com.bytechef.hermes.component.constant.ComponentConstants.CLIENT_SECRET;
+import static com.bytechef.hermes.component.definition.ComponentDSL.authorization;
+import static com.bytechef.hermes.component.definition.ComponentDSL.connection;
+import static com.bytechef.hermes.component.definition.ComponentDSL.display;
+import static com.bytechef.hermes.component.definition.ComponentDSL.string;
+
+import com.bytechef.hermes.component.definition.Authorization;
+import com.bytechef.hermes.component.definition.ComponentDSL;
+import java.util.List;
+
+/**
+ * Provides the component connection definition.
+ *
+ * @generated
+ */
+public class MailchimpConnection {
+    public static final ComponentDSL.ModifiableConnectionDefinition CONNECTION_DEFINITION = connection()
+        .baseUri(connection -> "/")
+        .authorizations(authorization(
+            Authorization.AuthorizationType.OAUTH2_AUTHORIZATION_CODE.name()
+                .toLowerCase(),
+            Authorization.AuthorizationType.OAUTH2_AUTHORIZATION_CODE)
+                .display(
+                    display("OAuth2 Authorization Code"))
+                .properties(
+                    string(CLIENT_ID)
+                        .label("Client Id")
+                        .required(true),
+                    string(CLIENT_SECRET)
+                        .label("Client Secret")
+                        .required(true))
+                .authorizationUrl(connection -> "https://login.mailchimp.com/oauth2/authorize")
+                .scopes(connection -> List.of())
+                .tokenUrl(connection -> "https://login.mailchimp.com/oauth2/token"));
+}
