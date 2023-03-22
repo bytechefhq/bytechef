80177a1f7702fc8469b95b59f50a45e53e96f39c
Ivica Čardić <ivica.cardic@gmail.com>
2023-03-22 13:28:53 +0100

    228 - Add a PageFooter component, it will be used in Executions page, for now

---
+++ client/src/components/PageFooter/PageFooter.tsx
@@ -0,0 +1,37 @@
+import React, {PropsWithChildren, ReactNode} from 'react';
+import {twMerge} from 'tailwind-merge';
+
+interface PageFooterProps {
+    leftSidebar?: boolean;
+    position?: 'main' | 'sidebar';
+    right?: ReactNode;
+}
+
+const PageFooter = ({
+    children,
+    position = 'sidebar',
+    leftSidebar = false,
+    right,
+}: PropsWithChildren<PageFooterProps>) => (
+    <footer
+        className={twMerge(
+            'p-4',
+            position === 'main' ? 'flex w-full place-self-center 2xl:w-4/5' : ''
+        )}
+    >
+        <div className="flex w-full items-center justify-between">
+            <div
+                className={twMerge(
+                    'flex h-[34px] w-full items-center text-lg tracking-tight text-gray-900 dark:text-gray-200',
+                    leftSidebar ? 'font-semibold' : ''
+                )}
+            >
+                {children}
+            </div>
+
+            {right && <div>{right}</div>}
+        </div>
+    </footer>
+);
+
+export default PageFooter;
