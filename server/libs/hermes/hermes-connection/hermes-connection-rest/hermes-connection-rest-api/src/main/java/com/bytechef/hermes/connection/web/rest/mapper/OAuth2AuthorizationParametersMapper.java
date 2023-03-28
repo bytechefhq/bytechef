1844e68a7a76ee25ccf5ec885a6c16d6940a7751
Ivica Čardić <ivica.cardic@gmail.com>
2023-03-28 07:35:22 +0200

    227 - Update controllers

--- server/libs/hermes/hermes-connection/hermes-connection-rest/hermes-connection-rest-api/src/main/java/com/bytechef/hermes/connection/web/rest/mapper/OAuth2AuthorizationParametersMapper.java
+++ server/libs/hermes/hermes-connection/hermes-connection-rest/hermes-connection-rest-api/src/main/java/com/bytechef/hermes/connection/web/rest/mapper/OAuth2AuthorizationParametersMapper.java
@@ -19,7 +19,7 @@ package com.bytechef.hermes.connection.web.rest.mapper;
 
 import com.bytechef.hermes.connection.web.rest.mapper.config.ConnectionMapperSpringConfig;
 import com.bytechef.hermes.connection.web.rest.model.OAuth2AuthorizationParametersModel;
-import com.bytechef.hermes.definition.registry.service.ConnectionDefinitionService;
+import com.bytechef.hermes.definition.registry.dto.OAuth2AuthorizationParameters;
 import org.mapstruct.Mapper;
 import org.springframework.core.convert.converter.Converter;
 
@@ -28,8 +28,8 @@ import org.springframework.core.convert.converter.Converter;
  */
 @Mapper(config = ConnectionMapperSpringConfig.class)
 public interface OAuth2AuthorizationParametersMapper
-    extends Converter<ConnectionDefinitionService.OAuth2AuthorizationParameters, OAuth2AuthorizationParametersModel> {
+    extends Converter<OAuth2AuthorizationParameters, OAuth2AuthorizationParametersModel> {
 
     @Override
-    OAuth2AuthorizationParametersModel convert(ConnectionDefinitionService.OAuth2AuthorizationParameters source);
+    OAuth2AuthorizationParametersModel convert(OAuth2AuthorizationParameters source);
 }
