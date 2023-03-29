bf82f27db947a1b75c74320cf33c22ed1f265328
Ivica Čardić <ivica.cardic@gmail.com>
2023-03-29 13:14:25 +0200

    227 - Generate

--- server/libs/helios/helios-project/helios-project-rest/generated/src/main/java/com/bytechef/helios/project/web/rest/model/ProjectInstanceBasicModel.java
+++ server/libs/helios/helios-project/helios-project-rest/generated/src/main/java/com/bytechef/helios/project/web/rest/model/ProjectInstanceBasicModel.java
@@ -2,16 +2,11 @@ package com.bytechef.helios.project.web.rest.model;
 
 import java.net.URI;
 import java.util.Objects;
-import com.bytechef.helios.project.web.rest.model.ProjectModel;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonTypeName;
 import com.fasterxml.jackson.annotation.JsonValue;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.openapitools.jackson.nullable.JsonNullable;
 import java.time.OffsetDateTime;
@@ -27,22 +22,10 @@ import jakarta.annotation.Generated;
  * Contains specific configuration required for the execution of project workflows.
  */
 
-@Schema(name = "ProjectInstance", description = "Contains specific configuration required for the execution of project workflows.")
-@JsonTypeName("ProjectInstance")
+@Schema(name = "ProjectInstanceBasic", description = "Contains specific configuration required for the execution of project workflows.")
+@JsonTypeName("ProjectInstanceBasic")
 @Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2023-03-29T14:04:57.997730+02:00[Europe/Zagreb]")
-public class ProjectInstanceModel {
-
-  @JsonProperty("configurationParameters")
-  @Valid
-  private Map<String, Object> configurationParameters = null;
-
-  @JsonProperty("connectionIds")
-  @Valid
-  private List<Long> connectionIds = null;
-
-  @JsonProperty("connections")
-  @Valid
-  private List<com.bytechef.hermes.connection.web.rest.model.ConnectionModel> connections = null;
+public class ProjectInstanceBasicModel {
 
   @JsonProperty("description")
   private String description;
@@ -70,9 +53,6 @@ public class ProjectInstanceModel {
 
   @JsonProperty("name")
   private String name;
-
-  @JsonProperty("project")
-  private ProjectModel project;
 
   @JsonProperty("projectId")
   private Long projectId;
@@ -115,95 +95,7 @@ public class ProjectInstanceModel {
   @JsonProperty("status")
   private StatusEnum status;
 
-  @JsonProperty("tags")
-  @Valid
-  private List<com.bytechef.tag.web.rest.model.TagModel> tags = null;
-
-  @JsonProperty("__version")
-  private Integer version;
-
-  public ProjectInstanceModel configurationParameters(Map<String, Object> configurationParameters) {
-    this.configurationParameters = configurationParameters;
-    return this;
-  }
-
-  public ProjectInstanceModel putConfigurationParametersItem(String key, Object configurationParametersItem) {
-    if (this.configurationParameters == null) {
-      this.configurationParameters = new HashMap<>();
-    }
-    this.configurationParameters.put(key, configurationParametersItem);
-    return this;
-  }
-
-  /**
-   * The configuration parameters of an project instance used as workflow input values.
-   * @return configurationParameters
-  */
-  
-  @Schema(name = "configurationParameters", description = "The configuration parameters of an project instance used as workflow input values.", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
-  public Map<String, Object> getConfigurationParameters() {
-    return configurationParameters;
-  }
-
-  public void setConfigurationParameters(Map<String, Object> configurationParameters) {
-    this.configurationParameters = configurationParameters;
-  }
-
-  public ProjectInstanceModel connectionIds(List<Long> connectionIds) {
-    this.connectionIds = connectionIds;
-    return this;
-  }
-
-  public ProjectInstanceModel addConnectionIdsItem(Long connectionIdsItem) {
-    if (this.connectionIds == null) {
-      this.connectionIds = new ArrayList<>();
-    }
-    this.connectionIds.add(connectionIdsItem);
-    return this;
-  }
-
-  /**
-   * The ids of connections used by a project instance.
-   * @return connectionIds
-  */
-  
-  @Schema(name = "connectionIds", description = "The ids of connections used by a project instance.", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
-  public List<Long> getConnectionIds() {
-    return connectionIds;
-  }
-
-  public void setConnectionIds(List<Long> connectionIds) {
-    this.connectionIds = connectionIds;
-  }
-
-  public ProjectInstanceModel connections(List<com.bytechef.hermes.connection.web.rest.model.ConnectionModel> connections) {
-    this.connections = connections;
-    return this;
-  }
-
-  public ProjectInstanceModel addConnectionsItem(com.bytechef.hermes.connection.web.rest.model.ConnectionModel connectionsItem) {
-    if (this.connections == null) {
-      this.connections = new ArrayList<>();
-    }
-    this.connections.add(connectionsItem);
-    return this;
-  }
-
-  /**
-   * The connections used by a project instance.
-   * @return connections
-  */
-  @Valid 
-  @Schema(name = "connections", accessMode = Schema.AccessMode.READ_ONLY, description = "The connections used by a project instance.", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
-  public List<com.bytechef.hermes.connection.web.rest.model.ConnectionModel> getConnections() {
-    return connections;
-  }
-
-  public void setConnections(List<com.bytechef.hermes.connection.web.rest.model.ConnectionModel> connections) {
-    this.connections = connections;
-  }
-
-  public ProjectInstanceModel description(String description) {
+  public ProjectInstanceBasicModel description(String description) {
     this.description = description;
     return this;
   }
@@ -222,7 +114,7 @@ public class ProjectInstanceModel {
     this.description = description;
   }
 
-  public ProjectInstanceModel createdBy(String createdBy) {
+  public ProjectInstanceBasicModel createdBy(String createdBy) {
     this.createdBy = createdBy;
     return this;
   }
@@ -241,7 +133,7 @@ public class ProjectInstanceModel {
     this.createdBy = createdBy;
   }
 
-  public ProjectInstanceModel createdDate(LocalDateTime createdDate) {
+  public ProjectInstanceBasicModel createdDate(LocalDateTime createdDate) {
     this.createdDate = createdDate;
     return this;
   }
@@ -260,7 +152,7 @@ public class ProjectInstanceModel {
     this.createdDate = createdDate;
   }
 
-  public ProjectInstanceModel id(Long id) {
+  public ProjectInstanceBasicModel id(Long id) {
     this.id = id;
     return this;
   }
@@ -279,7 +171,7 @@ public class ProjectInstanceModel {
     this.id = id;
   }
 
-  public ProjectInstanceModel lastExecutionDate(LocalDateTime lastExecutionDate) {
+  public ProjectInstanceBasicModel lastExecutionDate(LocalDateTime lastExecutionDate) {
     this.lastExecutionDate = lastExecutionDate;
     return this;
   }
@@ -298,7 +190,7 @@ public class ProjectInstanceModel {
     this.lastExecutionDate = lastExecutionDate;
   }
 
-  public ProjectInstanceModel lastModifiedBy(String lastModifiedBy) {
+  public ProjectInstanceBasicModel lastModifiedBy(String lastModifiedBy) {
     this.lastModifiedBy = lastModifiedBy;
     return this;
   }
@@ -317,7 +209,7 @@ public class ProjectInstanceModel {
     this.lastModifiedBy = lastModifiedBy;
   }
 
-  public ProjectInstanceModel lastModifiedDate(LocalDateTime lastModifiedDate) {
+  public ProjectInstanceBasicModel lastModifiedDate(LocalDateTime lastModifiedDate) {
     this.lastModifiedDate = lastModifiedDate;
     return this;
   }
@@ -336,7 +228,7 @@ public class ProjectInstanceModel {
     this.lastModifiedDate = lastModifiedDate;
   }
 
-  public ProjectInstanceModel name(String name) {
+  public ProjectInstanceBasicModel name(String name) {
     this.name = name;
     return this;
   }
@@ -355,26 +247,7 @@ public class ProjectInstanceModel {
     this.name = name;
   }
 
-  public ProjectInstanceModel project(ProjectModel project) {
-    this.project = project;
-    return this;
-  }
-
-  /**
-   * Get project
-   * @return project
-  */
-  @Valid 
-  @Schema(name = "project", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
-  public ProjectModel getProject() {
-    return project;
-  }
-
-  public void setProject(ProjectModel project) {
-    this.project = project;
-  }
-
-  public ProjectInstanceModel projectId(Long projectId) {
+  public ProjectInstanceBasicModel projectId(Long projectId) {
     this.projectId = projectId;
     return this;
   }
@@ -393,7 +266,7 @@ public class ProjectInstanceModel {
     this.projectId = projectId;
   }
 
-  public ProjectInstanceModel status(StatusEnum status) {
+  public ProjectInstanceBasicModel status(StatusEnum status) {
     this.status = status;
     return this;
   }
@@ -412,52 +285,6 @@ public class ProjectInstanceModel {
     this.status = status;
   }
 
-  public ProjectInstanceModel tags(List<com.bytechef.tag.web.rest.model.TagModel> tags) {
-    this.tags = tags;
-    return this;
-  }
-
-  public ProjectInstanceModel addTagsItem(com.bytechef.tag.web.rest.model.TagModel tagsItem) {
-    if (this.tags == null) {
-      this.tags = new ArrayList<>();
-    }
-    this.tags.add(tagsItem);
-    return this;
-  }
-
-  /**
-   * Get tags
-   * @return tags
-  */
-  @Valid 
-  @Schema(name = "tags", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
-  public List<com.bytechef.tag.web.rest.model.TagModel> getTags() {
-    return tags;
-  }
-
-  public void setTags(List<com.bytechef.tag.web.rest.model.TagModel> tags) {
-    this.tags = tags;
-  }
-
-  public ProjectInstanceModel version(Integer version) {
-    this.version = version;
-    return this;
-  }
-
-  /**
-   * Get version
-   * @return version
-  */
-  
-  @Schema(name = "__version", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
-  public Integer getVersion() {
-    return version;
-  }
-
-  public void setVersion(Integer version) {
-    this.version = version;
-  }
-
   @Override
   public boolean equals(Object o) {
     if (this == o) {
@@ -466,37 +293,28 @@ public class ProjectInstanceModel {
     if (o == null || getClass() != o.getClass()) {
       return false;
     }
-    ProjectInstanceModel projectInstance = (ProjectInstanceModel) o;
-    return Objects.equals(this.configurationParameters, projectInstance.configurationParameters) &&
-        Objects.equals(this.connectionIds, projectInstance.connectionIds) &&
-        Objects.equals(this.connections, projectInstance.connections) &&
-        Objects.equals(this.description, projectInstance.description) &&
-        Objects.equals(this.createdBy, projectInstance.createdBy) &&
-        Objects.equals(this.createdDate, projectInstance.createdDate) &&
-        Objects.equals(this.id, projectInstance.id) &&
-        Objects.equals(this.lastExecutionDate, projectInstance.lastExecutionDate) &&
-        Objects.equals(this.lastModifiedBy, projectInstance.lastModifiedBy) &&
-        Objects.equals(this.lastModifiedDate, projectInstance.lastModifiedDate) &&
-        Objects.equals(this.name, projectInstance.name) &&
-        Objects.equals(this.project, projectInstance.project) &&
-        Objects.equals(this.projectId, projectInstance.projectId) &&
-        Objects.equals(this.status, projectInstance.status) &&
-        Objects.equals(this.tags, projectInstance.tags) &&
-        Objects.equals(this.version, projectInstance.version);
+    ProjectInstanceBasicModel projectInstanceBasic = (ProjectInstanceBasicModel) o;
+    return Objects.equals(this.description, projectInstanceBasic.description) &&
+        Objects.equals(this.createdBy, projectInstanceBasic.createdBy) &&
+        Objects.equals(this.createdDate, projectInstanceBasic.createdDate) &&
+        Objects.equals(this.id, projectInstanceBasic.id) &&
+        Objects.equals(this.lastExecutionDate, projectInstanceBasic.lastExecutionDate) &&
+        Objects.equals(this.lastModifiedBy, projectInstanceBasic.lastModifiedBy) &&
+        Objects.equals(this.lastModifiedDate, projectInstanceBasic.lastModifiedDate) &&
+        Objects.equals(this.name, projectInstanceBasic.name) &&
+        Objects.equals(this.projectId, projectInstanceBasic.projectId) &&
+        Objects.equals(this.status, projectInstanceBasic.status);
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(configurationParameters, connectionIds, connections, description, createdBy, createdDate, id, lastExecutionDate, lastModifiedBy, lastModifiedDate, name, project, projectId, status, tags, version);
+    return Objects.hash(description, createdBy, createdDate, id, lastExecutionDate, lastModifiedBy, lastModifiedDate, name, projectId, status);
   }
 
   @Override
   public String toString() {
     StringBuilder sb = new StringBuilder();
-    sb.append("class ProjectInstanceModel {\n");
-    sb.append("    configurationParameters: ").append(toIndentedString(configurationParameters)).append("\n");
-    sb.append("    connectionIds: ").append(toIndentedString(connectionIds)).append("\n");
-    sb.append("    connections: ").append(toIndentedString(connections)).append("\n");
+    sb.append("class ProjectInstanceBasicModel {\n");
     sb.append("    description: ").append(toIndentedString(description)).append("\n");
     sb.append("    createdBy: ").append(toIndentedString(createdBy)).append("\n");
     sb.append("    createdDate: ").append(toIndentedString(createdDate)).append("\n");
@@ -505,11 +323,8 @@ public class ProjectInstanceModel {
     sb.append("    lastModifiedBy: ").append(toIndentedString(lastModifiedBy)).append("\n");
     sb.append("    lastModifiedDate: ").append(toIndentedString(lastModifiedDate)).append("\n");
     sb.append("    name: ").append(toIndentedString(name)).append("\n");
-    sb.append("    project: ").append(toIndentedString(project)).append("\n");
     sb.append("    projectId: ").append(toIndentedString(projectId)).append("\n");
     sb.append("    status: ").append(toIndentedString(status)).append("\n");
-    sb.append("    tags: ").append(toIndentedString(tags)).append("\n");
-    sb.append("    version: ").append(toIndentedString(version)).append("\n");
     sb.append("}");
     return sb.toString();
   }
