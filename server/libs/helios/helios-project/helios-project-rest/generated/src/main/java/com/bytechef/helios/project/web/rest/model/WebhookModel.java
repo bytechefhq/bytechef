b570bdc7f8ac545aa460216ab74d3f79de493a32
Ivica Čardić <ivica.cardic@gmail.com>
2023-03-31 07:29:36 +0200

    227 - Generate

--- server/libs/helios/helios-project/helios-project-rest/generated/src/main/java/com/bytechef/helios/project/web/rest/model/WebhookModel.java
+++ server/libs/helios/helios-project/helios-project-rest/generated/src/main/java/com/bytechef/helios/project/web/rest/model/WebhookModel.java
@@ -22,7 +22,7 @@ import jakarta.annotation.Generated;
 
 @Schema(name = "Webhook", description = "Used to register to receive notifications for certain events.")
 @JsonTypeName("Webhook")
-@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2023-03-30T15:38:18.423931+02:00[Europe/Zagreb]")
+@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2023-03-31T07:22:37.259464+02:00[Europe/Zagreb]")
 public class WebhookModel {
 
   @JsonProperty("type")
