c441231b6c4055c81359bab66cc439360cbda10e
Ivica Čardić <ivica.cardic@gmail.com>
2023-03-31 12:03:56 +0200

    310 - Reorganize

---
+++ server/libs/atlas/atlas-rsocket-client/src/main/java/com/bytechef/atlas/rsocket/client/WorkflowServiceRSocketClient.java
@@ -0,0 +1,91 @@
+
+/*
+ * Copyright 2021 <your company/name>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef.atlas.rsocket.client;
+
+import com.bytechef.atlas.domain.Workflow;
+import com.bytechef.atlas.service.WorkflowService;
+import java.util.List;
+
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.messaging.rsocket.RSocketRequester;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Ivica Cardic
+ */
+@Component
+public class WorkflowServiceRSocketClient implements WorkflowService {
+
+    private final RSocketRequester rSocketRequester;
+
+    public WorkflowServiceRSocketClient(RSocketRequester rSocketRequester) {
+        this.rSocketRequester = rSocketRequester;
+    }
+
+    @Override
+    public Workflow create(String definition, Workflow.Format format, Workflow.SourceType sourceType) {
+        return rSocketRequester
+            .route("createWorkflow")
+            .data(new Workflow(definition, format, sourceType))
+            .retrieveMono(Workflow.class)
+            .block();
+    }
+
+    @Override
+    public void delete(String id) {
+        rSocketRequester.route("deleteWorkflow")
+            .data(id)
+            .send()
+            .block();
+    }
+
+    @Override
+    public Workflow getWorkflow(String id) {
+        return rSocketRequester
+            .route("getWorkflow")
+            .data(id)
+            .retrieveMono(Workflow.class)
+            .block();
+    }
+
+    @Override
+    public List<Workflow> getWorkflows(List<String> ids) {
+        return rSocketRequester
+            .route("getWorkflows")
+            .data(ids)
+            .retrieveMono(new ParameterizedTypeReference<List<Workflow>>() {})
+            .block();
+    }
+
+    @Override
+    public List<Workflow> getWorkflows() {
+        return rSocketRequester
+            .route("getWorkflows")
+            .retrieveMono(new ParameterizedTypeReference<List<Workflow>>() {})
+            .block();
+    }
+
+    @Override
+    public Workflow update(String id, String definition) {
+        return rSocketRequester
+            .route("updateWorkflow")
+            .data(new Workflow(id, definition))
+            .retrieveMono(Workflow.class)
+            .block();
+    }
+}
---
+++ server/libs/hermes/hermes-definition-registry/hermes-definition-registry-rsocket-client/src/main/java/com/bytechef/hermes/definition/registry/rsocket/client/TaskDispatcherDefinitionServiceRSocketClient.java
@@ -0,0 +1,67 @@
+
+/*
+ * Copyright 2021 <your company/name>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef.hermes.definition.registry.rsocket.client;
+
+import com.bytechef.hermes.definition.registry.service.TaskDispatcherDefinitionService;
+import com.bytechef.hermes.task.dispatcher.definition.TaskDispatcherDefinition;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.messaging.rsocket.RSocketRequester;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author Ivica Cardic
+ */
+@Component
+public class TaskDispatcherDefinitionServiceRSocketClient implements TaskDispatcherDefinitionService {
+
+    private final RSocketRequester rSocketRequester;
+
+    public TaskDispatcherDefinitionServiceRSocketClient(
+        @Qualifier("coordinatorRSocketRequester") RSocketRequester rSocketRequester) {
+
+        this.rSocketRequester = rSocketRequester;
+    }
+
+    @Override
+    public Mono<TaskDispatcherDefinition> getTaskDispatcherDefinitionMono(String name, Integer version) {
+        return rSocketRequester
+            .route("TaskDispatcherDefinitionService.getTaskDispatcherDefinition")
+            .data(Map.of("name", name, "version", version))
+            .retrieveMono(TaskDispatcherDefinition.class);
+    }
+
+    @Override
+    public Mono<List<TaskDispatcherDefinition>> getTaskDispatcherDefinitionsMono() {
+        return rSocketRequester
+            .route("TaskDispatcherDefinitionService.getTaskDispatcherDefinitions")
+            .retrieveMono(new ParameterizedTypeReference<>() {});
+    }
+
+    @Override
+    public Mono<List<TaskDispatcherDefinition>> getTaskDispatcherDefinitionsMono(String name) {
+        return rSocketRequester
+            .route("TaskDispatcherDefinitionService.getComponentDefinitionsForName")
+            .data(name)
+            .retrieveMono(new ParameterizedTypeReference<>() {});
+    }
+}
