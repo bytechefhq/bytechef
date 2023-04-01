0d7241f075a9451dcb1707bad049df72930c77dc
Ivica Čardić <ivica.cardic@gmail.com>
2023-04-01 08:12:57 +0200

    310 - Improve logging

--- server/libs/atlas/atlas-coordinator/atlas-coordinator-impl/src/main/java/com/bytechef/atlas/coordinator/job/executor/JobExecutor.java
+++ server/libs/atlas/atlas-coordinator/atlas-coordinator-impl/src/main/java/com/bytechef/atlas/coordinator/job/executor/JobExecutor.java
@@ -30,7 +30,10 @@ import com.bytechef.atlas.task.WorkflowTask;
 import com.bytechef.atlas.coordinator.task.dispatcher.TaskDispatcher;
 import com.bytechef.atlas.task.evaluator.TaskEvaluator;
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.util.Assert;
+import org.springframework.util.StringUtils;
 
 import java.util.List;
 import java.util.Map;
@@ -41,6 +44,8 @@ import java.util.Map;
  * @since Apr 24, 2017
  */
 public class JobExecutor {
+
+    private static final Logger log = LoggerFactory.getLogger(JobExecutor.class);
 
     private final ContextService contextService;
     private final TaskDispatcher<? super TaskExecution> taskDispatcher;
@@ -88,6 +93,16 @@ public class JobExecutor {
         contextService.push(nextTaskExecution.getId(), Context.Classname.TASK_EXECUTION, context);
 
         taskDispatcher.dispatch(nextTaskExecution);
+
+        if (log.isDebugEnabled()) {
+            if (!StringUtils.hasText(nextTaskExecution.getName())) {
+                log.debug("Task id={}, type='{}' executed", nextTaskExecution.getId(), nextTaskExecution.getType());
+            } else {
+                log.debug(
+                    "Task id={}, type='{}', name='{}' executed",
+                    nextTaskExecution.getId(), nextTaskExecution.getType(), nextTaskExecution.getName());
+            }
+        }
     }
 
     private boolean hasMoreTasks(Job job, Workflow workflow) {
