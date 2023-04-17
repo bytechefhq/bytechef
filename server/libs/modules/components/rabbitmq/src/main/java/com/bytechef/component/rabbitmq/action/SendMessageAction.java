52ac913396e927abbe48607364396ccbbdf02f81
Ivica Čardić <ivica.cardic@gmail.com>
2023-04-17 11:27:30 +0200

    272 - rabbitmq - Add initial rabbitmq connector

---
+++ server/libs/modules/components/rabbitmq/src/main/java/com/bytechef/component/rabbitmq/action/SendMessageAction.java
@@ -0,0 +1,80 @@
+
+/*
+ * Copyright 2021 <your company/name>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef.component.rabbitmq.action;
+
+import com.bytechef.component.rabbitmq.util.RabbitMqUtils;
+import com.bytechef.hermes.component.ActionContext;
+import com.bytechef.hermes.component.Context.Connection;
+import com.bytechef.hermes.component.InputParameters;
+import com.bytechef.hermes.component.definition.ActionDefinition;
+import com.bytechef.hermes.component.exception.ComponentExecutionException;
+import com.bytechef.hermes.component.util.JsonUtils;
+import com.rabbitmq.client.Channel;
+
+import java.nio.charset.StandardCharsets;
+
+import static com.bytechef.component.rabbitmq.constant.RabbitMqConstants.HOSTNAME;
+import static com.bytechef.component.rabbitmq.constant.RabbitMqConstants.MESSAGE;
+import static com.bytechef.component.rabbitmq.constant.RabbitMqConstants.PASSWORD;
+import static com.bytechef.component.rabbitmq.constant.RabbitMqConstants.PORT;
+import static com.bytechef.component.rabbitmq.constant.RabbitMqConstants.QUEUE;
+import static com.bytechef.component.rabbitmq.constant.RabbitMqConstants.USERNAME;
+import static com.bytechef.hermes.component.definition.ComponentDSL.action;
+import static com.bytechef.hermes.definition.DefinitionDSL.display;
+import static com.bytechef.hermes.definition.DefinitionDSL.oneOf;
+import static com.bytechef.hermes.definition.DefinitionDSL.string;
+
+/**
+ * @author Ivica Cardic
+ */
+public class SendMessageAction {
+
+    public static final ActionDefinition ACTION_DEFINITION = action("sendMessage")
+        .display(display("Send Message").description("Send a new RabbitMQ message."))
+        .properties(
+            string(QUEUE)
+                .description("The name of the queue to read from")
+                .required(true),
+            oneOf(MESSAGE)
+                .description("The name of the queue to read from")
+                .required(true))
+        .execute(SendMessageAction::execute);
+
+    protected static Object execute(ActionContext context, InputParameters inputParameters) {
+        Connection connection = context.getConnection();
+
+        try (com.rabbitmq.client.Connection rabbitMqConnection = RabbitMqUtils.getConnection(
+            connection.getString(HOSTNAME), connection.getInteger(PORT, 5672), connection.getString(USERNAME),
+            connection.getString(PASSWORD))) {
+
+            Channel channel = rabbitMqConnection.createChannel();
+
+            String queueName = inputParameters.getRequiredString(QUEUE);
+            String message = JsonUtils.write(inputParameters.getRequired(MESSAGE));
+
+            channel.queueDeclare(queueName, true, false, false, null);
+            channel.basicPublish("", queueName, null, message.getBytes(StandardCharsets.UTF_8));
+
+            channel.close();
+        } catch (Exception e) {
+            throw new ComponentExecutionException(e.getMessage(), e);
+        }
+
+        return null;
+    }
+}
---
+++ server/libs/modules/components/rabbitmq/src/main/java/com/bytechef/component/rabbitmq/connection/RabbitMQConnection.java
@@ -0,0 +1,40 @@
+
+/*
+ * Copyright 2021 <your company/name>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef.component.rabbitmq.connection;
+
+import com.bytechef.component.rabbitmq.constant.RabbitMqConstants;
+import com.bytechef.hermes.component.definition.ComponentDSL;
+
+import static com.bytechef.hermes.component.definition.ComponentDSL.connection;
+import static com.bytechef.hermes.definition.DefinitionDSL.integer;
+import static com.bytechef.hermes.definition.DefinitionDSL.string;
+
+/**
+ * @author Ivica Cardic
+ */
+public class RabbitMQConnection {
+
+    public static final ComponentDSL.ModifiableConnectionDefinition CONNECTION_DEFINITION = connection()
+        .properties(
+            string(RabbitMqConstants.HOSTNAME).required(true)
+                .defaultValue("localhost"),
+            integer(RabbitMqConstants.PORT).required(true)
+                .defaultValue(5672),
+            string(RabbitMqConstants.USERNAME),
+            string(RabbitMqConstants.PASSWORD));
+}
