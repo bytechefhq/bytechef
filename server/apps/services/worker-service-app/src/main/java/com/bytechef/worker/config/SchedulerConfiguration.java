24a004f05f48bda90c900e71f248eba5c9f71585
Ivica Čardić <ivica.cardic@gmail.com>
2023-04-17 11:28:53 +0200

    272 - Add db-scheduler configuration

---
+++ server/apps/services/worker-service-app/src/main/java/com/bytechef/worker/config/SchedulerConfiguration.java
@@ -0,0 +1,50 @@
+
+/*
+ * Copyright 2021 <your company/name>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef.worker.config;
+
+import com.github.kagkarlsson.scheduler.SchedulerName;
+import com.github.kagkarlsson.scheduler.boot.config.DbSchedulerCustomizer;
+import com.github.kagkarlsson.scheduler.serializer.JacksonSerializer;
+import com.github.kagkarlsson.scheduler.serializer.Serializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Optional;
+
+/**
+ * @author Ivica Cardic
+ */
+@Configuration
+public class SchedulerConfiguration {
+
+    @Bean
+    DbSchedulerCustomizer customizer() {
+        return new DbSchedulerCustomizer() {
+
+            @Override
+            public Optional<SchedulerName> schedulerName() {
+                return Optional.of(new SchedulerName.Fixed("spring-boot-scheduler-1"));
+            }
+
+            @Override
+            public Optional<Serializer> serializer() {
+                return Optional.of(new JacksonSerializer());
+            }
+        };
+    }
+}
