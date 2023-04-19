94d4ebcf85cf8d853dd0ca76ee9fe57e16044340
Ivica Čardić <ivica.cardic@gmail.com>
2023-04-19 15:11:32 +0200

    303 - client - Define icons for property types

--- client/src/pages/automation/project/components/node-details-tabs/OutputTab.tsx
+++ client/src/pages/automation/project/components/node-details-tabs/OutputTab.tsx
@@ -1,19 +1,35 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+/// <reference types="vite-plugin-svgr/client" />
+
 import {
-    CodeBracketSquareIcon,
-    HashtagIcon,
-    LanguageIcon,
-    ListBulletIcon,
-    VariableIcon,
-} from '@heroicons/react/24/outline';
 import {ActionDefinitionModel} from 'middleware/definition-registry';
 
+import {ReactComponent as ArrayIcon} from '../../../../../assets/array.svg';
+import {ReactComponent as BooleanIcon} from '../../../../../assets/boolean.svg';
+import {ReactComponent as DateIcon} from '../../../../../assets/date.svg';
+import {ReactComponent as DateTimeIcon} from '../../../../../assets/datetime.svg';
+import {ReactComponent as DynamicIcon} from '../../../../../assets/dynamic.svg';
+import {ReactComponent as IntegerIcon} from '../../../../../assets/integer.svg';
+import {ReactComponent as NullIcon} from '../../../../../assets/null.svg';
+import {ReactComponent as NumberIcon} from '../../../../../assets/number.svg';
+import {ReactComponent as ObjectIcon} from '../../../../../assets/object.svg';
+import {ReactComponent as OneOfIcon} from '../../../../../assets/oneof.svg';
+import {ReactComponent as StringIcon} from '../../../../../assets/string.svg';
+import {ReactComponent as TimeIcon} from '../../../../../assets/time.svg';
+
 const TYPE_ICONS = {
-    STRING: <LanguageIcon className="h-5 w-5 text-gray-600" />,
-    BOOLEAN: <VariableIcon className="h-5 w-5 text-gray-600" />,
-    INTEGER: <HashtagIcon className="h-5 w-5 text-gray-600" />,
-    ARRAY: <ListBulletIcon className="h-5 w-5 text-gray-600" />,
-    OBJECT: <CodeBracketSquareIcon className="h-5 w-5 text-gray-600" />,
+    ARRAY: <ArrayIcon className="h-5 w-5 text-gray-600" />,
+    BOOLEAN: <BooleanIcon className="h-5 w-5 text-gray-600" />,
+    DATE: <DateIcon className="h-5 w-5 text-gray-600" />,
+    DATE_TIME: <DateTimeIcon className="h-5 w-5 text-gray-600" />,
+    DYNAMIC_PROPERTIES: <DynamicIcon className="h-5 w-5 text-gray-600" />,
+    INTEGER: <IntegerIcon className="h-5 w-5 text-gray-600" />,
+    NUMBER: <NumberIcon className="h-5 w-5 text-gray-600" />,
+    NULL: <NullIcon className="h-5 w-5 text-gray-600" />,
+    OBJECT: <ObjectIcon className="h-5 w-5 text-gray-600" />,
+    ONE_OF: <OneOfIcon className="h-5 w-5 text-gray-600" />,
+    STRING: <StringIcon className="h-5 w-5 text-gray-600" />,
+    TIME: <TimeIcon className="h-5 w-5 text-gray-600" />,
 };
 
 const SchemaProperties = ({properties}: {properties: any}) => {
