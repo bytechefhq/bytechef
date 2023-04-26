2340d05ec5e5ba1e2f94167a49225ea46cc7db2c
Krešimir Čoko <kresimircoko@gmail.com>
2023-04-26 13:50:35 +0200

    303 Add Label to Checkbox

--- client/src/components/Checkbox/Checkbox.tsx
+++ client/src/components/Checkbox/Checkbox.tsx
@@ -1,28 +1,54 @@
 import {Indicator, Root} from '@radix-ui/react-checkbox';
-import {CheckIcon} from '@radix-ui/react-icons';
-import React from 'react';
+import {CheckIcon, QuestionMarkCircledIcon} from '@radix-ui/react-icons';
+import {Label} from '@radix-ui/react-label';
+import Tooltip from 'components/Tooltip/Tooltip';
+import {twMerge} from 'tailwind-merge';
 
 export interface CheckboxProps {
     defaultChecked?: boolean;
+    description?: string;
     disabled?: boolean;
     id: string;
+    label?: string;
 }
 
 const Checkbox = ({
     defaultChecked = true,
+    description,
     disabled = false,
     id,
+    label,
 }: CheckboxProps) => (
-    <Root
-        id={id}
-        defaultChecked={defaultChecked}
-        disabled={disabled}
-        className="flex h-5 w-5 items-center justify-center rounded focus:outline-none focus-visible:ring focus-visible:ring-blue-500/75 radix-state-checked:bg-blue-600 radix-state-unchecked:bg-gray-100 dark:radix-state-unchecked:bg-gray-900"
-    >
-        <Indicator>
-            <CheckIcon className="h-4 w-4 self-center text-white" />
-        </Indicator>
-    </Root>
+    <fieldset className="flex w-full items-center space-x-1 py-2">
+        <Root
+            id={id}
+            defaultChecked={defaultChecked}
+            disabled={disabled}
+            className="flex h-5 w-5 items-center justify-center rounded focus:outline-none focus-visible:ring focus-visible:ring-blue-500/75 radix-state-checked:bg-blue-600 radix-state-unchecked:bg-gray-100 dark:radix-state-unchecked:bg-gray-900"
+        >
+            <Indicator>
+                <CheckIcon className="h-4 w-4 self-center text-white" />
+            </Indicator>
+        </Root>
+
+        {label && (
+            <Label
+                className={twMerge(
+                    'flex items-center text-sm font-medium leading-6 text-gray-900',
+                    description && 'space-x-1'
+                )}
+                htmlFor={id}
+            >
+                <span>{label}</span>
+
+                {description && (
+                    <Tooltip text={description}>
+                        <QuestionMarkCircledIcon />
+                    </Tooltip>
+                )}
+            </Label>
+        )}
+    </fieldset>
 );
 
 export default Checkbox;
