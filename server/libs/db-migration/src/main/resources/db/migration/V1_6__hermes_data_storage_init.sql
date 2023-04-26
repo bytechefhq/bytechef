4867d03773b29588ef73944fab0c90286a447857
Ivica Čardić <ivica.cardic@gmail.com>
2023-04-26 08:45:23 +0200

    272 - Update DataStorage definition

--- server/libs/db-migration/src/main/resources/db/migration/V1_6__hermes_data_storage_init.sql
+++ server/libs/db-migration/src/main/resources/db/migration/V1_6__hermes_data_storage_init.sql
@@ -1,10 +1,8 @@
 CREATE TABLE IF NOT EXISTS data_storage (
     id                       BIGSERIAL    NOT NULL PRIMARY KEY,
-    workflow_id              VARCHAR(256) NULL,
-    instance_id              BIGINT       NULL,
-    job_id                   BIGINT       NULL,
-    key                      VARCHAR(256) NOT NULL,
     scope                    INT          NOT NULL,
+    scope_id                 VARCHAR(256) NULL,
+    key                      VARCHAR(256) NOT NULL,
     value                    TEXT         NOT NULL,
     created_date             TIMESTAMP    NOT NULL,
     created_by               VARCHAR(256) NOT NULL,
