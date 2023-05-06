c3f65d855e1a041ccd4c6e1abaa7c2f5f99a703c
Ivica Čardić <ivica.cardic@gmail.com>
2023-05-06 17:24:00 +0200

    272 - Move context based impl classes under hermes-definition-registry-service

---
+++ server/libs/hermes/hermes-definition-registry/hermes-definition-registry-service/src/main/java/com/bytechef/hermes/definition/registry/component/factory/ContextFactoryImpl.java
@@ -0,0 +1,77 @@
+
+/*
+ * Copyright 2021 <your company/name>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef.hermes.definition.registry.component.factory;
+
+import com.bytechef.commons.util.MapValueUtils;
+import com.bytechef.event.EventPublisher;
+import com.bytechef.hermes.component.ActionContext;
+import com.bytechef.hermes.component.TriggerContext;
+import com.bytechef.hermes.connection.service.ConnectionService;
+import com.bytechef.hermes.definition.registry.component.ContextImpl;
+import com.bytechef.hermes.definition.registry.component.definition.WebhookBodyImpl;
+import com.bytechef.hermes.definition.registry.component.definition.WebhookHeadersImpl;
+import com.bytechef.hermes.definition.registry.component.definition.WebhookParametersImpl;
+import com.bytechef.hermes.definition.registry.service.ConnectionDefinitionService;
+import com.bytechef.hermes.file.storage.service.FileStorageService;
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+
+import java.util.Map;
+
+/**
+ * @author Ivica Cardic
+ */
+public class ContextFactoryImpl implements ContextFactory {
+
+    static {
+        MapValueUtils.addConverter(new WebhookBodyImpl.WebhookBodyConverter());
+        MapValueUtils.addConverter(new WebhookHeadersImpl.WebhookHeadersConverter());
+        MapValueUtils.addConverter(new WebhookParametersImpl.WebhookParametersConverter());
+    }
+
+    private final ConnectionDefinitionService connectionDefinitionService;
+    private final ConnectionService connectionService;
+    private final EventPublisher eventPublisher;
+    private final FileStorageService fileStorageService;
+
+    @SuppressFBWarnings("EI")
+    public ContextFactoryImpl(
+        ConnectionDefinitionService connectionDefinitionService, ConnectionService connectionService,
+        EventPublisher eventPublisher, FileStorageService fileStorageService) {
+
+        this.connectionDefinitionService = connectionDefinitionService;
+        this.connectionService = connectionService;
+        this.eventPublisher = eventPublisher;
+        this.fileStorageService = fileStorageService;
+    }
+
+    @Override
+    public ActionContext createActionContext(Map<String, Long> connectionIdMap, Long taskExecutionId) {
+        return createContext(connectionIdMap, taskExecutionId);
+    }
+
+    @Override
+    public TriggerContext createTriggerContext(Map<String, Long> connectionIdMap) {
+        return createContext(connectionIdMap, null);
+    }
+
+    private ContextImpl createContext(Map<String, Long> connectionIdMap, Long taskExecutionId) {
+        return new ContextImpl(
+            connectionDefinitionService, connectionIdMap, connectionService, eventPublisher, fileStorageService,
+            taskExecutionId);
+    }
+}
