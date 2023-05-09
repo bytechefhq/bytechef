de7df0f2f3a8ccf64ce3d7ba6c520c5d16da4d49
Krešimir Čoko <kresimircoko@gmail.com>
2023-05-09 11:19:39 +0200

    303 Extract TYPE_ICONS into a shared file to prevent duplicate code

--- client/src/pages/automation/project/components/node-details-tabs/OutputTab.tsx
+++ client/src/pages/automation/project/components/node-details-tabs/OutputTab.tsx
@@ -1,36 +1,10 @@
 /// <reference types="vite-plugin-svgr/client" />
 
 import {PropertyModel} from 'middleware/core/definition-registry';
+import {TYPE_ICONS} from 'shared/typeIcons';
 
-import {ReactComponent as ArrayIcon} from '../../../../../assets/array.svg';
-import {ReactComponent as BooleanIcon} from '../../../../../assets/boolean.svg';
-import {ReactComponent as DateIcon} from '../../../../../assets/date.svg';
-import {ReactComponent as DateTimeIcon} from '../../../../../assets/datetime.svg';
-import {ReactComponent as DynamicIcon} from '../../../../../assets/dynamic.svg';
-import {ReactComponent as IntegerIcon} from '../../../../../assets/integer.svg';
-import {ReactComponent as NullIcon} from '../../../../../assets/null.svg';
-import {ReactComponent as NumberIcon} from '../../../../../assets/number.svg';
-import {ReactComponent as ObjectIcon} from '../../../../../assets/object.svg';
-import {ReactComponent as OneOfIcon} from '../../../../../assets/oneof.svg';
-import {ReactComponent as StringIcon} from '../../../../../assets/string.svg';
-import {ReactComponent as TimeIcon} from '../../../../../assets/time.svg';
 import {PropertyType} from '../../../../../types/projectTypes';
 import {useNodeDetailsDialogStore} from '../../stores/useNodeDetailsDialogStore';
-
-const TYPE_ICONS = {
-    ARRAY: <ArrayIcon className="h-5 w-5 text-gray-600" />,
-    BOOLEAN: <BooleanIcon className="h-5 w-5 text-gray-600" />,
-    DATE: <DateIcon className="h-5 w-5 text-gray-600" />,
-    DATE_TIME: <DateTimeIcon className="h-5 w-5 text-gray-600" />,
-    DYNAMIC_PROPERTIES: <DynamicIcon className="h-5 w-5 text-gray-600" />,
-    INTEGER: <IntegerIcon className="h-5 w-5 text-gray-600" />,
-    NUMBER: <NumberIcon className="h-5 w-5 text-gray-600" />,
-    NULL: <NullIcon className="h-5 w-5 text-gray-600" />,
-    OBJECT: <ObjectIcon className="h-5 w-5 text-gray-600" />,
-    ONE_OF: <OneOfIcon className="h-5 w-5 text-gray-600" />,
-    STRING: <StringIcon className="h-5 w-5 text-gray-600" />,
-    TIME: <TimeIcon className="h-5 w-5 text-gray-600" />,
-};
 
 const PropertyField = ({data, label}: {data: PropertyType; label: string}) => (
     <div className="inline-flex rounded-md p-1 text-sm hover:bg-gray-100">
@@ -67,7 +41,7 @@ const OutputTab = ({outputSchema}: {outputSchema: PropertyModel[]}) => {
     const {currentNode} = useNodeDetailsDialogStore();
 
     return (
-        <div className="max-h-full flex-[1_1_1px] overflow-auto">
+        <div className="max-h-full flex-[1_1_1px] overflow-auto p-4">
             <div className="mb-2 flex items-center">
                 <span>{TYPE_ICONS.OBJECT}</span>
 
