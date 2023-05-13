a0203999fd7fc45f8011c91910e2341fdd675d02
Ivica Čardić <ivica.cardic@gmail.com>
2023-05-13 10:36:56 +0200

    272 - cli - Remove absolute paths

--- cli/cli-app/generate_connectors.sh
+++ cli/cli-app/generate_connectors.sh
@@ -3,20 +3,20 @@
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 echo "Generate Airtable OpenAPI Connector"
-$SCRIPTPATH/bytechef.sh component init --open-api-path=/Volumes/data/bytechef/bytechef/server/libs/modules/components/airtable/openapi.yaml -o /Volumes/data/bytechef/bytechef/server/libs/modules/components airtable
+$SCRIPTPATH/bytechef.sh component init --open-api-path=$SCRIPTPATH/../../server/libs/modules/components/airtable/openapi.yaml -o $SCRIPTPATH/../../server/libs/modules/components airtable
 
 
 echo "Generate Jira OpenAPI Connector"
-$SCRIPTPATH/bytechef.sh component init --open-api-path=/Volumes/data/bytechef/bytechef/server/libs/modules/components/jira/openapi.yaml -o /Volumes/data/bytechef/bytechef/server/libs/modules/components jira
+$SCRIPTPATH/bytechef.sh component init --open-api-path=$SCRIPTPATH/../../server/libs/modules/components/jira/openapi.yaml -o $SCRIPTPATH/../../server/libs/modules/components jira
 
 echo "Generate MailChimp OpenAPI Connector"
-$SCRIPTPATH/bytechef.sh component init --open-api-path=/Volumes/data/bytechef/bytechef/server/libs/modules/components/mailchimp/openapi.yaml -o /Volumes/data/bytechef/bytechef/server/libs/modules/components mailchimp
+$SCRIPTPATH/bytechef.sh component init --open-api-path=$SCRIPTPATH/../../server/libs/modules/components/mailchimp/openapi.yaml -o $SCRIPTPATH/../../server/libs/modules/components mailchimp
 
 echo "Generate PetStore OpenAPI Connector"
-$SCRIPTPATH/bytechef.sh component init --open-api-path=/Volumes/data/bytechef/bytechef/server/libs/modules/components/petstore/openapi.yaml -o /Volumes/data/bytechef/bytechef/server/libs/modules/components petstore
+$SCRIPTPATH/bytechef.sh component init --open-api-path=$SCRIPTPATH/../../server/libs/modules/components/petstore/openapi.yaml -o $SCRIPTPATH/../../server/libs/modules/components petstore
 
 echo "Generate Pipedrive OpenAPI Connector"
-$SCRIPTPATH/bytechef.sh component init --open-api-path=/Volumes/data/bytechef/bytechef/server/libs/modules/components/pipedrive/openapi.yaml -o /Volumes/data/bytechef/bytechef/server/libs/modules/components pipedrive
+$SCRIPTPATH/bytechef.sh component init --open-api-path=$SCRIPTPATH/../../server/libs/modules/components/pipedrive/openapi.yaml -o $SCRIPTPATH/../../server/libs/modules/components pipedrive
 
 #echo "Generate Shopify OpenAPI Connector"
-#$SCRIPTPATH/bytechef.sh component init --open-api-path=/Volumes/data/bytechef/bytechef/server/libs/modules/components/shopify/openapi.yaml -o /Volumes/data/bytechef/bytechef/server/libs/modules/components shopify
+#$SCRIPTPATH/bytechef.sh component init --open-api-path=$SCRIPTPATH/../../server/libs/modules/components/shopify/openapi.yaml -o $SCRIPTPATH/../../server/libs/modules/components shopify
