6428280299ff3ab7fe841ebba14fa8901ef3e4a4
Ivica Čardić <ivica.cardic@gmail.com>
2023-06-03 09:15:56 +0200

    272 - components - Generate

--- server/libs/modules/components/jira/src/main/java/com/bytechef/component/jira/action/JiraGetIssueAction.java
+++ server/libs/modules/components/jira/src/main/java/com/bytechef/component/jira/action/JiraGetIssueAction.java
@@ -21,8 +21,14 @@ import static com.bytechef.hermes.component.OpenApiComponentHandler.PropertyType
 import static com.bytechef.hermes.component.definition.ComponentDSL.action;
 import static com.bytechef.hermes.component.definition.ComponentDSL.array;
 import static com.bytechef.hermes.component.definition.ComponentDSL.bool;
+import static com.bytechef.hermes.component.definition.ComponentDSL.date;
+import static com.bytechef.hermes.component.definition.ComponentDSL.dateTime;
+import static com.bytechef.hermes.component.definition.ComponentDSL.integer;
+import static com.bytechef.hermes.component.definition.ComponentDSL.nullable;
+import static com.bytechef.hermes.component.definition.ComponentDSL.number;
 import static com.bytechef.hermes.component.definition.ComponentDSL.object;
 import static com.bytechef.hermes.component.definition.ComponentDSL.string;
+import static com.bytechef.hermes.component.definition.ComponentDSL.time;
 import static com.bytechef.hermes.component.util.HttpClientUtils.ResponseFormat;
 
 import com.bytechef.component.jira.property.JiraIssueBeanProperties;
@@ -132,6 +138,8 @@ public class JiraGetIssueAction {
                     Map.of(
                         "type", PropertyType.QUERY)))
         .outputSchema(object().properties(JiraIssueBeanProperties.PROPERTIES)
+            .additionalProperties(
+                array(), bool(), date(), dateTime(), integer(), nullable(), number(), object(), string(), time())
             .description("Details about an issue.")
             .metadata(
                 Map.of(
