e9d077af04bd46e0b40f21bdf3a824c0078e0d3f
Ivica Cardic <ivica.cardic@liferay.com>
2023-06-09 13:04:33 +0200

    272 - client - Generate

--- client/src/middleware/automation/connection/models/index.ts
+++ client/src/middleware/automation/connection/models/index.ts
@@ -1,6 +1,5 @@
 /* tslint:disable */
 /* eslint-disable */
 export * from './ConnectionModel';
-export * from './OAuth2PropertiesModel';
 export * from './TagModel';
 export * from './UpdateTagsRequestModel';
---
+++ client/src/middleware/core/connection/models/index.ts
@@ -0,0 +1,3 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './OAuth2PropertiesModel';
