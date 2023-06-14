6355432a0c6de07eae50cbe2b00c42637e8a0655
Ivica Cardic <ivica.cardic@liferay.com>
2023-06-14 10:12:38 +0200

    272 - client - Generate

--- client/src/middleware/automation/configuration/apis/index.ts
+++ client/src/middleware/automation/configuration/apis/index.ts
@@ -2,6 +2,7 @@
 /* eslint-disable */
 export * from './ProjectCategoriesApi';
 export * from './ProjectInstanceTagsApi';
+export * from './ProjectInstanceWorkflowsApi';
 export * from './ProjectInstancesApi';
 export * from './ProjectTagsApi';
 export * from './ProjectsApi';
--- client/src/middleware/automation/execution/apis/index.ts
+++ client/src/middleware/automation/execution/apis/index.ts
@@ -1,4 +1,3 @@
 /* tslint:disable */
 /* eslint-disable */
-export * from './ProjectInstanceWorkflowJobsApi';
 export * from './ProjectWorkflowExecutionsApi';
