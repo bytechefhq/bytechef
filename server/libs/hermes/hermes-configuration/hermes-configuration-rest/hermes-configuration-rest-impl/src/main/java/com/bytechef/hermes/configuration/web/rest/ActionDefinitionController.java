6c45a691daa181d23ee2bbcdd04b1776adec347d
Ivica Čardić <ivica.cardic@gmail.com>
2023-06-23 07:55:58 +0200

    272 - Due to client generator issues, adjust action-definitions endpoint to receive array of task types(httpClient/v1/post,httpClient/v1/get)

--- server/libs/hermes/hermes-configuration/hermes-configuration-rest/hermes-configuration-rest-impl/src/main/java/com/bytechef/hermes/configuration/web/rest/ActionDefinitionController.java
+++ server/libs/hermes/hermes-configuration/hermes-configuration-rest/hermes-configuration-rest-impl/src/main/java/com/bytechef/hermes/configuration/web/rest/ActionDefinitionController.java
@@ -18,21 +18,14 @@
 package com.bytechef.hermes.configuration.web.rest;
 
 import com.bytechef.hermes.configuration.web.rest.model.ActionDefinitionModel;
-import com.bytechef.hermes.configuration.web.rest.model.GetActionDefinitionsTaskTypesParameterInnerModel;
-import com.bytechef.hermes.definition.registry.dto.ComponentOperation;
 import com.bytechef.hermes.definition.registry.service.ActionDefinitionService;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.bytechef.hermes.definition.registry.util.WorkflowUtils;
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import org.springframework.core.convert.ConversionService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.beans.PropertyEditorSupport;
 import java.util.List;
 
 /**
@@ -44,61 +37,24 @@ public class ActionDefinitionController implements ActionDefinitionsApi {
 
     private final ActionDefinitionService actionDefinitionService;
     private final ConversionService conversionService;
-    private final ObjectMapper objectMapper;
-
-    @InitBinder
-    public void initBinder(WebDataBinder binder) {
-        binder.registerCustomEditor(
-            GetActionDefinitionsTaskTypesParameterInnerModel.class,
-            new GetActionDefinitionsTaskTypesParameterInnerModelPropertyEditorSupport(objectMapper));
-    }
 
     @SuppressFBWarnings("EI")
     public ActionDefinitionController(
-        ActionDefinitionService actionDefinitionService, ConversionService conversionService,
-        ObjectMapper objectMapper) {
+        ActionDefinitionService actionDefinitionService, ConversionService conversionService) {
 
         this.actionDefinitionService = actionDefinitionService;
         this.conversionService = conversionService;
-        this.objectMapper = objectMapper;
     }
 
     @Override
-    public ResponseEntity<List<ActionDefinitionModel>> getActionDefinitions(
-        List<GetActionDefinitionsTaskTypesParameterInnerModel> taskTypesParameterInnerModels) {
-
+    public ResponseEntity<List<ActionDefinitionModel>> getActionDefinitions(List<String> taskTypes) {
         return ResponseEntity.ok(
             actionDefinitionService.getActionDefinitions(
-                taskTypesParameterInnerModels.stream()
-                    .map(taskTypesParameterInnerModel -> new ComponentOperation(
-                        taskTypesParameterInnerModel.getComponentName(),
-                        taskTypesParameterInnerModel.getComponentVersion(),
-                        taskTypesParameterInnerModel.getActionName()))
+                taskTypes.stream()
+                    .map(WorkflowUtils::getComponentOperation)
                     .toList())
                 .stream()
                 .map(action -> conversionService.convert(action, ActionDefinitionModel.class))
                 .toList());
-    }
-
-    public static class GetActionDefinitionsTaskTypesParameterInnerModelPropertyEditorSupport
-        extends PropertyEditorSupport {
-
-        private final ObjectMapper objectMapper;
-
-        @SuppressFBWarnings("EI")
-        public GetActionDefinitionsTaskTypesParameterInnerModelPropertyEditorSupport(ObjectMapper objectMapper) {
-            this.objectMapper = objectMapper;
-        }
-
-        @Override
-        public void setAsText(String text) throws IllegalArgumentException {
-            try {
-                setValue(
-                    objectMapper.readValue(
-                        text, new TypeReference<GetActionDefinitionsTaskTypesParameterInnerModel>() {}));
-            } catch (JsonProcessingException e) {
-                throw new RuntimeException(e);
-            }
-        }
     }
 }
