0b5cc1f501b44319ed74ae639d923cab20d730dc
Ivica Čardić <ivica.cardic@gmail.com>
2023-07-04 22:07:44 +0200

    415 - client - Update to shadcn component library

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -7,186 +7,73 @@ module.exports = {
     darkMode: 'class',
     important: true,
     theme: {
-        fontFamily: {
-            display: [
-                'Inter',
-                '-apple-system',
-                'BlinkMacSystemFont',
-                'Segoe UI',
-                'Roboto',
-                'Helvetica Neue',
-                'Arial',
-                'Noto Sans',
-                'sans-serif',
-                'Apple Color Emoji',
-                'Segoe UI Emoji',
-                'Segoe UI Symbol',
-                'Noto Color Emoji',
-            ],
+        container: {
+            center: true,
+            padding: '2rem',
+            screens: {
+                '2xl': '1400px',
+            },
         },
         extend: {
-            animation: {
-                // Dropdown menu
-                'scale-in': 'scale-in 0.2s ease-in-out',
-                'slide-down': 'slide-down 0.6s cubic-bezier(0.16, 1, 0.3, 1)',
-                'slide-up': 'slide-up 0.6s cubic-bezier(0.16, 1, 0.3, 1)',
-                // Tooltip
-                'slide-up-fade':
-                    'slide-up-fade 0.4s cubic-bezier(0.16, 1, 0.3, 1)',
-                'slide-right-fade':
-                    'slide-right-fade 0.4s cubic-bezier(0.16, 1, 0.3, 1)',
-                'slide-down-fade':
-                    'slide-down-fade 0.4s cubic-bezier(0.16, 1, 0.3, 1)',
-                'slide-left-fade':
-                    'slide-left-fade 0.4s cubic-bezier(0.16, 1, 0.3, 1)',
-                // Navigation menu
-                'enter-from-right': 'enter-from-right 0.25s ease',
-                'enter-from-left': 'enter-from-left 0.25s ease',
-                'exit-to-right': 'exit-to-right 0.25s ease',
-                'exit-to-left': 'exit-to-left 0.25s ease',
-                'scale-in-content': 'scale-in-content 0.2s ease',
-                'scale-out-content': 'scale-out-content 0.2s ease',
-                'fade-in': 'fade-in 0.2s ease',
-                'fade-out': 'fade-out 0.2s ease',
-                // Toast
-                'toast-hide': 'toast-hide 100ms ease-in forwards',
-                'toast-slide-in-right':
-                    'toast-slide-in-right 150ms cubic-bezier(0.16, 1, 0.3, 1)',
-                'toast-slide-in-bottom':
-                    'toast-slide-in-bottom 150ms cubic-bezier(0.16, 1, 0.3, 1)',
-                'toast-swipe-out': 'toast-swipe-out 100ms ease-out forwards',
+            colors: {
+                border: 'hsl(var(--border))',
+                input: 'hsl(var(--input))',
+                ring: 'hsl(var(--ring))',
+                background: 'hsl(var(--background))',
+                foreground: 'hsl(var(--foreground))',
+                primary: {
+                    DEFAULT: 'hsl(var(--primary))',
+                    foreground: 'hsl(var(--primary-foreground))',
+                },
+                secondary: {
+                    DEFAULT: 'hsl(var(--secondary))',
+                    foreground: 'hsl(var(--secondary-foreground))',
+                },
+                destructive: {
+                    DEFAULT: 'hsl(var(--destructive))',
+                    foreground: 'hsl(var(--destructive-foreground))',
+                },
+                muted: {
+                    DEFAULT: 'hsl(var(--muted))',
+                    foreground: 'hsl(var(--muted-foreground))',
+                },
+                accent: {
+                    DEFAULT: 'hsl(var(--accent))',
+                    foreground: 'hsl(var(--accent-foreground))',
+                },
+                popover: {
+                    DEFAULT: 'hsl(var(--popover))',
+                    foreground: 'hsl(var(--popover-foreground))',
+                },
+                card: {
+                    DEFAULT: 'hsl(var(--card))',
+                    foreground: 'hsl(var(--card-foreground))',
+                },
             },
-            boxShadow: {
-                slider: '0 0 0 5px rgba(0, 0, 0, 0.3)',
-            },
-            gridColumn: {
-                'span-24': 'span 24 / span 24',
-                24: '24 / 24',
+            borderRadius: {
+                lg: 'var(--radius)',
+                md: 'calc(var(--radius) - 2px)',
+                sm: 'calc(var(--radius) - 4px)',
             },
             keyframes: {
-                // Dropdown menu
-                'scale-in': {
-                    '0%': {opacity: 0, transform: 'scale(0)'},
-                    '100%': {opacity: 1, transform: 'scale(1)'},
+                'accordion-down': {
+                    from: {height: 0},
+                    to: {height: 'var(--radix-accordion-content-height)'},
                 },
-                'slide-down': {
-                    '0%': {opacity: 0, transform: 'translateY(-10px)'},
-                    '100%': {opacity: 1, transform: 'translateY(0)'},
-                },
-                'slide-up': {
-                    '0%': {opacity: 0, transform: 'translateY(10px)'},
-                    '100%': {opacity: 1, transform: 'translateY(0)'},
-                },
-                // Tooltip
-                'slide-up-fade': {
-                    '0%': {opacity: 0, transform: 'translateY(2px)'},
-                    '100%': {opacity: 1, transform: 'translateY(0)'},
-                },
-                'slide-right-fade': {
-                    '0%': {opacity: 0, transform: 'translateX(-2px)'},
-                    '100%': {opacity: 1, transform: 'translateX(0)'},
-                },
-                'slide-down-fade': {
-                    '0%': {opacity: 0, transform: 'translateY(-2px)'},
-                    '100%': {opacity: 1, transform: 'translateY(0)'},
-                },
-                'slide-left-fade': {
-                    '0%': {opacity: 0, transform: 'translateX(2px)'},
-                    '100%': {opacity: 1, transform: 'translateX(0)'},
-                },
-                // Navigation menu
-                'enter-from-right': {
-                    '0%': {transform: 'translateX(200px)', opacity: 0},
-                    '100%': {transform: 'translateX(0)', opacity: 1},
-                },
-                'enter-from-left': {
-                    '0%': {transform: 'translateX(-200px)', opacity: 0},
-                    '100%': {transform: 'translateX(0)', opacity: 1},
-                },
-                'exit-to-right': {
-                    '0%': {transform: 'translateX(0)', opacity: 1},
-                    '100%': {transform: 'translateX(200px)', opacity: 0},
-                },
-                'exit-to-left': {
-                    '0%': {transform: 'translateX(0)', opacity: 1},
-                    '100%': {transform: 'translateX(-200px)', opacity: 0},
-                },
-                'scale-in-content': {
-                    '0%': {transform: 'rotateX(-30deg) scale(0.9)', opacity: 0},
-                    '100%': {transform: 'rotateX(0deg) scale(1)', opacity: 1},
-                },
-                'scale-out-content': {
-                    '0%': {transform: 'rotateX(0deg) scale(1)', opacity: 1},
-                    '100%': {
-                        transform: 'rotateX(-10deg) scale(0.95)',
-                        opacity: 0,
-                    },
-                },
-                'fade-in': {
-                    '0%': {opacity: 0},
-                    '100%': {opacity: 1},
-                },
-                'fade-out': {
-                    '0%': {opacity: 1},
-                    '100%': {opacity: 0},
-                },
-                // Toast
-                'toast-hide': {
-                    '0%': {opacity: 1},
-                    '100%': {opacity: 0},
-                },
-                'toast-slide-in-right': {
-                    '0%': {transform: `translateX(calc(100% + 1rem))`},
-                    '100%': {transform: 'translateX(0)'},
-                },
-                'toast-slide-in-bottom': {
-                    '0%': {transform: `translateY(calc(100% + 1rem))`},
-                    '100%': {transform: 'translateY(0)'},
-                },
-                'toast-swipe-out': {
-                    '0%': {
-                        transform: 'translateX(var(--radix-toast-swipe-end-x))',
-                    },
-                    '100%': {
-                        transform: `translateX(calc(100% + 1rem))`,
-                    },
+                'accordion-up': {
+                    from: {height: 'var(--radix-accordion-content-height)'},
+                    to: {height: 0},
                 },
             },
-            maxHeight: {
-                'select-content-available-height':
-                    'var(--radix-select-content-available-height)',
-                'select-content-available-height-1/2':
-                    'calc(var(--radix-select-content-available-height) / 2)',
-            },
-            maxWidth: {
-                'select-trigger-width': 'var(--radix-select-trigger-width)',
-            },
-            minWidth: {
-                'select-trigger-width': 'var(--radix-select-trigger-width)',
-            },
-            origin: {
-                'select-content-transform-origin':
-                    '--radix-select-content-transform-origin',
-            },
-            width: {
-                112: '30rem',
-                'select-trigger-width': 'var(--radix-select-trigger-width)',
+            animation: {
+                'accordion-down': 'accordion-down 0.2s ease-out',
+                'accordion-up': 'accordion-up 0.2s ease-out',
             },
         },
-    },
-    variants: {
-        extend: {},
     },
     plugins: [
         require('@headlessui/tailwindcss'),
+        require('tailwindcss-animate'),
         require('@tailwindcss/forms'),
-        require('tailwindcss-radix'),
-        plugin(function ({addBase, theme}) {
-            addBase({
-                h1: {fontSize: theme('fontSize.2xl')},
-                h2: {fontSize: theme('fontSize.xl')},
-                h3: {fontSize: theme('fontSize.lg')},
-            });
-        }),
     ],
 };
