527b25c03425edc6df22120f8ee79b9d9d6adc9a
Ivica Čardić <ivica.cardic@gmail.com>
2023-07-12 18:10:53 +0200

    272 - Generate

--- server/libs/helios/helios-configuration/helios-configuration-rest/helios-configuration-rest-api/generated/src/main/java/com/bytechef/helios/configuration/web/rest/model/InputModel.java
+++ server/libs/helios/helios-configuration/helios-configuration-rest/helios-configuration-rest-api/generated/src/main/java/com/bytechef/helios/configuration/web/rest/model/InputModel.java
@@ -20,7 +20,7 @@ import jakarta.annotation.Generated;
  */
 
 @JsonTypeName("Input")
-@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2023-07-12T16:26:39.213536+02:00[Europe/Zagreb]")
+@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2023-07-12T17:48:51.717386+02:00[Europe/Zagreb]")
 public class InputModel {
 
   private String label;
--- server/libs/helios/helios-configuration/helios-configuration-rest/helios-configuration-rest-api/generated/src/main/java/com/bytechef/helios/configuration/web/rest/model/ProjectInstanceBasicModel.java
+++ server/libs/helios/helios-configuration/helios-configuration-rest/helios-configuration-rest-api/generated/src/main/java/com/bytechef/helios/configuration/web/rest/model/ProjectInstanceBasicModel.java
@@ -23,7 +23,7 @@ import jakarta.annotation.Generated;
 
 @Schema(name = "ProjectInstanceBasic", description = "Contains configurations and connections required for the execution of project workflows.")
 @JsonTypeName("ProjectInstanceBasic")
-@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2023-07-12T16:26:39.213536+02:00[Europe/Zagreb]")
+@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2023-07-12T17:48:51.717386+02:00[Europe/Zagreb]")
 public class ProjectInstanceBasicModel {
 
   private String description;
