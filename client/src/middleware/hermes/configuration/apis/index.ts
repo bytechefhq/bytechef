ac7d8ba1f784faf6233946c0f4b965dfdc07c6ca
Ivica Čardić <ivica.cardic@gmail.com>
2023-07-21 07:56:41 +0200

    272 - client - Generate

--- client/src/middleware/hermes/configuration/apis/index.ts
+++ client/src/middleware/hermes/configuration/apis/index.ts
@@ -3,5 +3,6 @@
 export * from './ActionDefinitionsApi';
 export * from './ComponentDefinitionsApi';
 export * from './ConnectionDefinitionsApi';
+export * from './Oauth2Api';
 export * from './TaskDispatcherDefinitionsApi';
 export * from './TriggerDefinitionsApi';
