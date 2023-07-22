72365ec6a307b2245299c8b5754e4ba35686c709
Ivica Čardić <ivica.cardic@gmail.com>
2023-07-22 21:28:07 +0200

    272 - Generate

--- server/libs/helios/helios-execution/helios-execution-rest/generated/src/main/java/com/bytechef/helios/execution/web/rest/model/TaskExecutionModel.java
+++ server/libs/helios/helios-execution/helios-execution-rest/generated/src/main/java/com/bytechef/helios/execution/web/rest/model/TaskExecutionModel.java
@@ -28,7 +28,7 @@ import jakarta.annotation.Generated;
 
 @Schema(name = "TaskExecution", description = "Adds execution semantics to a task.")
 @JsonTypeName("TaskExecution")
-@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2023-07-22T20:55:03.230135+02:00[Europe/Zagreb]")
+@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2023-07-22T21:27:43.318208+02:00[Europe/Zagreb]")
 public class TaskExecutionModel {
 
   private ComponentDefinitionModel component;
