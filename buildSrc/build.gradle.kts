a8c0590ba264dad6f95106d2c5fa3726fd5ec432
Ivica Čardić <ivica.cardic@gmail.com>
2023-08-23 07:49:00 +0200

    272 - Initial support for Žžkotlin in gradle scripts

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -4,7 +4,9 @@
 
 plugins {
     // Support convention plugins written in Groovy. Convention plugins are build scripts in 'src/main' that automatically become available as plugins in the main build.
-    id 'groovy-gradle-plugin'
+    `groovy-gradle-plugin`
+    // Support convention plugins written in Kotlin. Convention plugins are build scripts in 'src/main' that automatically become available as plugins in the main build.
+    `kotlin-dsl`
 }
 
 repositories {
