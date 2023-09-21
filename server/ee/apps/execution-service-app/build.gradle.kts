4adc0854a02576db5842092e06596d0be9bcc856
Ivica Čardić <ivica.cardic@gmail.com>
2023-09-21 18:45:01 +0200

    272 - Add Swagger groups to separate api in UI

--- server/ee/apps/execution-service-app/build.gradle.kts
+++ server/ee/apps/execution-service-app/build.gradle.kts
@@ -7,6 +7,7 @@ springBoot {
 
 dependencies {
     implementation(libs.org.openapitools.jackson.databind.nullable)
+    implementation(libs.org.springdoc.springdoc.openapi.starter.common)
     implementation("org.springframework:spring-webflux")
     implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
     implementation("org.springframework.boot:spring-boot-starter-web")
