42707c4bbebfed086f960c972151040e6fdcb1e9
Krešimir Čoko <kresimircoko@gmail.com>
2023-09-23 14:27:58 +0200

    272 - Fix usage of vite-plugin-svgr to render icons once again

--- client/src/shared/typeIcons.tsx
+++ client/src/shared/typeIcons.tsx
@@ -1,77 +1,27 @@
-/// <reference types="vite-plugin-svgr/client" />
-
-import AnyIcon from 'assets/any.svg';
-import ArrayIcon from 'assets/array.svg';
-import BooleanIcon from 'assets/boolean.svg';
-import DateIcon from 'assets/date.svg';
-import DateTimeIcon from 'assets/datetime.svg';
-import DynamicIcon from 'assets/dynamic.svg';
-import IntegerIcon from 'assets/integer.svg';
-import NullIcon from 'assets/null.svg';
-import NumberIcon from 'assets/number.svg';
-import ObjectIcon from 'assets/object.svg';
-import StringIcon from 'assets/string.svg';
-import TimeIcon from 'assets/time.svg';
+import AnyIcon from 'assets/any.svg?react';
+import ArrayIcon from 'assets/array.svg?react';
+import BooleanIcon from 'assets/boolean.svg?react';
+import DateIcon from 'assets/date.svg?react';
+import DateTimeIcon from 'assets/datetime.svg?react';
+import DynamicIcon from 'assets/dynamic.svg?react';
+import IntegerIcon from 'assets/integer.svg?react';
+import NullIcon from 'assets/null.svg?react';
+import NumberIcon from 'assets/number.svg?react';
+import ObjectIcon from 'assets/object.svg?react';
+import StringIcon from 'assets/string.svg?react';
+import TimeIcon from 'assets/time.svg?react';
 
 export const TYPE_ICONS = {
-    ANY: (
-        <span className="h-4 w-4 text-gray-600">
-            <AnyIcon />
-        </span>
-    ),
-    ARRAY: (
-        <span className="h-4 w-4 text-gray-600">
-            <ArrayIcon />
-        </span>
-    ),
-    BOOLEAN: (
-        <span className="h-4 w-4 text-gray-600">
-            <BooleanIcon />
-        </span>
-    ),
-    DATE: (
-        <span className="h-4 w-4 text-gray-600">
-            <DateIcon />
-        </span>
-    ),
-    DATE_TIME: (
-        <span className="h-4 w-4 text-gray-600">
-            <DateTimeIcon />
-        </span>
-    ),
-    DYNAMIC_PROPERTIES: (
-        <span className="h-4 w-4 text-gray-600">
-            <DynamicIcon />
-        </span>
-    ),
-    INTEGER: (
-        <span className="h-4 w-4 text-gray-600">
-            <IntegerIcon />
-        </span>
-    ),
-    NULL: (
-        <span className="h-4 w-4 text-gray-600">
-            <NullIcon />
-        </span>
-    ),
-    NUMBER: (
-        <span className="h-4 w-4 text-gray-600">
-            <NumberIcon />
-        </span>
-    ),
-    OBJECT: (
-        <span className="h-4 w-4 text-gray-600">
-            <ObjectIcon />
-        </span>
-    ),
-    STRING: (
-        <span className="h-4 w-4 text-gray-600">
-            <StringIcon />
-        </span>
-    ),
-    TIME: (
-        <span className="h-4 w-4 text-gray-600">
-            <TimeIcon />
-        </span>
-    ),
+    ANY: <AnyIcon className="h-4 w-4 text-gray-600" />,
+    ARRAY: <ArrayIcon className="h-4 w-4 text-gray-600" />,
+    BOOLEAN: <BooleanIcon className="h-4 w-4 text-gray-600" />,
+    DATE: <DateIcon className="h-4 w-4 text-gray-600" />,
+    DATE_TIME: <DateTimeIcon className="h-4 w-4 text-gray-600" />,
+    DYNAMIC_PROPERTIES: <DynamicIcon className="h-4 w-4 text-gray-600" />,
+    INTEGER: <IntegerIcon className="h-4 w-4 text-gray-600" />,
+    NULL: <NullIcon className="h-4 w-4 text-gray-600" />,
+    NUMBER: <NumberIcon className="h-4 w-4 text-gray-600" />,
+    OBJECT: <ObjectIcon className="h-4 w-4 text-gray-600" />,
+    STRING: <StringIcon className="h-4 w-4 text-gray-600" />,
+    TIME: <TimeIcon className="h-4 w-4 text-gray-600" />,
 };
