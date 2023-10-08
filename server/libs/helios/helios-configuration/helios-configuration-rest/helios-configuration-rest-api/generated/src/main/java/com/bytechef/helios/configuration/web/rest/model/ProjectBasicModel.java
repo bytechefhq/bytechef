483a7862aabca347b2b52ee99126b9ff3f15f871
Ivica Čardić <ivica.cardic@gmail.com>
2023-10-08 22:28:08 +0200

    440 - Generate

--- server/libs/helios/helios-configuration/helios-configuration-rest/helios-configuration-rest-api/generated/src/main/java/com/bytechef/helios/configuration/web/rest/model/ProjectBasicModel.java
+++ server/libs/helios/helios-configuration/helios-configuration-rest/helios-configuration-rest-api/generated/src/main/java/com/bytechef/helios/configuration/web/rest/model/ProjectBasicModel.java
@@ -24,7 +24,7 @@ import jakarta.annotation.Generated;
 
 @Schema(name = "ProjectBasic", description = "A group of workflows that make one logical project.")
 @JsonTypeName("ProjectBasic")
-@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2023-10-06T20:36:52.146752+02:00[Europe/Zagreb]")
+@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2023-10-08T22:26:46.976326+02:00[Europe/Zagreb]")
 public class ProjectBasicModel {
 
   private String createdBy;
