18b7884aa1b3b75205c50a6c4b42e76cd4679dfc
Ivica Čardić <ivica.cardic@gmail.com>
2023-10-10 17:18:30 +0200

    0 - Update license

--- buildSrc/src/main/kotlin/com.bytechef.java-common-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/com.bytechef.java-common-conventions.gradle.kts
@@ -121,7 +121,7 @@ spotless {
         licenseHeader(
             """
             /*
-             * Copyright 2021 <your company/name>.
+             * Copyright 2023-present ByteChef Inc.
              *
              * Licensed under the Apache License, Version 2.0 (the "License");
              * you may not use this file except in compliance with the License.
@@ -142,7 +142,7 @@ spotless {
         licenseHeader(
             """
             /*
-             * Copyright 2016-2018 the original author or authors.
+             * Copyright 2016-2020 the original author or authors.
              *
              * Licensed under the Apache License, Version 2.0 (the "License");
              * you may not use this file except in compliance with the License.
@@ -156,7 +156,7 @@ spotless {
              * See the License for the specific language governing permissions and
              * limitations under the License.
              *
-             * Modifications copyright (C) 2021 <your company/name>
+             * Modifications copyright (C) 2023 ByteChef Inc.
              */
             """
         ).named(
