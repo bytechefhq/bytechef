4a781ef620cca68ba857580899a6c794c05e972a
Ivica Čardić <ivica.cardic@gmail.com>
2023-10-12 17:54:21 +0200

    0 - Update licenses formatting

--- buildSrc/src/main/kotlin/com.bytechef.java-common-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/com.bytechef.java-common-conventions.gradle.kts
@@ -109,7 +109,7 @@ spotbugs {
 spotless {
     format("misc") {
         // define the files to apply `misc` to
-        target("*.gradle", "*.md", ".gitignore", "*.java")
+        target("*.gradle", "*.md", ".gitignore")
 
         trimTrailingWhitespace()
         indentWithSpaces(4)
@@ -119,60 +119,65 @@ spotless {
         target("src/*/java/**/*.java")
 
         licenseHeader(
-            """/*
-             * Copyright 2023-present ByteChef Inc.
-             *
-             * Licensed under the Apache License, Version 2.0 (the "License");
-             * you may not use this file except in compliance with the License.
-             * You may obtain a copy of the License at
-             *
-             *      https://www.apache.org/licenses/LICENSE-2.0
-             *
-             * Unless required by applicable law or agreed to in writing, software
-             * distributed under the License is distributed on an "AS IS" BASIS,
-             * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-             * See the License for the specific language governing permissions and
-             * limitations under the License.
-             */
-            """
+            "/*\n" +
+                " * Copyright 2023-present ByteChef Inc.\n" +
+                " *\n" +
+                " * Licensed under the Apache License, Version 2.0 (the \"License\");\n" +
+                " * you may not use this file except in compliance with the License.\n" +
+                " * You may obtain a copy of the License at\n" +
+                " *\n" +
+                " *      https://www.apache.org/licenses/LICENSE-2.0\n" +
+                " *\n" +
+                " * Unless required by applicable law or agreed to in writing, software\n" +
+                " * distributed under the License is distributed on an \"AS IS\" BASIS,\n" +
+                " * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\n" +
+                " * See the License for the specific language governing permissions and\n" +
+                " * limitations under the License.\n" +
+                " */\n\n"
         ).named(
             "standard"
         )
         licenseHeader(
-            """/*
-             * Copyright 2016-2020 the original author or authors.
-             *
-             * Licensed under the Apache License, Version 2.0 (the "License");
-             * you may not use this file except in compliance with the License.
-             * You may obtain a copy of the License at
-             *
-             *      https://www.apache.org/licenses/LICENSE-2.0
-             *
-             * Unless required by applicable law or agreed to in writing, software
-             * distributed under the License is distributed on an "AS IS" BASIS,
-             * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-             * See the License for the specific language governing permissions and
-             * limitations under the License.
-             *
-             * Modifications copyright (C) 2023 ByteChef Inc.
-             */
-            """
+            "/*\n" +
+                " * Copyright 2016-2020 the original author or authors.\n" +
+                " *\n" +
+                " * Licensed under the Apache License, Version 2.0 (the \"License\");\n" +
+                " * you may not use this file except in compliance with the License.\n" +
+                " * You may obtain a copy of the License at\n" +
+                " *\n" +
+                " *      https://www.apache.org/licenses/LICENSE-2.0\n" +
+                " *\n" +
+                " * Unless required by applicable law or agreed to in writing, software\n" +
+                " * distributed under the License is distributed on an \"AS IS\" BASIS,\n" +
+                " * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\n" +
+                " * See the License for the specific language governing permissions and\n" +
+                " * limitations under the License.\n" +
+                " *\n" +
+                " * Modifications copyright (C) 2023 ByteChef Inc.\n" +
+                " */\n\n"
         ).named(
             "original"
         ).onlyIfContentMatches(
             "author\\sArik\\sCohen"
         )
-        eclipse().configFile("$rootDir/config/eclipse/eclipse-code-formatter-settings.xml")
-        removeUnusedImports()
-    }
-    java {
-        target("ee/**/src/*/java/**/*.java")
-
         licenseHeader(
-            "\n/*\n * Copyright 2023-present ByteChef Inc.\n *\n * Licensed under the ByteChef Enterprise license (the \"Enterprise License\");\n * you may not use this file except in compliance with the Enterprise License.\n */\n\n"
+            "/*\n" +
+                " * Copyright 2023-present ByteChef Inc.\n" +
+                " *\n" +
+                " * Licensed under the ByteChef Enterprise license (the \"Enterprise License\");\n" +
+                " * you may not use this file except in compliance with the Enterprise License.\n" +
+                " */\n\n"
         ).named(
             "ee"
+        ).onlyIfContentMatches(
+            "version\\see\\s"
         )
+        eclipse().configFile("$rootDir/config/eclipse/eclipse-code-formatter-settings.xml")
+        importOrder()
+        removeUnusedImports()
+        trimTrailingWhitespace()
+        indentWithSpaces(4)
+        endWithNewline()
     }
     yaml {
         target("cli/**/src/**/*.yaml", "server/**/src/**/*.yaml")
