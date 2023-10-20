4be3b4d72b9bed99544f54ce92e918af546a2558
Ivica Čardić <ivica.cardic@gmail.com>
2023-10-20 17:18:43 +0000

    480 - Add HTML helper component

--- server/apps/server-app/build.gradle.kts
+++ server/apps/server-app/build.gradle.kts
@@ -72,6 +72,7 @@ dependencies {
     implementation(project(":server:libs:modules:components:delay"))
     implementation(project(":server:libs:modules:components:filestorage"))
     implementation(project(":server:libs:modules:components:email"))
+    implementation(project(":server:libs:modules:components:htmlhelper"))
     implementation(project(":server:libs:modules:components:httpclient"))
     implementation(project(":server:libs:modules:components:hubspot"))
     implementation(project(":server:libs:modules:components:jira"))
