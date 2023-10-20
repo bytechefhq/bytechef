3cda4bac393306ca521c5eba76c13aabfeeaf22d
Ivica Čardić <ivica.cardic@gmail.com>
2023-10-20 17:18:43 +0000

    ISSUE-76 - atlas-coordinator-impl - Use JobParametersDTO

--- server/libs/atlas/atlas-coordinator/atlas-coordinator-impl/src/main/java/com/bytechef/atlas/coordinator/Coordinator.java
+++ server/libs/atlas/atlas-coordinator/atlas-coordinator-impl/src/main/java/com/bytechef/atlas/coordinator/Coordinator.java
@@ -23,6 +23,7 @@ import com.bytechef.atlas.coordinator.task.completion.TaskCompletionHandler;
 import com.bytechef.atlas.domain.Context;
 import com.bytechef.atlas.domain.Job;
 import com.bytechef.atlas.domain.TaskExecution;
+import com.bytechef.atlas.dto.JobParametersDTO;
 import com.bytechef.atlas.error.ErrorHandler;
 import com.bytechef.atlas.error.Errorable;
 import com.bytechef.atlas.error.ExecutionError;
@@ -39,7 +40,6 @@ import com.bytechef.atlas.task.execution.TaskStatus;
 import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.util.Assert;
 
@@ -86,13 +86,13 @@ public class Coordinator {
     /**
      * Starts a job instance.
      *
-     * @param jobParamsMap The Key-Value map representing the job parameters
+     * @param jobParametersDTO The Key-Value map representing the workflow parameters
      * @return The instance of the Job
      */
-    public void create(Map<String, Object> jobParamsMap) {
-        Assert.notNull(jobParamsMap, "request can't be null");
+    public void create(JobParametersDTO jobParametersDTO) {
+        Assert.notNull(jobParametersDTO, "request can't be null");
 
-        Job job = jobService.add(jobParamsMap);
+        Job job = jobService.add(jobParametersDTO);
 
         Context context = new Context(job.getInputs());
 
