33501eed592971612328f1f474d921a7acf89025
Ivica Čardić <ivica.cardic@gmail.com>
2023-10-20 17:18:43 +0000

    ISSUE-76 - logger - Adjust logger component

---
+++ server/libs/modules/components/logger/src/main/java/com/bytechef/component/logger/LoggerComponentHandler.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright 2021 <your company/name>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef.component.logger;
+
+import static com.bytechef.component.logger.constants.LoggerConstants.DEBUG;
+import static com.bytechef.component.logger.constants.LoggerConstants.ERROR;
+import static com.bytechef.component.logger.constants.LoggerConstants.INFO;
+import static com.bytechef.component.logger.constants.LoggerConstants.LOGGER;
+import static com.bytechef.component.logger.constants.LoggerConstants.TEXT;
+import static com.bytechef.component.logger.constants.LoggerConstants.WARN;
+import static com.bytechef.hermes.component.ComponentDSL.action;
+import static com.bytechef.hermes.component.ComponentDSL.any;
+import static com.bytechef.hermes.component.ComponentDSL.display;
+
+import com.bytechef.hermes.component.ComponentDSL;
+import com.bytechef.hermes.component.ComponentHandler;
+import com.bytechef.hermes.component.Context;
+import com.bytechef.hermes.component.ExecutionParameters;
+import com.bytechef.hermes.component.definition.ComponentDefinition;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Ivica Cardic
+ */
+public class LoggerComponentHandler implements ComponentHandler {
+
+    private static final Logger logger = LoggerFactory.getLogger(LoggerComponentHandler.class);
+
+    private final ComponentDefinition componentDefinition = ComponentDSL.createComponent(LOGGER)
+            .display(display("Logger").description("Logs a value to the system log."))
+            .actions(
+                    action(DEBUG).display(display("Debug")).inputs(any(TEXT)).performFunction(this::performDebug),
+                    action(ERROR).display(display("Error")).inputs(any(TEXT)).performFunction(this::performError),
+                    action(INFO).display(display("Info")).inputs(any(TEXT)).performFunction(this::performInfo),
+                    action(WARN).display(display("Warn")).inputs(any(TEXT)).performFunction(this::performWarn));
+
+    @Override
+    public ComponentDefinition getDefinition() {
+        return componentDefinition;
+    }
+
+    protected Object performDebug(Context context, ExecutionParameters executionParameters) {
+        logger.debug(executionParameters.getString(TEXT));
+
+        return null;
+    }
+
+    protected Object performError(Context context, ExecutionParameters executionParameters) {
+        logger.error(executionParameters.getString(TEXT));
+
+        return null;
+    }
+
+    protected Object performInfo(Context context, ExecutionParameters executionParameters) {
+        logger.info(executionParameters.getString(TEXT));
+
+        return null;
+    }
+
+    protected Object performWarn(Context context, ExecutionParameters executionParameters) {
+        logger.warn(executionParameters.getString(TEXT));
+
+        return null;
+    }
+}
