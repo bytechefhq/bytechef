fdecc3f40f717b0b62d059d4841aaa69822aca3c
Marta Jurcevic <marta.raptavi997@gmail.com>
2023-10-20 17:18:43 +0000

    0 - deleting integrationItem from this branch

--- client/src/pages/integrations/Integrations.tsx
+++ client/src/pages/integrations/Integrations.tsx
@@ -1,28 +1,16 @@
-import { SidebarContentLayout } from '../../components/Layouts/SidebarContentLayout'
-import { IntegrationList } from './IntegrationList'
-import React, { useState } from 'react'
-import { PageHeader } from 'components/PageHeader/PageHeader'
-
-type Props = {
-  title: string
-  subTitle: string
-  onClick: React.MouseEventHandler<HTMLButtonElement>
-}
+import {SidebarContentLayout} from '../../components/Layouts/SidebarContentLayout';
+import {PageHeader} from 'components/PageHeader/PageHeader';
+import {IntegrationList} from './IntegrationList';
 
 export const Integrations = (): JSX.Element => {
-  const [clickedButton, setClickedButton] = useState('')
+	return (
+		<SidebarContentLayout title={'Integrations'}>
+			<PageHeader
+				subTitle="All Integrations"
+				buttonLabel="New Integration"
+			/>
 
-  const buttonHandler = (event: React.MouseEvent<HTMLButtonElement>) => {
-    event.preventDefault()
-
-    const button: HTMLButtonElement = event.currentTarget
-    setClickedButton(button.name)
-  }
-
-  return (
-    <SidebarContentLayout title={'Integrations'}>
-      <PageHeader />
-      {/* <IntegrationList /> */}
-    </SidebarContentLayout>
-  )
-}
+			<IntegrationList />
+		</SidebarContentLayout>
+	);
+};
