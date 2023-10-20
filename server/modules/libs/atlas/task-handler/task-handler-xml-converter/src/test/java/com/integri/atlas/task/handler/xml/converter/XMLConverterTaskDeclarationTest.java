056757890da2f7513a63bb9f943b156227f098c3
Ivica Cardic <ivica.cardic@gmail.com>
2023-10-20 17:18:43 +0000

    ISSUE-66 - Add xml converter task handler

---
+++ server/modules/libs/atlas/task-handler/task-handler-xml-converter/src/test/java/com/integri/atlas/task/handler/xml/converter/XMLConverterTaskDeclarationTest.java
@@ -0,0 +1,101 @@
+/*
+ * Copyright 2021 <your company/name>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.integri.atlas.task.handler.xml.converter;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.json.JSONObject;
+import org.junit.jupiter.api.Test;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.skyscreamer.jsonassert.JSONParser;
+
+/**
+ * @author Ivica Cardic
+ */
+public class XMLConverterTaskDeclarationTest {
+
+    private final ObjectMapper objectMapper = new ObjectMapper() {
+        {
+            setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        }
+    };
+
+    @Test
+    public void testFileTaskSpecification() throws JsonProcessingException {
+        JSONAssert.assertEquals(
+            """
+            {
+                "description":"Converts between XML string and object/array.",
+                "displayName":"XML Converter",
+                "name":"xmlConverter",
+                "properties":[
+                    {
+                        "defaultValue":"FROM_XML",
+                        "description":"The operation to perform.",
+                        "displayName":"Operation",
+                        "name":"operation",
+                        "required":true,
+                        "type":"SELECT",
+                        "options":[
+                            {
+                                "name":"Convert from XML string",
+                                "value":"FROM_XML",
+                                "description": "Converts the XML string to object/array."
+                            },
+                            {
+                                "name":"Convert to XML string",
+                                "value":"TO_XML",
+                                 "description": "Writes the object/array to a XML string."
+                            }
+                        ]
+                    },
+                    {
+                        "description":"XML string to convert to the data.",
+                        "displayName":"Input",
+                        "displayOption":{
+                            "show":{
+                                "operation":["FROM_XML"]
+                            }
+                        },
+                        "name":"input",
+                        "required":true,
+                        "type":"STRING"
+                    },
+                        {
+                        "description":"The data to convert to XML string.",
+                        "displayName":"Input",
+                        "displayOption":{
+                            "show":{
+                                "operation":["TO_XML"]
+                            }
+                        },
+                        "name":"input",
+                        "required":true,
+                        "type":"JSON"
+                    },
+                ],
+                "version":1.0
+            }
+            """,
+            (JSONObject) JSONParser.parseJSON(
+                objectMapper.writeValueAsString(XMLConverterTaskDeclaration.TASK_SPECIFICATION)
+            ),
+            true
+        );
+    }
+}
---
+++ server/modules/libs/atlas/task-handler/task-handler-xml-converter/src/test/java/com/integri/atlas/task/handler/xml/converter/XMLConverterTaskHandlerTest.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2021 <your company/name>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.integri.atlas.task.handler.xml.converter;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.integri.atlas.engine.core.json.JSONHelper;
+import com.integri.atlas.engine.core.task.SimpleTaskExecution;
+import com.integri.atlas.engine.core.xml.XMLHelper;
+import java.util.List;
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Ivica Cardic
+ */
+public class XMLConverterTaskHandlerTest {
+
+    private static final JSONHelper jsonHelper = new JSONHelper(new ObjectMapper());
+    private static final XMLHelper xmlHelper = new XMLHelper();
+    private static final XMLConverterTaskHandler xmlConverterTaskHandler = new XMLConverterTaskHandler(
+        jsonHelper,
+        xmlHelper
+    );
+
+    @Test
+    public void testJSONToObject() {
+        SimpleTaskExecution taskExecution = new SimpleTaskExecution();
+
+        String input =
+            """
+            <Flower id="45">
+                <name>Poppy</name>
+            </Flower>
+            """;
+
+        taskExecution.put("input", input);
+        taskExecution.put("operation", "FROM_XML");
+
+        assertThat(xmlConverterTaskHandler.handle(taskExecution)).isEqualTo(Map.of("id", "45", "name", "Poppy"));
+
+        input =
+            """
+            <Flowers>
+                <Flower id="45">
+                    <name>Poppy</name>
+                </Flower>
+                <Flower id="50">
+                    <name>Rose</name>
+                </Flower>
+            </Flowers>
+            """;
+
+        taskExecution.put("input", input);
+        taskExecution.put("operation", "FROM_XML");
+
+        assertThat(xmlConverterTaskHandler.handle(taskExecution))
+            .isEqualTo(
+                Map.of("Flower", List.of(Map.of("id", "45", "name", "Poppy"), Map.of("id", "50", "name", "Rose")))
+            );
+    }
+
+    @Test
+    public void testObjectToJSON() {
+        SimpleTaskExecution taskExecution = new SimpleTaskExecution();
+
+        Map<String, ?> input = Map.of("id", 45, "name", "Poppy");
+
+        taskExecution.put("input", input);
+        taskExecution.put("operation", "TO_XML");
+
+        assertThat(xmlConverterTaskHandler.handle(taskExecution)).isEqualTo(xmlHelper.serialize(input));
+    }
+}
