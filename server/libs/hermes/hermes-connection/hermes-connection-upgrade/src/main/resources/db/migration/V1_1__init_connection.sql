b04780d11e72f1fc9857af5982f76107eb44e5a8
Ivica Cardic <ivica.cardic@gmail.com>
2023-10-20 17:18:43 +0000

    0 - herme-connection-upgrade - Add flyway for managing db tables

---
+++ server/libs/hermes/hermes-connection/hermes-connection-upgrade/src/main/resources/db/migration/V1_1__init_connection.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS connection (
+    id                       VARCHAR(256) NOT NULL PRIMARY KEY,
+    name                     VARCHAR(256) NOT NULL,
+    label                    VARCHAR(256) NOT NULL,
+    component_name           VARCHAR(256) NOT NULL,
+    component_version        INT          NOT NULL,
+    parameters               TEXT         NOT NULL,
+    created_date             TIMESTAMP    NOT NULL,
+    created_by               VARCHAR(256) NOT NULL,
+    last_modified_date       TIMESTAMP    NOT NULL,
+    last_modified_by         VARCHAR(256) NOT NULL,
+    version                  BIGINT       NOT NULL
+);
