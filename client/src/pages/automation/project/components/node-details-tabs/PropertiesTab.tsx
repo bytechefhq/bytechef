e0d0fb76fe7d6d1658fc1508fa67d0652e7d77ce
Krešimir Čoko <kresimircoko@gmail.com>
2023-10-20 17:18:43 +0000

    303 Delete PropertiesTab because it became just a wrapper around the Properties component

--- client/src/pages/automation/project/components/node-details-tabs/PropertiesTab.tsx
+++
@@ -1,8 +0,0 @@
-import Properties from 'components/Properties/Properties';
-import {PropertyType} from 'types/projectTypes';
-
-const PropertiesTab = ({properties}: {properties: Array<PropertyType>}) => (
-    <Properties properties={properties} />
-);
-
-export default PropertiesTab;
