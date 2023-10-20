d82a42e5f1e6ac407b357dddb63661719b3f08d0
Ivica Cardic <ivica.cardic@gmail.com>
2023-10-20 17:18:43 +0000

    ISSUE-76 - atlas-rest - Add openapi.yaml, regenerate rest layer using spring webflux

---
+++ server/libs/atlas/atlas-rest/src/main/java/com/bytechef/atlas/web/rest/WorkflowController.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright 2016-2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Modifications copyright (C) 2021 <your company/name>
+ */
+
+package com.bytechef.atlas.web.rest;
+
+import com.bytechef.atlas.domain.Workflow;
+import com.bytechef.atlas.service.WorkflowService;
+import com.bytechef.atlas.web.rest.model.WorkflowModel;
+import com.bytechef.autoconfigure.annotation.ConditionalOnApi;
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import org.springframework.core.convert.ConversionService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+/**
+ * @author Arik Cohen
+ * @author Ivica Cardic
+ */
+@RestController
+@ConditionalOnApi
+public class WorkflowController implements WorkflowControllerApi {
+
+    private final ConversionService conversionService;
+    private final WorkflowService workflowService;
+
+    @SuppressFBWarnings("EI2")
+    public WorkflowController(ConversionService conversionService, WorkflowService workflowService) {
+        this.conversionService = conversionService;
+        this.workflowService = workflowService;
+    }
+
+    @Override
+    public Mono<ResponseEntity<Void>> deleteWorkflow(String id, ServerWebExchange exchange) {
+        workflowService.delete(id);
+
+        return Mono.just(ResponseEntity.ok().build());
+    }
+
+    @Override
+    public Mono<ResponseEntity<WorkflowModel>> getWorkflow(String id, ServerWebExchange exchange) {
+        return Mono.just(
+                ResponseEntity.ok(conversionService.convert(workflowService.getWorkflow(id), WorkflowModel.class)));
+    }
+
+    @Override
+    public Mono<ResponseEntity<Flux<WorkflowModel>>> getWorkflows(ServerWebExchange exchange) {
+        return Mono.just(ResponseEntity.ok(Flux.fromIterable(workflowService.getWorkflows().stream()
+                .map(workflow -> conversionService.convert(workflow, WorkflowModel.class))
+                .toList())));
+    }
+
+    @Override
+    public Mono<ResponseEntity<WorkflowModel>> postWorkflow(
+            Mono<WorkflowModel> workflowModelMono, ServerWebExchange exchange) {
+        return workflowModelMono.map(workflowModel -> ResponseEntity.ok(conversionService.convert(
+                workflowService.add(conversionService.convert(workflowModel, Workflow.class)), WorkflowModel.class)));
+    }
+
+    @Override
+    public Mono<ResponseEntity<WorkflowModel>> putWorkflow(
+            String id, Mono<WorkflowModel> workflowModelMono, ServerWebExchange exchange) {
+        return workflowModelMono.map(workflowModel -> {
+            Workflow workflow = conversionService.convert(workflowModel, Workflow.class);
+
+            workflow.setId(id);
+
+            return ResponseEntity.ok(conversionService.convert(workflowService.update(workflow), WorkflowModel.class));
+        });
+    }
+}
---
+++ server/libs/atlas/atlas-rest/src/main/java/com/bytechef/atlas/web/rest/mapper/WorkflowMapper.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2021 <your company/name>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef.atlas.web.rest.mapper;
+
+import com.bytechef.atlas.domain.Workflow;
+import com.bytechef.atlas.web.rest.mapper.config.WorkflowMapperSpringConfig;
+import com.bytechef.atlas.web.rest.model.WorkflowModel;
+import org.mapstruct.Mapper;
+import org.springframework.core.convert.converter.Converter;
+
+/**
+ * @author Ivica Cardic
+ */
+@Mapper(config = WorkflowMapperSpringConfig.class)
+public interface WorkflowMapper extends Converter<Workflow, WorkflowModel> {
+    WorkflowModel convert(Workflow workflow);
+}
