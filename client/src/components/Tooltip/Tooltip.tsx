669827caf2891c4f6c433ba3d5f291fadbb6cd77
Ivica Čardić <ivica.cardic@gmail.com>
2023-10-20 17:18:43 +0000

    290 - client - Make the vertical navigation items more readable. For example a tooltip or a title.

--- client/src/components/Tooltip/Tooltip.tsx
+++ client/src/components/Tooltip/Tooltip.tsx
@@ -1,18 +1,21 @@
+import {Side} from '@radix-ui/react-popper';
 import {Arrow, Content, Provider, Root, Trigger} from '@radix-ui/react-tooltip';
 import {PropsWithChildren} from 'react';
 
 interface TooltipProps {
+    side?: Side;
     text: string;
 }
 
-const Tooltip = ({text, children}: PropsWithChildren<TooltipProps>) => {
+const Tooltip = ({side, text, children}: PropsWithChildren<TooltipProps>) => {
     return (
         <Provider>
             <Root>
                 <Trigger asChild>{children}</Trigger>
 
                 <Content
-                    className="inline-flex items-center rounded-md bg-gray-800 px-4 py-2.5 radix-side-bottom:animate-slide-up-fade radix-side-left:animate-slide-right-fade radix-side-right:animate-slide-left-fade radix-side-top:animate-slide-down-fade dark:bg-gray-800"
+                    className="z-10 inline-flex items-center rounded-md bg-gray-800 px-4 py-2.5 radix-side-bottom:animate-slide-up-fade radix-side-left:animate-slide-right-fade radix-side-right:animate-slide-left-fade radix-side-top:animate-slide-down-fade dark:bg-gray-800"
+                    side={side}
                     sideOffset={4}
                 >
                     <Arrow className="fill-current text-gray-800 dark:text-gray-800" />
