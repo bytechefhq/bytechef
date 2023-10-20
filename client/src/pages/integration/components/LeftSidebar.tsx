77346fba005368e7e9188f0d5cc9c5456562d74c
Krešimir Čoko <kresimircoko@gmail.com>
2023-10-20 17:18:43 +0000

    #28 Extract workflow node list into its own component to avoid code duplication

--- client/src/pages/integration/components/LeftSidebar.tsx
+++ client/src/pages/integration/components/LeftSidebar.tsx
@@ -1,15 +1,10 @@
 import React, {useEffect, useState} from 'react';
 import {Provider} from '@radix-ui/react-tooltip';
-import {Component1Icon} from '@radix-ui/react-icons';
 import {
     ComponentDefinitionBasicModel,
-    DisplayModel,
     TaskDispatcherDefinitionModel,
 } from '../../../middleware/definition-registry';
-
-interface DragEvent<T = Element> extends React.MouseEvent<T, DragEventInit> {
-    dataTransfer: DataTransfer;
-}
+import WorkflowNodesList from './WorkflowNodesList';
 
 interface SidebarProps {
     data: {
@@ -18,33 +13,6 @@ interface SidebarProps {
     };
     filter: string;
 }
-
-const Item = ({description, label}: DisplayModel): JSX.Element => {
-    const onDragStart = (event: DragEvent, label: string) => {
-        event.dataTransfer.setData('application/reactflow', label);
-        event.dataTransfer.effectAllowed = 'move';
-    };
-
-    return (
-        <li
-            className="mb-2 flex h-[72px] cursor-pointer items-center rounded-md bg-white p-2 hover:bg-gray-50"
-            draggable
-            id={label}
-            onDragStart={(event) => onDragStart(event, label!)}
-        >
-            <Component1Icon className="mr-2 h-7 w-7 flex-none" />
-
-            <div className="flex flex-col">
-                <p className="text-sm font-medium text-gray-900">{label}</p>
-
-                {/* eslint-disable-next-line tailwindcss/no-custom-classname */}
-                <p className="text-left text-xs text-gray-500 line-clamp-2">
-                    {description}
-                </p>
-            </div>
-        </li>
-    );
-};
 
 const LeftSidebar: React.FC<SidebarProps> = ({data, filter}): JSX.Element => {
     const [filteredComponents, setFilteredComponents] = useState<
@@ -84,53 +52,11 @@ const LeftSidebar: React.FC<SidebarProps> = ({data, filter}): JSX.Element => {
 
     return (
         <Provider>
-            <div className="px-2">
-                <span className="sticky top-0 z-10 block w-full bg-gray-100 p-2 text-center text-sm font-bold uppercase text-gray-500">
-                    Components
-                </span>
-
-                <ul role="list" className="mb-2">
-                    {filteredComponents.length ? (
-                        filteredComponents.map(
-                            (component: ComponentDefinitionBasicModel) => (
-                                <Item
-                                    description={component.display?.description}
-                                    label={component.display?.label}
-                                    key={component.name}
-                                />
-                            )
-                        )
-                    ) : (
-                        <span className="block py-2 px-3 text-xs text-gray-500">
-                            No components found.
-                        </span>
-                    )}
-                </ul>
-
-                <span className="sticky top-0 z-10 mt-2 block w-full bg-gray-100 p-2 text-center text-sm font-bold uppercase text-gray-500">
-                    Flow Controls
-                </span>
-
-                <ul role="list" className="mb-2">
-                    {filteredFlowControls.length ? (
-                        filteredFlowControls.map(
-                            (flowControl: TaskDispatcherDefinitionModel) => (
-                                <Item
-                                    description={
-                                        flowControl.display?.description
-                                    }
-                                    label={flowControl.display?.label}
-                                    key={flowControl.name}
-                                />
-                            )
-                        )
-                    ) : (
-                        <span className="block py-2 px-3 text-xs text-gray-500">
-                            No flow controls found.
-                        </span>
-                    )}
-                </ul>
-            </div>
+            <WorkflowNodesList
+                components={filteredComponents}
+                flowControls={filteredFlowControls}
+                itemsDraggable
+            />
         </Provider>
     );
 };
