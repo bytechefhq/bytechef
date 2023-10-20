3118a769e5632a2904a648a713d974f4ca07ce66
Krešimir Čoko <kresimircoko@gmail.com>
2023-10-20 17:18:43 +0000

    272 - Fix usage of vite-plugin-svgr to render icons once again

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,7 @@
-import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
+import {defineConfig} from 'vite';
+import svgr from 'vite-plugin-svgr';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import viteSvgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,30 +14,30 @@ export default defineConfig({
             target: 'es2020',
         },
     },
-    plugins: [react(), tsconfigPaths(), viteSvgr()],
+    plugins: [react(), tsconfigPaths(), svgr()],
     server: {
         host: '127.0.0.1',
         proxy: {
             '/api': {
-                target: 'http://localhost:9555',
                 changeOrigin: true,
                 secure: false,
+                target: 'http://localhost:9555',
                 // rewrite: (path) => path.replace(/^\/api/, ""),
             },
         },
     },
     test: {
-        globals: true,
-        environment: 'jsdom',
-        setupFiles: '.vitest/setup.ts',
         coverage: {
-            reporter: ['html', 'lcov', 'text'],
             exclude: [
                 '.vitest/',
                 'node_modules/',
                 'src/middleware',
                 '**/*.test.tsx',
             ],
+            reporter: ['html', 'lcov', 'text'],
         },
+        environment: 'jsdom',
+        globals: true,
+        setupFiles: '.vitest/setup.ts',
     },
 });
