aaad34a02c64eda0641dc2b10b3941c0b4b1827c
Ivica Čardić <ivica.cardic@gmail.com>
2023-10-20 17:18:43 +0000

    309 - client - Generate

--- client/src/middleware/workflow/models/index.ts
+++ client/src/middleware/workflow/models/index.ts
@@ -2,8 +2,10 @@
 /* eslint-disable */
 export * from './CreateJob200ResponseModel';
 export * from './ExecutionErrorModel';
+export * from './InputModel';
 export * from './JobModel';
 export * from './JobParametersModel';
+export * from './OutputModel';
 export * from './PageModel';
 export * from './TaskExecutionModel';
 export * from './WebhookModel';
