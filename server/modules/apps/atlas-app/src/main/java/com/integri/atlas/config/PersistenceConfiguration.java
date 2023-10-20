66ca63f86cbb46738375bd5e442dfd84368fb410
Ivica Cardic <ivica.cardic@gmail.com>
2023-10-20 17:18:43 +0000

    0 - Merge configuration

--- server/modules/apps/atlas-app/src/main/java/com/integri/atlas/config/PersistenceConfiguration.java
+++ server/modules/apps/atlas-app/src/main/java/com/integri/atlas/config/PersistenceConfiguration.java
@@ -29,17 +29,26 @@ import com.integri.atlas.engine.job.repository.jdbc.JdbcJobRepository;
 import com.integri.atlas.engine.task.execution.repository.TaskExecutionRepository;
 import com.integri.atlas.engine.task.execution.repository.jdbc.JdbcTaskExecutionRepository;
 import com.integri.atlas.engine.workflow.repository.WorkflowRepository;
+import com.integri.atlas.engine.workflow.repository.WorkflowRepositoryChain;
+import com.integri.atlas.engine.workflow.repository.git.GitWorkflowRepository;
 import com.integri.atlas.engine.workflow.repository.jdbc.JdbcWorkflowRepository;
+import com.integri.atlas.engine.workflow.repository.mapper.JsonWorkflowMapper;
 import com.integri.atlas.engine.workflow.repository.mapper.WorkflowMapper;
-import com.integri.atlas.task.auth.JdbcTaskAuthRepository;
+import com.integri.atlas.engine.workflow.repository.mapper.WorkflowMapperChain;
+import com.integri.atlas.engine.workflow.repository.mapper.YAMLWorkflowMapper;
+import com.integri.atlas.engine.workflow.repository.resource.ResourceBasedWorkflowRepository;
+import com.integri.atlas.task.auth.jdbc.JdbcTaskAuthRepository;
 import com.integri.atlas.task.auth.repository.TaskAuthRepository;
+import java.util.List;
 import com.integri.atlas.task.definition.repository.ExtTaskDefinitionHandlerRepository;
 import com.integri.atlas.task.definition.repository.memory.InMemoryExtTaskDefinitionHandlerRepository;
-import java.util.List;
-import com.integri.atlas.task.definition.repository.memory.InMemoryTaskDefinitionHandlerAccessorRepository;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cache.CacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.core.annotation.Order;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
@@ -57,15 +66,15 @@ public class PersistenceConfiguration {
     }
 
     @ConditionalOnProperty(name = "atlas.persistence.provider", havingValue = "jdbc")
-    public static class JdbcPersistenceConfiguration {
+    public static class JdbcRepositoryConfiguration {
 
         @Bean
-        CounterRepository jdbcCounterRepository(JdbcTemplate jdbcTemplate) {
+        CounterRepository counterRepository(JdbcTemplate jdbcTemplate) {
             return new JdbcCounterRepository(jdbcTemplate);
         }
 
         @Bean
-        ContextRepository jdbcContextRepository(JdbcTemplate jdbcTemplate, ObjectMapper objectMapper) {
+        ContextRepository contextRepository(JdbcTemplate jdbcTemplate, ObjectMapper objectMapper) {
             JdbcContextRepository jdbcContextRepository = new JdbcContextRepository();
 
             jdbcContextRepository.setJdbcTemplate(jdbcTemplate);
@@ -75,7 +84,7 @@ public class PersistenceConfiguration {
         }
 
         @Bean
-        JobRepository jdbcJobRepository(NamedParameterJdbcTemplate jdbcTemplate, ObjectMapper objectMapper) {
+        JobRepository jobRepository(NamedParameterJdbcTemplate jdbcTemplate, ObjectMapper objectMapper) {
             JdbcJobRepository jdbcJobRepository = new JdbcJobRepository();
 
             jdbcJobRepository.setJdbcTemplate(jdbcTemplate);
@@ -85,7 +94,7 @@ public class PersistenceConfiguration {
         }
 
         @Bean
-        TaskExecutionRepository jdbcTaskExecutionRepository(
+        TaskExecutionRepository taskExecutionRepository(
             NamedParameterJdbcTemplate jdbcTemplate,
             ObjectMapper objectMapper
         ) {
@@ -98,7 +107,7 @@ public class PersistenceConfiguration {
         }
 
         @Bean
-        TaskAuthRepository jdbcTaskAuthRepository(
+        TaskAuthRepository taskAuthRepository(
             Encryption encryption,
             NamedParameterJdbcTemplate jdbcTemplate,
             ObjectMapper objectMapper
@@ -111,11 +120,56 @@ public class PersistenceConfiguration {
 
             return jdbcTaskAuthRepository;
         }
+    }
+
+    @EnableConfigurationProperties(AtlasProperties.class)
+    public static class WorkflowRepositoryConfiguration {
+
+        @Bean
+        @Primary
+        WorkflowRepository workflowRepository(
+            CacheManager cacheManager,
+            List<WorkflowRepository> workflowRepositories
+        ) {
+            return new WorkflowRepositoryChain(cacheManager, workflowRepositories);
+        }
+
+        @Bean
+        @Order(1)
+        @ConditionalOnProperty(name = "atlas.workflow-repository.classpath.enabled", havingValue = "true")
+        ResourceBasedWorkflowRepository resourceBasedWorkflowRepository() {
+            return new ResourceBasedWorkflowRepository(workflowMapper());
+        }
+
+        @Bean
+        @Order(2)
+        @ConditionalOnProperty(name = "atlas.workflow-repository.filesystem.enabled", havingValue = "true")
+        ResourceBasedWorkflowRepository fileSystemBasedWorkflowRepository(
+            @Value("${atlas.workflow-repository.filesystem.location-pattern}") String aBasePath
+        ) {
+            return new ResourceBasedWorkflowRepository(String.format("file:%s", aBasePath), workflowMapper());
+        }
+
+        @Bean
+        @Order(3)
+        @ConditionalOnProperty(name = "atlas.workflow-repository.git.enabled", havingValue = "true")
+        GitWorkflowRepository gitWorkflowRepository(AtlasProperties atlasProperties) {
+            WorkflowRepositoryProperties.GitProperties gitProperties = atlasProperties.getWorkflowRepository().getGit();
+
+            return new GitWorkflowRepository(
+                gitProperties.getUrl(),
+                gitProperties.getBranch(),
+                gitProperties.getSearchPaths(),
+                gitProperties.getUsername(),
+                gitProperties.getPassword(),
+                workflowMapper()
+            );
+        }
 
         @Bean
         @ConditionalOnProperty(name = "atlas.workflow-repository.database.enabled", havingValue = "true")
         @Order(4)
-        WorkflowRepository jdbcWorkflowRepository(
+        JdbcWorkflowRepository jdbcWorkflowRepository(
             NamedParameterJdbcTemplate jdbcTemplate,
             WorkflowMapper aWorkflowMapper
         ) {
@@ -125,6 +179,22 @@ public class PersistenceConfiguration {
             jdbcWorkflowRepository.setWorkflowMapper(aWorkflowMapper);
 
             return jdbcWorkflowRepository;
+        }
+
+        @Bean
+        @Primary
+        WorkflowMapper workflowMapper() {
+            return new WorkflowMapperChain(List.of(jsonWorkflowMapper(), yamlWorkflowMapper()));
+        }
+
+        @Bean
+        JsonWorkflowMapper jsonWorkflowMapper() {
+            return new JsonWorkflowMapper();
+        }
+
+        @Bean
+        YAMLWorkflowMapper yamlWorkflowMapper() {
+            return new YAMLWorkflowMapper();
         }
     }
 }
