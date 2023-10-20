f85ec0e3f6ef4325d0d1abfd4721e1e5b1476904
Ivica Cardic <ivica.cardic@gmail.com>
2023-10-20 17:18:43 +0000

    ISSUE-76 - api-gateway-app - Add api gateway app to the services dir, add Consul and property support for service discovery

---
+++ server/apps/services/api-gateway-app/src/main/java/com/bytechef/ApiGatewayApplication.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright 2021 <your company/name>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Optional;
+import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
+import org.springframework.core.env.Environment;
+
+/**
+ * @author Ivica Cardic
+ */
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+    private static final Logger log = LoggerFactory.getLogger(ApiGatewayApplication.class);
+
+    public ApiGatewayApplication() {}
+
+    /**
+     * Main method, used to run the application.
+     *
+     * @param args the command line arguments.
+     */
+    public static void main(String[] args) {
+        SpringApplication springApplication = new SpringApplication(ApiGatewayApplication.class);
+
+        springApplication.addListeners(new ApplicationPidFileWriter());
+
+        Environment environment = springApplication.run(args).getEnvironment();
+
+        logApplicationStartup(environment);
+    }
+
+    private static void logApplicationStartup(Environment environment) {
+        String protocol = Optional.ofNullable(environment.getProperty("server.ssl.key-store"))
+                .map(key -> "https")
+                .orElse("http");
+        String serverPort = environment.getProperty("server.port");
+        String contextPath = Optional.ofNullable(environment.getProperty("server.servlet.context-path"))
+                .filter(StringUtils::isNotBlank)
+                .orElse("/");
+        String hostAddress = "localhost";
+
+        try {
+            InetAddress inetAddress = InetAddress.getLocalHost();
+
+            hostAddress = inetAddress.getHostAddress();
+        } catch (UnknownHostException e) {
+            log.warn("The host name could not be determined, using `localhost` as fallback");
+        }
+
+        log.info(
+                """
+                    \n----------------------------------------------------------
+                    \tApplication '{}' is running! Access URLs:
+                    \tLocal: \t\t{}://localhost:{}{}
+                    \tExternal: \t{}://{}:{}{}
+                    \tSwaggerUI: \t{}
+                    \tProfile(s): \t{}
+                    ----------------------------------------------------------""",
+                environment.getProperty("spring.application.name"),
+                protocol,
+                serverPort,
+                contextPath,
+                protocol,
+                hostAddress,
+                serverPort,
+                contextPath,
+                ArrayUtils.contains(environment.getActiveProfiles(), "api-docs")
+                        ? "%s://localhost:%s%s".formatted(protocol, serverPort, contextPath + "swagger-ui.html")
+                        : "",
+                environment.getActiveProfiles());
+    }
+}
--- server/apps/services/api-gateway-app/src/test/java/com/bytechef/ApiGatewayApplicationIntTest.java
+++ server/apps/services/api-gateway-app/src/test/java/com/bytechef/ApiGatewayApplicationIntTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016-2018 the original author or authors.
+ * Copyright 2021 <your company/name>.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,19 +12,19 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
- * Modifications copyright (C) 2021 <your company/name>
  */
 
-package com.bytechef.config;
+package com.bytechef;
 
-import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Configuration;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
 /**
- * @author Arik Cohen
  * @author Ivica Cardic
  */
-@Configuration
-@EnableCaching
-public class CacheConfiguration {}
+@SpringBootTest
+public class ApiGatewayApplicationIntTest {
+
+    @Test
+    void testContextLoads() {}
+}
