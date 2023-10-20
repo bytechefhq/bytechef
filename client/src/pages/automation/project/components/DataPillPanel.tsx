15dfbdb2ef945eda100507138604c58bb4252367
Krešimir Čoko <kresimircoko@gmail.com>
2023-10-20 17:18:43 +0000

    373 - Add filter feature to datapills

--- client/src/pages/automation/project/components/DataPillPanel.tsx
+++ client/src/pages/automation/project/components/DataPillPanel.tsx
@@ -4,167 +4,19 @@ import {
     TooltipProvider,
     TooltipTrigger,
 } from '@/components/ui/tooltip';
-import {ChevronDownIcon} from '@heroicons/react/24/outline';
-import {
-    Accordion,
-    AccordionContent,
-    AccordionItem,
-    AccordionTrigger,
-} from '@radix-ui/react-accordion';
 import * as Dialog from '@radix-ui/react-dialog';
 import {Cross1Icon, InfoCircledIcon} from '@radix-ui/react-icons';
 import Button from 'components/Button/Button';
-import {useGetActionDefinitionsQuery} from 'queries/actionDefinitions.queries';
-import {useGetComponentDefinitionsQuery} from 'queries/componentDefinitions.queries';
+import Input from 'components/Input/Input';
 import {useCallback, useState} from 'react';
-import InlineSVG from 'react-inlinesvg';
-import {PropertyType} from 'types/projectTypes';
 
 import {useDataPillPanelStore} from '../stores/useDataPillPanelStore';
 import {useNodeDetailsDialogStore} from '../stores/useNodeDetailsDialogStore';
-import useWorkflowDefinitionStore from '../stores/useWorkflowDefinitionStore';
-import DataPill from './DataPill';
-
-const DataPillPanelBody = ({containerHeight}: {containerHeight: number}) => {
-    const {componentActions, componentNames, dataPills, setDataPills} =
-        useWorkflowDefinitionStore();
-
-    const {currentNode, focusedInput} = useNodeDetailsDialogStore();
-
-    const taskTypes = componentActions?.map(
-        (componentAction) =>
-            `${componentAction.componentName}/1/${componentAction.actionName}`
-    );
-
-    const currentNodeIndex = componentNames.indexOf(currentNode.name);
-
-    const previousComponentNames =
-        componentNames.length > 1
-            ? componentNames.slice(0, currentNodeIndex)
-            : componentNames;
-
-    const {data: previousComponents} = useGetComponentDefinitionsQuery(
-        {
-            include: previousComponentNames,
-        },
-        !!componentNames.length
-    );
-
-    const {data: actionData} = useGetActionDefinitionsQuery(
-        {taskTypes},
-        !!componentActions?.length
-    );
-
-    const handleDataPillClick = (property: PropertyType) => {
-        const dataPillData = property.label || property.name;
-
-        if (focusedInput && dataPillData) {
-            const existingDataPill = dataPills.find(
-                (pill) => pill.name === focusedInput.name
-            );
-
-            if (existingDataPill) {
-                const remainingDataPills = dataPills.filter(
-                    (pill) => pill.name !== focusedInput.name
-                );
-
-                setDataPills([
-                    ...remainingDataPills,
-                    {
-                        name: focusedInput.name,
-                        value: [...existingDataPill.value, dataPillData],
-                    },
-                ]);
-            } else {
-                setDataPills([
-                    ...dataPills,
-                    {
-                        name: focusedInput.name,
-                        value: [dataPillData],
-                    },
-                ]);
-            }
-        }
-    };
-
-    return (
-        <Accordion className="h-full" collapsible type="single">
-            {previousComponents?.map((component, index) => {
-                const {icon, name, title} = component;
-
-                if (!actionData?.length) {
-                    return;
-                }
-
-                const outputSchema: PropertyType | undefined =
-                    actionData[index]?.outputSchema;
-
-                const properties = outputSchema?.properties?.length
-                    ? outputSchema.properties
-                    : outputSchema?.items;
-
-                const existingProperties = properties?.filter(
-                    (datum) => !!datum.name
-                );
-
-                return (
-                    <AccordionItem
-                        className="group group-data-[state=open]:h-full"
-                        key={name}
-                        style={{
-                            maxHeight: containerHeight / 2,
-                        }}
-                        value={name}
-                    >
-                        {!!existingProperties?.length && (
-                            <>
-                                <AccordionTrigger className="group flex w-full items-center justify-between border-gray-100 bg-white p-4 group-data-[state=closed]:border-b">
-                                    <div className="flex items-center space-x-4">
-                                        {icon && (
-                                            <div className="flex h-5 w-5 items-center">
-                                                <InlineSVG src={icon} />
-                                            </div>
-                                        )}
-
-                                        <span className="text-sm">{title}</span>
-                                    </div>
-
-                                    <ChevronDownIcon className="ease-[cubic-bezier(0.87,_0,_0.13,_1)] h-5 w-5 text-gray-400 transition-transform duration-300 group-data-[state=open]:rotate-180" />
-                                </AccordionTrigger>
-
-                                <AccordionContent
-                                    className="w-full space-y-4 overflow-y-scroll border-b border-gray-100 bg-gray-100 p-2 group-data-[state=open]:h-full"
-                                    style={{
-                                        maxHeight: containerHeight / 2 - 52,
-                                    }}
-                                >
-                                    <ul className="flex w-full flex-col space-y-2 group-data-[state=open]:h-full">
-                                        {existingProperties?.map(
-                                            (property: PropertyType) => (
-                                                <DataPill
-                                                    key={property.name}
-                                                    onClick={() =>
-                                                        handleDataPillClick(
-                                                            property
-                                                        )
-                                                    }
-                                                    property={property}
-                                                />
-                                            )
-                                        )}
-                                    </ul>
-                                </AccordionContent>
-                            </>
-                        )}
-                    </AccordionItem>
-                );
-            })}
-        </Accordion>
-    );
-};
+import DataPillPanelBody from './DataPillPanelBody';
 
 const DataPillPanel = () => {
     const [panelContainerHeight, setPanelContainerHeight] = useState(0);
+    const [dataPillFilterQuery, setDataPillFilterQuery] = useState('');
 
     const {dataPillPanelOpen, setDataPillPanelOpen} = useDataPillPanelStore();
     const {nodeDetailsDialogOpen} = useNodeDetailsDialogStore();
@@ -225,9 +77,20 @@ const DataPillPanel = () => {
                             </Dialog.Title>
                         </header>
 
-                        <main className="flex h-full flex-col">
+                        <main className="flex h-full w-full flex-col">
+                            <Input
+                                name="dataPillFilter"
+                                placeholder="Filter Data Pills..."
+                                fieldsetClassName="p-2 border-b border-gray-100 mb-0"
+                                onChange={(event) =>
+                                    setDataPillFilterQuery(event.target.value)
+                                }
+                                value={dataPillFilterQuery}
+                            />
+
                             <DataPillPanelBody
                                 containerHeight={panelContainerHeight}
+                                dataPillFilterQuery={dataPillFilterQuery}
                             />
                         </main>
                     </div>
---
+++ client/src/pages/automation/project/components/DataPillPanelBody.tsx
@@ -0,0 +1,195 @@
+import {ComponentDefinitionModel} from '@/middleware/automation/execution/models';
+import {useGetActionDefinitionsQuery} from '@/queries/actionDefinitions.queries';
+import {useGetComponentDefinitionsQuery} from '@/queries/componentDefinitions.queries';
+import {PropertyType} from '@/types/projectTypes';
+import {
+    Accordion,
+    AccordionContent,
+    AccordionItem,
+    AccordionTrigger,
+} from '@radix-ui/react-accordion';
+import {ChevronDownIcon} from 'lucide-react';
+import InlineSVG from 'react-inlinesvg';
+
+import {useNodeDetailsDialogStore} from '../stores/useNodeDetailsDialogStore';
+import useWorkflowDefinitionStore from '../stores/useWorkflowDefinitionStore';
+import DataPill from './DataPill';
+
+const DataPillPanelBody = ({
+    containerHeight,
+    dataPillFilterQuery,
+}: {
+    containerHeight: number;
+    dataPillFilterQuery: string;
+}) => {
+    const {componentActions, componentNames, dataPills, setDataPills} =
+        useWorkflowDefinitionStore();
+
+    const {currentNode, focusedInput} = useNodeDetailsDialogStore();
+
+    const taskTypes = componentActions?.map(
+        (componentAction) =>
+            `${componentAction.componentName}/1/${componentAction.actionName}`
+    );
+
+    const currentNodeIndex = componentNames.indexOf(currentNode.name);
+
+    const previousComponentNames =
+        componentNames.length > 1
+            ? componentNames.slice(0, currentNodeIndex)
+            : componentNames;
+
+    const {data: previousComponents} = useGetComponentDefinitionsQuery(
+        {
+            include: previousComponentNames,
+        },
+        !!componentNames.length
+    );
+
+    const {data: actionData} = useGetActionDefinitionsQuery(
+        {taskTypes},
+        !!componentActions?.length
+    );
+
+    const handleDataPillClick = (property: PropertyType) => {
+        const dataPillData = property.label || property.name;
+
+        if (focusedInput && dataPillData) {
+            const existingDataPill = dataPills.find(
+                (pill) => pill.name === focusedInput.name
+            );
+
+            if (existingDataPill) {
+                const remainingDataPills = dataPills.filter(
+                    (pill) => pill.name !== focusedInput.name
+                );
+
+                setDataPills([
+                    ...remainingDataPills,
+                    {
+                        name: focusedInput.name,
+                        value: [...existingDataPill.value, dataPillData],
+                    },
+                ]);
+            } else {
+                setDataPills([
+                    ...dataPills,
+                    {
+                        name: focusedInput.name,
+                        value: [dataPillData],
+                    },
+                ]);
+            }
+        }
+    };
+
+    const getFilteredProperties = (
+        properties: PropertyType[],
+        filterQuery: string
+    ) =>
+        properties?.reduce((previousValue: PropertyType[], currentValue) => {
+            const subProperties = getFilteredProperties(
+                currentValue.properties || [],
+                filterQuery
+            );
+
+            if (
+                currentValue.name
+                    ?.toLowerCase()
+                    .includes(filterQuery.toLowerCase()) ||
+                subProperties.length
+            ) {
+                previousValue.push(Object.assign({}, currentValue));
+            }
+
+            return previousValue;
+        }, []);
+
+    return (
+        <Accordion className="h-full" collapsible type="single">
+            {previousComponents?.map(
+                (component: ComponentDefinitionModel, index: number) => {
+                    const {icon, name, title} = component;
+
+                    if (!actionData?.length) {
+                        return;
+                    }
+
+                    const outputSchema: PropertyType | undefined =
+                        actionData[index]?.outputSchema;
+
+                    const properties = outputSchema?.properties?.length
+                        ? outputSchema.properties
+                        : outputSchema?.items;
+
+                    const existingProperties = properties?.filter(
+                        (property) => !!property.name
+                    );
+
+                    const filteredProperties = existingProperties?.length
+                        ? getFilteredProperties(
+                              existingProperties,
+                              dataPillFilterQuery
+                          )
+                        : [];
+
+                    return (
+                        <AccordionItem
+                            className="group group-data-[state=open]:h-full"
+                            key={name}
+                            style={{
+                                maxHeight: containerHeight / 2,
+                            }}
+                            value={name}
+                        >
+                            {!!filteredProperties?.length && (
+                                <>
+                                    <AccordionTrigger className="group flex w-full items-center justify-between border-gray-100 bg-white p-4 group-data-[state=closed]:border-b">
+                                        <div className="flex items-center space-x-4">
+                                            {icon && (
+                                                <div className="flex h-5 w-5 items-center">
+                                                    <InlineSVG src={icon} />
+                                                </div>
+                                            )}
+
+                                            <span className="text-sm">
+                                                {title}
+                                            </span>
+                                        </div>
+
+                                        <ChevronDownIcon className="h-5 w-5 text-gray-400 transition-transform duration-300 group-data-[state=open]:rotate-180" />
+                                    </AccordionTrigger>
+
+                                    <AccordionContent
+                                        className="w-full space-y-4 overflow-y-scroll border-b border-gray-100 bg-gray-100 p-2 group-data-[state=open]:h-full"
+                                        style={{
+                                            maxHeight: containerHeight / 2 - 52,
+                                        }}
+                                    >
+                                        <ul className="flex w-full flex-col space-y-2 group-data-[state=open]:h-full">
+                                            {filteredProperties?.map(
+                                                (property: PropertyType) => (
+                                                    <DataPill
+                                                        key={property.name}
+                                                        onClick={() =>
+                                                            handleDataPillClick(
+                                                                property
+                                                            )
+                                                        }
+                                                        property={property}
+                                                    />
+                                                )
+                                            )}
+                                        </ul>
+                                    </AccordionContent>
+                                </>
+                            )}
+                        </AccordionItem>
+                    );
+                }
+            )}
+        </Accordion>
+    );
+};
+
+export default DataPillPanelBody;
