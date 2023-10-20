dc9474b8cb55c5a4613eaf22f6633f85aba69fd8
Krešimir Čoko <kresimircoko@gmail.com>
2023-10-20 17:18:43 +0000

    303 Capitalize Input label

--- client/src/components/Input/Input.tsx
+++ client/src/components/Input/Input.tsx
@@ -44,7 +44,7 @@ const Input = forwardRef<HTMLInputElement, InputProps>(
                     <label
                         htmlFor={name}
                         className={twMerge(
-                            'block text-sm font-medium text-gray-700 dark:text-gray-400',
+                            'block text-sm font-medium capitalize text-gray-700 dark:text-gray-400',
                             description && 'mr-1',
                             labelClassName
                         )}
