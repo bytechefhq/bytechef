bf6d7c8e3db7fc79ca4d7c3bdb0afecb19f94d3c
Krešimir Čoko <kresimircoko@gmail.com>
2023-10-20 17:18:43 +0000

    #28 Fix zoom, zIndex and positioning of the ContextualMenu

--- client/src/pages/integration/nodes/ContextualMenu.tsx
+++ client/src/pages/integration/nodes/ContextualMenu.tsx
@@ -2,8 +2,8 @@ import {Component1Icon} from '@radix-ui/react-icons';
 import Input from 'components/Input/Input';
 import {ComponentDefinitionModel} from 'data-access/component-definition';
 import {TaskDispatcherDefinitionModel} from 'data-access/task-dispatcher-definition';
-import React, {memo, useEffect, useState} from 'react';
-import {Edge, Node, useReactFlow} from 'reactflow';
+import React, {HTMLAttributes, memo, useEffect, useState} from 'react';
+import {Edge, MarkerType, Node, useReactFlow} from 'reactflow';
 
 const uuid = (): string =>
     new Date().getTime().toString(36) + Math.random().toString(36).slice(2);
@@ -13,6 +13,33 @@ interface ContextualMenuProps {
     flowControls: TaskDispatcherDefinitionModel[] | undefined;
     id: string;
 }
+
+interface ContextualMenuItemProps extends HTMLAttributes<HTMLLIElement> {
+    item: ComponentDefinitionModel | TaskDispatcherDefinitionModel;
+    onClick: () => void;
+}
+
+const ContextualMenuItem = ({item, onClick}: ContextualMenuItemProps) => (
+    <li
+        className="flex cursor-pointer items-center py-2 px-3 hover:bg-gray-50"
+        onClick={onClick}
+    >
+        <Component1Icon className="mr-3 h-6 w-6 flex-none" />
+
+        <div>
+            <span className="flex items-center  text-sm">
+                {item.display?.label}
+            </span>
+
+            {item.display?.description && (
+                // eslint-disable-next-line tailwindcss/no-custom-classname
+                <p className="text-left text-xs text-gray-500 line-clamp-2">
+                    {item.display.description}
+                </p>
+            )}
+        </div>
+    </li>
+);
 
 const ContextualMenu = ({
     components,
@@ -36,21 +63,20 @@ const ContextualMenu = ({
         }
 
         const placeholderId = placeholderNode.id;
-
-        // create a unique id for the placeholder node that will be added as a child of the clicked node
         const childPlaceholderId = uuid();
 
         // create a placeholder node that will be added as a child of the clicked node
         const childPlaceholderNode = {
             id: childPlaceholderId,
-            // the placeholder is placed at the position of the clicked node
-            // the layout function will animate it to its new position
             position: {
                 x: placeholderNode.position.x,
                 y: placeholderNode.position.y,
             },
             type: 'placeholder',
             data: {label: '+'},
+            style: {
+                zIndex: 9999,
+            },
         };
 
         // we need a connection from the clicked node to the new placeholder
@@ -93,6 +119,9 @@ const ContextualMenu = ({
                         return {
                             ...edge,
                             type: 'workflow',
+                            markerEnd: {
+                                type: MarkerType.ArrowClosed,
+                            },
                         };
                     }
 
@@ -121,7 +150,7 @@ const ContextualMenu = ({
 
     return (
         // eslint-disable-next-line tailwindcss/no-custom-classname
-        <div className="nowheel rounded-md bg-white shadow-md" draggable>
+        <div className="nowheel rounded-md bg-white shadow-md">
             {typeof components === 'undefined' ||
                 (typeof flowControls === 'undefined' && (
                     <div className="py-2 px-3 text-xs text-gray-500">
@@ -129,7 +158,7 @@ const ContextualMenu = ({
                     </div>
                 ))}
 
-            <header className="flex items-center px-3 pt-2 text-center font-bold text-gray-600">
+            <header className="border-b border-gray-200 px-3 pt-2 text-center font-bold text-gray-600">
                 <Input
                     name="contextualMenuFilter"
                     placeholder="Filter nodes"
@@ -147,24 +176,13 @@ const ContextualMenu = ({
                                     | ComponentDefinitionModel
                                     | TaskDispatcherDefinitionModel
                             ) => (
-                                <li
-                                    className="py-2 px-4"
+                                <ContextualMenuItem
+                                    item={filteredItem}
                                     key={filteredItem.name}
                                     onClick={() =>
                                         handleItemClick(filteredItem)
                                     }
-                                >
-                                    <span className="flex items-center  text-sm">
-                                        <Component1Icon className="mr-1" />
-
-                                        {filteredItem.display?.label}
-                                    </span>
-
-                                    {/* eslint-disable-next-line tailwindcss/no-custom-classname */}
-                                    <p className="text-left text-xs text-gray-500 line-clamp-2">
-                                        {filteredItem.display?.description}
-                                    </p>
-                                </li>
+                                />
                             )
                         )
                     ) : (
