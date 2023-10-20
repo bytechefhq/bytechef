204eb5d5fb0ee62bd3ab7983747205a5c8aa4d2e
Marta Jurcevic <marta.raptavi997@gmail.com>
2023-10-20 17:18:43 +0000

    0 - creating a PageHeader component

---
+++ client/src/components/PageHeader/PageHeader.tsx
@@ -0,0 +1,26 @@
+import NewIntegrationButton from './NewIntegrationButton'
+import './styles.css'
+
+export const PageHeader = () => {
+  const onClickHandler = () => {
+    console.log('inside onClickHandler')
+  }
+
+  return (
+    <div className="mb-6 flex justify-center py-4">
+      <div className="flex w-full items-center justify-between">
+        <h2 className="text-2xl tracking-tight text-gray-900 dark:text-gray-200">
+          All integrations
+        </h2>
+
+        <div>
+          {/* <button onClick={onClickHandler} className="button">
+            New integration
+          </button> */}
+
+          <NewIntegrationButton />
+        </div>
+      </div>
+    </div>
+  )
+}
