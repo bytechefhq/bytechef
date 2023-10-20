393c353e0f0973244745161c0c09389b3c44c901
Ivica Cardic <ivica.cardic@gmail.com>
2023-10-20 17:18:43 +0000

    ISSUE-76 - hermes-connection-service - Move auth service based classes

---
+++ server/libs/hermes/hermes-connection/hermes-connection-service/src/main/java/com/bytechef/hermes/connection/config/ConnectionConfiguration.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2021 <your company/name>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef.hermes.connection.config;
+
+import com.bytechef.encryption.Encryption;
+import com.bytechef.hermes.connection.converter.EncryptedMapWrapperToStringConverter;
+import com.bytechef.hermes.connection.converter.EncryptedStringToMapWrapperConverter;
+import com.bytechef.hermes.connection.repository.ConnectionRepository;
+import com.bytechef.hermes.connection.service.ConnectionService;
+import com.bytechef.hermes.connection.service.impl.ConnectionServiceImpl;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import java.util.Arrays;
+import java.util.List;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jdbc.repository.config.AbstractJdbcConfiguration;
+
+/**
+ * @author Ivica Cardic
+ */
+@Configuration
+public class ConnectionConfiguration extends AbstractJdbcConfiguration {
+
+    private final Encryption encryption;
+    private final ObjectMapper objectMapper;
+
+    @SuppressFBWarnings("EI2")
+    public ConnectionConfiguration(Encryption encryption, ObjectMapper objectMapper) {
+        this.encryption = encryption;
+        this.objectMapper = objectMapper;
+    }
+
+    @Bean
+    ConnectionService connectionService(ConnectionRepository connectionRepository) {
+        return new ConnectionServiceImpl(connectionRepository);
+    }
+
+    @Override
+    protected List<?> userConverters() {
+        return Arrays.asList(
+                new EncryptedMapWrapperToStringConverter(encryption, objectMapper),
+                new EncryptedStringToMapWrapperConverter(encryption, objectMapper));
+    }
+}
---
+++ server/libs/hermes/hermes-connection/hermes-connection-service/src/test/java/com/bytechef/hermes/connection/repository/ConnectionRepositoryIntTest.java
@@ -0,0 +1,111 @@
+/*
+ * Copyright 2021 <your company/name>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef.hermes.connection.repository;
+
+import com.bytechef.hermes.connection.domain.Connection;
+import java.util.Map;
+import java.util.stream.StreamSupport;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author Ivica Cardic
+ */
+@SpringBootTest
+public class ConnectionRepositoryIntTest {
+
+    @Autowired
+    private ConnectionRepository connectionRepository;
+
+    @BeforeEach
+    public void beforeEach() {
+        for (Connection connection : connectionRepository.findAll()) {
+            connectionRepository.deleteById(connection.getId());
+        }
+    }
+
+    @Test
+    public void testCreate() {
+        Connection connection = connectionRepository.save(getConnection());
+
+        Assertions.assertEquals(
+                connection, connectionRepository.findById(connection.getId()).get());
+    }
+
+    @Test
+    public void testDelete() {
+        Connection connection = connectionRepository.save(getConnection());
+
+        Assertions.assertEquals(
+                connection, connectionRepository.findById(connection.getId()).orElseThrow());
+
+        connectionRepository.deleteById(connection.getId());
+
+        Assertions.assertFalse(connectionRepository.findById(connection.getId()).isPresent());
+    }
+
+    @Test
+    public void testFindAll() {
+        connectionRepository.save(getConnection());
+
+        Assertions.assertEquals(
+                1,
+                StreamSupport.stream(connectionRepository.findAll().spliterator(), false)
+                        .count());
+    }
+
+    @Test
+    public void testFindById() {
+        Connection connection = connectionRepository.save(getConnection());
+
+        Assertions.assertEquals(
+                connection, connectionRepository.findById(connection.getId()).get());
+    }
+
+    @Test
+    public void testUpdate() {
+        Connection connection = connectionRepository.save(getConnection());
+
+        connection.setLabel("label2");
+        connection.setName("name2");
+        connection.setParameters(Map.of("key2", "value2"));
+
+        connectionRepository.save(connection);
+
+        Connection updatedConnection =
+                connectionRepository.findById(connection.getId()).get();
+
+        Assertions.assertEquals(connection.getLabel(), updatedConnection.getLabel());
+        Assertions.assertEquals("name2", updatedConnection.getName());
+        Assertions.assertEquals(Map.of("key2", "value2"), updatedConnection.getParameters());
+    }
+
+    private static Connection getConnection() {
+        Connection connection = new Connection();
+
+        connection.setComponentName("componentName");
+        connection.setComponentVersion(1);
+        connection.setLabel("label");
+        connection.setName("name");
+        connection.setParameters(Map.of("key1", "value1"));
+
+        return connection;
+    }
+}
