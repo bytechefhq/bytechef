7ffab7e1ad78ac7ffd9d27f222ea9be4dec48365
Ivica Cardic <ivica.cardic@gmail.com>
2023-10-20 17:18:43 +0000

    ISSUE-12 - Update task descriptors

--- server/modules/libs/atlas/atlas-task-handler/atlas-task-handler-spreadsheet-file/src/main/java/com/integri/atlas/task/handler/spreadsheet/file/SpreadsheetFileTaskDescriptor.java
+++ server/modules/libs/atlas/atlas-task-handler/atlas-task-handler-spreadsheet-file/src/main/java/com/integri/atlas/task/handler/spreadsheet/file/SpreadsheetFileTaskDescriptor.java
@@ -18,7 +18,17 @@
 
 package com.integri.atlas.task.handler.spreadsheet.file;
 
-import static com.integri.atlas.engine.core.task.description.TaskDescription.description;
+import static com.integri.atlas.engine.core.task.description.TaskDescription.task;
+import static com.integri.atlas.engine.core.task.description.TaskParameterValue.parameterValue;
+import static com.integri.atlas.engine.core.task.description.TaskParameterValue.parameterValues;
+import static com.integri.atlas.engine.core.task.description.TaskProperty.BOOLEAN_PROPERTY;
+import static com.integri.atlas.engine.core.task.description.TaskProperty.COLLECTION_PROPERTY;
+import static com.integri.atlas.engine.core.task.description.TaskProperty.GROUP_PROPERTY;
+import static com.integri.atlas.engine.core.task.description.TaskProperty.NUMBER_PROPERTY;
+import static com.integri.atlas.engine.core.task.description.TaskProperty.SELECT_PROPERTY;
+import static com.integri.atlas.engine.core.task.description.TaskProperty.STRING_PROPERTY;
+import static com.integri.atlas.engine.core.task.description.TaskProperty.show;
+import static com.integri.atlas.engine.core.task.description.TaskPropertyOption.option;
 
 import com.integri.atlas.engine.core.task.TaskDescriptor;
 import com.integri.atlas.engine.core.task.description.TaskDescription;
@@ -27,7 +37,116 @@ import org.springframework.stereotype.Component;
 @Component
 public class SpreadsheetFileTaskDescriptor implements TaskDescriptor {
 
-    private static final TaskDescription TASK_DESCRIPTION = description();
+    private static final TaskDescription TASK_DESCRIPTION = task("spreadsheetFile")
+        .displayName("Spreadsheet File")
+        .description("Reads and writes data from a spreadsheet file")
+        .properties(
+            SELECT_PROPERTY("operation")
+                .displayName("Operation")
+                .description("The operation to perform.")
+                .options(option("Read to file", "READ"), option("Write from file", "WRITE"))
+                .defaultValue("read")
+                .required(true),
+            //
+            // read from file
+            //
+            STRING_PROPERTY("binaryPropertyName")
+                .displayName("Binary Property")
+                .displayOption(show("operation", "READ"))
+                .description("Name of the binary property from which to read the binary data of the spreadsheet file.")
+                .defaultValue("data"),
+            //
+            // write to file
+            //
+            SELECT_PROPERTY("fileFormat")
+                .displayName("FileFormat")
+                .description("The format of the file to save the data.")
+                .displayOption(show("operation", "WRITE"))
+                .options(
+                    option("CSV", "CSV", "Comma-separated value"),
+                    option("HTML", "HTML", "HTML Table"),
+                    option("ODS", "ODS", "OpenDocument Spreadsheet"),
+                    option("RTF", "RTF", "Rich Text Format"),
+                    option("XLS", "XLS", "Microsoft Excel"),
+                    option("XLSX", "XLSX", "Microsoft Excel")
+                )
+                .defaultValue("csv"),
+            STRING_PROPERTY("binaryPropertyName")
+                .displayName("Binary Property")
+                .displayOption(show("operation", "WRITE"))
+                .description("Name of the binary property in which to save the binary data of the spreadsheet file.")
+                .defaultValue("data"),
+            COLLECTION_PROPERTY("options")
+                .displayName("Options")
+                .placeholder("Add Option")
+                .options(
+                    BOOLEAN_PROPERTY("compression")
+                        .displayName("Compression")
+                        .displayOption(
+                            show("operation", parameterValues("WRITE"), "fileFormat", parameterValues("XLSX", "ODS"))
+                        )
+                        .description("Weather compression will be applied or not.")
+                        .defaultValue(false),
+                    STRING_PROPERTY("fileName")
+                        .displayName("File Name")
+                        .displayOption(show("operation", "WRITE"))
+                        .description(
+                            "File name to set in binary data. By default will \"spreadsheet.<fileFormat>\" be used."
+                        )
+                        .defaultValue(""),
+                    BOOLEAN_PROPERTY("headerRow")
+                        .displayName("Header Row")
+                        .displayOption(show("operation", "READ"))
+                        .description("The first row of the file contains the header names.")
+                        .defaultValue(true),
+                    BOOLEAN_PROPERTY("includeEmptyCells")
+                        .displayName("Include Empty Cells")
+                        .displayOption(show("operation", "READ"))
+                        .description("When reading from file the empty cells will be filled with an empty string.")
+                        .defaultValue(false),
+                    BOOLEAN_PROPERTY("rawData")
+                        .displayName("RAW Data")
+                        .displayOption(show("operation", "READ"))
+                        .description("If the data should be returned RAW instead of parsed.")
+                        .defaultValue(false),
+                    BOOLEAN_PROPERTY("readAsString")
+                        .displayName("Read As String")
+                        .displayOption(show("operation", "READ"))
+                        .description(
+                            "In some cases and file formats, it is necessary to read data specifically as string, otherwise some special characters are interpreted the wrong way."
+                        )
+                        .defaultValue(false),
+                    GROUP_PROPERTY("range")
+                        .displayName("Range")
+                        .displayOption(show("operation", "READ"))
+                        .description(
+                            "The range to read from the table. If set to a number it will be the starting row."
+                        )
+                        .fields(
+                            NUMBER_PROPERTY("startRow").displayName("Start Row index"),
+                            NUMBER_PROPERTY("endRow").displayName("End Row index")
+                        ),
+                    STRING_PROPERTY("sheetName")
+                        .displayName("Sheet Name")
+                        .displayOption(show("operation", "READ"))
+                        .description(
+                            "The name of the sheet to read from in the spreadsheet (if supported). If not set, the first one gets chosen."
+                        )
+                        .defaultValue("Sheet"),
+                    STRING_PROPERTY("sheetName")
+                        .displayName("Sheet Name")
+                        .displayOption(
+                            show(
+                                "operation",
+                                parameterValues("WRITE"),
+                                "fileFormat",
+                                parameterValues("ODS", "XLS", "XLSX")
+                            )
+                        )
+                        .description("The name of the sheet to create in the spreadsheet.")
+                        .defaultValue("Sheet")
+                )
+        );
 
     @Override
     public TaskDescription getDescription() {
