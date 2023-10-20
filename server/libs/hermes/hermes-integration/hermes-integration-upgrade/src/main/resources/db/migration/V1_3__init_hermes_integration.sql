748760206ce9812e5aa302ab2bbf2d7af262a49e
Ivica Čardić <ivica.cardic@gmail.com>
2023-10-20 17:18:43 +0000

    ISSUE-76 - hermes-integration-upgrade - Rename

---
+++ server/libs/hermes/hermes-integration/hermes-integration-upgrade/src/main/resources/db/migration/V1_3__init_hermes_integration.sql
@@ -0,0 +1,28 @@
+CREATE TABLE IF NOT EXISTS integration (
+    id                       BIGSERIAL    NOT NULL PRIMARY KEY,
+    name                     VARCHAR(256) NOT NULL,
+    description              TEXT         NULL,
+    category                 VARCHAR(256) NULL,
+    created_date             TIMESTAMP    NOT NULL,
+    created_by               TEXT         NOT NULL,
+    last_modified_date       TIMESTAMP    NOT NULL,
+    last_modified_by         TEXT         NOT NULL,
+    version                  BIGINT       NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS integration_tag (
+    id                       BIGSERIAL    NOT NULL PRIMARY KEY,
+    integration_id           BIGSERIAL    NOT NULL,
+    tag_id                   BIGSERIAL    NOT NULL
+);
+
+ALTER TABLE integration_tag ADD CONSTRAINT fk_integration_tag_integration FOREIGN KEY (integration_id) REFERENCES integration (id);
+ALTER TABLE integration_tag ADD CONSTRAINT fk_integration_tag_tag FOREIGN KEY (tag_id) REFERENCES tag (id);
+
+CREATE TABLE IF NOT EXISTS integration_workflow (
+    id                       BIGSERIAL    NOT NULL PRIMARY KEY,
+    integration_id           BIGSERIAL    NOT NULL,
+    workflow_id              VARCHAR(256) NOT NULL
+);
+
+ALTER TABLE integration_workflow ADD CONSTRAINT fk_integration_workflow_integration FOREIGN KEY (integration_id) REFERENCES integration (id);
