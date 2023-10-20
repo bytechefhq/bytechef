0a0096a2156d833645c6d0a0ec1d3136921686bf
Matija Petanjek <matija.petanjek7@gmail.com>
2023-10-20 17:18:43 +0000

    ISSUE-14 - add http client logic

--- server/modules/libs/atlas/task-handler/task-handler-http-client/src/main/java/com/integri/atlas/task/handler/http/client/HttpClientTaskHandler.java
+++ server/modules/libs/atlas/task-handler/task-handler-http-client/src/main/java/com/integri/atlas/task/handler/http/client/HttpClientTaskHandler.java
@@ -16,17 +16,73 @@
 
 package com.integri.atlas.task.handler.http.client;
 
+import com.integri.atlas.engine.core.MapObject;
+import com.integri.atlas.engine.core.json.JSONHelper;
 import com.integri.atlas.engine.core.task.TaskExecution;
 import com.integri.atlas.engine.worker.task.handler.TaskHandler;
-import java.util.List;
-import java.util.Map;
+import com.integri.atlas.task.handler.http.client.authentication.HttpAuthenticationFactory;
+import com.integri.atlas.task.handler.http.client.header.HttpHeadersFactory;
+import com.integri.atlas.task.handler.http.client.params.QueryParamsFactory;
+import com.integri.atlas.task.handler.http.client.response.HttpResponseHandler;
+import java.net.http.HttpResponse;
 import org.springframework.stereotype.Component;
 
 @Component("httpClient")
 public class HttpClientTaskHandler implements TaskHandler<Object> {
 
+    public HttpClientTaskHandler(
+        HttpAuthenticationFactory httpAuthenticationFactory,
+        HttpHeadersFactory httpHeadersFactory,
+        HttpResponseHandler httpResponseHandler,
+        QueryParamsFactory queryParamsFactory,
+        JSONHelper jsonHelper
+    ) {
+        this.httpAuthenticationFactory = httpAuthenticationFactory;
+        this.httpHeadersFactory = httpHeadersFactory;
+        this.httpResponseHandler = httpResponseHandler;
+        this.queryParamsFactory = queryParamsFactory;
+        this.jsonHelper = jsonHelper;
+    }
+
     @Override
     public Object handle(TaskExecution aTask) throws Exception {
-        return null;
+        IntegriHttpClient integriHttpClient = new IntegriHttpClient(
+            httpAuthenticationFactory.create(
+                aTask.getRequiredString("authenticationType"),
+                aTask.get("credentials", MapObject.class)
+            ),
+            aTask.getLong("timeout", 10000)
+        );
+
+        HttpResponse httpResponse = integriHttpClient.send(
+            aTask.getRequiredString("requestMethod"),
+            resolveURI(aTask.getRequiredString("uri"), queryParamsFactory.getQueryParams(aTask)),
+            httpHeadersFactory.getHttpHeaders(aTask),
+            getBody(aTask)
+        );
+
+        return httpResponseHandler.handle(aTask, httpResponse);
     }
+
+    private String resolveURI(String uri, String queryParameters) {
+        if (queryParameters.isEmpty()) {
+            return uri;
+        }
+
+        return uri + '?' + queryParameters;
+    }
+
+    private String getBody(TaskExecution aTask) {
+        if (aTask.containsKey("bodyParametersRaw")) {
+            return jsonHelper.serialize(aTask.get("bodyParametersRaw"));
+        } else if (aTask.containsKey("bodyParametersKeyValue")) {}
+
+        return "";
+    }
+
+    private final HttpAuthenticationFactory httpAuthenticationFactory;
+    private final HttpHeadersFactory httpHeadersFactory;
+    private final QueryParamsFactory queryParamsFactory;
+    private final JSONHelper jsonHelper;
+    private final HttpResponseHandler httpResponseHandler;
 }
