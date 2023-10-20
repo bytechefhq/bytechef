6c8952616cff78b0a5ca4a4c8db2e6b6c61f0869
Ivica Čardić <ivica.cardic@gmail.com>
2023-10-20 17:18:43 +0000

    ISSUE-76 - hermes-integration-rest - Generate

---
+++ server/libs/hermes/hermes-integration/hermes-integration-rest/generated/src/main/java/com/bytechef/hermes/integration/web/rest/model/PutIntegrationTagsRequestModel.java
@@ -0,0 +1,96 @@
+package com.bytechef.hermes.integration.web.rest.model;
+
+import java.net.URI;
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonTypeName;
+import java.util.ArrayList;
+import java.util.List;
+import org.openapitools.jackson.nullable.JsonNullable;
+import java.time.OffsetDateTime;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.*;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+
+import java.util.*;
+import jakarta.annotation.Generated;
+
+/**
+ * PutIntegrationTagsRequestModel
+ */
+
+@JsonTypeName("putIntegrationTags_request")
+@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2022-12-27T13:16:13.209880+01:00[Europe/Zagreb]")
+public class PutIntegrationTagsRequestModel {
+
+  @JsonProperty("tags")
+  @Valid
+  private List<String> tags = null;
+
+  public PutIntegrationTagsRequestModel tags(List<String> tags) {
+    this.tags = tags;
+    return this;
+  }
+
+  public PutIntegrationTagsRequestModel addTagsItem(String tagsItem) {
+    if (this.tags == null) {
+      this.tags = new ArrayList<>();
+    }
+    this.tags.add(tagsItem);
+    return this;
+  }
+
+  /**
+   * Get tags
+   * @return tags
+  */
+  
+  @Schema(name = "tags", required = false)
+  public List<String> getTags() {
+    return tags;
+  }
+
+  public void setTags(List<String> tags) {
+    this.tags = tags;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    PutIntegrationTagsRequestModel putIntegrationTagsRequest = (PutIntegrationTagsRequestModel) o;
+    return Objects.equals(this.tags, putIntegrationTagsRequest.tags);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(tags);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class PutIntegrationTagsRequestModel {\n");
+    sb.append("    tags: ").append(toIndentedString(tags)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
+
