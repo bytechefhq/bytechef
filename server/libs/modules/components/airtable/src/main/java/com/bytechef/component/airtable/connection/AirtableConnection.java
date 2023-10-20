7c2c139d2b3556f6fe43af338239585bb5aacf40
Ivica Čardić <ivica.cardic@gmail.com>
2023-10-20 17:18:43 +0000

    272 - airtable - Add initial airable connector

---
+++ server/libs/modules/components/airtable/src/main/java/com/bytechef/component/airtable/connection/AirtableConnection.java
@@ -0,0 +1,44 @@
+
+/*
+ * Copyright 2021 <your company/name>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef.component.airtable.connection;
+
+import static com.bytechef.hermes.component.definition.Authorization.AuthorizationType;
+import static com.bytechef.hermes.component.definition.Authorization.TOKEN;
+import static com.bytechef.hermes.component.definition.ComponentDSL.authorization;
+import static com.bytechef.hermes.component.definition.ComponentDSL.connection;
+import static com.bytechef.hermes.component.definition.ComponentDSL.display;
+import static com.bytechef.hermes.component.definition.ComponentDSL.string;
+
+import com.bytechef.hermes.component.definition.ComponentDSL;
+
+/**
+ * Provides the component connection definition.
+ *
+ * @generated
+ */
+public class AirtableConnection {
+    public static final ComponentDSL.ModifiableConnectionDefinition CONNECTION_DEFINITION = connection()
+        .baseUri(connection -> "https://api.airtable.com/v0")
+        .authorizations(authorization(AuthorizationType.BEARER_TOKEN.toLowerCase(), AuthorizationType.BEARER_TOKEN)
+            .display(
+                display("Bearer Token"))
+            .properties(
+                string(TOKEN)
+                    .label("Token")
+                    .required(true)));
+}
