a687b9efb2bac37b91b29d01c6c0ecd0493686fb
Krešimir Čoko <kresimircoko@gmail.com>
2023-10-20 17:18:43 +0000

    #28 Add zustand store and persistant store

---
+++ client/src/pages/integration/store.ts
@@ -0,0 +1,24 @@
+import {create} from 'zustand';
+
+interface Node {
+    label: string;
+}
+
+interface IntegrationState {
+    rightSlideOverOpen: boolean;
+    setRightSlideOverOpen: (rightSlideOverOpen: boolean) => void;
+
+    currentNode: Node;
+    setCurrentNode: (currentNode: Node) => void;
+}
+
+export const useStore = create<IntegrationState>()((set) => ({
+    rightSlideOverOpen: false,
+    setRightSlideOverOpen: (rightSlideOverOpen) =>
+        set((state) => ({...state, rightSlideOverOpen})),
+
+    currentNode: {label: ''},
+    setCurrentNode: (currentNode) => set((state) => ({...state, currentNode})),
+}));
+
+export default useStore;
