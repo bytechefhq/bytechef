e8bd8c585269125ab1753abb2119358aeb67736f
Ivica Čardić <ivica.cardic@gmail.com>
2023-10-20 17:18:43 +0000

    #169 - hermes-integration-upgrade - Add category table

--- server/libs/hermes/hermes-integration/hermes-integration-upgrade/src/main/resources/db/migration/V1_3__init_hermes_integration.sql
+++ server/libs/hermes/hermes-integration/hermes-integration-upgrade/src/main/resources/db/migration/V1_3__init_hermes_integration.sql
@@ -1,8 +1,19 @@
+CREATE TABLE IF NOT EXISTS category (
+    id                       BIGSERIAL    NOT NULL PRIMARY KEY,
+    name                     VARCHAR(256) NOT NULL,
+    description              TEXT         NULL,
+    created_date             TIMESTAMP    NOT NULL,
+    created_by               TEXT         NOT NULL,
+    last_modified_date       TIMESTAMP    NOT NULL,
+    last_modified_by         TEXT         NOT NULL,
+    version                  BIGINT       NOT NULL
+);
+
 CREATE TABLE IF NOT EXISTS integration (
     id                       BIGSERIAL    NOT NULL PRIMARY KEY,
     name                     VARCHAR(256) NOT NULL,
     description              TEXT         NULL,
-    category                 VARCHAR(256) NULL,
+    category_id              BIGINT       NULL,
     created_date             TIMESTAMP    NOT NULL,
     created_by               TEXT         NOT NULL,
     last_modified_date       TIMESTAMP    NOT NULL,
@@ -16,13 +27,13 @@ CREATE TABLE IF NOT EXISTS integration_tag (
     tag_id                   BIGSERIAL    NOT NULL
 );
 
-ALTER TABLE integration_tag ADD CONSTRAINT fk_integration_tag_integration FOREIGN KEY (integration_id) REFERENCES integration (id);
-ALTER TABLE integration_tag ADD CONSTRAINT fk_integration_tag_tag FOREIGN KEY (tag_id) REFERENCES tag (id);
-
 CREATE TABLE IF NOT EXISTS integration_workflow (
     id                       BIGSERIAL    NOT NULL PRIMARY KEY,
     integration_id           BIGSERIAL    NOT NULL,
     workflow_id              VARCHAR(256) NOT NULL
 );
 
+ALTER TABLE integration ADD CONSTRAINT fk_integration_category FOREIGN KEY (category_id) REFERENCES category (id);
+ALTER TABLE integration_tag ADD CONSTRAINT fk_integration_tag_integration FOREIGN KEY (integration_id) REFERENCES integration (id);
+ALTER TABLE integration_tag ADD CONSTRAINT fk_integration_tag_tag FOREIGN KEY (tag_id) REFERENCES tag (id);
 ALTER TABLE integration_workflow ADD CONSTRAINT fk_integration_workflow_integration FOREIGN KEY (integration_id) REFERENCES integration (id);
