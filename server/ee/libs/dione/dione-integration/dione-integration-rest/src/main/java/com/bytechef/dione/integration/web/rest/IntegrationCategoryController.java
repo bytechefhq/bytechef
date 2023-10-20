f1da60b4208ca7556fb354f559b9b9896dcb303f
Ivica Čardić <ivica.cardic@gmail.com>
2023-10-20 17:18:43 +0000

    #241 - dione-integration-rest - Update integration categories and tags endpoints

---
+++ server/ee/libs/dione/dione-integration/dione-integration-rest/src/main/java/com/bytechef/dione/integration/web/rest/IntegrationCategoryController.java
@@ -0,0 +1,57 @@
+
+/*
+ * Copyright 2021 <your company/name>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef.dione.integration.web.rest;
+
+import com.bytechef.autoconfigure.annotation.ConditionalOnApi;
+import com.bytechef.category.web.rest.model.CategoryModel;
+import com.bytechef.dione.integration.facade.IntegrationFacade;
+import org.springframework.core.convert.ConversionService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+/**
+ * @author Ivica Cardic
+ */
+@RestController
+@ConditionalOnApi
+@RequestMapping("${openapi.openAPIDefinition.base-path:}")
+public class IntegrationCategoryController implements IntegrationCategoriesApi {
+
+    private final ConversionService conversionService;
+    private final IntegrationFacade integrationFacade;
+
+    public IntegrationCategoryController(ConversionService conversionService, IntegrationFacade integrationFacade) {
+        this.conversionService = conversionService;
+        this.integrationFacade = integrationFacade;
+    }
+
+    @Override
+    public Mono<ResponseEntity<Flux<CategoryModel>>> getIntegrationCategories(ServerWebExchange exchange) {
+        return Mono.just(
+            ResponseEntity.ok(
+                Flux.fromIterable(
+                    integrationFacade.getIntegrationCategories()
+                        .stream()
+                        .map(category -> conversionService.convert(category, CategoryModel.class))
+                        .toList())));
+    }
+}
---
+++ server/ee/libs/dione/dione-integration/dione-integration-rest/src/test/java/com/bytechef/dione/integration/web/rest/IntegrationTagControllerIntTest.java
@@ -0,0 +1,69 @@
+
+/*
+ * Copyright 2021 <your company/name>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef.dione.integration.web.rest;
+
+import com.bytechef.dione.integration.facade.IntegrationFacade;
+import com.bytechef.tag.domain.Tag;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+
+/**
+ * @author Ivica Cardic
+ */
+@WebFluxTest(value = IntegrationController.class)
+public class IntegrationTagControllerIntTest {
+
+    @MockBean
+    private IntegrationFacade integrationFacade;
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @Test
+    public void testGetIntegrationTags() {
+        when(integrationFacade.getIntegrationTags()).thenReturn(List.of(new Tag(1L, "tag1"), new Tag(2L, "tag2")));
+
+        try {
+            this.webTestClient
+                .get()
+                .uri("/integrations/tags")
+                .exchange()
+                .expectStatus()
+                .isOk()
+                .expectBody()
+                .jsonPath("$.[0].id")
+                .isEqualTo(1)
+                .jsonPath("$.[1].id")
+                .isEqualTo(2)
+                .jsonPath("$.[0].name")
+                .isEqualTo("tag1")
+                .jsonPath("$.[1].name")
+                .isEqualTo("tag2");
+        } catch (Exception exception) {
+            Assertions.fail(exception);
+        }
+    }
+}
