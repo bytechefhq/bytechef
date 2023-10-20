11d7f07c79316f107e58fa081440660acba29163
Krešimir Čoko <kresimircoko@gmail.com>
2023-10-20 17:18:43 +0000

    373 - dedupe numerical and non-numerical inputs

--- client/src/components/Properties/InputProperty.tsx
+++ client/src/components/Properties/InputProperty.tsx
@@ -1,7 +1,7 @@
 import {DataPillType} from '@/types/types';
 import Input from 'components/Input/Input';
 import MentionsInput from 'components/MentionsInput/MentionsInput';
-import {ReactNode, useRef, useState} from 'react';
+import {ReactNode, useState} from 'react';
 
 interface InputPropertyProps {
     controlType?: string;
@@ -22,7 +22,6 @@ interface InputPropertyProps {
 const InputProperty = ({
     controlType,
     dataPills,
-    defaultValue,
     description,
     error,
     hidden,
@@ -34,7 +33,6 @@ const InputProperty = ({
     type,
 }: InputPropertyProps) => {
     const [integerValue, setIntegerValue] = useState('');
-    const inputRef = useRef(null);
 
     const getInputType = () => {
         switch (controlType) {
@@ -64,71 +62,47 @@ const InputProperty = ({
 
     return (
         <>
-            {!mention &&
-                (isNumericalInput ? (
-                    <Input
-                        description={description}
-                        error={error}
-                        fieldsetClassName="flex-1 mb-0"
-                        key={name}
-                        label={label || name}
-                        leadingIcon={leadingIcon}
-                        name={name!}
-                        onChange={({target}) => {
-                            console.log('onchange');
-                            const {value} = target;
+            {!mention && (
+                <Input
+                    description={description}
+                    error={error}
+                    fieldsetClassName="flex-1 mb-0"
+                    key={name}
+                    label={label || name}
+                    leadingIcon={leadingIcon}
+                    name={name!}
+                    onChange={(event) => {
+                        if (isNumericalInput) {
+                            const {value} = event.target;
 
                             const integerOnlyRegex = /^[0-9\b]+$/;
 
                             if (value === '' || integerOnlyRegex.test(value)) {
                                 setIntegerValue(value);
                             }
-                        }}
-                        required={required}
-                        title={type}
-                        type={hidden ? 'hidden' : getInputType()}
-                        value={integerValue}
-                    />
-                ) : (
-                    <Input
-                        description={description}
-                        defaultValue={defaultValue as string}
-                        error={error}
-                        fieldsetClassName="flex-1 mb-0"
-                        key={name}
-                        label={label || name}
-                        leadingIcon={leadingIcon}
-                        name={name!}
-                        required={required}
-                        title={type}
-                        type={hidden ? 'hidden' : getInputType()}
-                        ref={inputRef}
-                    />
-                ))}
-
-            {mention &&
-                !!dataPills?.length &&
-                !!name &&
-                (isNumericalInput ? (
-                    <MentionsInput
-                        controlType={controlType || getInputType()}
-                        description={description}
-                        data={dataPills}
-                        label={label}
-                        leadingIcon={leadingIcon}
-                        onKeyPress={(event: KeyboardEvent) =>
-                            event.key !== '{' && event.preventDefault()
                         }
-                    />
-                ) : (
-                    <MentionsInput
-                        controlType={controlType || getInputType()}
-                        description={description}
-                        data={dataPills}
-                        label={label}
-                        leadingIcon={leadingIcon}
-                    />
-                ))}
+                    }}
+                    required={required}
+                    title={type}
+                    type={hidden ? 'hidden' : getInputType()}
+                    value={isNumericalInput ? integerValue : undefined}
+                />
+            )}
+
+            {mention && !!dataPills?.length && !!name && (
+                <MentionsInput
+                    controlType={controlType || getInputType()}
+                    description={description}
+                    data={dataPills}
+                    label={label}
+                    leadingIcon={leadingIcon}
+                    onKeyPress={(event: KeyboardEvent) => {
+                        if (isNumericalInput) {
+                            event.key !== '{' && event.preventDefault();
+                        }
+                    }}
+                />
+            )}
         </>
     );
 };
