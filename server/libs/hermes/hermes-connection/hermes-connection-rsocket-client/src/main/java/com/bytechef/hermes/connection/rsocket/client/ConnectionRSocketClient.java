3a7ae1a327b1c41de20114adafc454a3c1742364
Ivica Čardić <ivica.cardic@gmail.com>
2023-10-20 17:18:43 +0000

    ISSUE-76 - hermes-connection-rsocket-client - Add RSocket clients

---
+++ server/libs/hermes/hermes-connection/hermes-connection-rsocket-client/src/main/java/com/bytechef/hermes/connection/rsocket/client/ConnectionRSocketClient.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright 2021 <your company/name>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef.hermes.connection.rsocket.client;
+
+import com.bytechef.hermes.connection.domain.Connection;
+import com.bytechef.hermes.connection.service.ConnectionService;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.messaging.rsocket.RSocketRequester;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Ivica Cardic
+ */
+@Component
+public class ConnectionRSocketClient implements ConnectionService {
+
+    private final RSocketRequester rSocketRequester;
+
+    public ConnectionRSocketClient(RSocketRequester rSocketRequester) {
+        this.rSocketRequester = rSocketRequester;
+    }
+
+    @Override
+    public Connection add(Connection connection) {
+        return rSocketRequester
+                .route("createConnection")
+                .data(connection)
+                .retrieveMono(Connection.class)
+                .block();
+    }
+
+    @Override
+    public void delete(String id) {
+        rSocketRequester.route("removeConnection").data(id).send().block();
+    }
+
+    @Override
+    public Optional<Connection> fetchConnection(String id) {
+        return Optional.ofNullable(rSocketRequester
+                .route("fetchConnection")
+                .data(id)
+                .retrieveMono(Connection.class)
+                .block());
+    }
+
+    @Override
+    public Connection getConnection(String id) {
+        return rSocketRequester
+                .route("getConnection")
+                .data(id)
+                .retrieveMono(Connection.class)
+                .block();
+    }
+
+    @Override
+    public List<Connection> getConnections() {
+        return rSocketRequester
+                .route("getConnections")
+                .retrieveMono(new ParameterizedTypeReference<List<Connection>>() {})
+                .block();
+    }
+
+    @Override
+    public Connection update(String id, String name) {
+        Connection connection = new Connection();
+
+        connection.setId(id);
+        connection.setName(name);
+
+        return rSocketRequester
+                .route("updateConnection")
+                .data(connection)
+                .retrieveMono(Connection.class)
+                .block();
+    }
+}
