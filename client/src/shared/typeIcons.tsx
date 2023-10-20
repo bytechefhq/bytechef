277433dc7f886b6bae5d327acfabcea046a91862
Krešimir Čoko <kresimircoko@gmail.com>
2023-10-20 17:18:43 +0000

    303 Extract TYPE_ICONS into a shared file to prevent duplicate code

---
+++ client/src/shared/typeIcons.tsx
@@ -0,0 +1,27 @@
+import {ReactComponent as ArrayIcon} from 'assets/array.svg';
+import {ReactComponent as BooleanIcon} from 'assets/boolean.svg';
+import {ReactComponent as DateIcon} from 'assets/date.svg';
+import {ReactComponent as DateTimeIcon} from 'assets/datetime.svg';
+import {ReactComponent as DynamicIcon} from 'assets/dynamic.svg';
+import {ReactComponent as IntegerIcon} from 'assets/integer.svg';
+import {ReactComponent as NullIcon} from 'assets/null.svg';
+import {ReactComponent as NumberIcon} from 'assets/number.svg';
+import {ReactComponent as ObjectIcon} from 'assets/object.svg';
+import {ReactComponent as OneOfIcon} from 'assets/oneof.svg';
+import {ReactComponent as StringIcon} from 'assets/string.svg';
+import {ReactComponent as TimeIcon} from 'assets/time.svg';
+
+export const TYPE_ICONS = {
+    ARRAY: <ArrayIcon className="h-5 w-5 text-gray-600" />,
+    BOOLEAN: <BooleanIcon className="h-5 w-5 text-gray-600" />,
+    DATE: <DateIcon className="h-5 w-5 text-gray-600" />,
+    DATE_TIME: <DateTimeIcon className="h-5 w-5 text-gray-600" />,
+    DYNAMIC_PROPERTIES: <DynamicIcon className="h-5 w-5 text-gray-600" />,
+    INTEGER: <IntegerIcon className="h-5 w-5 text-gray-600" />,
+    NUMBER: <NumberIcon className="h-5 w-5 text-gray-600" />,
+    NULL: <NullIcon className="h-5 w-5 text-gray-600" />,
+    OBJECT: <ObjectIcon className="h-5 w-5 text-gray-600" />,
+    ONE_OF: <OneOfIcon className="h-5 w-5 text-gray-600" />,
+    STRING: <StringIcon className="h-5 w-5 text-gray-600" />,
+    TIME: <TimeIcon className="h-5 w-5 text-gray-600" />,
+};
