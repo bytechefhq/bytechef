46e9428ff07a9eaa28592f3f4cdd6c86f6f958ee
Ivica Cardic <ivica.cardic@gmail.com>
2023-10-20 17:18:43 +0000

    ISSUE-76 - worker-app - Move worker app to the services dir, add Consul and property support for service discovery

--- server/apps/services/worker-service-app/src/main/java/com/bytechef/config/JacksonConfiguration.java
+++ server/apps/services/worker-service-app/src/main/java/com/bytechef/config/JacksonConfiguration.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016-2018 the original author or authors.
+ * Copyright 2021 <your company/name>.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,31 +12,19 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
- * Modifications copyright (C) 2021 <your company/name>
  */
 
 package com.bytechef.config;
 
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
 /**
- * @author Arik Cohen
  * @author Ivica Cardic
  */
 @Configuration
-public class JsonConfiguration implements Jackson2ObjectMapperBuilderCustomizer {
-
-    @Override
-    public void customize(Jackson2ObjectMapperBuilder jacksonObjectMapperBuilder) {
-        // aJacksonObjectMapperBuilder.serializerByType(Throwable.class, new ExceptionSerializer());
-        // aJacksonObjectMapperBuilder.deserializerByType(JobTaskException.class, new
-        // JobTaskExceptionDeserializer());
-    }
+public class JacksonConfiguration {
 
     @Bean
     JavaTimeModule javaTimeModule() {
