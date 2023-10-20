e9ccc1787dde1adf3158198a0d09ee1ba4e4f57c
Ivica Čardić <ivica.cardic@gmail.com>
2023-10-20 17:18:43 +0000

    272 - Each http based connector supports custom actions, so maybe there is no need to connect to unsupported endpoints, comment for now

--- server/libs/modules/components/httpclient/src/main/java/com/bytechef/component/httpclient/HttpClientComponentHandler.java
+++ server/libs/modules/components/httpclient/src/main/java/com/bytechef/component/httpclient/HttpClientComponentHandler.java
@@ -48,14 +48,14 @@ public class HttpClientComponentHandler implements ComponentHandler {
             HttpClientPutAction.ACTION_DEFINITION,
             HttpClientPatchAction.ACTION_DEFINITION,
             HttpClientDeleteAction.ACTION_DEFINITION,
-            HttpClientHeadAction.ACTION_DEFINITION)
+            HttpClientHeadAction.ACTION_DEFINITION);
 // Each http based connector supports custom actions, so maybe there is no need to connect to unsupported endpoints
 // via httpclient connector but instead use custom actions feature
 //        .filterCompatibleConnectionDefinitions(
 //            (componentDefinition, connectionDefinitions) -> connectionDefinitions.stream()
 //                .filter(ConnectionDefinition::containsAuthorizations)
 //                .toList())
-    ;
+//    ;
 
     @Override
     public ComponentDefinition getDefinition() {
