1c8f416ea7cfeb2c63715d292583a0aa4dcde27e
Ivica Čardić <ivica.cardic@gmail.com>
2023-10-20 17:18:43 +0000

    228 - Add a Label component, it will be used in ConnectionDialog

---
+++ client/src/components/Label/Label.tsx
@@ -0,0 +1,18 @@
+import {Label as LabelPrimitive} from '@radix-ui/react-label';
+import React from 'react';
+
+interface LabelProps {
+    htmlFor: string;
+    value: string;
+}
+
+const Label = ({htmlFor, value}: LabelProps) => (
+    <LabelPrimitive
+        htmlFor={htmlFor}
+        className="ml-3 select-none text-sm font-medium text-gray-900 dark:text-gray-100"
+    >
+        {value}
+    </LabelPrimitive>
+);
+
+export default Label;
