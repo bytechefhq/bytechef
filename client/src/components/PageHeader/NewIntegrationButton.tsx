2cf8d0dbfbe35061b2296b3a145f4d5205443510
Marta Jurcevic <marta.raptavi997@gmail.com>
2023-10-20 17:18:43 +0000

    0 - update page header

--- client/src/components/PageHeader/NewIntegrationButton.tsx
+++ client/src/components/PageHeader/NewIntegrationButton.tsx
@@ -1,47 +1,49 @@
-import React from 'react'
-import * as Dialog from '@radix-ui/react-dialog'
-import { Cross2Icon } from '@radix-ui/react-icons'
-import './styles.css'
+import {Root, Portal, Overlay, Content, Close} from '@radix-ui/react-dialog';
+import {Cross2Icon} from '@radix-ui/react-icons';
+import {useState} from 'react';
+import './NewIntegrationButton.css';
 
-const NewIntegrationButton = () => (
-  <Dialog.Root>
-    <Dialog.Trigger asChild>
-      <button className="Button violet">New Integration</button>
-    </Dialog.Trigger>
-    <Dialog.Portal>
-      <Dialog.Overlay className="DialogOverlay" />
-      <Dialog.Content className="DialogContent">
-        <Dialog.Title className="DialogTitle">Edit profile</Dialog.Title>
-        <Dialog.Description className="DialogDescription">
-          Make changes to your profile here. Click save when you're done.
-        </Dialog.Description>
-        <fieldset className="Fieldset">
-          <label className="Label" htmlFor="name">
-            Name
-          </label>
-          <input className="Input" id="name" defaultValue="Pedro Duarte" />
-        </fieldset>
-        <fieldset className="Fieldset">
-          <label className="Label" htmlFor="username">
-            Username
-          </label>
-          <input className="Input" id="username" defaultValue="@peduarte" />
-        </fieldset>
-        <div
-          style={{ display: 'flex', marginTop: 25, justifyContent: 'flex-end' }}
-        >
-          <Dialog.Close asChild>
-            <button className="Button green">Save changes</button>
-          </Dialog.Close>
-        </div>
-        <Dialog.Close asChild>
-          <button className="IconButton" aria-label="Close">
-            <Cross2Icon />
-          </button>
-        </Dialog.Close>
-      </Dialog.Content>
-    </Dialog.Portal>
-  </Dialog.Root>
-)
+const NewIntegrationButton = () => {
+	const [openDialog, setOpenDialog] = useState(false);
 
-export default NewIntegrationButton
+	const handleOpenDialog = () => {
+		setOpenDialog(true);
+	};
+
+	const handleCloseDialog = () => {
+		setOpenDialog(false);
+	};
+
+	return (
+		<>
+			<button
+				className="Button violet"
+				onClick={() => handleOpenDialog()}
+			>
+				New Integration
+			</button>
+
+			{openDialog && (
+				<Root open={openDialog}>
+					<Portal>
+						<Overlay className="DialogOverlay" />
+
+						<Content className="DialogContent">
+							<Close asChild>
+								<button
+									className="IconButton"
+									aria-label="Close"
+									onClick={() => handleCloseDialog()}
+								>
+									<Cross2Icon />
+								</button>
+							</Close>
+						</Content>
+					</Portal>
+				</Root>
+			)}
+		</>
+	);
+};
+
+export default NewIntegrationButton;
--- client/src/components/PageHeader/PageHeader.tsx
+++ client/src/components/PageHeader/PageHeader.tsx
@@ -1,27 +1,18 @@
-import NewIntegrationButton from './NewIntegrationButton'
-import './styles.css'
+import NewIntegrationButton from './NewIntegrationButton';
+import './NewIntegrationButton.css';
 
 export const PageHeader = () => {
-  const onClickHandler = () => {
-    // some comment
-    console.log('inside onClickHandler')
-  }
+	return (
+		<div className="mb-6 flex justify-center py-4">
+			<div className="flex w-full items-center justify-between">
+				<h2 className="text-2xl tracking-tight text-gray-900 dark:text-gray-200">
+					All integrations
+				</h2>
 
-  return (
-    <div className="mb-6 flex justify-center py-4">
-      <div className="flex w-full items-center justify-between">
-        <h2 className="text-2xl tracking-tight text-gray-900 dark:text-gray-200">
-          All integrations
-        </h2>
-
-        <div>
-          {/* <button onClick={onClickHandler} className="button">
-            New integration
-          </button> */}
-
-          <NewIntegrationButton />
-        </div>
-      </div>
-    </div>
-  )
-}
+				<div>
+					<NewIntegrationButton />
+				</div>
+			</div>
+		</div>
+	);
+};
