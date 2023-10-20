2499843794806cc3edf64ca6142dfc17eea1c283
Krešimir Čoko <kresimircoko@gmail.com>
2023-10-20 17:18:43 +0000

    198 Improve display of the Tooltip

--- client/src/components/Tooltip/Tooltip.tsx
+++ client/src/components/Tooltip/Tooltip.tsx
@@ -13,13 +13,13 @@ const Tooltip = ({side, text, children}: PropsWithChildren<TooltipProps>) => (
             <Trigger asChild>{children}</Trigger>
 
             <Content
-                className="z-10 inline-flex items-center rounded-md bg-gray-800 px-4 py-2.5 radix-side-bottom:animate-slide-up-fade radix-side-left:animate-slide-right-fade radix-side-right:animate-slide-left-fade radix-side-top:animate-slide-down-fade dark:bg-gray-800"
+                className="z-10 inline-flex max-w-sm items-center rounded-md bg-gray-800 px-4 py-2.5 radix-side-bottom:animate-slide-up-fade radix-side-left:animate-slide-right-fade radix-side-right:animate-slide-left-fade radix-side-top:animate-slide-down-fade dark:bg-gray-800"
                 side={side}
                 sideOffset={4}
             >
                 <Arrow className="fill-current text-gray-800 dark:text-gray-800" />
 
-                <span className="block text-xs leading-none text-white dark:text-gray-100">
+                <span className="block text-xs leading-4 text-white dark:text-gray-100">
                     {text}
                 </span>
             </Content>
