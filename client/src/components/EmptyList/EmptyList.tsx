407d5922b5b760c8a3ff61caf112fc471f932bf5
Ivica Čardić <ivica.cardic@gmail.com>
2023-10-20 17:18:43 +0000

    #167 - client - Add EmptyList component that is shown when there are no items in the list

---
+++ client/src/components/EmptyList/EmptyList.tsx
@@ -0,0 +1,33 @@
+import {ReactNode} from 'react';
+
+interface EmptyListProps {
+    button: ReactNode;
+    icon: ReactNode;
+    message: string;
+    title: string;
+}
+
+const EmptyList = ({
+    button,
+    icon,
+    title,
+    message,
+}: EmptyListProps): JSX.Element => {
+    return (
+        <div className="text-center">
+            <span className="mx-auto inline-block">{icon}</span>
+
+            <h3 className="mt-2 text-sm font-semibold text-gray-900">
+                {title}
+            </h3>
+
+            <p className="mt-1 text-sm text-gray-500">{message}</p>
+
+            <div className="mt-6">{button}</div>
+        </div>
+    );
+};
+
+EmptyList.displayName = 'EmptyList';
+
+export default EmptyList;
