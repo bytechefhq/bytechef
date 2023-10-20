48db034ea2ce4e84a5b9cd21eb96e303312b3f41
Ivica Cardic <ivica.cardic@gmail.com>
2023-10-20 17:18:43 +0000

    272 - client - Generate

--- client/src/middleware/automation/connection/apis/index.ts
+++ client/src/middleware/automation/connection/apis/index.ts
@@ -1,4 +1,5 @@
 /* tslint:disable */
 /* eslint-disable */
+export * from './ConnectionOauth2PropertiesApi';
 export * from './ConnectionTagsApi';
 export * from './ConnectionsApi';
--- client/src/middleware/automation/connection/models/index.ts
+++ client/src/middleware/automation/connection/models/index.ts
@@ -1,5 +1,6 @@
 /* tslint:disable */
 /* eslint-disable */
 export * from './ConnectionModel';
+export * from './OAuth2PropertiesModel';
 export * from './TagModel';
 export * from './UpdateTagsRequestModel';
