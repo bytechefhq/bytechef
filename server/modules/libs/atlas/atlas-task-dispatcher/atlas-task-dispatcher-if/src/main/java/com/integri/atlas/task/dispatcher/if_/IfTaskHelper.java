a2fbf286473fcf9204fdf8eeb9da5f551b4564d5
Matija Petanjek <matija.petanjek7@gmail.com>
2023-10-20 17:18:43 +0000

    ISSUE-9 if task dispatcher

---
+++ server/modules/libs/atlas/atlas-task-dispatcher/atlas-task-dispatcher-if/src/main/java/com/integri/atlas/task/dispatcher/if_/IfTaskHelper.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2021 <your company/name>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.integri.atlas.task.dispatcher.if_;
+
+import com.integri.atlas.engine.core.MapObject;
+import com.integri.atlas.engine.core.task.TaskExecution;
+import com.integri.atlas.engine.core.task.evaluator.TaskEvaluator;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Matija Petanjek
+ */
+public class IfTaskHelper {
+
+    public boolean resolveCase(TaskEvaluator taskEvaluator, TaskExecution aIfTask) {
+        List<MapObject> conditions = aIfTask.getList("conditions", MapObject.class);
+        String combineOperation = aIfTask.getRequiredString("combineOperation");
+
+        return taskEvaluator.evaluate(
+            String.join(getBooleanOperator(combineOperation), getConditions(conditions)),
+            Boolean.class
+        );
+    }
+
+    private List<String> getConditions(List<MapObject> aConditions) {
+        List<String> conditions = new ArrayList<>();
+
+        for (MapObject condition : aConditions) {
+            for (String key : condition.keySet()) {
+                MapObject conditionParts = condition.get(key, MapObject.class);
+
+                conditions.add(
+                    conditionParts.getRequiredString("value1") +
+                    conditionParts.getRequiredString("operation") +
+                    conditionParts.getRequiredString("value2")
+                );
+            }
+        }
+
+        return conditions;
+    }
+
+    private String getBooleanOperator(String combineOperation) {
+        if (combineOperation.equals("any")) {
+            return "||";
+        } else if (combineOperation.equals("all")) {
+            return "&&";
+        }
+
+        throw new IllegalArgumentException("Invalid combine operation: " + combineOperation);
+    }
+}
