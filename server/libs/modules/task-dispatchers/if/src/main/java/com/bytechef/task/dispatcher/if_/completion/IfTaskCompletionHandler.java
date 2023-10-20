2cbfef233ccfd0572ecc1a36b5273d79457e76f1
Ivica Cardic <ivica.cardic@gmail.com>
2023-10-20 17:18:43 +0000

    ISSUE-76 - if - Move if task dispatcher

---
+++ server/libs/modules/task-dispatchers/if/src/main/java/com/bytechef/task/dispatcher/if_/completion/IfTaskCompletionHandler.java
@@ -0,0 +1,133 @@
+/*
+ * Copyright 2021 <your company/name>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef.task.dispatcher.if_.completion;
+
+import static com.bytechef.hermes.task.dispatcher.constants.Versions.VERSION_1;
+import static com.bytechef.task.dispatcher.if_.constants.IfTaskDispatcherConstants.CASE_FALSE;
+import static com.bytechef.task.dispatcher.if_.constants.IfTaskDispatcherConstants.CASE_TRUE;
+import static com.bytechef.task.dispatcher.if_.constants.IfTaskDispatcherConstants.IF;
+
+import com.bytechef.atlas.coordinator.task.completion.TaskCompletionHandler;
+import com.bytechef.atlas.domain.Context;
+import com.bytechef.atlas.domain.TaskExecution;
+import com.bytechef.atlas.service.ContextService;
+import com.bytechef.atlas.service.TaskExecutionService;
+import com.bytechef.atlas.task.WorkflowTask;
+import com.bytechef.atlas.task.dispatcher.TaskDispatcher;
+import com.bytechef.atlas.task.evaluator.TaskEvaluator;
+import com.bytechef.atlas.task.execution.TaskStatus;
+import com.bytechef.commons.date.LocalDateTimeUtils;
+import com.bytechef.task.dispatcher.if_.util.IfTaskUtils;
+import java.time.LocalDateTime;
+import java.util.List;
+
+/**
+ * @author Matija Petanjek
+ */
+public class IfTaskCompletionHandler implements TaskCompletionHandler {
+
+    private final ContextService contextService;
+    private final TaskCompletionHandler taskCompletionHandler;
+    private final TaskDispatcher taskDispatcher;
+    private final TaskEvaluator taskEvaluator;
+    private final TaskExecutionService taskExecutionService;
+
+    public IfTaskCompletionHandler(
+            ContextService contextService,
+            TaskCompletionHandler taskCompletionHandler,
+            TaskDispatcher taskDispatcher,
+            TaskEvaluator taskEvaluator,
+            TaskExecutionService taskExecutionService) {
+        this.contextService = contextService;
+        this.taskCompletionHandler = taskCompletionHandler;
+        this.taskDispatcher = taskDispatcher;
+        this.taskEvaluator = taskEvaluator;
+        this.taskExecutionService = taskExecutionService;
+    }
+
+    @Override
+    public boolean canHandle(TaskExecution taskExecution) {
+        String parentId = taskExecution.getParentId();
+
+        if (parentId != null) {
+            TaskExecution parentTaskExecution = taskExecutionService.getTaskExecution(parentId);
+
+            return parentTaskExecution.getType().equals(IF + "/v" + VERSION_1);
+        }
+
+        return false;
+    }
+
+    @Override
+    public void handle(TaskExecution taskExecution) {
+        TaskExecution completedSubTaskExecution = new TaskExecution(taskExecution);
+
+        completedSubTaskExecution.setStatus(TaskStatus.COMPLETED);
+
+        taskExecutionService.update(completedSubTaskExecution);
+
+        TaskExecution ifTaskExecution =
+                new TaskExecution(taskExecutionService.getTaskExecution(taskExecution.getParentId()));
+
+        if (taskExecution.getOutput() != null && taskExecution.getName() != null) {
+            Context context = contextService.peek(ifTaskExecution.getId());
+
+            Context newContext = new Context(context);
+
+            newContext.put(taskExecution.getName(), taskExecution.getOutput());
+
+            contextService.push(ifTaskExecution.getId(), newContext);
+        }
+
+        List<WorkflowTask> subWorkflowTasks;
+
+        if (IfTaskUtils.resolveCase(ifTaskExecution)) {
+            subWorkflowTasks = ifTaskExecution.getWorkflowTasks(CASE_TRUE);
+        } else {
+            subWorkflowTasks = ifTaskExecution.getWorkflowTasks(CASE_FALSE);
+        }
+
+        if (taskExecution.getTaskNumber() < subWorkflowTasks.size()) {
+            WorkflowTask subWorkflowTask = subWorkflowTasks.get(taskExecution.getTaskNumber());
+
+            TaskExecution subTaskExecution = TaskExecution.of(
+                    subWorkflowTask,
+                    ifTaskExecution.getJobId(),
+                    ifTaskExecution.getId(),
+                    ifTaskExecution.getPriority(),
+                    taskExecution.getTaskNumber() + 1);
+
+            Context context = new Context(contextService.peek(ifTaskExecution.getId()));
+
+            contextService.push(subTaskExecution.getId(), context);
+
+            TaskExecution evaluatedTaskExecution = taskEvaluator.evaluate(subTaskExecution, context);
+
+            evaluatedTaskExecution = taskExecutionService.add(evaluatedTaskExecution);
+
+            taskDispatcher.dispatch(evaluatedTaskExecution);
+        }
+        // no more tasks to execute -- complete the If
+        else {
+            ifTaskExecution.setEndTime(LocalDateTime.now());
+            ifTaskExecution.setExecutionTime(LocalDateTimeUtils.getTime(ifTaskExecution.getEndTime())
+                    - LocalDateTimeUtils.getTime(ifTaskExecution.getStartTime()));
+
+            taskCompletionHandler.handle(ifTaskExecution);
+        }
+    }
+}
---
+++ server/libs/modules/task-dispatchers/if/src/test/java/com/bytechef/task/dispatcher/if_/IfTaskDispatcherIntTest.java
@@ -0,0 +1,144 @@
+/*
+ * Copyright 2021 <your company/name>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef.task.dispatcher.if_;
+
+import com.bytechef.atlas.service.ContextService;
+import com.bytechef.atlas.service.TaskExecutionService;
+import com.bytechef.atlas.test.workflow.WorkflowExecutor;
+import com.bytechef.hermes.test.task.handler.TestVarTaskHandler;
+import com.bytechef.task.dispatcher.if_.completion.IfTaskCompletionHandler;
+import java.util.List;
+import java.util.Map;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author Matija Petanjek
+ * @author Ivica Cardic
+ */
+@SpringBootTest
+public class IfTaskDispatcherIntTest {
+
+    private TestVarTaskHandler<Object, Object> testVarTaskHandler;
+
+    @Autowired
+    protected ContextService contextService;
+
+    @Autowired
+    protected TaskExecutionService taskExecutionService;
+
+    @Autowired
+    private WorkflowExecutor workflowExecutor;
+
+    @BeforeEach
+    void setUp() {
+        testVarTaskHandler = new TestVarTaskHandler<>(Map::put);
+    }
+
+    @Test
+    public void testDispatchBoolean() {
+        workflowExecutor.execute(
+                "if_v1-conditions-boolean",
+                Map.of("value1", "true", "value2", "false"),
+                getGetTaskCompletionHandlers(),
+                getGetTaskDispatcherResolvers(),
+                getTaskHandlerMap());
+
+        Assertions.assertEquals("false branch", testVarTaskHandler.get("equalsResult"));
+        Assertions.assertEquals("true branch", testVarTaskHandler.get("notEqualsResult"));
+    }
+
+    @Test
+    public void testDispatchDateTime() {
+        workflowExecutor.execute(
+                "if_v1-conditions-dateTime",
+                Map.of("value1", "2022-01-01T00:00:00", "value2", "2022-01-01T00:00:01"),
+                getGetTaskCompletionHandlers(),
+                getGetTaskDispatcherResolvers(),
+                getTaskHandlerMap());
+
+        Assertions.assertEquals("false branch", testVarTaskHandler.get("afterResult"));
+        Assertions.assertEquals("true branch", testVarTaskHandler.get("beforeResult"));
+    }
+
+    @Test
+    public void testDispatchExpression() {
+        workflowExecutor.execute(
+                "if_v1-conditions-expression",
+                Map.of("value1", 100, "value2", 200),
+                getGetTaskCompletionHandlers(),
+                getGetTaskDispatcherResolvers(),
+                getTaskHandlerMap());
+
+        Assertions.assertEquals("false branch", testVarTaskHandler.get("equalsResult"));
+    }
+
+    @Test
+    public void testDispatchNumber() {
+        workflowExecutor.execute(
+                "if_v1-conditions-number",
+                Map.of("value1", 100, "value2", 200),
+                getGetTaskCompletionHandlers(),
+                getGetTaskDispatcherResolvers(),
+                getTaskHandlerMap());
+
+        Assertions.assertEquals("false branch", testVarTaskHandler.get("equalsResult"));
+        Assertions.assertEquals("true branch", testVarTaskHandler.get("notEqualsResult"));
+        Assertions.assertEquals("false branch", testVarTaskHandler.get("greaterResult"));
+        Assertions.assertEquals("true branch", testVarTaskHandler.get("lessResult"));
+        Assertions.assertEquals("false branch", testVarTaskHandler.get("greaterEqualsResult"));
+        Assertions.assertEquals("true branch", testVarTaskHandler.get("lessEqualsResult"));
+    }
+
+    @Test
+    public void testDispatchString() {
+        workflowExecutor.execute(
+                "if_v1-conditions-string",
+                Map.of("value1", "Hello World", "value2", "Hello"),
+                getGetTaskCompletionHandlers(),
+                getGetTaskDispatcherResolvers(),
+                getTaskHandlerMap());
+
+        Assertions.assertEquals("false branch", testVarTaskHandler.get("equalsResult"));
+        Assertions.assertEquals("true branch", testVarTaskHandler.get("notEqualsResult"));
+        Assertions.assertEquals("true branch", testVarTaskHandler.get("containsResult"));
+        Assertions.assertEquals("false branch", testVarTaskHandler.get("notContainsResult"));
+        Assertions.assertEquals("true branch", testVarTaskHandler.get("startsWithResult"));
+        Assertions.assertEquals("false branch", testVarTaskHandler.get("endsWithResult"));
+        Assertions.assertEquals("false branch", testVarTaskHandler.get("isEmptyResult"));
+        Assertions.assertEquals("false branch", testVarTaskHandler.get("regexResult"));
+    }
+
+    private WorkflowExecutor.GetTaskHandlerMapSupplier getTaskHandlerMap() {
+        return () -> Map.of("var", testVarTaskHandler);
+    }
+
+    private static WorkflowExecutor.GetTaskDispatcherResolversFunction getGetTaskDispatcherResolvers() {
+        return (contextService, counterService, messageBroker, taskDispatcher, taskEvaluator, taskExecutionService) ->
+                List.of(new IfTaskDispatcher(
+                        contextService, messageBroker, taskDispatcher, taskEvaluator, taskExecutionService));
+    }
+
+    private WorkflowExecutor.GetTaskCompletionHandlersFunction getGetTaskCompletionHandlers() {
+        return (counterService, taskCompletionHandler, taskDispatcher, taskEvaluator, taskExecutionService) ->
+                List.of(new IfTaskCompletionHandler(
+                        contextService, taskCompletionHandler, taskDispatcher, taskEvaluator, taskExecutionService));
+    }
+}
