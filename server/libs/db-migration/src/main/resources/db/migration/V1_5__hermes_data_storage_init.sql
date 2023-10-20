61430107e6357563df63627dcaa4e5a34ed6ddaf
Ivica Čardić <ivica.cardic@gmail.com>
2023-10-20 17:18:43 +0000

    272 - Add - Update schemas for scheduler and data storage

---
+++ server/libs/db-migration/src/main/resources/db/migration/V1_5__hermes_data_storage_init.sql
@@ -0,0 +1,16 @@
+CREATE TABLE IF NOT EXISTS data_storage (
+    id                       BIGSERIAL    NOT NULL PRIMARY KEY,
+    workflow_id              VARCHAR(256) NULL,
+    instance_id              BIGINT       NULL,
+    job_id                   BIGINT       NULL,
+    key                      VARCHAR(256) NOT NULL,
+    scope                    VARCHAR(256) NOT NULL,
+    value                    TEXT         NOT NULL,
+    created_date             TIMESTAMP    NOT NULL,
+    created_by               VARCHAR(256) NOT NULL,
+    last_modified_date       TIMESTAMP    NOT NULL,
+    last_modified_by         VARCHAR(256) NOT NULL,
+    version                  BIGINT       NOT NULL
+);
+
+ALTER TABLE data_storage ADD CONSTRAINT uk_data_storage_key UNIQUE (key, scope);
--- server/libs/db-migration/src/main/resources/db/migration/V1_6__helios_project_init.sql
+++ server/libs/db-migration/src/main/resources/db/migration/V1_6__helios_project_init.sql
@@ -15,11 +15,29 @@ CREATE TABLE IF NOT EXISTS project (
 
 CREATE TABLE IF NOT EXISTS project_instance (
     id                       BIGSERIAL    NOT NULL PRIMARY KEY,
+    project_id               BIGINT       NULL,
     name                     VARCHAR(256) NOT NULL,
     description              TEXT         NULL,
     status                   VARCHAR(256) NOT NULL,
-    configuration_parameters TEXT         NOT NULL,
-    project_id               BIGINT       NULL,
+    created_date             TIMESTAMP    NOT NULL,
+    created_by               VARCHAR(256) NOT NULL,
+    last_modified_date       TIMESTAMP    NOT NULL,
+    last_modified_by         VARCHAR(256) NOT NULL,
+    version                  BIGINT       NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS project_instance_tag (
+   id                       BIGSERIAL    NOT NULL PRIMARY KEY,
+   project_instance_id      BIGSERIAL    NOT NULL,
+   tag_id                   BIGSERIAL    NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS project_instance_workflow (
+    id                       BIGSERIAL    NOT NULL PRIMARY KEY,
+    project_instance_id      BIGSERIAL    NOT NULL,
+    workflow_id              VARCHAR(256) NOT NULL,
+    input_parameters         TEXT         NOT NULL,
+    enabled                  BOOLEAN      NOT NULL,
     last_execution_date      TIMESTAMP    NULL,
     created_date             TIMESTAMP    NOT NULL,
     created_by               VARCHAR(256) NOT NULL,
@@ -28,22 +46,16 @@ CREATE TABLE IF NOT EXISTS project_instance (
     version                  BIGINT       NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS project_instance_connection (
-    id                       BIGSERIAL    NOT NULL PRIMARY KEY,
-    project_instance_id      BIGSERIAL    NOT NULL,
-    connection_id                   BIGSERIAL    NOT NULL
+CREATE TABLE IF NOT EXISTS project_instance_workflow_connection (
+    id                                BIGSERIAL    NOT NULL PRIMARY KEY,
+    project_instance_workflow_id      BIGSERIAL    NOT NULL,
+    connection_id                     BIGSERIAL    NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS project_instance_job (
-    id                       BIGSERIAL    NOT NULL PRIMARY KEY,
-    project_instance_id      BIGSERIAL    NOT NULL,
-    job_id                   BIGSERIAL    NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS project_instance_tag (
-   id                       BIGSERIAL    NOT NULL PRIMARY KEY,
-   project_instance_id      BIGSERIAL    NOT NULL,
-   tag_id                   BIGSERIAL    NOT NULL
+CREATE TABLE IF NOT EXISTS project_instance_workflow_job (
+    id                                BIGSERIAL    NOT NULL PRIMARY KEY,
+    project_instance_workflow_id      BIGSERIAL    NOT NULL,
+    job_id                            BIGSERIAL    NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS project_tag (
@@ -62,12 +74,13 @@ ALTER TABLE project ADD CONSTRAINT uk_project_name UNIQUE (name);
 ALTER TABLE project_instance ADD CONSTRAINT uk_project_instance_name UNIQUE (name);
 ALTER TABLE project ADD CONSTRAINT fk_project_category FOREIGN KEY (category_id) REFERENCES category (id);
 ALTER TABLE project_instance ADD CONSTRAINT fk_project_instance_project FOREIGN KEY (project_id) REFERENCES project (id);
-ALTER TABLE project_instance_connection ADD CONSTRAINT fk_project_instance_connection_project_instance FOREIGN KEY (project_instance_id) REFERENCES project_instance (id);
-ALTER TABLE project_instance_connection ADD CONSTRAINT fk_project_instance_connection_connection FOREIGN KEY (connection_id) REFERENCES connection (id);
-ALTER TABLE project_instance_job ADD CONSTRAINT fk_project_instance_job_project_instance FOREIGN KEY (project_instance_id) REFERENCES project_instance (id);
-ALTER TABLE project_instance_job ADD CONSTRAINT fk_project_instance_job_job FOREIGN KEY (job_id) REFERENCES job (id);
 ALTER TABLE project_instance_tag ADD CONSTRAINT fk_project_instance_tag_project_instance FOREIGN KEY (project_instance_id) REFERENCES project_instance (id);
 ALTER TABLE project_instance_tag ADD CONSTRAINT fk_project_instance_tag_tag FOREIGN KEY (tag_id) REFERENCES tag (id);
+ALTER TABLE project_instance_workflow ADD CONSTRAINT fk_project_instance_workflow_project_instance FOREIGN KEY (project_instance_id) REFERENCES project_instance (id);
+ALTER TABLE project_instance_workflow_connection ADD CONSTRAINT fk_project_instance_workflow_connection_piw FOREIGN KEY (project_instance_workflow_id) REFERENCES project_instance_workflow (id);
+ALTER TABLE project_instance_workflow_connection ADD CONSTRAINT fk_project_instance_workflow_connection_connection FOREIGN KEY (connection_id) REFERENCES connection (id);
+ALTER TABLE project_instance_workflow_job ADD CONSTRAINT fk_project_instance_workflow_job_project_instance_workflow FOREIGN KEY (project_instance_workflow_id) REFERENCES project_instance_workflow (id);
+ALTER TABLE project_instance_workflow_job ADD CONSTRAINT fk_project_instance_workflow_job_job FOREIGN KEY (job_id) REFERENCES job (id);
 ALTER TABLE project_tag ADD CONSTRAINT fk_project_tag_project FOREIGN KEY (project_id) REFERENCES project (id);
 ALTER TABLE project_tag ADD CONSTRAINT fk_project_tag_tag FOREIGN KEY (tag_id) REFERENCES tag (id);
 ALTER TABLE project_workflow ADD CONSTRAINT fk_project_workflow_project FOREIGN KEY (project_id) REFERENCES project (id);
