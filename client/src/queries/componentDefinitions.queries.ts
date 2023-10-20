891649880e25bf2b797ca099ef878490539315b7
Krešimir Čoko <kresimircoko@gmail.com>
2023-10-20 17:18:43 +0000

    303 Add a query to fetch an individual component definition

--- client/src/queries/componentDefinitions.queries.ts
+++ client/src/queries/componentDefinitions.queries.ts
@@ -2,16 +2,30 @@ import {useQuery} from '@tanstack/react-query';
 
 import {
     ComponentDefinitionBasicModel,
+    ComponentDefinitionWithBasicActionsModel,
     ComponentDefinitionsApi,
+    GetComponentDefinitionRequest,
     GetComponentDefinitionsRequest,
 } from '../middleware/definition-registry';
 
 export const ComponentDefinitionKeys = {
+    componentDefinition: (request: GetComponentDefinitionRequest) => [
+        'componentDefinition',
+        request,
+    ],
     componentDefinitions: (request?: GetComponentDefinitionsRequest) => [
         'componentDefinitions',
         request,
     ],
 };
+
+export const useGetComponentDefinitionQuery = (
+    request: GetComponentDefinitionRequest
+) =>
+    useQuery<ComponentDefinitionWithBasicActionsModel, Error>(
+        ComponentDefinitionKeys.componentDefinition(request),
+        () => new ComponentDefinitionsApi().getComponentDefinition(request)
+    );
 
 export const useGetComponentDefinitionsQuery = (
     request?: GetComponentDefinitionsRequest
