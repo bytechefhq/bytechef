a66ab5562d425c1f7476fbfe7475f79c0b7166bb
Ivica Čardić <ivica.cardic@gmail.com>
2023-10-20 17:18:43 +0000

    #68 - cli - Update script for building connectors

--- cli/cli-app/generate_connectors.sh
+++ cli/cli-app/generate_connectors.sh
@@ -2,7 +2,17 @@
 
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
+echo "Generate Jira OpenAPI Connector"
 $SCRIPTPATH/bytechef.sh component init --open-api-path=/Volumes/data/bytechef/bytechef/server/libs/modules/components/jira/openapi.yaml -o /Volumes/data/bytechef/bytechef/server/libs/modules/components jira
+
+#echo "Generate MailChimp OpenAPI Connector"
+#$SCRIPTPATH/bytechef.sh component init --open-api-path=/Volumes/data/bytechef/bytechef/server/libs/modules/components/mailchimp/openapi.yaml -o /Volumes/data/bytechef/bytechef/server/libs/modules/components mailchimp
+
+echo "Generate PetStore OpenAPI Connector"
 $SCRIPTPATH/bytechef.sh component init --open-api-path=/Volumes/data/bytechef/bytechef/server/libs/modules/components/petstore/openapi.yaml -o /Volumes/data/bytechef/bytechef/server/libs/modules/components petstore
+
+echo "Generate Pipedrive OpenAPI Connector"
 $SCRIPTPATH/bytechef.sh component init --open-api-path=/Volumes/data/bytechef/bytechef/server/libs/modules/components/pipedrive/openapi.yaml -o /Volumes/data/bytechef/bytechef/server/libs/modules/components pipedrive
+
+echo "Generate Shopify OpenAPI Connector"
 $SCRIPTPATH/bytechef.sh component init --open-api-path=/Volumes/data/bytechef/bytechef/server/libs/modules/components/shopify/openapi.yaml -o /Volumes/data/bytechef/bytechef/server/libs/modules/components shopify
