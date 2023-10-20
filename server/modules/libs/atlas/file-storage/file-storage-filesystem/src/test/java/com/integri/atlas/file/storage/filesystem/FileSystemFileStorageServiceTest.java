5bd32b52c0cb2e9f033fee775965806a631413a9
Ivica Cardic <ivica.cardic@gmail.com>
2023-10-20 17:18:43 +0000

    ISSSUE-38 - file-storage-filesystem - Rename file-storage-fs module to file-storage-filesystem module, rename FileStorageService to FileSystemFileStorageService

---
+++ server/modules/libs/atlas/file-storage/file-storage-filesystem/src/test/java/com/integri/atlas/file/storage/filesystem/FileSystemFileStorageServiceTest.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2021 <your company/name>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.integri.atlas.file.storage.filesystem;
+
+import com.integri.atlas.engine.core.file.storage.FileEntry;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+import org.assertj.core.api.Assertions;
+import org.assertj.core.util.Files;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Ivica Cardic
+ */
+public class FileSystemFileStorageServiceTest {
+
+    private static final String TEST_STRING = "test string";
+
+    private static final FileSystemFileStorageService fileStorageService = new FileSystemFileStorageService(
+        "/tmp/integri/files"
+    );
+
+    @Test
+    public void testOpenInputStream() throws IOException {
+        FileEntry fileEntry = fileStorageService.write(
+            "fileName.txt",
+            new ByteArrayInputStream(TEST_STRING.getBytes())
+        );
+
+        InputStream inputStream = fileStorageService.openInputStream(fileEntry.getUrl());
+
+        Assertions.assertThat(new String(inputStream.readAllBytes())).isEqualTo(TEST_STRING);
+    }
+
+    @Test
+    public void testRead() {
+        FileEntry fileEntry = fileStorageService.write(
+            "fileName.txt",
+            new ByteArrayInputStream(TEST_STRING.getBytes())
+        );
+
+        Assertions.assertThat(fileStorageService.read(fileEntry.getUrl())).isEqualTo(TEST_STRING);
+    }
+
+    @Test
+    public void testWrite() {
+        FileEntry fileEntry = fileStorageService.write(
+            "fileName.txt",
+            new ByteArrayInputStream(TEST_STRING.getBytes())
+        );
+
+        String path = fileEntry.getUrl();
+
+        Assertions.assertThat(path).startsWith("file:/tmp/integri/files/");
+
+        String url = fileEntry.getUrl();
+
+        Assertions
+            .assertThat(Files.contentOf(new File(url.replace("file:", "")), Charset.defaultCharset()))
+            .isEqualTo(TEST_STRING);
+    }
+}
