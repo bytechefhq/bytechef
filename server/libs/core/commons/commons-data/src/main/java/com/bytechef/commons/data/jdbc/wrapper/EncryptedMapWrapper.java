3db67d9d007960993488c14012b540b95a4f4d45
Ivica Cardic <ivica.cardic@gmail.com>
2023-10-20 17:18:43 +0000

    ISSUE-76 - commons-data - Add custom converters and wrappers for Map when Map is used as type for field in domain

---
+++ server/libs/core/commons/commons-data/src/main/java/com/bytechef/commons/data/jdbc/wrapper/EncryptedMapWrapper.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2021 <your company/name>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef.commons.data.jdbc.wrapper;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * See https://github.com/spring-projects/spring-data-relational/issues/867
+ *
+ * @author Ivica Cardic
+ */
+public class EncryptedMapWrapper {
+
+    private Map<String, Object> map = Collections.emptyMap();
+
+    public EncryptedMapWrapper() {}
+
+    public EncryptedMapWrapper(Map<String, Object> map) {
+        this.map = new HashMap<>(map);
+    }
+
+    public Map<String, Object> getMap() {
+        return new HashMap<>(map);
+    }
+
+    public void put(String key, Object value) {
+        this.map.put(key, value);
+    }
+
+    @Override
+    public String toString() {
+        return "EncryptedMapWrapper{" + "map=" + map + '}';
+    }
+}
