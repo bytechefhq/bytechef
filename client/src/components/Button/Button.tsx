8a7c725c52f5ad20a1570e5cb5d71b9ec2088cc6
Krešimir Čoko <kresimir.coko@liferay.com>
2023-10-20 17:18:43 +0000

    0 - Code formatting and small improvements

--- client/src/components/Button/Button.tsx
+++ client/src/components/Button/Button.tsx
@@ -1,16 +1,14 @@
-import {PropsWithChildren} from 'react';
+import React from 'react';
 
-type Props = {
-    title: string;
-};
+const Button: React.FC<{
+    buttonLabel: string;
+}> = ({buttonLabel}) => (
+    <button
+        type="button"
+        className="inline-flex items-center rounded-md border border-transparent bg-black px-2 py-1 text-base font-medium text-white shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:bg-sky-500 dark:hover:bg-sky-400"
+    >
+        {buttonLabel}
+    </button>
+);
 
-export const Button: React.FC<PropsWithChildren<Props>> = ({title}) => {
-    return (
-        <button
-            type="button"
-            className="inline-flex items-center rounded-md border border-transparent bg-black px-2 py-1 text-base font-medium text-white shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:bg-sky-500 dark:hover:bg-sky-400"
-        >
-            {title}
-        </button>
-    );
-};
+export default Button;
