9e5140f4c281dffb69810e9c2903c166100f40b1
Ivica Cardic <ivica.cardic@gmail.com>
2023-10-20 17:18:43 +0000

    ISSUE-76 - hermes-component-jdbc-impl - Update support for jdbc based componnets/connectors

---
+++ server/libs/hermes/hermes-component/hermes-component-jdbc-impl/src/test/java/com/bytechef/hermes/component/jdbc/operation/InsertJdbcActionIntTest.java
@@ -0,0 +1,102 @@
+/*
+ * Copyright 2021 <your company/name>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef.hermes.component.jdbc.operation;
+
+import com.bytechef.hermes.component.ConnectionParameters;
+import com.bytechef.hermes.component.jdbc.DataSourceFactory;
+import com.bytechef.hermes.component.jdbc.JdbcExecutor;
+import com.bytechef.hermes.component.test.MockContext;
+import com.bytechef.hermes.component.test.MockExecutionParameters;
+import java.util.List;
+import java.util.Map;
+import javax.sql.DataSource;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.transaction.PlatformTransactionManager;
+
+/**
+ * @author Ivica Cardic
+ */
+@SpringBootTest
+public class InsertJdbcActionIntTest {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Autowired
+    private InsertJdbcOperation insertJdbcOperation;
+
+    @BeforeEach
+    public void beforeEach() {
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+
+        jdbcTemplate.execute(
+                """
+            DROP TABLE IF EXISTS test;
+            CREATE TABLE test (
+                id   varchar(256) not null primary key,
+                name varchar(256) not null
+            );
+        """);
+    }
+
+    @Test
+    public void testInsert() {
+        MockExecutionParameters parameters = new MockExecutionParameters(Map.of(
+                "table",
+                "test",
+                "columns",
+                List.of("id", "name"),
+                "rows",
+                List.of(Map.of("id", "id1", "name", "name1"), Map.of("id", "id2", "name", "name2"))));
+
+        Map<String, Integer> result = insertJdbcOperation.execute(new MockContext(), parameters);
+
+        Assertions.assertEquals(2, result.get("rows"));
+    }
+
+    @TestConfiguration
+    public static class InsertJdbcTaskHandlerIntTestConfiguration {
+
+        @Autowired
+        private DataSource dataSource;
+
+        @Bean
+        InsertJdbcOperation insertJdbcOperation(PlatformTransactionManager transactionManager) {
+            return new InsertJdbcOperation(new JdbcExecutor(
+                    null,
+                    new DataSourceFactory() {
+
+                        @Override
+                        public DataSource getDataSource(
+                                ConnectionParameters connectionParameters,
+                                String databaseJdbcName,
+                                String jdbcDriverClassNamee) {
+                            return dataSource;
+                        }
+                    },
+                    null,
+                    transactionManager));
+        }
+    }
+}
