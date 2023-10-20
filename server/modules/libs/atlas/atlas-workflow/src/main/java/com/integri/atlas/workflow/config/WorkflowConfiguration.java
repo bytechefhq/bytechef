294a7168ff04396f0dd35b175c00bcd562ad6ca8
Ivica Cardic <ivica.cardic@gmail.com>
2023-10-20 17:18:43 +0000

    0 - Add WorkflowConfiguration

---
+++ server/modules/libs/atlas/atlas-workflow/src/main/java/com/integri/atlas/workflow/config/WorkflowConfiguration.java
@@ -0,0 +1,9 @@
+package com.integri.atlas.workflow.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.integri.integri.workflow")
+public class WorkflowConfiguration {
+}
