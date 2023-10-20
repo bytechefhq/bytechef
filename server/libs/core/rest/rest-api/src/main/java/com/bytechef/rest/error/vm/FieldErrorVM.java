936117e626a03f4ac0e45a77268179a20c15c104
Ivica Čardić <ivica.cardic@gmail.com>
2023-10-20 17:18:43 +0000

    ISSUE-76 - rest-api - Add helper logic used inside rest layer

---
+++ server/libs/core/rest/rest-api/src/main/java/com/bytechef/rest/error/vm/FieldErrorVM.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2021 <your company/name>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef.rest.error.vm;
+
+import java.io.Serializable;
+
+/**
+ * @author Ivica Cardic
+ */
+public class FieldErrorVM implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private final String objectName;
+
+    private final String field;
+
+    private final String message;
+
+    public FieldErrorVM(String dto, String field, String message) {
+        this.objectName = dto;
+        this.field = field;
+        this.message = message;
+    }
+
+    public String getObjectName() {
+        return objectName;
+    }
+
+    public String getField() {
+        return field;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
