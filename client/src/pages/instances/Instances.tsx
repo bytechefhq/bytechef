c65748fd9e39c3bcdfcdbefc00d8e3e1182c74ff
Ivica Čardić <ivica.cardic@gmail.com>
2023-10-20 17:18:43 +0000

    #227 - client - Initial commit for the Instances page

---
+++ client/src/pages/instances/Instances.tsx
@@ -0,0 +1,16 @@
+import LayoutContainer from '../../layouts/LayoutContainer/LayoutContainer';
+import PageHeader from '../../components/PageHeader/PageHeader';
+import React from 'react';
+
+export const Instances = () => {
+    return (
+        <LayoutContainer
+            header={<PageHeader title="All Instances" />}
+            leftSidebarHeader={
+                <PageHeader leftSidebar={true} title="Instances" />
+            }
+        ></LayoutContainer>
+    );
+};
+
+export default Instances;
