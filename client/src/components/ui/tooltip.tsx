9ee9facee58e842e3d522354a1f50dd54f8c2777
Ivica Čardić <ivica.cardic@gmail.com>
2023-10-20 17:18:43 +0000

    440 - client - Update shadcn components

--- client/src/components/ui/tooltip.tsx
+++ client/src/components/ui/tooltip.tsx
@@ -1,34 +1,28 @@
-import {cn} from '@/lib/utils';
-import {Content, Provider, Root, Trigger} from '@radix-ui/react-tooltip';
-import {ComponentPropsWithoutRef, ElementRef, forwardRef} from 'react';
+import * as React from "react"
+import * as TooltipPrimitive from "@radix-ui/react-tooltip"
 
-const TooltipProvider = Provider;
-const Tooltip = Root;
-const TooltipTrigger = Trigger;
+import { cn } from "@/lib/utils"
 
-const closedState =
-    'data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95';
+const TooltipProvider = TooltipPrimitive.Provider
 
-const sideClasses =
-    'data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2';
+const Tooltip = TooltipPrimitive.Root
 
-const TooltipContent = forwardRef<
-    ElementRef<typeof Content>,
-    ComponentPropsWithoutRef<typeof Content>
->(({className, sideOffset = 4, ...props}, ref) => (
-    <Content
-        ref={ref}
-        sideOffset={sideOffset}
-        className={cn(
-            'z-50 max-w-tooltip-lg overflow-hidden rounded-md border bg-popover px-3 py-1.5 text-sm text-popover-foreground shadow-md animate-in fade-in-0 zoom-in-95',
-            className,
-            closedState,
-            sideClasses
-        )}
-        {...props}
-    />
-));
+const TooltipTrigger = TooltipPrimitive.Trigger
 
-TooltipContent.displayName = Content.displayName;
+const TooltipContent = React.forwardRef<
+  React.ElementRef<typeof TooltipPrimitive.Content>,
+  React.ComponentPropsWithoutRef<typeof TooltipPrimitive.Content>
+>(({ className, sideOffset = 4, ...props }, ref) => (
+  <TooltipPrimitive.Content
+    ref={ref}
+    sideOffset={sideOffset}
+    className={cn(
+      "z-50 overflow-hidden rounded-md border bg-popover px-3 py-1.5 text-sm text-popover-foreground shadow-md animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
+      className
+    )}
+    {...props}
+  />
+))
+TooltipContent.displayName = TooltipPrimitive.Content.displayName
 
-export {Tooltip, TooltipTrigger, TooltipContent, TooltipProvider};
+export { Tooltip, TooltipTrigger, TooltipContent, TooltipProvider }
