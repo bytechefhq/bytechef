c184abf7a325f82d0c877fd1e947f1bf36430ab7
Ivica Čardić <ivica.cardic@gmail.com>
2023-10-20 17:18:43 +0000

    #222 - client - Add OAuth2 support, originates from https://github.com/tasoskakour/react-use-oauth2

---
+++ client/src/pages/connections/oauth2/constants.ts
@@ -0,0 +1,2 @@
+export const OAUTH_STATE_KEY = 'react-use-oauth2-state-key';
+export const OAUTH_RESPONSE = 'react-use-oauth2-response';
---
+++ client/src/pages/connections/oauth2/tools.ts
@@ -0,0 +1,8 @@
+export const objectToQuery = (object: Record<string, string>) => {
+    return new URLSearchParams(object).toString();
+};
+
+export const queryToObject = (query: string) => {
+    const parameters = new URLSearchParams(query);
+    return Object.fromEntries(parameters.entries());
+};
