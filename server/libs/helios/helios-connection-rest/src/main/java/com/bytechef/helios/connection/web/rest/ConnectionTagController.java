e9fa8e936bad82fdcf6b4d0065a1d446e28b35ce
Ivica Čardić <ivica.cardic@gmail.com>
2023-10-20 17:18:43 +0000

    272 - Move connection endpoints to helios-connection-rest module

---
+++ server/libs/helios/helios-connection-rest/src/main/java/com/bytechef/helios/connection/web/rest/ConnectionTagController.java
@@ -0,0 +1,58 @@
+
+/*
+ * Copyright 2021 <your company/name>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef.helios.connection.web.rest;
+
+import com.bytechef.hermes.connection.facade.ConnectionFacade;
+import com.bytechef.tag.web.rest.model.TagModel;
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import org.springframework.core.convert.ConversionService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+/**
+ * @author Ivica Cardic
+ */
+@RestController
+
+@RequestMapping("${openapi.openAPIDefinition.base-path:}")
+public class ConnectionTagController implements ConnectionTagsApi {
+
+    private final ConnectionFacade connectionFacade;
+    private final ConversionService conversionService;
+
+    @SuppressFBWarnings("EI")
+    public ConnectionTagController(ConnectionFacade connectionFacade, ConversionService conversionService) {
+        this.connectionFacade = connectionFacade;
+        this.conversionService = conversionService;
+    }
+
+    @Override
+    public Mono<ResponseEntity<Flux<TagModel>>> getConnectionTags(ServerWebExchange exchange) {
+        return Mono.just(
+            Flux.fromIterable(
+                connectionFacade.getConnectionTags()
+                    .stream()
+                    .map(tag -> conversionService.convert(tag, TagModel.class))
+                    .toList()))
+            .map(ResponseEntity::ok);
+    }
+}
---
+++ server/libs/hermes/hermes-oauth2-rest/src/test/java/com/bytechef/hermes/connection/web/rest/OAuth2PropertiesControllerTest.java
@@ -0,0 +1,16 @@
+package com.bytechef.hermes.connection.web.rest;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Ivica Cardic
+ */
+public class OAuth2PropertiesControllerTest {
+
+    @Disabled
+    @Test
+    public void testGetOAuth2Properties() {
+        // TODO
+    }
+}
