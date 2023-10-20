b2f95ab5ce99ff4108d5ca6864a296c7540167e6
Ivica Čardić <ivica.cardic@gmail.com>
2023-10-20 17:18:43 +0000

    272 - client - Update field name

--- client/src/pages/automation/executions/components/dialog-content/WorkflowTaskListAccordion.tsx
+++ client/src/pages/automation/executions/components/dialog-content/WorkflowTaskListAccordion.tsx
@@ -31,10 +31,13 @@ const WorkflowTaskListAccordion = ({
                     <AccordionItem key={id} value={id || ''}>
                         <AccordionTrigger className="flex w-full items-center justify-between border-gray-100 bg-white px-2 py-3 data-[state=closed]:border-b">
                             <div className="flex items-center text-sm">
-                                {taskExecution?.component?.icon && (
+                                {taskExecution?.componentDefinition?.icon && (
                                     <InlineSVG
                                         className="mr-1 h-6 w-6"
-                                        src={taskExecution?.component?.icon}
+                                        src={
+                                            taskExecution?.componentDefinition
+                                                ?.icon
+                                        }
                                     />
                                 )}
 
