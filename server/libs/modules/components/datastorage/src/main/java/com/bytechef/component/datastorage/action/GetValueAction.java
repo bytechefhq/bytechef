cbf5bd50a9ad162146c3a7ca7edd2fc0285e9e63
Ivica Čardić <ivica.cardic@gmail.com>
2023-10-20 17:18:43 +0000

    272 - datastorage - Add initial datastorage component definition

---
+++ server/libs/modules/components/datastorage/src/main/java/com/bytechef/component/datastorage/action/GetValueAction.java
@@ -0,0 +1,75 @@
+
+/*
+ * Copyright 2021 <your company/name>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef.component.datastorage.action;
+
+import com.bytechef.hermes.component.ActionContext;
+import com.bytechef.hermes.component.InputParameters;
+import com.bytechef.hermes.component.definition.ActionDefinition;
+import com.bytechef.hermes.data.storage.service.DataStorageService;
+
+import static com.bytechef.component.datastorage.constant.DataStorageConstants.DEFAULT_VALUE;
+import static com.bytechef.component.datastorage.constant.DataStorageConstants.KEY;
+import static com.bytechef.component.datastorage.constant.DataStorageConstants.SCOPE;
+import static com.bytechef.component.datastorage.constant.DataStorageConstants.SCOPE_OPTIONS;
+import static com.bytechef.hermes.component.definition.ComponentDSL.action;
+import static com.bytechef.hermes.definition.DefinitionDSL.array;
+import static com.bytechef.hermes.definition.DefinitionDSL.bool;
+import static com.bytechef.hermes.definition.DefinitionDSL.display;
+import static com.bytechef.hermes.definition.DefinitionDSL.integer;
+import static com.bytechef.hermes.definition.DefinitionDSL.nullable;
+import static com.bytechef.hermes.definition.DefinitionDSL.number;
+import static com.bytechef.hermes.definition.DefinitionDSL.object;
+import static com.bytechef.hermes.definition.DefinitionDSL.oneOf;
+import static com.bytechef.hermes.definition.DefinitionDSL.string;
+
+/**
+ * @author Ivica Cardic
+ */
+public class GetValueAction {
+
+    public final ActionDefinition actionDefinition = action("getValue")
+        .display(display("Get Value").description(""))
+        .properties(
+            string(KEY)
+                .label("Key")
+                .description("The identifier of a value to get, stored earlier in the selected scope.")
+                .required(true),
+            string(SCOPE)
+                .label("Scope")
+                .description(
+                    "The namespace to get a value from. The value should have been previously accessible, either in the present workflow execution, or the workflow itself for all the executions, or the user account for all the workflows the user has.")
+                .options(SCOPE_OPTIONS)
+                .required(true),
+            oneOf(DEFAULT_VALUE).types(array(), bool(), integer(), number(), object(), string(), nullable())
+                .label("Default value")
+                .description("The default value to return if no value exists under the given key.")
+                .required(true))
+        .execute(this::execute);
+
+    private final DataStorageService dataStorageService;
+
+    public GetValueAction(DataStorageService dataStorageService) {
+        this.dataStorageService = dataStorageService;
+    }
+
+    protected Object execute(ActionContext actionContext, InputParameters inputParameters) {
+        System.out.println(dataStorageService.toString());
+
+        return null;
+    }
+}
