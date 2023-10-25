b204218f1246e55300e6c705b8f83275ec4850a2
Ivica Čardić <ivica.cardic@gmail.com>
2023-10-25 06:51:12 +0200

    440 - client - Update dependencies

--- client/src/mocks/server.ts
+++ client/src/mocks/server.ts
@@ -1,5 +1,6 @@
-import {setupWorker} from 'msw';
+import {setupWorker} from 'msw/browser';
 
 import {handlers} from './handlers';
+
 // This configures a Service Worker with the given request handlers.
 export const worker = setupWorker(...handlers);
