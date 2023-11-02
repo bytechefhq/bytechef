d76f303675dbe748102a7233aa7cc6149418c76a
Ivica Čardić <ivica.cardic@gmail.com>
2023-11-02 11:49:25 +0100

    272 - Move OAuth2Properties to hermes-aouth2-service

--- server/libs/hermes/hermes-oauth2/hermes-oauth2-api/build.gradle.kts
+++ server/libs/hermes/hermes-oauth2/hermes-oauth2-api/build.gradle.kts
@@ -1,5 +1,3 @@
 dependencies {
     api(project(":server:libs:hermes:hermes-component:hermes-component-registry:hermes-component-registry-api"))
-
-    implementation("org.springframework.boot:spring-boot")
 }
