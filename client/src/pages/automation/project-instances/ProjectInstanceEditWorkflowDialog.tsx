a9e60d062c38797b6e8f854efcb9f2a1b86f0e05
Ivica Čardić <ivica.cardic@gmail.com>
2023-11-05 20:35:33 +0100

    440 - client - Fix connections in ProjectInstanceDialog

--- client/src/pages/automation/project-instances/ProjectInstanceEditWorkflowDialog.tsx
+++ client/src/pages/automation/project-instances/ProjectInstanceEditWorkflowDialog.tsx
@@ -1,18 +1,18 @@
 import Button from '@/components/Button/Button';
 import Dialog from '@/components/Dialog/Dialog';
+import {Form} from '@/components/ui/form';
 import {
     ProjectInstanceModel,
     ProjectInstanceWorkflowModel,
     WorkflowModel,
 } from '@/middleware/helios/configuration';
 import {useUpdateProjectInstanceWorkflowMutation} from '@/mutations/projectInstances.mutations';
+import ProjectInstanceDialogWorkflowListItem from '@/pages/automation/project-instances/ProjectInstanceDialogWorkflowListItem';
 import {ProjectInstanceKeys} from '@/queries/projectInstances.queries';
 import {Close} from '@radix-ui/react-dialog';
 import {useQueryClient} from '@tanstack/react-query';
 import {useState} from 'react';
 import {useForm} from 'react-hook-form';
-
-import {ProjectInstanceDialogWorkflowListItem} from './ProjectInstanceDialogWorkflowsStep';
 
 interface ProjectInstanceEditWorkflowDialogProps {
     onClose?: () => void;
@@ -31,13 +31,14 @@ const ProjectInstanceEditWorkflowDialog = ({
 }: ProjectInstanceEditWorkflowDialogProps) => {
     const [isOpen, setIsOpen] = useState(visible);
 
-    const {formState, getValues, handleSubmit, register} =
-        useForm<ProjectInstanceModel>({
-            defaultValues: {
-                projectInstanceWorkflows: [projectInstanceWorkflow],
-            } as ProjectInstanceModel,
-            mode: 'onBlur',
-        });
+    const form = useForm<ProjectInstanceModel>({
+        defaultValues: {
+            projectInstanceWorkflows: [projectInstanceWorkflow],
+        } as ProjectInstanceModel,
+        mode: 'onBlur',
+    });
+
+    const {control, formState, getValues, handleSubmit, register} = form;
 
     const queryClient = useQueryClient();
 
@@ -84,33 +85,39 @@ const ProjectInstanceEditWorkflowDialog = ({
             }}
             title={`Edit ${workflow?.label} Workflow`}
         >
-            <div className="flex flex-col">
-                <div className="mt-4 flex flex-col ">
-                    <ProjectInstanceDialogWorkflowListItem
-                        formState={formState}
-                        label="Enable"
-                        key={workflow.id!}
-                        register={register}
-                        switchHidden={true}
-                        workflow={workflow}
-                        workflowIndex={0}
-                    />
-
-                    <div className="mt-4 flex w-full justify-end space-x-2 self-end">
-                        <Close asChild>
-                            <Button displayType="lightBorder" label="Cancel" />
-                        </Close>
-
-                        <Button
-                            disabled={projectInstanceEnabled}
-                            label="Save"
-                            onClick={handleSubmit(
-                                updateProjectInstanceWorkflow
-                            )}
+            <Form {...form}>
+                <div className="flex flex-col">
+                    <div className="mt-4 flex flex-col ">
+                        <ProjectInstanceDialogWorkflowListItem
+                            control={control}
+                            formState={formState}
+                            label="Enable"
+                            key={workflow.id!}
+                            register={register}
+                            switchHidden={true}
+                            workflow={workflow}
+                            workflowIndex={0}
                         />
+
+                        <div className="mt-4 flex w-full justify-end space-x-2 self-end">
+                            <Close asChild>
+                                <Button
+                                    displayType="lightBorder"
+                                    label="Cancel"
+                                />
+                            </Close>
+
+                            <Button
+                                disabled={projectInstanceEnabled}
+                                label="Save"
+                                onClick={handleSubmit(
+                                    updateProjectInstanceWorkflow
+                                )}
+                            />
+                        </div>
                     </div>
                 </div>
-            </div>
+            </Form>
         </Dialog>
     );
 };
