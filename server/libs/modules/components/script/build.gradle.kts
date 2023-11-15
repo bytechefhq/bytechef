083bcef4723533eabfec240790a9da5d4c1ccc60
Ivica Čardić <ivica.cardic@gmail.com>
2023-11-15 06:08:45 +0100

    0 - Move graalvm dependencies under Script component

--- server/libs/modules/components/script/build.gradle.kts
+++ server/libs/modules/components/script/build.gradle.kts
@@ -1,6 +1,12 @@
 version="1.0"
 
 dependencies {
+    implementation(rootProject.libs.org.graalvm.polyglot.polyglot)
+    implementation(rootProject.libs.org.graalvm.polyglot.java)
+    implementation(rootProject.libs.org.graalvm.polyglot.js)
+    implementation(rootProject.libs.org.graalvm.polyglot.python)
+    implementation(rootProject.libs.org.graalvm.polyglot.ruby)
+
     testImplementation(project(":server:libs:atlas:atlas-execution:atlas-execution-api"))
     testImplementation(project(":server:libs:atlas:atlas-worker:atlas-worker-api"))
     testImplementation(project(":server:libs:atlas:atlas-file-storage:atlas-file-storage-api"))
