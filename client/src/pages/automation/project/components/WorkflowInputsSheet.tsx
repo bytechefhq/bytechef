66017558b62fa54e0f7bc448b8a5fe076cd93f52
Ivica Čardić <ivica.cardic@gmail.com>
2023-11-15 15:58:54 +0100

    446 - client - SF

--- client/src/pages/automation/project/components/WorkflowInputsSheet.tsx
+++ client/src/pages/automation/project/components/WorkflowInputsSheet.tsx
@@ -21,40 +21,36 @@ const WorkflowInputsSheet = ({
     onClose,
     projectId,
     workflow,
-}: WorkflowInputsSheetProps) => {
-    return (
-        <Sheet modal={false} onOpenChange={onClose} open={true}>
-            <SheetContent className="flex flex-col p-4 sm:max-w-[500px]">
-                <SheetHeader>
-                    <div className="flex items-center justify-between">
-                        <SheetTitle className="flex">
-                            Workflow Inputs
-                        </SheetTitle>
+}: WorkflowInputsSheetProps) => (
+    <Sheet modal={false} onOpenChange={onClose} open>
+        <SheetContent className="flex flex-col p-4 sm:max-w-[500px]">
+            <SheetHeader>
+                <div className="flex items-center justify-between">
+                    <SheetTitle className="flex">Workflow Inputs</SheetTitle>
 
-                        <div className="flex items-center gap-1">
-                            <WorkflowInputsSheetDialog
-                                projectId={projectId}
-                                triggerNode={<Button size="sm">Create</Button>}
-                                workflow={workflow}
-                            />
+                    <div className="flex items-center gap-1">
+                        <WorkflowInputsSheetDialog
+                            projectId={projectId}
+                            triggerNode={<Button size="sm">Create</Button>}
+                            workflow={workflow}
+                        />
 
-                            <SheetPrimitive.Close asChild>
-                                <Button size="icon" variant="ghost">
-                                    <Cross2Icon className="h-4 w-4 opacity-70" />
-                                </Button>
-                            </SheetPrimitive.Close>
-                        </div>
+                        <SheetPrimitive.Close asChild>
+                            <Button size="icon" variant="ghost">
+                                <Cross2Icon className="h-4 w-4 opacity-70" />
+                            </Button>
+                        </SheetPrimitive.Close>
                     </div>
-                </SheetHeader>
+                </div>
+            </SheetHeader>
 
-                <WorkflowInputsSheetTable
-                    inputs={workflow.inputs || []}
-                    projectId={projectId}
-                    workflow={workflow}
-                />
-            </SheetContent>
-        </Sheet>
-    );
-};
+            <WorkflowInputsSheetTable
+                inputs={workflow.inputs || []}
+                projectId={projectId}
+                workflow={workflow}
+            />
+        </SheetContent>
+    </Sheet>
+);
 
 export default WorkflowInputsSheet;
