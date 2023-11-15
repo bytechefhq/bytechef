09dc222a2a0e13112bbc9ecbc813bb2c4f5edd08
Ivica Čardić <ivica.cardic@gmail.com>
2023-11-15 06:24:39 +0100

    0 - Fix issue with new spotbugs version

--- ee/build.gradle.kts
+++ ee/build.gradle.kts
@@ -2,6 +2,8 @@
 subprojects {
     apply(plugin = "io.spring.dependency-management")
 
+    ext["commons-lang3.version"] = "3.13.0"
+
     configure<io.spring.gradle.dependencymanagement.dsl.DependencyManagementExtension> {
         imports {
             mavenBom("org.springframework.cloud:spring-cloud-dependencies:" + rootProject.libs.versions.org.springframework.cloud.dependencies.get())
