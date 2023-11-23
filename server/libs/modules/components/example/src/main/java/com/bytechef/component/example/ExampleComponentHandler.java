09fb6701b4c84e26de75b08b9b17a501addfc65a
Mario Cvjetojevic <mario.025025@gmail.com>
2023-11-23 13:01:58 +0100

    659 - example connector

---
+++ server/libs/modules/components/example/src/main/java/com/bytechef/component/example/ExampleComponentHandler.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2023-present ByteChef Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef.component.example;
+
+import static com.bytechef.component.example.constant.ExampleConstants.EXAMPLE;
+import static com.bytechef.hermes.component.definition.ComponentDSL.component;
+
+import com.bytechef.component.example.action.ExampleDummyAction;
+import com.bytechef.hermes.component.ComponentHandler;
+import com.bytechef.hermes.component.definition.ComponentDefinition;
+import com.google.auto.service.AutoService;
+
+/**
+ * @author Mario Cvjetojevic
+ */
+@AutoService(ComponentHandler.class)
+public class ExampleComponentHandler implements ComponentHandler {
+
+    private static final ComponentDefinition COMPONENT_DEFINITION = component(EXAMPLE)
+        .title("Example")
+        .description("Component description.")
+        .icon("path:assets/example.svg")
+        .actions(ExampleDummyAction.ACTION_DEFINITION);
+
+    @Override
+    public ComponentDefinition getDefinition() {
+        return COMPONENT_DEFINITION;
+    }
+}
