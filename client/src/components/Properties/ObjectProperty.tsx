91674d0d866252ad94df2cf256252c7b0bb1337a
Krešimir Čoko <kresimircoko@gmail.com>
2023-12-06 09:26:34 +0100

    373 - SF

--- client/src/components/Properties/ObjectProperty.tsx
+++ client/src/components/Properties/ObjectProperty.tsx
@@ -36,8 +36,6 @@ const ObjectProperty = ({
         property;
 
     if (objectType === 'FILE_ENTRY' && dataPills?.length) {
-
-
         return (
             <Property
                 actionName={actionName}
