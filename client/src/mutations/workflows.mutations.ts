c2701287451cb63654a5164522a3d7cbde7e8de5
Ivica Čardić <ivica.cardic@gmail.com>
2024-01-09 19:12:25 +0100

    421 - client - Remove console.log

--- client/src/mutations/workflows.mutations.ts
+++ client/src/mutations/workflows.mutations.ts
@@ -43,7 +43,6 @@ type UpdateWorkflowMutationProps = {
 export const useUpdateWorkflowMutation = (mutationProps?: UpdateWorkflowMutationProps) =>
     useMutation({
         mutationFn: (request: UpdateWorkflowRequest) => {
-            console.log(request);
             return new CoreWorkflowApi().updateWorkflow(request);
         },
         onError: mutationProps?.onError,
