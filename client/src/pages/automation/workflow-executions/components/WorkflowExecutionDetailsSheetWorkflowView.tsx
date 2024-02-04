b46e995766bd25925001faf2973859a30a9b4f08
Ivica Čardić <ivica.cardic@gmail.com>
2024-02-04 20:43:23 +0100

    676 - client - Fix Close button not aligneed in WorkflowsExecutionsDetailsView

--- client/src/pages/automation/workflow-executions/components/WorkflowExecutionDetailsSheetWorkflowView.tsx
+++ client/src/pages/automation/workflow-executions/components/WorkflowExecutionDetailsSheetWorkflowView.tsx
@@ -15,29 +15,33 @@ const WorkflowExecutionDetailsSheetWorkflowView = ({
     const navigate = useNavigate();
 
     return (
-        <div className="flex w-full justify-between bg-gray-100 p-4 align-middle">
-            <h3>
-                {workflow?.label
-                    ? `${project?.name ? project.name + ' / ' : ''}${
-                          projectInstance?.name ? projectInstance.name + ' / ' : ''
-                      }${workflow?.label}`
-                    : 'No data to show'}
-            </h3>
+        <div className="flex size-full flex-col bg-gray-100">
+            <div className="flex w-full items-center justify-between p-4">
+                <h3>
+                    {workflow?.label
+                        ? `${project?.name ? project.name + ' / ' : ''}${
+                              projectInstance?.name ? projectInstance.name + ' / ' : ''
+                          }${workflow?.label}`
+                        : 'No data to show'}
+                </h3>
 
-            <div className="flex align-middle">
-                <Button
-                    className="mr-1"
-                    onClick={() => navigate(`/automation/projects/${project?.id}/workflows/${workflow?.id}`)}
-                    size="sm"
-                    variant="outline"
-                >
-                    <PencilIcon className="mr-1 size-4 cursor-pointer" /> Edit
-                </Button>
+                <div className="flex items-center">
+                    <Button
+                        className="mr-1"
+                        onClick={() => navigate(`/automation/projects/${project?.id}/workflows/${workflow?.id}`)}
+                        size="sm"
+                        variant="outline"
+                    >
+                        <PencilIcon className="mr-1 size-4 cursor-pointer" /> Edit
+                    </Button>
 
-                <SheetPrimitive.Close asChild>
-                    <Cross2Icon className="size-4 cursor-pointer opacity-70" />
-                </SheetPrimitive.Close>
+                    <SheetPrimitive.Close asChild>
+                        <Cross2Icon className="size-4 cursor-pointer opacity-70" />
+                    </SheetPrimitive.Close>
+                </div>
             </div>
+
+            <div className="flex-1"></div>
         </div>
     );
 };
