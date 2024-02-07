725403db50df5062a2e8d7c9f78686a4e6be559d
Krešimir Čoko <kresimircoko@gmail.com>
2024-02-07 10:22:46 +0100

    735 - Add ArrayProperty saving

--- client/src/components/Properties/ArrayProperty.tsx
+++ client/src/components/Properties/ArrayProperty.tsx
@@ -1,16 +1,33 @@
 import PropertyDropdownMenu from '@/components/Properties/components/PropertyDropdownMenu';
 import {Button} from '@/components/ui/button';
-import {DataPillType} from '@/types/types';
+import {UpdateWorkflowRequest, WorkflowModel} from '@/middleware/automation/configuration';
+import {PropertyModel} from '@/middleware/platform/configuration';
+import {ComponentDataType, DataPillType} from '@/types/types';
 import {PlusIcon} from '@radix-ui/react-icons';
-import {useState} from 'react';
+import {UseMutationResult} from '@tanstack/react-query';
+import {useEffect, useState} from 'react';
 import {PropertyType} from 'types/projectTypes';
 
 import Property from './Property';
 
-const ArrayProperty = ({dataPills, property}: {dataPills?: Array<DataPillType>; property: PropertyType}) => {
+type ArrayPropertyProps = {
+    currentComponentData?: ComponentDataType;
+    dataPills?: Array<DataPillType>;
+    property: PropertyType;
+    updateWorkflowMutation?: UseMutationResult<WorkflowModel, Error, UpdateWorkflowRequest, unknown>;
+    workflow?: WorkflowModel;
+};
+
+const ArrayProperty = ({
+    currentComponentData,
+    dataPills,
+    property,
+    updateWorkflowMutation,
+    workflow,
+}: ArrayPropertyProps) => {
     const [arrayItems, setArrayItems] = useState(property.items);
 
-    const {items, multipleValues} = property;
+    const {items, multipleValues, name} = property;
 
     const newPropertyTypeOptions = items?.reduce((uniqueItems: Array<{label: string; value: string}>, item) => {
         if (!uniqueItems.find((uniqueItem) => uniqueItem.value === item.type)) {
@@ -25,17 +42,80 @@ const ArrayProperty = ({dataPills, property}: {dataPills?: Array<DataPillType>; 
         return uniqueItems;
     }, []);
 
-    const handleAddItemClick = () => arrayItems && setArrayItems([...arrayItems, arrayItems[0]]);
+    const handleAddItemClick = () => {
+        if (!arrayItems) {
+            return;
+        }
+
+        const newItem = {
+            name: `${name}_${arrayItems.length}`,
+            type: arrayItems[0].type,
+        };
+
+        setArrayItems([...arrayItems, newItem]);
+    };
+
+    useEffect(() => {
+        if (!currentComponentData?.parameters || !Object.keys(currentComponentData?.parameters).length) {
+            return;
+        }
+
+        const parameterArrayItems = Object.keys(currentComponentData!.parameters!).reduce(
+            (parameters: Array<PropertyModel & {defaultValue: string}>, key: string) => {
+                if (arrayItems?.length && key.startsWith(`${name}_`)) {
+                    const strippedValue = currentComponentData.parameters?.[key]
+                        .replace(/<p>/g, '')
+                        .replace(/<\/p>/g, '');
+
+                    parameters.push({
+                        ...arrayItems[0],
+                        defaultValue: strippedValue,
+                        name: key,
+                        type: arrayItems[0].type,
+                    });
+                }
+
+                return parameters;
+            },
+            []
+        );
+
+        parameterArrayItems.sort((first, second) => {
+            const firstName = first.name?.toLowerCase();
+            const secondName = second.name?.toLowerCase();
+
+            if (!firstName || !secondName) {
+                return 0;
+            }
+
+            if (firstName < secondName) {
+                return -1;
+            }
+
+            if (firstName > secondName) {
+                return 1;
+            }
+
+            return 0;
+        });
+
+        setArrayItems(parameterArrayItems);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [arrayItems?.length, currentComponentData?.parameters, name]);
 
     return (
         <ul className="w-full">
-            {items?.map((item, index) => (
+            {arrayItems?.map((item) => (
                 <Property
+                    arrayName={name}
+                    currentComponentData={currentComponentData}
                     customClassName="border-l ml-2 pl-2 pb-2 last-of-type:pb-0"
                     dataPills={dataPills}
-                    key={`${item.name}_${index}`}
+                    key={item.name || `${name}_0`}
                     mention={!!dataPills?.length}
                     property={item}
+                    updateWorkflowMutation={updateWorkflowMutation}
+                    workflow={workflow}
                 />
             ))}
 
