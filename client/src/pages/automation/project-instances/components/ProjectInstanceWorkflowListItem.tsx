e0156dfb1497903473414801ba1715f62363669a
Ivica Čardić <ivica.cardic@gmail.com>
2024-02-10 08:02:26 +0100

    272 - client - Add “run” button for manual workflows

--- client/src/pages/automation/project-instances/components/ProjectInstanceWorkflowListItem.tsx
+++ client/src/pages/automation/project-instances/components/ProjectInstanceWorkflowListItem.tsx
@@ -1,18 +1,21 @@
 import {Button} from '@/components/ui/button';
 import {DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger} from '@/components/ui/dropdown-menu';
-import {Switch} from '@/components/ui/switch';
 import {Tooltip, TooltipContent, TooltipTrigger} from '@/components/ui/tooltip';
-import {ProjectInstanceWorkflowModel, WorkflowModel} from '@/middleware/automation/configuration';
+import {useToast} from '@/components/ui/use-toast';
+import {ProjectInstanceApi, ProjectInstanceWorkflowModel, WorkflowModel} from '@/middleware/automation/configuration';
 import {ComponentDefinitionBasicModel} from '@/middleware/platform/configuration';
 import {useEnableProjectInstanceWorkflowMutation} from '@/mutations/automation/projectInstanceWorkflows.mutations';
 import ProjectInstanceEditWorkflowDialog from '@/pages/automation/project-instances/components/ProjectInstanceEditWorkflowDialog';
 import {ProjectInstanceKeys} from '@/queries/automation/projectInstances.queries';
 import {DotsVerticalIcon} from '@radix-ui/react-icons';
 import {useQueryClient} from '@tanstack/react-query';
-import {CalendarIcon} from 'lucide-react';
+import {CalendarIcon, PlayIcon} from 'lucide-react';
 import {useState} from 'react';
 import InlineSVG from 'react-inlinesvg';
 import {Link} from 'react-router-dom';
+import {twMerge} from 'tailwind-merge';
+
+const projectInstanceApi = new ProjectInstanceApi();
 
 const ProjectInstanceWorkflowListItem = ({
     filteredDefinitionNames,
@@ -39,6 +42,10 @@ const ProjectInstanceWorkflowListItem = ({
 }) => {
     const [showEditWorkflowDialog, setShowEditWorkflowDialog] = useState(false);
 
+    const {toast} = useToast();
+
+    const workflowHasManualTrigger = !workflow.triggers || workflow.triggers?.length === 0;
+
     const queryClient = useQueryClient();
 
     const enableProjectInstanceWorkflowMutation = useEnableProjectInstanceWorkflowMutation({
@@ -49,13 +56,51 @@ const ProjectInstanceWorkflowListItem = ({
         },
     });
 
+    const handleProjectInstanceEnable = () => {
+        enableProjectInstanceWorkflowMutation.mutate(
+            {
+                enable: !projectInstanceWorkflow.enabled,
+                id: projectInstanceId,
+                workflowId: workflow.id!,
+            },
+            {
+                onSuccess: () => {
+                    projectInstanceWorkflow = {
+                        ...projectInstanceWorkflow,
+                        enabled: !projectInstanceWorkflow?.enabled,
+                    };
+                },
+            }
+        );
+    };
+
+    const handleWorkflowRun = () => {
+        projectInstanceApi
+            .createProjectInstanceWorkflowJob({
+                id: projectInstanceId,
+                workflowId: workflow.id!,
+            })
+            .then(() => {
+                toast({
+                    description: 'Workflow request sent.',
+                });
+            });
+    };
+
     return (
         <>
             <Link
                 className="flex flex-1 items-center"
                 to={`/automation/projects/${projectId}/workflows/${workflow.id}`}
             >
-                <div className="w-6/12 text-sm font-semibold">{workflow.label}</div>
+                <div
+                    className={twMerge(
+                        'w-80 text-sm font-semibold',
+                        !projectInstanceWorkflow.enabled && 'text-muted-foreground'
+                    )}
+                >
+                    {workflow.label}
+                </div>
 
                 <div className="ml-6 flex">
                     {filteredDefinitionNames?.map((name) => {
@@ -103,24 +148,24 @@ const ProjectInstanceWorkflowListItem = ({
                 )}
 
                 {projectInstanceWorkflow && (
-                    <Switch
-                        checked={projectInstanceWorkflow.enabled}
-                        disabled={projectInstanceEnabled}
-                        onCheckedChange={(value) => {
-                            enableProjectInstanceWorkflowMutation.mutate(
-                                {
-                                    enable: value,
-                                    id: projectInstanceId,
-                                    workflowId: workflow.id!,
-                                },
-                                {
-                                    onSuccess: () => {
-                                        projectInstanceWorkflow.enabled = !projectInstanceWorkflow?.enabled;
-                                    },
-                                }
-                            );
-                        }}
-                    />
+                    <div className="w-8">
+                        {workflowHasManualTrigger && (
+                            <Button
+                                disabled={!projectInstanceEnabled || !projectInstanceWorkflow.enabled}
+                                onClick={() => handleWorkflowRun()}
+                                size="icon"
+                                variant="ghost"
+                            >
+                                <Tooltip>
+                                    <TooltipTrigger asChild>
+                                        <PlayIcon className="h-5 text-success" />
+                                    </TooltipTrigger>
+
+                                    <TooltipContent>Run workflow manually</TooltipContent>
+                                </Tooltip>
+                            </Button>
+                        )}
+                    </div>
                 )}
 
                 <DropdownMenu>
@@ -131,7 +176,19 @@ const ProjectInstanceWorkflowListItem = ({
                     </DropdownMenuTrigger>
 
                     <DropdownMenuContent align="end">
-                        <DropdownMenuItem onClick={() => setShowEditWorkflowDialog(true)}>Edit</DropdownMenuItem>
+                        <DropdownMenuItem
+                            disabled={projectInstanceEnabled}
+                            onClick={() => setShowEditWorkflowDialog(true)}
+                        >
+                            Edit
+                        </DropdownMenuItem>
+
+                        <DropdownMenuItem
+                            disabled={projectInstanceEnabled}
+                            onClick={() => handleProjectInstanceEnable()}
+                        >
+                            {projectInstanceWorkflow.enabled ? 'Disable' : 'Enable'}
+                        </DropdownMenuItem>
                     </DropdownMenuContent>
                 </DropdownMenu>
             </div>
