265f21343776cbce110dc9d95e3cad7e825685fc
Ivica Čardić <ivica.cardic@gmail.com>
2024-02-12 08:37:51 +0100

    797 - data-stream - Initial Commit

---
+++ server/libs/modules/components/data-stream/src/test/java/com/bytechef/component/data/stream/config/DataStreamConfigurationIntTest.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2023-present ByteChef Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef.component.data.stream.config;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.batch.test.JobRepositoryTestUtils;
+import org.springframework.batch.test.context.SpringBatchTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
+
+@Disabled
+@ActiveProfiles("test")
+@TestExecutionListeners({
+    DependencyInjectionTestExecutionListener.class,
+    DirtiesContextTestExecutionListener.class
+})
+// This is to avoid clashing of several JobRepository instances using the same data source for several test classes
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+@SpringBatchTest
+@SpringBootTest // This is required to be able to used spring boot features such as profiles
+class DataStreamConfigurationIntTest {
+
+    @Autowired
+    private JobLauncherTestUtils jobLauncherTestUtils;
+
+    @Autowired
+    private JobRepositoryTestUtils jobRepositoryTestUtils;
+
+    @Autowired
+    private Job importUserJob;
+
+    @BeforeEach
+    void setUp() {
+        jobLauncherTestUtils.setJob(importUserJob);
+    }
+
+    @AfterEach
+    void tearDown() {
+        jobRepositoryTestUtils.removeJobExecutions();
+    }
+
+    @Test
+    void importUserJob_WhenJobEnds_ThenStatusCompleted() throws Exception {
+        JobExecution jobExecution = jobLauncherTestUtils.launchJob();
+        Assertions.assertEquals(ExitStatus.COMPLETED, jobExecution.getExitStatus());
+    }
+}
