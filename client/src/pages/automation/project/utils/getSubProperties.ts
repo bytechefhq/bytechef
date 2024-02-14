53c0a9f679b7b395fdc914fa3ececd3898b3abdd
Krešimir Čoko <kresimircoko@gmail.com>
2024-02-14 05:32:59 +0100

    735 - improve the datapill formatting in mention input dropdown

--- client/src/pages/automation/project/utils/getSubProperties.ts
+++ client/src/pages/automation/project/utils/getSubProperties.ts
@@ -1,27 +1,24 @@
-import {ComponentDefinitionBasicModel} from '@/middleware/platform/workflow/execution';
 import {PropertyType} from '@/types/types';
 
 export default function getSubProperties(
-    componentDefinition: ComponentDefinitionBasicModel,
+    componentIcon: string,
     path: string,
     properties: Array<PropertyType>,
     propertyName?: string
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
 ): any {
     return properties.map((subProperty) => {
-        const subPropertyLabel = subProperty.label || subProperty.name;
-
         if (subProperty.properties?.length) {
-            return getSubProperties(componentDefinition, path, subProperty.properties, propertyName);
+            return getSubProperties(componentIcon, path, subProperty.properties, propertyName);
         } else if (subProperty.items?.length) {
-            return getSubProperties(componentDefinition, path, subProperty.items, propertyName);
+            return getSubProperties(componentIcon, path, subProperty.items, propertyName);
         }
 
         return {
-            componentDefinition: JSON.stringify(componentDefinition),
+            componentIcon,
             id: subProperty.name,
             path,
-            value: propertyName ? `${path}/${propertyName}/${subPropertyLabel}` : `${path}/${subPropertyLabel}`,
+            value: propertyName ? `${path}/${propertyName}/${subProperty.name}` : `${path}/${subProperty.name}`,
         };
     });
 }
