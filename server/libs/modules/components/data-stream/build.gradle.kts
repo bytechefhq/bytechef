938b6541173939d3d6d9515c6ac4f34f1ff5aeb9
Ivica Čardić <ivica.cardic@gmail.com>
2024-02-17 20:53:13 +0100

    797 - Reorganize

--- server/libs/modules/components/data-stream/build.gradle.kts
+++ server/libs/modules/components/data-stream/build.gradle.kts
@@ -1,6 +1,7 @@
 dependencies {
     implementation("org.slf4j:slf4j-api")
     implementation("org.springframework.batch:spring-batch-core")
+    implementation(project(":server:libs:platform:platform-component:platform-component-api"))
 
     testImplementation("org.springframework.batch:spring-batch-test")
 }
