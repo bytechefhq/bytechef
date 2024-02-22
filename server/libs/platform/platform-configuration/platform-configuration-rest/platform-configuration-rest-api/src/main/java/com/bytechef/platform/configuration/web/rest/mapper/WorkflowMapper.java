3cd66c61ad576f941518576bbfba5af74bd152a5
Ivica Čardić <ivica.cardic@gmail.com>
2024-02-22 15:03:49 +0100

    565 - Populate WorkflowBasicModel

--- server/libs/platform/platform-configuration/platform-configuration-rest/platform-configuration-rest-api/src/main/java/com/bytechef/platform/configuration/web/rest/mapper/WorkflowMapper.java
+++ server/libs/platform/platform-configuration/platform-configuration-rest/platform-configuration-rest-api/src/main/java/com/bytechef/platform/configuration/web/rest/mapper/WorkflowMapper.java
@@ -17,11 +17,20 @@
 package com.bytechef.platform.configuration.web.rest.mapper;
 
 import com.bytechef.atlas.configuration.domain.Workflow;
+import com.bytechef.atlas.configuration.domain.WorkflowTask;
+import com.bytechef.platform.configuration.domain.WorkflowTrigger;
 import com.bytechef.platform.configuration.dto.WorkflowDTO;
 import com.bytechef.platform.configuration.web.rest.mapper.config.PlatformConfigurationMapperSpringConfig;
 import com.bytechef.platform.configuration.web.rest.model.WorkflowBasicModel;
 import com.bytechef.platform.configuration.web.rest.model.WorkflowModel;
+import com.bytechef.platform.definition.WorkflowNodeType;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import org.mapstruct.AfterMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 import org.springframework.core.convert.converter.Converter;
 
 /**
@@ -37,9 +46,64 @@ public abstract class WorkflowMapper {
     }
 
     @Mapper(config = PlatformConfigurationMapperSpringConfig.class)
-    public interface WorkflowModelToWorkflowBasicModel extends Converter<Workflow, WorkflowBasicModel> {
+    public static abstract class WorkflowModelToWorkflowBasicModel implements Converter<Workflow, WorkflowBasicModel> {
 
         @Override
-        WorkflowBasicModel convert(Workflow source);
+        @Mapping(target = "manualTrigger", ignore = true)
+        @Mapping(target = "workflowTaskComponentNames", ignore = true)
+        @Mapping(target = "workflowTriggerComponentNames", ignore = true)
+        public abstract WorkflowBasicModel convert(Workflow source);
+
+        @AfterMapping
+        public void afterMapping(Workflow workflow, @MappingTarget WorkflowBasicModel workflowBasicModel) {
+            workflowBasicModel.setManualTrigger(WorkflowTrigger.of(workflow)
+                .isEmpty());
+            workflowBasicModel.setWorkflowTaskComponentNames(getWorkflowTaskComponentNames(workflow.getTasks()));
+
+            List<String> workflowTriggerComponentNames = WorkflowTrigger
+                .of(workflow)
+                .stream()
+                .map(workflowTrigger -> WorkflowNodeType.ofType(workflowTrigger.getType()))
+                .map(WorkflowNodeType::componentName)
+                .toList();
+
+            workflowBasicModel.setWorkflowTriggerComponentNames(
+                workflowTriggerComponentNames.isEmpty() ? List.of("manual") : workflowTriggerComponentNames);
+        }
+
+        private List<String> getWorkflowTaskComponentNames(List<WorkflowTask> workflowTasks) {
+            List<String> workflowTaskComponentNames = new ArrayList<>();
+
+            for (WorkflowTask workflowTask : workflowTasks) {
+                workflowTaskComponentNames.add(
+                    WorkflowNodeType
+                        .ofType(workflowTask.getType())
+                        .componentName());
+
+                Map<String, ?> parameters = workflowTask.getParameters();
+
+                for (Map.Entry<String, ?> entry : parameters.entrySet()) {
+                    if (entry.getValue() instanceof WorkflowTask curWorkflowTask) {
+                        workflowTaskComponentNames.addAll(getWorkflowTaskComponentNames(List.of(curWorkflowTask)));
+                    } else if (entry.getValue() instanceof List<?> curList) {
+                        if (!curList.isEmpty() && curList.getFirst() instanceof WorkflowTask) {
+                            for (Object item : curList) {
+                                workflowTaskComponentNames
+                                    .addAll(getWorkflowTaskComponentNames(List.of((WorkflowTask) item)));
+                            }
+                        }
+                    } else if (entry.getValue() instanceof Map<?, ?> curMap) {
+                        for (Map.Entry<?, ?> curMapEntry : curMap.entrySet()) {
+                            if (curMapEntry.getValue() instanceof WorkflowTask curWorkflowTask) {
+                                workflowTaskComponentNames
+                                    .addAll(getWorkflowTaskComponentNames(List.of(curWorkflowTask)));
+                            }
+                        }
+                    }
+                }
+            }
+
+            return workflowTaskComponentNames;
+        }
     }
 }
