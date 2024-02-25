4981c7a40c6d7ed96457d2f7ee19755dea35fb0b
Ivica Čardić <ivica.cardic@gmail.com>
2024-02-25 19:31:27 +0100

    377 - client - Show workflow executions inside WorkflowCodeEditor, update layout

--- client/src/pages/automation/workflow-executions/components/WorkflowExecutionDetailsTaskAccordionItem.tsx
+++ client/src/pages/automation/workflow-executions/components/WorkflowExecutionDetailsTaskAccordionItem.tsx
@@ -1,6 +1,6 @@
+import WorkflowExecutionDetailsAccordionBadge from '@/pages/automation/workflow-executions/components/WorkflowExecutionDetailsAccordionBadge';
 import WorkflowExecutionDetailsAccordionContent from '@/pages/automation/workflow-executions/components/WorkflowExecutionDetailsAccordionContent';
 import {AccordionItem, AccordionTrigger} from '@radix-ui/react-accordion';
-import {CheckCircledIcon, CrossCircledIcon} from '@radix-ui/react-icons';
 import {TaskExecutionModel} from 'middleware/automation/workflow/execution';
 import InlineSVG from 'react-inlinesvg';
 
@@ -11,23 +11,25 @@ const WorkflowExecutionDetailsTaskAccordionItem = ({taskExecution}: {taskExecuti
 
     return (
         <AccordionItem key={id} value={id || ''}>
-            <AccordionTrigger className="flex w-full items-center justify-between border-gray-100 bg-white px-2 py-3 data-[state=closed]:border-b">
-                <div className="flex items-center space-x-1 text-sm">
-                    {taskExecution?.component?.icon && (
-                        <InlineSVG className="mr-1 size-6" src={taskExecution?.component?.icon} />
-                    )}
+            <AccordionTrigger className="flex w-full items-center justify-between border-gray-100 bg-white p-3 data-[state=closed]:border-b">
+                <div className="flex items-center gap-x-2 text-sm">
+                    <WorkflowExecutionDetailsAccordionBadge success={taskExecution.status === 'COMPLETED'} />
 
-                    <span>{component?.title}</span>
+                    <div className="flex items-center gap-x-1">
+                        {taskExecution?.component?.icon && (
+                            <InlineSVG className="size-4" src={taskExecution?.component?.icon} />
+                        )}
 
-                    <span className="text-xs text-muted-foreground">({workflowTask?.name || workflowTask?.type})</span>
+                        <span>{component?.title}</span>
+
+                        <span className="text-xs text-muted-foreground">
+                            ({workflowTask?.name || workflowTask?.type})
+                        </span>
+                    </div>
                 </div>
 
                 <div className="flex items-center">
                     <span className="ml-auto mr-2 text-xs">{duration}ms</span>
-
-                    {taskExecution.status === 'COMPLETED' && <CheckCircledIcon className="size-5 text-green-500" />}
-
-                    {taskExecution.status === 'FAILED' && <CrossCircledIcon className="size-5 text-red-500" />}
                 </div>
             </AccordionTrigger>
 
