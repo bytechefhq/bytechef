9c7f0e0331034ff40f198764ad12f038c634813b
Krešimir Čoko <kresimircoko@gmail.com>
2024-03-07 19:39:52 +0100

    801 - OBJECT and ARRAY subProperty and delete handling alongside other minor improvements

--- client/src/components/Properties/ArrayProperty.tsx
+++ client/src/components/Properties/ArrayProperty.tsx
@@ -1,53 +1,61 @@
-import PropertyDropdownMenu from '@/components/Properties/components/PropertyDropdownMenu';
 import {Button} from '@/components/ui/button';
 import {UpdateWorkflowRequest} from '@/middleware/automation/configuration';
 import {ControlTypeModel, PropertyModel, WorkflowModel} from '@/middleware/platform/configuration';
+import {PROPERTY_CONTROL_TYPES} from '@/shared/constants';
 import {ComponentDataType, DataPillType, PropertyType} from '@/types/types';
-import {PlusIcon} from '@radix-ui/react-icons';
+import {Cross2Icon, PlusIcon} from '@radix-ui/react-icons';
+import {PopoverClose} from '@radix-ui/react-popover';
 import {UseMutationResult} from '@tanstack/react-query';
+import {XIcon} from 'lucide-react';
 import {useEffect, useState} from 'react';
 
+import {Popover, PopoverContent, PopoverTrigger} from '../ui/popover';
 import Property from './Property';
+import PropertySelect from './components/PropertySelect';
 
 interface ArrayPropertyProps {
     currentComponentData?: ComponentDataType;
     dataPills?: Array<DataPillType>;
+    handleDeleteProperty?: (subPropertyName: string, propertyName: string) => void;
     property: PropertyType;
     updateWorkflowMutation?: UseMutationResult<WorkflowModel, Error, UpdateWorkflowRequest, unknown>;
 }
 
-type ArrayPropertyType = PropertyModel & {controlType?: ControlTypeModel; defaultValue?: string};
+type ArrayPropertyType = PropertyModel & {controlType?: ControlTypeModel; custom?: boolean; defaultValue?: string};
 
-const ArrayProperty = ({currentComponentData, dataPills, property, updateWorkflowMutation}: ArrayPropertyProps) => {
+const ArrayProperty = ({
+    currentComponentData,
+    dataPills,
+    handleDeleteProperty,
+    property,
+    updateWorkflowMutation,
+}: ArrayPropertyProps) => {
     const [arrayItems, setArrayItems] = useState<Array<ArrayPropertyType>>([]);
+    const [newItemType, setNewItemType] = useState<keyof typeof PROPERTY_CONTROL_TYPES>('STRING');
 
-    const {items, multipleValues, name} = property;
-
-    const newPropertyTypeOptions = items?.reduce((uniqueItems: Array<{label: string; value: string}>, item) => {
-        if (!uniqueItems.find((uniqueItem) => uniqueItem.value === item.type)) {
-            if (item.type) {
-                uniqueItems.push({
-                    label: item.type,
-                    value: item.type,
-                });
-            }
-        }
-
-        return uniqueItems;
-    }, []);
+    const {items, name} = property;
 
     const handleAddItemClick = () => {
-        if (!property.items?.length) {
-            return;
-        }
-
-        const newItem: PropertyType = {
-            controlType: (property.items[0] as ArrayPropertyType).controlType,
+        const newItem: ArrayPropertyType = {
+            controlType: PROPERTY_CONTROL_TYPES[newItemType] as ControlTypeModel,
+            custom: true,
             name: `${name}_${arrayItems.length}`,
-            type: property.items[0].type,
+            type: newItemType,
         };
 
         setArrayItems([...arrayItems, newItem]);
+    };
+
+    const handleDeletePropertyClick = (subPropertyName: string, propertyName: string) => {
+        if (!subPropertyName || !propertyName) {
+            return;
+        }
+
+        if (handleDeleteProperty) {
+            handleDeleteProperty(subPropertyName, propertyName);
+        }
+
+        setArrayItems((subProperties) => subProperties.filter((subProperty) => subProperty.name !== subPropertyName));
     };
 
     // set property.items?.[0].name if it's not set
@@ -85,10 +93,10 @@ const ArrayProperty = ({currentComponentData, dataPills, property, updateWorkflo
                         .replace(/<\/p>/g, '');
 
                     parameters.push({
-                        ...property.items?.[0],
+                        ...items?.[0],
                         defaultValue: strippedValue,
                         name: key,
-                        type: property.items?.[0].type,
+                        type: items?.[0].type,
                     });
                 }
 
@@ -120,51 +128,77 @@ const ArrayProperty = ({currentComponentData, dataPills, property, updateWorkflo
             setArrayItems(parameterArrayItems);
         }
         // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [currentComponentData?.parameters, name]);
+    }, []);
 
     return (
         <ul className="w-full">
             {arrayItems?.map((item) => (
-                <Property
-                    arrayName={name}
-                    currentComponentData={currentComponentData}
-                    customClassName="border-l ml-2 pl-2 pb-2 last-of-type:pb-0"
-                    dataPills={dataPills}
-                    key={item.name || `${name}_0`}
-                    mention={!!dataPills?.length}
-                    property={item as PropertyType & {controlType?: ControlTypeModel; defaultValue?: string}}
-                    updateWorkflowMutation={updateWorkflowMutation}
-                />
-            ))}
+                <div className="ml-2 flex w-full border-l pb-2" key={item.name || `${name}_0`}>
+                    <Property
+                        arrayName={name}
+                        currentComponentData={currentComponentData}
+                        customClassName="pl-2 w-full"
+                        dataPills={dataPills}
+                        mention={!!dataPills?.length}
+                        property={item as PropertyType & {controlType?: ControlTypeModel; defaultValue?: string}}
+                        updateWorkflowMutation={updateWorkflowMutation}
+                    />
 
-            {multipleValues && (
-                <div className="relative mt-2 w-full self-start">
-                    {newPropertyTypeOptions?.length && newPropertyTypeOptions?.length > 1 ? (
-                        <PropertyDropdownMenu
-                            menuItems={newPropertyTypeOptions}
-                            trigger={
-                                <Button
-                                    className="rounded-sm bg-gray-100 text-sm font-medium hover:bg-gray-200"
-                                    onClick={handleAddItemClick}
-                                    size="sm"
-                                    variant="ghost"
-                                >
-                                    <PlusIcon className="size-4" /> Add item
-                                </Button>
-                            }
-                        />
-                    ) : (
+                    {item.custom && (
                         <Button
-                            className="rounded-sm bg-gray-100 text-xs font-medium hover:bg-gray-200"
-                            onClick={handleAddItemClick}
-                            size="sm"
+                            className="ml-1 self-center"
+                            onClick={() => handleDeletePropertyClick(item.name!, name!)}
+                            size="icon"
+                            title="Delete property"
                             variant="ghost"
                         >
-                            <PlusIcon className="size-4" /> Add item
+                            <XIcon className="h-full w-auto cursor-pointer p-2 hover:text-red-500" />
                         </Button>
                     )}
                 </div>
-            )}
+            ))}
+
+            <Popover>
+                <PopoverTrigger asChild>
+                    <Button
+                        className="mt-2 rounded-sm bg-gray-100 text-xs font-medium hover:bg-gray-200"
+                        size="sm"
+                        variant="ghost"
+                    >
+                        <PlusIcon className="size-4" /> Add item
+                    </Button>
+                </PopoverTrigger>
+
+                <PopoverContent className="min-w-[400px]">
+                    <header className="flex items-center justify-between">
+                        <span className="font-medium">Add item</span>
+
+                        <PopoverClose asChild>
+                            <Cross2Icon aria-hidden="true" className="size-4 cursor-pointer" />
+                        </PopoverClose>
+                    </header>
+
+                    <main className="my-2 space-y-2">
+                        <PropertySelect
+                            label="Type"
+                            onValueChange={(value: keyof typeof PROPERTY_CONTROL_TYPES) => setNewItemType(value)}
+                            options={Object.keys(PROPERTY_CONTROL_TYPES).map((type) => ({
+                                label: type,
+                                value: type,
+                            }))}
+                            value={newItemType}
+                        />
+                    </main>
+
+                    <footer className="flex items-center justify-end space-x-2">
+                        <PopoverClose asChild>
+                            <Button onClick={handleAddItemClick} size="sm">
+                                Add
+                            </Button>
+                        </PopoverClose>
+                    </footer>
+                </PopoverContent>
+            </Popover>
         </ul>
     );
 };
