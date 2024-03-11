5c9895c01cfcaa564fd3e54589d6c0c255417e3c
Ivica Čardić <ivica.cardic@gmail.com>
2024-03-11 20:38:39 +0100

    265 - client - Don’t load dynamic properties if dependencies are not set

--- client/src/components/Properties/components/PropertyDynamicProperties.tsx
+++ client/src/components/Properties/components/PropertyDynamicProperties.tsx
@@ -4,24 +4,51 @@ import {PropertiesDataSourceModel} from '@/middleware/platform/configuration';
 import useWorkflowDataStore from '@/pages/automation/project/stores/useWorkflowDataStore';
 import {useWorkflowNodeDetailsPanelStore} from '@/pages/automation/project/stores/useWorkflowNodeDetailsPanelStore';
 import {useGetWorkflowNodeDynamicPropertiesQuery} from '@/queries/platform/workflowNodeDynamicProperties.queries';
+import {useEffect, useState} from 'react';
 
 interface PropertyDynamicPropertiesProps {
     name?: string;
+    loadDependency?: {[key: string]: string};
     propertiesDataSource?: PropertiesDataSourceModel;
 }
 
-const PropertyDynamicProperties = ({name, propertiesDataSource}: PropertyDynamicPropertiesProps) => {
+const PropertyDynamicProperties = ({loadDependency, name, propertiesDataSource}: PropertyDynamicPropertiesProps) => {
+    const [loadDependencyValues, setLoadDependencyValues] = useState<Array<string>>(
+        Object.values(loadDependency ?? {})
+    );
+
     const {workflow} = useWorkflowDataStore();
     const {currentNode} = useWorkflowNodeDetailsPanelStore();
 
-    const {data: properties, isLoading} = useGetWorkflowNodeDynamicPropertiesQuery(
+    const {
+        data: properties,
+        isLoading,
+        refetch,
+    } = useGetWorkflowNodeDynamicPropertiesQuery(
         {
             id: workflow.id!,
             propertyName: name!,
             workflowNodeName: currentNode.name!,
         },
-        !!propertiesDataSource
+        !!propertiesDataSource &&
+            loadDependencyValues.length > 0 &&
+            loadDependencyValues.reduce((enabled: boolean, loadDependencyValue: string) => {
+                return loadDependencyValue !== undefined;
+            }, true)
     );
+
+    useEffect(() => {
+        if (loadDependency && typeof loadDependency === 'object') {
+            setLoadDependencyValues(Object.values(loadDependency));
+        }
+    }, [loadDependency]);
+
+    useEffect(() => {
+        if (loadDependencyValues?.length) {
+            refetch();
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [loadDependencyValues]);
 
     if (isLoading) {
         return (
