7d788a8ba4063d51345b2271c40170d61c177cde
Ivica Čardić <ivica.cardic@gmail.com>
2024-03-19 20:56:27 +0100

    830 - SF

--- server/libs/automation/automation-configuration/automation-configuration-service/build.gradle.kts
+++ server/libs/automation/automation-configuration/automation-configuration-service/build.gradle.kts
@@ -1,6 +1,5 @@
 dependencies {
     implementation("org.apache.commons:commons-lang3")
-    implementation("org.apache.commons:commons-lang3")
     implementation("org.springframework:spring-context")
     implementation("org.springframework.boot:spring-boot-autoconfigure")
     implementation("org.springframework.data:spring-data-jdbc")
