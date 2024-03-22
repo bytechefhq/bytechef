e903e817ff5dead67d1bedf9fc386c81c990de33
Krešimir Čoko <kresimircoko@gmail.com>
2024-03-22 13:07:25 +0100

    801 - extract ArrayPropertyItem into its own component, move delete logic into the DeletePropertyButton and fix deletion for arrays

--- client/src/components/Properties/ArrayProperty.tsx
+++ client/src/components/Properties/ArrayProperty.tsx
@@ -1,34 +1,29 @@
 import {Button} from '@/components/ui/button';
 import {UpdateWorkflowRequest} from '@/middleware/automation/configuration';
-import {ControlTypeModel, ObjectPropertyModel, PropertyModel, WorkflowModel} from '@/middleware/platform/configuration';
+import {ControlTypeModel, ObjectPropertyModel, WorkflowModel} from '@/middleware/platform/configuration';
 import {PROPERTY_CONTROL_TYPES} from '@/shared/constants';
-import {ComponentDataType, CurrentComponentType, DataPillType, PropertyType} from '@/types/types';
+import {ArrayPropertyType, ComponentDataType, CurrentComponentType, DataPillType, PropertyType} from '@/types/types';
 import {Cross2Icon, PlusIcon} from '@radix-ui/react-icons';
 import {PopoverClose} from '@radix-ui/react-popover';
 import {UseMutationResult} from '@tanstack/react-query';
 import {useEffect, useState} from 'react';
 
 import {Popover, PopoverContent, PopoverTrigger} from '../ui/popover';
-import Property from './Property';
-import DeletePropertyButton from './components/DeletePropertyButton';
+import ArrayPropertyItem from './components/ArrayPropertyItem';
 import PropertySelect from './components/PropertySelect';
 
 interface ArrayPropertyProps {
     currentComponent?: CurrentComponentType;
     currentComponentData?: ComponentDataType;
     dataPills?: Array<DataPillType>;
-    handleDeleteProperty?: (subPropertyName: string, propertyName: string) => void;
     property: PropertyType;
     updateWorkflowMutation?: UseMutationResult<WorkflowModel, Error, UpdateWorkflowRequest, unknown>;
 }
-
-type ArrayPropertyType = PropertyModel & {controlType?: ControlTypeModel; custom?: boolean; defaultValue?: string};
 
 const ArrayProperty = ({
     currentComponent,
     currentComponentData,
     dataPills,
-    handleDeleteProperty,
     property,
     updateWorkflowMutation,
 }: ArrayPropertyProps) => {
@@ -49,20 +44,6 @@ const ArrayProperty = ({
         };
 
         setArrayItems([...arrayItems, newItem]);
-    };
-
-    const handleDeletePropertyClick = (subPropertyName: string, propertyName: string) => {
-        if (!subPropertyName || !propertyName) {
-            return;
-        }
-
-        if (handleDeleteProperty) {
-            handleDeleteProperty(subPropertyName, propertyName);
-        }
-
-        setArrayItems((subProperties) =>
-            subProperties.filter((subProperty) => (subProperty as ArrayPropertyType).name !== subPropertyName)
-        );
     };
 
     // render individual array items with data gathered from parameters
@@ -141,59 +122,32 @@ const ArrayProperty = ({
     return (
         <ul className="w-full">
             {arrayItems?.map((arrayItem, index) =>
-                (arrayItem as Array<ArrayPropertyType>).length ? (
-                    (arrayItem as Array<ArrayPropertyType>).map((subItem: ArrayPropertyType) => (
-                        <div className="ml-2 flex w-full border-l pb-2" key={subItem.name || `${name}_0`}>
-                            <Property
-                                arrayIndex={index}
-                                arrayName={name}
-                                currentComponent={currentComponent}
-                                currentComponentData={currentComponentData}
-                                customClassName="pl-2 w-full"
-                                dataPills={dataPills}
-                                mention={!!dataPills?.length}
-                                property={
-                                    subItem as PropertyType & {controlType?: ControlTypeModel; defaultValue?: string}
-                                }
-                                updateWorkflowMutation={updateWorkflowMutation}
-                            />
-
-                            {subItem.custom && name && subItem.name && (
-                                <DeletePropertyButton
-                                    handleDeletePropertyClick={handleDeletePropertyClick}
-                                    propertyName={name}
-                                    subPropertyName={subItem.name}
-                                />
-                            )}
-                        </div>
-                    ))
-                ) : (
-                    <div
-                        className="ml-2 flex w-full border-l pb-2"
-                        key={(arrayItem as ArrayPropertyType).name || `${name}_0`}
-                    >
-                        <Property
-                            arrayIndex={index}
+                Array.isArray(arrayItem) ? (
+                    arrayItem.map((subItem: ArrayPropertyType) => (
+                        <ArrayPropertyItem
+                            arrayItem={subItem}
                             arrayName={name}
                             currentComponent={currentComponent}
                             currentComponentData={currentComponentData}
-                            customClassName="pl-2 w-full"
                             dataPills={dataPills}
-                            mention={!!dataPills?.length}
-                            property={
-                                arrayItem as PropertyType & {controlType?: ControlTypeModel; defaultValue?: string}
-                            }
+                            index={index}
+                            key={subItem.name}
+                            setArrayItems={setArrayItems}
                             updateWorkflowMutation={updateWorkflowMutation}
                         />
-
-                        {(arrayItem as ArrayPropertyType).custom && name && (arrayItem as ArrayPropertyType).name && (
-                            <DeletePropertyButton
-                                handleDeletePropertyClick={handleDeletePropertyClick}
-                                propertyName={name}
-                                subPropertyName={(arrayItem as ArrayPropertyType).name!}
-                            />
-                        )}
-                    </div>
+                    ))
+                ) : (
+                    <ArrayPropertyItem
+                        arrayItem={arrayItem}
+                        arrayName={name}
+                        currentComponent={currentComponent}
+                        currentComponentData={currentComponentData}
+                        dataPills={dataPills}
+                        index={index}
+                        key={arrayItem.name}
+                        setArrayItems={setArrayItems}
+                        updateWorkflowMutation={updateWorkflowMutation}
+                    />
                 )
             )}
 
--- client/src/components/Properties/components/DeletePropertyButton.tsx
+++ client/src/components/Properties/components/DeletePropertyButton.tsx
@@ -1,32 +1,79 @@
 import {Button} from '@/components/ui/button';
 import {Tooltip, TooltipContent, TooltipTrigger} from '@/components/ui/tooltip';
+import {UpdateWorkflowRequest, WorkflowModel} from '@/middleware/automation/configuration';
+import useWorkflowDataStore from '@/pages/automation/project/stores/useWorkflowDataStore';
+import saveWorkflowDefinition from '@/pages/automation/project/utils/saveWorkflowDefinition';
+import {ComponentDataType} from '@/types/types';
+import {UseMutationResult} from '@tanstack/react-query';
 import {XIcon} from 'lucide-react';
 
 interface DeletePropertyButtonProps {
-    handleDeletePropertyClick: (subPropertyName: string, propertyName: string) => void;
+    currentComponentData: ComponentDataType;
+    handleDeletePropertyClick: () => void;
     propertyName: string;
-    subPropertyName: string;
+    subPropertyIndex?: number;
+    subPropertyName?: string;
+    updateWorkflowMutation: UseMutationResult<WorkflowModel, Error, UpdateWorkflowRequest, unknown>;
 }
 
 const DeletePropertyButton = ({
+    currentComponentData,
     handleDeletePropertyClick,
     propertyName,
+    subPropertyIndex,
     subPropertyName,
-}: DeletePropertyButtonProps) => (
-    <Tooltip>
-        <TooltipTrigger asChild>
-            <Button
-                className="ml-1 self-center"
-                onClick={() => handleDeletePropertyClick(subPropertyName!, propertyName!)}
-                size="icon"
-                variant="ghost"
-            >
-                <XIcon className="size-8 cursor-pointer p-2 hover:text-red-500" />
-            </Button>
-        </TooltipTrigger>
+    updateWorkflowMutation,
+}: DeletePropertyButtonProps) => {
+    const {workflow} = useWorkflowDataStore();
 
-        <TooltipContent>Delete property</TooltipContent>
-    </Tooltip>
-);
+    const deleteProperty = ({
+        propertyName,
+        subPropertyIndex,
+        subPropertyName,
+    }: {
+        propertyName: string;
+        subPropertyName?: string;
+        subPropertyIndex?: number;
+    }) => {
+        if (!currentComponentData.parameters) {
+            return;
+        }
+
+        if (subPropertyName) {
+            delete currentComponentData.parameters[propertyName][subPropertyName];
+        } else if (subPropertyIndex !== undefined) {
+            currentComponentData.parameters[propertyName].splice(subPropertyIndex, 1);
+        } else {
+            delete currentComponentData.parameters[propertyName];
+        }
+
+        saveWorkflowDefinition(
+            {...currentComponentData, name: currentComponentData.workflowNodeName},
+            workflow,
+            updateWorkflowMutation
+        );
+
+        if (handleDeletePropertyClick) {
+            handleDeletePropertyClick();
+        }
+    };
+
+    return (
+        <Tooltip>
+            <TooltipTrigger asChild>
+                <Button
+                    className="ml-1 self-center"
+                    onClick={() => deleteProperty({propertyName, subPropertyIndex, subPropertyName})}
+                    size="icon"
+                    variant="ghost"
+                >
+                    <XIcon className="size-8 cursor-pointer p-2 hover:text-red-500" />
+                </Button>
+            </TooltipTrigger>
+
+            <TooltipContent>Delete property</TooltipContent>
+        </Tooltip>
+    );
+};
 
 export default DeletePropertyButton;
