4b2cdb7494608bd05be53dbf9ceb2ba45976baad
Monika Domiter <domitermonika@gmail.com>
2024-03-29 12:17:26 +0100

    761 - update connection definition, add dynamic options

---
+++ server/libs/modules/components/teamwork/src/main/java/com/bytechef/component/teamwork/util/TeamworkUtils.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2023-present ByteChef Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef.component.teamwork.util;
+
+import static com.bytechef.component.definition.ComponentDSL.option;
+import static com.bytechef.component.teamwork.constant.TeamworkConstants.SITE_NAME;
+
+import com.bytechef.component.definition.ActionContext;
+import com.bytechef.component.definition.Context.Http;
+import com.bytechef.component.definition.Context.TypeReference;
+import com.bytechef.component.definition.Option;
+import com.bytechef.component.definition.Parameters;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author Monika Domiter
+ */
+public class TeamworkUtils {
+
+    private TeamworkUtils() {
+    }
+
+    public static String getBaseUrl(Parameters connectionParameters) {
+        return "https://" + connectionParameters.getRequiredString(SITE_NAME) + ".teamwork.com/projects/api/v3";
+    }
+
+    public static List<Option<String>> getTaskListIdOptions(
+        Parameters inputParameters, Parameters connectionParameters, String searchText, ActionContext context) {
+
+        Map<String, ?> body = context.http(http -> http.get(getBaseUrl(connectionParameters) + "/tasklists"))
+            .configuration(Http.responseType(Http.ResponseType.JSON))
+            .execute()
+            .getBody(new TypeReference<>() {});
+
+        List<Option<String>> options = new ArrayList<>();
+
+        if (body.get("tasklists") instanceof List<?> list) {
+            for (Object item : list) {
+                if (item instanceof Map<?, ?> map) {
+                    options.add(option((String) map.get("name"), String.valueOf(map.get("id"))));
+                }
+            }
+        }
+
+        return options;
+    }
+
+}
