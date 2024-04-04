70a5e1bc206a07fcce531e303285d1be4ae7db2c
Ivica Čardić <ivica.cardic@gmail.com>
2024-04-04 23:05:07 +0200

    520 - client - Add className property to EmptyList

--- client/src/components/EmptyList.tsx
+++ client/src/components/EmptyList.tsx
@@ -1,14 +1,16 @@
 import {ReactNode} from 'react';
+import {twMerge} from 'tailwind-merge';
 
 interface EmptyListProps {
+    button?: ReactNode;
+    className?: string;
     icon: ReactNode;
     title: string;
-    button?: ReactNode;
     message?: string;
 }
 
-const EmptyList = ({button, icon, message, title}: EmptyListProps) => (
-    <div className="w-full place-self-center px-2 3xl:mx-auto 3xl:w-4/5">
+const EmptyList = ({button, className, icon, message, title}: EmptyListProps) => (
+    <div className={twMerge('w-full place-self-center px-2 3xl:mx-auto 3xl:w-4/5', className)}>
         <div className="text-center">
             <span className="mx-auto inline-block">{icon}</span>
 
