6b8a1442eb22e893857781fcab8102d9c5a96802
Igor Beslic <igor.beslic@bitnaprednost.hr>
2024-04-05 13:13:41 +0200

    ISSUE-859 docker - add classes to map yml configuration and to register web static paths

---
+++ server/apps/server-app/src/main/java/com/bytechef/server/config/WebConfiguration.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2023-present ByteChef Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef.server.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author Igor Beslic
+ */
+@Configuration
+@EnableConfigurationProperties(StaticResourceProperties.class)
+@Profile({
+    "docker"
+})
+public class WebConfiguration implements WebMvcConfigurer {
+
+    private static final Logger logger = LoggerFactory.getLogger(WebConfiguration.class);
+    private final StaticResourceProperties staticResourceProperties;
+
+    public WebConfiguration(StaticResourceProperties staticResourceProperties) {
+        this.staticResourceProperties = new StaticResourceProperties();
+
+        this.staticResourceProperties.setWeb(staticResourceProperties.getWeb());
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        ResourceHandlerRegistration resourceHandlerRegistration = registry.addResourceHandler("/**", "*");
+
+        resourceHandlerRegistration.addResourceLocations(staticResourceProperties.getWeb());
+
+        logger.info("Serving static web content at {}", staticResourceProperties.getWeb());
+    }
+}
