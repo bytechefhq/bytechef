fdb356a9644e517f594540f90e865a5f2dcc11e9
Krešimir Čoko <kresimircoko@gmail.com>
2024-04-08 08:13:18 +0200

    801 - SF prop naming

--- client/src/components/Properties/components/InputTypeSwitchButton.tsx
+++ client/src/components/Properties/components/InputTypeSwitchButton.tsx
@@ -5,17 +5,17 @@ import {twMerge} from 'tailwind-merge';
 
 const InputTypeSwitchButton = ({
     className,
-    handleInputTypeSwitchButtonClick,
+    handleClick,
     mentionInput,
 }: {
     className?: string;
-    handleInputTypeSwitchButtonClick: () => void;
+    handleClick: () => void;
     mentionInput: boolean;
 }) => {
     return (
         <Button
             className={twMerge('z-50 size-auto p-0.5', className)}
-            onClick={handleInputTypeSwitchButtonClick}
+            onClick={handleClick}
             size="icon"
             title="Switch input type"
             variant="ghost"
@@ -26,19 +26,19 @@ const InputTypeSwitchButton = ({
                         <FormInputIcon className="size-5 text-gray-800" />
                     </TooltipTrigger>
 
-                    <TooltipContent>Switch to constant value</TooltipContent>
-                </Tooltip>
-            ) : (
-                <Tooltip>
-                    <TooltipTrigger asChild>
-                        <FunctionSquareIcon className="size-5 text-gray-800" />
-                    </TooltipTrigger>
 
-                    <TooltipContent>Switch to dynamic value</TooltipContent>
-                </Tooltip>
-            )}
-        </Button>
-    );
-};
+                <TooltipContent>Switch to constant value</TooltipContent>
+            </Tooltip>
+        ) : (
+            <Tooltip>
+                <TooltipTrigger asChild>
+                    <FunctionSquareIcon className="size-5 text-gray-800" />
+                </TooltipTrigger>
+
+                <TooltipContent>Switch to dynamic value</TooltipContent>
+            </Tooltip>
+        )}
+    </Button>
+);
 
 export default InputTypeSwitchButton;
