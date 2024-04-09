a99eb79b2ee89a7383928fe7f25041905ee87df3
Krešimir Čoko <kresimircoko@gmail.com>
2024-04-09 07:55:10 +0200

    801 - SF

--- client/src/pages/automation/project/components/DataPillPanel.tsx
+++ client/src/pages/automation/project/components/DataPillPanel.tsx
@@ -31,72 +31,73 @@ const DataPillPanel = ({
                     workflowNodeOutput.triggerDefinition?.outputDefined ||
                     workflowNodeOutput.triggerDefinition?.outputFunctionDefined)
         )
-        .map((workflowNodeOutput) => {
-            return {
-                ...workflowNodeOutput.actionDefinition,
-                componentDefinition: previousComponentDefinitions?.find(
-                    (currentComponentDefinition) =>
-                        currentComponentDefinition.name === workflowNodeOutput.actionDefinition?.componentName ||
-                        currentComponentDefinition.name === workflowNodeOutput.triggerDefinition?.componentName
-                ),
-                outputSchema: workflowNodeOutput.outputSchema,
-                sampleOutput: workflowNodeOutput.sampleOutput,
-                workflowNodeName: workflowNodeOutput.workflowNodeName,
-            } as ComponentOperationType;
-        });
+        .map(
+            (workflowNodeOutput) =>
+                ({
+                    ...workflowNodeOutput.actionDefinition,
+                    componentDefinition: previousComponentDefinitions?.find(
+                        (currentComponentDefinition) =>
+                            currentComponentDefinition.name === workflowNodeOutput.actionDefinition?.componentName ||
+                            currentComponentDefinition.name === workflowNodeOutput.triggerDefinition?.componentName
+                    ),
+                    outputSchema: workflowNodeOutput.outputSchema,
+                    sampleOutput: workflowNodeOutput.sampleOutput,
+                    workflowNodeName: workflowNodeOutput.workflowNodeName,
+                }) as ComponentOperationType
+        );
+
+    if (!dataPillPanelOpen || !workflowNodeDetailsPanelOpen) {
+        return <></>;
+    }
 
     return (
-        <>
-            {dataPillPanelOpen && workflowNodeDetailsPanelOpen && (
-                <div className="absolute inset-y-4 right-[485px] z-10 w-screen max-w-[400px] overflow-hidden rounded-xl border-l bg-white shadow-lg">
-                    <div className="flex h-full flex-col divide-y divide-gray-100 bg-white">
-                        <header className="flex content-center items-center p-4 text-lg font-medium">
-                            <span>Data Pill Panel</span>
+        <div className="absolute inset-y-4 right-[485px] z-10 w-screen max-w-[400px] overflow-hidden rounded-xl border-l bg-white shadow-lg">
+            <div className="flex h-full flex-col divide-y divide-gray-100 bg-white">
+                <header className="flex content-center items-center p-4 text-lg font-medium">
+                    <span>Data Pill Panel</span>
 
-                            <Tooltip>
-                                <TooltipTrigger asChild>
-                                    <InfoCircledIcon className="ml-1 size-4" />
-                                </TooltipTrigger>
+                    <Tooltip>
+                        <TooltipTrigger asChild>
+                            <InfoCircledIcon className="ml-1 size-4" />
+                        </TooltipTrigger>
 
-                                <TooltipContent>
-                                    To use data from the previous step drag its data pill into a field, or click on the
-                                    data pill.
-                                </TooltipContent>
-                            </Tooltip>
+                        <TooltipContent>
+                            To use data from the previous step drag its data pill into a field, or click on the data
+                            pill.
+                        </TooltipContent>
+                    </Tooltip>
 
-                            <button
-                                aria-label="Close the data pill panel"
-                                className="ml-auto pr-0"
-                                onClick={() => setDataPillPanelOpen(false)}
-                            >
-                                <Cross2Icon aria-hidden="true" className="size-4 cursor-pointer" />
-                            </button>
-                        </header>
+                    <button
+                        aria-label="Close the data pill panel"
+                        className="ml-auto pr-0"
+                        onClick={() => setDataPillPanelOpen(false)}
+                    >
+                        <Cross2Icon aria-hidden="true" className="size-4 cursor-pointer" />
+                    </button>
+                </header>
 
-                        <main className="flex w-full grow flex-col">
-                            <div className="mb-0 border-b border-gray-100 p-4">
-                                <Input
-                                    name="dataPillFilter"
-                                    onChange={(event) => setDataPillFilterQuery(event.target.value)}
-                                    placeholder="Filter Data Pills..."
-                                    value={dataPillFilterQuery}
-                                />
-                            </div>
-
-                            {(componentOperations && componentOperations.length > 0) ||
-                            (workflow.inputs && workflow.inputs.length > 0) ? (
-                                <DataPillPanelBody
-                                    componentOperations={componentOperations}
-                                    dataPillFilterQuery={dataPillFilterQuery}
-                                />
-                            ) : (
-                                <span className="p-4 text-sm text-muted-foreground">No available data pills.</span>
-                            )}
-                        </main>
+                <main className="flex w-full grow flex-col">
+                    <div className="mb-0 border-b border-gray-100 p-4">
+                        <Input
+                            name="dataPillFilter"
+                            onChange={(event) => setDataPillFilterQuery(event.target.value)}
+                            placeholder="Filter Data Pills..."
+                            value={dataPillFilterQuery}
+                        />
                     </div>
-                </div>
-            )}
-        </>
+
+                    {(componentOperations && !!componentOperations.length) ||
+                    (workflow.inputs && !!workflow.inputs.length) ? (
+                        <DataPillPanelBody
+                            componentOperations={componentOperations}
+                            dataPillFilterQuery={dataPillFilterQuery}
+                        />
+                    ) : (
+                        <span className="p-4 text-sm text-muted-foreground">No available data pills.</span>
+                    )}
+                </main>
+            </div>
+        </div>
     );
 };
 
