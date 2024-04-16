963244466175d5d6cf34401388ed74faa76e93a3
Ivica Čardić <ivica.cardic@gmail.com>
2024-04-16 16:18:48 +0200

    801 - client - Add message when there is no defined inputs

--- client/src/pages/automation/project/components/DataPillPanelBodyInputsItem.tsx
+++ client/src/pages/automation/project/components/DataPillPanelBodyInputsItem.tsx
@@ -41,6 +41,8 @@ const DataPillPanelBodyInputsItem = () => {
                         );
                     })}
                 </ul>
+
+                <p className="text-sm">No defined inputs.</p>
             </AccordionContent>
         </>
     );
