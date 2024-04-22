b05fbfe26a3d69c505b909fe23b37ff5e5391412
igorbeslic <igor.beslic@bitnaprednost.hr>
2024-04-22 09:49:13 +0200

    ISSUE-901 httpclient - refactor - better name (class name distinguishes purpose)

--- server/libs/modules/components/http-client/src/main/java/com/bytechef/component/http/client/util/HttpClientActionUtils.java
+++ server/libs/modules/components/http-client/src/main/java/com/bytechef/component/http/client/util/HttpClientActionUtils.java
@@ -121,7 +121,7 @@ public class HttpClientActionUtils {
                 .configuration(
                     Http.allowUnauthorizedCerts(
                         inputParameters.getBoolean(HttpClientComponentConstants.ALLOW_UNAUTHORIZED_CERTS, false))
-                        .disableAuthorization(Objects.isNull(connectionParameters.get(HttpClientComponentConstants.PARM_AUTHORIZATION_TYPE)))
+                        .disableAuthorization(Objects.isNull(connectionParameters.get(HttpClientComponentConstants.AUTHORIZATION_TYPE)))
                         .filename(inputParameters.getString(HttpClientComponentConstants.RESPONSE_FILENAME))
                         .followAllRedirects(inputParameters.getBoolean(HttpClientComponentConstants.FOLLOW_ALL_REDIRECTS, false))
                         .followRedirect(inputParameters.getBoolean(HttpClientComponentConstants.FOLLOW_REDIRECT, false))
