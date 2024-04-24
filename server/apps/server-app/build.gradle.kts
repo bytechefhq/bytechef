e946d64266246590a4b2c0ad8cefbb2b649430f5
Luka Ljubic <luka@ljubic.me>
2024-04-24 20:57:23 +0200

    387 - gradle

--- server/apps/server-app/build.gradle.kts
+++ server/apps/server-app/build.gradle.kts
@@ -120,6 +120,7 @@ dependencies {
     implementation(project(":server:libs:modules:components:microsoft:microsoft-share-point"))
     implementation(project(":server:libs:modules:components:microsoft:microsoft-teams"))
     implementation(project(":server:libs:modules:components:mysql"))
+    implementation(project(":server:libs:modules:components:nifty"))
     implementation(project(":server:libs:modules:components:object-helper"))
     implementation(project(":server:libs:modules:components:ods-file"))
     implementation(project(":server:libs:modules:components:openai"))
