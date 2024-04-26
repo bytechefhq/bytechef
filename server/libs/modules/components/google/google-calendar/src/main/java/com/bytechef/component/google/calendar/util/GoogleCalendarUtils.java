426be0799354a0dc6c5489e7c73e425375e2c71d
Monika Domiter <domitermonika@gmail.com>
2024-04-26 10:58:50 +0200

    693 - use display condition instead of dynamic properties

--- server/libs/modules/components/google/google-calendar/src/main/java/com/bytechef/component/google/calendar/util/GoogleCalendarUtils.java
+++ server/libs/modules/components/google/google-calendar/src/main/java/com/bytechef/component/google/calendar/util/GoogleCalendarUtils.java
@@ -16,36 +16,18 @@
 
 package com.bytechef.component.google.calendar.util;
 
-import static com.bytechef.component.definition.ComponentDSL.array;
-import static com.bytechef.component.definition.ComponentDSL.date;
-import static com.bytechef.component.definition.ComponentDSL.dateTime;
-import static com.bytechef.component.definition.ComponentDSL.integer;
-import static com.bytechef.component.definition.ComponentDSL.object;
 import static com.bytechef.component.definition.ComponentDSL.option;
-import static com.bytechef.component.definition.ComponentDSL.string;
 import static com.bytechef.component.google.calendar.constant.GoogleCalendarConstants.ALL_DAY;
-import static com.bytechef.component.google.calendar.constant.GoogleCalendarConstants.END;
-import static com.bytechef.component.google.calendar.constant.GoogleCalendarConstants.METHOD;
-import static com.bytechef.component.google.calendar.constant.GoogleCalendarConstants.MINUTES;
-import static com.bytechef.component.google.calendar.constant.GoogleCalendarConstants.REMINDERS;
-import static com.bytechef.component.google.calendar.constant.GoogleCalendarConstants.START;
-import static com.bytechef.component.google.calendar.constant.GoogleCalendarConstants.TIME;
-import static com.bytechef.component.google.calendar.constant.GoogleCalendarConstants.USE_DEFAULT;
 
 import com.bytechef.component.definition.ActionContext;
-import com.bytechef.component.definition.ComponentDSL.ModifiableArrayProperty;
-import com.bytechef.component.definition.ComponentDSL.ModifiableDateProperty;
-import com.bytechef.component.definition.ComponentDSL.ModifiableDateTimeProperty;
 import com.bytechef.component.definition.Option;
 import com.bytechef.component.definition.Parameters;
-import com.bytechef.component.definition.Property.ValueProperty;
 import com.bytechef.google.commons.GoogleServices;
 import com.google.api.client.util.DateTime;
 import com.google.api.services.calendar.model.CalendarListEntry;
 import com.google.api.services.calendar.model.EventDateTime;
 import java.io.IOException;
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -55,51 +37,6 @@ import java.util.Map;
  * @author Monika Domiter
  */
 public class GoogleCalendarUtils {
-
-    protected static final ModifiableDateProperty END_DATE_PROPERTY = date(END)
-        .label("End date")
-        .description("The end date of the event.")
-        .required(true);
-
-    protected static final ModifiableDateTimeProperty END_DATE_TIME_PROPERTY = dateTime(END)
-        .label("End date time")
-        .description(
-            "The (exclusive) end time of the event. For a recurring event, this is the end time of the " +
-                "first instance.")
-        .required(true);
-
-    protected static final ModifiableArrayProperty REMINDERS_PROPERTY = array(REMINDERS)
-        .label("Reminders")
-        .items(
-            object()
-                .properties(
-                    string(METHOD)
-                        .label("How is reminder sent?")
-                        .options(
-                            option("Email", "email", "Reminders are sent via email."),
-                            option("Popup", "popup", "Reminders are sent via a UI popup."))
-                        .required(true),
-                    integer(MINUTES)
-                        .label("Minutes before reminder")
-                        .description(
-                            "Number of minutes before the start of the event when the reminder " +
-                                "should trigger.")
-                        .minValue(0)
-                        .maxValue(40320)
-                        .required(true)))
-        .required(false);
-
-    protected static final ModifiableDateProperty START_DATE_PROPERTY = date(START)
-        .label("Start date")
-        .description("The start date of the event.")
-        .required(true);
-
-    protected static final ModifiableDateTimeProperty START_DATE_TIME_PROPERTY = dateTime(START)
-        .label("Start date time")
-        .description(
-            "The (inclusive) start time of the event. For a recurring event, this is the start time of the " +
-                "first instance.")
-        .required(true);
 
     private GoogleCalendarUtils() {
     }
@@ -111,34 +48,14 @@ public class GoogleCalendarUtils {
     public static EventDateTime createEventDateTime(Parameters inputParameters, String time) {
         EventDateTime eventDateTime = new EventDateTime();
 
-        Map<String, String> timeMap = inputParameters.getMap(TIME, String.class);
-
         if (inputParameters.getRequiredBoolean(ALL_DAY)) {
-            eventDateTime.setDate(new DateTime(timeMap.get(time)));
+            eventDateTime.setDate(new DateTime(inputParameters.getRequiredDate(time)));
         } else {
-            DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm");
-
             eventDateTime.setDateTime(
-                new DateTime(
-                    convertToDateViaSqlTimestamp(LocalDateTime.parse(timeMap.get(time), dateTimeFormatter))));
+                new DateTime(convertToDateViaSqlTimestamp(inputParameters.getRequiredLocalDateTime(time))));
         }
 
         return eventDateTime;
-    }
-
-    public static List<? extends ValueProperty<?>> createRemindersProperties(
-        Parameters inputParameters, Parameters connectionParameters, Map<String, String> dependencyPaths,
-        ActionContext context) {
-
-        return inputParameters.getRequiredBoolean(USE_DEFAULT) ? List.of() : List.of(REMINDERS_PROPERTY);
-    }
-
-    public static List<? extends ValueProperty<?>> createTimeProperties(
-        Parameters inputParameters, Parameters connectionParameters, Map<String, String> dependencyPaths,
-        ActionContext context) {
-
-        return inputParameters.getRequiredBoolean(ALL_DAY) ? List.of(START_DATE_PROPERTY, END_DATE_PROPERTY)
-            : List.of(START_DATE_TIME_PROPERTY, END_DATE_TIME_PROPERTY);
     }
 
     public static List<Option<String>> getCalendarIdOptions(
