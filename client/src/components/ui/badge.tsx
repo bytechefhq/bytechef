ac2ea17c2970caaeea8d2e50a01aae51b0593110
Ivica Čardić <ivica.cardic@gmail.com>
2024-04-27 17:12:45 +0200

    392 - client - Update badge styling

--- client/src/components/ui/badge.tsx
+++ client/src/components/ui/badge.tsx
@@ -13,9 +13,13 @@ const badgeVariants = cva(
         secondary:
           "border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/80",
         destructive:
+          "border-transparent bg-destructive text-destructive-foreground shadow hover:bg-destructive/80",
+        destructive_outline:
           "border-destructive text-destructive shadow hover:bg-destructive/80",
         outline: "text-foreground",
         success:
+          "border-transparent bg-success text-success-foreground hover:bg-success",
+        success_outline:
           "border-success text-success hover:bg-success hover:text-success-foreground",
       },
     },
