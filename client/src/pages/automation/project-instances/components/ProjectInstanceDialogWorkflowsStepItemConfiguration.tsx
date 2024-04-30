615128365de5dd3bc538c520ac0257a7f3c0a861
Ivica Čardić <ivica.cardic@gmail.com>
2024-04-30 08:51:21 +0200

    440 - client - Fix input types conversions in ProjectInstanceDialog

--- client/src/pages/automation/project-instances/components/ProjectInstanceDialogWorkflowsStepItemConfiguration.tsx
+++ client/src/pages/automation/project-instances/components/ProjectInstanceDialogWorkflowsStepItemConfiguration.tsx
@@ -1,4 +1,5 @@
 import {ProjectInstanceModel, WorkflowModel} from '@/middleware/automation/configuration';
+import {ControlTypeModel, PropertyTypeModel} from '@/middleware/platform/configuration';
 import Properties from '@/pages/platform/workflow-editor/components/Properties/Properties';
 import {PropertyType} from '@/types/types';
 import {UseFormRegister} from 'react-hook-form';
@@ -18,30 +19,57 @@ const ProjectInstanceDialogWorkflowsStepItemConfiguration = ({
     workflow,
     workflowIndex,
 }: ProjectInstanceDialogWorkflowsStepItemConfigurationProps) => {
-    return workflow.inputs?.length ? (
+    const properties = workflow.inputs?.map((input) => {
+        if (input.type === 'boolean') {
+            return {
+                ...input,
+                controlType: ControlTypeModel.Select,
+                type: PropertyTypeModel.Boolean,
+            } as PropertyType;
+        } else if (input.type === 'date') {
+            return {
+                ...input,
+                controlType: ControlTypeModel.Date,
+                type: PropertyTypeModel.Date,
+            } as PropertyType;
+        } else if (input.type === 'date_time') {
+            return {
+                ...input,
+                controlType: ControlTypeModel.DateTime,
+                type: PropertyTypeModel.DateTime,
+            } as PropertyType;
+        } else if (input.type === 'integer') {
+            return {
+                ...input,
+                controlType: ControlTypeModel.Integer,
+                type: PropertyTypeModel.Integer,
+            } as PropertyType;
+        } else if (input.type === 'number') {
+            return {
+                ...input,
+                controlType: ControlTypeModel.Number,
+                type: PropertyTypeModel.Number,
+            } as PropertyType;
+        } else if (input.type === 'string') {
+            return {
+                ...input,
+                controlType: ControlTypeModel.Text,
+                type: PropertyTypeModel.String,
+            } as PropertyType;
+        } else {
+            return {
+                ...input,
+                controlType: ControlTypeModel.Time,
+                type: PropertyTypeModel.Time,
+            } as PropertyType;
+        }
+    });
+
+    return properties?.length ? (
         <Properties
             formState={formState}
             path={`projectInstanceWorkflows.${workflowIndex!}.inputs`}
-            properties={workflow.inputs.map((input) => {
-                if (input.type === 'string') {
-                    return {
-                        controlType: 'TEXT',
-                        type: 'STRING',
-                        ...input,
-                    } as PropertyType;
-                } else if (input.type === 'number') {
-                    return {
-                        type: 'NUMBER',
-                        ...input,
-                    } as PropertyType;
-                } else {
-                    return {
-                        controlType: 'SELECT',
-                        type: 'BOOLEAN',
-                        ...input,
-                    } as PropertyType;
-                }
-            })}
+            properties={properties}
             register={register}
         />
     ) : (
