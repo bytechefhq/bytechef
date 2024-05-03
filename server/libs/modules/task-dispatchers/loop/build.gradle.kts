c62346354f61a43fa61b0ff6382bcb938e1e42c8
igorbeslic <igor.beslic@bitnaprednost.hr>
2024-05-03 17:22:56 +0200

    ISSUE-630 gradle build - discard sequence

--- server/libs/modules/task-dispatchers/loop/build.gradle.kts
+++ server/libs/modules/task-dispatchers/loop/build.gradle.kts
@@ -2,5 +2,4 @@ version="1.0"
 
 dependencies {
     testImplementation(project(":server:libs:modules:task-dispatchers:condition"))
-    testImplementation(project(":server:libs:modules:task-dispatchers:sequence"))
 }
