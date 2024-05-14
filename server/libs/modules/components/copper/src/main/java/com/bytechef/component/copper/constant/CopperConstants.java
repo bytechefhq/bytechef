63f96e83f5594f0aabe5f6924835489ff3dd8212
Monika Domiter <domitermonika@gmail.com>
2024-05-14 13:34:34 +0200

    984 - added parent options

--- server/libs/modules/components/copper/src/main/java/com/bytechef/component/copper/constant/CopperConstants.java
+++ server/libs/modules/components/copper/src/main/java/com/bytechef/component/copper/constant/CopperConstants.java
@@ -28,6 +28,7 @@ public class CopperConstants {
     public static final String CATEGORY = "category";
     public static final String CATEGORY_LABEL = "Category";
     public static final String CITY = "city";
+    public static final String COMPANY = "company";
     public static final String COMPANY_ID = "company_id";
     public static final String CONTACT_TYPE_ID = "contact_type_id";
     public static final String COPPER = "copper";
@@ -40,10 +41,13 @@ public class CopperConstants {
     public static final String EMAILS = "emails";
     public static final String EMAIL_DOMAIN = "email_domain";
     public static final String ID = "id";
+    public static final String LEAD = "lead";
     public static final String NAME = "name";
     public static final String NUMBER = "number";
+    public static final String OPPORTUNITY = "opportunity";
     public static final String OTHER = "other";
     public static final String PARENT = "parent";
+    public static final String PERSON = "person";
     public static final String PHONE_NUMBERS = "phone_numbers";
     public static final String POSTAL_CODE = "postal_code";
     public static final String SOCIALS = "socials";
