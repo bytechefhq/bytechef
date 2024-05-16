bf192386a5dd7851a3ba14c6787836898fad03c5
Krešimir Čoko <kresimircoko@gmail.com>
2024-05-16 08:36:10 +0200

    889 - clarify argument name

--- client/src/pages/platform/workflow-editor/utils/getParameterByPath.ts
+++ client/src/pages/platform/workflow-editor/utils/getParameterByPath.ts
@@ -1,4 +1,4 @@
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export default function getParameterByPath(path: string, object: any) {
-    return path.split('.').reduce((object, key) => object?.[key], object);
+export default function getParameterByPath(path: string, workflowComponent: any) {
+    return path.split('.').reduce((workflowComponent, key) => workflowComponent?.[key], workflowComponent);
 }
