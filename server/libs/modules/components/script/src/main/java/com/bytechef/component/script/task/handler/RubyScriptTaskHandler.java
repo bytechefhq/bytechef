18c683040c54f379943ebfd55199c98c9c92290e
Ivica Čardić <ivica.cardic@gmail.com>
2024-05-17 08:11:53 +0200

    369 - Add AbstractTaskHandler to avoid duplicated code

--- server/libs/modules/components/script/src/main/java/com/bytechef/component/script/task/handler/RubyScriptTaskHandler.java
+++ server/libs/modules/components/script/src/main/java/com/bytechef/component/script/task/handler/RubyScriptTaskHandler.java
@@ -19,6 +19,7 @@ package com.bytechef.component.script.task.handler;
 import static com.bytechef.platform.component.definition.ScriptComponentDefinition.SCRIPT;
 
 import com.bytechef.platform.component.registry.facade.ActionDefinitionFacade;
+import com.bytechef.platform.component.registry.handler.AbstractTaskHandler;
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import org.springframework.stereotype.Component;
 
@@ -27,10 +28,10 @@ import org.springframework.stereotype.Component;
  * @author Ivica Cardic
  */
 @Component(SCRIPT + "/v1/ruby")
-public class RubyScriptTaskHandler extends AbstractScriptTaskHandler {
+public class RubyScriptTaskHandler extends AbstractTaskHandler {
 
     @SuppressFBWarnings("EI")
     public RubyScriptTaskHandler(ActionDefinitionFacade actionDefinitionFacade) {
-        super("ruby", actionDefinitionFacade);
+        super("script", 1, "ruby", actionDefinitionFacade);
     }
 }
