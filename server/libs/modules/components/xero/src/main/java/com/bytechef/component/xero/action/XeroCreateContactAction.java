a2940cfe1d5f7c62b5018f665dba89b20eb4aaad
Monika Domiter <domitermonika@gmail.com>
2024-05-17 23:27:42 +0200

    696 - update create contact and create invoice action

--- server/libs/modules/components/xero/src/main/java/com/bytechef/component/xero/action/XeroCreateContactAction.java
+++ server/libs/modules/components/xero/src/main/java/com/bytechef/component/xero/action/XeroCreateContactAction.java
@@ -18,151 +18,226 @@ package com.bytechef.component.xero.action;
 
 import static com.bytechef.component.definition.ComponentDSL.ModifiableActionDefinition;
 import static com.bytechef.component.definition.ComponentDSL.action;
-import static com.bytechef.component.definition.ComponentDSL.bool;
+import static com.bytechef.component.definition.ComponentDSL.array;
 import static com.bytechef.component.definition.ComponentDSL.object;
+import static com.bytechef.component.definition.ComponentDSL.option;
 import static com.bytechef.component.definition.ComponentDSL.string;
 import static com.bytechef.component.xero.constant.XeroConstants.ACCOUNT_NUMBER;
+import static com.bytechef.component.xero.constant.XeroConstants.ADDRESSES;
+import static com.bytechef.component.xero.constant.XeroConstants.ADDRESS_TYPE;
 import static com.bytechef.component.xero.constant.XeroConstants.BANK_ACCOUNT_DETAILS;
+import static com.bytechef.component.xero.constant.XeroConstants.BASE_URL;
+import static com.bytechef.component.xero.constant.XeroConstants.CITY;
 import static com.bytechef.component.xero.constant.XeroConstants.COMPANY_NUMBER;
-import static com.bytechef.component.xero.constant.XeroConstants.CONTACT_NUMBER;
+import static com.bytechef.component.xero.constant.XeroConstants.CONTACTS;
+import static com.bytechef.component.xero.constant.XeroConstants.CONTACT_ID;
+import static com.bytechef.component.xero.constant.XeroConstants.CONTACT_STATUS;
+import static com.bytechef.component.xero.constant.XeroConstants.COUNTRY;
 import static com.bytechef.component.xero.constant.XeroConstants.CREATE_CONTACT;
 import static com.bytechef.component.xero.constant.XeroConstants.EMAIL_ADDRESS;
 import static com.bytechef.component.xero.constant.XeroConstants.FIRST_NAME;
-import static com.bytechef.component.xero.constant.XeroConstants.IS_CUSTOMER;
-import static com.bytechef.component.xero.constant.XeroConstants.IS_SUPPLIER;
 import static com.bytechef.component.xero.constant.XeroConstants.LAST_NAME;
+import static com.bytechef.component.xero.constant.XeroConstants.MESSAGE;
 import static com.bytechef.component.xero.constant.XeroConstants.NAME;
+import static com.bytechef.component.xero.constant.XeroConstants.PHONES;
+import static com.bytechef.component.xero.constant.XeroConstants.PHONE_AREA_CODE;
+import static com.bytechef.component.xero.constant.XeroConstants.PHONE_COUNTRY_CODE;
+import static com.bytechef.component.xero.constant.XeroConstants.PHONE_NUMBER;
+import static com.bytechef.component.xero.constant.XeroConstants.PHONE_TYPE;
+import static com.bytechef.component.xero.constant.XeroConstants.POSTAL_CODE;
+import static com.bytechef.component.xero.constant.XeroConstants.REGION;
 import static com.bytechef.component.xero.constant.XeroConstants.TAX_NUMBER;
-import static com.bytechef.component.xero.constant.XeroConstants.TRACKING_CATEGORY_NAME;
-import static com.bytechef.component.xero.constant.XeroConstants.TRACKING_OPTION_NAME;
 
 import com.bytechef.component.definition.ActionContext;
+import com.bytechef.component.definition.Context.ContextFunction;
 import com.bytechef.component.definition.Context.Http;
 import com.bytechef.component.definition.Context.TypeReference;
 import com.bytechef.component.definition.Parameters;
-import java.util.LinkedHashMap;
+import com.bytechef.component.definition.Property.ControlType;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author Mario Cvjetojevic
+ * @author Monika Domiter
  */
-public final class XeroCreateContactAction {
+public class XeroCreateContactAction {
 
     public static final ModifiableActionDefinition ACTION_DEFINITION = action(CREATE_CONTACT)
         .title("Create contact")
-        .description("Description")
+        .description("Creates a new contact.")
         .properties(
             string(NAME)
                 .label("Name")
-                .description(
-                    "Full name of a contact or organisation.")
+                .description("Full name of a contact or organisation.")
                 .maxLength(255)
                 .required(true),
-            string(CONTACT_NUMBER)
-                .label("Contact number")
-                .description(
-                    "This can be updated via the API only i.e. This field is read only on the Xero contact screen, " +
-                        "used to identify contacts in external systems. If the Contact Number is used, this is " +
-                        "displayed as Contact Code in the Contacts UI in Xero.")
-                .maxLength(50),
-            string(ACCOUNT_NUMBER)
-                .label("Account number")
-                .description(
-                    "A user defined account number. This can be updated via the API and the Xero UI.")
-                .maxLength(50),
-            string(FIRST_NAME)
-                .label("First name")
-                .description(
-                    "First name of contact person.")
-                .maxLength(255),
-            string(LAST_NAME)
-                .label("Last name")
-                .description(
-                    "Last name of contact person.")
-                .maxLength(255),
             string(COMPANY_NUMBER)
                 .label("Company Number")
-                .description(
-                    "Company registration number.")
-                .maxLength(50),
+                .description("Company registration number.")
+                .maxLength(50)
+                .required(false),
+            string(ACCOUNT_NUMBER)
+                .label("Account number")
+                .description("Unique account number to identify, reference and search for the contact.")
+                .maxLength(50)
+                .required(false),
+            string(CONTACT_STATUS)
+                .label("Contact status")
+                .description("Current status of a contact.")
+                .options(
+                    option("Active", "ACTIVE", "The Contact is active and can be used in transactions."),
+                    option("Archived", "ARCHIVED",
+                        "The Contact is archived and can no longer be used in transactions."),
+                    option("GDPR erasure request", "GDPRREQUEST",
+                        "The Contact is the subject of a GDPR erasure request and can no longer be used in transctions."))
+                .required(false),
+            string(FIRST_NAME)
+                .label("First name")
+                .description("First name of primary person.")
+                .maxLength(255)
+                .required(false),
+            string(LAST_NAME)
+                .label("Last name")
+                .description("Last name of primary person.")
+                .maxLength(255)
+                .required(false),
             string(EMAIL_ADDRESS)
                 .label("Email address")
-                .description(
-                    "Email address of contact person (umlauts not supported). ")
-                .maxLength(255),
+                .description("Email address of contact person.")
+                .controlType(ControlType.EMAIL)
+                .maxLength(255)
+                .required(false),
             string(BANK_ACCOUNT_DETAILS)
-                .label("Bank account details")
-                .description(
-                    "Bank account number of contact."),
+                .label("Bank account number")
+                .description("Bank account number of contact.")
+                .required(false),
             string(TAX_NUMBER)
                 .label("Tax number")
                 .description(
                     "Tax number of contact – this is also known as the ABN (Australia), GST Number (New Zealand), " +
                         "VAT Number (UK) or Tax ID Number (US and global) in the Xero UI depending on which " +
                         "regionalized version of Xero you are using.")
-                .maxLength(50),
-            bool(IS_SUPPLIER)
-                .label("Is supplier")
-                .description(
-                    "Boolean that describes if a contact that has any AP invoices entered against them. Cannot be " +
-                        "set via PUT or POST – it is automatically set when an accounts payable invoice is generated " +
-                        "against this contact."),
-            bool(IS_CUSTOMER)
-                .label("Is customer")
-                .description(
-                    "Boolean that describes if a contact has any AR invoices entered against them. Cannot be set " +
-                        "via PUT or POST – it is automatically set when an accounts receivable invoice is generated " +
-                        "against this contact."),
-            string(TRACKING_CATEGORY_NAME)
-                .label("Tracking category name")
-                .description(
-                    "The name of the Tracking Category assigned to the contact under SalesTrackingCategories and " +
-                        "PurchasesTrackingCategories"),
-            string(TRACKING_OPTION_NAME)
-                .label("Tracking option name")
-                .description(
-                    "The name of the Tracking Option assigned to the contact under SalesTrackingCategories and " +
-                        "PurchasesTrackingCategories"))
+                .maxLength(50)
+                .required(false),
+            array(PHONES)
+                .label("Phones")
+                .items(
+                    object()
+                        .properties(
+                            string(PHONE_TYPE)
+                                .options(
+                                    option("Default", "DEFAULT"),
+                                    option("Fax", "FAX"),
+                                    option("Mobile", "MOBILE"),
+                                    option("Direct Dial", "DDI"))
+                                .required(true),
+                            string(PHONE_NUMBER)
+                                .label("Phone Number")
+                                .maxLength(50)
+                                .required(false),
+                            string(PHONE_AREA_CODE)
+                                .label("Phone Area Code")
+                                .maxLength(10)
+                                .required(false),
+                            string(PHONE_COUNTRY_CODE)
+                                .label("Phone Country Code")
+                                .maxLength(20)
+                                .required(false)))
+                .maxItems(4)
+                .required(false),
+            array(ADDRESSES)
+                .label("Addresses")
+                .items(
+                    object()
+                        .properties(
+                            string(ADDRESS_TYPE)
+                                .label("Address type")
+                                .options(
+                                    option("POBOX", "POBOX"),
+                                    option("STREET", "STREET"))
+                                .required(true),
+                            string(CITY)
+                                .label("City/Town")
+                                .maxLength(255)
+                                .required(false),
+                            string(REGION)
+                                .label("State/Region")
+                                .maxLength(255)
+                                .required(false),
+                            string(POSTAL_CODE)
+                                .label("Postal/Zip code")
+                                .maxLength(50)
+                                .required(false),
+                            string(COUNTRY)
+                                .label("Country")
+                                .maxLength(50)
+                                .required(false)))
+                .maxItems(2)
+                .required(false))
         .outputSchema(
             object()
                 .properties(
-                    string(NAME),
-                    string(CONTACT_NUMBER),
+                    string(CONTACT_ID),
+                    string(COMPANY_NUMBER),
                     string(ACCOUNT_NUMBER),
+                    string(CONTACT_STATUS),
+                    string(NAME),
                     string(FIRST_NAME),
                     string(LAST_NAME),
-                    string(COMPANY_NUMBER),
                     string(EMAIL_ADDRESS),
                     string(BANK_ACCOUNT_DETAILS),
                     string(TAX_NUMBER),
-                    bool(IS_SUPPLIER),
-                    bool(IS_CUSTOMER),
-                    string(TRACKING_CATEGORY_NAME),
-                    string(TRACKING_OPTION_NAME)))
+                    array(ADDRESSES)
+                        .items(
+                            object()
+                                .properties(
+                                    string(ADDRESS_TYPE),
+                                    string(CITY),
+                                    string(REGION),
+                                    string(POSTAL_CODE),
+                                    string(COUNTRY))),
+                    array(PHONES)
+                        .items(
+                            object()
+                                .properties(
+                                    string(PHONE_TYPE),
+                                    string(PHONE_NUMBER),
+                                    string(PHONE_AREA_CODE),
+                                    string(PHONE_COUNTRY_CODE)))))
         .perform(XeroCreateContactAction::perform);
+
+    protected static final ContextFunction<Http, Http.Executor> POST_CONTACTS_CONTEXT_FUNCTION =
+        http -> http.post(BASE_URL + "/" + CONTACTS);
 
     private XeroCreateContactAction() {
     }
 
-    public static LinkedHashMap<String, ?> perform(
+    public static Object perform(
         Parameters inputParameters, Parameters connectionParameters, ActionContext actionContext) {
 
-        return actionContext.http(http -> http.post("https://api.xero.com/api.xro/2.0/Contacts"))
+        Map<String, Object> body = actionContext.http(POST_CONTACTS_CONTEXT_FUNCTION)
             .body(
                 Http.Body.of(
                     NAME, inputParameters.getRequiredString(NAME),
-                    CONTACT_NUMBER, inputParameters.getString(CONTACT_NUMBER),
+                    COMPANY_NUMBER, inputParameters.getString(COMPANY_NUMBER),
+                    ACCOUNT_NUMBER, inputParameters.getString(ACCOUNT_NUMBER),
+                    CONTACT_STATUS, inputParameters.getString(CONTACT_STATUS),
                     FIRST_NAME, inputParameters.getString(FIRST_NAME),
                     LAST_NAME, inputParameters.getString(LAST_NAME),
-                    COMPANY_NUMBER, inputParameters.getString(COMPANY_NUMBER),
                     EMAIL_ADDRESS, inputParameters.getString(EMAIL_ADDRESS),
                     BANK_ACCOUNT_DETAILS, inputParameters.getString(BANK_ACCOUNT_DETAILS),
                     TAX_NUMBER, inputParameters.getString(TAX_NUMBER),
-                    IS_SUPPLIER, inputParameters.getBoolean(IS_SUPPLIER),
-                    IS_CUSTOMER, inputParameters.getBoolean(IS_CUSTOMER),
-                    TRACKING_CATEGORY_NAME, inputParameters.getString(TRACKING_CATEGORY_NAME),
-                    TRACKING_OPTION_NAME, inputParameters.getString(TRACKING_OPTION_NAME)))
+                    ADDRESSES, inputParameters.getList(ADDRESSES),
+                    PHONES, inputParameters.getList(PHONES)))
             .configuration(Http.responseType(Http.ResponseType.JSON))
             .execute()
             .getBody(new TypeReference<>() {});
+
+        if (body.get(CONTACTS) instanceof List<?> list) {
+            return list.getFirst();
+        } else {
+            return body.get(MESSAGE);
+        }
     }
 }
--- server/libs/modules/components/xero/src/main/java/com/bytechef/component/xero/constant/XeroConstants.java
+++ server/libs/modules/components/xero/src/main/java/com/bytechef/component/xero/constant/XeroConstants.java
@@ -16,44 +16,121 @@
 
 package com.bytechef.component.xero.constant;
 
+import static com.bytechef.component.definition.ComponentDSL.array;
+import static com.bytechef.component.definition.ComponentDSL.integer;
+import static com.bytechef.component.definition.ComponentDSL.number;
+import static com.bytechef.component.definition.ComponentDSL.object;
+import static com.bytechef.component.definition.ComponentDSL.option;
+import static com.bytechef.component.definition.ComponentDSL.string;
+
+import com.bytechef.component.definition.ComponentDSL.ModifiableArrayProperty;
+import com.bytechef.component.definition.ComponentDSL.ModifiableObjectProperty;
+import com.bytechef.component.definition.ComponentDSL.ModifiableStringProperty;
+
 /**
  * @author Mario Cvjetojevic
+ * @author Monika Domiter
  */
 public final class XeroConstants {
 
-    public static final String XERO = "xero";
-    public static final String CREATE_CONTACT = "createContact";
-    public static final String CREATE_INVOICE = "createInvoice";
-    public static final String NAME = "Name";
-    public static final String CONTACT_ID = "ContactID";
-    public static final String CONTACT_NUMBER = "ContactNumber";
     public static final String ACCOUNT_NUMBER = "AccountNumber";
+    public static final String ACCREC = "ACCREC";
+    public static final String ADDRESSES = "Addresses";
+    public static final String ADDRESS_TYPE = "AddressType";
+    public static final String BANK_ACCOUNT_DETAILS = "BankAccountDetails";
+    public static final String BASE_URL = "https://api.xero.com/api.xro/2.0";
+    public static final String CITY = "City";
+    public static final String CODE = "Code";
     public static final String COMPANY_NUMBER = "CompanyNumber";
+    public static final String CONTACT_ID = "ContactID";
+    public static final String CONTACT_STATUS = "ContactStatus";
+    public static final String CONTACT = "Contact";
+    public static final String CONTACTS = "Contacts";
+    public static final String COUNTRY = "Country";
+    public static final String CREATE_CONTACT = "createContact";
+    public static final String CREATE_SALES_INVOICE = "createSalesInvoice";
+    public static final String CURRENCY_CODE = "CurrencyCode";
+    public static final String DATE = "Date";
+    public static final String DESCRIPTION = "Description";
+    public static final String DISCOUNT_RATE = "DiscountRate";
+    public static final String DUE_DATE = "DueDate";
+    public static final String EMAIL_ADDRESS = "EmailAddress";
     public static final String FIRST_NAME = "FirstName";
     public static final String LAST_NAME = "LastName";
-    public static final String EMAIL_ADDRESS = "EmailAddress";
-    public static final String BANK_ACCOUNT_DETAILS = "BankAccountDetails";
-    public static final String TAX_NUMBER = "TaxNumber";
-    public static final String IS_SUPPLIER = "IsSupplier";
-    public static final String IS_CUSTOMER = "IsCustomer";
-    public static final String TRACKING_CATEGORY_NAME = "TrackingCategoryName";
-    public static final String TRACKING_OPTION_NAME = "TrackingOptionName";
-
-    public static final String TYPE = "Type";
-    public static final String ACCPAY = "ACCPAY";
-    public static final String ACCREC = "ACCREC";
-    public static final String LINE_ITEMS = "lineItems";
-    public static final String LINE_ITEM = "lineItem";
-
-    public static final String DESCRIPTION = "Description";
+    public static final String LINE_AMOUNT_TYPES = "LineAmountTypes";
+    public static final String LINE_ITEMS = "LineItems";
+    public static final String MESSAGE = "Message";
+    public static final String NAME = "Name";
+    public static final String PHONES = "Phones";
+    public static final String PHONE_TYPE = "PhoneType";
+    public static final String PHONE_NUMBER = "PhoneNumber";
+    public static final String PHONE_AREA_CODE = "PhoneAreaCode";
+    public static final String PHONE_COUNTRY_CODE = "PhoneCountryCode";
+    public static final String POSTAL_CODE = "PostalCode";
     public static final String QUANTITY = "Quantity";
+    public static final String REFERENCE = "Reference";
+    public static final String REGION = "Region";
+    public static final String STATUS = "Status";
+    public static final String TAX_NUMBER = "TaxNumber";
+    public static final String TYPE = "Type";
     public static final String UNIT_AMOUNT = "UnitAmount";
-    public static final String ITEM_CODE = "ItemCode";
-    public static final String LINE_ITEM_ID = "LineItemID";
-    public static final String TAX_TYPE = "TaxType";
-    public static final String TAX_AMOUNT = "TaxAmount";
-    public static final String LINE_AMOUNT = "LineAmount";
+    public static final String XERO = "xero";
+
+    public static final ModifiableObjectProperty INVOICE_OUTPUT_PROPERTY = object()
+        .properties(
+            string(TYPE),
+            string(REFERENCE),
+            object(CONTACT)
+                .properties(
+                    string(CONTACT_ID),
+                    string(NAME),
+                    string(EMAIL_ADDRESS)),
+            string("DateString"),
+            string("DueDateString"),
+            string(STATUS),
+            string(LINE_AMOUNT_TYPES),
+            array(LINE_ITEMS)
+                .items(
+                    object()
+                        .properties(
+                            string(DESCRIPTION),
+                            integer(QUANTITY),
+                            number(UNIT_AMOUNT))),
+            string(CURRENCY_CODE));
+
+    public static final ModifiableStringProperty LINE_AMOUNT_TYPE_PROPERTY = string(LINE_AMOUNT_TYPES)
+        .label("Line amount type")
+        .options(
+            option("Exclusive", "Exclusive"),
+            option("Inclusive", "Inclusive"),
+            option("NoTax", "NoTax"))
+        .required(false);
+
+    public static final ModifiableArrayProperty LINE_ITEMS_ACCREC_PROPERTY = array(LINE_ITEMS)
+        .label("Line items")
+        .description("Line items on the invoice.")
+        .required(true)
+        .minItems(1)
+        .items(
+            object()
+                .properties(
+                    string(DESCRIPTION)
+                        .label(DESCRIPTION)
+                        .maxLength(4000)
+                        .required(true),
+                    integer(QUANTITY)
+                        .label(QUANTITY)
+                        .description("LineItem quantity")
+                        .required(false),
+                    number(UNIT_AMOUNT)
+                        .label("Price")
+                        .required(false),
+                    number(DISCOUNT_RATE)
+                        .label("Discount (%)")
+                        .maxNumberPrecision(2)
+                        .required(false)));
 
     private XeroConstants() {
     }
+
 }
