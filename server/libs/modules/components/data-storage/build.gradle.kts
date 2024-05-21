10daed550361620977ad2b0ff2275325b08ceb08
Marko Krišković <marko.kriskovic@protonmail.com>
2024-05-21 09:39:13 +0200

    369 - updating dependencies

--- server/libs/modules/components/data-storage/build.gradle.kts
+++ server/libs/modules/components/data-storage/build.gradle.kts
@@ -1,1 +1,8 @@
 version="1.0"
+
+dependencies{
+    implementation("org.slf4j:slf4j-api")
+    implementation("org.springframework:spring-context")
+    implementation("org.springframework.boot:spring-boot-autoconfigure")
+    implementation(project(":server:libs:core:commons:commons-util"))
+}
