0e892af3f8d64a3298c0ea4d1f295fb28bb2fd8f
igorbeslic <igor.beslic@bitnaprednost.hr>
2024-05-24 20:10:47 +0200

    ISSUE-1107 component - date-helper - component implementation

---
+++ server/libs/modules/components/date-helper/src/main/java/com/bytechef/component/date/helper/DateHelperComponentHandler.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2023-present ByteChef Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef.component.date.helper;
+
+import static com.bytechef.component.definition.ComponentDSL.component;
+
+import com.bytechef.component.ComponentHandler;
+import com.bytechef.component.date.helper.action.DateHelperConvertAction;
+import com.bytechef.component.date.helper.constants.DateHelperConstants;
+import com.bytechef.component.definition.ComponentDefinition;
+import com.google.auto.service.AutoService;
+
+/**
+ * @author Igor Beslic
+ */
+@AutoService(ComponentHandler.class)
+public class DateHelperComponentHandler implements ComponentHandler {
+
+    private static final ComponentDefinition COMPONENT_DEFINITION = component(DateHelperConstants.DATE_HELPER)
+        .title("Date Helper")
+        .description("Helper component for converting technical date values to human friendly formatted expressions.")
+        .icon("path:assets/date-helper.svg")
+        .actions(DateHelperConvertAction.ACTION_DEFINITION);
+
+    @Override
+    public ComponentDefinition getDefinition() {
+        return COMPONENT_DEFINITION;
+    }
+}
