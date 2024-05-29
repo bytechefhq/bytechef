04fcf2a6e76711de7bf440b9291c6ec326e8dd81
Monika Domiter <domitermonika@gmail.com>
2024-05-29 13:43:13 +0200

    932 - Remove @SuppressWarnings("PMD.UnusedFormalParameter")

--- server/libs/modules/components/intercom/src/main/java/com/bytechef/component/intercom/connection/IntercomConnection.java
+++ server/libs/modules/components/intercom/src/main/java/com/bytechef/component/intercom/connection/IntercomConnection.java
@@ -17,17 +17,15 @@
 package com.bytechef.component.intercom.connection;
 
 import static com.bytechef.component.definition.Authorization.ACCESS_TOKEN;
+import static com.bytechef.component.definition.Authorization.ApplyResponse.ofHeaders;
 import static com.bytechef.component.definition.Authorization.CLIENT_ID;
 import static com.bytechef.component.definition.Authorization.CLIENT_SECRET;
 import static com.bytechef.component.definition.ComponentDSL.authorization;
 import static com.bytechef.component.definition.ComponentDSL.connection;
 import static com.bytechef.component.definition.ComponentDSL.string;
 
-import com.bytechef.component.definition.Authorization.ApplyResponse;
 import com.bytechef.component.definition.Authorization.AuthorizationType;
 import com.bytechef.component.definition.ComponentDSL.ModifiableConnectionDefinition;
-import com.bytechef.component.definition.Context;
-import com.bytechef.component.definition.Parameters;
 import java.util.List;
 import java.util.Map;
 
@@ -48,17 +46,12 @@ public class IntercomConnection {
                     string(CLIENT_SECRET)
                         .label("Client secret")
                         .required(true))
-                .apply(IntercomConnection::getApplyResponse)
+                .apply((connectionParameters, context) -> ofHeaders(
+                    Map.of("Accept", List.of("application/json"),
+                        "Authorization", List.of("Bearer " + connectionParameters.getString(ACCESS_TOKEN)))))
                 .authorizationUrl((connection, context) -> "https://app.intercom.com/oauth")
                 .tokenUrl((connection, context) -> "https://api.intercom.io/auth/eagle/token")
                 .scopes((connection, context) -> List.of()));
-
-    @SuppressWarnings("PMD.UnusedFormalParameter")
-    private static ApplyResponse getApplyResponse(Parameters connectionParameters, Context context) {
-        return ApplyResponse.ofHeaders(
-            Map.of("Accept", List.of("application/json"),
-                "Authorization", List.of("Bearer " + connectionParameters.getString(ACCESS_TOKEN))));
-    }
 
     private IntercomConnection() {
     }
