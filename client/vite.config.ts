6085c00d8ae97bce13b1d0fa184d8811ec0b69e7
Ivica Čardić <ivica.cardic@gmail.com>
2024-06-07 17:06:57 +0200

    972 - client - ssl in dev mode

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,38 +1,47 @@
 import react from '@vitejs/plugin-react';
-import {defineConfig} from 'vite';
+import {defineConfig, loadEnv} from 'vite';
 import svgr from 'vite-plugin-svgr';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import basicSsl from '@vitejs/plugin-basic-ssl';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    esbuild: {
-        // https://github.com/vitejs/vite/issues/8644#issuecomment-1159308803
-        logOverride: {'this-is-undefined-in-esm': 'silent'},
-    },
-    optimizeDeps: {
-        esbuildOptions: {
-            target: 'es2020',
+export default ({mode}) => {
+    // Make Vite env vars available.
+    // https://stackoverflow.com/a/66389044
+    process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+
+    const isHttps = () => process.env.VITE_HTTPS === 'true';
+
+    return defineConfig({
+        esbuild: {
+            // https://github.com/vitejs/vite/issues/8644#issuecomment-1159308803
+            logOverride: {'this-is-undefined-in-esm': 'silent'},
         },
-    },
-    plugins: [react(), tsconfigPaths(), svgr()],
-    server: {
-        host: '127.0.0.1',
-        proxy: {
-            '/api': {
-                changeOrigin: true,
-                secure: false,
-                target: 'http://localhost:9555',
-                // rewrite: (path) => path.replace(/^\/api/, ""),
+        optimizeDeps: {
+            esbuildOptions: {
+                target: 'es2020',
             },
         },
-    },
-    test: {
-        coverage: {
-            exclude: ['.vitest/', 'node_modules/', 'src/middleware', '**/*.test.tsx'],
-            reporter: ['html', 'lcov', 'text'],
+        plugins: [react(), tsconfigPaths(), svgr(), isHttps() && basicSsl()],
+        server: {
+            host: '127.0.0.1',
+            proxy: {
+                '/api': {
+                    changeOrigin: true,
+                    secure: false,
+                    target: 'http://localhost:9555',
+                    // rewrite: (path) => path.replace(/^\/api/, ""),
+                },
+            },
         },
-        environment: 'jsdom',
-        globals: true,
-        setupFiles: '.vitest/setup.ts',
-    },
-});
+        test: {
+            coverage: {
+                exclude: ['.vitest/', 'node_modules/', 'src/middleware', '**/*.test.tsx'],
+                reporter: ['html', 'lcov', 'text'],
+            },
+            environment: 'jsdom',
+            globals: true,
+            setupFiles: '.vitest/setup.ts',
+        },
+    });
+};
