70916d0382a29f863938ba3d89da7dd9dace35e7
Ivica Čardić <ivica.cardic@gmail.com>
2024-06-10 07:08:31 +0200

    272 - hubspot - Update openapi spec

--- server/libs/modules/components/hubspot/src/main/java/com/bytechef/component/hubspot/connection/HubspotConnection.java
+++ server/libs/modules/components/hubspot/src/main/java/com/bytechef/component/hubspot/connection/HubspotConnection.java
@@ -45,29 +45,7 @@ public class HubspotConnection {
                         .label("Client Secret")
                         .required(true))
                 .authorizationUrl((connectionParameters, context) -> "https://app.hubspot.com/oauth/authorize")
-                // needs to match the scopes perfectly
-                .scopes((connection, context) -> List.of(
-//                    "crm.dealsplits.read_write", "crm.lists.read",
-//                    "crm.lists.write", "crm.objects.carts.read", "crm.objects.carts.write",
-//                    "crm.objects.commercepayments.read", "crm.objects.companies.read", "crm.objects.companies.write",
-                    "crm.objects.contacts.read", "crm.objects.contacts.write"
-//                    , "crm.objects.custom.read",
-//                    "crm.objects.custom.write", "crm.objects.deals.read", "crm.objects.deals.write",
-//                    "crm.objects.feedback_submissions.read", "crm.objects.goals.read", "crm.objects.invoices.read",
-//                    "crm.objects.leads.read", "crm.objects.leads.write", "crm.objects.line_items.read",
-//                    "crm.objects.line_items.write", "crm.objects.marketing_events.read",
-//                    "crm.objects.marketing_events.write", "crm.objects.orders.read", "crm.objects.orders.write",
-//                    "crm.objects.owners.read", "crm.objects.partner-clients.read", "crm.objects.partner-clients.write",
-//                    "crm.objects.quotes.read", "crm.objects.quotes.write", "crm.objects.subscriptions.read",
-//                    "crm.objects.users.read", "crm.objects.users.write", "crm.pipelines.orders.read",
-//                    "crm.pipelines.orders.write", "crm.schemas.carts.read", "crm.schemas.carts.write",
-//                    "crm.schemas.commercepayments.read", "crm.schemas.companies.read", "crm.schemas.companies.write",
-//                    "crm.schemas.contacts.read", "crm.schemas.contacts.write", "crm.schemas.custom.read",
-//                    "crm.schemas.deals.read", "crm.schemas.deals.write", "crm.schemas.invoices.read",
-//                    "crm.schemas.line_items.read", "crm.schemas.orders.read", "crm.schemas.orders.write",
-//                    "crm.schemas.quotes.read", "crm.schemas.subscriptions.read", "actions", "automation", "files",
-//                    "forms", "tickets"
-                ))
+                .scopes((connection, context) -> List.of("crm.objects.contacts.write", "crm.objects.contacts.read"))
                 .tokenUrl((connectionParameters, context) -> "https://api.hubapi.com/oauth/v1/token"));
 
     private HubspotConnection() {
