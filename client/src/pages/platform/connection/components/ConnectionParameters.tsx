5507e28ae08881379caa6b5f7817ec1decdf900d
Ivica Čardić <ivica.cardic@gmail.com>
2024-06-12 12:15:32 +0200

    1120 - client - Update styling in ConnectionParameters

--- client/src/pages/platform/connection/components/ConnectionParameters.tsx
+++ client/src/pages/platform/connection/components/ConnectionParameters.tsx
@@ -15,20 +15,29 @@ const ConnectionParameters = ({
                 .length > 0
     );
 
+    if (
+        connection.connectionParameters &&
+        Object.values(connection.connectionParameters).every((x) => x === null || x === '') &&
+        connection.authorizationParameters &&
+        Object.values(connection.authorizationParameters).every((x) => x === null || x === '')
+    ) {
+        return <></>;
+    }
+
     return (
         <div className="flex flex-col gap-1">
             <div className="text-sm font-medium">Connection Parameters</div>
 
             <div className="flex flex-col gap-3 text-sm">
-                <div>
+                <div className="flex gap-1">
                     {connectionDefinition.properties &&
                         connection.connectionParameters &&
                         Object.keys(connection.connectionParameters).length > 0 &&
                         connectionDefinition.properties
                             .filter((property) => !!connection.connectionParameters![property.name!])
                             .map((property) => (
-                                <div className="flex" key={property.name}>
-                                    <div className="w-4/12">{property.name}:</div>
+                                <div className="flex w-full" key={property.name}>
+                                    <div className="w-4/12 text-muted-foreground">{property.name}:</div>
 
                                     <div className="col-span-2">{connection.connectionParameters![property.name!]}</div>
                                 </div>
@@ -39,19 +48,19 @@ const ConnectionParameters = ({
                     connection.authorizationParameters &&
                     Object.keys(connection.authorizationParameters).length > 0 &&
                     authorizations.map((authorization) => (
-                        <div key={authorization.name}>
+                        <div className="flex w-full gap-1" key={authorization.name}>
                             <div className="flex">
-                                <div className="w-4/12 font-medium">Authorization:</div>
+                                <div className="w-4/12 font-medium text-muted-foreground">Authorization:</div>
 
-                                <div className="col-span-2 font-medium">{authorization.title}</div>
+                                <div className="col-span-2">{authorization.title}</div>
                             </div>
 
                             {authorization.properties &&
                                 authorization.properties
                                     .filter((property) => !!connection.authorizationParameters![property.name!])
                                     .map((property) => (
-                                        <div className="flex" key={property.name}>
-                                            <div className="w-4/12">{property.name}:</div>
+                                        <div className="flex w-full" key={property.name}>
+                                            <div className="w-4/12 text-muted-foreground">{property.name}:</div>
 
                                             <div className="col-span-2">
                                                 {connection.authorizationParameters![property.name!]}
