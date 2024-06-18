6d270393e8df99fd4a2c8cf9e8c2e0bd4247d185
Ivica Čardić <ivica.cardic@gmail.com>
2024-06-18 07:19:50 +0200

    346 - Define ee version of workspace rest endpoints

--- server/ee/libs/automation/automation-configuration/automation-configuration-rest/build.gradle.kts
+++ server/ee/libs/automation/automation-configuration/automation-configuration-rest/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 val generateOpenAPISpring by tasks.registering(org.openapitools.generator.gradle.plugin.tasks.GenerateTask::class) {
-    apiPackage.set("com.bytechef.platform.connection.web.rest")
+    apiPackage.set("com.bytechef.automation.configuration.web.rest")
     configOptions.set(
         mapOf(
             "dateLibrary" to "java8-localdatetime",
@@ -15,20 +15,22 @@ val generateOpenAPISpring by tasks.registering(org.openapitools.generator.gradle
     generatorName.set("spring")
     inputSpec.set( "$projectDir/openapi.yaml")
     modelNameSuffix.set("Model")
-    modelPackage.set("com.bytechef.platform.connection.web.rest.model")
-    outputDir.set("$projectDir/../platform-connection-rest-api/generated")
+    modelPackage.set("com.bytechef.automation.configuration.web.rest.model")
+    outputDir.set("$projectDir/generated")
     schemaMappings.set(
         mapOf(
-            "Page" to "org.springframework.data.domain.Page"
+            "Workspace" to "com.bytechef.automation.configuration.web.rest.model.WorkspaceModel",
         )
     )
 }
+
+sourceSets.main.get().java.srcDir("$projectDir/generated/src/main/java")
 
 val generateOpenAPITypeScriptFetch by tasks.registering(org.openapitools.generator.gradle.plugin.tasks.GenerateTask::class) {
     generatorName.set("typescript-fetch")
     inputSpec.set("$projectDir/openapi.yaml")
     modelNameSuffix.set("Model")
-    outputDir.set("$rootDir/client/src/shared/middleware/platform/connection")
+    outputDir.set("$rootDir/client/src/shared/middleware/ee/automation/configuration")
     typeMappings.set(
         mapOf(
             "DateTime" to "Date"
@@ -41,11 +43,24 @@ tasks.register("generateOpenAPI") {
 }
 
 dependencies {
-    implementation("org.apache.commons:commons-lang3")
+    annotationProcessor(libs.org.mapstruct.mapstruct.processor)
+    annotationProcessor(libs.org.mapstruct.extensions.spring.mapstruct.spring.extensions)
+
+    compileOnly("jakarta.servlet:jakarta.servlet-api:6.0.0")
+
+    implementation(libs.io.swagger.core.v3.swagger.annotations)
+    implementation(libs.org.openapitools.jackson.databind.nullable)
+    implementation("jakarta.annotation:jakarta.annotation-api")
+    implementation("jakarta.validation:jakarta.validation-api")
+    implementation(libs.org.mapstruct)
+    implementation(libs.org.mapstruct.extensions.spring.mapstruct.spring.annotations)
+    implementation("org.slf4j:slf4j-api")
+    implementation("org.springframework:spring-context")
     implementation("org.springframework:spring-web")
     implementation("org.springframework.boot:spring-boot-autoconfigure")
     implementation(project(":server:libs:core:commons:commons-util"))
-    implementation(project(":server:libs:platform:platform-connection:platform-connection-rest:platform-connection-rest-api"))
+    implementation(project(":server:libs:core:annotation-api"))
+    implementation(project(":server:libs:automation:automation-configuration:automation-configuration-rest:automation-configuration-rest-api"))
 
     testImplementation("org.springframework:spring-webflux")
     testImplementation("org.springframework.boot:spring-boot-starter-web")
