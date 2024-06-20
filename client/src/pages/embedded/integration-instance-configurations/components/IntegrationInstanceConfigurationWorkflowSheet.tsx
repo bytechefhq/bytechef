3681b76cfa23c91960b3af0ec95a0b4c4d118357
Ivica Čardić <ivica.cardic@gmail.com>
2024-06-20 08:37:10 +0200

    1027 - client - Update integration side

--- client/src/pages/embedded/integration-instance-configurations/components/IntegrationInstanceConfigurationWorkflowSheet.tsx
+++ client/src/pages/embedded/integration-instance-configurations/components/IntegrationInstanceConfigurationWorkflowSheet.tsx
@@ -1,6 +1,9 @@
+import LoadingIcon from '@/components/LoadingIcon';
 import {Sheet, SheetContent} from '@/components/ui/sheet';
 import useIntegrationInstanceConfigurationWorkflowSheetStore from '@/pages/embedded/integration-instance-configurations/stores/useIntegrationInstanceConfigurationWorkflowSheetStore';
+import ReadOnlyWorkflowEditor from '@/shared/components/ReadOnlyWorkflow';
 import {useGetWorkflowQuery} from '@/shared/queries/embedded/workflows.queries';
+import {useGetComponentDefinitionsQuery} from '@/shared/queries/platform/componentDefinitions.queries';
 
 const IntegrationInstanceConfigurationWorkflowSheet = () => {
     const {
@@ -11,17 +14,33 @@ const IntegrationInstanceConfigurationWorkflowSheet = () => {
 
     const {data: workflow} = useGetWorkflowQuery(workflowId!, !!workflowId);
 
+    const workflowComponentNames = [
+        ...(workflow?.workflowTriggerComponentNames ?? []),
+        ...(workflow?.workflowTaskComponentNames ?? []),
+    ];
+
+    const {data: componentDefinitions} = useGetComponentDefinitionsQuery(
+        {include: workflowComponentNames},
+        workflowComponentNames !== undefined
+    );
+
     return (
         <Sheet
             onOpenChange={() =>
                 setIntegrationInstanceConfigurationWorkflowSheetOpen(!integrationInstanceConfigurationWorkflowSheetOpen)
             }
-            open={integrationInstanceConfigurationWorkflowSheetOpen}
+            open
         >
-            <SheetContent className="flex w-11/12 gap-0 p-0 sm:max-w-screen-xl">
-                {/*TODO WorkflowEditor in readonly mode*/}
+            <SheetContent className="flex flex-col  bg-gray-100 p-4 sm:max-w-[780px]">
+                <h1 className="text-lg font-semibold">
+                    {workflow?.label} <span className="text-sm font-normal text-gray-500">(read-only)</span>
+                </h1>
 
-                {workflow?.label}
+                {componentDefinitions && workflow ? (
+                    <ReadOnlyWorkflowEditor componentDefinitions={componentDefinitions} workflow={workflow} />
+                ) : (
+                    <LoadingIcon />
+                )}
             </SheetContent>
         </Sheet>
     );
