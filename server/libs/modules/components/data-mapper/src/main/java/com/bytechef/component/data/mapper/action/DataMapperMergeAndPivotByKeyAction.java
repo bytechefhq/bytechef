0b42d31ad83d7e9adae91fc6be556f9ea3634efa
Marko Krišković <marko.kriskovic@protonmail.com>
2024-06-24 11:14:52 +0200

    367 - DataMapperMergeAndPivotByKeyAction implemented

--- server/libs/modules/components/data-mapper/src/main/java/com/bytechef/component/data/mapper/action/DataMapperMergeAndPivotByKeyAction.java
+++ server/libs/modules/components/data-mapper/src/main/java/com/bytechef/component/data/mapper/action/DataMapperMergeAndPivotByKeyAction.java
@@ -19,6 +19,8 @@ package com.bytechef.component.data.mapper.action;
 import static com.bytechef.component.data.mapper.constant.DataMapperConstants.FIELD_KEY;
 import static com.bytechef.component.data.mapper.constant.DataMapperConstants.FIELD_VALUE;
 import static com.bytechef.component.data.mapper.constant.DataMapperConstants.INPUT;
+import static com.bytechef.component.data.mapper.constant.DataMapperConstants.VALUE_KEY;
+import static com.bytechef.component.definition.ComponentDSL.array;
 import static com.bytechef.component.definition.ComponentDSL.object;
 import static com.bytechef.component.definition.ComponentDSL.string;
 
@@ -26,6 +28,11 @@ import com.bytechef.component.definition.ActionContext;
 import com.bytechef.component.definition.ComponentDSL;
 import com.bytechef.component.definition.ComponentDSL.ModifiableActionDefinition;
 import com.bytechef.component.definition.Parameters;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author Ivica Cardic
@@ -36,11 +43,12 @@ public class DataMapperMergeAndPivotByKeyAction {
         .action("mergeAndPivotPropertiesByKey")
         .title("Merge and pivot properties by key")
         .description(
-            "Creates a new object that has the specified field key as kay and an object as value. The object in the value has all properties defined in the input field that have the specified field key. Each one of those input values becomes a property key with the specified field value as property value.")
+            "Creates a new object out of all objects that have the same key as the specified field kay and an object as value. That value of the new object contains values of all properties that share the specified field key as keys and the they all have the specified field value as a value.")
         .properties(
-            object(INPUT)
+            array(INPUT)
                 .label("Input")
-                .description("The input object that contains property keys and values.")
+                .description("An array that contains objects with key-value properties that need do be merged.")
+                .items(object())
                 .required(true),
             string(FIELD_KEY)
                 .label("Field Key")
@@ -53,9 +61,21 @@ public class DataMapperMergeAndPivotByKeyAction {
         .output()
         .perform(DataMapperMergeAndPivotByKeyAction::perform);
 
-    protected static Object perform(
+    protected static Map<String, Object> perform(
         Parameters inputParameters, Parameters connectionParameters, ActionContext context) {
+        List<Object> input = inputParameters.getList(INPUT, Object.class, List.of());
+        String key = inputParameters.getRequiredString(FIELD_KEY);
 
-        return null;
+        Map<Object, Object> objectHashMap = new HashMap<>();
+        for(Object object : input) {
+            for (Map.Entry<String, Object> entry : ((Map<String, Object>)object).entrySet()) {
+                if (entry.getKey().equals(key))
+                    objectHashMap.put(entry.getValue(), inputParameters.getRequiredString(FIELD_VALUE));
+            }
+        }
+
+        Map<String, Object> output = new HashMap<>();
+        output.put(key, objectHashMap);
+        return output;
     }
 }
