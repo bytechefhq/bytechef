25a07611b3dbd73aafdb7d55fff589b7cc1a0eea
Ivica Čardić <ivica.cardic@gmail.com>
2024-06-25 15:34:13 +0200

    1165 - Clean up ProviderException, it should be logic-less as it is in sdk API, move and update logic to RefreshCredentialsUtils

--- sdks/backend/java/component-api/src/main/java/com/bytechef/component/exception/ProviderException.java
+++ sdks/backend/java/component-api/src/main/java/com/bytechef/component/exception/ProviderException.java
@@ -16,152 +16,103 @@
 
 package com.bytechef.component.exception;
 
-import java.util.Objects;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import javax.annotation.Nonnull;
-
 /**
  * @author Igor Beslic
+ * @author Ivica Cardic
  */
-public abstract class ProviderException extends RuntimeException {
+public class ProviderException extends RuntimeException {
 
-    private static final Pattern http4nnPattern = Pattern.compile(
-        "^.*(4\\d\\d)(\\s(Unauthorized)?.*)?$", Pattern.DOTALL);
-    private static final Pattern messagePattern = Pattern.compile(
-        "\\{\"componentName\":\"(\\w+)\",\"exceptionMessage\":\"(.+)\",\"exceptionClass\":\"(.+)\"}", Pattern.DOTALL);
-
-    @Nonnull
-    public static ProviderException fromHttpResponseCode(int code, String message) {
-        return switch (code) {
-            case 400 -> new BadRequestException(message);
-            case 401 -> new AuthorizationFailedException(message);
-            case 403 -> new UnauthorizedRequestException(message);
-            case 404 -> new NotFoundException(message);
-            default -> new GeneralException(message);
-        };
-    }
+    private Integer statusCode;
 
     /**
-     * Returns specialized ProviderException based on content of exception message. Content is scanned for http response
-     * code 4xx.
      *
-     * @param message the content of exception message
-     * @return specialized ProviderException if 4xx pattern is resolved in message, or null if http error code can't be
-     *         resolved
+     * @param message
      */
-    public static ProviderException fromExceptionMessage(@Nonnull String message) {
-        Matcher matcher = http4nnPattern.matcher(message);
-
-        if (matcher.matches() && matcher.group(1) != null) {
-            return fromHttpResponseCode(Integer.parseInt(matcher.group(1)), message);
-        }
-
-        return null;
-    }
-
-    public static ProviderException fromException(@Nonnull Exception exception) {
-        String exceptionMessage = exception.getMessage();
-
-        Matcher matcher = messagePattern.matcher(exception.getMessage());
-
-        if (!matcher.matches()) {
-            throw new UnsupportedOperationException("Unable to convert " + exceptionMessage);
-        }
-
-        AuthorizationFailedException exceptionMessage1 = new AuthorizationFailedException(
-            matcher.group(2));
-
-        exceptionMessage1.withComponentName(matcher.group(1));
-
-        return exceptionMessage1;
-    }
-
-    public static boolean hasAuthorizationFailedExceptionContent(@Nonnull Exception exception) {
-        ProviderException providerException = fromExceptionMessage(exception.getMessage());
-
-        if ((providerException == null) ||
-            !Objects.equals(AuthorizationFailedException.class, providerException.getClass())) {
-
-            return false;
-        }
-
-        return true;
-    }
-
-    public static String getComponentName(@Nonnull Exception exception) {
-        if (exception instanceof AuthorizationFailedException providerException) {
-            return providerException.getComponentName();
-        }
-
-        if (hasAuthorizationFailedExceptionContent(exception)) {
-            Matcher matcher = messagePattern.matcher(exception.getMessage());
-
-            if (matcher.matches()) {
-                return matcher.group(1);
-            }
-        }
-
-        return null;
-    }
-
-    public ProviderException withComponentName(@Nonnull String componentName) {
-        this.componentName = componentName;
-
-        return this;
-    }
-
-    public String getComponentName() {
-        return componentName;
-    }
-
-    @Nonnull
-    @Override
-    public String getMessage() {
-        return String.format("{\"componentName\":\"%s\",\"exceptionMessage\":\"%s\",\"exceptionClass\":\"%s\"}",
-            getComponentName(), super.getMessage(), getClass().getName());
-    }
-
-    private String componentName;
-
     public ProviderException(String message) {
         super(message);
     }
 
-    public static class UnauthorizedRequestException extends ProviderException {
-        public UnauthorizedRequestException(String message) {
-            super(message);
-        }
+    /**
+     *
+     * @param statusCode
+     */
+    public ProviderException(Integer statusCode) {
+        this.statusCode = statusCode;
     }
 
-    public static class AuthorizationFailedException extends ProviderException {
-        public AuthorizationFailedException(String message) {
-            super(message);
-        }
+    /**
+     *
+     * @param statusCode
+     * @param message
+     */
+    public ProviderException(Integer statusCode, String message) {
+        super(message);
+
+        this.statusCode = statusCode;
     }
 
-    public static class AuthenticationFailedException extends ProviderException {
-        public AuthenticationFailedException(String message) {
-            super(message);
-        }
+    /**
+     *
+     * @return
+     */
+    public Integer getStatusCode() {
+        return statusCode;
     }
 
+    /**
+     *
+     */
     public static class BadRequestException extends ProviderException {
+
+        /**
+         *
+         * @param message
+         */
         public BadRequestException(String message) {
-            super(message);
+            super(400, message);
         }
     }
 
+    /**
+     *
+     */
+    public static class ForbiddenException extends ProviderException {
+
+        /**
+         *
+         * @param message
+         */
+        public ForbiddenException(String message) {
+            super(403, message);
+        }
+
+    }
+
+    /**
+     *
+     */
     public static class NotFoundException extends ProviderException {
+
+        /**
+         *
+         * @param message
+         */
         public NotFoundException(String message) {
-            super(message);
+            super(404, message);
         }
     }
 
-    public static class GeneralException extends ProviderException {
-        public GeneralException(String message) {
-            super(message);
+    /**
+     *
+     */
+    public static class UnauthorizedException extends ProviderException {
+
+        /**
+         *
+         * @param message
+         */
+        public UnauthorizedException(String message) {
+            super(401, message);
         }
     }
-
 }
