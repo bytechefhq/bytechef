8cd7ab1af2c6acb5b5e12cc4f6d70dcc5700bfb4
Ivica Čardić <ivica.cardic@gmail.com>
2024-06-26 12:24:50 +0200

    889 - Generate

--- server/libs/platform/platform-configuration/platform-configuration-rest/platform-configuration-rest-api/generated/src/main/java/com/bytechef/platform/configuration/web/rest/model/WorkflowTaskModel.java
+++ server/libs/platform/platform-configuration/platform-configuration-rest/platform-configuration-rest-api/generated/src/main/java/com/bytechef/platform/configuration/web/rest/model/WorkflowTaskModel.java
@@ -28,7 +28,7 @@ import jakarta.annotation.Generated;
 
 @Schema(name = "WorkflowTask", description = "Represents a definition of a workflow task.")
 @JsonTypeName("WorkflowTask")
-@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2024-06-26T11:09:40.503215+02:00[Europe/Zagreb]", comments = "Generator version: 7.5.0")
+@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2024-06-26T12:24:38.500893+02:00[Europe/Zagreb]", comments = "Generator version: 7.5.0")
 public class WorkflowTaskModel {
 
   @Valid
