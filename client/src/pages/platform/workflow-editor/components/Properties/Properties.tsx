4c3c98721db8a8fab444a31658b2f36cea5cdd04
Krešimir Čoko <kresimircoko@gmail.com>
2024-07-02 08:10:20 +0200

    1035 - reduce text size of Advanced Properties collapsible

--- client/src/pages/platform/workflow-editor/components/Properties/Properties.tsx
+++ client/src/pages/platform/workflow-editor/components/Properties/Properties.tsx
@@ -51,7 +51,7 @@ const Properties = ({control, customClassName, formState, operationName, path, p
             {!!advancedProperties.length && (
                 <Collapsible className="flex w-full flex-col justify-center">
                     <CollapsibleTrigger className="heading-tertiary mx-4 flex items-center justify-center rounded-md px-4 py-2 text-center hover:bg-gray-100">
-                        <h2>Advanced Properties</h2>
+                        <h2 className="text-sm">Advanced Properties</h2>
 
                         <ChevronDownIcon className="ml-2 size-4" />
                     </CollapsibleTrigger>
