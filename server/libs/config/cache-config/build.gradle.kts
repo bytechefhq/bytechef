c20d59db64eefdc3c195be12dc23d171bf52d791
Ivica Čardić <ivica.cardic@gmail.com>
2024-07-02 21:34:48 +0200

    520 - Add multitenancy to cache keys

--- server/libs/config/cache-config/build.gradle.kts
+++ server/libs/config/cache-config/build.gradle.kts
@@ -1,4 +1,6 @@
 dependencies {
+    implementation("org.apache.commons:commons-lang3")
     implementation("org.springframework.boot:spring-boot-autoconfigure")
     implementation("org.springframework.data:spring-data-redis")
+    implementation(project(":server:libs:core:tenant:tenant-api"))
 }
