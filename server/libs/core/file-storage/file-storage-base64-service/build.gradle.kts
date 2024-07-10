a2c69a61819dcf8acccb1bcc0365933b3366d504
Ivica Čardić <ivica.cardic@gmail.com>
2024-07-10 12:52:32 +0200

    477 - Reorganize dependencies

--- server/libs/core/file-storage/file-storage-base64-service/build.gradle.kts
+++ server/libs/core/file-storage/file-storage-base64-service/build.gradle.kts
@@ -1,7 +1,6 @@
 dependencies {
-    api(project(":server:libs:core:file-storage:file-storage-api"))
-
     implementation("org.slf4j:slf4j-api")
     implementation("org.springframework.boot:spring-boot-autoconfigure")
+    implementation(project(":server:libs:core:file-storage:file-storage-api"))
     implementation(project(":server:libs:core:commons:commons-util"))
 }
--- server/libs/core/file-storage/file-storage-filesystem-service/build.gradle.kts
+++ server/libs/core/file-storage/file-storage-filesystem-service/build.gradle.kts
@@ -1,11 +1,7 @@
 dependencies {
-    api(project(":server:libs:core:file-storage:file-storage-api"))
-
     implementation("org.apache.commons:commons-lang3")
     implementation("org.slf4j:slf4j-api")
-    implementation("jakarta.annotation:jakarta.annotation-api")
     implementation("org.springframework.boot:spring-boot-autoconfigure")
-    implementation(project(":server:libs:core:commons:commons-data"))
-    implementation(project(":server:libs:core:commons:commons-util"))
+    implementation(project(":server:libs:core:file-storage:file-storage-api"))
     implementation(project(":server:libs:core:tenant:tenant-api"))
 }
