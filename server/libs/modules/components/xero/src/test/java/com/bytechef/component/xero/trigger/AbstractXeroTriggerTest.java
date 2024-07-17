70641458680e9aa81d3dbe4d12e2abcc7d7ea648
Monika Domiter <domitermonika@gmail.com>
2024-07-17 10:08:16 +0200

    696 - fix tests

---
+++ server/libs/modules/components/xero/src/test/java/com/bytechef/component/xero/trigger/AbstractXeroTriggerTest.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2023-present ByteChef Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef.component.xero.trigger;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.mockStatic;
+
+import com.bytechef.component.definition.Parameters;
+import com.bytechef.component.definition.TriggerContext;
+import com.bytechef.component.definition.TriggerDefinition.HttpHeaders;
+import com.bytechef.component.definition.TriggerDefinition.HttpParameters;
+import com.bytechef.component.definition.TriggerDefinition.WebhookBody;
+import com.bytechef.component.definition.TriggerDefinition.WebhookMethod;
+import com.bytechef.component.xero.util.XeroUtils;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.mockito.MockedStatic;
+
+/**
+ * @author Monika Domiter
+ */
+public abstract class AbstractXeroTriggerTest {
+
+    protected WebhookBody mockedBody = mock(WebhookBody.class);
+    protected HttpHeaders mockedHttpHeaders = mock(HttpHeaders.class);
+    protected HttpParameters mockedHttpParameters = mock(HttpParameters.class);
+    protected WebhookMethod mockedMethod = mock(WebhookMethod.class);
+    protected Object mockedObject = mock(Object.class);
+    protected Parameters mockedParameters = mock(Parameters.class);
+    protected TriggerContext mockedTriggerContext = mock(TriggerContext.class);
+    protected MockedStatic<XeroUtils> xeroUtilsMockedStatic;
+
+    @BeforeEach
+    public void beforeEach() {
+        xeroUtilsMockedStatic = mockStatic(XeroUtils.class);
+    }
+
+    @AfterEach
+    public void afterEach() {
+        xeroUtilsMockedStatic.close();
+    }
+
+}
---
+++ server/libs/modules/components/xero/src/test/java/com/bytechef/component/xero/trigger/XeroNewInvoiceTriggerTest.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2023-present ByteChef Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef.component.xero.trigger;
+
+import static com.bytechef.component.xero.constant.XeroConstants.ACCREC;
+import static com.bytechef.component.xero.constant.XeroConstants.INVOICE;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.bytechef.component.xero.util.XeroUtils;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Monika Domiter
+ */
+class XeroNewInvoiceTriggerTest extends AbstractXeroTriggerTest {
+
+    @Test
+    void testStaticWebhookRequest() {
+        xeroUtilsMockedStatic
+            .when(() -> XeroUtils.getCreatedObject(mockedBody, mockedTriggerContext, INVOICE, ACCREC))
+            .thenReturn(mockedObject);
+
+        Object result = XeroNewInvoiceTrigger.staticWebhookRequest(mockedParameters, mockedHttpHeaders,
+            mockedHttpParameters, mockedBody, mockedMethod, mockedTriggerContext);
+
+        assertEquals(mockedObject, result);
+    }
+
+}
