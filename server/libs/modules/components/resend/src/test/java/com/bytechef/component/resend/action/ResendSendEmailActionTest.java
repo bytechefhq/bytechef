e392e313c99abe293eac23252eec7aeb7a608162
Monika Domiter <domitermonika@gmail.com>
2024-07-18 12:15:08 +0200

    471 - Fix tests

--- server/libs/modules/components/resend/src/test/java/com/bytechef/component/resend/action/ResendSendEmailActionTest.java
+++ server/libs/modules/components/resend/src/test/java/com/bytechef/component/resend/action/ResendSendEmailActionTest.java
@@ -16,7 +16,7 @@
 
 package com.bytechef.component.resend.action;
 
-import static com.bytechef.component.definition.Authorization.TOKEN;
+import static com.bytechef.component.resend.constant.ResendConstants.ATTACHMENTS;
 import static com.bytechef.component.resend.constant.ResendConstants.BCC;
 import static com.bytechef.component.resend.constant.ResendConstants.CC;
 import static com.bytechef.component.resend.constant.ResendConstants.FROM;
@@ -28,28 +28,21 @@ import static com.bytechef.component.resend.constant.ResendConstants.TAGS;
 import static com.bytechef.component.resend.constant.ResendConstants.TEXT;
 import static com.bytechef.component.resend.constant.ResendConstants.TO;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.mockConstruction;
 import static org.mockito.Mockito.mockStatic;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import com.bytechef.component.definition.ActionContext;
+import com.bytechef.component.definition.Context.Http;
+import com.bytechef.component.definition.Context.TypeReference;
 import com.bytechef.component.definition.Parameters;
 import com.bytechef.component.resend.util.ResendUtils;
-import com.resend.Resend;
-import com.resend.core.exception.ResendException;
-import com.resend.services.emails.Emails;
-import com.resend.services.emails.model.Attachment;
-import com.resend.services.emails.model.CreateEmailOptions;
-import com.resend.services.emails.model.CreateEmailResponse;
-import com.resend.services.emails.model.Tag;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
-import org.mockito.MockedConstruction;
 import org.mockito.MockedStatic;
 
 /**
@@ -57,90 +50,88 @@ import org.mockito.MockedStatic;
  */
 class ResendSendEmailActionTest {
 
-    private final ArgumentCaptor<CreateEmailOptions> createEmailOptionsArgumentCaptor =
-        ArgumentCaptor.forClass(CreateEmailOptions.class);
+    private final ArgumentCaptor<Http.Body> bodyArgumentCaptor = ArgumentCaptor.forClass(Http.Body.class);
     private final ActionContext mockedContext = mock(ActionContext.class);
-    private final CreateEmailResponse mockedCreateEmailResponse = mock(CreateEmailResponse.class);
-    private final Emails mockedEmails = mock(Emails.class);
+    private final Http.Executor mockedExecutor = mock(Http.Executor.class);
+    private final Object mockedObject = mock(Object.class);
     private final Parameters mockedParameters = mock(Parameters.class);
+    private final Http.Response mockedResponse = mock(Http.Response.class);
+    private final List<Map<String, String>> attachments = List.of(Map.of("filename", "fileName"));
 
     @Test
-    void testPerform() throws ResendException {
-        List<String> toList = List.of("to@mail.com");
-        List<String> bccList = List.of("bcc@mail.com");
-        List<String> ccList = List.of("cc@mail.com");
-        List<String> replyToList = List.of("reply@mail.com");
-        List<Tag> tags = List.of(Tag.builder()
-            .name("category")
-            .value("confirm_email")
-            .build());
-        Map<String, String> headerMap = Map.of("X-Entity-Ref-ID", "123456789");
-        List<Attachment> attachments = List.of(new Attachment.Builder().fileName("fileName")
-            .build());
+    void testPerform() {
+        Map<String, Object> propertyStubsMap = createPropertyStubsMap();
 
-        when(mockedParameters.get(TOKEN))
-            .thenReturn("token");
         when(mockedParameters.getRequiredString(FROM))
-            .thenReturn("from@mail.com");
-        when(mockedParameters.getList(TO, String.class, List.of()))
-            .thenReturn(toList);
-        when(mockedParameters.getString(SUBJECT))
-            .thenReturn("subject");
-        when(mockedParameters.getList(BCC, String.class, List.of()))
-            .thenReturn(bccList);
-        when(mockedParameters.getList(CC, String.class, List.of()))
-            .thenReturn(ccList);
-        when(mockedParameters.getList(REPLY_TO, String.class, List.of()))
-            .thenReturn(replyToList);
+            .thenReturn((String) propertyStubsMap.get(FROM));
+        when(mockedParameters.getRequiredList(TO, String.class))
+            .thenReturn((List<String>) propertyStubsMap.get(TO));
+        when(mockedParameters.getRequiredString(SUBJECT))
+            .thenReturn((String) propertyStubsMap.get(SUBJECT));
+        when(mockedParameters.getList(BCC, String.class))
+            .thenReturn((List<String>) propertyStubsMap.get(BCC));
+        when(mockedParameters.getList(CC, String.class))
+            .thenReturn((List<String>) propertyStubsMap.get(CC));
+        when(mockedParameters.getList(REPLY_TO, String.class))
+            .thenReturn((List<String>) propertyStubsMap.get(REPLY_TO));
         when(mockedParameters.getString(HTML))
-            .thenReturn("html");
+            .thenReturn((String) propertyStubsMap.get(HTML));
         when(mockedParameters.getString(TEXT))
-            .thenReturn("text");
-        when(mockedParameters.getMap(HEADERS, String.class, Map.of()))
-            .thenReturn(headerMap);
-        when(mockedParameters.getList(TAGS, Tag.class, List.of()))
-            .thenReturn(tags);
+            .thenReturn((String) propertyStubsMap.get(TEXT));
+        when(mockedParameters.getMap(HEADERS, String.class))
+            .thenReturn((Map<String, String>) propertyStubsMap.get(HEADERS));
+        when(mockedParameters.getFileEntries(ATTACHMENTS, List.of()))
+            .thenReturn(List.of());
+        when((List<Map<String, String>>) mockedParameters.getList(TAGS))
+            .thenReturn((List<Map<String, String>>) propertyStubsMap.get(TAGS));
 
-        try (MockedConstruction<Resend> resendMockedConstruction = mockConstruction(
-            Resend.class,
-            (mock, context) -> {
-                when(mock.emails()).thenReturn(mockedEmails);
-                when(mockedEmails.send(createEmailOptionsArgumentCaptor.capture()))
-                    .thenReturn(mockedCreateEmailResponse);
-            })) {
+        when(mockedContext.http(any()))
+            .thenReturn(mockedExecutor);
+        when(mockedExecutor.body(bodyArgumentCaptor.capture()))
+            .thenReturn(mockedExecutor);
+        when(mockedExecutor.configuration(any()))
+            .thenReturn(mockedExecutor);
+        when(mockedExecutor.execute())
+            .thenReturn(mockedResponse);
+        when(mockedResponse.getBody(any(TypeReference.class)))
+            .thenReturn(mockedObject);
 
-            try (MockedStatic<ResendUtils> resendUtilsMockedStatic = mockStatic(ResendUtils.class)) {
-                resendUtilsMockedStatic
-                    .when(() -> ResendUtils.getAttachments(mockedParameters, mockedContext))
-                    .thenReturn(attachments);
+        try (MockedStatic<ResendUtils> resendUtilsMockedStatic = mockStatic(ResendUtils.class)) {
+            resendUtilsMockedStatic
+                .when(() -> ResendUtils.getAttachments(List.of(), mockedContext))
+                .thenReturn(attachments);
 
-                CreateEmailResponse response = ResendSendEmailAction.perform(
-                    mockedParameters, mockedParameters, mockedContext);
+            Object result = ResendSendEmailAction.perform(mockedParameters, mockedParameters, mockedContext);
 
-                List<Resend> resends = resendMockedConstruction.constructed();
+            assertEquals(mockedObject, result);
 
-                assertEquals(1, resends.size());
-                assertEquals(mockedCreateEmailResponse, response);
+            Http.Body bodyValue = bodyArgumentCaptor.getValue();
 
-                Resend resend = resends.getFirst();
-
-                verify(resend, times(1)).emails();
-                verify(resend.emails(), times(1)).send(createEmailOptionsArgumentCaptor.capture());
-
-                CreateEmailOptions sendEmailRequest = createEmailOptionsArgumentCaptor.getValue();
-
-                assertEquals("from@mail.com", sendEmailRequest.getFrom());
-                assertEquals(toList, sendEmailRequest.getTo());
-                assertEquals("subject", sendEmailRequest.getSubject());
-                assertEquals(bccList, sendEmailRequest.getBcc());
-                assertEquals(ccList, sendEmailRequest.getCc());
-                assertEquals(replyToList, sendEmailRequest.getReplyTo());
-                assertEquals("html", sendEmailRequest.getHtml());
-                assertEquals("text", sendEmailRequest.getText());
-                assertEquals(attachments, sendEmailRequest.getAttachments());
-                assertEquals(headerMap, sendEmailRequest.getHeaders());
-                assertEquals(tags, sendEmailRequest.getTags());
-            }
+            assertEquals(propertyStubsMap, bodyValue.getContent());
         }
+    }
+
+    private Map<String, Object> createPropertyStubsMap() {
+        Map<String, Object> propertyStubsMap = new HashMap<>();
+
+        List<Map<String, String>> tags = List.of(Map.of(
+            "name", "category",
+            "value", "confirm_email"));
+
+        Map<String, String> headerMap = Map.of("X-Entity-Ref-ID", "123456789");
+
+        propertyStubsMap.put(FROM, "from@mail.com");
+        propertyStubsMap.put(TO, List.of("to@mail.com"));
+        propertyStubsMap.put(SUBJECT, "subject");
+        propertyStubsMap.put(BCC, List.of("bcc@mail.com"));
+        propertyStubsMap.put(CC, List.of("cc@mail.com"));
+        propertyStubsMap.put(REPLY_TO, List.of("reply@mail.com"));
+        propertyStubsMap.put(HTML, "html");
+        propertyStubsMap.put(TEXT, "text");
+        propertyStubsMap.put(HEADERS, headerMap);
+        propertyStubsMap.put(ATTACHMENTS, attachments);
+        propertyStubsMap.put(TAGS, tags);
+
+        return propertyStubsMap;
     }
 }
