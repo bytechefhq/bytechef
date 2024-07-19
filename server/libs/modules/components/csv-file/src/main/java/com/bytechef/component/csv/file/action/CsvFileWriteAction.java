6eb8ea69d2cd99a49f28330fef772f6763390845
Marko Krišković <marko.kriskovic@protonmail.com>
2024-07-19 16:37:04 +0200

    rewritten CsvFileWriteAction

--- server/libs/modules/components/csv-file/src/main/java/com/bytechef/component/csv/file/action/CsvFileWriteAction.java
+++ server/libs/modules/components/csv-file/src/main/java/com/bytechef/component/csv/file/action/CsvFileWriteAction.java
@@ -16,30 +16,37 @@
 
 package com.bytechef.component.csv.file.action;
 
+import static com.bytechef.component.csv.file.constant.CsvFileConstants.FILE_ENTRY;
+import static com.bytechef.component.csv.file.constant.CsvFileConstants.ROWS;
 import static com.bytechef.component.definition.ComponentDSL.action;
 import static com.bytechef.component.definition.ComponentDSL.array;
 import static com.bytechef.component.definition.ComponentDSL.bool;
 import static com.bytechef.component.definition.ComponentDSL.dateTime;
 import static com.bytechef.component.definition.ComponentDSL.fileEntry;
 import static com.bytechef.component.definition.ComponentDSL.number;
+import static com.bytechef.component.definition.ComponentDSL.object;
 import static com.bytechef.component.definition.ComponentDSL.string;
 
 import com.bytechef.component.csv.file.constant.CsvFileConstants;
 import com.bytechef.component.definition.ActionContext;
-import com.bytechef.component.definition.ComponentDSL;
 import com.bytechef.component.definition.ComponentDSL.ModifiableActionDefinition;
-import com.bytechef.component.definition.Context;
+import com.bytechef.component.definition.Context.TypeReference;
 import com.bytechef.component.definition.FileEntry;
 import com.bytechef.component.definition.Parameters;
-import com.fasterxml.jackson.databind.SequenceWriter;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
+import com.fasterxml.jackson.databind.MappingIterator;
+import com.fasterxml.jackson.dataformat.csv.CsvSchema;
+
+import java.io.BufferedReader;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.PrintWriter;
+import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 /**
  * @author Ivica Cardic
@@ -50,53 +57,65 @@ public class CsvFileWriteAction {
         .title("Write to file")
         .description("Writes the data to a csv file.")
         .properties(
-            array(CsvFileConstants.ROWS)
+            array(ROWS)
                 .label("Rows")
                 .description("The array of objects to write to the file.")
                 .required(true)
-                .items(ComponentDSL.object()
+                .items(object()
                     .additionalProperties(bool(), dateTime(), number(), string())),
-            string(CsvFileConstants.FILENAME)
-                .label("Filename")
-                .description(
-                    "Filename to set for binary data. By default, \"file.csv\" will be used.")
-                .defaultValue("file.csv")
-                .advancedOption(true))
+            fileEntry(FILE_ENTRY)
+                .label("File")
+                .description("File you want to write to.")
+                .required(true))
         .outputSchema(fileEntry())
         .perform(CsvFileWriteAction::perform);
 
-    protected static FileEntry perform(
+    private CsvFileWriteAction() {}
+
+    protected static Object perform(
         Parameters inputParameters, Parameters connectionParameters, ActionContext context) throws IOException {
 
         List<Map<String, ?>> rows =
-            inputParameters.getList(CsvFileConstants.ROWS, new Context.TypeReference<>() {}, List.of());
+            inputParameters.getList(ROWS, new TypeReference<>() {}, List.of());
+        FileEntry requiredFileEntry = inputParameters.getRequiredFileEntry(CsvFileConstants.FILE_ENTRY);
 
-        try (InputStream inputStream = new ByteArrayInputStream(write(rows))) {
-            return context.file(file -> file.storeContent("file.csv", inputStream));
-        }
+        write(rows, requiredFileEntry, context);
+
+        return requiredFileEntry;
     }
 
-    private static byte[] write(List<Map<String, ?>> rows) throws IOException {
-        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-        boolean headerRow = false;
+    private static void write(List<Map<String, ?>> rows, FileEntry requiredFileEntry, ActionContext context) throws IOException {
+        InputStream inputStream = context.file(file -> file.getStream(requiredFileEntry));
+        Set<String> keys = null;
+        try (BufferedReader bufferedReader = new BufferedReader(
+            new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
+            CsvSchema headerSchema = CsvSchema
+                .emptySchema()
+                .withHeader()
+                .withColumnSeparator(',');
 
-        try (PrintWriter printWriter = new PrintWriter(byteArrayOutputStream, false, StandardCharsets.UTF_8)) {
-            SequenceWriter sequenceWriter = CsvFileConstants.CSV_MAPPER.writer()
-                .writeValues(printWriter);
+            MappingIterator<Map<String, String>> iterator = CsvFileConstants.CSV_MAPPER
+                .readerForMapOf(String.class)
+                .with(headerSchema)
+                .readValues(bufferedReader);
 
+            keys = iterator.next().keySet();
+        }
+
+        FileOutputStream outputStream = new FileOutputStream(requiredFileEntry.getUrl());
+        if(keys!=null) {
             for (Map<String, ?> row : rows) {
-                if (!headerRow) {
-                    headerRow = true;
-
-                    sequenceWriter.write(row.keySet());
+                Map<String, String> tempMap = keys.stream()
+                    .collect(Collectors.toMap(key->key, key->""));
+                for (Map.Entry<String, ?> enrty: row.entrySet()){
+                    if(keys.contains(enrty.getKey())) tempMap.put(enrty.getKey(), enrty.getValue().toString());
                 }
 
-                sequenceWriter.write(row.values());
+                for(Map.Entry<String, String> entry : tempMap.entrySet()) {
+                    outputStream.write(entry.getValue().getBytes(StandardCharsets.UTF_8));
+                }
             }
-
-            sequenceWriter.close();
         }
-
-        return byteArrayOutputStream.toByteArray();
     }
+
 }
