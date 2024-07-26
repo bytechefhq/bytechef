8785c3fdd44c3fe62d3cfb9854e57204ae76418e
Monika Kušter <domitermonika@gmail.com>
2024-07-26 09:42:55 +0200

    557 - update outputSchema

--- server/libs/modules/components/pipedrive/src/main/java/com/bytechef/component/pipedrive/trigger/PipedriveNewActivityTrigger.java
+++ server/libs/modules/components/pipedrive/src/main/java/com/bytechef/component/pipedrive/trigger/PipedriveNewActivityTrigger.java
@@ -16,13 +16,9 @@
 
 package com.bytechef.component.pipedrive.trigger;
 
-import static com.bytechef.component.definition.ComponentDSL.array;
-import static com.bytechef.component.definition.ComponentDSL.bool;
-import static com.bytechef.component.definition.ComponentDSL.dateTime;
 import static com.bytechef.component.definition.ComponentDSL.integer;
 import static com.bytechef.component.definition.ComponentDSL.object;
 import static com.bytechef.component.definition.ComponentDSL.string;
-import static com.bytechef.component.definition.ComponentDSL.time;
 import static com.bytechef.component.definition.ComponentDSL.trigger;
 import static com.bytechef.component.pipedrive.constant.PipedriveConstants.ADDED;
 import static com.bytechef.component.pipedrive.constant.PipedriveConstants.CURRENT;
@@ -43,6 +39,7 @@ import java.util.Map;
 
 /**
  * @author Ivica Cardic
+ * @author Monika Kušter
  */
 public class PipedriveNewActivityTrigger {
 
@@ -53,170 +50,14 @@ public class PipedriveNewActivityTrigger {
         .outputSchema(
             object()
                 .properties(
-                    integer("id"),
-                    integer("company_id"),
-                    integer("user_id"),
-                    bool("done"),
-                    string("type"),
-                    string("reference_type"),
-                    integer("reference_id"),
-                    string("conference_meeting_client"),
-                    string("conference_meeting_url"),
-                    string("conference_meeting_id"),
-                    string("due_date"),
-                    time("due_time"),
-                    time("duration"),
-                    bool("busy_flag"),
-                    dateTime("add_time"),
-                    dateTime("marked_as_done_time"),
-                    dateTime("last_notification_time"),
-                    integer("last_notification_user_id"),
-                    integer("notification_language_id"),
-                    string("subject"),
+                    string("type_name"),
                     string("public_description"),
-                    string("calendar_sync_include_context"),
-                    string("location"),
-                    integer("org_id"),
-                    integer("person_id"),
-                    integer("deal_id"),
-                    string("lead_id"),
-                    bool("active_flag"),
-                    dateTime("update_time"),
-                    integer("update_user_id"),
-                    string("gcal_event_id"),
-                    string("google_calendar_id"),
-                    string("google_calendar_etag"),
-                    string("source_timezone"),
-                    string("rec_rule"),
-                    string("rec_rule_extension"),
-                    integer("rec_master_activity_id"),
-                    array("series"),
-                    string("note"),
-                    integer("created_by_user_id"),
-                    string("location_subpremise"),
-                    string("location_street_number"),
-                    string("location_route"),
-                    string("location_sublocality"),
-                    string("location_locality"),
-                    string("location_admin_area_level_1"),
-                    string("location_admin_area_level_2"),
-                    string("location_country"),
-                    string("location_postal_code"),
-                    string("location_formatted_address"),
-                    array("attendees").items(
-                        object().properties(
-                            string("email_address"),
-                            integer("is_organizer"),
-                            string("name"),
-                            integer("person_id"),
-                            string("status"),
-                            string("user_id"))),
-                    array("participants").items(
-                        object().properties(
-                            integer("person_id"),
-                            bool("primary_flag"))),
-                    string("org_name"),
-                    string("person_name"),
-                    string("deal_title"),
+                    string("subject"),
+                    string("type"),
+                    integer(ID),
                     string("owner_name"),
-                    string("person_dropbox_bcc"),
-                    string("deal_dropbox_bcc"),
-                    integer("assigned_to_user_id"),
-                    object("file").properties(
-                        string("id"),
-                        string("clean_name"),
-                        string("url"))))
-        .sampleOutput(
-            """
-                {
-                    "id": 8,
-                    "company_id": 22122,
-                    "user_id": 1234,
-                    "done": false,
-                    "type": "deadline",
-                    "reference_type": "scheduler-service",
-                    "reference_id": 7,
-                    "conference_meeting_client": "871b8bc88d3a1202",
-                    "conference_meeting_url": "https://pipedrive.zoom.us/link",
-                    "conference_meeting_id": "01758746701",
-                    "due_date": "2020-06-09",
-                    "due_time": "10:00",
-                    "duration": "01:00",
-                    "busy_flag": true,
-                    "add_time": "2020-06-08 12:37:56",
-                    "marked_as_done_time": "2020-08-08 08:08:38",
-                    "last_notification_time": "2020-08-08 12:37:56",
-                    "last_notification_user_id": 7655,
-                    "notification_language_id": 1,
-                    "subject": "Deadline",
-                    "public_description": "This is a description",
-                    "calendar_sync_include_context": "",
-                    "location": "Mustamäe tee 3, Tallinn, Estonia",
-                    "org_id": 5,
-                    "person_id": 1101,
-                    "deal_id": 300,
-                    "lead_id": "46c3b0e1-db35-59ca-1828-4817378dff71",
-                    "project_id": null,
-                    "active_flag": true,
-                    "update_time": "2020-08-08 12:37:56",
-                    "update_user_id": 5596,
-                    "gcal_event_id": "",
-                    "google_calendar_id": "",
-                    "google_calendar_etag": "",
-                    "source_timezone": "",
-                    "rec_rule": "RRULE:FREQ=WEEKLY;BYDAY=WE",
-                    "rec_rule_extension": "",
-                    "rec_master_activity_id": 1,
-                    "series": [],
-                    "note": "A note for the activity",
-                    "created_by_user_id": 1234,
-                    "location_subpremise": "",
-                    "location_street_number": "3",
-                    "location_route": "Mustamäe tee",
-                    "location_sublocality": "Kristiine",
-                    "location_locality": "Tallinn",
-                    "location_admin_area_level_1": "Harju maakond",
-                    "location_admin_area_level_2": "",
-                    "location_country": "Estonia",
-                    "location_postal_code": "10616",
-                    "location_formatted_address": "Mustamäe tee 3, 10616 Tallinn, Estonia",
-                    "attendees":
-                    [
-                        {
-                            "email_address": "attendee@pipedrivemail.com",
-                            "is_organizer": 0,
-                            "name": "Attendee",
-                            "person_id": 25312,
-                            "status": "noreply",
-                            "user_id": null
-                        }
-                    ],
-                    "participants":
-                    [
-                        {
-                            "person_id": 17985,
-                            "primary_flag": false
-                        },
-                        {
-                            "person_id": 1101,
-                            "primary_flag": true
-                        }
-                    ],
-                    "org_name": "Organization",
-                    "person_name": "Person",
-                    "deal_title": "Deal",
-                    "owner_name": "Creator",
-                    "person_dropbox_bcc": "company@pipedrivemail.com",
-                    "deal_dropbox_bcc": "company+deal300@pipedrivemail.com",
-                    "assigned_to_user_id": 1235,
-                    "file":
-                    {
-                        "id": "376892,",
-                        "clean_name": "Audio 10:55:07.m4a",
-                        "url": "https://pipedrive-files.s3-eu-west-1.amazonaws.com/Audio-recording.m4a"
-                    }
-                }
-                """)
+                    integer("user_id"),
+                    integer("company_id")))
         .dynamicWebhookDisable(PipedriveNewActivityTrigger::dynamicWebhookDisable)
         .dynamicWebhookEnable(PipedriveNewActivityTrigger::dynamicWebhookEnable)
         .dynamicWebhookRequest(PipedriveNewActivityTrigger::dynamicWebhookRequest);
--- server/libs/modules/components/pipedrive/src/main/java/com/bytechef/component/pipedrive/trigger/PipedriveNewPersonTrigger.java
+++ server/libs/modules/components/pipedrive/src/main/java/com/bytechef/component/pipedrive/trigger/PipedriveNewPersonTrigger.java
@@ -16,18 +16,11 @@
 
 package com.bytechef.component.pipedrive.trigger;
 
-import static com.bytechef.component.definition.ComponentDSL.array;
-import static com.bytechef.component.definition.ComponentDSL.bool;
-import static com.bytechef.component.definition.ComponentDSL.date;
-import static com.bytechef.component.definition.ComponentDSL.dateTime;
-import static com.bytechef.component.definition.ComponentDSL.integer;
-import static com.bytechef.component.definition.ComponentDSL.object;
-import static com.bytechef.component.definition.ComponentDSL.string;
-import static com.bytechef.component.definition.ComponentDSL.time;
 import static com.bytechef.component.definition.ComponentDSL.trigger;
 import static com.bytechef.component.pipedrive.constant.PipedriveConstants.ADDED;
 import static com.bytechef.component.pipedrive.constant.PipedriveConstants.CURRENT;
 import static com.bytechef.component.pipedrive.constant.PipedriveConstants.ID;
+import static com.bytechef.component.pipedrive.constant.PipedriveConstants.PERSON_OUTPUT_PROPERTY;
 
 import com.bytechef.component.definition.ComponentDSL.ModifiableTriggerDefinition;
 import com.bytechef.component.definition.Context.TypeReference;
@@ -44,6 +37,7 @@ import java.util.Map;
 
 /**
  * @author Ivica Cardic
+ * @author Monika Kušter
  */
 public class PipedriveNewPersonTrigger {
 
@@ -51,176 +45,7 @@ public class PipedriveNewPersonTrigger {
         .title("New Person")
         .description("Trigger off whenever a new person is added.")
         .type(TriggerType.DYNAMIC_WEBHOOK)
-        .outputSchema(
-            object()
-                .properties(
-                    integer("id"),
-                    integer("company_id"),
-                    object("owner_id").properties(
-                        integer("id"),
-                        string("name"),
-                        string("email"),
-                        integer("has_pic"),
-                        string("pic_hash"),
-                        bool("active_flag"),
-                        integer("value")),
-                    object("org_id").properties(
-                        integer("id"),
-                        integer("people_count"),
-                        integer("owner_id"),
-                        string("address"),
-                        bool("active_flag"),
-                        string("cc_email"),
-                        integer("value")),
-                    string("name"),
-                    string("first_name"),
-                    string("last_name"),
-                    integer("open_deals_count"),
-                    integer("related_open_deals_count"),
-                    integer("closed_deals_count"),
-                    integer("related_closed_deals_count"),
-                    integer("participant_open_deals_count"),
-                    integer("participant_closed_deals_count"),
-                    integer("email_messages_count"),
-                    integer("activities_count"),
-                    integer("done_activities_count"),
-                    integer("undone_activities_count"),
-                    integer("files_count"),
-                    integer("notes_count"),
-                    integer("followers_count"),
-                    integer("won_deals_count"),
-                    integer("related_won_deals_count"),
-                    integer("lost_deals_count"),
-                    integer("related_lost_deals_count"),
-                    bool("active_flag"),
-                    array("phone").items(
-                        object().properties(
-                            string("label"),
-                            string("value"),
-                            bool("primary"))),
-                    array("email").items(
-                        object().properties(
-                            string("label"),
-                            string("value"),
-                            bool("primary"))),
-                    string("primary_email"),
-                    string("first_char"),
-                    dateTime("update_time"),
-                    dateTime("add_time"),
-                    string("visible_to"),
-                    string("marketing_status"),
-                    object("picture_id").properties(
-                        string("item_type"),
-                        integer("item_id"),
-                        bool("active_flag"),
-                        dateTime("add_time"),
-                        dateTime("update_time"),
-                        integer("added_by_user_id"),
-                        object("pictures").properties(
-                            string("128"),
-                            string("512")),
-                        integer("value")),
-                    date("next_activity_date"),
-                    time("next_activity_time"),
-                    integer("next_activity_id"),
-                    integer("last_activity_id"),
-                    date("last_activity_date"),
-                    dateTime("last_incoming_mail_time"),
-                    dateTime("last_outgoing_mail_time"),
-                    integer("label"),
-                    string("org_name"),
-                    string("owner_name"),
-                    string("cc_email")))
-        .sampleOutput(
-            """
-                {
-                    "id": 1,
-                    "company_id": 12,
-                    "owner_id": {
-                      "id": 123,
-                      "name": "Jane Doe",
-                      "email": "jane@pipedrive.com",
-                      "has_pic": 1,
-                      "pic_hash": "2611ace8ac6a3afe2f69ed56f9e08c6b",
-                      "active_flag": true,
-                      "value": 123
-                    },
-                    "org_id": {
-                      "name": "Org Name",
-                      "people_count": 1,
-                      "owner_id": 123,
-                      "address": "Mustamäe tee 3a, 10615 Tallinn",
-                      "active_flag": true,
-                      "cc_email": "org@pipedrivemail.com",
-                      "value": 1234
-                    },
-                    "name": "Will Smith",
-                    "first_name": "Will",
-                    "last_name": "Smith",
-                    "open_deals_count": 2,
-                    "related_open_deals_count": 2,
-                    "closed_deals_count": 3,
-                    "related_closed_deals_count": 3,
-                    "participant_open_deals_count": 1,
-                    "participant_closed_deals_count": 1,
-                    "email_messages_count": 1,
-                    "activities_count": 1,
-                    "done_activities_count": 1,
-                    "undone_activities_count": 2,
-                    "files_count": 2,
-                    "notes_count": 2,
-                    "followers_count": 3,
-                    "won_deals_count": 3,
-                    "related_won_deals_count": 3,
-                    "lost_deals_count": 1,
-                    "related_lost_deals_count": 1,
-                    "active_flag": true,
-                    "phone": [
-                      {
-                        "value": "12345",
-                        "primary": true,
-                        "label": "work"
-                      }
-                    ],
-                    "email": [
-                      {
-                        "value": "12345@email.com",
-                        "primary": true,
-                        "label": "work"
-                      }
-                    ],
-                    "primary_email": "12345@email.com",
-                    "first_char": "w",
-                    "update_time": "2020-05-08 05:30:20",
-                    "add_time": "2017-10-18 13:23:07",
-                    "visible_to": "3",
-                    "marketing_status": "no_consent",
-                    "picture_id": {
-                      "item_type": "person",
-                      "item_id": 25,
-                      "active_flag": true,
-                      "add_time": "2020-09-08 08:17:52",
-                      "update_time": "0000-00-00 00:00:00",
-                      "added_by_user_id": 967055,
-                      "pictures": {
-                        "128": "https://pipedrive-profile-pics.s3.example.com/f8893852574273f2747bf6ef09d11cfb4ac8f269_128.jpg",
-                        "512": "https://pipedrive-profile-pics.s3.example.com/f8893852574273f2747bf6ef09d11cfb4ac8f269_512.jpg"
-                      },
-                      "value": 4
-                    },
-                    "next_activity_date": "2019-11-29",
-                    "next_activity_time": "11:30:00",
-                    "next_activity_id": 128,
-                    "last_activity_id": 34,
-                    "last_activity_date": "2019-11-28",
-                    "last_incoming_mail_time": "2019-05-29 18:21:42",
-                    "last_outgoing_mail_time": "2019-05-30 03:45:35",
-                    "label": 1,
-                    "org_name": "Organization name",
-                    "owner_name": "Jane Doe",
-                    "cc_email": "org@pipedrivemail.com"
-                  }
-                """)
+        .outputSchema(PERSON_OUTPUT_PROPERTY)
         .dynamicWebhookDisable(PipedriveNewPersonTrigger::dynamicWebhookDisable)
         .dynamicWebhookEnable(PipedriveNewPersonTrigger::dynamicWebhookEnable)
         .dynamicWebhookRequest(PipedriveNewPersonTrigger::dynamicWebhookRequest);
