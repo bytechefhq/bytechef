8bcc63a3c22999e3c482151a22094c7da7bab91b
Monika Kušter <domitermonika@gmail.com>
2024-07-26 09:42:55 +0200

    557 - update outputSchema

--- server/libs/modules/components/pipedrive/src/main/java/com/bytechef/component/pipedrive/constant/PipedriveConstants.java
+++ server/libs/modules/components/pipedrive/src/main/java/com/bytechef/component/pipedrive/constant/PipedriveConstants.java
@@ -16,15 +16,71 @@
 
 package com.bytechef.component.pipedrive.constant;
 
+import static com.bytechef.component.definition.ComponentDSL.array;
+import static com.bytechef.component.definition.ComponentDSL.bool;
+import static com.bytechef.component.definition.ComponentDSL.integer;
+import static com.bytechef.component.definition.ComponentDSL.object;
+import static com.bytechef.component.definition.ComponentDSL.string;
+
+import com.bytechef.component.definition.ComponentDSL.ModifiableObjectProperty;
+
 /**
  * @author Monika Kušter
  */
 public final class PipedriveConstants {
 
     public static final String ADDED = "added";
+    public static final String CC_EMAIL = "cc_email";
     public static final String CURRENT = "current";
+    public static final String EMAIL_MESSAGES_COUNT = "email_messages_count";
     public static final String ID = "id";
+    public static final String OWNER_NAME = "owner_name";
     public static final String UPDATED = "updated";
+    public static final String VALUE = "value";
+
+    public static final ModifiableObjectProperty DEAL_OUTPUT_PROPERTY = object()
+        .properties(
+            integer(EMAIL_MESSAGES_COUNT),
+            string(CC_EMAIL),
+            integer("id"),
+            integer("person_id"),
+            string(OWNER_NAME),
+            string("status"),
+            string("title"),
+            string("currency"),
+            integer(VALUE));
+
+    public static final ModifiableObjectProperty ORGANIZATION_OUTPUT_PROPERTY = object()
+        .properties(
+            integer(EMAIL_MESSAGES_COUNT),
+            string(CC_EMAIL),
+            integer("owner_id"),
+            integer("id"),
+            string(OWNER_NAME),
+            string("name"),
+            integer("company_id"));
+
+    public static final ModifiableObjectProperty PERSON_OUTPUT_PROPERTY = object()
+        .properties(
+            integer(EMAIL_MESSAGES_COUNT),
+            string(CC_EMAIL),
+            integer("owner_id"),
+            integer("id"),
+            string(OWNER_NAME),
+            array("phone")
+                .items(
+                    object()
+                        .properties(
+                            string(VALUE),
+                            bool("primary"))),
+            string("name"),
+            array("email")
+                .items(
+                    object()
+                        .properties(
+                            string(VALUE),
+                            bool("primary"))),
+            integer("company_id"));
 
     private PipedriveConstants() {
     }
--- server/libs/modules/components/pipedrive/src/main/java/com/bytechef/component/pipedrive/trigger/PipedriveUpdatedOrganizationTrigger.java
+++ server/libs/modules/components/pipedrive/src/main/java/com/bytechef/component/pipedrive/trigger/PipedriveUpdatedOrganizationTrigger.java
@@ -16,16 +16,10 @@
 
 package com.bytechef.component.pipedrive.trigger;
 
-import static com.bytechef.component.definition.ComponentDSL.bool;
-import static com.bytechef.component.definition.ComponentDSL.date;
-import static com.bytechef.component.definition.ComponentDSL.dateTime;
-import static com.bytechef.component.definition.ComponentDSL.integer;
-import static com.bytechef.component.definition.ComponentDSL.object;
-import static com.bytechef.component.definition.ComponentDSL.string;
-import static com.bytechef.component.definition.ComponentDSL.time;
 import static com.bytechef.component.definition.ComponentDSL.trigger;
 import static com.bytechef.component.pipedrive.constant.PipedriveConstants.CURRENT;
 import static com.bytechef.component.pipedrive.constant.PipedriveConstants.ID;
+import static com.bytechef.component.pipedrive.constant.PipedriveConstants.ORGANIZATION_OUTPUT_PROPERTY;
 import static com.bytechef.component.pipedrive.constant.PipedriveConstants.UPDATED;
 
 import com.bytechef.component.definition.ComponentDSL.ModifiableTriggerDefinition;
@@ -43,6 +37,7 @@ import java.util.Map;
 
 /**
  * @author Ivica Cardic
+ * @author Monika Kušter
  */
 public class PipedriveUpdatedOrganizationTrigger {
 
@@ -50,146 +45,7 @@ public class PipedriveUpdatedOrganizationTrigger {
         .title("Updated Organization")
         .description("Trigger off whenever an existing organization is updated.")
         .type(TriggerType.DYNAMIC_WEBHOOK)
-        .outputSchema(
-            object()
-                .properties(
-                    integer("id"),
-                    integer("company_id"),
-                    object("owner_id").properties(
-                        integer("id"),
-                        string("name"),
-                        string("email"),
-                        integer("has_pic"),
-                        string("pic_hash"),
-                        bool("active_flag"),
-                        integer("value")),
-                    string("name"),
-                    integer("open_deals_count"),
-                    integer("related_open_deals_count"),
-                    integer("closed_deals_count"),
-                    integer("related_closed_deals_count"),
-                    integer("email_messages_count"),
-                    integer("people_count"),
-                    integer("activities_count"),
-                    integer("done_activities_count"),
-                    integer("undone_activities_count"),
-                    integer("files_count"),
-                    integer("notes_count"),
-                    integer("followers_count"),
-                    integer("won_deals_count"),
-                    integer("related_won_deals_count"),
-                    integer("lost_deals_count"),
-                    integer("related_lost_deals_count"),
-                    bool("active_flag"),
-                    object("picture_id").properties(
-                        string("item_type"),
-                        integer("item_id"),
-                        bool("active_flag"),
-                        dateTime("add_time"),
-                        dateTime("update_time"),
-                        integer("added_by_user_id"),
-                        object("pictures").properties(
-                            string("128"),
-                            string("512")),
-                        integer("value")),
-                    string("country_code"),
-                    string("first_char"),
-                    dateTime("update_time"),
-                    dateTime("add_time"),
-                    string("visible_to"),
-                    date("next_activity_date"),
-                    time("next_activity_time"),
-                    integer("next_activity_id"),
-                    integer("last_activity_id"),
-                    date("last_activity_date"),
-                    integer("label"),
-                    string("address"),
-                    string("address_subpremise"),
-                    string("address_street_number"),
-                    string("address_route"),
-                    string("address_sublocality"),
-                    string("address_locality"),
-                    string("address_admin_area_level_1"),
-                    string("address_admin_area_level_2"),
-                    string("address_country"),
-                    string("address_postal_code"),
-                    string("address_formatted_address"),
-                    string("owner_name"),
-                    string("cc_email")))
-        .sampleOutput(
-            """
-                {
-                    "id": 1,
-                    "company_id": 77,
-                    "owner_id":
-                    {
-                        "id": 10,
-                        "name": "Will Smith",
-                        "email": "will.smith@pipedrive.com",
-                        "has_pic": 0,
-                        "pic_hash": "2611ace8ac6a3afe2f69ed56f9e08c6b",
-                        "active_flag": true,
-                        "value": 10
-                    },
-                    "name": "Bolt",
-                    "open_deals_count": 1,
-                    "related_open_deals_count": 2,
-                    "closed_deals_count": 3,
-                    "related_closed_deals_count": 1,
-                    "email_messages_count": 2,
-                    "people_count": 1,
-                    "activities_count": 2,
-                    "done_activities_count": 1,
-                    "undone_activities_count": 0,
-                    "files_count": 0,
-                    "notes_count": 0,
-                    "followers_count": 1,
-                    "won_deals_count": 0,
-                    "related_won_deals_count": 0,
-                    "lost_deals_count": 0,
-                    "related_lost_deals_count": 0,
-                    "active_flag": true,
-                    "picture_id":
-                    {
-                        "item_type": "person",
-                        "item_id": 25,
-                        "active_flag": true,
-                        "add_time": "2020-09-08 08:17:52",
-                        "update_time": "0000-00-00 00:00:00",
-                        "added_by_user_id": 967055,
-                        "pictures":
-                        {
-                            "128": "https://pipedrive-profile-pics.s3.example.com/f8893852574273f2747bf6ef09d11cf14ac8f269_128.jpg",
-                            "512": "https://pipedrive-profile-pics.s3.example.com/f8893852574273f2747bf6ef09d11cfb2ac8f269_512.jpg"
-                        },
-                        "value": 101
-                    },
-                    "country_code": "USA",
-                    "first_char": "b",
-                    "update_time": "2020-09-08 12:14:11",
-                    "add_time": "2020-02-25 10:04:08",
-                    "visible_to": "3",
-                    "next_activity_date": "2019-11-29",
-                    "next_activity_time": "11:30:00",
-                    "next_activity_id": 128,
-                    "last_activity_id": 34,
-                    "last_activity_date": "2019-11-28",
-                    "label": 7,
-                    "address": "Mustamäe tee 3a, 10615 Tallinn",
-                    "address_subpremise": "",
-                    "address_street_number": "3a",
-                    "address_route": "Mustamäe tee",
-                    "address_sublocality": "Kristiine",
-                    "address_locality": "Tallinn",
-                    "address_admin_area_level_1": "Harju maakond",
-                    "address_admin_area_level_2": "",
-                    "address_country": "Estonia",
-                    "address_postal_code": "10616",
-                    "address_formatted_address": "Mustamäe tee 3a, 10616 Tallinn, Estonia",
-                    "owner_name": "John Doe",
-                    "cc_email": "org@pipedrivemail.com"
-                }
-                """)
+        .outputSchema(ORGANIZATION_OUTPUT_PROPERTY)
         .dynamicWebhookDisable(PipedriveUpdatedOrganizationTrigger::dynamicWebhookDisable)
         .dynamicWebhookEnable(PipedriveUpdatedOrganizationTrigger::dynamicWebhookEnable)
         .dynamicWebhookRequest(PipedriveUpdatedOrganizationTrigger::dynamicWebhookRequest);
