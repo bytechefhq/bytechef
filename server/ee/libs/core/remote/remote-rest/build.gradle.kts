b105a581315e83f86f7737e63ee5e0c99dc49312
Ivica Čardić <ivica.cardic@gmail.com>
2024-07-30 08:02:40 +0200

    182 - Switch gateway to spring-gateway-mvc

--- server/ee/libs/core/remote/remote-rest/build.gradle.kts
+++ server/ee/libs/core/remote/remote-rest/build.gradle.kts
@@ -2,5 +2,6 @@ dependencies {
     compileOnly("jakarta.servlet:jakarta.servlet-api")
 
     implementation("org.springframework:spring-webmvc")
+    implementation("org.springframework.security:spring-security-web")
     implementation(project(":server:libs:core:tenant:tenant-api"))
 }
