a4a38ca5aa6c8e8e2c58a85ba8ed64953e8f5141
F1nn-T <finn.trefzer@outlook.de>
2024-07-31 15:19:44 +0200

    1317 - cleanup

--- client/src/pages/platform/workflow-editor/components/Properties/components/PropertyInput/PropertyInput.stories.tsx
+++ client/src/pages/platform/workflow-editor/components/Properties/components/PropertyInput/PropertyInput.stories.tsx
@@ -14,9 +14,7 @@ export default {
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 /* eslint-disable @typescript-eslint/no-explicit-any */
-const Template: StoryFn<typeof PropertyInput> = (args: any) => (
-    <PropertyInput {...args} />
-);
+const Template: StoryFn<typeof PropertyInput> = (args: any) => <PropertyInput {...args} />;
 
 export const Normal = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
