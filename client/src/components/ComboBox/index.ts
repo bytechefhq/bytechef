6f3be916c72c8c8ccd5e6ba9ad0bc0d6a90e31e1
F1nn-T <finn.trefzer@outlook.de>
2024-07-31 17:56:58 +0200

    1317 - fixed formatting

--- client/src/components/ComboBox/index.ts
+++ client/src/components/ComboBox/index.ts
@@ -1,5 +1,4 @@
-import ComboBox from './ComboBox';
-import {ComboBoxItemType, ComboBoxProps} from './ComboBox';
+import ComboBox, {ComboBoxItemType, ComboBoxProps} from './ComboBox';
 
 export type {ComboBoxItemType, ComboBoxProps};
 export default ComboBox;
