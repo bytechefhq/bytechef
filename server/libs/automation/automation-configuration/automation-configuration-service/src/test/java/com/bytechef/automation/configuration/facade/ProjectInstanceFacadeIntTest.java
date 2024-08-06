1c3b629d76c57f47099ff13e26e5ce2d2d82644e
igorbeslic <igor.beslic@bitnaprednost.hr>
2024-08-06 22:09:50 +0200

    1307 automation-configuration - TEST - start test of projectInstance - there is more work here to do, let's start with this

--- server/libs/automation/automation-configuration/automation-configuration-service/src/test/java/com/bytechef/automation/configuration/facade/ProjectInstanceFacadeIntTest.java
+++ server/libs/automation/automation-configuration/automation-configuration-service/src/test/java/com/bytechef/automation/configuration/facade/ProjectInstanceFacadeIntTest.java
@@ -16,10 +16,28 @@
 
 package com.bytechef.automation.configuration.facade;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.bytechef.atlas.configuration.repository.WorkflowCrudRepository;
+import com.bytechef.automation.configuration.ProjectInstanceFacadeHelper;
 import com.bytechef.automation.configuration.config.ProjectIntTestConfiguration;
+import com.bytechef.automation.configuration.domain.Workspace;
+import com.bytechef.automation.configuration.dto.ProjectDTO;
+import com.bytechef.automation.configuration.dto.ProjectInstanceDTO;
+import com.bytechef.automation.configuration.repository.ProjectRepository;
+import com.bytechef.automation.configuration.repository.ProjectWorkflowRepository;
+import com.bytechef.automation.configuration.repository.WorkspaceRepository;
+import com.bytechef.platform.category.repository.CategoryRepository;
+import com.bytechef.platform.configuration.facade.WorkflowFacade;
+import com.bytechef.platform.constant.Environment;
+import com.bytechef.platform.tag.repository.TagRepository;
 import com.bytechef.test.config.testcontainers.PostgreSQLContainerConfiguration;
+import java.util.List;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 
@@ -34,6 +52,58 @@ import org.springframework.context.annotation.Import;
 @Import(PostgreSQLContainerConfiguration.class)
 public class ProjectInstanceFacadeIntTest {
 
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Autowired
+    private ProjectFacade projectFacade;
+
+    @Autowired
+    private ProjectRepository projectRepository;
+
+    @Autowired
+    private ProjectInstanceFacade projectInstanceFacade;
+
+    @Autowired
+    private ProjectWorkflowRepository projectWorkflowRepository;
+
+    @Autowired
+    TagRepository tagRepository;
+
+    @Autowired
+    private WorkflowFacade workflowFacade;
+
+    @Autowired
+    private WorkflowCrudRepository workflowRepository;
+
+    private Workspace workspace;
+
+    @Autowired
+    private WorkspaceRepository workspaceRepository;
+
+    ProjectInstanceFacadeHelper projectFacadeInstanceHelper;
+
+    @AfterEach
+    public void afterEach() {
+        projectWorkflowRepository.deleteAll();
+        projectRepository.deleteAll();
+        workspaceRepository.deleteAll();
+
+        categoryRepository.deleteAll();
+        tagRepository.deleteAll();
+
+    }
+
+    @BeforeEach
+    public void beforeEach() {
+        workspace = workspaceRepository.save(new Workspace("test"));
+
+        projectFacadeInstanceHelper = new ProjectInstanceFacadeHelper(
+            categoryRepository, projectFacade, projectRepository, projectInstanceFacade, projectWorkflowRepository,
+            tagRepository,
+            workflowFacade, workflowRepository, workspace, workspaceRepository);
+    }
+
     @Disabled
     @Test
     public void testCreateProjectInstance() {
@@ -46,10 +116,21 @@ public class ProjectInstanceFacadeIntTest {
         // TODO
     }
 
-    @Disabled
     @Test
     public void testDeleteProjectInstance() {
-        // TODO
+        ProjectDTO projectDTO = projectFacadeInstanceHelper.createProject(workspace.getId());
+        ProjectInstanceDTO projectInstanceDTO =
+            projectFacadeInstanceHelper.createProjectInstance(workspace.getId(), projectDTO);
+
+        ProjectInstanceDTO projectInstanceToDelete = projectInstanceFacade.getProjectInstance(projectInstanceDTO.id());
+
+        projectInstanceFacade.deleteProjectInstance(projectInstanceToDelete.id());
+
+        List<ProjectInstanceDTO> workspaceProjectInstances = projectInstanceFacade
+            .getWorkspaceProjectInstances(workspace.getId(), Environment.TEST, projectInstanceToDelete.projectId(),
+                null);
+
+        assertThat(workspaceProjectInstances).hasSize(0);
     }
 
     @Disabled
