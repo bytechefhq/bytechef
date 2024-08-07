d7c29afcb3c7715e8ea684fc7d9a9db96b446975
Monika Kušter <domitermonika@gmail.com>
2024-08-07 08:25:10 +0200

    1013 - update actions, add dynamic options

---
+++ server/libs/modules/components/clickup/src/main/java/com/bytechef/component/clickup/constant/ClickupConstants.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2023-present ByteChef Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef.component.clickup.constant;
+
+import static com.bytechef.component.definition.ComponentDSL.number;
+
+import com.bytechef.component.clickup.util.ClickupUtils;
+import com.bytechef.component.definition.ComponentDSL.ModifiableNumberProperty;
+import com.bytechef.component.definition.OptionsDataSource.ActionOptionsFunction;
+
+/**
+ * @author Monika Kušter
+ */
+public class ClickupConstants {
+
+    public static final String FOLDER_ID = "folderId";
+    public static final String ID = "id";
+    public static final String NAME = "name";
+    public static final String SPACE_ID = "spaceId";
+    public static final String WORKSPACE_ID = "workspaceId";
+
+    public static final ModifiableNumberProperty FOLDER_ID_PROPERTY = number(FOLDER_ID)
+        .label("Folder")
+        .options((ActionOptionsFunction<String>) ClickupUtils::getFolderIdOptions)
+        .optionsLookupDependsOn(SPACE_ID, WORKSPACE_ID)
+        .required(false);
+
+    public static final ModifiableNumberProperty SPACE_ID_PROPERTY = number(SPACE_ID)
+        .label("Space")
+        .options((ActionOptionsFunction<String>) ClickupUtils::getSpaceIdOptions)
+        .optionsLookupDependsOn(WORKSPACE_ID)
+        .required(true);
+
+    public static final ModifiableNumberProperty WORKSPACE_ID_PROPERTY = number(WORKSPACE_ID)
+        .label("Workspace")
+        .options((ActionOptionsFunction<String>) ClickupUtils::getWorkspaceIdOptions)
+        .required(true);
+
+    private ClickupConstants() {
+    }
+}
---
+++ server/libs/modules/components/clickup/src/main/java/com/bytechef/component/clickup/util/ClickupUtils.java
@@ -0,0 +1,110 @@
+/*
+ * Copyright 2023-present ByteChef Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef.component.clickup.util;
+
+import static com.bytechef.component.clickup.constant.ClickupConstants.FOLDER_ID;
+import static com.bytechef.component.clickup.constant.ClickupConstants.ID;
+import static com.bytechef.component.clickup.constant.ClickupConstants.NAME;
+import static com.bytechef.component.clickup.constant.ClickupConstants.SPACE_ID;
+import static com.bytechef.component.clickup.constant.ClickupConstants.WORKSPACE_ID;
+import static com.bytechef.component.definition.ComponentDSL.option;
+
+import com.bytechef.component.definition.ActionContext;
+import com.bytechef.component.definition.Context;
+import com.bytechef.component.definition.Context.Http;
+import com.bytechef.component.definition.Context.TypeReference;
+import com.bytechef.component.definition.Option;
+import com.bytechef.component.definition.Parameters;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author Monika Kušter
+ */
+public class ClickupUtils {
+
+    private ClickupUtils() {
+    }
+
+    public static List<Option<String>> getAllListIdOptions(
+        Parameters inputParameters, Parameters connectionParameters, Map<String, String> dependencyPaths,
+        String searchText, ActionContext context) {
+
+        List<Option<String>> options = new ArrayList<>();
+        String folderId = inputParameters.getString(FOLDER_ID);
+        if (folderId != null) {
+            options.addAll(getListsWithinFolder(context, folderId));
+        }
+
+        options.addAll(getFolderlessLists(context, inputParameters.getRequiredString(SPACE_ID)));
+
+        return options;
+    }
+
+    private static List<Option<String>> getListsWithinFolder(ActionContext context, String folderId) {
+
+        return getOptions(fetchDataFromHttpEndpoint(context, "/folder/" + folderId + "/list"), "lists");
+    }
+
+    private static List<Option<String>> getFolderlessLists(ActionContext context, String spaceId) {
+        return getOptions(fetchDataFromHttpEndpoint(context, "/space/" + spaceId + "/list"), "lists");
+    }
+
+    public static List<Option<String>> getFolderIdOptions(
+        Parameters inputParameters, Parameters connectionParameters, Map<String, String> dependencyPaths,
+        String searchText, ActionContext context) {
+
+        return getOptions(
+            fetchDataFromHttpEndpoint(context, "/space/" + inputParameters.getRequiredString(SPACE_ID) + "/folder"),
+            "folders");
+    }
+
+    public static List<Option<String>> getSpaceIdOptions(
+        Parameters inputParameters, Parameters connectionParameters, Map<String, String> dependencyPaths,
+        String searchText, ActionContext context) {
+
+        return getOptions(
+            fetchDataFromHttpEndpoint(context, "/team/" + inputParameters.getRequiredString(WORKSPACE_ID) + "/space"),
+            "spaces");
+    }
+
+    public static List<Option<String>> getWorkspaceIdOptions(
+        Parameters inputParameters, Parameters connectionParameters, Map<String, String> dependencyPaths,
+        String searchText, Context context) {
+
+        return getOptions(fetchDataFromHttpEndpoint(context, "/team"), "teams");
+    }
+
+    private static List<Option<String>> getOptions(Map<String, List<Map<String, Object>>> body, String resource) {
+        List<Option<String>> options = new ArrayList<>();
+
+        for (Map<String, Object> map : body.get(resource)) {
+            options.add(option((String) map.get(NAME), (String) map.get(ID)));
+        }
+
+        return options;
+    }
+
+    private static Map<String, List<Map<String, Object>>> fetchDataFromHttpEndpoint(Context context, String path) {
+        return context
+            .http(http -> http.get(path))
+            .configuration(Http.responseType(Http.ResponseType.JSON))
+            .execute()
+            .getBody(new TypeReference<>() {});
+    }
+}
