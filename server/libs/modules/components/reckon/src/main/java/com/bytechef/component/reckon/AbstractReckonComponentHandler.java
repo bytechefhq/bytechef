c0f53c0a7eb93adb9787ed8c8a1618311a023dee
Monika Kušter <domitermonika@gmail.com>
2024-08-08 15:00:59 +0200

    1017 - initial Reckon connector

---
+++ server/libs/modules/components/reckon/src/main/java/com/bytechef/component/reckon/AbstractReckonComponentHandler.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2023-present ByteChef Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef.component.reckon;
+
+import static com.bytechef.component.definition.ComponentDSL.component;
+
+import com.bytechef.component.OpenApiComponentHandler;
+import com.bytechef.component.definition.ComponentDefinition;
+import com.bytechef.component.reckon.action.ReckonCreateContactAction;
+import com.bytechef.component.reckon.action.ReckonCreateInvoiceAction;
+import com.bytechef.component.reckon.action.ReckonCreatePaymentAction;
+import com.bytechef.component.reckon.connection.ReckonConnection;
+
+/**
+ * Provides the base implementation for the REST based component.
+ *
+ * @generated
+ */
+public abstract class AbstractReckonComponentHandler implements OpenApiComponentHandler {
+    private final ComponentDefinition componentDefinition = modifyComponent(
+        component("reckon")
+            .title("Reckon")
+            .description("Reckon is an accounting software used for financial management and bookkeeping tasks."))
+                .actions(modifyActions(ReckonCreateContactAction.ACTION_DEFINITION,
+                    ReckonCreateInvoiceAction.ACTION_DEFINITION, ReckonCreatePaymentAction.ACTION_DEFINITION))
+                .connection(modifyConnection(ReckonConnection.CONNECTION_DEFINITION))
+                .triggers(getTriggers());
+
+    @Override
+    public ComponentDefinition getDefinition() {
+        return componentDefinition;
+    }
+}
---
+++ server/libs/modules/components/reckon/src/main/java/com/bytechef/component/reckon/action/ReckonCreateContactAction.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2023-present ByteChef Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef.component.reckon.action;
+
+import static com.bytechef.component.OpenApiComponentHandler.PropertyType;
+import static com.bytechef.component.definition.ComponentDSL.action;
+import static com.bytechef.component.definition.ComponentDSL.object;
+import static com.bytechef.component.definition.ComponentDSL.string;
+import static com.bytechef.component.definition.Context.Http.BodyContentType;
+import static com.bytechef.component.definition.Context.Http.ResponseType;
+
+import com.bytechef.component.definition.ComponentDSL;
+import java.util.Map;
+
+/**
+ * Provides a list of the component actions.
+ *
+ * @generated
+ */
+public class ReckonCreateContactAction {
+    public static final ComponentDSL.ModifiableActionDefinition ACTION_DEFINITION = action("createContact")
+        .title("Create Contact")
+        .description("Creates a new Contact.")
+        .metadata(
+            Map.of(
+                "method", "POST",
+                "path", "/{bookId}/contacts", "bodyContentType", BodyContentType.JSON, "mimeType", "application/json"
+
+            ))
+        .properties(string("bookId").label("Book")
+            .description("Book where new contact will be created.")
+            .required(true)
+            .metadata(
+                Map.of(
+                    "type", PropertyType.PATH)),
+            object("__item").properties(string("name").maxLength(100)
+                .label("Name")
+                .description("The name of the contact.")
+                .required(true))
+                .label("Contact")
+                .metadata(
+                    Map.of(
+                        "type", PropertyType.BODY)))
+        .outputSchema(object().properties(object("body").properties(string("id").required(false))
+            .required(false))
+            .metadata(
+                Map.of(
+                    "responseType", ResponseType.JSON)));
+
+    private ReckonCreateContactAction() {
+    }
+}
