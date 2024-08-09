783617a0be7f4cb1cf7562cd30b4c6cecd66fbee
Monika Kušter <domitermonika@gmail.com>
2024-08-09 08:33:22 +0200

    1017 - add dynamic options

--- server/libs/modules/components/reckon/src/main/java/com/bytechef/component/reckon/ReckonComponentHandler.java
+++ server/libs/modules/components/reckon/src/main/java/com/bytechef/component/reckon/ReckonComponentHandler.java
@@ -16,10 +16,23 @@
 
 package com.bytechef.component.reckon;
 
+import static com.bytechef.component.reckon.constant.ReckonConstants.BOOK_ID;
+
 import com.bytechef.component.OpenApiComponentHandler;
+import com.bytechef.component.definition.ActionDefinition;
 import com.bytechef.component.definition.ComponentCategory;
 import com.bytechef.component.definition.ComponentDSL.ModifiableComponentDefinition;
+import com.bytechef.component.definition.ComponentDSL.ModifiableObjectProperty;
+import com.bytechef.component.definition.ComponentDSL.ModifiableProperty;
+import com.bytechef.component.definition.ComponentDSL.ModifiableStringProperty;
+import com.bytechef.component.definition.OptionsDataSource.ActionOptionsFunction;
+import com.bytechef.component.definition.Property.ValueProperty;
+import com.bytechef.component.reckon.util.ReckonUtils;
+import com.bytechef.definition.BaseProperty;
 import com.google.auto.service.AutoService;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 /**
  * @author Monika Kušter
@@ -33,6 +46,31 @@ public class ReckonComponentHandler extends AbstractReckonComponentHandler {
             .customAction(true)
             .icon("path:assets/reckon.svg")
             .categories(ComponentCategory.ACCOUNTING);
+    }
+
+    @Override
+    public ModifiableProperty<?> modifyProperty(
+        ActionDefinition actionDefinition, ModifiableProperty<?> modifiableProperty) {
+
+        if (Objects.equals(modifiableProperty.getName(), BOOK_ID)) {
+            ((ModifiableStringProperty) modifiableProperty)
+                .options((ActionOptionsFunction<String>) ReckonUtils::getBookIdOptions);
+        } else if (Objects.equals(modifiableProperty.getName(), "__item")) {
+            Optional<List<? extends ValueProperty<?>>> propertiesOptional =
+                ((ModifiableObjectProperty) modifiableProperty).getProperties();
+
+            for (BaseProperty baseProperty : propertiesOptional.get()) {
+                if (Objects.equals(baseProperty.getName(), "customer")) {
+                    ((ModifiableStringProperty) baseProperty)
+                        .options((ActionOptionsFunction<String>) ReckonUtils::getCustomerOptions);
+                } else if (Objects.equals(baseProperty.getName(), "supplier")) {
+                    ((ModifiableStringProperty) baseProperty)
+                        .options((ActionOptionsFunction<String>) ReckonUtils::getSupplierOptions);
+                }
+            }
+        }
+
+        return modifiableProperty;
     }
 
 }
