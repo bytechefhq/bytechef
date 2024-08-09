de7da39466802a054687f6f1063ff850322ad70e
Monika Kušter <domitermonika@gmail.com>
2024-08-09 13:57:18 +0200

    1323 - tests

---
+++ server/libs/modules/components/todoist/src/test/java/com/bytechef/component/todoist/util/TodoistUtilsTest.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2023-present ByteChef Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef.component.todoist.util;
+
+import static com.bytechef.component.definition.ComponentDSL.option;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import com.bytechef.component.definition.ActionContext;
+import com.bytechef.component.definition.Context.Http;
+import com.bytechef.component.definition.Context.TypeReference;
+import com.bytechef.component.definition.Option;
+import com.bytechef.component.definition.Parameters;
+import java.util.List;
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Monika Kušter
+ */
+class TodoistUtilsTest {
+
+    private final Http.Executor mockedExecutor = mock(Http.Executor.class);
+    private final Parameters mockedParameters = mock(Parameters.class);
+    private final Http.Response mockedResponse = mock(Http.Response.class);
+    private final ActionContext mockedActionContext = mock(ActionContext.class);
+
+    @Test
+    void testGetOptions() throws Exception {
+        when(mockedActionContext.http(any()))
+            .thenReturn(mockedExecutor);
+        when(mockedExecutor.configuration(any()))
+            .thenReturn(mockedExecutor);
+        when(mockedExecutor.execute())
+            .thenReturn(mockedResponse);
+        when(mockedResponse.getBody(any(TypeReference.class)))
+            .thenReturn(List.of(Map.of("id", "123", "label", "ime")));
+
+        List<Option<String>> expectedOptions = List.of(option("ime", "123"));
+
+        List<? extends Option<String>> result = TodoistUtils.getOptions(anyString(), "label").apply(mockedParameters, mockedParameters, null, "", mockedActionContext);
+
+        assertEquals(expectedOptions, result);
+    }
+}
