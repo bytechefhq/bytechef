2ec5c13b34c31f0d575e961627d9b1fcc9370fef
Ivica Čardić <ivica.cardic@gmail.com>
2024-08-10 21:59:20 +0200

    1279 - Emphasize odd rows in list views

--- client/src/pages/automation/project-instances/components/ProjectInstanceWorkflowList.tsx
+++ client/src/pages/automation/project-instances/components/ProjectInstanceWorkflowList.tsx
@@ -68,7 +68,7 @@ const ProjectInstanceWorkflowList = ({
         <div className="border-b border-b-gray-100 py-3 pl-4">
             <h3 className="heading-tertiary flex justify-start pl-2 text-sm">Workflows</h3>
 
-            <ul>
+            <ul className="divide-y divide-gray-100">
                 {workflows &&
                     workflows
                         .sort((a, b) => a.label!.localeCompare(b.label!))
