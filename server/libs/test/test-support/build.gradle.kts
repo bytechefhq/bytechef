7a930f139c295f9b3ca532c31ef8b991029de3ad
igorbeslic <igor.beslic@bitnaprednost.hr>
2024-08-14 17:58:02 +0200

    1330 test-support - gradle - depend on commons-util to be able to deal with MapsUtil

--- server/libs/test/test-support/build.gradle.kts
+++ server/libs/test/test-support/build.gradle.kts
@@ -1,6 +1,9 @@
 dependencies {
-    implementation("org.apache.commons:commons-lang3")
+   implementation("org.apache.commons:commons-lang3")
    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jdk8")
    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
    implementation("org.skyscreamer:jsonassert")
+
+   implementation(project(":sdks:backend:java:component-api"))
+   implementation(project(":server:libs:core:commons:commons-util"))
 }
