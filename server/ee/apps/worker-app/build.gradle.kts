d7b2527d1b1ea7f17595cc4bb786aa896314a760
Marko Krišković <marko.kriskovic@protonmail.com>
2024-08-16 15:09:45 +0200

    1018 - minimax - created

--- server/ee/apps/worker-app/build.gradle.kts
+++ server/ee/apps/worker-app/build.gradle.kts
@@ -104,6 +104,7 @@ dependencies {
     implementation(project(":server:libs:modules:components:llm:anthropic"))
     implementation(project(":server:libs:modules:components:llm:azure-openai"))
     implementation(project(":server:libs:modules:components:llm:hugging-face"))
+    implementation(project(":server:libs:modules:components:llm:minimax"))
     implementation(project(":server:libs:modules:components:llm:mistral"))
     implementation(project(":server:libs:modules:components:llm:openai"))
     implementation(project(":server:libs:modules:components:petstore"))
