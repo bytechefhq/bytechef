f0e76324ed3127e07e41cee71617d2a004e9f63b
Marko Krišković <marko.kriskovic@protonmail.com>
2024-08-19 20:24:44 +0200

    1018 - watsonx - created

---
+++ server/libs/modules/components/llm/watsonx/src/main/java/com/bytechef/component/watsonx/action/WatsonxChatAction.java
@@ -0,0 +1,128 @@
+/*
+ * Copyright 2023-present ByteChef Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef.component.watsonx.action;
+
+import static com.bytechef.component.definition.Authorization.TOKEN;
+import static com.bytechef.component.definition.ComponentDSL.action;
+import static com.bytechef.component.definition.ComponentDSL.integer;
+import static com.bytechef.component.definition.ComponentDSL.number;
+import static com.bytechef.component.definition.ComponentDSL.string;
+
+import static com.bytechef.component.watsonx.constant.WatsonxConstants.DECODING_METHOD;
+import static com.bytechef.component.watsonx.constant.WatsonxConstants.MIN_TOKENS;
+import static com.bytechef.component.watsonx.constant.WatsonxConstants.PROJECT_ID;
+import static com.bytechef.component.watsonx.constant.WatsonxConstants.REPETITION_PENALTY;
+import static com.bytechef.component.watsonx.constant.WatsonxConstants.STREAM_ENDPOINT;
+import static com.bytechef.component.watsonx.constant.WatsonxConstants.TEXT_ENDPOINT;
+import static constants.LLMConstants.ASK;
+import static constants.LLMConstants.MAX_TOKENS;
+import static constants.LLMConstants.MAX_TOKENS_PROPERTY;
+import static constants.LLMConstants.MESSAGE_PROPERTY;
+import static constants.LLMConstants.MODEL;
+import static constants.LLMConstants.N;
+import static constants.LLMConstants.N_PROPERTY;
+import static constants.LLMConstants.SEED;
+import static constants.LLMConstants.SEED_PROPERTY;
+import static constants.LLMConstants.STOP;
+import static constants.LLMConstants.STOP_PROPERTY;
+import static constants.LLMConstants.TEMPERATURE;
+import static constants.LLMConstants.TEMPERATURE_PROPERTY;
+import static constants.LLMConstants.TOP_P;
+import static constants.LLMConstants.TOP_P_PROPERTY;
+import static constants.LLMConstants.URL;
+
+import com.bytechef.component.definition.ActionContext;
+import com.bytechef.component.definition.ComponentDSL.ModifiableActionDefinition;
+import com.bytechef.component.definition.Context.TypeReference;
+import com.bytechef.component.definition.Parameters;
+
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.chat.prompt.ChatOptions;
+import org.springframework.ai.watsonx.WatsonxAiChatModel;
+import org.springframework.ai.watsonx.WatsonxAiChatOptions;
+import org.springframework.ai.watsonx.api.WatsonxAiApi;
+import org.springframework.web.client.RestClient;
+import util.interfaces.Chat;
+
+public class WatsonxChatAction {
+
+    public static final ModifiableActionDefinition ACTION_DEFINITION = action(ASK)
+        .title("Ask")
+        .description("Ask anything you want.")
+        .properties(
+            string(MODEL)
+                .label("Model")
+                .description("google/flan-ul2")
+                .exampleValue("")
+                .required(false),
+            string(DECODING_METHOD)
+                .label("Decoding method")
+                .description("Decoding is the process that a model uses to choose the tokens in the generated output.")
+                .exampleValue("greedy")
+                .required(false),
+            number(REPETITION_PENALTY)
+                .label("Repetition penalty")
+                .description("Sets how strongly to penalize repetitions. A higher value (e.g., 1.8) will penalize repetitions more strongly, while a lower value (e.g., 1.1) will be more lenient.")
+                .required(false),
+            MESSAGE_PROPERTY,
+            N_PROPERTY,
+            integer(MIN_TOKENS)
+                .label("Min tokens")
+                .description("Sets how many tokens must the LLM generate.")
+                .required(false),
+            MAX_TOKENS_PROPERTY,
+            TEMPERATURE_PROPERTY,
+            STOP_PROPERTY,
+            TOP_P_PROPERTY,
+            SEED_PROPERTY)
+        .outputSchema(string())
+        .perform(WatsonxChatAction::perform);
+
+    private WatsonxChatAction() {
+    }
+
+    public static String perform(
+        Parameters inputParameters, Parameters connectionParameters, ActionContext context) {
+        return Chat.getResponse(CHAT, inputParameters, connectionParameters);
+    }
+
+    public static final Chat CHAT = new Chat() {
+        @Override
+        public ChatOptions createChatOptions(Parameters inputParameters) {
+            return WatsonxAiChatOptions.builder()
+                .withModel(inputParameters.getString(MODEL))
+                .withTemperature(inputParameters.getFloat(TEMPERATURE))
+                .withMaxNewTokens(inputParameters.getInteger(MAX_TOKENS))
+                .withTopP(inputParameters.getFloat(TOP_P))
+                .withStopSequences(inputParameters.getList(STOP, new TypeReference<>() {}))
+                .withTopK(inputParameters.getInteger(N))
+                .withMinNewTokens(inputParameters.getInteger(MIN_TOKENS))
+                .withRandomSeed(inputParameters.getInteger(SEED))
+                .withRepetitionPenalty(inputParameters.getFloat(REPETITION_PENALTY))
+                .withDecodingMethod(inputParameters.getString(DECODING_METHOD))
+                .build();
+        }
+
+        @Override
+        public ChatModel createChatModel(Parameters inputParameters, Parameters connectionParameters) {
+            return new WatsonxAiChatModel(new WatsonxAiApi(connectionParameters.getString(URL),
+                connectionParameters.getString(STREAM_ENDPOINT), connectionParameters.getString(TEXT_ENDPOINT),
+                connectionParameters.getString(PROJECT_ID), connectionParameters.getString(TOKEN), RestClient.builder()),
+                (WatsonxAiChatOptions) createChatOptions(inputParameters));
+        }
+    };
+}
---
+++ server/libs/modules/components/llm/watsonx/src/main/java/com/bytechef/component/watsonx/connection/WatsonxConnection.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2023-present ByteChef Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef.component.watsonx.connection;
+
+import static com.bytechef.component.definition.Authorization.AuthorizationType.BEARER_TOKEN;
+import static com.bytechef.component.definition.Authorization.TOKEN;
+import static com.bytechef.component.definition.ComponentDSL.authorization;
+import static com.bytechef.component.definition.ComponentDSL.connection;
+import static com.bytechef.component.definition.ComponentDSL.string;
+import static com.bytechef.component.watsonx.constant.WatsonxConstants.PROJECT_ID;
+import static com.bytechef.component.watsonx.constant.WatsonxConstants.STREAM_ENDPOINT;
+import static com.bytechef.component.watsonx.constant.WatsonxConstants.TEXT_ENDPOINT;
+import static constants.LLMConstants.URL;
+
+import com.bytechef.component.definition.ComponentDSL.ModifiableConnectionDefinition;
+
+/**
+ * @author Monika Domiter
+ */
+public final class WatsonxConnection {
+
+    public static final ModifiableConnectionDefinition CONNECTION_DEFINITION = connection()
+//        .baseUri((connectionParameters, context) -> "https://api-inference.huggingface.co")
+        .authorizations(
+            authorization(BEARER_TOKEN)
+                .title("Bearer Token")
+                .properties(
+                    string(URL)
+                        .label("Url")
+                        .description("URL to connect to.")
+                        .required(true),
+                    string(STREAM_ENDPOINT)
+                        .label("Stream Endpoint")
+                        .description("The streaming endpoint.")
+                        .defaultValue("generation/stream?version=2023-05-29")
+                        .required(true),
+                    string(TEXT_ENDPOINT)
+                        .label("Text Endpoint")
+                        .description("The text endpoint.")
+                        .defaultValue("generation/text?version=2023-05-29")
+                        .required(true),
+                    string(PROJECT_ID)
+                        .label("Project ID")
+                        .description("The project ID.")
+                        .required(true),
+                    string(TOKEN)
+                        .label("IAM Token")
+                        .description("The IBM Cloud account IAM token.")
+                        .required(true)
+                    ));
+
+    private WatsonxConnection() {
+    }
+}
