d480ac3bd7d7cb1e221201a0fe9179b5bf053c00
Marko Krišković <marko.kriskovic@protonmail.com>
2024-08-20 00:29:40 +0200

    1018 - amazonBedrock - created

---
+++ server/libs/modules/components/llm/amazon-bedrock/src/main/java/com/bytechef/component/amazon/bedrock/AmazonBedrockComponentHandler.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2023-present ByteChef Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef.component.amazon.bedrock;
+
+import static com.bytechef.component.definition.ComponentDSL.component;
+
+import com.bytechef.component.ComponentHandler;
+import com.bytechef.component.amazon.bedrock.action.AmazonBedrockJurassic2ChatAction;
+import com.bytechef.component.amazon.bedrock.action.AmazonBedrockLlamaChatAction;
+import com.bytechef.component.amazon.bedrock.action.AmazonBedrockTitanChatAction;
+import com.bytechef.component.amazon.bedrock.connection.AmazonBedrockConnection;
+import com.bytechef.component.amazon.bedrock.constant.AmazonBedrockConstants;
+import com.bytechef.component.amazon.bedrock.action.AmazonBedrockAnthropic2ChatAction;
+import com.bytechef.component.amazon.bedrock.action.AmazonBedrockCohereChatAction;
+import com.bytechef.component.definition.ComponentCategory;
+import com.bytechef.component.definition.ComponentDefinition;
+import com.bytechef.component.amazon.bedrock.action.AmazonBedrockAnthropic3ChatAction;
+import com.google.auto.service.AutoService;
+
+/**
+ * @author Monika Domiter
+ */
+@AutoService(ComponentHandler.class)
+public class AmazonBedrockComponentHandler implements ComponentHandler {
+
+    private static final ComponentDefinition COMPONENT_DEFINITION = component(AmazonBedrockConstants.AMAZON_BEDROCK)
+        .title("Amazon Bedrock")
+        .description(
+            "Amazon Bedrock is a fully managed service that offers a choice of high-performing foundation models (FMs) from leading AI companies.")
+        .icon("path:assets/amazon-bedrock.svg")
+        .categories(ComponentCategory.ARTIFICIAL_INTELLIGENCE)
+        .connection(AmazonBedrockConnection.CONNECTION_DEFINITION)
+        .actions(AmazonBedrockAnthropic3ChatAction.ACTION_DEFINITION,
+            AmazonBedrockAnthropic2ChatAction.ACTION_DEFINITION,
+            AmazonBedrockCohereChatAction.ACTION_DEFINITION,
+            AmazonBedrockJurassic2ChatAction.ACTION_DEFINITION,
+            AmazonBedrockLlamaChatAction.ACTION_DEFINITION,
+            AmazonBedrockTitanChatAction.ACTION_DEFINITION);
+
+    @Override
+    public ComponentDefinition getDefinition() {
+        return COMPONENT_DEFINITION;
+    }
+}
---
+++ server/libs/modules/components/llm/amazon-bedrock/src/main/java/com/bytechef/component/amazon/bedrock/action/AmazonBedrockLlamaChatAction.java
@@ -0,0 +1,93 @@
+/*
+ * Copyright 2023-present ByteChef Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef.component.amazon.bedrock.action;
+
+import com.bytechef.component.amazon.bedrock.constant.AmazonBedrockConstants;
+import com.bytechef.component.definition.ActionContext;
+import com.bytechef.component.definition.ComponentDSL.ModifiableActionDefinition;
+import com.bytechef.component.definition.Parameters;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.ai.bedrock.llama.BedrockLlamaChatModel;
+import org.springframework.ai.bedrock.llama.BedrockLlamaChatOptions;
+import org.springframework.ai.bedrock.llama.api.LlamaChatBedrockApi;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.chat.prompt.ChatOptions;
+import software.amazon.awssdk.auth.credentials.EnvironmentVariableCredentialsProvider;
+import util.LLMUtils;
+import util.interfaces.Chat;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+import static com.bytechef.component.definition.ComponentDSL.action;
+import static com.bytechef.component.definition.ComponentDSL.string;
+import static constants.LLMConstants.MAX_TOKENS;
+import static constants.LLMConstants.MAX_TOKENS_PROPERTY;
+import static constants.LLMConstants.MESSAGE_PROPERTY;
+import static constants.LLMConstants.MODEL;
+import static constants.LLMConstants.TEMPERATURE;
+import static constants.LLMConstants.TEMPERATURE_PROPERTY;
+import static constants.LLMConstants.TOP_P;
+import static constants.LLMConstants.TOP_P_PROPERTY;
+
+public class AmazonBedrockLlamaChatAction {
+
+    public static final ModifiableActionDefinition ACTION_DEFINITION = action(AmazonBedrockConstants.ASK_LLAMA)
+        .title("Ask Llama")
+        .description("Ask anything you want.")
+        .properties(
+            string(MODEL)
+                .label("Model")
+                .description("ID of the model to use.")
+                .required(true)
+                .options(LLMUtils.getEnumOptions(
+                    Arrays.stream(LlamaChatBedrockApi.LlamaChatModel.values())
+                        .collect(Collectors.toMap(
+                            LlamaChatBedrockApi.LlamaChatModel::getName, LlamaChatBedrockApi.LlamaChatModel::getName, (f,s)->f)))),
+            MESSAGE_PROPERTY,
+            MAX_TOKENS_PROPERTY,
+            TEMPERATURE_PROPERTY,
+            TOP_P_PROPERTY)
+        .outputSchema(string())
+        .perform(AmazonBedrockLlamaChatAction::perform);
+
+    private AmazonBedrockLlamaChatAction() {
+    }
+
+    public static String perform(
+        Parameters inputParameters, Parameters connectionParameters, ActionContext context) {
+        return Chat.getResponse(CHAT, inputParameters, connectionParameters);
+    }
+
+    public static final Chat CHAT = new Chat() {
+        @Override
+        public ChatOptions createChatOptions(Parameters inputParameters) {
+            return BedrockLlamaChatOptions.builder()
+                .withTemperature(inputParameters.getFloat(TEMPERATURE))
+                .withMaxGenLen(inputParameters.getInteger(MAX_TOKENS))
+                .withTopP(inputParameters.getFloat(TOP_P))
+                .build();
+        }
+
+        @Override
+        public ChatModel createChatModel(Parameters inputParameters, Parameters connectionParameters) {
+            return new BedrockLlamaChatModel(new LlamaChatBedrockApi(inputParameters.getRequiredString(MODEL),
+                EnvironmentVariableCredentialsProvider.create(), connectionParameters.getRequiredString(AmazonBedrockConstants.REGION), new ObjectMapper()),
+                (BedrockLlamaChatOptions) createChatOptions(inputParameters));
+        }
+    };
+}
