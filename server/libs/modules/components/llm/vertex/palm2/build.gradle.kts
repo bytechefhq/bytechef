f61c90151c229f4c8f3f676c9562d38b98d25058
Marko Krišković <marko.kriskovic@protonmail.com>
2024-08-19 22:31:11 +0200

    1018 - vertexAI - created

---
+++ server/libs/modules/components/llm/vertex/palm2/build.gradle.kts
@@ -0,0 +1,6 @@
+version="1.0"
+
+dependencies {
+    implementation("org.springframework.ai:spring-ai-vertex-ai-palm2-spring-boot-starter:1.0.0-SNAPSHOT")
+    implementation(project(":server:libs:modules:components:llm"))
+}
