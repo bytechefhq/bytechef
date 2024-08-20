615241329ef14e26d78da38316ec3674b1475479
Marko Krišković <marko.kriskovic@protonmail.com>
2024-08-20 10:10:50 +0200

    1018 - qianfan - created

---
+++ server/libs/modules/components/llm/qianfan/src/main/java/com/bytechef/component/qianfan/action/QIanFanChatAction.java
@@ -0,0 +1,107 @@
+/*
+ * Copyright 2023-present ByteChef Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef.component.qianfan.action;
+
+import static com.bytechef.component.definition.Authorization.TOKEN;
+import static com.bytechef.component.definition.ComponentDSL.action;
+import static com.bytechef.component.definition.ComponentDSL.string;
+
+import static constants.LLMConstants.ASK;
+import static constants.LLMConstants.FREQUENCY_PENALTY;
+import static constants.LLMConstants.FREQUENCY_PENALTY_PROPERTY;
+import static constants.LLMConstants.MAX_TOKENS;
+import static constants.LLMConstants.MAX_TOKENS_PROPERTY;
+import static constants.LLMConstants.MESSAGE_PROPERTY;
+import static constants.LLMConstants.MODEL;
+import static constants.LLMConstants.PRESENCE_PENALTY;
+import static constants.LLMConstants.PRESENCE_PENALTY_PROPERTY;
+import static constants.LLMConstants.STOP;
+import static constants.LLMConstants.STOP_PROPERTY;
+import static constants.LLMConstants.TEMPERATURE;
+import static constants.LLMConstants.TEMPERATURE_PROPERTY;
+import static constants.LLMConstants.TOP_P;
+import static constants.LLMConstants.TOP_P_PROPERTY;
+
+import com.bytechef.component.definition.ActionContext;
+import com.bytechef.component.definition.ComponentDSL.ModifiableActionDefinition;
+import com.bytechef.component.definition.Context.TypeReference;
+import com.bytechef.component.definition.Parameters;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+import com.bytechef.component.qianfan.constant.QIanFanConstants;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.chat.prompt.ChatOptions;
+import org.springframework.ai.qianfan.QianFanChatModel;
+import org.springframework.ai.qianfan.QianFanChatOptions;
+import org.springframework.ai.qianfan.api.QianFanApi;
+import util.LLMUtils;
+import util.interfaces.Chat;
+
+public class QIanFanChatAction {
+
+    public static final ModifiableActionDefinition ACTION_DEFINITION = action(ASK)
+        .title("Ask")
+        .description("Ask anything you want.")
+        .properties(
+            string(MODEL)
+                .label("Model")
+                .description("ID of the model to use.")
+                .required(true)
+                .options(LLMUtils.getEnumOptions(
+                    Arrays.stream(QianFanApi.ChatModel.values())
+                        .collect(Collectors.toMap(
+                            QianFanApi.ChatModel::getValue, QianFanApi.ChatModel::getValue, (f,s)->f)))),
+            MESSAGE_PROPERTY,
+            MAX_TOKENS_PROPERTY,
+            TEMPERATURE_PROPERTY,
+            STOP_PROPERTY,
+            TOP_P_PROPERTY,
+            PRESENCE_PENALTY_PROPERTY,
+            FREQUENCY_PENALTY_PROPERTY)
+        .outputSchema(string())
+        .perform(QIanFanChatAction::perform);
+
+    private QIanFanChatAction() {
+    }
+
+    public static String perform(
+        Parameters inputParameters, Parameters connectionParameters, ActionContext context) {
+        return Chat.getResponse(CHAT, inputParameters, connectionParameters);
+    }
+
+    public static final Chat CHAT = new Chat() {
+        @Override
+        public ChatOptions createChatOptions(Parameters inputParameters) {
+            return QianFanChatOptions.builder()
+                .withModel(inputParameters.getRequiredString(MODEL))
+                .withTemperature(inputParameters.getFloat(TEMPERATURE))
+                .withMaxTokens(inputParameters.getInteger(MAX_TOKENS))
+                .withTopP(inputParameters.getFloat(TOP_P))
+                .withStop(inputParameters.getList(STOP, new TypeReference<>() {}))
+                .withPresencePenalty(inputParameters.getFloat(PRESENCE_PENALTY))
+                .withFrequencyPenalty(inputParameters.getFloat(FREQUENCY_PENALTY))
+                .build();
+        }
+
+        @Override
+        public ChatModel createChatModel(Parameters inputParameters, Parameters connectionParameters) {
+            return new QianFanChatModel(new QianFanApi(connectionParameters.getString(TOKEN), connectionParameters.getString(QIanFanConstants.SECRET_KEY)), (QianFanChatOptions) createChatOptions(inputParameters));
+        }
+    };
+}
