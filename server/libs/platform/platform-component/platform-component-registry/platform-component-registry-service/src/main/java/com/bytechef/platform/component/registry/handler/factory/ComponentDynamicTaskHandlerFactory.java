4578d7dbcd8cbb2e1f74dfc5dc9059da20d12b8e
Ivica Čardić <ivica.cardic@gmail.com>
2024-08-20 08:48:44 +0200

    1024 - Allow dynamic registration of TaskHandlers

---
+++ server/libs/platform/platform-component/platform-component-registry/platform-component-registry-service/src/main/java/com/bytechef/platform/component/registry/handler/factory/ComponentDynamicTaskHandlerFactory.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2023-present ByteChef Inc.
+ *
+ * Licensed under the ByteChef Enterprise license (the "Enterprise License");
+ * you may not use this file except in compliance with the Enterprise License.
+ */
+
+package com.bytechef.platform.component.registry.handler.factory;
+
+import com.bytechef.atlas.worker.task.factory.DynamicTaskHandlerFactory;
+import com.bytechef.atlas.worker.task.handler.TaskHandler;
+import com.bytechef.platform.component.registry.facade.ActionDefinitionFacade;
+import com.bytechef.platform.component.registry.handler.ComponentTaskHandler;
+import com.bytechef.platform.definition.WorkflowNodeType;
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+/**
+ * @version ee
+ *
+ * @author Ivica Cardic
+ */
+@Component
+@Order
+public class ComponentDynamicTaskHandlerFactory implements DynamicTaskHandlerFactory {
+
+    private final ActionDefinitionFacade actionDefinitionFacade;
+
+    @SuppressFBWarnings("EI")
+    public ComponentDynamicTaskHandlerFactory(ActionDefinitionFacade actionDefinitionFacade) {
+        this.actionDefinitionFacade = actionDefinitionFacade;
+    }
+
+    @Override
+    public TaskHandler<?> getTaskHandler(String type) {
+        WorkflowNodeType workflowNodeType = WorkflowNodeType.ofType(type);
+
+        return new ComponentTaskHandler(
+            workflowNodeType.componentName(), workflowNodeType.componentVersion(),
+            workflowNodeType.componentOperationName(), actionDefinitionFacade);
+    }
+}
