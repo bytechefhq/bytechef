8a1815f128c0fe118a87993eb634d78ac06cf364
Ivica Čardić <ivica.cardic@gmail.com>
2024-08-22 17:41:57 +0200

    1378 - Convert ComponentCategory from enum to record to allow defining custom categories

--- sdks/backend/java/component-api/src/main/java/com/bytechef/component/definition/ComponentCategory.java
+++ sdks/backend/java/component-api/src/main/java/com/bytechef/component/definition/ComponentCategory.java
@@ -19,34 +19,33 @@ package com.bytechef.component.definition;
 /**
  * @author Monika Domiter
  */
-public enum ComponentCategory {
+public record ComponentCategory(String key, String label) {
 
-    ACCOUNTING("accounting"),
-    ADVERTISING("advertising"),
-    ANALYTICS("analytics"),
-    ARTIFICIAL_INTELLIGENCE("artificial-intelligence"),
-    ATS("ats"),
-    CALENDARS_AND_SCHEDULING("calendars-and-scheduling"),
-    COMMUNICATION("communication"),
-    CRM("crm"),
-    CUSTOMER_SUPPORT("customer-support"),
-    DEVELOPER_TOOLS("developer-tools"),
-    E_COMMERCE("e-commerce"),
-    FILE_STORAGE("file-storage"),
-    HELPERS("helpers"),
-    HRIS("hris"),
-    MARKETING_AUTOMATION("marketing-automation"),
-    PAYMENT_PROCESSING("payment-processing"),
-    PRODUCTIVITY_AND_COLLABORATION("productivity-and-collaboration"),
-    PROJECT_MANAGEMENT("project-management"),
-    SOCIAL_MEDIA("social-media"),
-    SURVEYS_AND_FEEDBACK("surveys-and-feedback");
-
-    private final String key;
-
-    ComponentCategory(String key) {
-        this.key = key;
+    public ComponentCategory(String key) {
+        this(key, key);
     }
+
+    public static final ComponentCategory ACCOUNTING = new ComponentCategory("accounting");
+    public static final ComponentCategory ADVERTISING = new ComponentCategory("advertising");
+    public static final ComponentCategory ANALYTICS = new ComponentCategory("analytics");
+    public static final ComponentCategory ARTIFICIAL_INTELLIGENCE = new ComponentCategory("artificial-intelligence");
+    public static final ComponentCategory ATS = new ComponentCategory("ats");
+    public static final ComponentCategory CALENDARS_AND_SCHEDULING = new ComponentCategory("calendars-and-scheduling");
+    public static final ComponentCategory COMMUNICATION = new ComponentCategory("communication");
+    public static final ComponentCategory CRM = new ComponentCategory("crm");
+    public static final ComponentCategory CUSTOMER_SUPPORT = new ComponentCategory("customer-support");
+    public static final ComponentCategory DEVELOPER_TOOLS = new ComponentCategory("developer-tools");
+    public static final ComponentCategory E_COMMERCE = new ComponentCategory("e-commerce");
+    public static final ComponentCategory FILE_STORAGE = new ComponentCategory("file-storage");
+    public static final ComponentCategory HELPERS = new ComponentCategory("helpers");
+    public static final ComponentCategory HRIS = new ComponentCategory("hris");
+    public static final ComponentCategory MARKETING_AUTOMATION = new ComponentCategory("marketing-automation");
+    public static final ComponentCategory PAYMENT_PROCESSING = new ComponentCategory("payment-processing");
+    public static final ComponentCategory PRODUCTIVITY_AND_COLLABORATION = new ComponentCategory(
+        "productivity-and-collaboration");
+    public static final ComponentCategory PROJECT_MANAGEMENT = new ComponentCategory("project-management");
+    public static final ComponentCategory SOCIAL_MEDIA = new ComponentCategory("social-media");
+    public static final ComponentCategory SURVEYS_AND_FEEDBACK = new ComponentCategory("surveys-and-feedback");
 
     public String getKey() {
         return key;
