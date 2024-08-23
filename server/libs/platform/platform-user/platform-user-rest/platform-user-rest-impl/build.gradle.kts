9f4814e884865dd9ab6635860b026ef1a7d4c1e5
Ivica Čardić <ivica.cardic@gmail.com>
2024-08-23 07:40:35 +0200

    1024 - Add gradle script for generating the client API

--- server/libs/platform/platform-user/platform-user-rest/platform-user-rest-impl/build.gradle.kts
+++ server/libs/platform/platform-user/platform-user-rest/platform-user-rest-impl/build.gradle.kts
@@ -24,10 +24,21 @@ val generateOpenAPISpring by tasks.registering(org.openapitools.generator.gradle
     )
 }
 
-tasks.register("generateOpenAPI") {
-    dependsOn(generateOpenAPISpring)
+val generateOpenAPITypeScriptFetch by tasks.registering(org.openapitools.generator.gradle.plugin.tasks.GenerateTask::class) {
+    generatorName.set("typescript-fetch")
+    inputSpec.set("$projectDir/openapi.yaml")
+    modelNameSuffix.set("Model")
+    outputDir.set("$rootDir/client/src/shared/middleware/platform/user")
+    typeMappings.set(
+        mapOf(
+            "DateTime" to "Date"
+        )
+    )
 }
 
+tasks.register("generateOpenAPI") {
+    dependsOn(generateOpenAPISpring, generateOpenAPITypeScriptFetch)
+}
 
 dependencies {
     compileOnly("jakarta.servlet:jakarta.servlet-api")
