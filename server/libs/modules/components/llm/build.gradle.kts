588a3d93f0150f8628464cb6b4258076a9b74637
Marko Krišković <marko.kriskovic@protonmail.com>
2024-08-26 13:22:27 +0200

    1018 - gradle updated

--- server/libs/modules/components/llm/build.gradle.kts
+++ server/libs/modules/components/llm/build.gradle.kts
@@ -14,5 +14,5 @@ artifacts{
 }
 
 dependencies {
-    implementation("org.springframework.ai:spring-ai-openai-spring-boot-starter:1.0.0-SNAPSHOT")
+    implementation("org.springframework.ai:spring-ai-openai-spring-boot-starter:1.0.0-M2")
 }
