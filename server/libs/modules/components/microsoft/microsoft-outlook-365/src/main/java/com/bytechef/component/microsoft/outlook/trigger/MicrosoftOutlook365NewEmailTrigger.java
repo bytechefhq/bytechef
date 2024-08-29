d6c50c03ab932b4f296a22152fa01e5b9ebb4c77
Monika Kušter <domitermonika@gmail.com>
2024-08-29 09:05:28 +0200

    695 - implemented new email trigger

--- server/libs/modules/components/microsoft/microsoft-outlook-365/src/main/java/com/bytechef/component/microsoft/outlook/trigger/MicrosoftOutlook365NewEmailTrigger.java
+++ server/libs/modules/components/microsoft/microsoft-outlook-365/src/main/java/com/bytechef/component/microsoft/outlook/trigger/MicrosoftOutlook365NewEmailTrigger.java
@@ -16,10 +16,86 @@
 
 package com.bytechef.component.microsoft.outlook.trigger;
 
+import static com.bytechef.component.definition.ComponentDSL.array;
+import static com.bytechef.component.definition.ComponentDSL.trigger;
+import static com.bytechef.component.microsoft.outlook.constant.MicrosoftOutlook365Constants.MESSAGE_OUTPUT_PROPERTY;
+import static com.bytechef.component.microsoft.outlook.constant.MicrosoftOutlook365Constants.ODATA_NEXT_LINK;
+import static com.bytechef.component.microsoft.outlook.constant.MicrosoftOutlook365Constants.VALUE;
+import static com.bytechef.component.microsoft.outlook.util.MicrosoftOutlook365Utils.getItemsFromNextPage;
+
+import com.bytechef.component.definition.ComponentDSL.ModifiableTriggerDefinition;
+import com.bytechef.component.definition.Context.Http;
+import com.bytechef.component.definition.Context.TypeReference;
+import com.bytechef.component.definition.Parameters;
+import com.bytechef.component.definition.TriggerContext;
+import com.bytechef.component.definition.TriggerDefinition.PollOutput;
+import com.bytechef.component.definition.TriggerDefinition.TriggerType;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 /**
- * @author Monika Domiter
+ * @author Monika Kušter
  */
 public class MicrosoftOutlook365NewEmailTrigger {
 
-    // TODO
+    public static final ModifiableTriggerDefinition TRIGGER_DEFINITION = trigger("newEmail")
+        .title("New Email")
+        .description("Triggers when new mail is received.")
+        .type(TriggerType.POLLING)
+        .outputSchema(array().items(MESSAGE_OUTPUT_PROPERTY))
+        .poll(MicrosoftOutlook365NewEmailTrigger::poll);
+
+    protected static final String LAST_TIME_CHECKED = "lastTimeChecked";
+
+    private MicrosoftOutlook365NewEmailTrigger() {
+    }
+
+    protected static PollOutput poll(
+        Parameters inputParameters, Parameters connectionParameters, Parameters closureParameters,
+        TriggerContext context) {
+
+        LocalDateTime startDate = closureParameters.getLocalDateTime(LAST_TIME_CHECKED, LocalDateTime.now());
+        LocalDateTime endDate = LocalDateTime.now();
+
+        List<Map<?, ?>> maps = new ArrayList<>();
+
+        Map<String, Object> body = context
+            .http(http -> http.get("/mailFolders/Inbox/messages"))
+            .queryParameters("$filter", "isRead eq false", "$orderby", "receivedDateTime desc")
+            .configuration(Http.responseType(Http.ResponseType.JSON))
+            .execute()
+            .getBody(new TypeReference<>() {});
+
+        if (body.get(VALUE) instanceof List<?> list) {
+            for (Object o : list) {
+                if (o instanceof Map<?, ?> map) {
+                    addValidEmail(map, startDate, endDate, maps);
+                }
+            }
+        }
+
+        List<Map<?, ?>> otherItems = getItemsFromNextPage(context, (String) body.get(ODATA_NEXT_LINK));
+
+        for (Map<?, ?> map : otherItems) {
+            addValidEmail(map, startDate, endDate, maps);
+        }
+
+        return new PollOutput(maps, Map.of(LAST_TIME_CHECKED, endDate), false);
+    }
+
+    private static void addValidEmail(
+        Map<?, ?> map, LocalDateTime startDate, LocalDateTime endDate, List<Map<?, ?>> maps) {
+
+        LocalDateTime createdDateTime =
+            LocalDateTime.ofInstant(ZonedDateTime.parse((String) map.get("receivedDateTime"))
+                .toInstant(), ZoneOffset.systemDefault());
+
+        if (createdDateTime.isAfter(startDate) && createdDateTime.isBefore(endDate)) {
+            maps.add(map);
+        }
+    }
 }
