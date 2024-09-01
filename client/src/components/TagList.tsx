4c7058a778535952f4447d0afd8d28e9102ca062
Krešimir Čoko <kresimircoko@gmail.com>
2024-09-01 16:39:24 +0200

    1336 - improve project tag handling

--- client/src/components/TagList.tsx
+++ client/src/components/TagList.tsx
@@ -22,14 +22,22 @@ interface TagProps {
 }
 
 const Tag = ({onDeleteTag, tag}: TagProps) => (
-    <span className="inline-flex items-center rounded-full border border-muted px-2 py-1 text-xs text-gray-700 hover:border-gray-200">
-        {tag.name}
+    <div className="group flex max-h-8 items-center rounded-full border border-gray-300 pl-2 pr-1 text-xs text-gray-700">
+        <span className="py-1">{tag.name}</span>
 
-        <Cross2Icon
-            className="ml-1.5 size-3 cursor-pointer rounded-full hover:bg-gray-200"
-            onClick={() => onDeleteTag(tag)}
-        />
-    </span>
+        <Tooltip>
+            <TooltipTrigger>
+                <button
+                    className="ml-1.5 rounded-full p-1 text-red-500 hover:bg-red-100 hover:text-red-700"
+                    onClick={() => onDeleteTag(tag)}
+                >
+                    <Cross2Icon className="size-3 cursor-pointer" />
+                </button>
+            </TooltipTrigger>
+
+            <TooltipContent>Remove</TooltipContent>
+        </Tooltip>
+    </div>
 );
 
 interface TagListProps {
@@ -61,7 +69,9 @@ const TagList = ({getRequest, id, remainingTags, tags, updateTagsMutation}: TagL
     };
 
     return (
-        <div className="mr-4 flex items-center space-x-1">
+        <div className="mr-4 flex items-center space-x-2">
+            <span className="text-xs text-gray-500">Tags:</span>
+
             {tags.slice(0, 3).map((tag) => (
                 <Tag key={tag.id} onDeleteTag={handleDeleteTag} tag={tag} />
             ))}
@@ -86,29 +96,51 @@ const TagList = ({getRequest, id, remainingTags, tags, updateTagsMutation}: TagL
             )}
 
             {isNewTagWindowVisible ? (
-                <CreatableSelect
-                    className="w-40 text-start"
-                    name="newTag"
-                    onChange={(selectedOption: OnChangeValue<SelectOptionType, false>) => {
-                        if (selectedOption) {
-                            handleAddTag(selectedOption.tag);
-                        }
+                <>
+                    <CreatableSelect
+                        className="w-40 text-start"
+                        name="newTag"
+                        onChange={(selectedOption: OnChangeValue<SelectOptionType, false>) => {
+                            if (selectedOption) {
+                                handleAddTag(selectedOption.tag);
+                            }
 
-                        setIsNewTagWindowVisible(false);
-                    }}
-                    onCreateOption={(inputValue: string) => {
-                        handleAddTag({
-                            name: inputValue,
-                        });
+                            setIsNewTagWindowVisible(false);
+                        }}
+                        onCreateOption={(inputValue: string) => {
+                            handleAddTag({
+                                name: inputValue,
+                            });
 
-                        setIsNewTagWindowVisible(false);
-                    }}
-                    options={remainingTags!.map((tag: TagModelType) => ({
-                        label: `${tag.name}`,
-                        tag,
-                        value: tag.name.toLowerCase().replace(/\W/g, ''),
-                    }))}
-                />
+                            setIsNewTagWindowVisible(false);
+                        }}
+                        options={remainingTags!.map((tag: TagModelType) => ({
+                            label: `${tag.name}`,
+                            tag,
+                            value: tag.name.toLowerCase().replace(/\W/g, ''),
+                        }))}
+                        styles={{
+                            control: (styles) => ({...styles, fontSize: '0.75rem', minHeight: '1.5rem', padding: '0'}),
+                            dropdownIndicator: (styles) => ({...styles, padding: '0.2rem'}),
+                            valueContainer: (styles) => ({...styles, padding: '0 0.4rem'}),
+                        }}
+                    />
+
+                    <Tooltip>
+                        <TooltipTrigger>
+                            <Button
+                                className="px-2 text-red-500 hover:bg-red-100 hover:text-red-700"
+                                onClick={() => setIsNewTagWindowVisible(false)}
+                                size="sm"
+                                variant="ghost"
+                            >
+                                <Cross2Icon />
+                            </Button>
+                        </TooltipTrigger>
+
+                        <TooltipContent>Cancel adding a new tag</TooltipContent>
+                    </Tooltip>
+                </>
             ) : (
                 <div
                     className="flex size-6 cursor-pointer items-center justify-center rounded border border-gray-100 hover:bg-gray-200"
