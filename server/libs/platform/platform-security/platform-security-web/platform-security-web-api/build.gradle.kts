759c221a1f71bae8ca6f12278ecb78d6c9027c9e
Ivica Čardić <ivica.cardic@gmail.com>
2024-09-03 09:09:45 +0200

    520 - Add dependency

--- server/libs/platform/platform-security/platform-security-web/platform-security-web-api/build.gradle.kts
+++ server/libs/platform/platform-security/platform-security-web/platform-security-web-api/build.gradle.kts
@@ -1,8 +1,10 @@
 dependencies {
     api("org.springframework.security:spring-security-config")
     api("org.springframework.security:spring-security-web")
-    api(project(":server:libs:platform:platform-api"))
     api(project(":server:libs:platform:platform-security:platform-security-api"))
+    api(project(":server:libs:platform:platform-user:platform-user-api"))
+
+    implementation(project(":server:libs:core:tenant:tenant-api"))
 
     compileOnly("jakarta.servlet:jakarta.servlet-api")
 }
