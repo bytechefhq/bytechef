33c3c103cf4181fbdc1ee7565d8b041191cc4567
Monika Kušter <domitermonika@gmail.com>
2024-09-03 09:48:17 +0200

    717 - refactor

--- server/libs/modules/components/google/google-sheets/src/main/java/com/bytechef/component/google/sheets/constant/GoogleSheetsConstants.java
+++ server/libs/modules/components/google/google-sheets/src/main/java/com/bytechef/component/google/sheets/constant/GoogleSheetsConstants.java
@@ -27,7 +27,6 @@ import com.bytechef.component.definition.ComponentDSL.ModifiableDynamicPropertie
 import com.bytechef.component.definition.ComponentDSL.ModifiableIntegerProperty;
 import com.bytechef.component.definition.ComponentDSL.ModifiableStringProperty;
 import com.bytechef.component.definition.OptionsDataSource.ActionOptionsFunction;
-import com.bytechef.component.definition.OptionsDataSource.TriggerOptionsFunction;
 import com.bytechef.component.google.sheets.util.GoogleSheetsUtils;
 
 /**
@@ -35,8 +34,8 @@ import com.bytechef.component.google.sheets.util.GoogleSheetsUtils;
  */
 public class GoogleSheetsConstants {
 
+    public static final String HEADERS = "headers";
     public static final String INCLUDE_ITEMS_FROM_ALL_DRIVES = "includeItemsFromAllDrives";
-    public static final String VALUE_INPUT_OPTION = "valueInputOption";
     public static final String IS_THE_FIRST_ROW_HEADER = "isTheFirstRowHeader";
     public static final String ROW_NUMBER = "rowNumber";
     public static final String ROW = "row";
@@ -44,6 +43,7 @@ public class GoogleSheetsConstants {
     public static final String SHEET_ID = "sheetId";
     public static final String SHEET_NAME = "sheetName";
     public static final String SPREADSHEET_ID = "spreadsheetId";
+    public static final String VALUE_INPUT_OPTION = "valueInputOption";
     public static final String VALUES = "values";
 
     public static final ModifiableBooleanProperty INCLUDE_ITEMS_FROM_ALL_DRIVES_PROPERTY =
@@ -62,19 +62,7 @@ public class GoogleSheetsConstants {
     public static final ModifiableStringProperty SPREADSHEET_ID_PROPERTY = string(SPREADSHEET_ID)
         .label("Spreadsheet")
         .description("The spreadsheet to apply the updates to.")
-        .options(
-            (ActionOptionsFunction<String>) (
-                inputParameters, connectionParameters, lookupDependsOnPaths, searchText,
-                context) -> GoogleSheetsUtils.getSpreadsheetIdOptions(inputParameters, connectionParameters))
-        .required(true);
-
-    public static final ModifiableStringProperty SPREADSHEET_ID_PROPERTY_TRIGGER = string(SPREADSHEET_ID)
-        .label("Spreadsheet")
-        .description("The spreadsheet to apply the updates to.")
-        .options(
-            (TriggerOptionsFunction<String>) (
-                inputParameters, connectionParameters, lookupDependsOnPaths, searchText,
-                context) -> GoogleSheetsUtils.getSpreadsheetIdOptions(inputParameters, connectionParameters))
+        .options((ActionOptionsFunction<String>) GoogleSheetsUtils::getSpreadsheetIdOptions)
         .required(true);
 
     public static final ModifiableIntegerProperty SHEET_ID_PROPERTY = integer(SHEET_ID)
@@ -87,20 +75,7 @@ public class GoogleSheetsConstants {
     public static final ModifiableStringProperty SHEET_NAME_PROPERTY = string(SHEET_NAME)
         .label("Sheet")
         .description("The name of the sheet")
-        .options(
-            (ActionOptionsFunction<String>) (
-                inputParameters, connectionParameters, lookupDependsOnPaths, searchText,
-                context) -> GoogleSheetsUtils.getSheetNameOptions(inputParameters, connectionParameters))
-        .optionsLookupDependsOn(SPREADSHEET_ID)
-        .required(true);
-
-    public static final ModifiableStringProperty SHEET_NAME_PROPERTY_TRIGGER = string(SHEET_NAME)
-        .label("Sheet")
-        .description("The name of the sheet")
-        .options(
-            (TriggerOptionsFunction<String>) (
-                inputParameters, connectionParameters, lookupDependsOnPaths, searchText,
-                context) -> GoogleSheetsUtils.getSheetNameOptions(inputParameters, connectionParameters))
+        .options((ActionOptionsFunction<String>) GoogleSheetsUtils::getSheetNameOptions)
         .optionsLookupDependsOn(SPREADSHEET_ID)
         .required(true);
 
