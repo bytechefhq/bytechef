da3d5452b8e8d20920727b69c5c0ad5ef9a20399
Ivica Čardić <ivica.cardic@gmail.com>
2024-09-03 07:28:46 +0200

    272 - Rename

--- server/libs/platform/platform-webhook/platform-webhook-impl/src/main/java/com/bytechef/platform/workflow/webhook/executor/WebhookExecutorImpl.java
+++ server/libs/platform/platform-webhook/platform-webhook-impl/src/main/java/com/bytechef/platform/workflow/webhook/executor/WebhookExecutorImpl.java
@@ -52,7 +52,8 @@ public class WebhookExecutorImpl implements WebhookExecutor {
     @SuppressFBWarnings("EI")
     public WebhookExecutorImpl(
         ApplicationEventPublisher eventPublisher, InstanceAccessorRegistry instanceAccessorRegistry,
-        InstanceJobFacade instanceJobFacade, JobSyncExecutor jobSyncExecutor, WebhookTriggerSyncExecutor webhookTriggerSyncExecutor,
+        InstanceJobFacade instanceJobFacade, JobSyncExecutor jobSyncExecutor,
+        WebhookTriggerSyncExecutor webhookTriggerSyncExecutor,
         TaskFileStorage taskFileStorage) {
 
         this.instanceAccessorRegistry = instanceAccessorRegistry;
