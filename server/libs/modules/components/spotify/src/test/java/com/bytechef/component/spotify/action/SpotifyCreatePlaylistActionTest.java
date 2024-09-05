7d12db272bc3cf04a69ada204197b582aedecbb6
Monika Kušter <domitermonika@gmail.com>
2024-09-05 10:15:43 +0200

    1423 - tests

---
+++ server/libs/modules/components/spotify/src/test/java/com/bytechef/component/spotify/action/SpotifyCreatePlaylistActionTest.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright 2023-present ByteChef Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef.component.spotify.action;
+
+import static com.bytechef.component.spotify.action.SpotifyCreatePlaylistAction.COLLABORATIVE;
+import static com.bytechef.component.spotify.action.SpotifyCreatePlaylistAction.DESCRIPTION;
+import static com.bytechef.component.spotify.action.SpotifyCreatePlaylistAction.PUBLIC;
+import static com.bytechef.component.spotify.constant.SpotifyConstants.NAME;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import com.bytechef.component.definition.ActionContext;
+import com.bytechef.component.definition.Context.Http;
+import com.bytechef.component.definition.Parameters;
+import com.bytechef.test.component.properties.ParametersFactory;
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+
+/**
+ * @author Monika Kušter
+ */
+class SpotifyCreatePlaylistActionTest {
+
+    private final ArgumentCaptor<Http.Body> bodyArgumentCaptor = ArgumentCaptor.forClass(Http.Body.class);
+    private final ActionContext mockedContext = mock(ActionContext.class);
+    private final Http.Executor mockedExecutor = mock(Http.Executor.class);
+    private final Http.Response mockedResponse = mock(Http.Response.class);
+
+    @Test
+    void testPerform() {
+        Map<String, Object> propertyStubsMap = createPropertyStubsMap();
+        Parameters parameters = ParametersFactory.createParameters(propertyStubsMap);
+
+        when(mockedContext.http(any()))
+            .thenReturn(mockedExecutor);
+        when(mockedExecutor.body(bodyArgumentCaptor.capture()))
+            .thenReturn(mockedExecutor);
+        when(mockedExecutor.configuration(any()))
+            .thenReturn(mockedExecutor);
+        when(mockedExecutor.execute())
+            .thenReturn(mockedResponse);
+
+        Object result = SpotifyCreatePlaylistAction.perform(parameters, parameters, mockedContext);
+
+        assertNull(result);
+
+        Http.Body body = bodyArgumentCaptor.getValue();
+
+        assertEquals(propertyStubsMap, body.getContent());
+    }
+
+    private static Map<String, Object> createPropertyStubsMap() {
+        Map<String, Object> propertyStubsMap = new HashMap<>();
+
+        propertyStubsMap.put(NAME, "name");
+        propertyStubsMap.put(DESCRIPTION, "desc");
+        propertyStubsMap.put(PUBLIC, true);
+        propertyStubsMap.put(COLLABORATIVE, true);
+
+        return propertyStubsMap;
+    }
+}
