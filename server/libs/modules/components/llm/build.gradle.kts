d8533889f2f155b3586f2ea91f6195155a73ed6e
Ivica Čardić <ivica.cardic@gmail.com>
2024-09-05 19:47:08 +0200

    1018 - Remove application.properties

--- server/libs/modules/components/llm/build.gradle.kts
+++ server/libs/modules/components/llm/build.gradle.kts
@@ -1,7 +1,7 @@
 version="1.0"
 
 dependencies {
-    implementation("org.springframework.ai:spring-ai-openai-spring-boot-starter:1.0.0-M2")
+    implementation("org.springframework.ai:spring-ai-openai:1.0.0-M2")
     implementation("org.junit.jupiter:junit-jupiter-api:5.10.3")
     implementation("org.mockito:mockito-core:5.11.0")
 }
--- server/libs/modules/components/llm/mistral/build.gradle.kts
+++ server/libs/modules/components/llm/mistral/build.gradle.kts
@@ -1,5 +1,5 @@
 version="1.0"
 
 dependencies {
-    implementation("org.springframework.ai:spring-ai-mistral-ai-spring-boot-starter:1.0.0-M2")
+    implementation("org.springframework.ai:spring-ai-mistral-ai:1.0.0-M2")
 }
