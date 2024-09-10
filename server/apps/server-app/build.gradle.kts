747192d550b13bad09e511dbb4f1f712bafdcffd
Ivica Čardić <ivica.cardic@gmail.com>
2024-09-10 11:48:24 +0200

    797 - Rename DataStream sync to stream method

--- server/apps/server-app/build.gradle.kts
+++ server/apps/server-app/build.gradle.kts
@@ -88,8 +88,6 @@ dependencies {
     implementation(project(":server:libs:platform:platform-configuration:platform-configuration-service"))
     implementation(project(":server:libs:platform:platform-data-storage:platform-data-storage-file-storage:platform-data-storage-file-storage-service"))
     implementation(project(":server:libs:platform:platform-data-storage:platform-data-storage-jdbc:platform-data-storage-jdbc-service"))
-    implementation(project(":server:libs:platform:platform-data-sync:platform-data-sync-configuration:platform-data-sync-configuration-rest"))
-    implementation(project(":server:libs:platform:platform-data-sync:platform-data-sync-configuration:platform-data-sync-configuration-service"))
     implementation(project(":server:libs:platform:platform-file-storage:platform-file-storage-impl"))
     implementation(project(":server:libs:platform:platform-oauth2:platform-oauth2-service"))
     implementation(project(":server:libs:platform:platform-rest:platform-rest-impl"))
