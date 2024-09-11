f6555568218964ab0d99f614434f16ac510af4bb
Ivica Čardić <ivica.cardic@gmail.com>
2024-09-11 20:31:22 +0200

    520 - Generate

--- server/libs/embedded/embedded-configuration/embedded-configuration-connected-user-token-rest/generated/src/main/java/com/bytechef/embedded/configuration/connected/user/token/web/rest/IntegrationApi.java
+++ server/libs/embedded/embedded-configuration/embedded-configuration-connected-user-token-rest/generated/src/main/java/com/bytechef/embedded/configuration/connected/user/token/web/rest/IntegrationApi.java
@@ -31,7 +31,7 @@ import java.util.Map;
 import java.util.Optional;
 import jakarta.annotation.Generated;
 
-@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2024-09-05T10:08:02.563800+02:00[Europe/Zagreb]", comments = "Generator version: 7.8.0")
+@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2024-09-11T19:04:35.387622+02:00[Europe/Zagreb]", comments = "Generator version: 7.8.0")
 @Validated
 @Tag(name = "integration", description = "The Embedded Integration by Connected User Token accessible API")
 public interface IntegrationApi {
