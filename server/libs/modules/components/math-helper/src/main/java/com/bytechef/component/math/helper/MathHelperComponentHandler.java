d4b305b6b4955eab95a965fdcd54326d6ad2eeba
Monika Kušter <domitermonika@gmail.com>
2024-09-16 14:10:50 +0200

    1455 - initial math-helper component

---
+++ server/libs/modules/components/math-helper/src/main/java/com/bytechef/component/math/helper/MathHelperComponentHandler.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2023-present ByteChef Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef.component.math.helper;
+
+import static com.bytechef.component.definition.ComponentDsl.component;
+
+import com.bytechef.component.ComponentHandler;
+import com.bytechef.component.definition.ComponentCategory;
+import com.bytechef.component.definition.ComponentDefinition;
+import com.bytechef.component.math.helper.action.MathHelperAdditionAction;
+import com.bytechef.component.math.helper.action.MathHelperDivisionAction;
+import com.bytechef.component.math.helper.action.MathHelperModuloAction;
+import com.bytechef.component.math.helper.action.MathHelperMultiplicationAction;
+import com.bytechef.component.math.helper.action.MathHelperSubtractionAction;
+import com.google.auto.service.AutoService;
+
+/**
+ * @author Monika Kušter
+ */
+@AutoService(ComponentHandler.class)
+public class MathHelperComponentHandler implements ComponentHandler {
+
+    private static final ComponentDefinition COMPONENT_DEFINITION = component("mathHelper")
+        .title("Math Helper")
+        .description("Helper component to perform mathematical operations.")
+        .icon("path:assets/math-helper.svg")
+        .categories(ComponentCategory.HELPERS)
+        .actions(
+            MathHelperAdditionAction.ACTION_DEFINITION,
+            MathHelperDivisionAction.ACTION_DEFINITION,
+            MathHelperModuloAction.ACTION_DEFINITION,
+            MathHelperMultiplicationAction.ACTION_DEFINITION,
+            MathHelperSubtractionAction.ACTION_DEFINITION);
+
+    @Override
+    public ComponentDefinition getDefinition() {
+        return COMPONENT_DEFINITION;
+    }
+}
---
+++ server/libs/modules/components/math-helper/src/test/java/com/bytechef/component/math/helper/action/MathHelperAdditionActionTest.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2023-present ByteChef Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef.component.math.helper.action;
+
+import static com.bytechef.component.math.helper.constants.MathHelperConstants.FIRST_NUMBER;
+import static com.bytechef.component.math.helper.constants.MathHelperConstants.SECOND_NUMBER;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+
+import com.bytechef.component.definition.ActionContext;
+import com.bytechef.component.definition.Parameters;
+import com.bytechef.component.test.definition.MockParametersFactory;
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Monika Kušter
+ */
+class MathHelperAdditionActionTest {
+
+    private final Parameters mockedParameters = MockParametersFactory.create(Map.of(FIRST_NUMBER, 1, SECOND_NUMBER, 1));
+
+    @Test
+    void testPerform() {
+        Double result = MathHelperAdditionAction.perform(mockedParameters, mockedParameters, mock(ActionContext.class));
+
+        assertEquals(2.0, result);
+    }
+}
