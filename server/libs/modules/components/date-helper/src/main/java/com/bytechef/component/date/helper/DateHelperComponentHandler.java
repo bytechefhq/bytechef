12be147c5e3428542773dc1b7ecc2f4061187569
Monika Kušter <domitermonika@gmail.com>
2024-09-18 07:45:10 +0200

    1454 - add new actions to component definition

--- server/libs/modules/components/date-helper/src/main/java/com/bytechef/component/date/helper/DateHelperComponentHandler.java
+++ server/libs/modules/components/date-helper/src/main/java/com/bytechef/component/date/helper/DateHelperComponentHandler.java
@@ -19,7 +19,12 @@ package com.bytechef.component.date.helper;
 import static com.bytechef.component.definition.ComponentDsl.component;
 
 import com.bytechef.component.ComponentHandler;
+import com.bytechef.component.date.helper.action.DateHelperAddTimeAction;
 import com.bytechef.component.date.helper.action.DateHelperConvertAction;
+import com.bytechef.component.date.helper.action.DateHelperDateDifferenceAction;
+import com.bytechef.component.date.helper.action.DateHelperExtractDateUnitsAction;
+import com.bytechef.component.date.helper.action.DateHelperGetCurrentDateAction;
+import com.bytechef.component.date.helper.action.DateHelperSubtractTimeAction;
 import com.bytechef.component.definition.ComponentCategory;
 import com.bytechef.component.definition.ComponentDefinition;
 import com.google.auto.service.AutoService;
@@ -36,7 +41,13 @@ public class DateHelperComponentHandler implements ComponentHandler {
         .description("Helper component for date and time manipulation.")
         .icon("path:assets/date-helper.svg")
         .categories(ComponentCategory.HELPERS)
-        .actions(DateHelperConvertAction.ACTION_DEFINITION);
+        .actions(
+            DateHelperAddTimeAction.ACTION_DEFINITION,
+            DateHelperConvertAction.ACTION_DEFINITION,
+            DateHelperDateDifferenceAction.ACTION_DEFINITION,
+            DateHelperExtractDateUnitsAction.ACTION_DEFINITION,
+            DateHelperGetCurrentDateAction.ACTION_DEFINITION,
+            DateHelperSubtractTimeAction.ACTION_DEFINITION);
 
     @Override
     public ComponentDefinition getDefinition() {
