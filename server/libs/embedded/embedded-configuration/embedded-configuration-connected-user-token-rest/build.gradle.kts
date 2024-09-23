625de176a49f8c985f39072f270df864d7358a57
Ivica Čardić <ivica.cardic@gmail.com>
2024-09-23 09:01:31 +0200

    520 - Use environment value from the header

--- server/libs/embedded/embedded-configuration/embedded-configuration-connected-user-token-rest/build.gradle.kts
+++ server/libs/embedded/embedded-configuration/embedded-configuration-connected-user-token-rest/build.gradle.kts
@@ -38,6 +38,7 @@ dependencies {
 
     compileOnly("jakarta.servlet:jakarta.servlet-api:6.0.0")
 
+    implementation("org.apache.commons:commons-lang3")
     implementation(libs.io.swagger.core.v3.swagger.annotations)
     implementation(libs.org.openapitools.jackson.databind.nullable)
     implementation("jakarta.annotation:jakarta.annotation-api")
