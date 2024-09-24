34f992b4d59e4c1eeddaca48db551e21ccfdd67e
Ivica Čardić <ivica.cardic@gmail.com>
2024-09-24 09:25:43 +0200

    1023 - client - Generate

---
+++ client/src/middleware/automation/api-platform/models/index.ts
@@ -0,0 +1,8 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './ApiCollection';
+export * from './ApiCollectionEndpoint';
+export * from './Environment';
+export * from './HttpMethod';
+export * from './Tag';
+export * from './UpdateTagsRequest';
