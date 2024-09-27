b2ad0567c98a285d9d032de8da78e1df8b96e32c
Ivica Čardić <ivica.cardic@gmail.com>
2024-09-27 06:36:49 +0200

    1502 - client - Add description validation when publishing

--- client/src/pages/embedded/integration/components/integration-header/IntegrationHeaderPublishPopover.tsx
+++ client/src/pages/embedded/integration/components/integration-header/IntegrationHeaderPublishPopover.tsx
@@ -1,5 +1,5 @@
 import {Button} from '@/components/ui/button';
-import {Label} from '@/components/ui/label';
+import {Form, FormControl, FormField, FormItem, FormLabel, FormMessage} from '@/components/ui/form';
 import {Popover, PopoverContent, PopoverTrigger} from '@/components/ui/popover';
 import {Textarea} from '@/components/ui/textarea';
 import {Tooltip, TooltipContent, TooltipTrigger} from '@/components/ui/tooltip';
@@ -7,15 +7,27 @@ import {useToast} from '@/components/ui/use-toast';
 import {Integration} from '@/shared/middleware/embedded/configuration';
 import {usePublishIntegrationMutation} from '@/shared/mutations/embedded/integrations.mutations';
 import {IntegrationKeys} from '@/shared/queries/embedded/integrations.queries';
+import {zodResolver} from '@hookform/resolvers/zod';
 import {useQueryClient} from '@tanstack/react-query';
 import {CircleDotIcon} from 'lucide-react';
 import {useState} from 'react';
+import {useForm} from 'react-hook-form';
+import * as z from 'zod';
+
+const formSchema = z.object({
+    description: z.string().max(256),
+});
 
 const IntegrationHeaderPublishPopover = ({integration}: {integration: Integration}) => {
     const [open, setOpen] = useState(false);
-    const [description, setDescription] = useState<string | undefined>(undefined);
 
     const {toast} = useToast();
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+    });
+
+    const {control, handleSubmit} = form;
 
     const queryClient = useQueryClient();
 
@@ -37,6 +49,15 @@ const IntegrationHeaderPublishPopover = ({integration}: {integration: Integratio
         },
     });
 
+    function publishIntegration({description}: {description: string}) {
+        publishIntegrationMutation.mutate({
+            id: integration.id!,
+            publishIntegrationRequest: {
+                description,
+            },
+        });
+    }
+
     return (
         <Popover onOpenChange={setOpen} open={open}>
             <PopoverTrigger asChild>
@@ -52,29 +73,35 @@ const IntegrationHeaderPublishPopover = ({integration}: {integration: Integratio
             </PopoverTrigger>
 
             <PopoverContent align="end" className="flex h-full w-96 flex-col justify-between space-y-4">
-                <h3 className="font-semibold">Publish Integration</h3>
+                <Form {...form}>
+                    <form className="flex flex-col gap-4" onSubmit={handleSubmit(publishIntegration)}>
+                        <h3 className="font-semibold">Publish Integration</h3>
 
-                <div className="flex-1">
-                    <Label>Description</Label>
+                        <div className="flex-1">
+                            <FormField
+                                control={control}
+                                name="description"
+                                render={({field}) => (
+                                    <FormItem>
+                                        <FormLabel>Description</FormLabel>
 
-                    <Textarea className="h-28" onChange={(event) => setDescription(event.target.value)}></Textarea>
-                </div>
+                                        <FormControl>
+                                            <Textarea className="h-28" {...field}></Textarea>
+                                        </FormControl>
 
-                <div className="flex justify-end">
-                    <Button
-                        onClick={() =>
-                            publishIntegrationMutation.mutate({
-                                id: integration.id!,
-                                publishIntegrationRequest: {
-                                    description,
-                                },
-                            })
-                        }
-                        size="sm"
-                    >
-                        Publish
-                    </Button>
-                </div>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                        </div>
+
+                        <div className="flex justify-end">
+                            <Button size="sm" type="submit">
+                                Publish
+                            </Button>
+                        </div>
+                    </form>
+                </Form>
             </PopoverContent>
         </Popover>
     );
