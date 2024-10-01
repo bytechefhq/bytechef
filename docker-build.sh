ab500d51193670c9cd8e317900b296b517ad0b71
igorbeslic <igor.beslic@bitnaprednost.hr>
2024-10-01 17:29:14 +0200

    964 docker - improve build script - allow arguments, provide usage message to caller and tag with latest if caller requested

--- docker-build.sh
+++ docker-build.sh
@@ -1,7 +1,40 @@
 #!/bin/sh
 
+if [ -z $1 ]; then
+    echo "Required argument misses. Please provide docker image tag."
+    echo ""
+    echo "USAGE"
+    echo "    docker-build.sh tag [latest]"
+    echo "DESCRIPTION"
+    echo "    tag\t\t- arbitrary docker image tag. In bytechef we use yyyyMMdd to reflect date of image build"
+    echo "    latest\t- optional flag that would instruct script to tag image with additional tag with value \`latest\`"
+
+    exit 1
+fi
+
 cd server/apps/server-app
 ../../../gradlew clean build -Pprod
-docker build -t bytechef-server .
-cd ../../../
-docker build -t bytechef .
+
+if [ -n "$2" ] && [ "latest" = "$2" ]; then
+    echo "Building docker image with tag \`$2\`"
+
+    docker build --platform linux/amd64 -t bytechef/bytechef-server:$2 .
+fi
+
+docker build --platform linux/amd64 -t bytechef/bytechef-server:$1 .
+
+cd ../../../client
+
+npm install
+
+npm run build
+
+cd ..
+
+if [ -n "$2" ] && [ "latest" = "$2" ]; then
+    echo "Building docker image with tag \`$2\`"
+
+    docker build --platform linux/amd64 -t bytechef/bytechef:$2 .
+fi
+
+docker build --platform linux/amd64 -t bytechef/bytechef:$1 .
