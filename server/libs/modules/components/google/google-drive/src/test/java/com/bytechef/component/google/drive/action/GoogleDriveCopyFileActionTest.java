b01a7a4f75c814ae522aa315316b6471fbfd2136
Mayank Madan <madanmayank10@gmail.com>
2024-10-08 18:10:40 +0530

    1534 - SF

--- server/libs/modules/components/google/google-drive/src/test/java/com/bytechef/component/google/drive/action/GoogleDriveCopyFileActionTest.java
+++ server/libs/modules/components/google/google-drive/src/test/java/com/bytechef/component/google/drive/action/GoogleDriveCopyFileActionTest.java
@@ -16,46 +16,48 @@
 
 package com.bytechef.component.google.drive.action;
 
-import com.bytechef.component.definition.Parameters;
-import com.bytechef.component.test.definition.MockParametersFactory;
-import com.google.api.services.drive.model.File;
-import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
-
-import java.io.IOException;
-import java.util.Collections;
-import java.util.Map;
-
-import static com.bytechef.component.google.drive.constant.GoogleDriveConstants.*;
+import static com.bytechef.component.google.drive.constant.GoogleDriveConstants.FILE_ID;
+import static com.bytechef.component.google.drive.constant.GoogleDriveConstants.FILE_NAME;
+import static com.bytechef.component.google.drive.constant.GoogleDriveConstants.PARENT_FOLDER;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+
+import com.bytechef.component.definition.Parameters;
+import com.bytechef.component.test.definition.MockParametersFactory;
+import com.google.api.services.drive.model.File;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Map;
+import org.junit.jupiter.api.Test;
 
 /**
  * @author Mayank Madan
  */
 public class GoogleDriveCopyFileActionTest extends AbstractGoogleDriveActionTest {
-  private final Parameters mockInputParameters = MockParametersFactory.create(Map.of(FILE_ID, "originalFileId",
-          FILE_NAME, "newFileName", PARENT_FOLDER, "newFolderId"));
-  private final File testFile =
-          new File().setName("newFileName").setParents(Collections.singletonList("newFolderId")).setMimeType(
-                  "application/pdf");
+    private final Parameters mockInputParameters = MockParametersFactory.create(Map.of(FILE_ID, "originalFileId",
+        FILE_NAME, "newFileName", PARENT_FOLDER, "newFolderId"));
+    private final File testFile =
+        new File().setName("newFileName")
+            .setParents(Collections.singletonList("newFolderId"))
+            .setMimeType("application" +
+                "/pdf");
 
-  @Test
-  public void testPerform() throws IOException {
+    @Test
+    public void testPerform() throws IOException {
 
-    when(mockedFiles.get(fileIdArgumentCaptor.capture())).thenReturn(mockedGet);
-    when(mockedGet.execute()).thenReturn(testFile);
-    when(mockedFiles.copy("originalFileId", testFile)).thenReturn(mockedCopy);
-    when(mockedCopy.execute()).thenReturn(testFile);
+        when(mockedFiles.get(fileIdArgumentCaptor.capture())).thenReturn(mockedGet);
+        when(mockedGet.execute()).thenReturn(testFile);
+        when(mockedFiles.copy("originalFileId", testFile)).thenReturn(mockedCopy);
+        when(mockedCopy.execute()).thenReturn(testFile);
 
-    File copiedFile = GoogleDriveCopyFileAction.perform(mockInputParameters, mockInputParameters, mockedContext);
+        File copiedFile = GoogleDriveCopyFileAction.perform(mockInputParameters, mockInputParameters, mockedContext);
 
-    verify(mockedDrive.files()).get("originalFileId");
-    verify(mockedFiles).copy("originalFileId", testFile);
+        verify(mockedDrive.files()).get("originalFileId");
+        verify(mockedFiles).copy("originalFileId", testFile);
 
-    assertEquals("newFileName", copiedFile.getName());
-    assertEquals(Collections.singletonList("newFolderId"), copiedFile.getParents());
-    assertEquals("application/pdf", copiedFile.getMimeType());
-  }
+        assertEquals("newFileName", copiedFile.getName());
+        assertEquals(Collections.singletonList("newFolderId"), copiedFile.getParents());
+        assertEquals("application/pdf", copiedFile.getMimeType());
+    }
 }
