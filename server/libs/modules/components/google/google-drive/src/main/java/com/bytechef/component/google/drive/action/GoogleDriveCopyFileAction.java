fa1967aa1ebee561279c2a3818041d324e6278b0
Mayank Madan <madanmayank10@gmail.com>
2024-10-08 18:10:40 +0530

    1534 - SF

--- server/libs/modules/components/google/google-drive/src/main/java/com/bytechef/component/google/drive/action/GoogleDriveCopyFileAction.java
+++ server/libs/modules/components/google/google-drive/src/main/java/com/bytechef/component/google/drive/action/GoogleDriveCopyFileAction.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2023-present ByteChef Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.bytechef.component.google.drive.action;
 
 import static com.bytechef.component.definition.ComponentDsl.action;
@@ -8,9 +24,6 @@ import static com.bytechef.component.google.drive.constant.GoogleDriveConstants.
 import static com.bytechef.component.google.drive.constant.GoogleDriveConstants.FILE_NAME;
 import static com.bytechef.component.google.drive.constant.GoogleDriveConstants.PARENT_FOLDER;
 
-import java.io.IOException;
-import java.util.Collections;
-
 import com.bytechef.component.definition.ActionContext;
 import com.bytechef.component.definition.ComponentDsl.ModifiableActionDefinition;
 import com.bytechef.component.definition.OptionsDataSource.ActionOptionsFunction;
@@ -19,45 +32,52 @@ import com.bytechef.component.google.drive.util.GoogleDriveUtils;
 import com.bytechef.google.commons.GoogleServices;
 import com.google.api.services.drive.Drive;
 import com.google.api.services.drive.model.File;
+import java.io.IOException;
+import java.util.Collections;
 
 public class GoogleDriveCopyFileAction {
 
-        public static final ModifiableActionDefinition ACTION_DEFINITION = action("copyFile")
-                        .title("Copy file")
-                        .description("Copy a selected file to a different location within Google Drive.")
-                        .properties(
-                                        string(FILE_ID)
-                                                        .label("File")
-                                                        .description("The id of the file to be copied.")
-                                                        .options((ActionOptionsFunction<String>) GoogleDriveUtils::getFileOptions)
-                                                        .required(true),
-                                        string(FILE_NAME)
-                                                        .label("New file name")
-                                                        .description("The name of the new file created as a result of the copy operation.")
-                                                        .required(true),
-                                        string(PARENT_FOLDER)
-                                                        .label("Destination folder")
-                                                        .required(true)
-                                                        .description(
-                                                                        "The ID of the folder where the copied file will be stored.")
-                                                        .options((ActionOptionsFunction<String>) GoogleDriveUtils::getFolderOptions))
-                        .output(outputSchema(fileEntry()))
-                        .perform(GoogleDriveCopyFileAction::perform);
+    public static final ModifiableActionDefinition ACTION_DEFINITION = action("copyFile")
+        .title("Copy file")
+        .description("Copy a selected file to a different location within Google Drive.")
+        .properties(
+            string(FILE_ID)
+                .label("File")
+                .description("The id of the file to be copied.")
+                .options((ActionOptionsFunction<String>) GoogleDriveUtils::getFileOptions)
+                .required(true),
+            string(FILE_NAME)
+                .label("New file name")
+                .description("The name of the new file created as a result of the copy operation.")
+                .required(true),
+            string(PARENT_FOLDER)
+                .label("Destination folder")
+                .required(true)
+                .description(
+                    "The ID of the folder where the copied file will be stored.")
+                .options((ActionOptionsFunction<String>) GoogleDriveUtils::getFolderOptions))
+        .output(outputSchema(fileEntry()))
+        .perform(GoogleDriveCopyFileAction::perform);
 
-        private GoogleDriveCopyFileAction() {
-        }
+    private GoogleDriveCopyFileAction() {
+    }
 
-        public static File perform(Parameters inputParameters, Parameters connectionParameters,
-                        ActionContext actionContext) throws IOException {
-                Drive drive = GoogleServices.getDrive(connectionParameters);
-                File originalFile = drive.files().get(inputParameters.getRequiredString(FILE_ID)).execute();
-                // Setting parent to copy it to correct destination folder
-                File newFile = new File()
-                                .setName(inputParameters.getRequiredString(FILE_NAME))
-                                .setParents(Collections.singletonList(inputParameters.getRequiredString(PARENT_FOLDER)))
-                                .setMimeType(originalFile.getMimeType());
+    public static File perform(
+        Parameters inputParameters, Parameters connectionParameters,
+        ActionContext actionContext) throws IOException {
+        Drive drive = GoogleServices.getDrive(connectionParameters);
+        File originalFile = drive.files()
+            .get(inputParameters.getRequiredString(FILE_ID))
+            .execute();
+        // Setting parent to copy it to correct destination folder
+        File newFile = new File()
+            .setName(inputParameters.getRequiredString(FILE_NAME))
+            .setParents(Collections.singletonList(inputParameters.getRequiredString(PARENT_FOLDER)))
+            .setMimeType(originalFile.getMimeType());
 
-                return drive.files().copy(inputParameters.getRequiredString(FILE_ID), newFile).execute();
-        }
+        return drive.files()
+            .copy(inputParameters.getRequiredString(FILE_ID), newFile)
+            .execute();
+    }
 
 }
