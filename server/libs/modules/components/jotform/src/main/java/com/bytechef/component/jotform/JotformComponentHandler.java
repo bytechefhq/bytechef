1873063231dbc4e61affab64caca3cfb4a777cdf
Monika Kušter <domitermonika@gmail.com>
2024-10-09 14:49:40 +0200

    1603 - update connection definition, add form options

--- server/libs/modules/components/jotform/src/main/java/com/bytechef/component/jotform/JotformComponentHandler.java
+++ server/libs/modules/components/jotform/src/main/java/com/bytechef/component/jotform/JotformComponentHandler.java
@@ -16,10 +16,24 @@
 
 package com.bytechef.component.jotform;
 
+import static com.bytechef.component.definition.Authorization.KEY;
+import static com.bytechef.component.definition.Authorization.VALUE;
+import static com.bytechef.component.definition.ComponentDsl.authorization;
+import static com.bytechef.component.definition.ComponentDsl.option;
+import static com.bytechef.component.definition.ComponentDsl.string;
+
 import com.bytechef.component.OpenApiComponentHandler;
+import com.bytechef.component.definition.ActionDefinition;
+import com.bytechef.component.definition.Authorization.AuthorizationType;
 import com.bytechef.component.definition.ComponentCategory;
 import com.bytechef.component.definition.ComponentDsl.ModifiableComponentDefinition;
+import com.bytechef.component.definition.ComponentDsl.ModifiableConnectionDefinition;
+import com.bytechef.component.definition.ComponentDsl.ModifiableProperty;
+import com.bytechef.component.definition.ComponentDsl.ModifiableStringProperty;
+import com.bytechef.component.definition.OptionsDataSource.ActionOptionsFunction;
+import com.bytechef.component.jotform.util.JotformUtils;
 import com.google.auto.service.AutoService;
+import java.util.Objects;
 
 /**
  * @author Monika Kušter
@@ -27,11 +41,55 @@ import com.google.auto.service.AutoService;
 @AutoService(OpenApiComponentHandler.class)
 public class JotformComponentHandler extends AbstractJotformComponentHandler {
 
+    private static final String REGION = "region";
+
     @Override
     public ModifiableComponentDefinition modifyComponent(ModifiableComponentDefinition modifiableComponentDefinition) {
         return modifiableComponentDefinition
             .customAction(true)
             .icon("path:assets/jotform.svg")
             .categories(ComponentCategory.SURVEYS_AND_FEEDBACK);
+    }
+
+    @Override
+    public ModifiableConnectionDefinition modifyConnection(
+        ModifiableConnectionDefinition modifiableConnectionDefinition) {
+
+        return modifiableConnectionDefinition
+            .authorizations(
+                authorization(AuthorizationType.API_KEY)
+                    .title("API Key")
+                    .properties(
+                        string(REGION)
+                            .label("Region")
+                            .options(
+                                option("US (jotform.com)", "us"),
+                                option("EU (eu.jotform.com)", "eu"))
+                            .required(true),
+                        string(KEY)
+                            .label("Key")
+                            .required(true)
+                            .defaultValue("APIKEY")
+                            .hidden(true),
+                        string(VALUE)
+                            .label("API Key")
+                            .required(true)))
+            .baseUri((connectionParameters, context) -> {
+                String region = connectionParameters.getRequiredString(REGION);
+
+                return region.equals("us") ? "https://api.jotform.com" : "https://eu-api.jotform.com";
+            });
+    }
+
+    @Override
+    public ModifiableProperty<?> modifyProperty(
+        ActionDefinition actionDefinition, ModifiableProperty<?> modifiableProperty) {
+
+        if (Objects.equals(modifiableProperty.getName(), "formId")) {
+            ((ModifiableStringProperty) modifiableProperty)
+                .options((ActionOptionsFunction<String>) JotformUtils::getFormOptions);
+        }
+
+        return modifiableProperty;
     }
 }
