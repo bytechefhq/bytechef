994722fe1d01aca2c34b00bc6e5358da752e291b
Mayank Madan <madanmayank10@gmail.com>
2024-10-09 18:45:18 +0530

    1567 - Added test files

--- server/libs/modules/components/github/src/main/java/com/bytechef/component/github/action/GithubAddAssigneesToIssueAction.java
+++ server/libs/modules/components/github/src/main/java/com/bytechef/component/github/action/GithubAddAssigneesToIssueAction.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2023-present ByteChef Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.bytechef.component.github.action;
 
 import com.bytechef.component.definition.*;
@@ -16,6 +32,9 @@ import static com.bytechef.component.github.constant.GithubConstants.ISSUE_OUTPU
 import static com.bytechef.component.github.constant.GithubConstants.ASSIGNEES;
 import static com.bytechef.component.github.util.GithubUtils.getOwnerName;
 
+/**
+ * @author Mayank Madan
+ */
 public class GithubAddAssigneesToIssueAction {
 
 
