52900c28a103886e423770ecb2b55cb493eace3a
Aakarshit07 <aakarshit070@gmail.com>
2024-10-10 00:28:02 +0530

    1526 - client - integrated enter key to trigger workflow deletion

---
+++ client/src/shared/hooks/useKeyDown.ts
@@ -0,0 +1,27 @@
+import { useEffect } from 'react';
+
+export enum KeyboardKey {
+    enter = 'Enter',
+    // Add more keys as needed
+}
+
+export const useKeyDown = (callback: () => void, keys: KeyboardKey[]) => {
+  const onKeyDown = (event: KeyboardEvent) => {
+    const wasAnyKeyPressed = keys.some((key) => event.key === key);
+
+    if (wasAnyKeyPressed) {
+      event.preventDefault();
+      callback();
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('keydown', onKeyDown);
+
+    // Clean up event listener on unmount
+    return () => {
+      document.removeEventListener('keydown', onKeyDown);
+    };
+  }, [keys, callback]);
+};
+
