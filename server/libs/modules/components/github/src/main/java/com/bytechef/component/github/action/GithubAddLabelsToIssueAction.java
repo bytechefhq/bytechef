fc09b3b09e38dacf2485cba3a52215275e9b28af
Mayank Madan <madanmayank10@gmail.com>
2024-10-10 10:50:17 +0530

    1568 - SF

--- server/libs/modules/components/github/src/main/java/com/bytechef/component/github/action/GithubAddLabelsToIssueAction.java
+++ server/libs/modules/components/github/src/main/java/com/bytechef/component/github/action/GithubAddLabelsToIssueAction.java
@@ -16,25 +16,23 @@
 
 package com.bytechef.component.github.action;
 
-
 import static com.bytechef.component.definition.ComponentDsl.action;
 import static com.bytechef.component.definition.ComponentDsl.outputSchema;
 import static com.bytechef.component.definition.ComponentDsl.string;
 import static com.bytechef.component.definition.Context.Http.responseType;
-import static com.bytechef.component.github.constant.GithubConstants.ISSUE;
-import static com.bytechef.component.github.constant.GithubConstants.LABELS;
 import static com.bytechef.component.github.constant.GithubConstants.ADD_LABELS_TO_ISSUE;
+import static com.bytechef.component.github.constant.GithubConstants.ISSUE;
 import static com.bytechef.component.github.constant.GithubConstants.ISSUE_OUTPUT_PROPERTY;
+import static com.bytechef.component.github.constant.GithubConstants.LABELS;
 import static com.bytechef.component.github.constant.GithubConstants.REPOSITORY;
 import static com.bytechef.component.github.util.GithubUtils.getOwnerName;
 
-import com.bytechef.component.definition.OptionsDataSource;
-import com.bytechef.component.definition.Context;
-import com.bytechef.component.definition.Parameters;
 import com.bytechef.component.definition.ActionContext;
 import com.bytechef.component.definition.ComponentDsl.ModifiableActionDefinition;
+import com.bytechef.component.definition.Context;
+import com.bytechef.component.definition.OptionsDataSource;
+import com.bytechef.component.definition.Parameters;
 import com.bytechef.component.github.util.GithubUtils;
-
 import java.util.Collections;
 import java.util.Map;
 
@@ -43,45 +41,45 @@ import java.util.Map;
  */
 public class GithubAddLabelsToIssueAction {
 
-  public static final ModifiableActionDefinition ACTION_DEFINITION = action(ADD_LABELS_TO_ISSUE)
-    .title("Add labels to an issue")
-    .description("Adds labels to the specified issue.")
-    .properties(
-      string(REPOSITORY)
-        .options((OptionsDataSource.ActionOptionsFunction<String>) GithubUtils::getRepositoryOptions)
-        .label("Repository")
-        .required(true),
-      string(ISSUE)
-        .options((OptionsDataSource.ActionOptionsFunction<String>) GithubUtils::getIssueOptions)
-        .optionsLookupDependsOn(REPOSITORY)
-        .label("Issue")
-        .description("The issue to add labels to.")
-        .required(true),
-      string(LABELS)
-        .options((OptionsDataSource.ActionOptionsFunction<String>) GithubUtils::getLabels)
-        .optionsLookupDependsOn(REPOSITORY)
-        .label("Labels")
-        .description("The list of labels to add to the issue.")
-        .required(true))
-    .output(outputSchema(ISSUE_OUTPUT_PROPERTY))
-    .perform(GithubAddLabelsToIssueAction::perform);
+    public static final ModifiableActionDefinition ACTION_DEFINITION = action(ADD_LABELS_TO_ISSUE)
+        .title("Add labels to an issue")
+        .description("Adds labels to the specified issue.")
+        .properties(
+            string(REPOSITORY)
+                .options((OptionsDataSource.ActionOptionsFunction<String>) GithubUtils::getRepositoryOptions)
+                .label("Repository")
+                .required(true),
+            string(ISSUE)
+                .options((OptionsDataSource.ActionOptionsFunction<String>) GithubUtils::getIssueOptions)
+                .optionsLookupDependsOn(REPOSITORY)
+                .label("Issue")
+                .description("The issue to add labels to.")
+                .required(true),
+            string(LABELS)
+                .options((OptionsDataSource.ActionOptionsFunction<String>) GithubUtils::getLabels)
+                .optionsLookupDependsOn(REPOSITORY)
+                .label("Labels")
+                .description("The list of labels to add to the issue.")
+                .required(true))
+        .output(outputSchema(ISSUE_OUTPUT_PROPERTY))
+        .perform(GithubAddLabelsToIssueAction::perform);
 
-  private GithubAddLabelsToIssueAction() {
-  }
+    private GithubAddLabelsToIssueAction() {
+    }
 
-  public static Map<String, Object> perform(
-    Parameters inputParameters, Parameters connectionParameters, ActionContext context) {
+    public static Map<String, Object> perform(
+        Parameters inputParameters, Parameters connectionParameters, ActionContext context) {
 
-    // add label api returns back the list of added labels
-    context
-      .http(http -> http.post(
-        "/repos/" + getOwnerName(context) + "/" + inputParameters.getRequiredString(REPOSITORY)
-          + "/issues/" + inputParameters.getRequiredString(ISSUE) + "/labels"))
-      .body(Context.Http.Body.of(
-        Map.of(LABELS, Collections.singletonList(inputParameters.getRequiredString(LABELS)))))
-      .configuration(responseType(Context.Http.ResponseType.JSON))
-      .execute();
-    // Fetching update issue post label assignment
-    return GithubGetIssueAction.perform(inputParameters, connectionParameters, context);
-  }
+        // add label api returns back the list of added labels
+        context
+            .http(http -> http.post(
+                "/repos/" + getOwnerName(context) + "/" + inputParameters.getRequiredString(REPOSITORY)
+                    + "/issues/" + inputParameters.getRequiredString(ISSUE) + "/labels"))
+            .body(Context.Http.Body.of(
+                Map.of(LABELS, Collections.singletonList(inputParameters.getRequiredString(LABELS)))))
+            .configuration(responseType(Context.Http.ResponseType.JSON))
+            .execute();
+        // Fetching update issue post label assignment
+        return GithubGetIssueAction.perform(inputParameters, connectionParameters, context);
+    }
 }
--- server/libs/modules/components/github/src/test/java/com/bytechef/component/github/action/GithubAddLabelsToIssueActionTest.java
+++ server/libs/modules/components/github/src/test/java/com/bytechef/component/github/action/GithubAddLabelsToIssueActionTest.java
@@ -16,16 +16,14 @@
 
 package com.bytechef.component.github.action;
 
-import static com.bytechef.component.github.constant.GithubConstants.ASSIGNEES;
 import static com.bytechef.component.github.constant.GithubConstants.ISSUE;
-import static com.bytechef.component.github.constant.GithubConstants.REPOSITORY;
 import static com.bytechef.component.github.constant.GithubConstants.LABELS;
+import static com.bytechef.component.github.constant.GithubConstants.REPOSITORY;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import com.bytechef.component.definition.Context.Http;
 import com.bytechef.component.definition.Parameters;
 import com.bytechef.component.test.definition.MockParametersFactory;
-
 import java.util.Collections;
 import java.util.Map;
 import org.junit.jupiter.api.Test;
@@ -34,18 +32,18 @@ import org.junit.jupiter.api.Test;
  * @author Mayank Madan
  */
 class GithubAddLabelsToIssueActionTest extends AbstractGithubActionTest {
-  private final Parameters mockedParameters = MockParametersFactory.create(
-    Map.of(REPOSITORY, "testRepo", ISSUE, "testIssue", LABELS, "help-wanted"));
+    private final Parameters mockedParameters = MockParametersFactory.create(
+        Map.of(REPOSITORY, "testRepo", ISSUE, "testIssue", LABELS, "help-wanted"));
 
-  @Test
-  void testPerform() {
-    Map<String, Object> result =
-      GithubAddLabelsToIssueAction.perform(mockedParameters, mockedParameters, mockedContext);
+    @Test
+    void testPerform() {
+        Map<String, Object> result =
+            GithubAddLabelsToIssueAction.perform(mockedParameters, mockedParameters, mockedContext);
 
-    assertEquals(responseMap, result);
+        assertEquals(responseMap, result);
 
-    Http.Body body = bodyArgumentCaptor.getValue();
+        Http.Body body = bodyArgumentCaptor.getValue();
 
-    assertEquals(Map.of(LABELS, Collections.singletonList("help-wanted")), body.getContent());
-  }
+        assertEquals(Map.of(LABELS, Collections.singletonList("help-wanted")), body.getContent());
+    }
 }
