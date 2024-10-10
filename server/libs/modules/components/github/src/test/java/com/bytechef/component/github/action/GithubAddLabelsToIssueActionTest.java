bb38516f399a3d502550c4f602191242ed1be427
Mayank Madan <madanmayank10@gmail.com>
2024-10-10 10:46:52 +0530

    1568 - Added test cases

---
+++ server/libs/modules/components/github/src/test/java/com/bytechef/component/github/action/GithubAddLabelsToIssueActionTest.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2023-present ByteChef Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef.component.github.action;
+
+import static com.bytechef.component.github.constant.GithubConstants.ASSIGNEES;
+import static com.bytechef.component.github.constant.GithubConstants.ISSUE;
+import static com.bytechef.component.github.constant.GithubConstants.REPOSITORY;
+import static com.bytechef.component.github.constant.GithubConstants.LABELS;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.bytechef.component.definition.Context.Http;
+import com.bytechef.component.definition.Parameters;
+import com.bytechef.component.test.definition.MockParametersFactory;
+
+import java.util.Collections;
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Mayank Madan
+ */
+class GithubAddLabelsToIssueActionTest extends AbstractGithubActionTest {
+  private final Parameters mockedParameters = MockParametersFactory.create(
+    Map.of(REPOSITORY, "testRepo", ISSUE, "testIssue", LABELS, "help-wanted"));
+
+  @Test
+  void testPerform() {
+    Map<String, Object> result =
+      GithubAddLabelsToIssueAction.perform(mockedParameters, mockedParameters, mockedContext);
+
+    assertEquals(responseMap, result);
+
+    Http.Body body = bodyArgumentCaptor.getValue();
+
+    assertEquals(Map.of(LABELS, Collections.singletonList("help-wanted")), body.getContent());
+  }
+}
