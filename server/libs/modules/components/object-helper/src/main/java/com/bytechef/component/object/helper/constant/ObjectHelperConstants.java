06280f0aebc312b4794862c64ab6ca129606e476
Iamsamang, Jirawat <j.iamsamang@tue.nl>
2024-10-10 15:07:20 +0200

    1540 - Add workflow action: Object-helper AddValueByKeyAction

--- server/libs/modules/components/object-helper/src/main/java/com/bytechef/component/object/helper/constant/ObjectHelperConstants.java
+++ server/libs/modules/components/object-helper/src/main/java/com/bytechef/component/object/helper/constant/ObjectHelperConstants.java
@@ -16,14 +16,35 @@
 
 package com.bytechef.component.object.helper.constant;
 
+import static com.bytechef.component.definition.ComponentDsl.option;
+
+import com.bytechef.component.definition.Option;
+import java.util.List;
+
 /**
  * @author Ivica Cardic
  */
 public class ObjectHelperConstants {
 
+    public static final String ADD_VALUE_BY_KEY = "addValueByKey";
     public static final String OBJECT_HELPER = "objectHelper";
     public static final String PARSE = "parse";
-    public static final String STRINGIFY = "stringify";
     public static final String SOURCE = "source";
+    public static final String STRINGIFY = "stringify";
     public static final String TYPE = "type";
+    public static final String KEY = "key";
+    public static final String VALUE = "value";
+    public static final String VALUE_TYPE = "valueType";
+
+    public static final List<Option<Long>> TYPE_OPTIONS = List.of(
+        option("Array", 1),
+        option("Boolean", 2),
+        option("Date", 3),
+        option("Date Time", 4),
+        option("Integer", 5),
+        option("Nullable", 6),
+        option("Number", 7),
+        option("Object", 8),
+        option("String", 9),
+        option("Time", 10));
 }
