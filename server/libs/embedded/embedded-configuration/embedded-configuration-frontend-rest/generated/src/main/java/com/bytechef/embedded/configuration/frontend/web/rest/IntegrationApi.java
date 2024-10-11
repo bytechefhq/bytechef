804573007955f0bedd5f1fd1e938f149ed5a4ce6
Ivica Čardić <ivica.cardic@gmail.com>
2024-10-11 12:35:20 +0200

    520 - Generate

--- server/libs/embedded/embedded-configuration/embedded-configuration-frontend-rest/generated/src/main/java/com/bytechef/embedded/configuration/frontend/web/rest/IntegrationApi.java
+++ server/libs/embedded/embedded-configuration/embedded-configuration-frontend-rest/generated/src/main/java/com/bytechef/embedded/configuration/frontend/web/rest/IntegrationApi.java
@@ -5,12 +5,15 @@
  */
 package com.bytechef.embedded.configuration.frontend.web.rest;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
 import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import org.springframework.http.HttpStatus;
@@ -19,12 +22,16 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.multipart.MultipartFile;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.*;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import jakarta.annotation.Generated;
 
-@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2024-09-23T07:57:42.823273+02:00[Europe/Zagreb]", comments = "Generator version: 7.8.0")
+@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2024-10-11T12:35:07.912715+02:00[Europe/Zagreb]", comments = "Generator version: 7.8.0")
 @Validated
 @Tag(name = "integration", description = "The Embedded Integration by Connected User Token accessible API")
 public interface IntegrationApi {
@@ -37,6 +44,7 @@ public interface IntegrationApi {
      * GET /integrations : Get active configurations
      * Get active integrations.
      *
+     * @param connectedUserId The id of a connected user. (required)
      * @param xEnvironment The environment. (optional)
      * @return The list of active integrations. (status code 200)
      */
@@ -56,8 +64,9 @@ public interface IntegrationApi {
         value = "/integrations",
         produces = { "application/json" }
     )
-
+    
     default ResponseEntity<List<com.bytechef.embedded.configuration.public_.web.rest.model.IntegrationModel>> getIntegrations(
+        @NotNull @Parameter(name = "connectedUserId", description = "The id of a connected user.", required = true, in = ParameterIn.QUERY) @Valid @RequestParam(value = "connectedUserId", required = true) Long connectedUserId,
         @Parameter(name = "x-environment", description = "The environment.", in = ParameterIn.HEADER) @RequestHeader(value = "x-environment", required = false) com.bytechef.embedded.configuration.public_.web.rest.model.EnvironmentModel xEnvironment
     ) {
         getRequest().ifPresent(request -> {
