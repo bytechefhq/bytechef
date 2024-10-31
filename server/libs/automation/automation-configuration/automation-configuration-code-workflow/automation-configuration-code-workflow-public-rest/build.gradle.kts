fdea843343efc487cbbf7c6ad404c6a02f9c4c20
Ivica Cardic <ivica.cardic@gmail.com>
2024-10-31 18:22:00 +0100

    1025 - Move automation code-workflow logic under automation

---
+++ server/libs/automation/automation-configuration/automation-configuration-code-workflow/automation-configuration-code-workflow-public-rest/build.gradle.kts
@@ -0,0 +1,57 @@
+plugins {
+    alias(libs.plugins.org.openapi.generator)
+}
+
+val generateOpenAPISpring by tasks.registering(org.openapitools.generator.gradle.plugin.tasks.GenerateTask::class) {
+    apiPackage.set("com.bytechef.automation.configuration.codeworkflow.public_.web.rest")
+    configOptions.set(
+        mapOf(
+            "dateLibrary" to "java8-localdatetime",
+            "interfaceOnly" to "true",
+            "useSpringBoot3" to "true",
+            "useTags" to "true"
+        )
+    )
+    generatorName.set("spring")
+    inputSpec.set( "$projectDir/openapi.yaml")
+    modelNameSuffix.set("Model")
+    modelPackage.set("com.bytechef.automation.configuration.codeworkflow.public_.web.rest.model")
+    outputDir.set("$projectDir/generated")
+    schemaMappings.set(
+        mapOf(
+            "Page" to "org.springframework.data.domain.Page",
+        )
+    )
+}
+
+sourceSets.main.get().java.srcDir("$projectDir/generated/src/main/java")
+
+tasks.register("generateOpenAPI") {
+    dependsOn(generateOpenAPISpring)
+}
+
+dependencies {
+    annotationProcessor(libs.org.mapstruct.mapstruct.processor)
+    annotationProcessor(libs.org.mapstruct.extensions.spring.mapstruct.spring.extensions)
+
+    compileOnly("jakarta.servlet:jakarta.servlet-api")
+
+    implementation("org.apache.commons:commons-lang3")
+    implementation(libs.io.swagger.core.v3.swagger.annotations)
+    implementation(libs.org.openapitools.jackson.databind.nullable)
+    implementation("jakarta.annotation:jakarta.annotation-api")
+    implementation("jakarta.validation:jakarta.validation-api")
+    implementation(libs.org.mapstruct)
+    implementation(libs.org.mapstruct.extensions.spring.mapstruct.spring.annotations)
+    implementation("org.slf4j:slf4j-api")
+    implementation("org.springframework:spring-context")
+    implementation("org.springframework:spring-web")
+    implementation("org.springframework.boot:spring-boot-autoconfigure")
+    implementation(project(":server:libs:core:commons:commons-util"))
+    implementation(project(":server:libs:automation:automation-configuration:automation-configuration-api"))
+    implementation(project(":server:libs:automation:automation-configuration:automation-configuration-code-workflow:automation-configuration-code-workflow-api"))
+    implementation(project(":server:libs:platform:platform-code-workflow:platform-code-workflow-configuration:platform-code-workflow-configuration-api"))
+
+    testImplementation("org.springframework:spring-webflux")
+    testImplementation("org.springframework.boot:spring-boot-starter-web")
+}
