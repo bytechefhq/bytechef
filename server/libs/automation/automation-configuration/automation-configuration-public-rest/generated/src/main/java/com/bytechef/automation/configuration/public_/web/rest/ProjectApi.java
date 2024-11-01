256cec358808d048b76557883e832c7675cd6c56
Ivica Cardic <ivica.cardic@gmail.com>
2024-11-01 16:42:59 +0100

    1025 - Generate

--- server/libs/automation/automation-configuration/automation-configuration-public-rest/generated/src/main/java/com/bytechef/automation/configuration/public_/web/rest/ProjectApi.java
+++ server/libs/automation/automation-configuration/automation-configuration-public-rest/generated/src/main/java/com/bytechef/automation/configuration/public_/web/rest/ProjectApi.java
@@ -5,11 +5,19 @@
  */
 package com.bytechef.automation.configuration.public_.web.rest;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -17,13 +25,15 @@ import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.multipart.MultipartFile;
 
 import jakarta.validation.Valid;
-
+import jakarta.validation.constraints.*;
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import jakarta.annotation.Generated;
 
-@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2024-10-28T06:11:38.459545+01:00[Europe/Zagreb]", comments = "Generator version: 7.9.0")
+@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2024-11-01T16:42:36.483090+01:00[Europe/Zagreb]", comments = "Generator version: 7.9.0")
 @Validated
-@Tag(name = "project-code-workflow", description = "The Platform Code-Based Project Public API")
+@Tag(name = "project", description = "The Automation Project Public API")
 public interface ProjectApi {
 
     default Optional<NativeWebRequest> getRequest() {
@@ -42,7 +52,7 @@ public interface ProjectApi {
         operationId = "deployProject",
         summary = "Deploy a new code based project",
         description = "Deploy a new code based project.",
-        tags = { "project-code-workflow" },
+        tags = { "project" },
         responses = {
             @ApiResponse(responseCode = "204", description = "Successful operation.")
         }
@@ -52,7 +62,7 @@ public interface ProjectApi {
         value = "/projects/deploy",
         consumes = { "multipart/form-data" }
     )
-
+    
     default ResponseEntity<Void> deployProject(
         @Parameter(name = "workspaceId", description = "The id of a workspace this project will belong.") @Valid @RequestParam(value = "workspaceId", required = false) Long workspaceId,
         @Parameter(name = "projectFile", description = "The file of a code-native project.") @RequestPart(value = "projectFile", required = false) MultipartFile projectFile
