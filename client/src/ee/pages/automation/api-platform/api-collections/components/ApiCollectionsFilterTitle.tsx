cfb925236810e56a06be99a70fb99380ac0cd88d
Ivica Cardic <ivica.cardic@gmail.com>
2024-11-02 09:12:38 +0100

    1023 - client - Add filter title to api collections page

--- client/src/ee/pages/automation/api-platform/api-collections/components/ApiCollectionsFilterTitle.tsx
+++ client/src/ee/pages/automation/api-platform/api-collections/components/ApiCollectionsFilterTitle.tsx
@@ -10,7 +10,7 @@ const ApiCollectionsFilterTitle = ({
     projects,
     tags,
 }: {
-    environment: number;
+    environment?: number;
     filterData: {id?: number; type: Type};
     projects: Project[] | undefined;
     tags: Tag[] | undefined;
@@ -30,7 +30,9 @@ const ApiCollectionsFilterTitle = ({
             <span className="text-sm uppercase text-muted-foreground">Filter by environment:</span>
 
             <Badge variant="secondary">
-                <span className="text-sm">{environment === 1 ? 'Test' : 'Production'}</span>
+                <span className="text-sm">
+                    {environment === undefined ? 'All environments' : environment === 1 ? 'Test' : 'Production'}
+                </span>
             </Badge>
 
             <span className="text-sm uppercase text-muted-foreground">
