5d70e1e56b37a40dce81d959305f468b86f7f7c3
Ivica Cardic <ivica.cardic@gmail.com>
2024-11-05 08:31:21 +0100

    521 - Generate

--- server/libs/embedded/embedded-execution/embedded-execution-public-rest/generated/src/main/java/com/bytechef/embedded/execution/public_/web/rest/model/EnvironmentModel.java
+++ server/libs/embedded/embedded-execution/embedded-execution-public-rest/generated/src/main/java/com/bytechef/embedded/execution/public_/web/rest/model/EnvironmentModel.java
@@ -20,7 +20,7 @@ import com.fasterxml.jackson.annotation.JsonValue;
  * The environment of a project.
  */
 
-@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2024-10-27T13:08:41.101258+01:00[Europe/Zagreb]", comments = "Generator version: 7.9.0")
+@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2024-11-04T19:39:51.421432+01:00[Europe/Zagreb]", comments = "Generator version: 7.9.0")
 public enum EnvironmentModel {
   
   TEST("TEST"),
@@ -46,7 +46,7 @@ public enum EnvironmentModel {
   @JsonCreator
   public static EnvironmentModel fromValue(String value) {
     for (EnvironmentModel b : EnvironmentModel.values()) {
-      if (b.value.equals(value)) {
+      if (b.value.equalsIgnoreCase(value)) {
         return b;
       }
     }
