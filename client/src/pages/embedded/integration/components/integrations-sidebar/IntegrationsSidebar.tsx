57c6d41c71bd308686c538e5b4018c633eeb076b
Ivica Cardic <ivica.cardic@gmail.com>
2024-11-06 10:38:24 +0100

    520 - client - Set small text for names

--- client/src/pages/embedded/integration/components/integrations-sidebar/IntegrationsSidebar.tsx
+++ client/src/pages/embedded/integration/components/integrations-sidebar/IntegrationsSidebar.tsx
@@ -26,7 +26,7 @@ const IntegrationsSidebar = ({integrationId}: {integrationId: number}) => {
                     .map((curIntegration) => (
                         <div
                             className={twMerge(
-                                'py-3 px-2 flex cursor-pointer items-center justify-between hover:bg-background/50 rounded-lg',
+                                'py-3 px-2 flex cursor-pointer items-center justify-between hover:bg-background/50 rounded-lg text-sm',
                                 curIntegration.id === integrationId && 'bg-background/50'
                             )}
                             key={curIntegration.id}
