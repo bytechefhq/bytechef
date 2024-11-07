42abf85e2fca169c4621dc553b6631571081720a
Monika Kušter <domitermonika@gmail.com>
2024-11-07 14:39:27 +0100

    1390 - update connection definition, add dynamic options

--- server/libs/modules/components/stripe/src/main/java/com/bytechef/component/stripe/StripeComponentHandler.java
+++ server/libs/modules/components/stripe/src/main/java/com/bytechef/component/stripe/StripeComponentHandler.java
@@ -16,10 +16,30 @@
 
 package com.bytechef.component.stripe;
 
+import static com.bytechef.component.definition.Authorization.AUTHORIZATION;
+import static com.bytechef.component.definition.Authorization.TOKEN;
+import static com.bytechef.component.definition.ComponentDsl.authorization;
+import static com.bytechef.component.definition.ComponentDsl.string;
+
 import com.bytechef.component.OpenApiComponentHandler;
+import com.bytechef.component.definition.ActionDefinition;
+import com.bytechef.component.definition.Authorization;
+import com.bytechef.component.definition.Authorization.AuthorizationType;
 import com.bytechef.component.definition.ComponentCategory;
 import com.bytechef.component.definition.ComponentDsl.ModifiableComponentDefinition;
+import com.bytechef.component.definition.ComponentDsl.ModifiableConnectionDefinition;
+import com.bytechef.component.definition.ComponentDsl.ModifiableObjectProperty;
+import com.bytechef.component.definition.ComponentDsl.ModifiableProperty;
+import com.bytechef.component.definition.ComponentDsl.ModifiableStringProperty;
+import com.bytechef.component.definition.OptionsDataSource.ActionOptionsFunction;
+import com.bytechef.component.definition.Property.ValueProperty;
+import com.bytechef.component.stripe.util.StripeUtils;
+import com.bytechef.definition.BaseProperty;
 import com.google.auto.service.AutoService;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
 
 /**
  * @author Monika Kušter
@@ -33,6 +53,44 @@ public class StripeComponentHandler extends AbstractStripeComponentHandler {
             .customAction(true)
             .icon("path:assets/stripe.svg")
             .categories(ComponentCategory.PAYMENT_PROCESSING);
+    }
+
+    @Override
+    public ModifiableConnectionDefinition modifyConnection(
+        ModifiableConnectionDefinition modifiableConnectionDefinition) {
+
+        return modifiableConnectionDefinition
+            .baseUri((connectionParameters, context) -> "https://api.stripe.com/v1")
+            .authorizations(authorization(AuthorizationType.BEARER_TOKEN)
+                .title("Bearer Token")
+                .properties(
+                    string(TOKEN)
+                        .label("Token")
+                        .required(true))
+                .apply((connectionParameters, context) -> Authorization.ApplyResponse
+                    .ofHeaders(
+                        Map.of(
+                            "Content-Type", List.of("application/x-www-form-urlencoded"),
+                            AUTHORIZATION, List.of("Bearer " + connectionParameters.getRequiredString(TOKEN))))));
+    }
+
+    @Override
+    public ModifiableProperty<?> modifyProperty(
+        ActionDefinition actionDefinition, ModifiableProperty<?> modifiableProperty) {
+
+        if (Objects.equals(modifiableProperty.getName(), "__item")) {
+            Optional<List<? extends ValueProperty<?>>> propertiesOptional =
+                ((ModifiableObjectProperty) modifiableProperty).getProperties();
+
+            for (BaseProperty baseProperty : propertiesOptional.get()) {
+                if (Objects.equals(baseProperty.getName(), "customer")) {
+                    ((ModifiableStringProperty) baseProperty)
+                        .options((ActionOptionsFunction<String>) StripeUtils::getCustomerOptions);
+                }
+            }
+        }
+
+        return modifiableProperty;
     }
 
 }
