8f754c909454de95a9cbfdc083bc6ff76ad19e29
Monika Kušter <domitermonika@gmail.com>
2024-11-07 14:39:27 +0100

    1390 - update connection definition, add dynamic options

---
+++ server/libs/modules/components/stripe/src/main/java/com/bytechef/component/stripe/util/StripeUtils.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2023-present ByteChef Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef.component.stripe.util;
+
+import static com.bytechef.component.definition.ComponentDsl.option;
+import static com.bytechef.component.stripe.constant.StripeConstants.ID;
+
+import com.bytechef.component.definition.ActionContext;
+import com.bytechef.component.definition.Context.Http;
+import com.bytechef.component.definition.Option;
+import com.bytechef.component.definition.Parameters;
+import com.bytechef.component.definition.TypeReference;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author Monika Kušter
+ */
+public class StripeUtils {
+
+    public static List<Option<String>> getCustomerOptions(
+        Parameters inputParameters, Parameters connectionParameters, Map<String, String> dependencyPaths,
+        String searchText, ActionContext context) {
+
+        Map<String, Object> body = context.http(
+            http -> http.get("/customers"))
+            .configuration(Http.responseType(Http.ResponseType.JSON))
+            .execute()
+            .getBody(new TypeReference<>() {});
+
+        List<Option<String>> options = new ArrayList<>();
+
+        if (body.get("data") instanceof List<?> list) {
+            for (Object object : list) {
+                if (object instanceof Map<?, ?> map) {
+                    options.add(option((String) map.get("name"), (String) map.get(ID)));
+                }
+            }
+        }
+        return options;
+
+    }
+}
