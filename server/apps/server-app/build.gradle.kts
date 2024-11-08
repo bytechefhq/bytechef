fbd60efd9b0017a951bdb308d48ee4c073fbe4fa
Monika Kušter <domitermonika@gmail.com>
2024-11-08 09:54:31 +0100

    1391 - initial Calendly component

--- server/apps/server-app/build.gradle.kts
+++ server/apps/server-app/build.gradle.kts
@@ -158,6 +158,7 @@ dependencies {
     implementation(project(":server:libs:modules:components:bash"))
     implementation(project(":server:libs:modules:components:beamer"))
     implementation(project(":server:libs:modules:components:box"))
+    implementation(project(":server:libs:modules:components:calendly"))
     implementation(project(":server:libs:modules:components:capsule-crm"))
     implementation(project(":server:libs:modules:components:chat"))
     implementation(project(":server:libs:modules:components:clickup"))
