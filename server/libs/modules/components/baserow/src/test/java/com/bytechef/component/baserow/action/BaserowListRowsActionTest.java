7feaf7319af02163a9b5a38f0b8736571b94d399
Monika Kušter <domitermonika@gmail.com>
2024-11-08 12:40:41 +0100

    1566 - add tests

---
+++ server/libs/modules/components/baserow/src/test/java/com/bytechef/component/baserow/action/BaserowListRowsActionTest.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2023-present ByteChef Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef.component.baserow.action;
+
+import static com.bytechef.component.baserow.constant.BaserowConstants.ORDER_BY;
+import static com.bytechef.component.baserow.constant.BaserowConstants.SIZE;
+import static com.bytechef.component.baserow.constant.BaserowConstants.USER_FIELD_NAMES;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import com.bytechef.component.definition.ActionContext;
+import com.bytechef.component.definition.Context.Http;
+import com.bytechef.component.definition.Parameters;
+import com.bytechef.component.definition.TypeReference;
+import com.bytechef.component.test.definition.MockParametersFactory;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+
+/**
+ * @author Monika Kušter
+ */
+class BaserowListRowsActionTest {
+
+    private final ActionContext mockedActionContext = mock(ActionContext.class);
+    private final Http.Executor mockedExecutor = mock(Http.Executor.class);
+    private final Object mockedObject = mock(Object.class);
+    private final Parameters mockedParameters = MockParametersFactory.create(
+        Map.of(USER_FIELD_NAMES, true, SIZE, 100, ORDER_BY, "field"));
+    private final Http.Response mockedResponse = mock(Http.Response.class);
+    private final ArgumentCaptor<Object[]> queryArgumentCaptor = ArgumentCaptor.forClass(Object[].class);
+
+    @Test
+    void testPerform() {
+        when(mockedActionContext.http(any()))
+            .thenReturn(mockedExecutor);
+        when(mockedExecutor.queryParameters(queryArgumentCaptor.capture()))
+            .thenReturn(mockedExecutor);
+        when(mockedExecutor.configuration(any()))
+            .thenReturn(mockedExecutor);
+        when(mockedExecutor.execute())
+            .thenReturn(mockedResponse);
+        when(mockedResponse.getBody(any(TypeReference.class)))
+            .thenReturn(mockedObject);
+
+        Object result = BaserowListRowsAction.perform(mockedParameters, mockedParameters, mockedActionContext);
+
+        assertEquals(mockedObject, result);
+
+        Object[] query = queryArgumentCaptor.getValue();
+
+        assertEquals(List.of(USER_FIELD_NAMES, "true", SIZE, 100, ORDER_BY, "field"), Arrays.asList(query));
+    }
+}
