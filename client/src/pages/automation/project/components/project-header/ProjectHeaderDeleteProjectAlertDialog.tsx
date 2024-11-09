2d3d03705807e3e781a3f504a3a3e57a328d4af8
Krešimir Čoko <kresimircoko@gmail.com>
2024-11-09 09:14:38 +0100

    1627 - navigate to projects page after deleting a project

--- client/src/pages/automation/project/components/project-header/ProjectHeaderDeleteProjectAlertDialog.tsx
+++ client/src/pages/automation/project/components/project-header/ProjectHeaderDeleteProjectAlertDialog.tsx
@@ -9,29 +9,26 @@ import {
     AlertDialogTitle,
 } from '@/components/ui/alert-dialog';
 
-const ProjectHeaderDeleteProjectAlertDialog = ({onClose, onDelete}: {onClose: () => void; onDelete: () => void}) => {
-    return (
-        <AlertDialog open={true}>
-            <AlertDialogContent>
-                <AlertDialogHeader>
-                    <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+const ProjectHeaderDeleteProjectAlertDialog = ({onClose, onDelete}: {onClose: () => void; onDelete: () => void}) => (
+    <AlertDialog open>
+        <AlertDialogContent>
+            <AlertDialogHeader>
+                <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
 
-                    <AlertDialogDescription>
-                        This action cannot be undone. This will permanently delete the project and workflows it
-                        contains.
-                    </AlertDialogDescription>
-                </AlertDialogHeader>
+                <AlertDialogDescription>
+                    This action cannot be undone. This will permanently delete the project and workflows it contains.
+                </AlertDialogDescription>
+            </AlertDialogHeader>
 
-                <AlertDialogFooter>
-                    <AlertDialogCancel onClick={() => onClose()}>Cancel</AlertDialogCancel>
+            <AlertDialogFooter>
+                <AlertDialogCancel onClick={() => onClose()}>Cancel</AlertDialogCancel>
 
-                    <AlertDialogAction className="bg-destructive" onClick={() => onDelete()}>
-                        Delete
-                    </AlertDialogAction>
-                </AlertDialogFooter>
-            </AlertDialogContent>
-        </AlertDialog>
-    );
-};
+                <AlertDialogAction className="bg-destructive" onClick={() => onDelete()}>
+                    Delete
+                </AlertDialogAction>
+            </AlertDialogFooter>
+        </AlertDialogContent>
+    </AlertDialog>
+);
 
 export default ProjectHeaderDeleteProjectAlertDialog;
