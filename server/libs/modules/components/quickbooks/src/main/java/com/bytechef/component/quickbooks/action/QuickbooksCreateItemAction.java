43de55691e202a849f6fec41890e50d39e3b5a13
Monika Kušter <domitermonika@gmail.com>
2024-11-12 19:31:23 +0100

    684 - createItem action - add missing properties

--- server/libs/modules/components/quickbooks/src/main/java/com/bytechef/component/quickbooks/action/QuickbooksCreateItemAction.java
+++ server/libs/modules/components/quickbooks/src/main/java/com/bytechef/component/quickbooks/action/QuickbooksCreateItemAction.java
@@ -17,29 +17,42 @@
 package com.bytechef.component.quickbooks.action;
 
 import static com.bytechef.component.definition.ComponentDsl.action;
+import static com.bytechef.component.definition.ComponentDsl.dynamicProperties;
 import static com.bytechef.component.definition.ComponentDsl.number;
 import static com.bytechef.component.definition.ComponentDsl.object;
+import static com.bytechef.component.definition.ComponentDsl.option;
 import static com.bytechef.component.definition.ComponentDsl.outputSchema;
 import static com.bytechef.component.definition.ComponentDsl.string;
 import static com.bytechef.component.definition.Context.Http.responseType;
+import static com.bytechef.component.quickbooks.constant.QuickbooksConstants.ACCOUNT;
 import static com.bytechef.component.quickbooks.constant.QuickbooksConstants.ACTIVE;
+import static com.bytechef.component.quickbooks.constant.QuickbooksConstants.ASSET_ACCOUNT_REF;
 import static com.bytechef.component.quickbooks.constant.QuickbooksConstants.DOMAIN;
+import static com.bytechef.component.quickbooks.constant.QuickbooksConstants.EXPENSE_ACCOUNT_REF;
 import static com.bytechef.component.quickbooks.constant.QuickbooksConstants.FULLY_QUALIFIED_NAME;
 import static com.bytechef.component.quickbooks.constant.QuickbooksConstants.ID;
+import static com.bytechef.component.quickbooks.constant.QuickbooksConstants.INCOME_ACCOUNT_REF;
+import static com.bytechef.component.quickbooks.constant.QuickbooksConstants.INVENTORY;
+import static com.bytechef.component.quickbooks.constant.QuickbooksConstants.INV_START_DATE;
 import static com.bytechef.component.quickbooks.constant.QuickbooksConstants.ITEM;
 import static com.bytechef.component.quickbooks.constant.QuickbooksConstants.NAME;
 import static com.bytechef.component.quickbooks.constant.QuickbooksConstants.QTY_ON_HAND;
+import static com.bytechef.component.quickbooks.constant.QuickbooksConstants.SERVICE;
 import static com.bytechef.component.quickbooks.constant.QuickbooksConstants.TYPE;
+import static com.bytechef.component.quickbooks.constant.QuickbooksConstants.VALUE;
 
 import com.bytechef.component.definition.ActionContext;
 import com.bytechef.component.definition.ComponentDsl.ModifiableActionDefinition;
 import com.bytechef.component.definition.Context.Http;
 import com.bytechef.component.definition.Parameters;
 import com.bytechef.component.definition.TypeReference;
+import com.bytechef.component.quickbooks.util.QuickbooksUtils;
+import java.util.Map;
 
 /**
  * @author Mario Cvjetojevic
  * @author Luka Ljubić
+ * @author Monika Kušter
  */
 public class QuickbooksCreateItemAction {
 
@@ -52,10 +65,27 @@ public class QuickbooksCreateItemAction {
                 .description("Name of the item.")
                 .maxLength(100)
                 .required(true),
+            string(TYPE)
+                .label("Type")
+                .description("Type of item.")
+                .options(
+                    option(INVENTORY, INVENTORY),
+                    option(SERVICE, SERVICE),
+                    option("Non-inventory", "NonInventory"))
+                .defaultValue(SERVICE)
+                .required(true),
+            dynamicProperties(ACCOUNT)
+                .propertiesLookupDependsOn(TYPE)
+                .properties(QuickbooksUtils::addPropertiesForItem),
+            string(EXPENSE_ACCOUNT_REF)
+                .label("Expense Account")
+                .options(QuickbooksUtils.getAccountOptions("Expense"))
+                .required(true),
             number(QTY_ON_HAND)
                 .label("Quantity on Hand")
-                .description("Current quantity of the Inventory items available for sale.")
-                .required(false))
+                .description("Current quantity of the inventory items available for sale.")
+                .displayCondition("%s == '%s'".formatted(TYPE, INVENTORY))
+                .required(true))
         .output(
             outputSchema(
                 object()
@@ -65,10 +95,18 @@ public class QuickbooksCreateItemAction {
                                 string(DOMAIN),
                                 string(ID),
                                 string(NAME),
-                                string("Description"),
                                 string(ACTIVE),
                                 string(FULLY_QUALIFIED_NAME),
-                                string(TYPE)))))
+                                string(TYPE),
+                                object(INCOME_ACCOUNT_REF)
+                                    .properties(
+                                        string("name")),
+                                object(ASSET_ACCOUNT_REF)
+                                    .properties(
+                                        string("name")),
+                                object(EXPENSE_ACCOUNT_REF)
+                                    .properties(
+                                        string("name"))))))
         .perform(QuickbooksCreateItemAction::perform);
 
     private QuickbooksCreateItemAction() {
@@ -77,12 +115,23 @@ public class QuickbooksCreateItemAction {
     protected static Object perform(
         Parameters inputParameters, Parameters connectionParameters, ActionContext actionContext) {
 
+        Map<String, String> account = inputParameters.getMap(ACCOUNT, String.class);
+
+        String incomeAccount = account.get(INCOME_ACCOUNT_REF);
+        String assetAccount = account.get(ASSET_ACCOUNT_REF);
+        String invStartDate = account.get(INV_START_DATE);
+        String expenseAccount = inputParameters.getString(EXPENSE_ACCOUNT_REF);
+
         return actionContext
             .http(http -> http.post("/item"))
             .body(
                 Http.Body.of(
                     NAME, inputParameters.getRequiredString(NAME),
-                    QTY_ON_HAND, inputParameters.getRequiredDouble(QTY_ON_HAND)))
+                    QTY_ON_HAND, inputParameters.getDouble(QTY_ON_HAND),
+                    INCOME_ACCOUNT_REF, incomeAccount == null ? null : Map.of(VALUE, incomeAccount),
+                    ASSET_ACCOUNT_REF, assetAccount == null ? null : Map.of(VALUE, assetAccount),
+                    INV_START_DATE, invStartDate,
+                    EXPENSE_ACCOUNT_REF, expenseAccount == null ? null : Map.of(VALUE, expenseAccount)))
             .configuration(responseType(Http.ResponseType.XML))
             .execute()
             .getBody(new TypeReference<>() {});
