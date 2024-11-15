beab47381821937cc9184f407b7826cb921ca0f7
Arina Kolodeznikova <arinak1017@gmail.com>
2024-11-15 14:07:55 -0500

    1642 - added test for delete row action and regenrated json definition for Baserow component test

---
+++ server/libs/modules/components/baserow/src/test/java/com/bytechef/component/baserow/action/BaserowDeleteRowActionTest.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2023-present ByteChef Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef.component.baserow.action;
+
+import static com.bytechef.component.baserow.constant.BaserowConstants.ROW_ID;
+import static com.bytechef.component.baserow.constant.BaserowConstants.TABLE_ID;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import com.bytechef.component.definition.ActionContext;
+import com.bytechef.component.definition.Context.Http;
+import com.bytechef.component.definition.Parameters;
+import com.bytechef.component.definition.TypeReference;
+import com.bytechef.component.test.definition.MockParametersFactory;
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+
+class BaserowDeleteRowActionTest {
+
+    private final ActionContext mockedActionContext = mock(ActionContext.class);
+    private final Http.Executor mockedExecutor = mock(Http.Executor.class);
+    private final Http.Response mockedResponse = mock(Http.Response.class);
+    private final Parameters mockedParameters = MockParametersFactory.create(Map.of(TABLE_ID, 1, ROW_ID, 1));
+
+    @Test
+    void testPerform() {
+        when(mockedActionContext.http(any()))
+            .thenReturn(mockedExecutor);
+        when(mockedExecutor.execute())
+            .thenReturn(mockedResponse);
+        when(mockedResponse.getBody(any(TypeReference.class)))
+            .thenReturn(null);
+
+        Object result = BaserowDeleteRowAction.perform(mockedParameters, mockedParameters, mockedActionContext);
+
+        assertNull(result);
+    }
+}
