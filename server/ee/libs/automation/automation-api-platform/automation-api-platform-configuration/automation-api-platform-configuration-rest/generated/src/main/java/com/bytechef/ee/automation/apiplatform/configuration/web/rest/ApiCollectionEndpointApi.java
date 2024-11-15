56ceb1dd6a61cefadef33ee2561603d0b971bb9d
Ivica Cardic <ivica.cardic@gmail.com>
2024-11-15 06:55:20 +0100

    1023 - Generate

--- server/ee/libs/automation/automation-api-platform/automation-api-platform-configuration/automation-api-platform-configuration-rest/generated/src/main/java/com/bytechef/ee/automation/apiplatform/configuration/web/rest/ApiCollectionEndpointApi.java
+++ server/ee/libs/automation/automation-api-platform/automation-api-platform-configuration/automation-api-platform-configuration-rest/generated/src/main/java/com/bytechef/ee/automation/apiplatform/configuration/web/rest/ApiCollectionEndpointApi.java
@@ -32,7 +32,7 @@ import java.util.Map;
 import java.util.Optional;
 import jakarta.annotation.Generated;
 
-@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2024-11-07T12:07:57.901582+01:00[Europe/Zagreb]", comments = "Generator version: 7.9.0")
+@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2024-11-15T12:48:42.005777+01:00[Europe/Zagreb]", comments = "Generator version: 7.9.0")
 @Validated
 @Tag(name = "api-collection-endpoint", description = "The Automation API Platform Collection Endpoint Internal API")
 public interface ApiCollectionEndpointApi {
---
+++ server/ee/libs/automation/automation-api-platform/automation-api-platform-configuration/automation-api-platform-configuration-rest/generated/src/main/java/com/bytechef/ee/automation/apiplatform/configuration/web/rest/model/CreateApiClient200ResponseModel.java
@@ -0,0 +1,85 @@
+package com.bytechef.ee.automation.apiplatform.configuration.web.rest.model;
+
+import java.net.URI;
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonTypeName;
+import org.openapitools.jackson.nullable.JsonNullable;
+import java.time.OffsetDateTime;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.*;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+
+import java.util.*;
+import jakarta.annotation.Generated;
+
+/**
+ * CreateApiClient200ResponseModel
+ */
+
+@JsonTypeName("createApiClient_200_response")
+@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2024-11-15T12:48:42.005777+01:00[Europe/Zagreb]", comments = "Generator version: 7.9.0")
+public class CreateApiClient200ResponseModel {
+
+  private String secretKey;
+
+  public CreateApiClient200ResponseModel secretKey(String secretKey) {
+    this.secretKey = secretKey;
+    return this;
+  }
+
+  /**
+   * The secret API key.
+   * @return secretKey
+   */
+  
+  @Schema(name = "secretKey", description = "The secret API key.", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+  @JsonProperty("secretKey")
+  public String getSecretKey() {
+    return secretKey;
+  }
+
+  public void setSecretKey(String secretKey) {
+    this.secretKey = secretKey;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    CreateApiClient200ResponseModel createApiClient200Response = (CreateApiClient200ResponseModel) o;
+    return Objects.equals(this.secretKey, createApiClient200Response.secretKey);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(secretKey);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class CreateApiClient200ResponseModel {\n");
+    sb.append("    secretKey: ").append(toIndentedString(secretKey)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
+
