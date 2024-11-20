b4ba42bee98cc20a9604d65324b2f1ea834f04f4
Monika Kušter <domitermonika@gmail.com>
2024-11-20 13:56:40 +0100

    1537 - implemented equals action

---
+++ server/libs/modules/components/object-helper/src/main/java/com/bytechef/component/object/helper/action/ObjectHelperEqualsAction.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2023-present ByteChef Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef.component.object.helper.action;
+
+import static com.bytechef.component.definition.ComponentDsl.action;
+import static com.bytechef.component.definition.ComponentDsl.bool;
+import static com.bytechef.component.definition.ComponentDsl.object;
+import static com.bytechef.component.definition.ComponentDsl.outputSchema;
+import static com.bytechef.component.object.helper.constant.ObjectHelperConstants.SOURCE;
+import static com.bytechef.component.object.helper.constant.ObjectHelperConstants.TARGET;
+
+import com.bytechef.component.definition.ActionContext;
+import com.bytechef.component.definition.ComponentDsl.ModifiableActionDefinition;
+import com.bytechef.component.definition.Parameters;
+
+/**
+ * @author Monika Kušter
+ */
+public class ObjectHelperEqualsAction {
+
+    public static final ModifiableActionDefinition ACTION_DEFINITION = action("equals")
+        .title("Equals")
+        .description("Compares two objects and returns true if they are equal.")
+        .properties(
+            object(SOURCE)
+                .label("Source")
+                .description("The source object to compare.")
+                .required(true),
+            object(TARGET)
+                .label("Target")
+                .description("The target object to compare against.")
+                .required(true))
+        .output(outputSchema(bool()))
+        .perform(ObjectHelperEqualsAction::perform);
+
+    private ObjectHelperEqualsAction() {
+    }
+
+    protected static Boolean perform(
+        Parameters inputParameters, Parameters connectionParameters, ActionContext actionContext) {
+
+        Object input = inputParameters.getRequired(SOURCE);
+        Object target = inputParameters.getRequired(TARGET);
+
+        return input.equals(target);
+    }
+}
