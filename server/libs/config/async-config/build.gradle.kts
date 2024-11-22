2c73c975d59735749bfaa5d377ff4eb13b6ed002
Ivica Cardic <ivica.cardic@gmail.com>
2024-11-22 06:32:44 +0100

    520 - Avoid warning message about wrong async task executor

--- server/libs/config/async-config/build.gradle.kts
+++ server/libs/config/async-config/build.gradle.kts
@@ -1,5 +1,6 @@
 dependencies {
     implementation("org.slf4j:slf4j-api")
+    implementation("org.springframework:spring-webmvc")
     implementation("org.springframework.boot:spring-boot-autoconfigure")
     implementation(project(":server:libs:platform:platform-tenant:platform-tenant-api"))
 }
--- server/libs/config/async-config/src/main/java/com/bytechef/async/config/AsyncConfiguration.java
+++ server/libs/config/async-config/src/main/java/com/bytechef/async/config/AsyncConfiguration.java
@@ -23,13 +23,17 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
 import org.springframework.aop.interceptor.SimpleAsyncUncaughtExceptionHandler;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.task.TaskExecutionProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.AsyncTaskExecutor;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.web.servlet.config.annotation.AsyncSupportConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * @author Ivica Cardic
@@ -68,5 +72,16 @@ public class AsyncConfiguration implements AsyncConfigurer {
     @Override
     public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
         return new SimpleAsyncUncaughtExceptionHandler();
+    }
+
+    @Bean
+    protected WebMvcConfigurer webMvcConfigurer(@Qualifier("taskExecutor") Executor executor) {
+        return new WebMvcConfigurer() {
+
+            @Override
+            public void configureAsyncSupport(AsyncSupportConfigurer configurer) {
+                configurer.setTaskExecutor((AsyncTaskExecutor) executor);
+            }
+        };
     }
 }
