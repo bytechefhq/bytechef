4bc68010316cd63e35492969c8cf08ebc41c113f
Monika Kušter <domitermonika@gmail.com>
2024-11-23 16:13:30 +0100

    1531 - implemented update event action

---
+++ server/libs/modules/components/google/google-calendar/src/main/java/com/bytechef/component/google/calendar/action/GoogleCalendarUpdateEventAction.java
@@ -0,0 +1,173 @@
+/*
+ * Copyright 2023-present ByteChef Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef.component.google.calendar.action;
+
+import static com.bytechef.component.definition.ComponentDsl.action;
+import static com.bytechef.component.definition.ComponentDsl.array;
+import static com.bytechef.component.definition.ComponentDsl.bool;
+import static com.bytechef.component.definition.ComponentDsl.date;
+import static com.bytechef.component.definition.ComponentDsl.dateTime;
+import static com.bytechef.component.definition.ComponentDsl.outputSchema;
+import static com.bytechef.component.definition.ComponentDsl.string;
+import static com.bytechef.component.google.calendar.constant.GoogleCalendarConstants.ALL_DAY;
+import static com.bytechef.component.google.calendar.constant.GoogleCalendarConstants.ATTENDEES;
+import static com.bytechef.component.google.calendar.constant.GoogleCalendarConstants.CALENDAR_ID;
+import static com.bytechef.component.google.calendar.constant.GoogleCalendarConstants.CALENDAR_ID_PROPERTY;
+import static com.bytechef.component.google.calendar.constant.GoogleCalendarConstants.DESCRIPTION;
+import static com.bytechef.component.google.calendar.constant.GoogleCalendarConstants.EMAIL;
+import static com.bytechef.component.google.calendar.constant.GoogleCalendarConstants.END;
+import static com.bytechef.component.google.calendar.constant.GoogleCalendarConstants.EVENT_ID;
+import static com.bytechef.component.google.calendar.constant.GoogleCalendarConstants.EVENT_OUTPUT_PROPERTY;
+import static com.bytechef.component.google.calendar.constant.GoogleCalendarConstants.START;
+import static com.bytechef.component.google.calendar.constant.GoogleCalendarConstants.SUMMARY;
+import static com.bytechef.component.google.calendar.util.GoogleCalendarUtils.createCustomEvent;
+import static com.bytechef.component.google.calendar.util.GoogleCalendarUtils.createEventDateTime;
+
+import com.bytechef.component.definition.ActionContext;
+import com.bytechef.component.definition.ComponentDsl.ModifiableActionDefinition;
+import com.bytechef.component.definition.OptionsDataSource.ActionOptionsFunction;
+import com.bytechef.component.definition.Parameters;
+import com.bytechef.component.google.calendar.util.GoogleCalendarUtils;
+import com.bytechef.component.google.calendar.util.GoogleCalendarUtils.CustomEvent;
+import com.bytechef.google.commons.GoogleServices;
+import com.google.api.services.calendar.Calendar;
+import com.google.api.services.calendar.model.Event;
+import com.google.api.services.calendar.model.EventAttendee;
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * @author Monika Kušter
+ */
+public class GoogleCalendarUpdateEventAction {
+
+    public static final ModifiableActionDefinition ACTION_DEFINITION = action("updateEvent")
+        .title("Update Event")
+        .description("Updates event in Google Calendar.")
+        .properties(
+            CALENDAR_ID_PROPERTY,
+            string(EVENT_ID)
+                .label("Event")
+                .description("Event to update.")
+                .options((ActionOptionsFunction<String>) GoogleCalendarUtils::getEventIdOptions)
+                .optionsLookupDependsOn(CALENDAR_ID)
+                .required(true),
+            string(SUMMARY)
+                .label("Title")
+                .description("New title of the event.")
+                .required(false),
+            bool(ALL_DAY)
+                .label("All Day Event?")
+                .required(false),
+            date(START)
+                .label("Start Date")
+                .description("New start date of the event.")
+                .displayCondition("%s == true".formatted(ALL_DAY))
+                .required(true),
+            date(END)
+                .label("End Date")
+                .description("New end date of the event.")
+                .displayCondition("%s == true".formatted(ALL_DAY))
+                .required(true),
+            dateTime(START)
+                .label("Start Date Time")
+                .description(
+                    "New (inclusive) start time of the event. For a recurring event, this is the start time of the " +
+                        "first instance.")
+                .displayCondition("%s == false".formatted(ALL_DAY))
+                .required(true),
+            dateTime(END)
+                .label("End Date Time")
+                .description(
+                    "New (exclusive) end time of the event. For a recurring event, this is the end time of the " +
+                        "first instance.")
+                .displayCondition("%s == false".formatted(ALL_DAY))
+                .required(true),
+            string(DESCRIPTION)
+                .label("Description")
+                .description("New description of the event. Can contain HTML.")
+                .required(false),
+            array(ATTENDEES)
+                .label("Attendees")
+                .description("New attendees of the event.")
+                .items(
+                    string(EMAIL)
+                        .label("Email")
+                        .description("The attendee's email address."))
+                .required(false))
+        .output(outputSchema(EVENT_OUTPUT_PROPERTY))
+        .perform(GoogleCalendarUpdateEventAction::perform);
+
+    private GoogleCalendarUpdateEventAction() {
+    }
+
+    public static CustomEvent perform(
+        Parameters inputParameters, Parameters connectionParameters, ActionContext actionContext) throws IOException {
+
+        Calendar calendar = GoogleServices.getCalendar(connectionParameters);
+
+        String calendarId = inputParameters.getRequiredString(CALENDAR_ID);
+        String eventId = inputParameters.getRequiredString(EVENT_ID);
+
+        Event currentEvent = calendar
+            .events()
+            .get(calendarId, eventId)
+            .execute();
+
+        List<String> attendees = inputParameters.getList(ATTENDEES, String.class, List.of());
+
+        if (!attendees.isEmpty()) {
+            List<EventAttendee> eventAttendees = attendees
+                .stream()
+                .map(attendee -> new EventAttendee().setEmail(attendee))
+                .toList();
+
+            List<EventAttendee> existingAttendees = currentEvent.getAttendees();
+            if (existingAttendees == null || existingAttendees.isEmpty()) {
+                currentEvent.setAttendees(eventAttendees);
+            } else {
+                currentEvent.getAttendees()
+                    .addAll(eventAttendees);
+            }
+        }
+
+        String description = inputParameters.getString(DESCRIPTION);
+
+        if (description != null) {
+            currentEvent.setDescription(description);
+        }
+
+        String summary = inputParameters.getString(SUMMARY);
+
+        if (summary != null) {
+            currentEvent.setSummary(summary);
+        }
+
+        if (inputParameters.getBoolean(ALL_DAY) != null) {
+            currentEvent
+                .setEnd(createEventDateTime(inputParameters, END))
+                .setStart(createEventDateTime(inputParameters, START));
+        }
+
+        Event updatedEvent = calendar
+            .events()
+            .update(calendarId, eventId, currentEvent)
+            .execute();
+
+        return createCustomEvent(updatedEvent);
+    }
+}
