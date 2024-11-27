4b74440fc083939bb1435e9716b6c4435a20265b
Ivica Cardic <ivica.cardic@gmail.com>
2024-11-27 08:02:06 +0100

    1639 - Add JSON schema generator, still does not work properly for all cases

---
+++ server/libs/platform/platform-configuration/platform-configuration-rest/platform-configuration-rest-impl/src/main/java/com/bytechef/platform/configuration/web/rest/WorkflowSchemaGeneratorApiController.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2023-present ByteChef Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef.platform.configuration.web.rest;
+
+import com.bytechef.atlas.coordinator.annotation.ConditionalOnCoordinator;
+import com.bytechef.platform.configuration.web.rest.schema.SchemaGenerator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Ivica Cardic
+ */
+@RestController
+@RequestMapping("${openapi.openAPIDefinition.base-path.platform:}/internal")
+@ConditionalOnCoordinator
+public class WorkflowSchemaGeneratorApiController {
+
+    private static final Logger logger = LoggerFactory.getLogger(WorkflowSchemaGeneratorApiController.class);
+
+    private final SchemaGenerator schemaGenerator;
+
+    public WorkflowSchemaGeneratorApiController(SchemaGenerator schemaGenerator) {
+        this.schemaGenerator = schemaGenerator;
+    }
+
+    @PostMapping("/generate-schema")
+    public String generateSchema(@RequestBody String json) {
+        return schemaGenerator.generateSchemaFromJson(json);
+    }
+}
