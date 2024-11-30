2720a386b79d0afd443bbb02182f6566466c2442
Ivica Cardic <ivica.cardic@gmail.com>
2024-11-30 08:34:54 +0100

    1808 - Generate

--- server/libs/platform/platform-configuration/platform-configuration-rest/platform-configuration-rest-api/generated/src/main/java/com/bytechef/platform/configuration/web/rest/model/TimePropertyModel.java
+++ server/libs/platform/platform-configuration/platform-configuration-rest/platform-configuration-rest-api/generated/src/main/java/com/bytechef/platform/configuration/web/rest/model/TimePropertyModel.java
@@ -33,7 +33,7 @@ import jakarta.annotation.Generated;
 
 @Schema(name = "TimeProperty", description = "A time property.")
 
-@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2024-11-27T22:14:33.227249+01:00[Europe/Zagreb]", comments = "Generator version: 7.10.0")
+@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2024-11-30T08:22:07.952410+01:00[Europe/Zagreb]", comments = "Generator version: 7.10.0")
 public class TimePropertyModel extends ValuePropertyModel {
 
   private String defaultValue;
