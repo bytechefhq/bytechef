1cff89e6bc7c74bd4775a235ab357359a68993bb
Ivica Cardic <ivica.cardic@gmail.com>
2024-11-30 17:41:53 +0100

    Revert "1713 - Don't invalidate related queries"
    This reverts commit f4145c2e229f384b06272ef136c784bf991b3b8e.

--- client/src/pages/automation/project/Project.tsx
+++ client/src/pages/automation/project/Project.tsx
@@ -187,6 +187,10 @@ const Project = () => {
                 queryKey: ProjectWorkflowKeys.projectWorkflows(parseInt(projectId!)),
             });
 
+            queryClient.invalidateQueries({
+                queryKey: ProjectWorkflowKeys.projectWorkflows(parseInt(projectId!)),
+            });
+
             setShowEditWorkflowDialog(false);
         },
         useUpdateWorkflowMutation: useUpdateWorkflowMutation,
