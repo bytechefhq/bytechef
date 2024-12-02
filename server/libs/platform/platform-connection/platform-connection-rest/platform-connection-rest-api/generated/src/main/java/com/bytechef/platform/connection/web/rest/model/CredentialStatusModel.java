7f36338f1c62634e0b666162801cc873f6c8a798
Monika Kušter <domitermonika@gmail.com>
2024-12-02 11:10:05 +0100

    1790 - fix imports

--- server/libs/platform/platform-connection/platform-connection-rest/platform-connection-rest-api/generated/src/main/java/com/bytechef/platform/connection/web/rest/model/CredentialStatusModel.java
+++ server/libs/platform/platform-connection/platform-connection-rest/platform-connection-rest-api/generated/src/main/java/com/bytechef/platform/connection/web/rest/model/CredentialStatusModel.java
@@ -1,20 +1,8 @@
 package com.bytechef.platform.connection.web.rest.model;
 
-import java.net.URI;
-import java.util.Objects;
-import com.fasterxml.jackson.annotation.JsonValue;
-import org.openapitools.jackson.nullable.JsonNullable;
-import java.time.OffsetDateTime;
-import jakarta.validation.Valid;
-import jakarta.validation.constraints.*;
-import io.swagger.v3.oas.annotations.media.Schema;
-
-
-import java.util.*;
-import jakarta.annotation.Generated;
-
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonValue;
+import jakarta.annotation.Generated;
 
 /**
  * Gets or Sets credential_status
@@ -22,9 +10,9 @@ import com.fasterxml.jackson.annotation.JsonValue;
 
 @Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2024-11-27T15:02:07.192340+01:00[Europe/Zagreb]", comments = "Generator version: 7.10.0")
 public enum CredentialStatusModel {
-  
+
   VALID("VALID"),
-  
+
   INVALID("INVALID");
 
   private String value;
