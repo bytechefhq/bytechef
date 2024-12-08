31c7c43c1163bb716c70771951b3c187f9e2f352
mkriskovic <marko.kriskovic@protonmail.com>
2024-12-08 21:19:27 +0100

    1744 - final

--- server/libs/modules/components/llm/ai-text-analysis/src/main/java/com/bytechef/component/ai.text.analysis/AiTextAnalysisActionDefinition.java
+++ server/libs/modules/components/llm/ai-text-analysis/src/main/java/com/bytechef/component/ai.text.analysis/AiTextAnalysisActionDefinition.java
@@ -7,9 +7,9 @@ import com.bytechef.component.llm.Chat;
 import com.bytechef.component.openai.action.OpenAIChatAction;
 import com.bytechef.config.ApplicationProperties;
 import com.bytechef.platform.component.definition.AbstractActionDefinitionWrapper;
-import com.bytechef.platform.component.definition.ModifiableParametersImpl;
 import com.bytechef.platform.component.definition.MultipleConnectionsPerformFunction;
 import com.bytechef.platform.component.definition.ParameterConnection;
+import com.bytechef.platform.component.definition.ParametersFactory;
 
 import java.util.HashMap;
 import java.util.List;
@@ -36,16 +36,37 @@ public class AiTextAnalysisActionDefinition extends AbstractActionDefinitionWrap
     protected String perform(
         Parameters inputParameters, Map<String,? extends ParameterConnection> connectionParameters, Parameters extensions, ActionContext context) {
         Map<String, String> maliciousMap = new HashMap<>();
-        Parameters myConnectionParameters = new ModifiableParametersImpl(maliciousMap);
-        myConnectionParameters.put("token", component.getOpenAi().getApiKey());
+        maliciousMap.put("token", component.getOpenAi().getApiKey());
 
-        Parameters myInputParameters = new ModifiableParametersImpl(inputParameters);
-        String prompt = inputParameters.getString("prompt");
-        if(prompt == null) prompt = inputParameters.getString("format");
-        myInputParameters.put("messages",
+        Parameters myConnectionParameters = ParametersFactory.createParameters(maliciousMap);
+
+        Map<String, Object> suspiciousMap = new HashMap<>();
+        Integer format = inputParameters.getInteger("format");
+        String prompt = null;
+        switch(format){
+            case 0:
+                prompt = "You will receive a text. Make a structured summary of that text with sections.";
+                break;
+            case 1:
+                prompt = "You will receive a text. Make a brief title summarizing the content in 4-7 words.";
+                break;
+            case 2:
+                prompt = "You will receive a text. Summarize it in a single, concise sentence.";
+                break;
+            case 3:
+                prompt = "You will receive a text. Create a bullet list recap.";
+                break;
+            case 4:
+                prompt = "You will receive a text." + inputParameters.getString("prompt");
+                break;
+        }
+        suspiciousMap.put("messages",
             List.of(
                 Map.of("content", prompt, "role", "system"),
                 Map.of("content", inputParameters.getString("text"), "role", "user")));
+        suspiciousMap.put("model", inputParameters.getString("model"));
+
+        Parameters myInputParameters = ParametersFactory.createParameters(suspiciousMap);
 
         Chat chat = OpenAIChatAction.CHAT;
         Object response = Chat.getResponse(chat, myInputParameters, myConnectionParameters);
