bd817da835299efeff8b21f63f1c9cce81e481f5
Ivica Cardic <ivica.cardic@gmail.com>
2024-12-10 08:59:24 +0100

    1570 - client - Add CopilotChatModelAdapterto separate file

--- client/src/pages/platform/copilot/CopilotRuntimeProvider.tsx
+++ client/src/pages/platform/copilot/CopilotRuntimeProvider.tsx
@@ -1,42 +1,14 @@
-import {AssistantRuntimeProvider, type ChatModelAdapter, useLocalRuntime} from '@assistant-ui/react';
+import CopilotChatModelAdapter from '@/pages/platform/copilot/CopilotChatModelAdapter';
+import {AssistantRuntimeProvider, useLocalRuntime} from '@assistant-ui/react';
 
 import type {ReactNode} from 'react';
-
-const MyModelAdapter: ChatModelAdapter = {
-    async run({abortSignal, messages}) {
-        const result = await fetch('/api/platform/internal/ai/chat', {
-            // forward the last message in the chat to the API
-            body: JSON.stringify({
-                message: messages[messages.length - 1],
-            }),
-            headers: {
-                'Content-Type': 'application/json',
-                'X-Copilot-Conversation-Id': sessionStorage.getItem('bytechef.copilot-conversation-id')??'',
-            },
-            method: 'POST',
-            // if the user hits the "cancel" button or escape keyboard key, cancel the request
-            signal: abortSignal,
-        });
-
-        const responses: {text: string}[] = await result.json();
-
-        return {
-            content: [
-                {
-                    text: responses.map((message) => message.text).join(''),
-                    type: 'text',
-                },
-            ],
-        };
-    },
-};
 
 export function CopilotRuntimeProvider({
     children,
 }: Readonly<{
     children: ReactNode;
 }>) {
-    const runtime = useLocalRuntime(MyModelAdapter);
+    const runtime = useLocalRuntime(CopilotChatModelAdapter);
 
     return <AssistantRuntimeProvider runtime={runtime}>{children}</AssistantRuntimeProvider>;
 }
