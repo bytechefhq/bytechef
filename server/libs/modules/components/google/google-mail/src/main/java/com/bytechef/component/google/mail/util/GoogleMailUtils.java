d2276811b8746a20c58e35e5effd4160ab1a3837
Monika Kušter <domitermonika@gmail.com>
2024-12-16 17:13:18 +0100

    692 - Fix - Add ByteArrayInputStream for storing decoded email attachments

--- server/libs/modules/components/google/google-mail/src/main/java/com/bytechef/component/google/mail/util/GoogleMailUtils.java
+++ server/libs/modules/components/google/google-mail/src/main/java/com/bytechef/component/google/mail/util/GoogleMailUtils.java
@@ -75,6 +75,7 @@ import jakarta.mail.internet.MimeBodyPart;
 import jakarta.mail.internet.MimeMessage;
 import jakarta.mail.internet.MimeMultipart;
 import jakarta.mail.util.ByteArrayDataSource;
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -406,7 +407,8 @@ public class GoogleMailUtils {
                     messagePartBody.getAttachmentId());
 
                 fileEntries.add(actionContext.file(
-                    file -> file.storeContent(messagePart.getFilename(), attachment.getData())));
+                    file -> file.storeContent(
+                        messagePart.getFilename(), new ByteArrayInputStream(attachment.decodeData()))));
             }
         }
         return fileEntries;
