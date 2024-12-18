ed46d30f4989342914f538a6831e99700f9b6581
mkriskovic <marko.kriskovic@protonmail.com>
2024-12-18 23:58:20 +0100

    1856 - refactoring

---
+++ server/libs/modules/components/ai/ai-text-analysis/src/main/java/com/bytechef/component/ai/text/analysis/action/AITextAnalysisAction.java
@@ -0,0 +1,7 @@
+package com.bytechef.component.ai.text.analysis.action;
+
+import com.bytechef.component.definition.Parameters;
+
+public interface AITextAnalysisAction {
+    Parameters createParameters(Parameters inputParameters);
+}
