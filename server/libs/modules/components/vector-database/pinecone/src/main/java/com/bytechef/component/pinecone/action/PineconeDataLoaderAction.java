da0d4f3f3e9c7335ef61b4812ac76392e7cf07ef
Monika Kušter <domitermonika@gmail.com>
2024-12-18 14:02:13 +0100

    1344 - Refactor data loading process using a new VectorDatabase interface

--- server/libs/modules/components/vector-database/pinecone/src/main/java/com/bytechef/component/pinecone/action/PineconeDataLoaderAction.java
+++ server/libs/modules/components/vector-database/pinecone/src/main/java/com/bytechef/component/pinecone/action/PineconeDataLoaderAction.java
@@ -17,38 +17,23 @@
 package com.bytechef.component.pinecone.action;
 
 import static com.bytechef.component.definition.ComponentDsl.action;
-import static com.bytechef.component.definition.ComponentDsl.array;
-import static com.bytechef.component.definition.ComponentDsl.fileEntry;
-import static com.bytechef.component.definition.ComponentDsl.option;
-import static com.bytechef.component.definition.ComponentDsl.string;
 import static com.bytechef.component.pinecone.constant.PineconeConstants.API_KEY;
-import static com.bytechef.component.pinecone.constant.PineconeConstants.DOCUMENT;
-import static com.bytechef.component.pinecone.constant.PineconeConstants.DOCUMENT_TYPE;
 import static com.bytechef.component.pinecone.constant.PineconeConstants.EMBEDDING_API_KEY;
 import static com.bytechef.component.pinecone.constant.PineconeConstants.ENVIRONMENT;
 import static com.bytechef.component.pinecone.constant.PineconeConstants.INDEX_NAME;
-import static com.bytechef.component.pinecone.constant.PineconeConstants.JSON;
-import static com.bytechef.component.pinecone.constant.PineconeConstants.JSON_KEYS_TO_USE;
 import static com.bytechef.component.pinecone.constant.PineconeConstants.PROJECT_ID;
+import static com.bytechef.component.vector.database.constant.VectorDatabaseConstants.DOCUMENT_PROPERTY;
+import static com.bytechef.component.vector.database.constant.VectorDatabaseConstants.DOCUMENT_TYPE_PROPERTY;
+import static com.bytechef.component.vector.database.constant.VectorDatabaseConstants.JSON_KEYS_TO_USE_PROPERTY;
 
 import com.bytechef.component.definition.ActionContext;
 import com.bytechef.component.definition.ComponentDsl.ModifiableActionDefinition;
-import com.bytechef.component.definition.FileEntry;
 import com.bytechef.component.definition.Parameters;
-import java.io.File;
-import java.util.List;
-import org.springframework.ai.document.Document;
+import com.bytechef.component.vector.database.VectorDatabase;
 import org.springframework.ai.openai.OpenAiEmbeddingModel;
 import org.springframework.ai.openai.api.OpenAiApi;
-import org.springframework.ai.reader.ExtractedTextFormatter;
-import org.springframework.ai.reader.JsonReader;
-import org.springframework.ai.reader.TextReader;
-import org.springframework.ai.reader.pdf.PagePdfDocumentReader;
-import org.springframework.ai.reader.pdf.config.PdfDocumentReaderConfig;
-import org.springframework.ai.reader.tika.TikaDocumentReader;
 import org.springframework.ai.vectorstore.PineconeVectorStore;
 import org.springframework.ai.vectorstore.PineconeVectorStore.PineconeVectorStoreConfig;
-import org.springframework.core.io.FileSystemResource;
 
 /**
  * @author Monika Kušter
@@ -59,25 +44,9 @@ public class PineconeDataLoaderAction {
         .title("Data Loader")
         .description("")
         .properties(
-            string(DOCUMENT_TYPE)
-                .label("Document Type")
-                .description("The type of the document.")
-                .options(
-                    option("JSON document", JSON),
-                    option("text document", "txt"),
-                    option("PDF document", "pdf"),
-                    option("Tika (DOCX, PPTX, HTML...)", "tika"))
-                .required(true),
-            array(JSON_KEYS_TO_USE)
-                .label("JSON Keys to Use")
-                .description(
-                    "Json keys on which extraction of content is based. If no keys are specified, it uses the entire " +
-                        "JSON object as content.")
-                .displayCondition("%s == '%s".formatted(DOCUMENT, JSON))
-                .items(string())
-                .required(false),
-            fileEntry(DOCUMENT)
-                .required(true))
+            DOCUMENT_TYPE_PROPERTY,
+            JSON_KEYS_TO_USE_PROPERTY,
+            DOCUMENT_PROPERTY)
         .perform(PineconeDataLoaderAction::perform);
 
     private PineconeDataLoaderAction() {
@@ -86,65 +55,12 @@ public class PineconeDataLoaderAction {
     protected static Object perform(
         Parameters inputParameters, Parameters connectionParameters, ActionContext actionContext) {
 
-        PineconeVectorStore pineconeVectorStore = getPineconeVectorStore(connectionParameters);
-
-        FileEntry fileEntry = inputParameters.getFileEntry(DOCUMENT);
-
-        File file1 = actionContext.file(file -> file.toTempFile(fileEntry));
-        FileSystemResource fileSystemResource = new FileSystemResource(file1);
-
-        String documentType = inputParameters.getRequiredString(DOCUMENT_TYPE);
-
-        switch (documentType) {
-            case JSON -> {
-                List<String> list = inputParameters.getList(JSON_KEYS_TO_USE, String.class);
-                JsonReader jsonReader;
-                if (list == null) {
-                    jsonReader = new JsonReader(fileSystemResource);
-
-                } else {
-                    jsonReader = new JsonReader(fileSystemResource, list.toArray(new String[0]));
-                }
-
-                List<Document> documents = jsonReader.get();
-
-                pineconeVectorStore.add(documents);
-            }
-            case "txt" -> {
-                TextReader textReader = new TextReader(fileSystemResource);
-
-                textReader.getCustomMetadata()
-                    .put("filename", fileEntry.getName());
-
-                List<Document> documents = textReader.read();
-                pineconeVectorStore.add(documents);
-            }
-            case "pdf" -> {
-                PagePdfDocumentReader pdfReader = new PagePdfDocumentReader(fileSystemResource,
-                    PdfDocumentReaderConfig.builder()
-                        .withPageTopMargin(0)
-                        .withPageExtractedTextFormatter(ExtractedTextFormatter.builder()
-                            .withNumberOfTopTextLinesToDelete(0)
-                            .build())
-                        .withPagesPerDocument(1)
-                        .build());
-
-                List<Document> documents = pdfReader.read();
-                pineconeVectorStore.add(documents);
-            }
-            case "tika" -> {
-                TikaDocumentReader tikaDocumentReader = new TikaDocumentReader(fileSystemResource);
-
-                List<Document> documents = tikaDocumentReader.read();
-
-                pineconeVectorStore.add(documents);
-            }
-        }
+        VECTOR_STORE.loadData(inputParameters, connectionParameters, actionContext);
 
         return null;
     }
 
-    private static PineconeVectorStore getPineconeVectorStore(Parameters connectionParameters) {
+    public static final VectorDatabase VECTOR_STORE = connectionParameters -> {
         OpenAiEmbeddingModel openAiEmbeddingModel = new OpenAiEmbeddingModel(
             new OpenAiApi(connectionParameters.getRequiredString(EMBEDDING_API_KEY)));
 
@@ -156,5 +72,5 @@ public class PineconeDataLoaderAction {
             .build();
 
         return new PineconeVectorStore(pineconeVectorStoreConfig, openAiEmbeddingModel);
-    }
+    };
 }
---
+++ server/libs/modules/components/vector-database/src/main/java/com/bytechef/component/vector/database/VectorDatabase.java
@@ -0,0 +1,102 @@
+/*
+ * Copyright 2023-present ByteChef Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef.component.vector.database;
+
+import static com.bytechef.component.vector.database.constant.VectorDatabaseConstants.DOCUMENT;
+import static com.bytechef.component.vector.database.constant.VectorDatabaseConstants.DOCUMENT_TYPE;
+import static com.bytechef.component.vector.database.constant.VectorDatabaseConstants.JSON;
+import static com.bytechef.component.vector.database.constant.VectorDatabaseConstants.JSON_KEYS_TO_USE;
+
+import com.bytechef.component.definition.ActionContext;
+import com.bytechef.component.definition.FileEntry;
+import com.bytechef.component.definition.Parameters;
+import java.io.File;
+import java.util.List;
+import org.springframework.ai.document.Document;
+import org.springframework.ai.reader.ExtractedTextFormatter;
+import org.springframework.ai.reader.JsonReader;
+import org.springframework.ai.reader.TextReader;
+import org.springframework.ai.reader.pdf.PagePdfDocumentReader;
+import org.springframework.ai.reader.pdf.config.PdfDocumentReaderConfig;
+import org.springframework.ai.reader.tika.TikaDocumentReader;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.core.io.FileSystemResource;
+
+/**
+ * @author Monika Kušter
+ */
+public interface VectorDatabase {
+
+    VectorStore createVectorStore(Parameters connectionParameters);
+
+    default void loadData(Parameters inputParameters, Parameters connectionParameters, ActionContext actionContext) {
+        VectorStore vectorStore = createVectorStore(connectionParameters);
+
+        FileEntry fileEntry = inputParameters.getFileEntry(DOCUMENT);
+
+        File file1 = actionContext.file(file -> file.toTempFile(fileEntry));
+        FileSystemResource fileSystemResource = new FileSystemResource(file1);
+
+        String documentType = inputParameters.getRequiredString(DOCUMENT_TYPE);
+
+        switch (documentType) {
+            case JSON -> {
+                List<String> list = inputParameters.getList(JSON_KEYS_TO_USE, String.class);
+                JsonReader jsonReader;
+                if (list == null) {
+                    jsonReader = new JsonReader(fileSystemResource);
+
+                } else {
+                    jsonReader = new JsonReader(fileSystemResource, list.toArray(new String[0]));
+                }
+
+                List<Document> documents = jsonReader.get();
+
+                vectorStore.add(documents);
+            }
+            case "txt" -> {
+                TextReader textReader = new TextReader(fileSystemResource);
+
+                textReader.getCustomMetadata()
+                    .put("filename", fileEntry.getName());
+
+                List<Document> documents = textReader.read();
+                vectorStore.add(documents);
+            }
+            case "pdf" -> {
+                PagePdfDocumentReader pdfReader = new PagePdfDocumentReader(fileSystemResource,
+                    PdfDocumentReaderConfig.builder()
+                        .withPageTopMargin(0)
+                        .withPageExtractedTextFormatter(ExtractedTextFormatter.builder()
+                            .withNumberOfTopTextLinesToDelete(0)
+                            .build())
+                        .withPagesPerDocument(1)
+                        .build());
+
+                List<Document> documents = pdfReader.read();
+                vectorStore.add(documents);
+            }
+            case "tika" -> {
+                TikaDocumentReader tikaDocumentReader = new TikaDocumentReader(fileSystemResource);
+
+                List<Document> documents = tikaDocumentReader.read();
+
+                vectorStore.add(documents);
+            }
+        }
+    }
+}
