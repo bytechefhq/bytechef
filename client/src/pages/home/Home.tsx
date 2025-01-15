d2e81324726131e79fc2a3157f1f392c674908a8
katarina-cala <katarinacala@gmail.com>
2025-01-15 09:31:38 +0100

    1710-ModeType selection dialog improvement

--- client/src/pages/home/Home.tsx
+++ client/src/pages/home/Home.tsx
@@ -1,20 +1,18 @@
-import {ModeType, useModeTypeStore} from '@/pages/home/stores/useModeTypeStore';
+import {useModeTypeStore} from '@/pages/home/stores/useModeTypeStore';
 import {useFeatureFlagsStore} from '@/shared/stores/useFeatureFlagsStore';
-import * as Dialog from '@radix-ui/react-dialog';
-import {FolderIcon, SquareIcon} from 'lucide-react';
-import {useEffect} from 'react';
+import {useEffect, useState} from 'react';
 import {useNavigate} from 'react-router-dom';
 
-const Home = () => {
-    const {currentType, setCurrentType} = useModeTypeStore();
+import ModeSelectionDialog from './ModeSelectionDialog';
 
-    const ff_520 = useFeatureFlagsStore()('ff-520');
+const Home = () => {
+    const [isDialogOpen, setIsDialogOpen] = useState(false);
+
+    const {currentType} = useModeTypeStore();
 
     const navigate = useNavigate();
 
-    const handleClick = (modeType: ModeType) => {
-        setCurrentType(modeType);
-    };
+    const ff_520 = useFeatureFlagsStore()('ff-520');
 
     useEffect(() => {
         if (!ff_520) {
@@ -23,61 +21,17 @@ const Home = () => {
             return;
         }
 
-        if (currentType !== undefined) {
-            if (currentType === ModeType.AUTOMATION) {
-                navigate('/automation');
-            } else {
-                navigate('/embedded');
-            }
+        if (currentType === undefined) {
+            setIsDialogOpen(true);
         }
     }, [currentType, navigate, ff_520]);
 
-    return ff_520 ? (
-        <Dialog.Root open={currentType === undefined}>
-            <Dialog.Portal>
-                <Dialog.Overlay className="fixed inset-0 z-50 bg-black/80" />
-
-                <Dialog.Content className="fixed left-1/2 top-1/2 z-50 -translate-x-1/2 -translate-y-1/2 bg-background p-4 shadow-lg sm:rounded-lg">
-                    <div className="mx-auto flex h-full items-center">
-                        <div className="grid grid-cols-2 gap-4">
-                            <div>
-                                <button onClick={() => handleClick(ModeType.EMBEDDED)}>
-                                    <div className="flex size-80 flex-col items-center justify-between rounded-md p-4 hover:bg-green-100 hover:text-accent-foreground">
-                                        <div className="text-2xl font-semibold">Embedded</div>
-
-                                        <div>
-                                            <SquareIcon className="size-16 text-green-300/70" />
-                                        </div>
-
-                                        <div>
-                                            Allow your users to integrate your product with applications they use.
-                                        </div>
-                                    </div>
-                                </button>
-                            </div>
-
-                            <div>
-                                <button onClick={() => handleClick(ModeType.AUTOMATION)}>
-                                    <div className="flex size-80 flex-col items-center justify-between rounded-md p-4 hover:bg-blue-100 hover:text-accent-foreground">
-                                        <div className="text-2xl font-semibold">Automation</div>
-
-                                        <div>
-                                            <FolderIcon className="size-16 text-blue-300/70" />
-                                        </div>
-
-                                        <div>
-                                            Integrate applications and automate processes inside your organization.
-                                        </div>
-                                    </div>
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                </Dialog.Content>
-            </Dialog.Portal>
-        </Dialog.Root>
-    ) : (
-        <></>
+    return (
+        <>
+            {ff_520 && isDialogOpen && (
+                <ModeSelectionDialog isDialogOpen={isDialogOpen} onDialogClose={() => setIsDialogOpen(false)} />
+            )}
+        </>
     );
 };
 
