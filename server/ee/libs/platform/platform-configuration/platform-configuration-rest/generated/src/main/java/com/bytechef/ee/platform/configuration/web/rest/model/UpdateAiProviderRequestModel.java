0bdfa905136abaaf0de54b5fbbc552c8898a57bd
Ivica Cardic <ivica.cardic@gmail.com>
2025-01-18 09:29:18 +0100

    1345 - Generate

---
+++ server/ee/libs/platform/platform-configuration/platform-configuration-rest/generated/src/main/java/com/bytechef/ee/platform/configuration/web/rest/model/UpdateAiProviderRequestModel.java
@@ -0,0 +1,85 @@
+package com.bytechef.ee.platform.configuration.web.rest.model;
+
+import java.net.URI;
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonTypeName;
+import org.openapitools.jackson.nullable.JsonNullable;
+import java.time.OffsetDateTime;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.*;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+
+import java.util.*;
+import jakarta.annotation.Generated;
+
+/**
+ * UpdateAiProviderRequestModel
+ */
+
+@JsonTypeName("updateAiProvider_request")
+@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2025-01-16T13:45:38.829908+01:00[Europe/Zagreb]", comments = "Generator version: 7.10.0")
+public class UpdateAiProviderRequestModel {
+
+  private String apiKey;
+
+  public UpdateAiProviderRequestModel apiKey(String apiKey) {
+    this.apiKey = apiKey;
+    return this;
+  }
+
+  /**
+   * The API key of an AI provider.
+   * @return apiKey
+   */
+  
+  @Schema(name = "apiKey", description = "The API key of an AI provider.", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+  @JsonProperty("apiKey")
+  public String getApiKey() {
+    return apiKey;
+  }
+
+  public void setApiKey(String apiKey) {
+    this.apiKey = apiKey;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    UpdateAiProviderRequestModel updateAiProviderRequest = (UpdateAiProviderRequestModel) o;
+    return Objects.equals(this.apiKey, updateAiProviderRequest.apiKey);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(apiKey);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class UpdateAiProviderRequestModel {\n");
+    sb.append("    apiKey: ").append(toIndentedString(apiKey)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
+
