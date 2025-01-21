05caef08707cbb5d8912a410134153eaa7283edd
Monika Kušter <domitermonika@gmail.com>
2025-01-21 09:30:27 +0100

    783 - rename action to "createDocumentFromTemplate" and update title and description to better describe functionality

--- server/libs/modules/components/google/google-docs/src/main/java/com/bytechef/component/google/docs/constant/GoogleDocsConstants.java
+++ server/libs/modules/components/google/google-docs/src/main/java/com/bytechef/component/google/docs/constant/GoogleDocsConstants.java
@@ -17,7 +17,7 @@
 package com.bytechef.component.google.docs.constant;
 
 /**
- * @author Monika Domiter
+ * @author Monika Kušter
  */
 public class GoogleDocsConstants {
 
@@ -25,9 +25,9 @@ public class GoogleDocsConstants {
     }
 
     public static final String BODY = "body";
-    public static final String DESTINATION_FILE = "destinationFile";
     public static final String DOCUMENT_ID = "documentId";
     public static final String IMAGES = "images";
+    public static final String TEMPLATE_DOCUMENT_ID = "templateDocumentId";
     public static final String TITLE = "title";
     public static final String VALUES = "values";
 }
