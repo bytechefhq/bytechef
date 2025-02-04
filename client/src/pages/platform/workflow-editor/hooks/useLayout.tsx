111cffe90e0c0d3df85f5faf66aba5c6582cdc65
Krešimir Čoko <kresimircoko@gmail.com>
2025-02-04 13:54:34 +0100

    1726 - SF

--- client/src/pages/platform/workflow-editor/hooks/useLayout.tsx
+++ client/src/pages/platform/workflow-editor/hooks/useLayout.tsx
@@ -14,6 +14,7 @@ import InlineSVG from 'react-inlinesvg';
 import {useShallow} from 'zustand/react/shallow';
 
 import useWorkflowDataStore from '../stores/useWorkflowDataStore';
+import createConditionNode from '../utils/createConditionNode';
 import getNextPlaceholderId from '../utils/getNextPlaceholderId';
 
 const NODE_WIDTH = 240;
@@ -60,51 +61,6 @@ const convertTaskToNode = (
         position: {x: 0, y: 0},
         type: 'workflow',
     };
-};
-
-const createConditionNode = ({
-    allNodes,
-    belowPlaceholderNode,
-    sourcePlaceholderIndex,
-    taskNode,
-}: {
-    allNodes: Array<Node>;
-    belowPlaceholderNode?: Node;
-    sourcePlaceholderIndex?: number;
-    taskNode: Node;
-}) => {
-    const leftPlaceholderNode: Node = {
-        data: {conditionCase: CONDITION_CASE_TRUE, conditionId: taskNode.id, label: '+'},
-        id: `${taskNode.id}-left-placeholder-0`,
-        position: {x: 0, y: 0},
-        type: 'placeholder',
-    };
-
-    const rightPlaceholderNode: Node = {
-        data: {conditionCase: CONDITION_CASE_FALSE, conditionId: taskNode.id, label: '+'},
-        id: `${taskNode.id}-right-placeholder-0`,
-        position: {x: 0, y: 0},
-        type: 'placeholder',
-    };
-
-    if (taskNode.data.conditionData && belowPlaceholderNode && sourcePlaceholderIndex) {
-        allNodes.splice(sourcePlaceholderIndex + 1, 0, leftPlaceholderNode, rightPlaceholderNode, belowPlaceholderNode);
-
-        return allNodes;
-    }
-
-    const bottomPlaceholderNode: Node = {
-        data: {label: '+'},
-        id: `${taskNode.id}-bottom-placeholder`,
-        position: {x: 0, y: 0},
-        type: 'placeholder',
-    };
-
-    const insertIndex = allNodes.findIndex((node) => node.id === taskNode.id) + 1;
-
-    allNodes.splice(insertIndex, 0, leftPlaceholderNode, rightPlaceholderNode, bottomPlaceholderNode);
-
-    return allNodes;
 };
 
 const filterConditionCaseNodes = (nodes: Node[], node: Node) => {
