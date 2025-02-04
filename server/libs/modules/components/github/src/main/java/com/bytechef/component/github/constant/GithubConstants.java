e699c08048e88a0141c15ef11a1a5b0279dbe891
marijahorvat171 <marijahorvat171@gmail.com>
2025-02-04 15:27:06 +0100

    2033 - create list issues action

--- server/libs/modules/components/github/src/main/java/com/bytechef/component/github/constant/GithubConstants.java
+++ server/libs/modules/components/github/src/main/java/com/bytechef/component/github/constant/GithubConstants.java
@@ -20,9 +20,11 @@ import static com.bytechef.component.definition.ComponentDsl.array;
 import static com.bytechef.component.definition.ComponentDsl.integer;
 import static com.bytechef.component.definition.ComponentDsl.number;
 import static com.bytechef.component.definition.ComponentDsl.object;
+import static com.bytechef.component.definition.ComponentDsl.option;
 import static com.bytechef.component.definition.ComponentDsl.string;
 
 import com.bytechef.component.definition.ComponentDsl.ModifiableObjectProperty;
+import com.bytechef.component.definition.ComponentDsl.ModifiableStringProperty;
 
 /**
  * @author Luka Ljubić
@@ -31,13 +33,45 @@ public class GithubConstants {
 
     public static final String ASSIGNEES = "assignees";
     public static final String BODY = "body";
+    public static final String FILTER = "filter";
     public static final String ID = "id";
     public static final String ISSUE = "issue";
     public static final String LABELS = "labels";
     public static final String NAME = "name";
     public static final String OWNER = "owner";
     public static final String REPOSITORY = "repository";
+    public static final String STATE = "state";
     public static final String TITLE = "title";
+
+    public static final ModifiableStringProperty FILTER_PROPERTY = string(FILTER)
+        .label("Filter")
+        .description("Specifies the types of issues to return.")
+        .options(
+            option("Assigned", "assigned",
+                "Issues assigned to the authenticated user."),
+            option("Created", "created",
+                "Issues created by the authenticated user."),
+            option("Mentioned", "mentioned",
+                "Issues mentioning the authenticated user."),
+            option("Subscribed", "subscribed",
+                "Issues the authenticated user is subscribed to updates for."),
+            option("Repos", "repos",
+                "All issues from repositories that the authenticated user has explicit access to."),
+            option("All", "all",
+                "All issues related to the authenticated user."))
+        .required(true);
+
+    public static final ModifiableStringProperty STATE_PROPERTY = string(STATE)
+        .label("State")
+        .description("Indicates the state of the issues to return.")
+        .options(
+            option("Open", "open",
+                "Open issues."),
+            option("Closed", "closed",
+                "Closed issues."),
+            option("All", "all",
+                "All issues."))
+        .required(true);
 
     public static final ModifiableObjectProperty ISSUE_OUTPUT_PROPERTY = object()
         .properties(
