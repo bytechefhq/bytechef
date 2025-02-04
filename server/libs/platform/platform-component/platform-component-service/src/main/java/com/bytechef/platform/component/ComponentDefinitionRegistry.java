7db65ec3256bec767377a99d45fd7329e39eacf4
Ivica Cardic <ivica.cardic@gmail.com>
2025-02-04 20:39:14 +0100

    Revert "2054 - Messing up a project in code editor once, breaks the project"
    This reverts commit ba422afb7668cc256a07057a27cd4684408f9a9d.

--- server/libs/platform/platform-component/platform-component-service/src/main/java/com/bytechef/platform/component/ComponentDefinitionRegistry.java
+++ server/libs/platform/platform-component/platform-component-service/src/main/java/com/bytechef/platform/component/ComponentDefinitionRegistry.java
@@ -175,9 +175,7 @@ public class ComponentDefinitionRegistry {
             Map<Integer, ComponentDefinition> componentDefinitionMap = componentDefinitions.get(name);
 
             if (componentDefinitionMap == null) {
-//                throw new IllegalArgumentException("The component '%s' does not exist".formatted(name));
-
-                return MISSING_COMPONENT_DEFINITION;
+                throw new IllegalArgumentException("The component '%s' does not exist".formatted(name));
             }
 
             componentDefinition = componentDefinitionMap.get(version);
@@ -190,9 +188,8 @@ public class ComponentDefinitionRegistry {
                     .filter(Optional::isPresent)
                     .map(Optional::get)
                     .findFirst()
-//                    .orElseThrow(() -> new IllegalArgumentException(
-//                        "No component with name=" + name + ", version=" + version))
-                    .orElseGet(() -> () -> MISSING_COMPONENT_DEFINITION)
+                    .orElseThrow(() -> new IllegalArgumentException(
+                        "No component with name=" + name + ", version=" + version))
                     .getDefinition();
             }
         }
