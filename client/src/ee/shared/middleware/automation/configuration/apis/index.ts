6e94c3d76138f1ecf919106dc0a9bfb3884e9cf9
Ivica Cardic <ivica.cardic@gmail.com>
2025-02-10 06:49:20 +0100

    1039 - client - Generate

--- client/src/ee/shared/middleware/automation/configuration/apis/index.ts
+++ client/src/ee/shared/middleware/automation/configuration/apis/index.ts
@@ -1,3 +1,4 @@
 /* tslint:disable */
 /* eslint-disable */
+export * from './ProjectGitApi';
 export * from './WorkspaceApi';
--- client/src/ee/shared/middleware/platform/configuration/apis/index.ts
+++ client/src/ee/shared/middleware/platform/configuration/apis/index.ts
@@ -1,3 +1,4 @@
 /* tslint:disable */
 /* eslint-disable */
 export * from './AiProviderApi';
+export * from './GitConfigurationApi';
