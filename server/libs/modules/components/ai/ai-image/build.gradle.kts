4751d6daff48820b6bae243034f5e1b03be9bf9b
Monika Kušter <domitermonika@gmail.com>
2025-02-14 07:43:08 +0100

    538 - Update module names

--- server/libs/modules/components/ai/ai-image/build.gradle.kts
+++ server/libs/modules/components/ai/ai-image/build.gradle.kts
@@ -7,7 +7,7 @@ dependencies {
     implementation(project(":server:libs:platform:platform-workflow:platform-workflow-worker:platform-workflow-worker-api"))
 
     implementation(project(":server:libs:modules:components:ai:llm"))
-    implementation(project(":server:libs:modules:components:ai:llm:azure-openai"))
-    implementation(project(":server:libs:modules:components:ai:llm:openai"))
+    implementation(project(":server:libs:modules:components:ai:llm:azure-open-ai"))
+    implementation(project(":server:libs:modules:components:ai:llm:open-ai"))
     implementation(project(":server:libs:modules:components:ai:llm:stability"))
 }
