2db93fa682e776a8336ba3a8c00c0df7b6233ac8
Nikolina Spehar <speharnikolina87@gmail.com>
2025-02-18 15:54:03 +0100

    2042 - add Pushover component

---
+++ server/libs/modules/components/pushover/src/main/java/com/bytechef/component/pushover/constant/PushoverConstants.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2023-present ByteChef Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef.component.pushover.constant;
+
+/**
+ * @author Nikolina Spehar
+ */
+public class PushoverConstants {
+
+    public static final String ATTACHMENT = "attachment";
+    public static final String EXPIRE = "expire";
+    public static final String MESSAGE = "message";
+    public static final String PRIORITY = "priority";
+    public static final String RETRY = "retry";
+    public static final String REQUEST = "request";
+    public static final String STATUS = "status";
+    public static final String TITLE = "title";
+    public static final String TOKEN = "token";
+    public static final String URL = "url";
+    public static final String URL_TITLE = "url_title";
+    public static final String USER = "user";
+
+    private PushoverConstants() {
+    }
+}
