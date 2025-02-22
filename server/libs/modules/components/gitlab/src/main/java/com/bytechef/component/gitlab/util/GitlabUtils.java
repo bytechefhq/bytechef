5ac6086870209466a83471a0096356f44a72b99d
Monika Kušter <domitermonika@gmail.com>
2025-02-22 18:44:01 +0100

    1316 - use x-dynamic-options property in openapi spec

--- server/libs/modules/components/gitlab/src/main/java/com/bytechef/component/gitlab/util/GitlabUtils.java
+++ server/libs/modules/components/gitlab/src/main/java/com/bytechef/component/gitlab/util/GitlabUtils.java
@@ -17,12 +17,12 @@
 package com.bytechef.component.gitlab.util;
 
 import static com.bytechef.component.definition.ComponentDsl.option;
-import static com.bytechef.component.definition.Context.Http;
 import static com.bytechef.component.definition.Context.Http.responseType;
 import static com.bytechef.component.gitlab.constant.GitlabConstants.ID;
 import static com.bytechef.component.gitlab.constant.GitlabConstants.PROJECT_ID;
 
 import com.bytechef.component.definition.Context;
+import com.bytechef.component.definition.Context.Http;
 import com.bytechef.component.definition.Option;
 import com.bytechef.component.definition.Parameters;
 import com.bytechef.component.definition.TypeReference;
@@ -33,12 +33,12 @@ import java.util.Map;
 /**
  * @author Monika Kušter
  */
-public class GitlabUtils {
+public class GitlabUtils extends AbstractGitlabUtils {
 
     private GitlabUtils() {
     }
 
-    public static List<Option<String>> getIssueOptions(
+    public static List<Option<Long>> getIssueIdOptions(
         Parameters inputParameters, Parameters connectionParameters, Map<String, String> stringStringMap, String s,
         Context context) {
 
@@ -48,17 +48,16 @@ public class GitlabUtils {
             .execute()
             .getBody(new TypeReference<>() {});
 
-        List<Option<String>> options = new ArrayList<>();
+        List<Option<Long>> options = new ArrayList<>();
 
         for (Map<String, Object> item : body) {
-            options.add(option((String) item.get("title"), item.get("iid")
-                .toString()));
+            options.add(option((String) item.get("title"), ((Integer) item.get("iid")).intValue()));
         }
 
         return options;
     }
 
-    public static List<Option<String>> getProjectOptions(
+    public static List<Option<String>> getProjectIdOptions(
         Parameters inputParameters, Parameters connectionParameters, Map<String, String> stringStringMap, String s,
         Context context) {
 
@@ -81,5 +80,4 @@ public class GitlabUtils {
 
         return options;
     }
-
 }
