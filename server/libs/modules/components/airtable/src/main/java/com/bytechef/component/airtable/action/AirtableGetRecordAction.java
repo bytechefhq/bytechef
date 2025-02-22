0c186925e769bb34a2c3c023fc263cb88c11ebcb
Monika Kušter <domitermonika@gmail.com>
2025-02-22 19:09:18 +0100

    2089 - use x-dynamic-options property in openapi spec

--- server/libs/modules/components/airtable/src/main/java/com/bytechef/component/airtable/action/AirtableGetRecordAction.java
+++ server/libs/modules/components/airtable/src/main/java/com/bytechef/component/airtable/action/AirtableGetRecordAction.java
@@ -31,7 +31,9 @@ import static com.bytechef.component.definition.ComponentDsl.string;
 import static com.bytechef.component.definition.ComponentDsl.time;
 import static com.bytechef.component.definition.Context.Http.ResponseType;
 
+import com.bytechef.component.airtable.util.AirtableUtils;
 import com.bytechef.component.definition.ComponentDsl;
+import com.bytechef.component.definition.OptionsDataSource;
 import java.util.Map;
 
 /**
@@ -52,18 +54,23 @@ public class AirtableGetRecordAction {
         .properties(string("baseId").label("Base ID")
             .description("ID of the base where table is located.")
             .required(true)
+            .options((OptionsDataSource.ActionOptionsFunction<String>) AirtableUtils::getBaseIdOptions)
             .metadata(
                 Map.of(
                     "type", PropertyType.PATH)),
             string("tableId").label("Table ID")
                 .description("ID of the table where the record is located.")
                 .required(true)
+                .options((OptionsDataSource.ActionOptionsFunction<String>) AirtableUtils::getTableIdOptions)
+                .optionsLookupDependsOn("baseId")
                 .metadata(
                     Map.of(
                         "type", PropertyType.PATH)),
             string("recordId").label("Record ID")
                 .description("ID of the record that will be deleted.")
                 .required(true)
+                .options((OptionsDataSource.ActionOptionsFunction<String>) AirtableUtils::getRecordIdOptions)
+                .optionsLookupDependsOn("tableId", "baseId")
                 .metadata(
                     Map.of(
                         "type", PropertyType.PATH)))
