0bceb2e0403b7bf2623a467a036ac6eed4a469aa
Monika Kušter <domitermonika@gmail.com>
2025-02-24 10:09:04 +0100

    1323 - use x-dynamic-options property

--- server/libs/modules/components/todoist/src/main/java/com/bytechef/component/todoist/util/AbstractTodoistUtils.java
+++ server/libs/modules/components/todoist/src/main/java/com/bytechef/component/todoist/util/AbstractTodoistUtils.java
@@ -16,31 +16,24 @@
 
 package com.bytechef.component.todoist.util;
 
-import static com.bytechef.component.definition.ComponentDsl.option;
-
 import com.bytechef.component.definition.Context;
-import com.bytechef.component.definition.Context.Http;
 import com.bytechef.component.definition.Option;
 import com.bytechef.component.definition.Parameters;
-import com.bytechef.component.definition.TypeReference;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 /**
- * @author Monika Kušter
+ * Provides methods for retrieving dynamic options for various properties within the component.
+ *
+ * @generated
  */
-public class TodoistUtils extends AbstractTodoistUtils {
-
-    private TodoistUtils() {
-    }
-
+public abstract class AbstractTodoistUtils {
     public static List<Option<String>> getTaskIdOptions(
         Parameters inputParameters,
         Parameters connectionParameters, Map<String, String> dependencyPaths, String searchText,
         Context context) {
 
-        return getOptions(context, "/tasks", "content");
+        return List.of();
     }
 
     public static List<Option<String>> getProjectIdOptions(
@@ -48,23 +41,6 @@ public class TodoistUtils extends AbstractTodoistUtils {
         Parameters connectionParameters, Map<String, String> dependencyPaths, String searchText,
         Context context) {
 
-        return getOptions(context, "/projects", "name");
-
-    }
-
-    private static List<Option<String>> getOptions(Context context, String path, String label) {
-        List<Map<String, Object>> body = context
-            .http(http -> http.get(path))
-            .configuration(Http.responseType(Http.ResponseType.JSON))
-            .execute()
-            .getBody(new TypeReference<>() {});
-
-        List<Option<String>> options = new ArrayList<>();
-
-        for (Map<String, Object> map : body) {
-            options.add(option((String) map.get(label), (String) map.get("id")));
-        }
-
-        return options;
+        return List.of();
     }
 }
