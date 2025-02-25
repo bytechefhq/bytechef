df136ed699594146fac10e44b75cf5eb2dadee21
Monika Kušter <domitermonika@gmail.com>
2025-02-25 12:57:03 +0100

    884 - use x-dynamic-options property

--- server/libs/modules/components/asana/src/main/java/com/bytechef/component/asana/AsanaComponentHandler.java
+++ server/libs/modules/components/asana/src/main/java/com/bytechef/component/asana/AsanaComponentHandler.java
@@ -16,25 +16,10 @@
 
 package com.bytechef.component.asana;
 
-import static com.bytechef.component.asana.constant.AsanaConstants.WORKSPACE;
-import static com.bytechef.component.definition.ComponentDsl.string;
-
 import com.bytechef.component.OpenApiComponentHandler;
-import com.bytechef.component.asana.util.AsanaUtils;
-import com.bytechef.component.definition.ActionDefinition;
 import com.bytechef.component.definition.ComponentCategory;
-import com.bytechef.component.definition.ComponentDsl.ModifiableArrayProperty;
 import com.bytechef.component.definition.ComponentDsl.ModifiableComponentDefinition;
-import com.bytechef.component.definition.ComponentDsl.ModifiableObjectProperty;
-import com.bytechef.component.definition.ComponentDsl.ModifiableProperty;
-import com.bytechef.component.definition.ComponentDsl.ModifiableStringProperty;
-import com.bytechef.component.definition.OptionsDataSource.ActionOptionsFunction;
-import com.bytechef.component.definition.Property.ValueProperty;
-import com.bytechef.definition.BaseProperty;
 import com.google.auto.service.AutoService;
-import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
 
 /**
  * @author Monika Domiter
@@ -48,48 +33,5 @@ public class AsanaComponentHandler extends AbstractAsanaComponentHandler {
             .customAction(true)
             .icon("path:assets/asana.svg")
             .categories(ComponentCategory.PROJECT_MANAGEMENT);
-    }
-
-    @Override
-    public ModifiableProperty<?> modifyProperty(
-        ActionDefinition actionDefinition, ModifiableProperty<?> modifiableProperty) {
-
-        if (Objects.equals(modifiableProperty.getName(), "__item")) {
-            Optional<List<? extends ValueProperty<?>>> propertiesOptional =
-                ((ModifiableObjectProperty) modifiableProperty).getProperties();
-
-            for (BaseProperty baseProperty : propertiesOptional.get()) {
-                if (Objects.equals(baseProperty.getName(), "data")) {
-                    Optional<List<? extends ValueProperty<?>>> propertiesOptional2 =
-                        ((ModifiableObjectProperty) baseProperty).getProperties();
-
-                    for (BaseProperty baseProperty2 : propertiesOptional2.get()) {
-                        if (Objects.equals(baseProperty2.getName(), WORKSPACE)) {
-                            ((ModifiableStringProperty) baseProperty2)
-                                .options((ActionOptionsFunction<String>) AsanaUtils::getWorkspaceIdOptions);
-                        } else if (Objects.equals(baseProperty2.getName(), "project")) {
-                            ((ModifiableStringProperty) baseProperty2)
-                                .optionsLookupDependsOn(WORKSPACE)
-                                .options((ActionOptionsFunction<String>) AsanaUtils::getProjectIdOptions);
-                        } else if (Objects.equals(baseProperty2.getName(), "assignee")) {
-                            ((ModifiableStringProperty) baseProperty2)
-                                .optionsLookupDependsOn(WORKSPACE)
-                                .options((ActionOptionsFunction<String>) AsanaUtils::getAssigneeOptions);
-                        } else if (Objects.equals(baseProperty2.getName(), "team")) {
-                            ((ModifiableStringProperty) baseProperty2)
-                                .optionsLookupDependsOn("__item.data." + WORKSPACE)
-                                .options((ActionOptionsFunction<String>) AsanaUtils::getTeamOptions);
-                        } else if (Objects.equals(baseProperty2.getName(), "tags")) {
-                            ((ModifiableArrayProperty) baseProperty2)
-                                .items(
-                                    string()
-                                        .options((ActionOptionsFunction<String>) AsanaUtils::getTagOptions));
-                        }
-                    }
-                }
-            }
-        }
-
-        return modifiableProperty;
     }
 }
