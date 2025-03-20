27d093e828b508f714d78ad704e0c8267d1107ea
mkriskovic <marko.kriskovic@protonmail.com>
2025-03-20 21:27:04 +0100

    2274 - Perplexity initial

--- server/apps/server-app/build.gradle.kts
+++ server/apps/server-app/build.gradle.kts
@@ -136,6 +136,7 @@ dependencies {
     implementation(project(":server:libs:modules:components:ai:llm:nvidia"))
     implementation(project(":server:libs:modules:components:ai:llm:ollama"))
     implementation(project(":server:libs:modules:components:ai:llm:open-ai"))
+    implementation(project(":server:libs:modules:components:ai:llm:perplexity"))
     implementation(project(":server:libs:modules:components:ai:llm:stability"))
     implementation(project(":server:libs:modules:components:ai:llm:vertex:gemini"))
     implementation(project(":server:libs:modules:components:ai:llm:watsonx"))
