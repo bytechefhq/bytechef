4e49058b374b4392790eeb13ff6bfa18f9020a1c
Monika Kušter <domitermonika@gmail.com>
2025-03-20 12:37:26 +0100

    1391 - add triggers to handle events when invitees create or cancel appointments

---
+++ server/libs/modules/components/calendly/src/test/java/com/bytechef/component/calendly/trigger/CalendlyInviteeCanceledTriggerTest.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2023-present ByteChef Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef.component.calendly.trigger;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+import com.bytechef.component.calendly.util.CalendlyUtils;
+import com.bytechef.component.definition.TriggerDefinition.WebhookEnableOutput;
+import com.bytechef.component.definition.TypeReference;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.mockito.stubbing.Answer;
+
+/**
+ * @author Monika Kušter
+ */
+class CalendlyInviteeCanceledTriggerTest extends AbstractCalendlyTriggerTest {
+
+    @Test
+    void testWebhookDisable() {
+        calendlyUtilsMockedStatic.when(
+            () -> CalendlyUtils.unsubscribeWebhook(
+                contextArgumentCaptor.capture(), stringArgumentCaptor.capture()))
+            .thenAnswer((Answer<Void>) invocation -> null);
+
+        CalendlyInviteeCanceledTrigger.webhookDisable(
+            mockedParameters, mockedParameters, mockedParameters, workflowExecutionId, mockedTriggerContext);
+
+        assertEquals(mockedTriggerContext, contextArgumentCaptor.getValue());
+        assertEquals("123", stringArgumentCaptor.getValue());
+    }
+
+    @Test
+    void testWebhookEnable() {
+        String webhookUrl = "testWebhookUrl";
+
+        calendlyUtilsMockedStatic.when(
+            () -> CalendlyUtils.subscribeWebhook(
+                contextArgumentCaptor.capture(), stringArgumentCaptor.capture(), stringArgumentCaptor.capture(),
+                stringArgumentCaptor.capture()))
+            .thenReturn(mockedWebhookEnableOutput);
+
+        WebhookEnableOutput webhookEnableOutput = CalendlyInviteeCanceledTrigger.webhookEnable(
+            mockedParameters, mockedParameters, webhookUrl, workflowExecutionId, mockedTriggerContext);
+
+        assertEquals(mockedWebhookEnableOutput, webhookEnableOutput);
+
+        assertEquals(mockedTriggerContext, contextArgumentCaptor.getValue());
+        assertEquals(List.of(webhookUrl, "scope", "invitee.canceled"), stringArgumentCaptor.getAllValues());
+    }
+
+    @Test
+    void testWebhookRequest() {
+        when(mockedWebhookBody.getContent(any(TypeReference.class)))
+            .thenReturn(mockedObject);
+
+        Object result = CalendlyInviteeCanceledTrigger.webhookRequest(
+            mockedParameters, mockedParameters, mockedHttpHeaders, mockedHttpParameters, mockedWebhookBody,
+            mockedWebhookMethod, mockedWebhookEnableOutput, mockedTriggerContext);
+
+        assertEquals(mockedObject, result);
+    }
+}
---
+++ server/libs/modules/components/calendly/src/test/java/com/bytechef/component/calendly/util/CalendlyUtilsTest.java
@@ -0,0 +1,97 @@
+/*
+ * Copyright 2023-present ByteChef Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef.component.calendly.util;
+
+import static com.bytechef.component.calendly.constant.CalendlyConstants.RESOURCE;
+import static com.bytechef.component.calendly.constant.CalendlyConstants.SCOPE;
+import static com.bytechef.component.calendly.constant.CalendlyConstants.URI;
+import static com.bytechef.component.calendly.constant.CalendlyConstants.UUID;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import com.bytechef.component.definition.Context;
+import com.bytechef.component.definition.Context.Http;
+import com.bytechef.component.definition.TriggerDefinition.WebhookEnableOutput;
+import com.bytechef.component.definition.TypeReference;
+import java.util.List;
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+
+/**
+ * @author Monika Kušter
+ */
+class CalendlyUtilsTest {
+
+    private final ArgumentCaptor<Http.Body> bodyArgumentCaptor = ArgumentCaptor.forClass(Http.Body.class);
+    private final Context mockedContext = mock(Context.class);
+    private final Http.Executor mockedExecutor = mock(Http.Executor.class);
+    private final Http.Response mockedResponse = mock(Http.Response.class);
+
+    @Test
+    void testSubscribeWebhok() {
+        when(mockedContext.http(any()))
+            .thenReturn(mockedExecutor);
+        when(mockedExecutor.body(bodyArgumentCaptor.capture()))
+            .thenReturn(mockedExecutor);
+        when(mockedExecutor.configuration(any()))
+            .thenReturn(mockedExecutor);
+        when(mockedExecutor.execute())
+            .thenReturn(mockedResponse);
+        when(mockedResponse.getBody(any(TypeReference.class)))
+            .thenReturn(
+                Map.of(RESOURCE, Map.of("current_organization", "organization", URI, "user_uri")),
+                Map.of(RESOURCE, Map.of("uri", "uri/AAAAA")));
+
+        WebhookEnableOutput result = CalendlyUtils.subscribeWebhook(mockedContext, "webhookUrl", "scope", "event");
+
+        assertEquals(new WebhookEnableOutput(Map.of(UUID, "AAAAA"), null), result);
+
+        Http.Body body = bodyArgumentCaptor.getValue();
+
+        Object content = body.getContent();
+
+        Map<String, Object> expectedBody = Map.of(
+            "url", "webhookUrl",
+            "organization", "organization",
+            "user", "user_uri",
+            SCOPE, "scope",
+            "events", List.of("event"));
+
+        assertEquals(expectedBody, content);
+    }
+
+    @Test
+    void testUnsubscribeWebhook() {
+        when(mockedContext.http(any()))
+            .thenReturn(mockedExecutor);
+        when(mockedExecutor.configuration(any()))
+            .thenReturn(mockedExecutor);
+        when(mockedExecutor.execute())
+            .thenReturn(mockedResponse);
+
+        CalendlyUtils.unsubscribeWebhook(mockedContext, "uuid");
+
+        verify(mockedContext, times(1)).http(any());
+        verify(mockedExecutor, times(1)).configuration(any());
+        verify(mockedExecutor, times(1)).execute();
+    }
+}
