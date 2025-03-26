3acb91d07dda107650b9f7a8b3ff890ab0027c33
Monika Kušter <domitermonika@gmail.com>
2025-03-26 09:17:55 +0100

    388 - Add TwilioSendWhatsAppMessageAction for sending WhatsApp messages through Twilio

---
+++ server/libs/modules/components/twilio/src/main/java/com/bytechef/component/twilio/action/TwilioSendWhatsAppMessageAction.java
@@ -0,0 +1,149 @@
+/*
+ * Copyright 2023-present ByteChef Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef.component.twilio.action;
+
+import static com.bytechef.component.definition.Authorization.USERNAME;
+import static com.bytechef.component.definition.ComponentDsl.action;
+import static com.bytechef.component.definition.ComponentDsl.bool;
+import static com.bytechef.component.definition.ComponentDsl.object;
+import static com.bytechef.component.definition.ComponentDsl.outputSchema;
+import static com.bytechef.component.definition.ComponentDsl.string;
+import static com.bytechef.component.definition.Context.Http.BodyContentType.FORM_URL_ENCODED;
+import static com.bytechef.component.definition.Context.Http.responseType;
+import static com.bytechef.component.twilio.constant.TwilioConstants.BODY;
+import static com.bytechef.component.twilio.constant.TwilioConstants.CONTENT_SID;
+import static com.bytechef.component.twilio.constant.TwilioConstants.CONTENT_VARIABLES;
+import static com.bytechef.component.twilio.constant.TwilioConstants.FROM;
+import static com.bytechef.component.twilio.constant.TwilioConstants.MESSAGE_OUTPUT_PROPERTY;
+import static com.bytechef.component.twilio.constant.TwilioConstants.TO;
+import static com.bytechef.component.twilio.constant.TwilioConstants.USE_TEMPLATE;
+
+import com.bytechef.component.definition.ActionContext;
+import com.bytechef.component.definition.ComponentDsl.ModifiableActionDefinition;
+import com.bytechef.component.definition.Context.Http;
+import com.bytechef.component.definition.OptionsDataSource.ActionOptionsFunction;
+import com.bytechef.component.definition.Parameters;
+import com.bytechef.component.definition.Property.ControlType;
+import com.bytechef.component.definition.TypeReference;
+import com.bytechef.component.twilio.util.TwilioUtils;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @author Monika Kušter
+ */
+public class TwilioSendWhatsAppMessageAction {
+
+    public static final ModifiableActionDefinition ACTION_DEFINITION = action("sendWhatsAppMessage")
+        .title("Send WhatsApp Message")
+        .description("Send a new WhatsApp message.")
+        .properties(
+            string(TO)
+                .label("To")
+                .description("The recipient channel address.")
+                .controlType(ControlType.PHONE)
+                .exampleValue("whatsapp:+15554449999")
+                .required(true),
+            string(FROM)
+                .label("From")
+                .description("The sender's Twilio channel address.")
+                .exampleValue("whatsapp:+15554449999")
+                .controlType(ControlType.PHONE)
+                .required(true),
+            bool(USE_TEMPLATE)
+                .label("Use Template")
+                .description("Use a template for the message body.")
+                .defaultValue(true)
+                .required(true),
+            string(CONTENT_SID)
+                .label("Content Sid")
+                .description("The SID of the content template to be used for the message body.")
+                .options((ActionOptionsFunction<String>) TwilioUtils::getContentSidOptions)
+                .maxLength(34)
+                .minLength(34)
+                .displayCondition("%s == true".formatted(USE_TEMPLATE))
+                .required(true),
+            object(CONTENT_VARIABLES)
+                .description("Key-value pairs of template variables and their substitution values.")
+                .additionalProperties(string())
+                .displayCondition("%s == true".formatted(USE_TEMPLATE))
+                .required(false),
+            string(BODY)
+                .label("Body")
+                .description("The text content of the outgoing message.")
+                .maxLength(1600)
+                .displayCondition("%s == false".formatted(USE_TEMPLATE))
+                .required(true))
+        .output(outputSchema(MESSAGE_OUTPUT_PROPERTY))
+        .perform(TwilioSendWhatsAppMessageAction::perform);
+
+    private TwilioSendWhatsAppMessageAction() {
+    }
+
+    protected static Object perform(
+        Parameters inputParameters, Parameters connectionParameters, ActionContext actionContext) {
+
+        Map<String, String> bodyMap = createBodyMap(inputParameters, actionContext);
+
+        return actionContext
+            .http(http -> http.post("/Accounts/" + connectionParameters.getRequiredString(USERNAME) + "/Messages.json"))
+            .body(Http.Body.of(bodyMap, FORM_URL_ENCODED))
+            .configuration(responseType(Http.ResponseType.JSON))
+            .execute()
+            .getBody(new TypeReference<>() {});
+    }
+
+    private static Map<String, String> createBodyMap(Parameters inputParameters, ActionContext actionContext) {
+        String from = inputParameters.getRequiredString(FROM);
+        String to = inputParameters.getRequiredString(TO);
+
+        validatePhoneNumber(from);
+        validatePhoneNumber(to);
+
+        Map<String, String> bodyMap = new HashMap<>();
+
+        bodyMap.put(FROM, from);
+        bodyMap.put(TO, to);
+
+        if (inputParameters.getRequiredBoolean(USE_TEMPLATE)) {
+            bodyMap.put(CONTENT_SID, inputParameters.getRequiredString(CONTENT_SID));
+
+            Map<String, String> map = inputParameters.getMap(CONTENT_VARIABLES, String.class);
+
+            if (map != null) {
+                String contentVariables = actionContext.json(json -> json.write(map));
+
+                bodyMap.put(CONTENT_VARIABLES, contentVariables);
+            }
+        } else {
+            bodyMap.put(BODY, inputParameters.getRequiredString(BODY));
+        }
+
+        return bodyMap;
+    }
+
+    private static void validatePhoneNumber(String phoneNumber) {
+        Pattern pattern = Pattern.compile("whatsapp:\\+\\d{11,15}");
+        Matcher matcher = pattern.matcher(phoneNumber);
+
+        if (!matcher.find()) {
+            throw new IllegalArgumentException("Invalid phone number format.");
+        }
+    }
+}
---
+++ server/libs/modules/components/twilio/src/test/java/com/bytechef/component/twilio/action/TwilioSendWhatsAppMessageActionTest.java
@@ -0,0 +1,95 @@
+/*
+ * Copyright 2023-present ByteChef Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef.component.twilio.action;
+
+import static com.bytechef.component.definition.Authorization.USERNAME;
+import static com.bytechef.component.twilio.constant.TwilioConstants.CONTENT_SID;
+import static com.bytechef.component.twilio.constant.TwilioConstants.FROM;
+import static com.bytechef.component.twilio.constant.TwilioConstants.TO;
+import static com.bytechef.component.twilio.constant.TwilioConstants.USE_TEMPLATE;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import com.bytechef.component.definition.ActionContext;
+import com.bytechef.component.definition.Context.Http;
+import com.bytechef.component.definition.Parameters;
+import com.bytechef.component.definition.TypeReference;
+import com.bytechef.component.test.definition.MockParametersFactory;
+import java.util.Map;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+
+/**
+ * @author Monika Kušter
+ */
+class TwilioSendWhatsAppMessageActionTest {
+
+    private final ArgumentCaptor<Http.Body> bodyArgumentCaptor = ArgumentCaptor.forClass(Http.Body.class);
+    private final ActionContext mockedActionContext = mock(ActionContext.class);
+    private final Http.Executor mockedExecutor = mock(Http.Executor.class);
+    private Parameters mockedParameters;
+    private final Http.Response mockedResponse = mock(Http.Response.class);
+    private final Object mockedObject = mock(Object.class);
+
+    @BeforeEach
+    void beforeEach() {
+        when(mockedActionContext.http(any()))
+            .thenReturn(mockedExecutor);
+        when(mockedExecutor.body(bodyArgumentCaptor.capture()))
+            .thenReturn(mockedExecutor);
+        when(mockedExecutor.configuration(any()))
+            .thenReturn(mockedExecutor);
+        when(mockedExecutor.execute())
+            .thenReturn(mockedResponse);
+        when(mockedResponse.getBody(any(TypeReference.class)))
+            .thenReturn(mockedObject);
+    }
+
+    @Test
+    void testPerformWithInvalidPhoneNumberFormat() {
+        mockedParameters = MockParametersFactory.create(Map.of(FROM, "from", TO, "to"));
+
+        assertThrows(IllegalArgumentException.class,
+            () -> TwilioSendWhatsAppMessageAction.perform(mockedParameters, mockedParameters, mockedActionContext),
+            "Invalid phone number format.");
+    }
+
+    @Test
+    void testPerform() {
+        mockedParameters = MockParametersFactory.create(
+            Map.of(
+                FROM, "whatsapp:+12345678912", TO, "whatsapp:+12345678982", USERNAME, "accountSid", USE_TEMPLATE,
+                "true", CONTENT_SID, "1"));
+
+        Object result = TwilioSendWhatsAppMessageAction.perform(
+            mockedParameters, mockedParameters, mockedActionContext);
+
+        assertEquals(mockedObject, result);
+
+        Http.Body body = bodyArgumentCaptor.getValue();
+
+        Map<String, Object> expectedBody = Map.of(
+            FROM, "whatsapp:+12345678912", TO, "whatsapp:+12345678982", CONTENT_SID, "1");
+
+        assertEquals(expectedBody, body.getContent());
+        assertEquals(Http.BodyContentType.FORM_URL_ENCODED, body.getContentType());
+    }
+}
