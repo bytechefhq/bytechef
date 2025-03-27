a898b2e7f7df23f611384be11afb76c274745742
Matija Petanjek <matija.petanjek7@gmail.com>
2025-03-27 20:12:30 +0100

    1132 add classes for sending email and webhook notifications

---
+++ server/libs/platform/platform-configuration/platform-configuration-api/src/main/java/com/bytechef/platform/configuration/notification/NotificationSender.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2023-present ByteChef Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef.platform.configuration.notification;
+
+import com.bytechef.platform.configuration.domain.notification.Notification;
+
+/**
+ * @author Matija Petanjek
+ */
+public interface NotificationSender<T extends NotificationHandler> {
+
+    Notification.Type getType();
+
+    void send(Notification notification, T notificationHandler, NotificationHandlerContext notificationHandlerContext);
+}
