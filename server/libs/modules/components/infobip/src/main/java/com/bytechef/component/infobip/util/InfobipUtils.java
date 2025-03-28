e65801916c53a98b2354a533a4aaed121962051b
Monika Kušter <domitermonika@gmail.com>
2025-03-28 19:22:35 +0100

    1545 - Implement new SMS trigger

---
+++ server/libs/modules/components/infobip/src/main/java/com/bytechef/component/infobip/util/InfobipUtils.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2023-present ByteChef Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef.component.infobip.util;
+
+import static com.bytechef.component.infobip.constant.InfobipConstants.CONFIGURATION_KEY;
+import static com.bytechef.component.infobip.constant.InfobipConstants.NUMBER_KEY;
+
+import com.bytechef.component.definition.Context.Http;
+import com.bytechef.component.definition.TriggerContext;
+import com.bytechef.component.definition.TriggerDefinition.WebhookEnableOutput;
+import com.bytechef.component.definition.TypeReference;
+import java.util.Map;
+
+/**
+ * @author Monika Kušter
+ */
+public class InfobipUtils {
+
+    public static WebhookEnableOutput getWebhookEnableOutput(
+        String numberKey, String channel, String webhookUrl, TriggerContext triggerContext) {
+
+        Map<String, Object> body =
+            triggerContext.http(http -> http.post("/resource-management/1/inbound-message-configurations"))
+                .body(
+                    Http.Body.of(
+                        "channel", channel,
+                        NUMBER_KEY, numberKey,
+                        "forwarding", Map.of("type", "HTTP_FORWARD", "url", webhookUrl)))
+                .configuration(Http.responseType(Http.ResponseType.JSON))
+                .execute()
+                .getBody(new TypeReference<>() {});
+
+        return new WebhookEnableOutput(Map.of(CONFIGURATION_KEY, (String) body.get(CONFIGURATION_KEY)), null);
+    }
+
+    public static void unsubscribeWebhook(String configurationKey, TriggerContext triggerContext) {
+        triggerContext.http(http -> http.delete(
+            "/resource-management/1/inbound-message-configurations/%s".formatted(configurationKey)))
+            .configuration(Http.responseType(Http.ResponseType.JSON))
+            .execute();
+    }
+
+}
---
+++ server/libs/modules/components/infobip/src/test/java/com/bytechef/component/infobip/util/InfobipUtilsTest.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2023-present ByteChef Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef.component.infobip.util;
+
+import static com.bytechef.component.infobip.constant.InfobipConstants.CONFIGURATION_KEY;
+import static com.bytechef.component.infobip.constant.InfobipConstants.NUMBER_KEY;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import com.bytechef.component.definition.Context.Http;
+import com.bytechef.component.definition.TriggerContext;
+import com.bytechef.component.definition.TriggerDefinition.WebhookEnableOutput;
+import com.bytechef.component.definition.TypeReference;
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+
+/**
+ * @author Monika Kušter
+ */
+class InfobipUtilsTest {
+
+    private final ArgumentCaptor<Http.Body> bodyArgumentCaptor = ArgumentCaptor.forClass(Http.Body.class);
+    private final Http.Executor mockedExecutor = mock(Http.Executor.class);
+    private final Http.Response mockedResponse = mock(Http.Response.class);
+    private final TriggerContext mockedTriggerContext = mock(TriggerContext.class);
+    private final Map<String, Object> responseMap = Map.of(CONFIGURATION_KEY, "abc");
+
+    @Test
+    void testGetWebhookEnableOutput() {
+        when(mockedTriggerContext.http(any()))
+            .thenReturn(mockedExecutor);
+        when(mockedExecutor.body(bodyArgumentCaptor.capture()))
+            .thenReturn(mockedExecutor);
+        when(mockedExecutor.configuration(any()))
+            .thenReturn(mockedExecutor);
+        when(mockedExecutor.execute())
+            .thenReturn(mockedResponse);
+        when(mockedResponse.getBody(any(TypeReference.class)))
+            .thenReturn(responseMap);
+
+        WebhookEnableOutput webhookEnableOutput =
+            InfobipUtils.getWebhookEnableOutput("numberKey", "SMS", "webhookUrl", mockedTriggerContext);
+
+        assertEquals(new WebhookEnableOutput(Map.of(CONFIGURATION_KEY, "abc"), null), webhookEnableOutput);
+
+        Http.Body body = bodyArgumentCaptor.getValue();
+
+        Map<String, Object> expectedBody = Map.of("channel", "SMS",
+            NUMBER_KEY, "numberKey",
+            "forwarding", Map.of("type", "HTTP_FORWARD", "url", "webhookUrl"));
+
+        assertEquals(expectedBody, body.getContent());
+    }
+}
