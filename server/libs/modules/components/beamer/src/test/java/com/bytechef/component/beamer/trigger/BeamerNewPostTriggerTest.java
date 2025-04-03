7c32df59d3ee8460c4a07b7316521b7ffcd606e3
Nikolina Spehar <speharnikolina87@gmail.com>
2025-04-03 15:26:45 +0200

    2038 - newPostTrigger added

---
+++ server/libs/modules/components/beamer/src/test/java/com/bytechef/component/beamer/trigger/BeamerNewPostTriggerTest.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright 2023-present ByteChef Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef.component.beamer.trigger;
+
+import static com.bytechef.component.beamer.constant.BeamerConstants.DATE_FROM;
+import static com.bytechef.component.beamer.constant.BeamerConstants.LAST_TIME_CHECKED;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.mockStatic;
+import static org.mockito.Mockito.when;
+
+import com.bytechef.component.definition.Context.Http;
+import com.bytechef.component.definition.Parameters;
+import com.bytechef.component.definition.TriggerContext;
+import com.bytechef.component.definition.TriggerDefinition.PollOutput;
+import com.bytechef.component.definition.TypeReference;
+import com.bytechef.component.test.definition.MockParametersFactory;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.List;
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+
+/**
+ * @author Nikolina Spehar
+ */
+class BeamerNewPostTriggerTest {
+
+    private final Http.Executor mockedExecutor = mock(Http.Executor.class);
+    private final Http.Response mockedResponse = mock(Http.Response.class);
+    private final TriggerContext mockedTriggerContext = mock(TriggerContext.class);
+    private final ArgumentCaptor<String> stringArgumentCaptor = ArgumentCaptor.forClass(String.class);
+    private final List<Map<String, Object>> responseList = List.of(Map.of());
+
+    @Test
+    void poll() {
+        LocalDateTime startDate = LocalDateTime.of(2025, 4, 3, 0, 0, 0);
+        LocalDateTime endDate = LocalDateTime.of(2025, 4, 4, 0, 0, 0);
+
+        Parameters mockedParameters = MockParametersFactory.create(
+            Map.of(LAST_TIME_CHECKED, startDate.toString()));
+
+        try (MockedStatic<LocalDateTime> localDateTimeMockedStatic = mockStatic(
+            LocalDateTime.class, Mockito.CALLS_REAL_METHODS)) {
+
+            localDateTimeMockedStatic.when(() -> LocalDateTime.now(any(ZoneId.class)))
+                .thenReturn(endDate);
+
+            when(mockedParameters.getLocalDateTime(LAST_TIME_CHECKED, LocalDateTime.now(ZoneId.of("GMT"))))
+                .thenReturn(startDate);
+            when(mockedTriggerContext.http(any()))
+                .thenReturn(mockedExecutor);
+            when(mockedExecutor.queryParameter(stringArgumentCaptor.capture(), stringArgumentCaptor.capture()))
+                .thenReturn(mockedExecutor);
+            when(mockedExecutor.configuration(any()))
+                .thenReturn(mockedExecutor);
+            when(mockedExecutor.execute())
+                .thenReturn(mockedResponse);
+            when(mockedResponse.getBody(any(TypeReference.class)))
+                .thenReturn(responseList);
+
+            PollOutput pollOutput = BeamerNewPostTrigger.poll(
+                mockedParameters, mockedParameters, mockedParameters, mockedTriggerContext);
+
+            assertEquals(List.of(Map.of()), pollOutput.records());
+            assertFalse(pollOutput.pollImmediately());
+
+            assertEquals(List.of(DATE_FROM, startDate.toString()), stringArgumentCaptor.getAllValues());
+        }
+    }
+}
