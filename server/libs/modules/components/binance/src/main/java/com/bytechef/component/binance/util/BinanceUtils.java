b3b7992d375ca3f2c5065f24e426ef12e29a96d8
marijahorvat171 <marijahorvat171@gmail.com>
2025-04-07 10:54:17 +0200

    2284- add binance component

---
+++ server/libs/modules/components/binance/src/main/java/com/bytechef/component/binance/util/BinanceUtils.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2023-present ByteChef Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef.component.binance.util;
+
+import static com.bytechef.component.binance.constant.BinanceConstants.SYMBOL;
+import static com.bytechef.component.definition.ComponentDsl.option;
+import static com.bytechef.component.definition.Context.Http.responseType;
+
+import com.bytechef.component.definition.Context;
+import com.bytechef.component.definition.Option;
+import com.bytechef.component.definition.Parameters;
+import com.bytechef.component.definition.TypeReference;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author Marija Horvat
+ */
+public class BinanceUtils {
+
+    private BinanceUtils() {
+    }
+
+    public static List<Option<String>> getSymbolsOptions(
+        Parameters inputParameters, Parameters connectionParameters, Map<String, String> dependencyPaths,
+        String searchText, Context context) {
+
+        Map<String, Object> body = context
+            .http(http -> http.get("https://api.binance.com/api/v3/exchangeInfo"))
+            .configuration(responseType(Context.Http.ResponseType.JSON))
+            .execute()
+            .getBody(new TypeReference<>() {});
+
+        List<Option<String>> options = new ArrayList<>();
+
+        if (body.get("symbols") instanceof List<?> contacts) {
+            for (Object contact : contacts) {
+                if (contact instanceof Map<?, ?> map) {
+                    String symbol = (String) map.get(SYMBOL);
+
+                    options.add(option(symbol, symbol));
+                }
+            }
+        }
+
+        return options;
+    }
+}
