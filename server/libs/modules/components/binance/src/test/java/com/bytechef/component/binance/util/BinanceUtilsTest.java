224c474acf02f09dac3ccca34ca20b40e4bc0a4a
marijahorvat171 <marijahorvat171@gmail.com>
2025-04-07 10:54:17 +0200

    2284- add binance component

---
+++ server/libs/modules/components/binance/src/test/java/com/bytechef/component/binance/util/BinanceUtilsTest.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2023-present ByteChef Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef.component.binance.util;
+
+import static com.bytechef.component.binance.constant.BinanceConstants.SYMBOL;
+import static com.bytechef.component.definition.ComponentDsl.option;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import com.bytechef.component.definition.Context;
+import com.bytechef.component.definition.Option;
+import com.bytechef.component.definition.Parameters;
+import com.bytechef.component.definition.TypeReference;
+import java.util.List;
+import java.util.Map;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Marija Horvat
+ */
+class BinanceUtilsTest {
+
+    private final List<Option<String>> expectedOptions = List.of(
+        option("ETHBTC", "ETHBTC"),
+        option("LTCBTC", "LTCBTC"));
+    private final Context mockedContext = mock(Context.class);
+    private final Context.Http.Executor mockedExecutor = mock(Context.Http.Executor.class);
+    private final Parameters mockedParameters = mock(Parameters.class);
+    private final Context.Http.Response mockedResponse = mock(Context.Http.Response.class);
+
+    @Test
+    void testGetSymbolsOptions() {
+        when(mockedContext.http(any()))
+            .thenReturn(mockedExecutor);
+        when(mockedExecutor.configuration(any()))
+            .thenReturn(mockedExecutor);
+        when(mockedExecutor.execute())
+            .thenReturn(mockedResponse);
+        when(mockedResponse.getBody(any(TypeReference.class)))
+            .thenReturn(Map.of(
+                "symbols", List.of(
+                    Map.of(SYMBOL, "ETHBTC"),
+                    Map.of(SYMBOL, "LTCBTC"))));
+
+        List<Option<String>> result = BinanceUtils.getSymbolsOptions(
+            mockedParameters, mockedParameters, Map.of(), "", mockedContext);
+
+        assertThat(result, Matchers.containsInAnyOrder(expectedOptions.toArray()));
+    }
+}
