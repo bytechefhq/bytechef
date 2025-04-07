2d284e1d3115d1a1377948dec11e268d2c5a79f3
Monika Kušter <domitermonika@gmail.com>
2025-04-07 13:12:53 +0200

    2040 - SF

--- server/libs/modules/components/brevo/src/main/java/com/bytechef/component/brevo/trigger/BrevoTransactionalEmailOpenedTrigger.java
+++ server/libs/modules/components/brevo/src/main/java/com/bytechef/component/brevo/trigger/BrevoTransactionalEmailOpenedTrigger.java
@@ -19,11 +19,18 @@ package com.bytechef.component.brevo.trigger;
 import static com.bytechef.component.brevo.constant.BrevoConstants.ID;
 import static com.bytechef.component.definition.ComponentDsl.trigger;
 
-import com.bytechef.component.brevo.util.BrevoUtils;
-import com.bytechef.component.definition.ComponentDsl;
+import com.bytechef.component.definition.ComponentDsl.ModifiableTriggerDefinition;
+import com.bytechef.component.definition.Context.Http;
 import com.bytechef.component.definition.Parameters;
 import com.bytechef.component.definition.TriggerContext;
-import com.bytechef.component.definition.TriggerDefinition;
+import com.bytechef.component.definition.TriggerDefinition.HttpHeaders;
+import com.bytechef.component.definition.TriggerDefinition.HttpParameters;
+import com.bytechef.component.definition.TriggerDefinition.TriggerType;
+import com.bytechef.component.definition.TriggerDefinition.WebhookBody;
+import com.bytechef.component.definition.TriggerDefinition.WebhookEnableOutput;
+import com.bytechef.component.definition.TriggerDefinition.WebhookMethod;
+import com.bytechef.component.definition.TypeReference;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -31,42 +38,44 @@ import java.util.Map;
  */
 public class BrevoTransactionalEmailOpenedTrigger {
 
-    public static final ComponentDsl.ModifiableTriggerDefinition TRIGGER_DEFINITION =
-        trigger("transactionalEmailOpened")
-            .title("Transactional Email Opened")
-            .description("Triggers when transactional email is opened.")
-            .type(TriggerDefinition.TriggerType.DYNAMIC_WEBHOOK)
-            .properties()
-            .output()
-            .webhookEnable(BrevoTransactionalEmailOpenedTrigger::webhookEnable)
-            .webhookDisable(BrevoTransactionalEmailOpenedTrigger::webhookDisable)
-            .webhookRequest(BrevoTransactionalEmailOpenedTrigger::webhookRequest);
+    public static final ModifiableTriggerDefinition TRIGGER_DEFINITION = trigger("transactionalEmailOpened")
+        .title("Transactional Email Opened")
+        .description("Triggers when transactional email is opened.")
+        .type(TriggerType.DYNAMIC_WEBHOOK)
+        .output()
+        .webhookEnable(BrevoTransactionalEmailOpenedTrigger::webhookEnable)
+        .webhookDisable(BrevoTransactionalEmailOpenedTrigger::webhookDisable)
+        .webhookRequest(BrevoTransactionalEmailOpenedTrigger::webhookRequest);
 
     private BrevoTransactionalEmailOpenedTrigger() {
     }
 
-    protected static TriggerDefinition.WebhookEnableOutput webhookEnable(
+    protected static WebhookEnableOutput webhookEnable(
         Parameters inputParameters, Parameters connectionParameters, String webhookUrl,
         String workflowExecutionId, TriggerContext context) {
 
-        return new TriggerDefinition.WebhookEnableOutput(
-            Map.of(ID,
-                BrevoUtils.createWebhook(webhookUrl, context)),
-            null);
+        Map<String, ?> body = context.http(http -> http.post("/webhooks"))
+            .body(Http.Body.of(
+                "url", webhookUrl,
+                "events", List.of("opened")))
+            .configuration(Http.responseType(Http.ResponseType.JSON))
+            .execute()
+            .getBody(new TypeReference<>() {});
+
+        return new WebhookEnableOutput(Map.of(ID, body.get(ID)), null);
     }
 
     protected static void webhookDisable(
         Parameters inputParameters, Parameters connectionParameters, Parameters outputParameters,
         String workflowExecutionId, TriggerContext context) {
 
-        BrevoUtils.deleteWebhook(outputParameters, context);
+        context.http(http -> http.delete("/webhooks/" + outputParameters.getRequiredInteger(ID)))
+            .execute();
     }
 
     protected static Object webhookRequest(
-        Parameters inputParameters, Parameters connectionParameters, TriggerDefinition.HttpHeaders headers,
-        TriggerDefinition.HttpParameters parameters,
-        TriggerDefinition.WebhookBody body, TriggerDefinition.WebhookMethod method,
-        TriggerDefinition.WebhookEnableOutput output, TriggerContext context) {
+        Parameters inputParameters, Parameters connectionParameters, HttpHeaders headers, HttpParameters parameters,
+        WebhookBody body, WebhookMethod method, WebhookEnableOutput output, TriggerContext context) {
 
         return body.getContent();
     }
