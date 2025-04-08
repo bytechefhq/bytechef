3646431b34d2de17598ccba5ae687f6d489df001
Nikolina Spehar <speharnikolina87@gmail.com>
2025-04-08 14:43:51 +0200

    2299 - mautic component - without testing

---
+++ server/libs/modules/components/mautic/src/main/java/com/bytechef/component/mautic/constant/MauticConstants.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2023-present ByteChef Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef.component.mautic.constant;
+
+/**
+ * @author Nikolina Spehar
+ */
+public class MauticConstants {
+
+    public static final String BASE_URL = "baseUrl";
+    public static final String COMPANY_NAME = "companyname";
+    public static final String EMAIL = "email";
+    public static final String FIRSTNAME = "firstname";
+    public static final String ID = "id";
+    public static final String IS_PUBLISHED = "isPublished";
+    public static final String LASTNAME = "lastname";
+    public static final String OVERWRITE_WITH_BLANK = "overwriteWithBlank";
+    public static final String PASSWORD = "password";
+    public static final String USERNAME = "username";
+
+    private MauticConstants() {
+    }
+}
