83d3f9c0dbcd6b2a081ac7daf9c7af301c69a30a
Nikolina Spehar <speharnikolina87@gmail.com>
2025-04-08 14:43:51 +0200

    2299 - mautic component - without testing

---
+++ server/libs/modules/components/mautic/src/main/java/com/bytechef/component/mautic/util/MauticUtils.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2023-present ByteChef Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef.component.mautic.util;
+
+import static com.bytechef.component.definition.ComponentDsl.option;
+import static com.bytechef.component.definition.Context.Http.responseType;
+import static com.bytechef.component.mautic.constant.MauticConstants.ID;
+
+import com.bytechef.component.definition.Context;
+import com.bytechef.component.definition.Context.Http.ResponseType;
+import com.bytechef.component.definition.Option;
+import com.bytechef.component.definition.Parameters;
+import com.bytechef.component.definition.TypeReference;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author Nikolina Spehar
+ */
+public class MauticUtils {
+
+    public static List<Option<String>> getCompanyOptions(
+        Parameters inputParameters, Parameters connectionParameters, Map<String, String> stringStringMap, String s,
+        Context context) {
+
+        List<Map<String, Object>> companyList = context.http(http -> http.get("/companies"))
+            .configuration(responseType(ResponseType.JSON))
+            .execute()
+            .getBody(new TypeReference<>() {});
+
+        return getOptions(companyList);
+    }
+
+    public static List<Option<String>> getContactOptions(
+        Parameters inputParameters, Parameters connectionParameters, Map<String, String> stringStringMap, String s,
+        Context context) {
+
+        List<Map<String, Object>> contactsList = context.http(http -> http.get("/contacts"))
+            .configuration(responseType(ResponseType.JSON))
+            .execute()
+            .getBody(new TypeReference<>() {});
+
+        return getOptions(contactsList);
+    }
+
+    private static List<Option<String>> getOptions(List<Map<String, Object>> options) {
+        List<Option<String>> optionsList = new ArrayList<>();
+
+        for (Map<String, Object> option : options) {
+            optionsList.add(option(option.get(ID)
+                .toString(),
+                option.get(ID)
+                    .toString()));
+        }
+
+        return optionsList;
+    }
+}
