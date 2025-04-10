8277c2b2d90edfb421089758cf0bf17381f55872
marijahorvat171 <marijahorvat171@gmail.com>
2025-04-10 14:28:07 +0200

    2291- add google meet component

---
+++ server/libs/modules/components/google/google-meet/src/main/java/com/bytechef/component/google/meet/action/GoogleMeetListParticipantsAction.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright 2023-present ByteChef Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef.component.google.meet.action;
+
+import static com.bytechef.component.definition.ComponentDsl.ModifiableActionDefinition;
+import static com.bytechef.component.definition.ComponentDsl.action;
+import static com.bytechef.component.definition.ComponentDsl.array;
+import static com.bytechef.component.definition.ComponentDsl.object;
+import static com.bytechef.component.definition.ComponentDsl.outputSchema;
+import static com.bytechef.component.definition.ComponentDsl.string;
+import static com.bytechef.component.definition.Context.Http.responseType;
+import static com.bytechef.component.google.meet.constant.GoogleMeetConstants.CONFERENCE_RECORDS;
+import static com.bytechef.component.google.meet.constant.GoogleMeetConstants.NAME;
+
+import com.bytechef.component.definition.Context;
+import com.bytechef.component.definition.OptionsDataSource.ActionOptionsFunction;
+import com.bytechef.component.definition.Parameters;
+import com.bytechef.component.definition.TypeReference;
+import com.bytechef.component.google.meet.util.GoogleMeetUtils;
+
+/**
+ * @author Marija Horvat
+ */
+public class GoogleMeetListParticipantsAction {
+
+    public static final ModifiableActionDefinition ACTION_DEFINITION = action("listParticipants")
+        .title("List Participants")
+        .description("Lists the participants in a conference record. By default, ordered by join time " +
+            "and in descending order.")
+        .properties(
+            string(CONFERENCE_RECORDS)
+                .label("Conference Records")
+                .description("Conference Records")
+                .options((ActionOptionsFunction<String>) GoogleMeetUtils::getConferenceRecordsOptions)
+                .required(true))
+        .output(
+            outputSchema(
+                object()
+                    .properties(
+                        array("participants")
+                            .description("List of participants in one page.")
+                            .items(
+                                string(NAME)
+                                    .description("Resource name of the participant. " +
+                                        "Format: conferenceRecords/{conferenceRecord}/participants/{participant}"),
+                                object("user")
+                                    .description("User can be of type: signedinUser, anonymousUser, phoneUser"),
+                                string("earliestStartTime")
+                                    .description("Time when the participant first joined the meeting."),
+                                string("latestEndTime")
+                                    .description("Time when the participant left the meeting for the last time. " +
+                                        "This can be null if it's an active meeting.")))))
+        .perform(GoogleMeetListParticipantsAction::perform);
+
+    private GoogleMeetListParticipantsAction() {
+    }
+
+    public static Object perform(Parameters inputParameters, Parameters connectionParameters, Context context) {
+        return context
+            .http(http -> http.get("https://meet.googleapis.com/v2/" +
+                inputParameters.getRequiredString(CONFERENCE_RECORDS) + "/participants"))
+            .configuration(responseType(Context.Http.ResponseType.JSON))
+            .execute()
+            .getBody(new TypeReference<>() {});
+    }
+}
---
+++ server/libs/modules/components/google/google-meet/src/main/java/com/bytechef/component/google/meet/util/GoogleMeetUtils.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2023-present ByteChef Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef.component.google.meet.util;
+
+import static com.bytechef.component.definition.ComponentDsl.option;
+import static com.bytechef.component.definition.Context.Http.responseType;
+
+import com.bytechef.component.definition.Context;
+import com.bytechef.component.definition.Option;
+import com.bytechef.component.definition.Parameters;
+import com.bytechef.component.definition.TypeReference;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author Marija Horvat
+ */
+public class GoogleMeetUtils {
+
+    public static List<Option<String>> getConferenceRecordsOptions(
+        Parameters inputParameters, Parameters connectionParameters, Map<String, String> stringStringMap, String s,
+        Context context) {
+
+        Map<?, ?> map =
+            context.http(http -> http.get("https://meet.googleapis.com/v2/conferenceRecords"))
+                .configuration(responseType(Context.Http.ResponseType.JSON))
+                .execute()
+                .getBody(new TypeReference<>() {});
+
+        List<Option<String>> options = new ArrayList<>();
+
+        List<Map<String, String>> items = (List<Map<String, String>>) map.get("conferenceRecords");
+
+        for (Map<String, String> item : items) {
+            String name = item.get("name");
+            options.add(option(name.substring("conferenceRecords/".length()), name));
+        }
+        return options;
+    }
+}
