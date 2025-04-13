f138a6ae6d5da101e5a418b9ebe11622ac8fbb42
Ivica Cardic <ivica.cardic@gmail.com>
2025-04-13 19:13:41 +0200

    2223 - Not used

--- server/libs/modules/components/encryption-helper/build.gradle.kts
+++ server/libs/modules/components/encryption-helper/build.gradle.kts
@@ -3,5 +3,4 @@ version="1.0"
 dependencies {
     implementation("org.bouncycastle:bcprov-jdk18on:1.80")
     implementation("org.bouncycastle:bcpg-jdk18on:1.76")
-    implementation("commons-io:commons-io:2.18.0")
 }
