c66d4d9cea18a1fbead21e8cf4a58e7b02917961
Monika Kušter <domitermonika@gmail.com>
2025-04-14 17:32:19 +0200

    2305 -  initial NocoDB component

---
+++ server/libs/modules/components/nocodb/src/test/java/com/bytechef/component/nocodb/action/NocoDbUpdateRecordsTest.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2023-present ByteChef Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef.component.nocodb.action;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.mockStatic;
+import static org.mockito.Mockito.when;
+
+import com.bytechef.component.definition.Context;
+import com.bytechef.component.definition.Context.Http;
+import com.bytechef.component.definition.Parameters;
+import com.bytechef.component.nocodb.util.NocoDbUtils;
+import java.util.List;
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.MockedStatic;
+
+/**
+ * @author Monika Kušter
+ */
+class NocoDbUpdateRecordsTest {
+
+    private final ArgumentCaptor<Http.Body> bodyArgumentCaptor = ArgumentCaptor.forClass(Http.Body.class);
+    private final ArgumentCaptor<Parameters> parametersArgumentCaptor = ArgumentCaptor.forClass(Parameters.class);
+    private final Context mockedContext = mock(Context.class);
+    private final Http.Executor mockedExecutor = mock(Http.Executor.class);
+    private final Parameters mockedParameters = mock(Parameters.class);
+    private final Object mockedObject = mock(Object.class);
+    private final Http.Response mockedResponse = mock(Http.Response.class);
+
+    @Test
+    void testPerform() {
+        try (MockedStatic<NocoDbUtils> nocoDbUtilsMockedStatic = mockStatic(NocoDbUtils.class)) {
+            List<Map<String, String>> records = List.of(Map.of("key", "value"));
+            nocoDbUtilsMockedStatic.when(
+                () -> NocoDbUtils.transformRecordsForInsertion(parametersArgumentCaptor.capture()))
+                .thenReturn(records);
+
+            when(mockedContext.http(any()))
+                .thenReturn(mockedExecutor);
+            when(mockedExecutor.body(bodyArgumentCaptor.capture()))
+                .thenReturn(mockedExecutor);
+            when(mockedExecutor.configuration(any()))
+                .thenReturn(mockedExecutor);
+            when(mockedExecutor.execute())
+                .thenReturn(mockedResponse);
+            when(mockedResponse.getBody())
+                .thenReturn(mockedObject);
+
+            Object result = NocoDbUpdateRecords.perform(mockedParameters, mockedParameters, mockedContext);
+
+            assertEquals(mockedObject, result);
+            assertEquals(mockedParameters, parametersArgumentCaptor.getValue());
+
+            Http.Body body = bodyArgumentCaptor.getValue();
+
+            assertEquals(records, body.getContent());
+        }
+    }
+}
