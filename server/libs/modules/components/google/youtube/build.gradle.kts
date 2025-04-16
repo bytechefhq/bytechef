3ee2c8a2fd31fc434eee65706329be88aefd6195
Nikolina Spehar <speharnikolina87@gmail.com>
2025-04-16 08:16:57 +0200

    2316 - youtube component added

---
+++ server/libs/modules/components/google/youtube/build.gradle.kts
@@ -0,0 +1,6 @@
+version="1.0"
+
+dependencies {
+    api(project(":server:libs:modules:components:google:google-commons"))
+    implementation("com.google.apis:google-api-services-youtube:v3-rev222-1.25.0")
+}
