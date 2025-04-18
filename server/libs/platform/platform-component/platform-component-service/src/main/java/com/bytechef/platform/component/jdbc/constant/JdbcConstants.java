551960a6cf7eaef2450c710e07cead251814a999
Monika Kušter <domitermonika@gmail.com>
2025-04-18 09:06:14 +0200

    2246 - SF

--- server/libs/platform/platform-component/platform-component-service/src/main/java/com/bytechef/platform/component/jdbc/constant/JdbcConstants.java
+++ server/libs/platform/platform-component/platform-component-service/src/main/java/com/bytechef/platform/component/jdbc/constant/JdbcConstants.java
@@ -21,25 +21,25 @@ package com.bytechef.platform.component.jdbc.constant;
  */
 public final class JdbcConstants {
 
-    public static final String QUERY = "query";
-    public static final String INSERT = "insert";
-    public static final String UPDATE = "update";
-    public static final String DELETE = "delete";
-    public static final String EXECUTE = "execute";
-    public static final String TABLE = "table";
-    public static final String PARAMETERS = "parameters";
-    public static final String SCHEMA = "schema";
     public static final String COLUMNS = "columns";
-    public static final String ROWS = "rows";
-    public static final String DELETE_KEY = "deleteKey";
-    public static final String UPDATE_KEY = "updateKey";
-    public static final String HOST = "host";
     public static final String DATABASE = "database";
-    public static final String USERNAME = "username";
+    public static final String DELETE = "delete";
+    public static final String DELETE_KEY = "deleteKey";
+    public static final String EXECUTE = "execute";
+    public static final String HOST = "host";
+    public static final String INSERT = "insert";
+    public static final String NAME = "name";
+    public static final String PARAMETERS = "parameters";
     public static final String PASSWORD = "password";
     public static final String PORT = "port";
-    public static final String NAME = "name";
+    public static final String QUERY = "query";
+    public static final String ROWS = "rows";
+    public static final String SCHEMA = "schema";
+    public static final String TABLE = "table";
     public static final String TYPE = "type";
+    public static final String UPDATE = "update";
+    public static final String UPDATE_KEY = "updateKey";
+    public static final String USERNAME = "username";
     public static final String VALUES = "values";
 
     private JdbcConstants() {
