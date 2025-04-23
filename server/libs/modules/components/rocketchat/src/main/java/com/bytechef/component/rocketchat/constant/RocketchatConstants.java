7b5a50b62346210004d0dabd606b6af05e3543f8
marijahorvat171 <marijahorvat171@gmail.com>
2025-04-23 10:46:35 +0200

    2323 - add rocketchat component

---
+++ server/libs/modules/components/rocketchat/src/main/java/com/bytechef/component/rocketchat/constant/RocketchatConstants.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright 2023-present ByteChef Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef.component.rocketchat.constant;
+
+import static com.bytechef.component.definition.ComponentDsl.ModifiableObjectProperty;
+import static com.bytechef.component.definition.ComponentDsl.array;
+import static com.bytechef.component.definition.ComponentDsl.bool;
+import static com.bytechef.component.definition.ComponentDsl.integer;
+import static com.bytechef.component.definition.ComponentDsl.object;
+import static com.bytechef.component.definition.ComponentDsl.string;
+
+public class RocketchatConstants {
+
+    public static final String AUTH_TOKEN = "X-Auth-Token";
+    public static final String USER_ID = "X-User-Id";
+    public static final String USERNAME = "username";
+    public static final String NAME = "name";
+    public static final String DOMAIN = "domain";
+    public static final String TEXT = "text";
+    public static final String MEMBERS = "members";
+    public static final String READ_ONLY = "readOnly";
+    public static final String EXCLUDE_SELF = "excludeSelf";
+    public static final String ID = "id";
+
+    public static final ModifiableObjectProperty POST_MESSAGE_RESPONSE_PROPERTY = object()
+        .properties(
+            integer("ts"),
+            string("channel"),
+            object("message")
+                .properties(
+                    string("alias"),
+                    string("msg"),
+                    array("attachments"),
+                    bool("parseUrls"),
+                    bool("groupable"),
+                    string("ts"),
+                    object("u")
+                        .properties(
+                            string("_id"),
+                            string("username"),
+                            string("name")),
+                    string("rid"),
+                    string("_id"),
+                    string("_updateAt"),
+                    array("urls"),
+                    array("mentions"),
+                    array("channels"),
+                    array("md")),
+            bool("success"));
+
+    public static final ModifiableObjectProperty CHANNEL_RESPONSE_PROPERTY = object()
+        .properties(
+            object("channel")
+                .properties(
+                    string("_id"),
+                    string("fname"),
+                    string("_updateAt"),
+                    object("customFields"),
+                    string("name"),
+                    string("t"),
+                    integer("msgs"),
+                    integer("usersCount"),
+                    object("u")
+                        .properties(
+                            string("_id"),
+                            string("username"),
+                            string("name")),
+                    string("ts"),
+                    bool("ro"),
+                    bool("default"),
+                    bool("sysMes")),
+            bool("success"));
+
+    private RocketchatConstants() {
+    }
+}
---
+++ server/libs/modules/components/rocketchat/src/test/java/com/bytechef/component/rocketchat/util/RocketchatUtilsTest.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright 2023-present ByteChef Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef.component.rocketchat.util;
+
+import static com.bytechef.component.definition.ComponentDsl.option;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import com.bytechef.component.definition.Context;
+import com.bytechef.component.definition.Context.Http;
+import com.bytechef.component.definition.Option;
+import com.bytechef.component.definition.Parameters;
+import com.bytechef.component.definition.TypeReference;
+import java.util.List;
+import java.util.Map;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Marija Horvat
+ */
+class RocketchatUtilsTest {
+
+    private final Context mockedContext = mock(Context.class);
+    private final Http.Executor mockedExecutor = mock(Http.Executor.class);
+    private final Parameters mockedParameters = mock(Parameters.class);
+    private final Http.Response mockedResponse = mock(Http.Response.class);
+
+    @BeforeEach
+    void beforeEach() {
+        when(mockedContext.http(any()))
+            .thenReturn(mockedExecutor);
+        when(mockedExecutor.configuration(any()))
+            .thenReturn(mockedExecutor);
+        when(mockedExecutor.execute())
+            .thenReturn(mockedResponse);
+    }
+
+    @Test
+    void testGetUsersOptions() {
+        when(mockedResponse.getBody(any(TypeReference.class)))
+            .thenReturn(Map.of(
+                "users", List.of(
+                    Map.of("username", "user1"),
+                    Map.of("username", "user2"))));
+
+        List<Option<String>> expectedOptions = List.of(
+            option("user1", "user1"),
+            option("user2", "user2"));
+
+        List<Option<String>> result = RocketchatUtils.getUsersOptions(
+            mockedParameters, mockedParameters, Map.of(), "", mockedContext);
+
+        assertEquals(result, expectedOptions);
+    }
+
+    @Test
+    void testGetChannelsOptions() {
+        when(mockedResponse.getBody(any(TypeReference.class)))
+            .thenReturn(Map.of(
+                "channels", List.of(
+                    Map.of("name", "channel1"),
+                    Map.of("name", "channel2"))));
+
+        List<Option<String>> expectedOptions = List.of(
+            option("channel1", "#channel1"),
+            option("channel2", "#channel2"));
+
+        List<Option<String>> result = RocketchatUtils.getChannelsOptions(
+            mockedParameters, mockedParameters, Map.of(), "", mockedContext);
+
+        assertEquals(result, expectedOptions);
+    }
+}
