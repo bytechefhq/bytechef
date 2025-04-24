d9fadf4335edd5affc0b05f0f2cd97447a120db6
marijahorvat171 <marijahorvat171@gmail.com>
2025-04-24 11:55:01 +0200

    2319 - add tavily component

---
+++ server/libs/modules/components/tavily/src/main/java/com/bytechef/component/tavily/AbstractTavilyComponentHandler.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2025 ByteChef
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef.component.tavily;
+
+import static com.bytechef.component.definition.ComponentDsl.component;
+import static com.bytechef.component.definition.ComponentDsl.tool;
+
+import com.bytechef.component.OpenApiComponentHandler;
+import com.bytechef.component.definition.ComponentDefinition;
+import com.bytechef.component.tavily.action.TavilyExtractAction;
+import com.bytechef.component.tavily.action.TavilySearchAction;
+import com.bytechef.component.tavily.connection.TavilyConnection;
+
+/**
+ * Provides the base implementation for the REST based component.
+ *
+ * @generated
+ */
+public abstract class AbstractTavilyComponentHandler implements OpenApiComponentHandler {
+    private final ComponentDefinition componentDefinition = modifyComponent(
+        component("tavily")
+            .title("Tavily")
+            .description(
+                "Tavily is an AI-powered search tool designed to help users quickly find accurate, relevant, and up-to-date information from the web."))
+                    .actions(modifyActions(TavilySearchAction.ACTION_DEFINITION, TavilyExtractAction.ACTION_DEFINITION))
+                    .connection(modifyConnection(TavilyConnection.CONNECTION_DEFINITION))
+                    .clusterElements(modifyClusterElements(tool(TavilySearchAction.ACTION_DEFINITION),
+                        tool(TavilyExtractAction.ACTION_DEFINITION)))
+                    .triggers(getTriggers());
+
+    @Override
+    public ComponentDefinition getDefinition() {
+        return componentDefinition;
+    }
+}
---
+++ server/libs/modules/components/tavily/src/main/java/com/bytechef/component/tavily/TavilyComponentHandler.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2025 ByteChef
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef.component.tavily;
+
+import static com.bytechef.component.definition.ComponentDsl.option;
+
+import com.bytechef.component.OpenApiComponentHandler;
+import com.bytechef.component.definition.ActionDefinition;
+import com.bytechef.component.definition.ComponentCategory;
+import com.bytechef.component.definition.ComponentDsl.ModifiableArrayProperty;
+import com.bytechef.component.definition.ComponentDsl.ModifiableComponentDefinition;
+import com.bytechef.component.definition.ComponentDsl.ModifiableProperty;
+import com.bytechef.component.definition.ComponentDsl.ModifiableStringProperty;
+import com.google.auto.service.AutoService;
+import java.util.Objects;
+
+/**
+ * This class will not be overwritten on the subsequent calls of the generator.
+ */
+@AutoService(OpenApiComponentHandler.class)
+public class TavilyComponentHandler extends AbstractTavilyComponentHandler {
+
+    @Override
+    public ModifiableComponentDefinition modifyComponent(ModifiableComponentDefinition modifiableComponentDefinition) {
+        return modifiableComponentDefinition
+            .icon("path:assets/tavily.svg")
+            .categories(ComponentCategory.ARTIFICIAL_INTELLIGENCE);
+    }
+
+    @Override
+    public ModifiableProperty<?> modifyProperty(
+        ActionDefinition actionDefinition, ModifiableProperty<?> modifiableProperty) {
+
+        if (Objects.equals(modifiableProperty.getName(), "topic")) {
+            ((ModifiableStringProperty) modifiableProperty)
+                .options(
+                    option("General", "general", "general-purpose searches that may include a wide range of sources"),
+                    option("News", "news", "real-time updates"));
+        } else if (Objects.equals(modifiableProperty.getName(), "search_depth")) {
+            ((ModifiableStringProperty) modifiableProperty)
+                .options(
+                    option("Basic", "basic", "provides generic content snippets from each source"),
+                    option("Advanced", "advanced", "retrieves the most relevant sources"));
+        } else if (Objects.equals(modifiableProperty.getName(), "time_range")) {
+            ((ModifiableStringProperty) modifiableProperty)
+                .options(
+                    option("Day", "day", "returns results from the past 24 hours"),
+                    option("Week", "week", "returns results from the past 7 days"),
+                    option("Month", "month", "returns results from the past 30 days"),
+                    option("Year", "year", "returns results from the past 365 days"));
+        } else if (Objects.equals(modifiableProperty.getName(), "urls")) {
+            ((ModifiableArrayProperty) modifiableProperty)
+                .label("URLs");
+        } else if (Objects.equals(modifiableProperty.getName(), "extract_depth")) {
+            ((ModifiableStringProperty) modifiableProperty)
+                .options(
+                    option("Basic", "basic", "retrieves basic data"),
+                    option("Advanced", "advanced", "retrieves more data, including tables and embedded content"));
+        }
+
+        return modifiableProperty;
+    }
+}
