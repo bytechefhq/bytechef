d0a4e08c0c68a71fea8228e21f0a367a7b1ad33a
marijahorvat171 <marijahorvat171@gmail.com>
2025-04-29 12:42:31 +0200

    2314 - update zoho crm component

--- server/libs/modules/components/zoho/zoho-crm/src/main/java/com/bytechef/component/zoho/connection/ZohoCrmConnection.java
+++ server/libs/modules/components/zoho/zoho-crm/src/main/java/com/bytechef/component/zoho/connection/ZohoCrmConnection.java
@@ -20,15 +20,13 @@ import static com.bytechef.component.definition.Authorization.ACCESS_TOKEN;
 import static com.bytechef.component.definition.Authorization.AUTHORIZATION;
 import static com.bytechef.component.definition.Authorization.CLIENT_ID;
 import static com.bytechef.component.definition.Authorization.CLIENT_SECRET;
-import static com.bytechef.component.definition.ComponentDsl.authorization;
-import static com.bytechef.component.definition.ComponentDsl.connection;
 import static com.bytechef.component.definition.ComponentDsl.option;
 import static com.bytechef.component.definition.ComponentDsl.string;
 import static com.bytechef.component.zoho.constant.ZohoCrmConstants.REGION;
 
-import com.bytechef.component.definition.Authorization.ApplyResponse;
-import com.bytechef.component.definition.Authorization.AuthorizationType;
+import com.bytechef.component.definition.Authorization;
 import com.bytechef.component.definition.ComponentDsl.ModifiableConnectionDefinition;
+import com.bytechef.component.zoho.commons.ZohoConnection;
 import java.util.List;
 import java.util.Map;
 
@@ -38,39 +36,29 @@ import java.util.Map;
  */
 public class ZohoCrmConnection {
 
-    public static final ModifiableConnectionDefinition CONNECTION_DEFINITION = connection()
-        .baseUri((connectionParameters, context) -> connectionParameters.getString("api_domain") + "/crm/v7")
-        .authorizations(
-            authorization(AuthorizationType.OAUTH2_AUTHORIZATION_CODE)
-                .title("OAuth2 Authorization Code")
-                .properties(
-                    string(REGION)
-                        .label("Region")
-                        .options(
-                            option("zoho.eu (Europe)", "zoho.eu"),
-                            option("zoho.com (United States)", "zoho.com"),
-                            option("zoho.com.au (Australia)", "zoho.com.au"),
-                            option("zoho.jp (Japan)", "zoho.jp"),
-                            option("zoho.in (India)", "zoho.in"),
-                            option("zohocloud.ca (Canada)", "zohocloud.ca"))
-                        .required(true),
-                    string(CLIENT_ID)
-                        .label("Client Id")
-                        .required(true),
-                    string(CLIENT_SECRET)
-                        .label("Client Secret")
-                        .required(true))
-                .authorizationUrl(
-                    (connection, context) -> "https://accounts." + connection.getString(REGION) + "/oauth/v2/auth")
-                .tokenUrl(
-                    (connection, context) -> "https://accounts." + connection.getString(REGION) + "/oauth/v2/token")
-                .refreshUrl(
-                    (connection, context) -> "https://accounts." + connection.getString(REGION) + "/oauth/v2/token")
-                .scopes((connection, context) -> List.of("ZohoCRM.users.ALL", "ZohoCRM.org.READ",
-                    "ZohoCRM.settings.roles.READ", "ZohoCRM.settings.profiles.READ"))
-                .apply((connectionParameters, context) -> ApplyResponse.ofHeaders(
-                    Map.of(AUTHORIZATION,
-                        List.of("Zoho-oauthtoken " + connectionParameters.getRequiredString(ACCESS_TOKEN))))));
+    public static final ModifiableConnectionDefinition CONNECTION_DEFINITION = ZohoConnection.createConnection(
+        (connectionParameters, context) -> connectionParameters.getString("api_domain") + "/crm/v7",
+        (connection, context) -> List.of("ZohoCRM.users.ALL", "ZohoCRM.org.READ",
+            "ZohoCRM.settings.roles.READ", "ZohoCRM.settings.profiles.READ"),
+        (connectionParameters, context) -> Authorization.ApplyResponse.ofHeaders(
+            Map.of(AUTHORIZATION,
+                List.of("Zoho-oauthtoken " + connectionParameters.getRequiredString(ACCESS_TOKEN)))),
+        string(REGION)
+            .label("Region")
+            .options(
+                option("zoho.eu (Europe)", "zoho.eu"),
+                option("zoho.com (United States)", "zoho.com"),
+                option("zoho.com.au (Australia)", "zoho.com.au"),
+                option("zoho.jp (Japan)", "zoho.jp"),
+                option("zoho.in (India)", "zoho.in"),
+                option("zohocloud.ca (Canada)", "zohocloud.ca"))
+            .required(true),
+        string(CLIENT_ID)
+            .label("Client Id")
+            .required(true),
+        string(CLIENT_SECRET)
+            .label("Client Secret")
+            .required(true));
 
     private ZohoCrmConnection() {
     }
