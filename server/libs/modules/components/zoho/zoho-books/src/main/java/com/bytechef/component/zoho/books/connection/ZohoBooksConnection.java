24a59fbf1e7c5fcc9e6e395424c0ae0e4a83ea61
Monika Kušter <domitermonika@gmail.com>
2025-04-30 09:34:19 +0200

    2314 - Refactor Zoho connection implementations

--- server/libs/modules/components/zoho/zoho-books/src/main/java/com/bytechef/component/zoho/books/connection/ZohoBooksConnection.java
+++ server/libs/modules/components/zoho/zoho-books/src/main/java/com/bytechef/component/zoho/books/connection/ZohoBooksConnection.java
@@ -16,20 +16,10 @@
 
 package com.bytechef.component.zoho.books.connection;
 
-import static com.bytechef.component.definition.Authorization.ACCESS_TOKEN;
-import static com.bytechef.component.definition.Authorization.AUTHORIZATION;
-import static com.bytechef.component.definition.Authorization.CLIENT_ID;
-import static com.bytechef.component.definition.Authorization.CLIENT_SECRET;
 import static com.bytechef.component.definition.ComponentDsl.ModifiableConnectionDefinition;
-import static com.bytechef.component.definition.ComponentDsl.option;
-import static com.bytechef.component.definition.ComponentDsl.string;
-import static com.bytechef.component.zoho.books.constant.ZohoBooksConstants.ORGANIZATION_ID;
-import static com.bytechef.component.zoho.books.constant.ZohoBooksConstants.REGION;
 
-import com.bytechef.component.definition.Authorization;
 import com.bytechef.component.zoho.commons.ZohoConnection;
 import java.util.List;
-import java.util.Map;
 
 /**
  * @author Marija Horvat
@@ -37,33 +27,11 @@ import java.util.Map;
 public class ZohoBooksConnection {
 
     public static final ModifiableConnectionDefinition CONNECTION_DEFINITION = ZohoConnection.createConnection(
-        (connectionParameters, context) -> connectionParameters.getString("api_domain") + "/books/v3",
-        (connection, context) -> List.of(
+        "/books/v3",
+        List.of(
             "ZohoBooks.contacts.CREATE", "ZohoBooks.invoices.CREATE", "ZohoBooks.salesorders.CREATE",
             "ZohoBooks.settings.READ", "ZohoBooks.contacts.READ"),
-        (connectionParameters, context) -> Authorization.ApplyResponse.ofHeaders(
-            Map.of(AUTHORIZATION,
-                List.of("Zoho-oauthtoken " + connectionParameters.getRequiredString(ACCESS_TOKEN)),
-                ORGANIZATION_ID, List.of(connectionParameters.getRequiredString(ORGANIZATION_ID)))),
-        string(REGION)
-            .label("Region")
-            .options(
-                option("zoho.eu (Europe)", "zoho.eu"),
-                option("zoho.com (United States)", "zoho.com"),
-                option("zoho.com.au (Australia)", "zoho.com.au"),
-                option("zoho.jp (Japan)", "zoho.jp"),
-                option("zoho.in (India)", "zoho.in"),
-                option("zohocloud.ca (Canada)", "zohocloud.ca"))
-            .required(true),
-        string(ORGANIZATION_ID)
-            .label("Organization Id")
-            .required(true),
-        string(CLIENT_ID)
-            .label("Client Id")
-            .required(true),
-        string(CLIENT_SECRET)
-            .label("Client Secret")
-            .required(true));
+        true);
 
     private ZohoBooksConnection() {
     }
--- server/libs/modules/components/zoho/zoho-crm/src/main/java/com/bytechef/component/zoho/crm/connection/ZohoCrmConnection.java
+++ server/libs/modules/components/zoho/zoho-crm/src/main/java/com/bytechef/component/zoho/crm/connection/ZohoCrmConnection.java
@@ -16,19 +16,9 @@
 
 package com.bytechef.component.zoho.crm.connection;
 
-import static com.bytechef.component.definition.Authorization.ACCESS_TOKEN;
-import static com.bytechef.component.definition.Authorization.AUTHORIZATION;
-import static com.bytechef.component.definition.Authorization.CLIENT_ID;
-import static com.bytechef.component.definition.Authorization.CLIENT_SECRET;
-import static com.bytechef.component.definition.ComponentDsl.option;
-import static com.bytechef.component.definition.ComponentDsl.string;
-import static com.bytechef.component.zoho.crm.constant.ZohoCrmConstants.REGION;
-
-import com.bytechef.component.definition.Authorization;
 import com.bytechef.component.definition.ComponentDsl.ModifiableConnectionDefinition;
 import com.bytechef.component.zoho.commons.ZohoConnection;
 import java.util.List;
-import java.util.Map;
 
 /**
  * @author Luka Ljubić
@@ -37,28 +27,10 @@ import java.util.Map;
 public class ZohoCrmConnection {
 
     public static final ModifiableConnectionDefinition CONNECTION_DEFINITION = ZohoConnection.createConnection(
-        (connectionParameters, context) -> connectionParameters.getString("api_domain") + "/crm/v7",
-        (connection, context) -> List.of("ZohoCRM.users.ALL", "ZohoCRM.org.READ",
-            "ZohoCRM.settings.roles.READ", "ZohoCRM.settings.profiles.READ"),
-        (connectionParameters, context) -> Authorization.ApplyResponse.ofHeaders(
-            Map.of(AUTHORIZATION,
-                List.of("Zoho-oauthtoken " + connectionParameters.getRequiredString(ACCESS_TOKEN)))),
-        string(REGION)
-            .label("Region")
-            .options(
-                option("zoho.eu (Europe)", "zoho.eu"),
-                option("zoho.com (United States)", "zoho.com"),
-                option("zoho.com.au (Australia)", "zoho.com.au"),
-                option("zoho.jp (Japan)", "zoho.jp"),
-                option("zoho.in (India)", "zoho.in"),
-                option("zohocloud.ca (Canada)", "zohocloud.ca"))
-            .required(true),
-        string(CLIENT_ID)
-            .label("Client Id")
-            .required(true),
-        string(CLIENT_SECRET)
-            .label("Client Secret")
-            .required(true));
+        "/crm/v7",
+        List.of("ZohoCRM.users.ALL", "ZohoCRM.org.READ", "ZohoCRM.settings.roles.READ",
+            "ZohoCRM.settings.profiles.READ"),
+        false);
 
     private ZohoCrmConnection() {
     }
