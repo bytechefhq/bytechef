d04c8976fe31b55b93d2bbd1b9a9f89139d557b6
Monika Kušter <domitermonika@gmail.com>
2025-04-30 13:30:31 +0200

    2285 - update tests

--- server/libs/modules/components/bamboohr/src/test/java/com/bytechef/component/bamboohr/trigger/BambooHrNewEmployeeTriggerTest.java
+++ server/libs/modules/components/bamboohr/src/test/java/com/bytechef/component/bamboohr/trigger/BambooHrNewEmployeeTriggerTest.java
@@ -16,13 +16,9 @@
 
 package com.bytechef.component.bamboohr.trigger;
 
-import static com.bytechef.component.bamboohr.constant.BambooHrConstants.EMPLOYEE_NUMBER;
-import static com.bytechef.component.bamboohr.constant.BambooHrConstants.FIELDS;
-import static com.bytechef.component.bamboohr.constant.BambooHrConstants.FIRST_NAME;
+import static com.bytechef.component.bamboohr.constant.BambooHrConstants.ALL_EMPLOYEES;
 import static com.bytechef.component.bamboohr.constant.BambooHrConstants.ID;
-import static com.bytechef.component.bamboohr.constant.BambooHrConstants.LAST_NAME;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -30,7 +26,7 @@ import static org.mockito.Mockito.when;
 import com.bytechef.component.definition.Context.Http;
 import com.bytechef.component.definition.Parameters;
 import com.bytechef.component.definition.TriggerContext;
-import com.bytechef.component.definition.TriggerDefinition;
+import com.bytechef.component.definition.TriggerDefinition.PollOutput;
 import com.bytechef.component.definition.TypeReference;
 import com.bytechef.component.test.definition.MockParametersFactory;
 import java.util.List;
@@ -46,21 +42,13 @@ class BambooHrNewEmployeeTriggerTest {
     private final Http.Executor mockedExecutor = mock(Http.Executor.class);
     private final Http.Response mockedResponse = mock(Http.Response.class);
     private final TriggerContext mockedTriggerContext = mock(TriggerContext.class);
+    private final Parameters mockedParameters =
+        MockParametersFactory.create(Map.of(ALL_EMPLOYEES, List.of("1", "2", "3")));
     private final ArgumentCaptor<String> stringArgumentCaptor = ArgumentCaptor.forClass(String.class);
-    private final Parameters mockedParameters = MockParametersFactory.create(
-        Map.of(FIELDS, List.of("firstName", "lastName", "employeeNumber")));
 
     @Test
     void testPoll() {
-
-        BambooHrNewEmployeeTrigger.EMPLOYEE_LIST.add("1");
-
-        Map<String, String> employee1 =
-            Map.of(ID, "2", FIRST_NAME, "test", LAST_NAME, "test", EMPLOYEE_NUMBER, "2");
-
         when(mockedTriggerContext.http(any()))
-            .thenReturn(mockedExecutor);
-        when(mockedExecutor.queryParameter(stringArgumentCaptor.capture(), stringArgumentCaptor.capture()))
             .thenReturn(mockedExecutor);
         when(mockedExecutor.header(stringArgumentCaptor.capture(), stringArgumentCaptor.capture()))
             .thenReturn(mockedExecutor);
@@ -69,19 +57,12 @@ class BambooHrNewEmployeeTriggerTest {
         when(mockedExecutor.execute())
             .thenReturn(mockedResponse);
         when(mockedResponse.getBody(any(TypeReference.class)))
-            .thenReturn(Map.of("employees", List.of(employee1)));
+            .thenReturn(Map.of("employees", List.of(Map.of(ID, "2"), Map.of(ID, "3"), Map.of(ID, "4"))));
 
-        TriggerDefinition.PollOutput pollOutput =
-            BambooHrNewEmployeeTrigger.poll(mockedParameters, mockedParameters, mockedParameters, mockedTriggerContext);
+        PollOutput pollOutput = BambooHrNewEmployeeTrigger.poll(
+            mockedParameters, mockedParameters, mockedParameters, mockedTriggerContext);
 
-        assertEquals(
-            List.of("accept", "application/json", FIELDS, "firstName,lastName,employeeNumber", "accept",
-                "application/json"),
-            stringArgumentCaptor.getAllValues());
-
-        assertEquals(Map.of("employees", List.of(employee1)), pollOutput.records()
-            .getFirst());
-        assertFalse(pollOutput.pollImmediately());
-
+        assertEquals(new PollOutput(List.of("4"), Map.of(ALL_EMPLOYEES, List.of("2", "3", "4")), false), pollOutput);
+        assertEquals(List.of("accept", "application/json"), stringArgumentCaptor.getAllValues());
     }
 }
