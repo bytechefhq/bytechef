a1d6cf6202ad4921d8e50861f0d0c377e779aab4
marijahorvat171 <marijahorvat171@gmail.com>
2025-05-07 10:49:48 +0200

    2085 - add scrapegraphai component

---
+++ server/libs/modules/components/scrape-graph-ai/src/main/java/com/bytechef/component/scrape/graph/ai/action/ScrapeGraphAiMarkdownifyAction.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2025 ByteChef
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef.component.scrape.graph.ai.action;
+
+import static com.bytechef.component.OpenApiComponentHandler.PropertyType;
+import static com.bytechef.component.definition.ComponentDsl.action;
+import static com.bytechef.component.definition.ComponentDsl.object;
+import static com.bytechef.component.definition.ComponentDsl.outputSchema;
+import static com.bytechef.component.definition.ComponentDsl.string;
+import static com.bytechef.component.definition.Context.Http.BodyContentType;
+import static com.bytechef.component.definition.Context.Http.ResponseType;
+
+import com.bytechef.component.definition.ComponentDsl;
+import java.util.Map;
+
+/**
+ * Provides a list of the component actions.
+ *
+ * @generated
+ */
+public class ScrapeGraphAiMarkdownifyAction {
+    public static final ComponentDsl.ModifiableActionDefinition ACTION_DEFINITION = action("markdownify")
+        .title("Markdownify")
+        .description("Convert any webpage into clean, readable Markdown format.")
+        .metadata(
+            Map.of(
+                "method", "POST",
+                "path", "/markdownify", "bodyContentType", BodyContentType.JSON, "mimeType", "application/json"
+
+            ))
+        .properties(string("website_url").metadata(
+            Map.of(
+                "type", PropertyType.BODY))
+            .label("Website URL")
+            .description("Website URL.")
+            .required(true))
+        .output(outputSchema(object().properties(string("request_id").description("Unique identifier for the request.")
+            .required(false),
+            string("status")
+                .description("Status of the request. One of: “queued”, “processing”, “completed”, “failed”.")
+                .required(false),
+            string("website_url").description("The original website URL that was submitted.")
+                .required(false),
+            string("result").description("The search results.")
+                .required(false),
+            string("error").description("Error message if the request failed. Empty string if successful.")
+                .required(false))
+            .metadata(
+                Map.of(
+                    "responseType", ResponseType.JSON))));
+
+    private ScrapeGraphAiMarkdownifyAction() {
+    }
+}
