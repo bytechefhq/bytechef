8d4b5032282131d802da2ddaa255ee58e073d478
mkriskovic <marko.kriskovic@protonmail.com>
2025-05-11 23:23:42 +0200

    1570 - refactoring

--- server/libs/platform/platform-ai/platform-ai-service/src/main/java/com/bytechef/platform/ai/config/AiCopilotConfiguration.java
+++ server/libs/platform/platform-ai/platform-ai-service/src/main/java/com/bytechef/platform/ai/config/AiCopilotConfiguration.java
@@ -1,5 +1,23 @@
+/*
+ * Copyright 2025 ByteChef
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.bytechef.platform.ai.config;
 
+import java.time.Duration;
+import java.util.function.Consumer;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.ai.openai.OpenAiChatOptions;
@@ -14,9 +32,6 @@ import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.RestClientException;
 
-import java.time.Duration;
-import java.util.function.Consumer;
-
 @Configuration
 @ConditionalOnProperty(prefix = "bytechef.ai.copilot", name = "enabled", havingValue = "true")
 public class AiCopilotConfiguration {
@@ -26,7 +41,8 @@ public class AiCopilotConfiguration {
 
     @Bean
     OpenAiApi openAiApi() {
-        HttpComponentsClientHttpRequestFactory httpComponentsClientHttpRequestFactory = new HttpComponentsClientHttpRequestFactory();
+        HttpComponentsClientHttpRequestFactory httpComponentsClientHttpRequestFactory =
+            new HttpComponentsClientHttpRequestFactory();
         httpComponentsClientHttpRequestFactory.setConnectionRequestTimeout(Duration.ofSeconds(60));
         httpComponentsClientHttpRequestFactory.setConnectTimeout(Duration.ofSeconds(60));
         httpComponentsClientHttpRequestFactory.setReadTimeout(Duration.ofSeconds(60));
@@ -34,7 +50,8 @@ public class AiCopilotConfiguration {
         RestClient.Builder builder = RestClient.builder()
             .requestFactory(httpComponentsClientHttpRequestFactory)
             .defaultStatusHandler(HttpStatusCode::isError, (request, response) -> {
-                throw new RestClientException("Error response: " + response.getStatusCode() + "; " + response.getStatusText());
+                throw new RestClientException(
+                    "Error response: " + response.getStatusCode() + "; " + response.getStatusText());
             })
             .defaultHeaders(new Consumer<HttpHeaders>() {
                 @Override
