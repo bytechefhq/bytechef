ba9206e7e8689dd1f099b625bb1d311519e1954e
igorbeslic <igor.beslic@bitnaprednost.hr>
2025-05-15 16:03:52 +0200

    2572 platform - component - email - dependencies

--- server/libs/modules/components/email/build.gradle.kts
+++ server/libs/modules/components/email/build.gradle.kts
@@ -1,5 +1,6 @@
 dependencies {
     implementation("org.eclipse.angus:angus-mail")
+    api(project(":server:libs:core:commons:commons-util"))
 
     testImplementation("com.icegreen:greenmail:2.1.0")
     testImplementation("com.icegreen:greenmail-junit5:2.1.0")
@@ -7,5 +8,6 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api")
     testImplementation("org.junit.jupiter:junit-jupiter-engine")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation(project(":server:libs:config:jackson-config"))
 
 }
