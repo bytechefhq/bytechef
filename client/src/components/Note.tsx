4d0a9fa935afedc093caf9853d06c7d7c15166af
Krešimir Čoko <kresimircoko@gmail.com>
2025-05-15 16:14:48 +0200

    2536 - improve Note component

--- client/src/components/Note.tsx
+++ client/src/components/Note.tsx
@@ -4,21 +4,14 @@ import {twMerge} from 'tailwind-merge';
 
 import {Button} from './ui/button';
 
-export const Note = ({content, icon}: {content: string; icon?: ReactNode}) => {
-    return (
-        <div
-            className={twMerge(
-                'relative flex items-center rounded-md bg-surface-warning-secondary p-4',
-                icon && 'gap-2'
-            )}
-        >
-            {icon && icon}
+export const Note = ({content, icon}: {content: string; icon?: ReactNode}) => (
+    <div className={twMerge('bg-surface-warning-secondary relative flex items-center rounded-md p-4', icon && 'gap-2')}>
+        {icon && icon}
 
-            <p className="text-sm font-medium">{content}</p>
+        <p className="text-sm font-medium">{content}</p>
 
-            <Button className="absolute right-1 top-1 hover:bg-surface-warning-primary" size="icon" variant="ghost">
-                <XIcon className="size-4 text-content-neutral-secondary" />
-            </Button>
-        </div>
-    );
-};
+        <Button className="group absolute right-0 top-0 hover:bg-transparent" size="icon" variant="ghost">
+            <XIcon className="size-4 text-content-neutral-secondary group-hover:text-content-destructive" />
+        </Button>
+    </div>
+);
