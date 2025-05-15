eb79f6d24cb30daf81f72f8e80d80ddcfe4a87dd
igorbeslic <igor.beslic@bitnaprednost.hr>
2025-05-15 12:44:17 +0200

    2572 platform - component - email - refactor - move common code to utils class

---
+++ server/libs/modules/components/email/src/main/java/com/bytechef/component/email/commons/EmailUtils.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2025 ByteChef
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef.component.email.commons;
+
+import static com.bytechef.component.definition.Authorization.USERNAME;
+import static com.bytechef.component.email.constant.EmailConstants.*;
+import static com.bytechef.component.email.constant.EmailConstants.HOST;
+
+import com.bytechef.component.definition.Parameters;
+import com.bytechef.component.email.EmailProtocol;
+import java.util.Properties;
+
+/**
+ * @author Igor Beslic
+ */
+public class EmailUtils {
+
+    public static Properties
+        getMailSessionProperties(int port, EmailProtocol protocol, Parameters connectionParameters) {
+        Properties props = new Properties();
+
+        props.setProperty("mail.store.protocol", protocol.name());
+        props.setProperty("mail.debug", "true");
+
+        if (connectionParameters.containsKey(CRYPTOGRAPHIC_PROTOCOL)) {
+            if (TLS.contentEquals(connectionParameters.getRequiredString(CRYPTOGRAPHIC_PROTOCOL))) {
+                props.put(String.format("mail.%s.starttls.enable", protocol), "true");
+            } else {
+                props.put(String.format("mail.%s.ssl.enable", protocol), "true");
+                props.put(String.format("mail.%s.ssl.trust", protocol), connectionParameters.getRequiredString(HOST));
+            }
+        }
+
+        if (connectionParameters.containsKey(USERNAME)) {
+            props.put(String.format("mail.%s.user", protocol), connectionParameters.getRequiredString(USERNAME));
+            props.put(String.format("mail.%s.auth", protocol), true);
+        }
+
+        props.put(String.format("mail.%s.host", protocol), connectionParameters.getRequiredString(HOST));
+        props.put(String.format("mail.%s.port", protocol), port);
+
+        return props;
+    }
+
+}
