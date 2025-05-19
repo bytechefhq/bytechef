b0efc812fa82e30ba58486ec98299be058b0a16e
Monika Kušter <domitermonika@gmail.com>
2025-05-19 12:35:05 +0200

    2597 - SF

--- server/libs/platform/platform-component/platform-component-service/src/main/java/com/bytechef/platform/component/jackson/WebhookRequestDeserializer.java
+++ server/libs/platform/platform-component/platform-component-service/src/main/java/com/bytechef/platform/component/jackson/WebhookRequestDeserializer.java
@@ -28,12 +28,11 @@ import com.fasterxml.jackson.core.ObjectCodec;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.JsonNodeType;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
-
-import com.fasterxml.jackson.databind.node.JsonNodeType;
 import org.springframework.boot.jackson.JsonComponent;
 
 /**
