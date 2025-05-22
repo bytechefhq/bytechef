3ec473a49a39a4677ca025cdf7dc85830af0dd92
katarina-cala <katarinacala@gmail.com>
2025-05-22 13:38:36 +0200

    1652-updated getFormattedName to check all nodes, including clusterElement nodes

--- client/src/pages/platform/workflow-editor/utils/getFormattedName.ts
+++ client/src/pages/platform/workflow-editor/utils/getFormattedName.ts
@@ -1,24 +1,63 @@
+import {WorkflowTask} from '@/shared/middleware/platform/configuration';
 import {NodeDataType} from '@/shared/types';
 
 import useWorkflowDataStore from '../stores/useWorkflowDataStore';
 
 export default function getFormattedName(itemName: string): string {
-    const {nodes} = useWorkflowDataStore.getState();
+    const {nodes, workflow} = useWorkflowDataStore.getState();
+
     const nodeNames = nodes.map((node) => (node.data as NodeDataType).name);
 
     const existingNodes = nodeNames.filter((name) => name?.includes(itemName));
 
-    if (!existingNodes.length) {
+    const workflowDefinition = JSON.parse(workflow.definition!);
+
+    const clusterElementNames = workflowDefinition.tasks.map((task: WorkflowTask) => {
+        const elementNames: string[] = [];
+
+        const {clusterElements} = task;
+
+        if (clusterElements) {
+            Object.keys(clusterElements).forEach((elementType) => {
+                const elements = clusterElements[elementType];
+
+                if (Array.isArray(elements)) {
+                    elements.forEach((element) => {
+                        if (element.name?.includes(itemName)) {
+                            elementNames.push(element.name);
+                        }
+                    });
+                } else if (elements?.name?.includes(itemName)) {
+                    elementNames.push(elements.name);
+                }
+            });
+        }
+
+        return elementNames;
+    });
+
+    const existingClusterElementNodes = clusterElementNames.flatMap((names: string[] | string) => {
+        if (Array.isArray(names)) {
+            return names.filter((name: string) => name?.includes(itemName));
+        }
+
+        return names?.includes(itemName) ? [names] : [];
+    });
+
+    const allExistingNodes = [...existingNodes, ...existingClusterElementNodes];
+
+    if (!allExistingNodes.length) {
         return `${itemName}_1`;
     }
 
-    const existingNodeNumbers = existingNodes.map((name) => {
+    const allNumbers = allExistingNodes.map((name: string) => {
         const nodeNameSplit = name.split('_');
+        const lastSegmentIndex = nodeNameSplit.length - 1;
 
-        return parseInt(nodeNameSplit[nodeNameSplit.length - 1]);
+        return parseInt(nodeNameSplit[lastSegmentIndex]);
     });
 
-    const maxExistingNodeNumber = Math.max(...existingNodeNumbers);
+    const maxExistingNumber = Math.max(...allNumbers);
 
-    return `${itemName}_${maxExistingNodeNumber + 1}`;
+    return `${itemName}_${maxExistingNumber + 1}`;
 }
