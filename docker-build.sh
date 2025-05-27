cabb933c92ef523e5b706e9a7055044350aff852
igorbeslic <igor.beslic@bitnaprednost.hr>
2025-05-27 23:16:04 +0200

    2664 docker - build - make possible to set arbitrary registry url and as many image tags, update script to push images

--- docker-build.sh
+++ docker-build.sh
@@ -1,27 +1,42 @@
 #!/bin/sh
 
-if [ -z $1 ]; then
-    echo "Required argument misses. Please provide docker image tag."
+if [ -z "$1" ]; then
+    echo "Required argument misses. Please provide docker image tag or AWS login flag."
     echo ""
     echo "USAGE"
-    echo "    docker-build.sh tag [latest]"
+    echo "    docker-build.sh [--aws url] tag1 [tag2 tag3 ...]"
     echo "DESCRIPTION"
-    echo "    tag\t\t- arbitrary docker image tag. In bytechef we use yyyyMMdd to reflect date of image build"
-    echo "    latest\t- optional flag that would instruct script to tag image with additional tag with value \`latest\`"
+    echo "    --aws url\t- optional flag to login to AWS ECR with the provided URL."
+    echo "    tag\t\t- arbitrary docker image tag(s). In bytechef we use yyyyMMdd to reflect date of image build."
 
     exit 1
+fi
+
+dckr_img_registry_bytechef_server="bytechef/bytechef-server"
+dckr_img_registry_bytechef="bytechef/bytechef"
+
+if [ "$1" = "--registry-url" ]; then
+    if [ -z "$2" ]; then
+        echo "AWS URL is required when using the --registry-url flag."
+
+        exit 1
+    fi
+
+    echo "Logging in to AWS ECR with URL: $2"
+    aws ecr get-login-password --region eu-west-1 | docker login --username AWS --password-stdin "$2"
+
+    dckr_img_registry_bytechef="$2/bc-prod-app"
+
+    shift 2
 fi
 
 cd server/apps/server-app
 ../../../gradlew clean build -Pprod
 
-if [ -n "$2" ] && [ "latest" = "$2" ]; then
-    echo "Building docker image with tag \`$2\`"
-
-    docker build --platform linux/amd64 -t bytechef/bytechef-server:$2 .
-fi
-
-docker build --platform linux/amd64 -t bytechef/bytechef-server:$1 .
+for tag in "$@"; do
+    echo "Building docker image with tag \`$tag\`"
+    docker build --platform linux/amd64 -t $dckr_img_registry_bytechef_server:$tag .
+done
 
 cd ../../../client
 
@@ -31,10 +46,14 @@ npm run build
 
 cd ..
 
-if [ -n "$2" ] && [ "latest" = "$2" ]; then
-    echo "Building docker image with tag \`$2\`"
+for tag in "$@"; do
+    echo "Building docker image with tag \`$tag\`"
+    docker build --platform linux/amd64 -t $dckr_img_registry_bytechef:$tag .
+done
 
-    docker build --platform linux/amd64 -t bytechef/bytechef:$2 .
-fi
+echo "Push images to the remote docker registry \`$dckr_img_registry_bytechef\`"
 
-docker build --platform linux/amd64 -t bytechef/bytechef:$1 .
+for tag in "$@"; do
+    docker push $dckr_img_registry_bytechef_server:$tag .
+    docker push $dckr_img_registry_bytechef:$tag .
+done
