77e12a9e4ff9828ccca9398a321c4dc79e410ea3
Krešimir Čoko <kresimircoko@gmail.com>
2025-05-30 16:58:19 +0200

    2680 - SF

--- sdks/frontend/embedded/connection-dialog/src/index.tsx
+++ sdks/frontend/embedded/connection-dialog/src/index.tsx
@@ -5,7 +5,6 @@ import './styles.css';
 const JWT =
     'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImNIVmliR2xqT20xaGQwOUdOSFYyUVVsd1RXdHFlakZ1T1U5YVZGZ3dLMHhJV1hoUWJ6WlcifQ.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.NDL-cUc9i7DCwVmtRVEos4pUmGPZjx0BSEpMQkqhEXTg0bA-I0nPBoZfej3z6tSdFK5qPFVu3lW42crkKqo-lxo07U4AsbDxRuPWR4ELQdHAA0e3GDRFI-fnSJ4FkNR39-IGWyHiSxuOkyYVS8TyQ2BrSB5D48yF_ooTY-Qvs-r9zJ-mI7ksqtF3V_-KeqHNvQRJY8A4WhmrmS1XTGeTbdLCWdNr7TeMa-AFNszEHyIk4_th-H2iHosyFLqAiD7L1RdpkzIXDlsMlLkI2dTjezJaTwwpCJDcInlZSB-wlK0X9Sat1c1JY9eECLpIK1vL6N3MZMhia4yNj0kI_9vhKg';
 
-// Define the return type explicitly to help TypeScript understand the structure
 interface ConnectionDialogHook {
     openDialog: () => void;
     closeDialog: () => void;
@@ -23,8 +22,6 @@ export default function useEmbeddedByteChefConnectionDialog({
 
     async function getData() {
         const url = `${baseUrl}/api/embedded/v1/integrations/${integrationId}`;
-
-        console.log('url', url);
 
         try {
             const response = await fetch(url, {
@@ -48,9 +45,7 @@ export default function useEmbeddedByteChefConnectionDialog({
         }
     }
 
-
     const openDialog = async () => {
-        console.log('openDialog called');
         getData();
 
         setIsOpen(true);
