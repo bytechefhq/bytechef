e532adcccee23b286b77d1e63bc6e2303a52f52d
Ivica Cardic <ivica.cardic@gmail.com>
2025-05-31 12:23:12 +0200

    520 -

--- server/ee/libs/embedded/embedded-security-web/embedded-security-web-impl/src/main/java/com/bytechef/ee/embedded/security/web/filter/ConnectedUserAuthenticationFilter.java
+++ server/ee/libs/embedded/embedded-security-web/embedded-security-web-impl/src/main/java/com/bytechef/ee/embedded/security/web/filter/ConnectedUserAuthenticationFilter.java
@@ -9,9 +9,18 @@ package com.bytechef.ee.embedded.security.web.filter;
 
 import com.bytechef.ee.embedded.security.web.authentication.ConnectedUserAuthenticationToken;
 import com.bytechef.platform.security.web.filter.AbstractPublicApiAuthenticationFilter;
+import com.bytechef.platform.user.service.SigningKeyService;
 import com.bytechef.tenant.domain.TenantKey;
+import com.bytechef.tenant.util.TenantUtils;
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Header;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.JwsHeader;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.Locator;
 import jakarta.servlet.http.HttpServletRequest;
+import java.security.Key;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -25,29 +34,74 @@ import org.springframework.security.core.Authentication;
 public class ConnectedUserAuthenticationFilter extends AbstractPublicApiAuthenticationFilter {
 
     private static final Pattern EXTERNAL_USER_ID_PATTERN = Pattern.compile(".*/v\\d+/([^/]+)/.*");
+    private static final Pattern JWT_TOKEN_PATTERN =
+        Pattern.compile("^[A-Za-z0-9-_=]+\\.[A-Za-z0-9-_=]+\\.[A-Za-z0-9-_.+/=]*$");
+
+    private final SigningKeyService signingKeyService;
 
     @SuppressFBWarnings("EI")
-    public ConnectedUserAuthenticationFilter(AuthenticationManager authenticationManager) {
-        super("^/api/embedded/v[0-9]+/(?!frontend/).+", authenticationManager);
+    public ConnectedUserAuthenticationFilter(
+        AuthenticationManager authenticationManager, SigningKeyService signingKeyService) {
 
+        super("^/api/embedded/v[0-9]+/.+", authenticationManager);
+
+        this.signingKeyService = signingKeyService;
     }
 
     @Override
     protected Authentication getAuthentication(HttpServletRequest request) {
         String token = getAuthToken(request);
 
-        TenantKey tenantKey = TenantKey.parse(token);
+        Matcher jwtTokenMatcher = JWT_TOKEN_PATTERN.matcher(token);
 
-        Matcher matcher = EXTERNAL_USER_ID_PATTERN.matcher(request.getRequestURI());
+        if (jwtTokenMatcher.find()) {
+            Jws<Claims> jws = getJws(token);
 
-        String externalUserId;
+            Claims payload = jws.getPayload();
 
-        if (matcher.matches()) {
-            externalUserId = matcher.group(1);
+            String externalUserId = payload.getSubject();
+
+            JwsHeader header = jws.getHeader();
+
+            TenantKey tenantKey = TenantKey.parse(header.getKeyId());
+
+            return new ConnectedUserAuthenticationToken(
+                externalUserId, getEnvironment(request), tenantKey.getTenantId());
         } else {
-            throw new IllegalArgumentException("externalUserId parameter is required");
-        }
+            TenantKey tenantKey = TenantKey.parse(token);
 
-        return new ConnectedUserAuthenticationToken(externalUserId, getEnvironment(request), tenantKey.getTenantId());
+            Matcher matcher = EXTERNAL_USER_ID_PATTERN.matcher(request.getRequestURI());
+
+            String externalUserId;
+
+            if (matcher.matches()) {
+                externalUserId = matcher.group(1);
+            } else {
+                throw new IllegalArgumentException("externalUserId parameter is required");
+            }
+
+            return new ConnectedUserAuthenticationToken(externalUserId, getEnvironment(request),
+                tenantKey.getTenantId());
+        }
+    }
+
+    private Jws<Claims> getJws(String secretKey) {
+        return Jwts.parser()
+            .keyLocator(new SigningKeyLocator())
+            .build()
+            .parseSignedClaims(secretKey);
+    }
+
+    private class SigningKeyLocator implements Locator<Key> {
+
+        @Override
+        public Key locate(Header header) {
+            String keyId = (String) header.get("kid");
+
+            TenantKey tenantKey = TenantKey.parse(keyId);
+
+            return TenantUtils.callWithTenantId(
+                tenantKey.getTenantId(), () -> signingKeyService.getPublicKey(keyId));
+        }
     }
 }
