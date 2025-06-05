48bbc6670f80b9714cf7a9be76d3ded8bad7b164
Ivica Cardic <ivica.cardic@gmail.com>
2025-06-05 22:22:59 +0200

    1779 - client - Change “Run” to “Test” label for run button in the workflow editor

--- client/src/ee/pages/embedded/automations/components/workflow-builder-header/components/WorkflowActionsButton.tsx
+++ client/src/ee/pages/embedded/automations/components/workflow-builder-header/components/WorkflowActionsButton.tsx
@@ -39,7 +39,7 @@ const WorkflowActionsButton = ({
                             </>
                         ) : (
                             <>
-                                <PlayIcon /> Run
+                                <PlayIcon /> Test
                             </>
                         )}
                     </Button>
