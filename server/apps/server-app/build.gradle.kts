07212012017ec4c5ee0b3ab44b7c7a880ea3c15f
Nikolina Spehar <speharnikolina87@gmail.com>
2025-06-09 08:44:22 +0200

    2086 - Contiguity component added

--- server/apps/server-app/build.gradle.kts
+++ server/apps/server-app/build.gradle.kts
@@ -171,6 +171,7 @@ dependencies {
     implementation(project(":server:libs:modules:components:capsule-crm"))
     implementation(project(":server:libs:modules:components:chat"))
     implementation(project(":server:libs:modules:components:clickup"))
+    implementation(project(":server:libs:modules:components:contiguity"))
     implementation(project(":server:libs:modules:components:copper"))
     implementation(project(":server:libs:modules:components:crypto-helper"))
     implementation(project(":server:libs:modules:components:csv-file"))
