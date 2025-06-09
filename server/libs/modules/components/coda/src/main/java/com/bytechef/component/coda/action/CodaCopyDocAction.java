21d2b6202e697030f768c96682ca9e0676bfd545
marijahorvat171 <marijahorvat171@gmail.com>
2025-06-09 15:41:23 +0200

    2385 - add coda component

---
+++ server/libs/modules/components/coda/src/main/java/com/bytechef/component/coda/action/CodaCopyDocAction.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2025 ByteChef
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef.component.coda.action;
+
+import static com.bytechef.component.OpenApiComponentHandler.PropertyType;
+import static com.bytechef.component.definition.ComponentDsl.action;
+import static com.bytechef.component.definition.ComponentDsl.object;
+import static com.bytechef.component.definition.ComponentDsl.outputSchema;
+import static com.bytechef.component.definition.ComponentDsl.string;
+import static com.bytechef.component.definition.Context.Http.BodyContentType;
+import static com.bytechef.component.definition.Context.Http.ResponseType;
+
+import com.bytechef.component.coda.property.CodaDocumentCreationResultProperties;
+import com.bytechef.component.coda.util.CodaUtils;
+import com.bytechef.component.definition.ComponentDsl;
+import com.bytechef.component.definition.OptionsDataSource;
+import java.util.Map;
+
+/**
+ * Provides a list of the component actions.
+ *
+ * @generated
+ */
+public class CodaCopyDocAction {
+    public static final ComponentDsl.ModifiableActionDefinition ACTION_DEFINITION = action("copyDoc")
+        .title("Copy doc")
+        .description("Copies an existing doc.")
+        .metadata(
+            Map.of(
+                "method", "POST",
+                "path", "/docs", "bodyContentType", BodyContentType.JSON, "mimeType", "application/json"
+
+            ))
+        .properties(string("title").metadata(
+            Map.of(
+                "type", PropertyType.BODY))
+            .label("Title")
+            .description("Title of the new doc.")
+            .required(true),
+            string("sourceDoc").metadata(
+                Map.of(
+                    "type", PropertyType.BODY))
+                .label("Source Doc")
+                .description("A doc ID from which to create a copy.")
+                .required(true)
+                .options((OptionsDataSource.ActionOptionsFunction<String>) CodaUtils::getSourceDocOptions))
+        .output(outputSchema(object().properties(CodaDocumentCreationResultProperties.PROPERTIES)
+            .description("The result of a doc creation.")
+            .metadata(
+                Map.of(
+                    "responseType", ResponseType.JSON))));
+
+    private CodaCopyDocAction() {
+    }
+}
---
+++ server/libs/modules/components/coda/src/main/java/com/bytechef/component/coda/util/CodaUtils.java
@@ -0,0 +1,112 @@
+/*
+ * Copyright 2025 ByteChef
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef.component.coda.util;
+
+import static com.bytechef.component.definition.ComponentDsl.option;
+import static com.bytechef.component.definition.Context.Http.responseType;
+
+import com.bytechef.component.definition.Context;
+import com.bytechef.component.definition.Context.Http.ResponseType;
+import com.bytechef.component.definition.Option;
+import com.bytechef.component.definition.Parameters;
+import com.bytechef.component.definition.TypeReference;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * This class will not be overwritten on the subsequent calls of the generator.
+ */
+public class CodaUtils extends AbstractCodaUtils {
+    private CodaUtils() {
+    }
+
+    public static List<Option<String>> getTableIdOptions(
+        Parameters inputParameters, Parameters connectionParameters, Map<String, String> lookupDependsOnPaths,
+        String searchText, Context context) {
+
+        Map<String, Object> body = context
+            .http(http -> http.get("/docs/" + inputParameters.getRequiredString("docId") + "/tables"))
+            .configuration(responseType(ResponseType.JSON))
+            .execute()
+            .getBody(new TypeReference<>() {});
+
+        return getOptions(body);
+    }
+
+    public static List<Option<String>> getDocIdOptions(
+        Parameters inputParameters, Parameters connectionParameters, Map<String, String> lookupDependsOnPaths,
+        String searchText, Context context) {
+
+        Map<String, Object> body = context
+            .http(http -> http.get("/docs"))
+            .configuration(responseType(ResponseType.JSON))
+            .execute()
+            .getBody(new TypeReference<>() {});
+
+        return getOptions(body);
+    }
+
+    public static List<Option<String>> getRowIdOptions(
+        Parameters inputParameters, Parameters connectionParameters, Map<String, String> lookupDependsOnPaths,
+        String searchText, Context context) {
+
+        Map<String, Object> body = context
+            .http(http -> http.get("/docs/" + inputParameters.getRequiredString("docId") + "/tables/"
+                + inputParameters.getRequiredString("tableId") + "/rows"))
+            .configuration(responseType(ResponseType.JSON))
+            .execute()
+            .getBody(new TypeReference<>() {});
+
+        return getOptions(body);
+    }
+
+    public static List<Option<String>> getColumnOptions(
+        Parameters inputParameters, Parameters connectionParameters, Map<String, String> lookupDependsOnPaths,
+        String searchText, Context context) {
+
+        Map<String, Object> body = context
+            .http(http -> http.get("/docs/" + inputParameters.getRequiredString("docId") + "/tables/"
+                + inputParameters.getRequiredString("tableId") + "/columns"))
+            .configuration(responseType(ResponseType.JSON))
+            .execute()
+            .getBody(new TypeReference<>() {});
+
+        return getOptions(body);
+    }
+
+    public static List<Option<String>> getSourceDocOptions(
+        Parameters inputParameters, Parameters connectionParameters, Map<String, String> lookupDependsOnPaths,
+        String searchText, Context context) {
+
+        return getDocIdOptions(inputParameters, connectionParameters, lookupDependsOnPaths, searchText, context);
+    }
+
+    private static List<Option<String>> getOptions(Map<String, Object> body) {
+
+        List<Option<String>> options = new ArrayList<>();
+
+        if (body.get("items") instanceof List<?> items) {
+            for (Object item : items) {
+                if (item instanceof Map<?, ?> map) {
+                    options.add(option((String) map.get("name"), (String) map.get("id")));
+                }
+            }
+        }
+        return options;
+    }
+}
