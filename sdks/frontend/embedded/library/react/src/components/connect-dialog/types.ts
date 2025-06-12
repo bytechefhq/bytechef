59e0869e230e7550e34716075baa5356db062d15
Krešimir Čoko <kresimircoko@gmail.com>
2025-06-12 11:25:50 +0200

    2680 - sdk - fix types

---
+++ sdks/frontend/embedded/library/react/src/components/connect-dialog/types.ts
@@ -0,0 +1,52 @@
+export interface IntegrationType {
+    description?: string;
+    connectionConfig?: {
+        authorizationType: string;
+        inputs?: PropertyType[];
+        oauth2?: {
+            authorizationUrl: string;
+            scopes: string[];
+            redirectUri: string;
+            clientId: string;
+        };
+    };
+    icon?: string;
+    id?: number;
+    workflows?: WorkflowType[];
+}
+
+export interface WorkflowType {
+    inputs?: WorkflowInputType[];
+    label?: string;
+    workflowReferenceCode?: string;
+}
+
+export interface PropertyType {
+    name: string;
+    label: string;
+    type: 'string' | 'number' | 'boolean' | 'object' | 'array';
+    required?: boolean;
+    options?: string[];
+    placeholder?: string;
+}
+
+export interface WorkflowInputType {
+    name: string;
+    label: string;
+    type: 'string' | 'number' | 'boolean' | 'object' | 'array';
+    required?: boolean;
+    defaultValue?: unknown;
+}
+
+export interface FormType {
+    register: (name: string) => {
+        name: string;
+        defaultValue: string;
+        ref: (element: HTMLInputElement) => void;
+        onInput: (event: React.FormEvent<HTMLInputElement>) => void;
+    };
+    handleSubmit: (callback: (data: {[key: string]: unknown}) => void) => (event?: React.FormEvent) => boolean;
+    formState: {
+        errors: Record<string, {message: string}>;
+    };
+}
