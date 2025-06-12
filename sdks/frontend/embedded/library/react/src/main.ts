861e2ae18c00e45eec028cd2dbf1af74a2e03477
Krešimir Čoko <kresimircoko@gmail.com>
2025-06-12 07:39:42 +0200

    2680 - sdk - SF

--- sdks/frontend/embedded/library/react/src/main.ts
+++ sdks/frontend/embedded/library/react/src/main.ts
@@ -1,5 +1,5 @@
 import useConnectDialog from './components/connect-dialog';
 import EmbeddedWorkflowBuilder from './components/embedded-workflow-builder';
 
-export { EmbeddedWorkflowBuilder };
-export { useConnectDialog };
+export {EmbeddedWorkflowBuilder};
+export {useConnectDialog};
