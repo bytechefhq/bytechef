f489465081025487eb583fba7bf6c9ffa2e9de45
marijahorvat171 <marijahorvat171@gmail.com>
2025-06-12 12:47:52 +0200

    2530 - add reddit component

---
+++ server/libs/modules/components/reddit/src/main/java/com/bytechef/component/reddit/constant/RedditConstants.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2025 ByteChef
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef.component.reddit.constant;
+
+/**
+ * @author Marija Horvat
+ */
+public class RedditConstants {
+
+    public static final String KIND = "kind";
+    public static final String SUBREDDIT_NAME = "sr";
+    public static final String TEXT = "text";
+    public static final String THING_ID = "thing_id";
+    public static final String TITLE = "title";
+    public static final String URL = "url";
+
+    private RedditConstants() {
+    }
+}
---
+++ server/libs/modules/components/reddit/src/test/java/com/bytechef/component/reddit/action/RedditCreatePostActionTest.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2025 ByteChef
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef.component.reddit.action;
+
+import static com.bytechef.component.reddit.constant.RedditConstants.KIND;
+import static com.bytechef.component.reddit.constant.RedditConstants.SUBREDDIT_NAME;
+import static com.bytechef.component.reddit.constant.RedditConstants.TEXT;
+import static com.bytechef.component.reddit.constant.RedditConstants.TITLE;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import com.bytechef.component.definition.Context;
+import com.bytechef.component.definition.Context.Http.Body;
+import com.bytechef.component.definition.Context.Http.Executor;
+import com.bytechef.component.definition.Parameters;
+import com.bytechef.component.definition.TypeReference;
+import com.bytechef.component.test.definition.MockParametersFactory;
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+
+/**
+ * @author Marija Horvat
+ */
+class RedditCreatePostActionTest {
+
+    private final ArgumentCaptor<Body> bodyArgumentCaptor = ArgumentCaptor.forClass(Body.class);
+    private final Context mockedContext = mock(Context.class);
+    private final Executor mockedExecutor = mock(Executor.class);
+    private final Parameters mockedParameters = MockParametersFactory.create(Map.of(
+        SUBREDDIT_NAME, "test", TITLE, "Title", KIND, "self", TEXT, "This is a test."));
+    private final Context.Http.Response mockedResponse = mock(Context.Http.Response.class);
+    private final Map<String, Object> responseMap = Map.of("success", true, "id", "abc123");
+
+    @Test
+    void testPerform() {
+        when(mockedContext.http(any()))
+            .thenReturn(mockedExecutor);
+        when(mockedExecutor.configuration(any()))
+            .thenReturn(mockedExecutor);
+        when(mockedExecutor.body(bodyArgumentCaptor.capture()))
+            .thenReturn(mockedExecutor);
+        when(mockedExecutor.execute())
+            .thenReturn(mockedResponse);
+        when(mockedResponse.getBody(any(TypeReference.class)))
+            .thenReturn(responseMap);
+
+        Map<String, Object> expectedResponse = Map.of("success", true, "id", "abc123");
+        Object result = RedditCreatePostAction.perform(mockedParameters, mockedParameters, mockedContext);
+        assertEquals(expectedResponse, result);
+
+        Map<String, Object> expectedBodyMap = Map.of(
+            SUBREDDIT_NAME, "test", TITLE, "Title", KIND, "self", TEXT, "This is a test.");
+        Body body = bodyArgumentCaptor.getValue();
+        assertEquals(expectedBodyMap, body.getContent());
+    }
+}
