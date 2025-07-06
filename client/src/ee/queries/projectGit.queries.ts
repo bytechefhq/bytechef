b8b8bd036634d78ee12db4ff892567b225663678
Ivica Cardic <ivica.cardic@gmail.com>
2025-07-06 11:43:10 +0200

    1039 - client - Add support for selecting remote Git branches in ProjectGitConfigurationDialog

--- client/src/ee/queries/projectGit.queries.ts
+++ client/src/ee/queries/projectGit.queries.ts
@@ -6,12 +6,21 @@ import {useQuery} from '@tanstack/react-query';
 export const ProjectGitConfigurationKeys = {
     projectGitConfiguration: (id: number) => [...ProjectGitConfigurationKeys.projectGitConfigurations, id],
     projectGitConfigurations: ['projectGitConfigurations'] as const,
+    projectRemoteBranches: (id: number) => [...ProjectGitConfigurationKeys.projectGitConfigurations, 'remoteBranches', id],
 };
 
 export const useGetProjectGitConfigurationQuery = (id: number) =>
     useQuery<ProjectGitConfiguration, Error>({
         queryKey: ProjectGitConfigurationKeys.projectGitConfiguration(id),
         queryFn: () => new ProjectGitApi().getProjectGitConfiguration({id}),
+        retry: false,
+    });
+
+export const useGetProjectRemoteBranchesQuery = (id: number, enabled: boolean = true) =>
+    useQuery<string[], Error>({
+        queryKey: ProjectGitConfigurationKeys.projectRemoteBranches(id),
+        queryFn: () => new ProjectGitApi().getProjectRemoteBranches({id}),
+        enabled,
         retry: false,
     });
 
