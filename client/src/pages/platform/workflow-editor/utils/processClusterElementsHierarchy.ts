f3b0e3b76c831885285df948b86a669e9225b614
katarina-cala <katarinacala@gmail.com>
2025-07-16 10:37:25 +0200

    1652-updated processing and adding new cluster elements

--- client/src/pages/platform/workflow-editor/utils/processClusterElementsHierarchy.ts
+++ client/src/pages/platform/workflow-editor/utils/processClusterElementsHierarchy.ts
@@ -1,5 +1,6 @@
 import {ClusterElementItemType, ClusterElementsType} from '@/shared/types';
 
+import {addElementToClusterRoot, isPlainObject} from '../../cluster-element-editor/utils/clusterElementsUtils';
 import updateClusterElementsPositions from './updateClusterElementsPositions';
 
 interface ProcessClusterElementsHierarchyProps {
@@ -21,136 +22,137 @@ export default function processClusterElementsHierarchy({
     nodePositions,
     sourceNodeId,
 }: ProcessClusterElementsHierarchyProps): {parentFound: boolean; nestedClusterElements: ClusterElementsType} {
-    let updatedClusterElements = {...clusterElements};
     let parentFound = false;
 
     if (!sourceNodeId || !clusterElementData || elementType === undefined) {
-        updatedClusterElements = updateClusterElementsPositions({
-            clusterElements: updatedClusterElements,
-            nodePositions,
-        });
+        return {
+            nestedClusterElements: updateClusterElementsPositions({
+                clusterElements,
+                nodePositions,
+            }),
 
-        return {nestedClusterElements: updatedClusterElements, parentFound};
+            parentFound: false,
+        };
     }
 
+    // Add element to the main root
     if (mainRootId && sourceNodeId === mainRootId) {
-        // Add element to the main root
-        if (isMultipleElements) {
-            updatedClusterElements[elementType] = [
-                ...(Array.isArray(updatedClusterElements[elementType]) ? updatedClusterElements[elementType] : []),
-                clusterElementData,
-            ];
-        } else {
-            updatedClusterElements[elementType] = clusterElementData;
-        }
+        return {
+            nestedClusterElements: addElementToClusterRoot({
+                clusterElementTypeLabel: elementType,
+                clusterElementValue: clusterElementData,
+                clusterElements,
+                isMultipleElements,
+            }),
 
-        parentFound = true;
-    } else {
-        // Process the hierarchy to find the parent node and add the element
-        Object.values(updatedClusterElements).forEach((value) => {
+            parentFound: true,
+        };
+    }
+
+    // Process the hierarchy to find the parent node and add the element
+    const updatedClusterElements = Object.fromEntries(
+        Object.entries(clusterElements).map(([clusterElementType, clusterElementValue]) => {
             if (parentFound) {
-                return;
+                return [clusterElementType, clusterElementValue];
             }
 
-            if (Array.isArray(value)) {
-                value.forEach((element) => {
+            if (Array.isArray(clusterElementValue)) {
+                const updatedElements = clusterElementValue.map((element) => {
                     if (parentFound) {
-                        return;
+                        return element;
                     }
 
-                    // Check if this is the correct root
-                    if (element.name === sourceNodeId) {
-                        if (!element.clusterElements) {
-                            element.clusterElements = {};
-                        }
-
-                        // Add element to this root
-                        if (isMultipleElements) {
-                            element.clusterElements[elementType] = [
-                                ...(Array.isArray(element.clusterElements[elementType])
-                                    ? element.clusterElements[elementType]
-                                    : []),
-                                clusterElementData,
-                            ];
-                        } else {
-                            element.clusterElements[elementType] = clusterElementData;
-                        }
-
+                    // Check if this is the correct root and add the element
+                    if (element.name === sourceNodeId && element.clusterElements) {
                         parentFound = true;
 
-                        return;
-                    }
-                    // Process nested cluster elements
-                    if (element.clusterElements) {
-                        const result = processClusterElementsHierarchy({
+                        return {
+                            ...element,
+                            clusterElements: addElementToClusterRoot({
+                                clusterElementTypeLabel: elementType,
+                                clusterElementValue: clusterElementData,
+                                clusterElements: element.clusterElements || {},
+                                isMultipleElements,
+                            }),
+                        };
+                    } else if (element.clusterElements) {
+                        // Process nested cluster elements
+                        const updatedElements = processClusterElementsHierarchy({
                             clusterElementData,
                             clusterElements: element.clusterElements,
                             elementType,
                             isMultipleElements,
+                            mainRootId,
                             nodePositions,
                             sourceNodeId,
                         });
 
-                        if (result.parentFound) {
-                            element.clusterElements = result.nestedClusterElements;
-
+                        if (updatedElements.parentFound) {
                             parentFound = true;
 
-                            return;
+                            return {...element, clusterElements: updatedElements.nestedClusterElements};
                         }
                     }
+
+                    return element;
                 });
-            } else if (value && typeof value === 'object') {
-                // Check if this is the correct root
-                if (value.name === sourceNodeId) {
-                    if (!value.clusterElements) {
-                        value.clusterElements = {};
-                    }
 
-                    // Add element to this root
-                    if (isMultipleElements) {
-                        value.clusterElements[elementType] = [
-                            ...(Array.isArray(value.clusterElements[elementType])
-                                ? value.clusterElements[elementType]
-                                : []),
-                            clusterElementData,
-                        ];
-                    } else {
-                        value.clusterElements[elementType] = clusterElementData;
-                    }
-
-                    parentFound = true;
-
-                    return;
+                return [clusterElementType, updatedElements];
+            } else if (isPlainObject(clusterElementValue)) {
+                if (parentFound) {
+                    return [clusterElementType, clusterElementValue];
                 }
 
-                // Process nested cluster elements
-                if (value.clusterElements) {
-                    const result = processClusterElementsHierarchy({
+                // Check if this is the correct root and add the element
+                if (clusterElementValue.name === sourceNodeId) {
+                    parentFound = true;
+
+                    return [
+                        clusterElementType,
+                        {
+                            ...clusterElementValue,
+                            clusterElements: addElementToClusterRoot({
+                                clusterElementTypeLabel: elementType,
+                                clusterElementValue: clusterElementData,
+                                clusterElements: clusterElementValue.clusterElements || {},
+                                isMultipleElements,
+                            }),
+                        },
+                    ];
+                } else if (clusterElementValue.clusterElements) {
+                    // Process nested cluster elements
+                    const updatedElements = processClusterElementsHierarchy({
                         clusterElementData,
-                        clusterElements: value.clusterElements,
+                        clusterElements: clusterElementValue.clusterElements,
                         elementType,
                         isMultipleElements,
+                        mainRootId,
                         nodePositions,
                         sourceNodeId,
                     });
 
-                    if (result.parentFound) {
-                        value.clusterElements = result.nestedClusterElements;
-
+                    if (updatedElements.parentFound) {
                         parentFound = true;
 
-                        return;
+                        return [
+                            clusterElementType,
+                            {...clusterElementValue, clusterElements: updatedElements.nestedClusterElements},
+                        ];
                     }
                 }
+
+                return [clusterElementType, clusterElementValue];
             }
-        });
-    }
 
-    updatedClusterElements = updateClusterElementsPositions({
-        clusterElements: updatedClusterElements,
-        nodePositions,
-    });
+            return [clusterElementType, clusterElementValue];
+        })
+    );
 
-    return {nestedClusterElements: updatedClusterElements, parentFound};
+    return {
+        nestedClusterElements: updateClusterElementsPositions({
+            clusterElements: updatedClusterElements,
+            nodePositions,
+        }),
+        parentFound,
+    };
 }
