876d0e474d52a9dd639c40cbb61c49500354c3fd
Nikolina Spehar <speharnikolina87@gmail.com>
2025-07-18 09:44:55 +0200

    2288 - added functions for getting project key and workspace options

--- server/libs/modules/components/bitbucket/src/main/java/com/bytechef/component/bitbucket/util/BitbucketUtils.java
+++ server/libs/modules/components/bitbucket/src/main/java/com/bytechef/component/bitbucket/util/BitbucketUtils.java
@@ -16,10 +16,71 @@
 
 package com.bytechef.component.bitbucket.util;
 
+import static com.bytechef.component.bitbucket.constant.BitbucketConstants.KEY;
+import static com.bytechef.component.bitbucket.constant.BitbucketConstants.NAME;
+import static com.bytechef.component.bitbucket.constant.BitbucketConstants.PAGE;
+import static com.bytechef.component.bitbucket.constant.BitbucketConstants.VALUES;
+import static com.bytechef.component.bitbucket.constant.BitbucketConstants.WORKSPACE;
+import static com.bytechef.component.definition.ComponentDsl.option;
+import static com.bytechef.component.definition.Context.Http.responseType;
+
+import com.bytechef.component.definition.Context;
+import com.bytechef.component.definition.Context.Http.ResponseType;
+import com.bytechef.component.definition.Option;
+import com.bytechef.component.definition.Parameters;
+import com.bytechef.component.definition.TypeReference;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 /**
  * @author Nikolina Spehar
  */
 public class BitbucketUtils extends AbstractBitbucketUtils {
+
+    private static List<Option<String>> getPaginationValues(
+        Context context, String startUrl, String optionLabel, String optionValue) {
+
+        List<Option<String>> options = new ArrayList<>();
+
+        Map<String, Object> response;
+
+        do {
+            int page = 0;
+
+            response = context.http(http -> http.get(startUrl))
+                .queryParameter(PAGE, String.valueOf(++page))
+                .configuration(responseType(ResponseType.JSON))
+                .execute()
+                .getBody(new TypeReference<>() {});
+
+            if (response.get(VALUES) instanceof List<?> values) {
+                for (Object value : values) {
+                    if (value instanceof Map<?, ?> valueMap)
+                        options.add(option((String) valueMap.get(optionLabel), (String) valueMap.get(optionValue)));
+                }
+            }
+
+        } while (response.get("next") != null);
+
+        return options;
+    }
+
+    public static List<Option<String>> getKeyOptions(
+        Parameters inputParameters, Parameters connectionParameters, Map<String, String> lookupDependsOnPaths,
+        String searchText, Context context) {
+
+        return getPaginationValues(
+            context, "/workspaces/%s/projects".formatted(inputParameters.getRequiredString(WORKSPACE)), NAME, KEY);
+    }
+
+    public static List<Option<String>> getWorkspaceOptions(
+        Parameters inputParameters, Parameters connectionParameters, Map<String, String> lookupDependsOnPaths,
+        String searchText, Context context) {
+
+        return getPaginationValues(context, "/workspaces", NAME, NAME);
+    }
+
     private BitbucketUtils() {
     }
 }
