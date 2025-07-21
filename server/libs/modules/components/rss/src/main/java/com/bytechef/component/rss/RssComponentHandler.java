21b8f129161ab629c15480d7f24b112a0eb0e314
Nikolina Spehar <speharnikolina87@gmail.com>
2025-07-21 11:51:08 +0200

    2191 - added icon, modified category and connection

--- server/libs/modules/components/rss/src/main/java/com/bytechef/component/rss/RssComponentHandler.java
+++ server/libs/modules/components/rss/src/main/java/com/bytechef/component/rss/RssComponentHandler.java
@@ -16,12 +16,58 @@
 
 package com.bytechef.component.rss;
 
+import static com.bytechef.component.definition.ComponentDsl.authorization;
+import static com.bytechef.component.definition.ComponentDsl.string;
+import static com.bytechef.component.rss.constant.RssConstants.API_KEY;
+import static com.bytechef.component.rss.constant.RssConstants.API_SECRET;
+
 import com.bytechef.component.OpenApiComponentHandler;
+import com.bytechef.component.definition.Authorization.ApplyResponse;
+import com.bytechef.component.definition.Authorization.AuthorizationType;
+import com.bytechef.component.definition.ComponentCategory;
+import com.bytechef.component.definition.ComponentDsl.ModifiableComponentDefinition;
+import com.bytechef.component.definition.ComponentDsl.ModifiableConnectionDefinition;
 import com.google.auto.service.AutoService;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author Nikolina Spehar
  */
 @AutoService(OpenApiComponentHandler.class)
 public class RssComponentHandler extends AbstractRssComponentHandler {
+
+    @Override
+    public ModifiableComponentDefinition modifyComponent(ModifiableComponentDefinition modifiableComponentDefinition) {
+        return modifiableComponentDefinition
+            .icon("path:assets/rss.svg")
+            .categories(ComponentCategory.SOCIAL_MEDIA)
+            .customAction(true);
+    }
+
+    @Override
+    public ModifiableConnectionDefinition
+        modifyConnection(ModifiableConnectionDefinition modifiableConnectionDefinition) {
+
+        return modifiableConnectionDefinition
+            .baseUri((connectionParameters, context) -> "https://api.rss.app/v1")
+            .authorizations(
+                authorization(AuthorizationType.BEARER_TOKEN)
+                    .properties(
+                        string(API_KEY)
+                            .label("API Key")
+                            .description("Your API key can be found in Account Settings -> API.")
+                            .required(true),
+                        string(API_SECRET)
+                            .label("API Secret")
+                            .description("Your API secret can be found in Account Settings -> API.")
+                            .required(true))
+                    .apply((connectionParameters, context) -> {
+                        String apiKey = connectionParameters.getRequiredString(API_KEY);
+                        String apiSecret = connectionParameters.getRequiredString(API_SECRET);
+
+                        return ApplyResponse.ofHeaders(
+                            Map.of("Authorization", List.of("Bearer " + apiKey + ":" + apiSecret)));
+                    }));
+    }
 }
