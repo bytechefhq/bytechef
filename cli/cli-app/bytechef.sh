c47bc9ee56ba9e4326131853d4e0702f07a59c75
Matija Petanjek <matija.petanjek7@gmail.com>
2025-07-23 17:01:18 +0200

    2354 add remote debug script

--- cli/cli-app/bytechef.sh
+++ cli/cli-app/bytechef.sh
@@ -3,3 +3,6 @@
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 $SCRIPTPATH/../../gradlew -p $SCRIPTPATH run --args="$* --internal-component true"
+
+# Enable remote debugging on port 5005
+# $SCRIPTPATH/../../gradlew -p $SCRIPTPATH run --args="$* --internal-component true" --debug-jvm
