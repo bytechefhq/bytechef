cf6160c8ea3b87269c3dac05b841546246ab7f3c
Ivica Cardic <ivica.cardic@gmail.com>
2025-07-25 11:59:40 +0200

    1652 - SF

--- server/libs/platform/platform-component/platform-component-service/src/main/java/com/bytechef/platform/component/service/ConnectionDefinitionServiceImpl.java
+++ server/libs/platform/platform-component/platform-component-service/src/main/java/com/bytechef/platform/component/service/ConnectionDefinitionServiceImpl.java
@@ -68,7 +68,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
