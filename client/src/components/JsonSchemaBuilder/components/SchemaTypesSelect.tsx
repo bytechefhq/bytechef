52a1f8344e5f309611d48e7888feb7c3aeadc9d6
Krešimir Čoko <kresimircoko@gmail.com>
2025-07-29 14:45:20 +0200

    2821 - restrict root schema types to array and object for the json schema builder

--- client/src/components/JsonSchemaBuilder/components/SchemaTypesSelect.tsx
+++ client/src/components/JsonSchemaBuilder/components/SchemaTypesSelect.tsx
@@ -11,35 +11,42 @@ import {TYPE_ICONS} from '@/shared/typeIcons';
 
 interface SchemaTypesSelectProps {
     type: SchemaType;
+    root?: boolean;
     onChange: (type: SchemaType) => void;
 }
 
-const SchemaTypesSelect = ({onChange, type}: SchemaTypesSelectProps) => (
-    <fieldset className="space-y-1 p-0.5">
-        <Label>Pill Type</Label>
+const SchemaTypesSelect = ({onChange, root = false, type}: SchemaTypesSelectProps) => {
+    const rootSchemaTypes = SCHEMA_TYPES.filter((option) => option.value === 'array' || option.value === 'object');
 
-        <Select
-            defaultValue="object"
-            onValueChange={(option: SchemaType) => onChange(option)}
-            value={helpers.findOption(type)(SCHEMA_TYPES)?.value}
-        >
-            <SelectTrigger className="flex w-full min-w-48">
-                <SelectValue className="flex" placeholder="Type" />
-            </SelectTrigger>
+    const schemaTypes = root ? rootSchemaTypes : SCHEMA_TYPES;
 
-            <SelectContent>
-                {SCHEMA_TYPES.map((option) => (
-                    <SelectItem key={option.value} value={option.value}>
-                        <div className="flex w-full items-center space-x-2">
-                            <span>{TYPE_ICONS[option.value.toUpperCase() as keyof typeof TYPE_ICONS]}</span>
+    return (
+        <fieldset className="space-y-1 p-0.5">
+            <Label>Pill Type</Label>
 
-                            <span>{option.label}</span>
-                        </div>
-                    </SelectItem>
-                ))}
-            </SelectContent>
-        </Select>
-    </fieldset>
-);
+            <Select
+                defaultValue="object"
+                onValueChange={(option: SchemaType) => onChange(option)}
+                value={helpers.findOption(type)(schemaTypes)?.value}
+            >
+                <SelectTrigger className="flex w-full min-w-48">
+                    <SelectValue className="flex" placeholder="Type" />
+                </SelectTrigger>
+
+                <SelectContent>
+                    {schemaTypes.map((option) => (
+                        <SelectItem key={option.value} value={option.value}>
+                            <div className="flex w-full items-center space-x-2">
+                                <span>{TYPE_ICONS[option.value.toUpperCase() as keyof typeof TYPE_ICONS]}</span>
+
+                                <span>{option.label}</span>
+                            </div>
+                        </SelectItem>
+                    ))}
+                </SelectContent>
+            </Select>
+        </fieldset>
+    );
+};
 
 export default SchemaTypesSelect;
