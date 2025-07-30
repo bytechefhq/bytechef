af00083663a236583ddded517c0188cbde9dbf69
marijahorvat171 <marijahorvat171@gmail.com>
2025-07-30 11:24:22 +0200

    2822 - initial urlscan component

---
+++ server/libs/modules/components/urlscan/src/main/java/com/bytechef/component/urlscan/AbstractUrlscanComponentHandler.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2025 ByteChef
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef.component.urlscan;
+
+import static com.bytechef.component.definition.ComponentDsl.component;
+import static com.bytechef.component.definition.ComponentDsl.tool;
+
+import com.bytechef.component.OpenApiComponentHandler;
+import com.bytechef.component.definition.ComponentDefinition;
+import com.bytechef.component.urlscan.action.UrlscanResultAction;
+import com.bytechef.component.urlscan.action.UrlscanScanAction;
+import com.bytechef.component.urlscan.action.UrlscanScreenshotAction;
+import com.bytechef.component.urlscan.connection.UrlscanConnection;
+
+/**
+ * Provides the base implementation for the REST based component.
+ *
+ * @generated
+ */
+public abstract class AbstractUrlscanComponentHandler implements OpenApiComponentHandler {
+    private final ComponentDefinition componentDefinition = modifyComponent(
+        component("urlscan")
+            .title("Urlscan.io")
+            .description(
+                "Urlscan.io is an online service that allows you to safely analyze websites and URLs to determine potential security threats and risks."))
+                    .actions(modifyActions(UrlscanScanAction.ACTION_DEFINITION, UrlscanResultAction.ACTION_DEFINITION,
+                        UrlscanScreenshotAction.ACTION_DEFINITION))
+                    .connection(modifyConnection(UrlscanConnection.CONNECTION_DEFINITION))
+                    .clusterElements(modifyClusterElements(tool(UrlscanScanAction.ACTION_DEFINITION),
+                        tool(UrlscanResultAction.ACTION_DEFINITION), tool(UrlscanScreenshotAction.ACTION_DEFINITION)))
+                    .triggers(getTriggers());
+
+    @Override
+    public ComponentDefinition getDefinition() {
+        return componentDefinition;
+    }
+}
