27c49f98e4a68e742ce505624c08c3d2b0f889a2
Nikolina Spehar <speharnikolina87@gmail.com>
2025-07-30 13:08:22 +0200

    2293 - Jenkins component added

--- server/ee/apps/worker-app/build.gradle.kts
+++ server/ee/apps/worker-app/build.gradle.kts
@@ -163,6 +163,7 @@ dependencies {
     implementation(project(":server:libs:modules:components:infobip"))
     implementation(project(":server:libs:modules:components:insightly"))
     implementation(project(":server:libs:modules:components:intercom"))
+    implementation(project(":server:libs:modules:components:jenkins"))
     implementation(project(":server:libs:modules:components:jira"))
     implementation(project(":server:libs:modules:components:jotform"))
     implementation(project(":server:libs:modules:components:json-file"))
