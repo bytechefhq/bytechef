6a3e769339dbc694e9b2be7562e5a6ab5c5633d7
Krešimir Čoko <kresimircoko@gmail.com>
2025-08-08 08:55:13 +0200

    963 - create a LoadingDots component and use it in PageLoader

---
+++ client/src/components/LoadingDots.tsx
@@ -0,0 +1,13 @@
+const LoadingDots = () => (
+    <div className="flex animate-pulse space-x-2">
+        <div className="size-4 rounded-full bg-gray-400"></div>
+
+        <div className="size-4 rounded-full bg-gray-400"></div>
+
+        <div className="size-4 rounded-full bg-gray-400"></div>
+
+        <div className="size-4 rounded-full bg-gray-400"></div>
+    </div>
+);
+
+export default LoadingDots;
