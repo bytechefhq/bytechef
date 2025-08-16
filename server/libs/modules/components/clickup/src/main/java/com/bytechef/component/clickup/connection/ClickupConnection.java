46100121d8301157028bbbcdbf3ec239e1f9db85
Karlo Cehulic <karlocehlic@gmail.com>
2025-08-16 21:07:54 +0200

    1550 - Update Clickup connection config
    Clickup API uses body parameters to enter creds

--- server/libs/modules/components/clickup/src/main/java/com/bytechef/component/clickup/connection/ClickupConnection.java
+++ server/libs/modules/components/clickup/src/main/java/com/bytechef/component/clickup/connection/ClickupConnection.java
@@ -16,14 +16,22 @@
 
 package com.bytechef.component.clickup.connection;
 
-import static com.bytechef.component.definition.Authorization.AuthorizationType;
 import static com.bytechef.component.definition.Authorization.CLIENT_ID;
 import static com.bytechef.component.definition.Authorization.CLIENT_SECRET;
 import static com.bytechef.component.definition.ComponentDsl.authorization;
 import static com.bytechef.component.definition.ComponentDsl.connection;
 import static com.bytechef.component.definition.ComponentDsl.string;
 
+import java.util.List;
+import java.util.Map;
+
+import javax.naming.ConfigurationException;
+
+import com.bytechef.component.definition.Authorization.AuthorizationCallbackResponse;
+import com.bytechef.component.definition.Authorization.AuthorizationType;
 import com.bytechef.component.definition.ComponentDsl;
+import com.bytechef.component.definition.Context.Http;
+import com.bytechef.component.definition.TypeReference;
 
 /**
  * Provides the component connection definition.
@@ -32,18 +40,46 @@ import com.bytechef.component.definition.ComponentDsl;
  */
 public class ClickupConnection {
     public static final ComponentDsl.ModifiableConnectionDefinition CONNECTION_DEFINITION = connection()
-        .baseUri((connectionParameters, context) -> "https://api.clickup.com/api/v2")
-        .authorizations(authorization(AuthorizationType.OAUTH2_AUTHORIZATION_CODE)
-            .title("OAuth2 Authorization Code")
-            .properties(
-                string(CLIENT_ID)
-                    .label("Client Id")
-                    .required(true),
-                string(CLIENT_SECRET)
-                    .label("Client Secret")
-                    .required(true))
-            .authorizationUrl((connectionParameters, context) -> "https://app.clickup.com/api")
-            .tokenUrl((connectionParameters, context) -> "https://app.clickup.com/api/v2/oauth/token"));
+            .baseUri((connectionParameters, context) -> "https://api.clickup.com/api/v2")
+            .authorizations(authorization(AuthorizationType.OAUTH2_AUTHORIZATION_CODE)
+                    .title("OAuth2 Authorization Code")
+                    .properties(
+                            string(CLIENT_ID)
+                                    .label("Client Id")
+                                    .required(true),
+                            string(CLIENT_SECRET)
+                                    .label("Client Secret")
+                                    .required(true))
+                    .authorizationUrl((connectionParameters, context) -> "https://app.clickup.com/api")
+                    .tokenUrl((connectionParameters, context) -> "https://app.clickup.com/api/v2/oauth/token")
+                    .authorizationCallback((connectionParameters, code, redirectUri, codeVerifier, context) -> {
+                        String clientId = connectionParameters.getString(CLIENT_ID);
+                        String clientSecret = connectionParameters.getString(CLIENT_SECRET);
+
+                        Http.Response response = context
+                                .http(http -> http.post("https://api.clickup.com/api/v2/oauth/token"))
+                                .headers(
+                                        Map.of(
+                                                "Accept", List.of("application/json")))
+                                .body(Http.Body.of(
+                                        Map.of(
+                                                "code", code,
+                                                "client_id", clientId,
+                                                "client_secret", clientSecret)))
+                                .configuration(Http.responseType(Http.ResponseType.JSON))
+                                .execute();
+
+                        if (response.getStatusCode() < 200 || response.getStatusCode() > 299) {
+                            throw new ConfigurationException("Invalid claim");
+                        }
+
+                        if (response.getBody() == null) {
+                            throw new ConfigurationException("Invalid claim");
+                        }
+
+                        return new AuthorizationCallbackResponse(response.getBody(new TypeReference<>() {
+                        }));
+                    }));
 
     private ClickupConnection() {
     }
