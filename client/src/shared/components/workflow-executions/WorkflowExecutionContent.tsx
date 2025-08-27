98e9f3f56af3db594d7b96cc4a8e261d09f8a8c8
Monika Kušter <domitermonika@gmail.com>
2025-08-27 09:47:14 +0200

    2961 - show the output value corresponding to the specific instance for which the workflow was executed

--- client/src/shared/components/workflow-executions/WorkflowExecutionContent.tsx
+++ client/src/shared/components/workflow-executions/WorkflowExecutionContent.tsx
@@ -19,83 +19,36 @@ interface WorkflowExecutionContentProps {
     input?: {[key: string]: string};
     output?: object;
     startDate?: Date;
+    /* eslint-disable @typescript-eslint/no-explicit-any */
+    jobInputs?: {[key: string]: any};
+    workflowTriggerName?: string;
 }
 
-const WorkflowExecutionContent = ({endDate, error, input, output, startDate}: WorkflowExecutionContentProps) => (
-    <>
-        <div className="space-y-2 rounded-md p-2">
-            <header className="flex items-center justify-between">
-                <span className="text-sm font-semibold uppercase">Input</span>
+const WorkflowExecutionContent = ({
+    endDate,
+    error,
+    input,
+    jobInputs,
+    output,
+    startDate,
+    workflowTriggerName,
+}: WorkflowExecutionContentProps) => {
+    let filteredOutput = output;
 
-                <div className="flex items-center space-x-1">
-                    <span className="text-xs">{startDate?.toLocaleString()}</span>
+    if (jobInputs && Object.keys(jobInputs).length) {
+        filteredOutput = Object.keys(jobInputs)
+            .filter((key) => key === workflowTriggerName)
+            .map((key) => jobInputs[key]);
+    }
 
-                    <Dialog>
-                        <DialogTrigger asChild>
-                            <ExpandIcon className="h-4 cursor-pointer" />
-                        </DialogTrigger>
-
-                        <DialogContent className="max-w-workflow-execution-content-width">
-                            <DialogHeader className="flex flex-row items-center justify-between space-y-0">
-                                <DialogTitle>Input</DialogTitle>
-
-                                <div className="flex items-center gap-1">
-                                    <WorkflowExecutionContentClipboardButton value={input} />
-
-                                    <DialogCloseButton />
-                                </div>
-                            </DialogHeader>
-
-                            <div className="max-h-workflow-execution-content-height overflow-y-auto">
-                                {input && (typeof input !== 'object' || Object.keys(input).length > 0) ? (
-                                    typeof input === 'object' ? (
-                                        <Suspense
-                                            fallback={
-                                                <div className="p-4 text-sm text-muted-foreground">Loading...</div>
-                                            }
-                                        >
-                                            <ReactJson
-                                                collapsed={false}
-                                                enableClipboard={false}
-                                                src={input as object}
-                                            />
-                                        </Suspense>
-                                    ) : (
-                                        input
-                                    )
-                                ) : (
-                                    <span className="text-sm">No input data.</span>
-                                )}
-                            </div>
-                        </DialogContent>
-                    </Dialog>
-
-                    <WorkflowExecutionContentClipboardButton value={input} />
-                </div>
-            </header>
-
-            <div className="overflow-x-auto text-nowrap">
-                {input && (typeof input !== 'object' || Object.keys(input).length > 0) ? (
-                    typeof input === 'object' ? (
-                        <Suspense fallback={<div className="p-4 text-sm text-muted-foreground">Loading...</div>}>
-                            <ReactJson collapsed={false} enableClipboard={false} src={input as object} />
-                        </Suspense>
-                    ) : (
-                        <span className="text-sm">{input}</span>
-                    )
-                ) : (
-                    <span className="text-sm">No input data.</span>
-                )}
-            </div>
-        </div>
-
-        {!error && (
+    return (
+        <>
             <div className="space-y-2 rounded-md p-2">
                 <header className="flex items-center justify-between">
-                    <span className="text-sm font-semibold uppercase">Output</span>
+                    <span className="text-sm font-semibold uppercase">Input</span>
 
                     <div className="flex items-center space-x-1">
-                        <span className="text-xs">{endDate?.toLocaleString()}</span>
+                        <span className="text-xs">{startDate?.toLocaleString()}</span>
 
                         <Dialog>
                             <DialogTrigger asChild>
@@ -104,102 +57,172 @@ const WorkflowExecutionContent = ({endDate, error, input, output, startDate}: Wo
 
                             <DialogContent className="max-w-workflow-execution-content-width">
                                 <DialogHeader className="flex flex-row items-center justify-between space-y-0">
-                                    <DialogTitle>Output</DialogTitle>
+                                    <DialogTitle>Input</DialogTitle>
 
                                     <div className="flex items-center gap-1">
-                                        <WorkflowExecutionContentClipboardButton value={output} />
+                                        <WorkflowExecutionContentClipboardButton value={input} />
 
                                         <DialogCloseButton />
                                     </div>
                                 </DialogHeader>
 
                                 <div className="max-h-workflow-execution-content-height overflow-y-auto">
-                                    {output === undefined ? (
-                                        <span className="text-sm">No output data.</span>
-                                    ) : output && typeof output === 'object' ? (
-                                        <Suspense
-                                            fallback={
-                                                <div className="p-4 text-sm text-muted-foreground">Loading...</div>
-                                            }
-                                        >
-                                            <ReactJson enableClipboard={false} src={output as object} />
-                                        </Suspense>
+                                    {input && (typeof input !== 'object' || Object.keys(input).length > 0) ? (
+                                        typeof input === 'object' ? (
+                                            <Suspense
+                                                fallback={
+                                                    <div className="p-4 text-sm text-muted-foreground">Loading...</div>
+                                                }
+                                            >
+                                                <ReactJson
+                                                    collapsed={false}
+                                                    enableClipboard={false}
+                                                    src={input as object}
+                                                />
+                                            </Suspense>
+                                        ) : (
+                                            input
+                                        )
                                     ) : (
-                                        <span className="text-sm">{output}</span>
+                                        <span className="text-sm">No input data.</span>
                                     )}
                                 </div>
                             </DialogContent>
                         </Dialog>
 
-                        <WorkflowExecutionContentClipboardButton value={output} />
+                        <WorkflowExecutionContentClipboardButton value={input} />
                     </div>
                 </header>
 
                 <div className="overflow-x-auto text-nowrap">
-                    {output === undefined && <span className="text-sm">No output data.</span>}
-
-                    {output !== undefined &&
-                        (typeof output === 'object' ? (
+                    {input && (typeof input !== 'object' || Object.keys(input).length > 0) ? (
+                        typeof input === 'object' ? (
                             <Suspense fallback={<div className="p-4 text-sm text-muted-foreground">Loading...</div>}>
-                                <ReactJson enableClipboard={false} src={output as object} />
+                                <ReactJson collapsed={false} enableClipboard={false} src={input as object} />
                             </Suspense>
                         ) : (
-                            <span className="text-sm">{(output as boolean).toString()}</span>
-                        ))}
+                            <span className="text-sm">{input}</span>
+                        )
+                    ) : (
+                        <span className="text-sm">No input data.</span>
+                    )}
                 </div>
             </div>
-        )}
 
-        {error && (
-            <div className="space-y-2 rounded-md bg-muted/50 p-2">
-                <header className="flex items-center justify-between">
-                    <span className="text-sm font-semibold uppercase text-destructive">Error</span>
+            {!error && (
+                <div className="space-y-2 rounded-md p-2">
+                    <header className="flex items-center justify-between">
+                        <span className="text-sm font-semibold uppercase">Output</span>
 
-                    <div className="flex items-center space-x-1">
-                        <span className="text-xs">{endDate?.toLocaleString()}</span>
+                        <div className="flex items-center space-x-1">
+                            <span className="text-xs">{endDate?.toLocaleString()}</span>
 
-                        <Dialog>
-                            <DialogTrigger asChild>
-                                <ExpandIcon className="h-4 cursor-pointer" />
-                            </DialogTrigger>
+                            <Dialog>
+                                <DialogTrigger asChild>
+                                    <ExpandIcon className="h-4 cursor-pointer" />
+                                </DialogTrigger>
 
-                            <DialogContent className="max-w-workflow-execution-content-width">
-                                <DialogHeader className="flex flex-row items-center justify-between space-y-0">
-                                    <DialogTitle className="uppercase text-destructive">Error</DialogTitle>
+                                <DialogContent className="max-w-workflow-execution-content-width">
+                                    <DialogHeader className="flex flex-row items-center justify-between space-y-0">
+                                        <DialogTitle>Output</DialogTitle>
 
-                                    <DialogCloseButton />
-                                </DialogHeader>
+                                        <div className="flex items-center gap-1">
+                                            <WorkflowExecutionContentClipboardButton value={filteredOutput} />
 
-                                <div className="max-h-workflow-execution-content-height space-y-3 overflow-y-auto">
-                                    <div className="flex flex-col space-y-1 text-sm">
-                                        <div className="text-sm font-semibold">Message</div>
+                                            <DialogCloseButton />
+                                        </div>
+                                    </DialogHeader>
 
-                                        <div className="text-sm">{error.message || 'No message.'}</div>
+                                    <div className="max-h-workflow-execution-content-height overflow-y-auto">
+                                        {filteredOutput === undefined ? (
+                                            <span className="text-sm">No output data.</span>
+                                        ) : filteredOutput && typeof filteredOutput === 'object' ? (
+                                            <Suspense
+                                                fallback={
+                                                    <div className="p-4 text-sm text-muted-foreground">Loading...</div>
+                                                }
+                                            >
+                                                <ReactJson enableClipboard={false} src={filteredOutput as object} />
+                                            </Suspense>
+                                        ) : (
+                                            <span className="text-sm">{filteredOutput}</span>
+                                        )}
                                     </div>
+                                </DialogContent>
+                            </Dialog>
 
-                                    <div className="flex flex-col space-y-1">
-                                        <div className="text-sm font-semibold">Stack Trace</div>
+                            <WorkflowExecutionContentClipboardButton value={filteredOutput} />
+                        </div>
+                    </header>
 
-                                        <div className="text-sm">
-                                            {error?.stackTrace?.map((line, index) => (
-                                                <div key={index}>{line}</div>
-                                            ))}
+                    <div className="overflow-x-auto text-nowrap">
+                        {filteredOutput === undefined && <span className="text-sm">No output data.</span>}
+
+                        {filteredOutput !== undefined &&
+                            (typeof filteredOutput === 'object' ? (
+                                <Suspense
+                                    fallback={<div className="p-4 text-sm text-muted-foreground">Loading...</div>}
+                                >
+                                    <ReactJson enableClipboard={false} src={filteredOutput as object} />
+                                </Suspense>
+                            ) : (
+                                <span className="text-sm">{(filteredOutput as boolean).toString()}</span>
+                            ))}
+                    </div>
+                </div>
+            )}
+
+            {error && (
+                <div className="space-y-2 rounded-md bg-muted/50 p-2">
+                    <header className="flex items-center justify-between">
+                        <span className="text-sm font-semibold uppercase text-destructive">Error</span>
+
+                        <div className="flex items-center space-x-1">
+                            <span className="text-xs">{endDate?.toLocaleString()}</span>
+
+                            <Dialog>
+                                <DialogTrigger asChild>
+                                    <ExpandIcon className="h-4 cursor-pointer" />
+                                </DialogTrigger>
+
+                                <DialogContent className="max-w-workflow-execution-content-width">
+                                    <DialogHeader className="flex flex-row items-center justify-between space-y-0">
+                                        <DialogTitle className="uppercase text-destructive">Error</DialogTitle>
+
+                                        <DialogCloseButton />
+                                    </DialogHeader>
+
+                                    <div className="max-h-workflow-execution-content-height space-y-3 overflow-y-auto">
+                                        <div className="flex flex-col space-y-1 text-sm">
+                                            <div className="text-sm font-semibold">Message</div>
+
+                                            <div className="text-sm">{error.message || 'No message.'}</div>
+                                        </div>
+
+                                        <div className="flex flex-col space-y-1">
+                                            <div className="text-sm font-semibold">Stack Trace</div>
+
+                                            <div className="text-sm">
+                                                {error?.stackTrace?.map((line, index) => (
+                                                    <div key={index}>{line}</div>
+                                                ))}
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
-                            </DialogContent>
-                        </Dialog>
-                    </div>
-                </header>
+                                </DialogContent>
+                            </Dialog>
+                        </div>
+                    </header>
 
-                <div className="overflow-x-auto">
-                    <div className="flex flex-col space-y-1">
-                        <span className="text-sm">{error.message || 'No message.'}</span>
+                    <div className="overflow-x-auto">
+                        <div className="flex flex-col space-y-1">
+                            <span className="text-sm">{error.message || 'No message.'}</span>
+                        </div>
                     </div>
                 </div>
-            </div>
-        )}
-    </>
-);
+            )}
+        </>
+    );
+};
 
 export default WorkflowExecutionContent;
