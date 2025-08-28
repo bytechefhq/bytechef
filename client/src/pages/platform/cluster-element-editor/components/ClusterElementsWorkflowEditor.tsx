96a090b685c6cf4d42ed6da546e82545b954059e
katarina-cala <katarinacala@gmail.com>
2025-08-28 15:57:03 +0200

    1652-add handle to track and save node position changes

--- client/src/pages/platform/cluster-element-editor/components/ClusterElementsWorkflowEditor.tsx
+++ client/src/pages/platform/cluster-element-editor/components/ClusterElementsWorkflowEditor.tsx
@@ -1,24 +1,108 @@
-import {Background, BackgroundVariant, Controls, ReactFlow, ReactFlowProvider} from '@xyflow/react';
+import {
+    Background,
+    BackgroundVariant,
+    Controls,
+    Node,
+    OnNodesChange,
+    ReactFlow,
+    ReactFlowProvider,
+} from '@xyflow/react';
 
 import '@xyflow/react/dist/style.css';
 import {DEFAULT_CLUSTER_ELEMENT_CANVAS_ZOOM} from '@/shared/constants';
+import {useMemo, useRef} from 'react';
 import {useShallow} from 'zustand/react/shallow';
 
 import PlaceholderNode from '../../workflow-editor/nodes/PlaceholderNode';
 import WorkflowNode from '../../workflow-editor/nodes/WorkflowNode';
+import {useWorkflowEditor} from '../../workflow-editor/providers/workflowEditorProvider';
+import useWorkflowDataStore from '../../workflow-editor/stores/useWorkflowDataStore';
+import saveClusterElementNodesPosition from '../../workflow-editor/utils/saveClusterElementNodesPosition';
 import LabeledClusterElementsEdge from '../edges/LabeledClusterElementsEdge';
 import useClusterElementsLayout from '../hooks/useClusterElementsLayout';
 import useClusterElementsDataStore from '../stores/useClusterElementsDataStore';
 
 const ClusterElementsWorkflowEditor = () => {
-    const {edges, nodes, onEdgesChange, onNodesChange} = useClusterElementsDataStore(
+    const {edges, nodes, onEdgesChange, onNodesChange, setDraggingNodeId, setIsDragging} = useClusterElementsDataStore(
         useShallow((state) => ({
             edges: state.edges,
             nodes: state.nodes,
             onEdgesChange: state.onEdgesChange,
             onNodesChange: state.onNodesChange,
+            setDraggingNodeId: state.setDraggingNodeId,
+            setIsDragging: state.setIsDragging,
         }))
     );
+    const {invalidateWorkflowQueries, updateWorkflowMutation} = useWorkflowEditor();
+    const {workflow} = useWorkflowDataStore();
+
+    const previousNodePositionsRef = useRef<Record<string, {x: number; y: number}>>({});
+
+    const currentPositions = useMemo(() => {
+        const positions: Record<string, {x: number; y: number}> = {};
+
+        nodes.forEach((node) => {
+            positions[node.id] = {x: node.position.x, y: node.position.y};
+        });
+
+        return positions;
+    }, [nodes]);
+
+    const handleNodesChange: OnNodesChange<Node> = (changes) => {
+        onNodesChange(changes);
+
+        // Track dragging state and node being dragged currently
+        changes.forEach((change) => {
+            if (change.type === 'position') {
+                setIsDragging(change.dragging ?? false);
+
+                if (change.dragging) {
+                    setDraggingNodeId(change.id);
+                } else {
+                    setDraggingNodeId(null);
+                }
+            }
+        });
+
+        // Find the new position of the node that was dragged
+        changes.forEach((change) => {
+            if (change.type === 'position' && change.dragging === false) {
+                // Get the positions of the node that was dragged
+                const changedPositions: Record<string, {x: number; y: number}> = {};
+                const previousPositions = previousNodePositionsRef.current;
+
+                // Check if the node that was dragged has actually changed position
+                Object.entries(currentPositions).forEach(([nodeId, currentPos]) => {
+                    const previousPosition = previousPositions[nodeId];
+
+                    // If position of the node that was dragged changed or node is newly added then record the position for that node
+                    if (
+                        !previousPosition ||
+                        previousPosition.x !== currentPos.x ||
+                        previousPosition.y !== currentPos.y
+                    ) {
+                        changedPositions[nodeId] = currentPos;
+                    }
+                });
+
+                if (Object.keys(changedPositions).length > 0) {
+                    // Prevent race conditions with layout
+                    setTimeout(() => {
+                        // Save the nodes new positions
+                        saveClusterElementNodesPosition({
+                            invalidateWorkflowQueries,
+                            movedClusterElementId: change.id,
+                            updateWorkflowMutation,
+                            workflow,
+                        });
+                    }, 100);
+                }
+
+                // Update the previous positions reference for future comparison
+                previousNodePositionsRef.current = {...currentPositions};
+            }
+        });
+    };
 
     const clusterElementsEdgeTypes = {
         labeledClusterElementsEdge: LabeledClusterElementsEdge,
@@ -45,7 +129,7 @@ const ClusterElementsWorkflowEditor = () => {
                     nodesConnectable={false}
                     nodesDraggable
                     onEdgesChange={onEdgesChange}
-                    onNodesChange={onNodesChange}
+                    onNodesChange={handleNodesChange}
                     panOnDrag
                     panOnScroll
                     proOptions={{hideAttribution: true}}
