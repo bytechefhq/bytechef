1f8311a0d143f874f5273d9d9f1aa3ce8d5dba43
Ivica Cardic <ivica.cardic@gmail.com>
2025-09-01 22:42:32 +0200

    2916 Refactor security modules for EE version: update licensing, restructure packages, enhance CSRF and HTTP request authorization configurations, and adjust dependencies for modularity.

--- server/libs/config/security-config/build.gradle.kts
+++ server/libs/config/security-config/build.gradle.kts
@@ -3,13 +3,16 @@ dependencies {
 
     implementation("org.apache.commons:commons-lang3")
     implementation("org.slf4j:slf4j-api")
+    implementation("org.springframework:spring-tx")
     implementation("org.springframework:spring-webmvc")
     implementation("org.springframework.boot:spring-boot-autoconfigure")
     implementation("org.springframework.security:spring-security-config")
     implementation("org.springframework.security:spring-security-web")
     implementation(project(":server:libs:config:app-config"))
     implementation(project(":server:libs:core:commons:commons-util"))
+    implementation(project(":server:libs:core:tenant:tenant-api"))
     implementation(project(":server:libs:platform:platform-security-web:platform-security-web-api"))
+    implementation(project(":server:libs:platform:platform-user:platform-user-api"))
 
     testImplementation("org.springframework.boot:spring-boot-starter-web")
     testImplementation("org.springframework.security:spring-security-test")
