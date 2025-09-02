ee524bd317bde94f33b0fb37e0793cf3545be2e2
Ivica Cardic <ivica.cardic@gmail.com>
2025-09-02 21:01:58 +0200

    520 client - Generate

--- client/src/shared/middleware/platform/api-key/apis/index.ts
+++ client/src/shared/middleware/platform/api-key/apis/index.ts
@@ -1,3 +1,3 @@
 /* tslint:disable */
 /* eslint-disable */
-export * from './AuthorityApi';
+export * from './ApiKeyApi';
--- client/src/shared/middleware/platform/user/models/index.ts
+++ client/src/shared/middleware/platform/user/models/index.ts
@@ -1,6 +1,3 @@
 /* tslint:disable */
 /* eslint-disable */
-export * from './ApiKey';
 export * from './Authority';
-export * from './CreateApiKey200Response';
-export * from './SigningKey';
