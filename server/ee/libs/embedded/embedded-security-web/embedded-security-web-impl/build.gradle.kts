1cb91d3c6840870fc11b2d2ee81e0e1ea1df2d32
Ivica Cardic <ivica.cardic@gmail.com>
2025-09-09 20:27:29 +0200

    520 Reorganize signing key module

--- server/ee/libs/embedded/embedded-security-web/embedded-security-web-impl/build.gradle.kts
+++ server/ee/libs/embedded/embedded-security-web/embedded-security-web-impl/build.gradle.kts
@@ -9,8 +9,8 @@ dependencies {
     implementation(project(":server:libs:platform:platform-security-web:platform-security-web-api"))
     implementation(project(":server:libs:platform:platform-user:platform-user-api"))
 
-    implementation(project(":server:ee:libs:embedded:embedded-signing-key:embedded-signing-key-api"))
     implementation(project(":server:ee:libs:embedded:embedded-connected-user:embedded-connected-user-api"))
+    implementation(project(":server:ee:libs:embedded:embedded-security:embedded-security-api"))
 
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.5")
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.5")
---
+++ server/ee/libs/embedded/embedded-security/embedded-security-api/build.gradle.kts
@@ -0,0 +1,8 @@
+dependencies {
+    api(project(":server:libs:platform:platform-api"))
+    api(project(":server:libs:platform:platform-configuration:platform-configuration-api"))
+    api(project(":server:libs:platform:platform-user:platform-user-api"))
+
+    implementation("org.springframework.data:spring-data-jdbc")
+    implementation(project(":server:libs:core:commons:commons-util"))
+}
