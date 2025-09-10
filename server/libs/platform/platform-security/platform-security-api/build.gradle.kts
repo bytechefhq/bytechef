75b3677472af05b8979216206b020d2ec638f843
Ivica Cardic <ivica.cardic@gmail.com>
2025-09-10 23:41:07 +0200

    520 - Reorganize, move platform-api-key  to platfrom-security

---
+++ server/libs/platform/platform-security/platform-security-api/build.gradle.kts
@@ -0,0 +1,8 @@
+dependencies {
+    api(project(":server:libs:platform:platform-configuration:platform-configuration-api"))
+
+    implementation("org.apache.commons:commons-lang3")
+    implementation("org.springframework.data:spring-data-jdbc")
+    implementation(project(":server:libs:core:commons:commons-data"))
+    implementation(project(":server:libs:platform:platform-user:platform-user-api"))
+}
