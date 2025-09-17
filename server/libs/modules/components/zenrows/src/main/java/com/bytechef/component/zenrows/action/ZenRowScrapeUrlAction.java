3ad821592b91988f3f6f95075d927fcaedd329dc
Nikolina Spehar <speharnikolina87@gmail.com>
2025-09-17 09:55:18 +0200

    2825 - ZenRows component added

---
+++ server/libs/modules/components/zenrows/src/main/java/com/bytechef/component/zenrows/action/ZenRowScrapeUrlAction.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2025 ByteChef
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef.component.zenrows.action;
+
+import static com.bytechef.component.definition.ComponentDsl.action;
+import static com.bytechef.component.definition.ComponentDsl.bool;
+import static com.bytechef.component.definition.ComponentDsl.outputSchema;
+import static com.bytechef.component.definition.ComponentDsl.string;
+import static com.bytechef.component.definition.Context.Http.responseType;
+import static com.bytechef.component.zenrows.constant.ZenRowsConstants.JS_RENDER;
+import static com.bytechef.component.zenrows.constant.ZenRowsConstants.ORIGINAL_STATUS;
+import static com.bytechef.component.zenrows.constant.ZenRowsConstants.URL;
+
+import com.bytechef.component.definition.ComponentDsl.ModifiableActionDefinition;
+import com.bytechef.component.definition.Context;
+import com.bytechef.component.definition.Context.Http.ResponseType;
+import com.bytechef.component.definition.Parameters;
+import com.bytechef.component.definition.TypeReference;
+
+public class ZenRowScrapeUrlAction {
+
+    public static final ModifiableActionDefinition ACTION_DEFINITION = action("scrapeUrl")
+        .title("Scrape URL")
+        .description("Extracts HTML data from a given URL.")
+        .properties(
+            string(URL)
+                .label("URL")
+                .description("URL of the site that will be scraped.")
+                .required(true),
+            bool(ORIGINAL_STATUS)
+                .label("Original Status")
+                .description(
+                    "Return the original HTTP status code from the target page. Useful for debugging in case of " +
+                        "errors.")
+                .required(false),
+            bool(JS_RENDER)
+                .label("JS Render")
+                .description(
+                    "Enable JavaScript rendering with a headless browser. Essential for modern web apps, SPAs, and " +
+                        "sites with dynamic content.")
+                .required(false))
+        .output(
+            outputSchema(
+                string()
+                    .description("Scraped data.")))
+        .perform(ZenRowScrapeUrlAction::perform);
+
+    private ZenRowScrapeUrlAction() {
+    }
+
+    public static String perform(Parameters inputParameters, Parameters connectionParameters, Context context) {
+
+        return context.http(http -> http.get(""))
+            .configuration(responseType(ResponseType.TEXT))
+            .queryParameters(
+                URL, inputParameters.getRequiredString(URL),
+                ORIGINAL_STATUS, inputParameters.getBoolean(ORIGINAL_STATUS),
+                JS_RENDER, inputParameters.getBoolean(JS_RENDER))
+            .execute()
+            .getBody(new TypeReference<>() {});
+    }
+}
