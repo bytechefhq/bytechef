0b10439035cb18bc67dada138431f54df1364b6a
igorbeslic <igor.beslic@bitnaprednost.hr>
2025-10-15 23:49:06 +0200

    3288 csv-file - gradle - use commons-util

--- server/libs/modules/components/csv-file/build.gradle.kts
+++ server/libs/modules/components/csv-file/build.gradle.kts
@@ -3,6 +3,7 @@ version="1.0"
 dependencies {
     implementation("org.apache.commons:commons-lang3")
     implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-csv")
+    implementation(project(":server:libs:core:commons:commons-util"))
 
     testImplementation(project(":server:libs:atlas:atlas-execution:atlas-execution-api"))
     testImplementation(project(":server:libs:atlas:atlas-file-storage:atlas-file-storage-api"))
