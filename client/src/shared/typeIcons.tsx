138ef23a78cbc5375ae340935d5756004a6cc922
petrapaz <petra.pazanin10@gmail.com>
2025-10-16 14:52:38 +0200

    2654 - Change data type icons

--- client/src/shared/typeIcons.tsx
+++ client/src/shared/typeIcons.tsx
@@ -1,31 +1,34 @@
 /// <reference types="vite-plugin-svgr/client" />
 
-import AnyIcon from '@/assets/any.svg?react';
-import ArrayIcon from '@/assets/array.svg?react';
-import BooleanIcon from '@/assets/boolean.svg?react';
-import DateIcon from '@/assets/date.svg?react';
-import DateTimeIcon from '@/assets/datetime.svg?react';
-import DynamicIcon from '@/assets/dynamic.svg?react';
-import FileEntryIcon from '@/assets/file-entry.svg?react';
-import IntegerIcon from '@/assets/integer.svg?react';
-import NullIcon from '@/assets/null.svg?react';
-import NumberIcon from '@/assets/number.svg?react';
-import ObjectIcon from '@/assets/object.svg?react';
-import StringIcon from '@/assets/string.svg?react';
-import TimeIcon from '@/assets/time.svg?react';
-
-export const TYPE_ICONS = {
-    ANY: <AnyIcon className="size-4 text-gray-600" />,
-    ARRAY: <ArrayIcon className="size-4 text-gray-600" />,
-    BOOLEAN: <BooleanIcon className="size-4 text-gray-600" />,
-    DATE: <DateIcon className="size-4 text-gray-600" />,
-    DATE_TIME: <DateTimeIcon className="size-4 text-gray-600" />,
-    DYNAMIC_PROPERTIES: <DynamicIcon className="size-4 text-gray-600" />,
-    FILE_ENTRY: <FileEntryIcon className="size-4 text-gray-600" />,
-    INTEGER: <IntegerIcon className="size-4 text-gray-600" />,
-    NULL: <NullIcon className="size-4 text-gray-600" />,
-    NUMBER: <NumberIcon className="size-4 text-gray-600" />,
-    OBJECT: <ObjectIcon className="size-4 text-gray-600" />,
-    STRING: <StringIcon className="size-4 text-gray-600" />,
-    TIME: <TimeIcon className="size-4 text-gray-600" />,
-};
+import {
+    TriangleIcon,
+    BracketsIcon,
+    ToggleLeftIcon,
+    CalendarIcon,
+    CalendarClockIcon,
+    FileStackIcon,
+    FileInputIcon,
+    HashIcon,
+    CircleSlash2Icon,
+    BracesIcon,
+    CaseUpperIcon,
+    ClockIcon,
+  } from 'lucide-react';
+  
+  import TwoHashesIcon from '@/assets/TwoHashesIcon.svg?react';
+  
+  export const TYPE_ICONS = {
+    ANY: <TriangleIcon className="size-4 content-neutral-primary" />,
+    ARRAY: <BracketsIcon className="size-4 content-neutral-primary" />,
+    BOOLEAN: <ToggleLeftIcon className="size-4 content-neutral-primary" />,
+    DATE: <CalendarIcon className="size-4 content-neutral-primary" />,
+    DATE_TIME: <CalendarClockIcon className="size-4 content-neutral-primary" />,
+    DYNAMIC_PROPERTIES: <FileStackIcon className="size-4 content-neutral-primary" />,
+    FILE_ENTRY: <FileInputIcon className="size-4 content-neutral-primary" />,
+    INTEGER: <HashIcon className="size-4 content-neutral-primary" />,
+    NULL: <CircleSlash2Icon className="size-4 content-neutral-primary" />,
+    NUMBER: <TwoHashesIcon className="size-4 content-neutral-primary" />,
+    OBJECT: <BracesIcon className="size-4 content-neutral-primary" />,
+    STRING: <CaseUpperIcon className="size-4 content-neutral-primary" />,
+    TIME: <ClockIcon className="size-4 content-neutral-primary" />,
+  };
