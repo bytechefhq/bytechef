03d7d0c7ea68685b3f25271718c2ed1df87df9dc
Monika Kušter <domitermonika@gmail.com>
2025-10-17 20:39:57 +0200

    update Google API dependencies to latest versions

--- server/libs/modules/components/google/youtube/build.gradle.kts
+++ server/libs/modules/components/google/youtube/build.gradle.kts
@@ -2,5 +2,4 @@ version="1.0"
 
 dependencies {
     api(project(":server:libs:modules:components:google:google-commons"))
-    implementation("com.google.apis:google-api-services-youtube:v3-rev222-1.25.0")
 }
