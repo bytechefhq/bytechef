81401357f1a4f744c5f480c8567476c48b1a49c4
Ivica Cardic <ivica.cardic@gmail.com>
2025-10-19 23:05:40 +0200

    2629 Move multitenant quartz scheduler configuration to ee

--- server/ee/libs/platform/platform-scheduler/platform-scheduler-impl/src/main/java/com/bytechef/ee/platform/scheduler/tenant/MultiTenantLocalDataSourceJobStore.java
+++ server/ee/libs/platform/platform-scheduler/platform-scheduler-impl/src/main/java/com/bytechef/ee/platform/scheduler/tenant/MultiTenantLocalDataSourceJobStore.java
@@ -1,20 +1,11 @@
 /*
  * Copyright 2025 ByteChef
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Licensed under the ByteChef Enterprise license (the "Enterprise License");
+ * you may not use this file except in compliance with the Enterprise License.
  */
 
-package com.bytechef.platform.scheduler.tenant;
+package com.bytechef.ee.platform.scheduler.tenant;
 
 import org.quartz.impl.jdbcjobstore.DriverDelegate;
 import org.quartz.impl.jdbcjobstore.NoSuchDelegateException;
@@ -22,6 +13,8 @@ import org.quartz.impl.jdbcjobstore.Semaphore;
 import org.springframework.scheduling.quartz.LocalDataSourceJobStore;
 
 /**
+ * @version ee
+ *
  * @author Ivica Cardic
  */
 public class MultiTenantLocalDataSourceJobStore extends LocalDataSourceJobStore {
