f32e99458f40d04bc0bc79ce2b3debac3234d560
Ivica Cardic <ivica.cardic@gmail.com>
2025-10-27 22:50:24 +0100

    1570 Enable AI Copilot support with OpenAI and Anthropic models, add PGVector integration in the server-app module

--- server/ee/libs/ai/ai-copilot/ai-copilot-service/src/main/java/com/bytechef/ee/ai/copilot/config/AiCopilotEnvironmentPostProcessor.java
+++ server/ee/libs/ai/ai-copilot/ai-copilot-service/src/main/java/com/bytechef/ee/ai/copilot/config/AiCopilotEnvironmentPostProcessor.java
@@ -9,7 +9,6 @@ package com.bytechef.ee.ai.copilot.config;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.env.EnvironmentPostProcessor;
 import org.springframework.core.env.ConfigurableEnvironment;
@@ -27,19 +26,10 @@ public class AiCopilotEnvironmentPostProcessor implements EnvironmentPostProcess
     public void postProcessEnvironment(ConfigurableEnvironment environment, SpringApplication application) {
         Map<String, Object> source = new HashMap<>();
 
-        if (!environment.getProperty("bytechef.ai.copilot.enabled", Boolean.class, false)) {
-            source.put("spring.ai.chat.client.enabled", false);
-
-            if (!Objects.equals(
-                environment.getProperty("bytechef.ai.copilot.provider", String.class, "openai"), "openai")) {
-
-                source.put(
-                    "spring.autoconfigure.exclude",
-                    environment.getProperty("spring.autoconfigure.exclude") +
-                        ",org.springframework.ai.model.openai.autoconfigure.OpenAiChatAutoConfiguration");
-            }
-
-            // TODO Add support for other providers
+        if (environment.getProperty("bytechef.ai.copilot.enabled", Boolean.class, false)) {
+            source.put("spring.ai.model.chat", environment.getProperty("bytechef.ai.copilot.chat.provider"));
+            source.put("spring.ai.model.embedding", environment.getProperty("bytechef.ai.copilot.embedding.provider"));
+            source.put("spring.ai.vectorstore.type", "pgvector");
         }
 
         MapPropertySource mapPropertySource = new MapPropertySource("Custom AI Config", source);
