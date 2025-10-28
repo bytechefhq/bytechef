4b8872ad16b69e26f3f2a9ee2ec7a2280cd54c33
Ivica Cardic <ivica.cardic@gmail.com>
2025-10-28 22:08:53 +0100

    1041 client - Consolidate all project/workflow creation possibilities to the ProjectLeftSidebar

--- client/src/pages/automation/project/components/projects-sidebar/ProjectsLeftSidebar.tsx
+++ client/src/pages/automation/project/components/projects-sidebar/ProjectsLeftSidebar.tsx
@@ -1,6 +1,7 @@
 import {Button} from '@/components/ui/button';
 import {DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger} from '@/components/ui/dropdown-menu';
 import {ScrollArea} from '@/components/ui/scroll-area';
+import {Tooltip, TooltipContent, TooltipTrigger} from '@/components/ui/tooltip';
 import {useToast} from '@/hooks/use-toast';
 import ProjectSelect from '@/pages/automation/project/components/projects-sidebar/components/ProjectSelect';
 import ProjectWorkflowsList from '@/pages/automation/project/components/projects-sidebar/components/ProjectWorkflowsList';
@@ -8,6 +9,7 @@ import WorkflowsListFilter from '@/pages/automation/project/components/projects-
 import WorkflowsListItem from '@/pages/automation/project/components/projects-sidebar/components/WorkflowsListItem';
 import WorkflowsListSkeleton from '@/pages/automation/project/components/projects-sidebar/components/WorkflowsListSkeleton';
 import {useProjectsLeftSidebar} from '@/pages/automation/project/components/projects-sidebar/hooks/useProjectsLeftSidebar';
+import ProjectDialog from '@/pages/automation/projects/components/ProjectDialog';
 import {useWorkspaceStore} from '@/pages/automation/stores/useWorkspaceStore';
 import WorkflowDialog from '@/shared/components/workflow/WorkflowDialog';
 import {useAnalytics} from '@/shared/hooks/useAnalytics';
@@ -16,7 +18,6 @@ import {useGetProjectWorkflowsQuery, useGetWorkflowsQuery} from '@/shared/querie
 import {ProjectKeys, useGetWorkspaceProjectsQuery} from '@/shared/queries/automation/projects.queries';
 import {useGetWorkflowQuery} from '@/shared/queries/automation/workflows.queries';
 import {useFeatureFlagsStore} from '@/shared/stores/useFeatureFlagsStore';
-import {UpdateWorkflowMutationType} from '@/shared/types';
 import {useQueryClient} from '@tanstack/react-query';
 import {LayoutTemplateIcon, PlusIcon, UploadIcon} from 'lucide-react';
 import {ChangeEvent, RefObject, useEffect, useMemo, useRef, useState} from 'react';
@@ -27,7 +28,6 @@ interface ProjectsLeftSidebarProps {
     bottomResizablePanelRef: RefObject<ImperativePanelHandle>;
     onProjectClick: (projectId: number, projectWorkflowId: number) => void;
     projectId: number;
-    updateWorkflowMutation: UpdateWorkflowMutationType;
     currentWorkflowId: string;
 }
 
@@ -36,7 +36,6 @@ const ProjectsLeftSidebar = ({
     currentWorkflowId,
     onProjectClick,
     projectId,
-    updateWorkflowMutation,
 }: ProjectsLeftSidebarProps) => {
     const [selectedProjectId, setSelectedProjectId] = useState(projectId || 0);
     const [sortBy, setSortBy] = useState('last-edited');
@@ -44,14 +43,17 @@ const ProjectsLeftSidebar = ({
     const [isLoading, setIsLoading] = useState(false);
     const [showWorkflowDialog, setShowWorkflowDialog] = useState(false);
 
-    const hiddenFileInputRef = useRef<HTMLInputElement>(null);
+    const projectHiddenFileInputRef = useRef<HTMLInputElement>(null);
     const searchInputRef = useRef<HTMLInputElement>(null);
+    const workflowHiddenFileInputRef = useRef<HTMLInputElement>(null);
+
     const {toast} = useToast();
     const navigate = useNavigate();
 
     const {captureProjectWorkflowImported} = useAnalytics();
 
     const ff_1041 = useFeatureFlagsStore()('ff-1041');
+    const ff_2482 = useFeatureFlagsStore()('ff-2482');
 
     const {data: eachProjectWorkflows, isLoading: projectWorkflowsLoading} = useGetProjectWorkflowsQuery(
         selectedProjectId,
@@ -65,7 +67,7 @@ const ProjectsLeftSidebar = ({
     const {calculateTimeDifference, createProjectWorkflowMutation, getFilteredWorkflows, getWorkflowsProjectId} =
         useProjectsLeftSidebar({
             bottomResizablePanelRef,
-            projectId,
+            projectId: selectedProjectId === 0 ? projectId : selectedProjectId,
         });
 
     const currentWorkspaceId = useWorkspaceStore((state) => state.currentWorkspaceId);
@@ -90,8 +92,8 @@ const ProjectsLeftSidebar = ({
             queryClient.invalidateQueries({queryKey: ProjectKeys.project(selectedProjectId)});
             queryClient.invalidateQueries({queryKey: ProjectKeys.projects});
 
-            if (hiddenFileInputRef.current) {
-                hiddenFileInputRef.current.value = '';
+            if (workflowHiddenFileInputRef.current) {
+                workflowHiddenFileInputRef.current.value = '';
             }
 
             toast({
@@ -109,7 +111,40 @@ const ProjectsLeftSidebar = ({
         });
     };
 
-    const handleFileChange = async (e: ChangeEvent<HTMLInputElement>) => {
+    const handleImportProject = async (event: React.ChangeEvent<HTMLInputElement>) => {
+        const file = event.target.files?.[0];
+
+        if (!file) {
+            return;
+        }
+
+        const formData = new FormData();
+
+        formData.append('file', file);
+
+        try {
+            const response = await fetch(`/api/automation/internal/workspaces/${currentWorkspaceId}/projects/import`, {
+                body: formData,
+                method: 'POST',
+            });
+
+            if (response.ok) {
+                queryClient.invalidateQueries({
+                    queryKey: ProjectKeys.projects,
+                });
+            } else {
+                console.error('Failed to import project');
+            }
+        } catch (error) {
+            console.error('Error importing project:', error);
+        }
+
+        if (event.target) {
+            event.target.value = '';
+        }
+    };
+
+    const handleImportWorkflow = async (e: ChangeEvent<HTMLInputElement>) => {
         if (e.target.files) {
             const file = e.target.files[0];
 
@@ -143,15 +178,70 @@ const ProjectsLeftSidebar = ({
     }, [selectedProjectId]);
 
     return (
-        <aside className="flex h-full flex-col items-center gap-2 bg-surface-main pt-0.5">
-            <div className="flex flex-col gap-2 px-4">
+        <aside className="flex h-full flex-col items-center gap-2 bg-surface-main pt-3">
+            <div className="mx-4 flex w-80 flex-col gap-2">
                 {projects && (
-                    <ProjectSelect
-                        projectId={projectId}
-                        projects={projects}
-                        selectedProjectId={selectedProjectId}
-                        setSelectedProjectId={setSelectedProjectId}
-                    />
+                    <div className="flex items-center gap-2">
+                        <ProjectSelect
+                            projectId={projectId}
+                            projects={projects}
+                            selectedProjectId={selectedProjectId}
+                            setSelectedProjectId={setSelectedProjectId}
+                        />
+
+                        {ff_2482 ? (
+                            <DropdownMenu>
+                                <Tooltip>
+                                    <DropdownMenuTrigger asChild>
+                                        <TooltipTrigger asChild>
+                                            <Button
+                                                aria-label="Sort by"
+                                                className="h-auto border-stroke-neutral-secondary p-2 shadow-none hover:bg-surface-neutral-primary-hover data-[state=open]:border-stroke-brand-secondary data-[state=open]:bg-surface-brand-secondary data-[state=open]:text-content-brand-primary"
+                                                variant="outline"
+                                            >
+                                                <PlusIcon />
+                                            </Button>
+                                        </TooltipTrigger>
+                                    </DropdownMenuTrigger>
+
+                                    <TooltipContent>New project</TooltipContent>
+                                </Tooltip>
+
+                                <DropdownMenuContent align="end">
+                                    <ProjectDialog
+                                        project={undefined}
+                                        triggerNode={
+                                            <DropdownMenuItem onSelect={(event) => event.preventDefault()}>
+                                                <PlusIcon className="mr-2 size-4" />
+                                                From Scratch
+                                            </DropdownMenuItem>
+                                        }
+                                    />
+
+                                    {ff_1041 && (
+                                        <DropdownMenuItem onClick={() => navigate(`templates`)}>
+                                            <LayoutTemplateIcon className="mr-2 size-4" />
+                                            From Template
+                                        </DropdownMenuItem>
+                                    )}
+
+                                    <DropdownMenuItem onClick={() => projectHiddenFileInputRef.current?.click()}>
+                                        <UploadIcon className="mr-2 size-4" />
+                                        Import Project
+                                    </DropdownMenuItem>
+                                </DropdownMenuContent>
+                            </DropdownMenu>
+                        ) : (
+                            <ProjectDialog
+                                project={undefined}
+                                triggerNode={
+                                    <Button>
+                                        <PlusIcon className="mr-2 size-4" />
+                                    </Button>
+                                }
+                            />
+                        )}
+                    </div>
                 )}
 
                 <WorkflowsListFilter
@@ -162,45 +252,43 @@ const ProjectsLeftSidebar = ({
                     sortBy={sortBy}
                 />
 
-                {selectedProjectId === projectId && (
-                    <DropdownMenu>
-                        <DropdownMenuTrigger asChild>
-                            <Button
-                                className="w-full bg-surface-neutral-secondary py-2 hover:bg-background [&_svg]:size-5"
-                                size="icon"
-                                variant="ghost"
-                            >
-                                <div className="flex items-center justify-center gap-2">
-                                    <PlusIcon />
+                <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                        <Button
+                            className="w-full bg-surface-neutral-secondary py-2 hover:bg-background [&_svg]:size-5"
+                            size="icon"
+                            variant="ghost"
+                        >
+                            <div className="flex items-center justify-center gap-2">
+                                <PlusIcon />
 
-                                    <span>New workflow</span>
-                                </div>
-                            </Button>
-                        </DropdownMenuTrigger>
+                                <span>New workflow</span>
+                            </div>
+                        </Button>
+                    </DropdownMenuTrigger>
 
-                        <DropdownMenuContent align="end">
-                            <DropdownMenuItem onClick={() => setShowWorkflowDialog(true)}>
-                                <PlusIcon /> From Scratch
+                    <DropdownMenuContent align="center" className="w-80">
+                        <DropdownMenuItem onClick={() => setShowWorkflowDialog(true)}>
+                            <PlusIcon /> From Scratch
+                        </DropdownMenuItem>
+
+                        {ff_1041 && (
+                            <DropdownMenuItem onClick={() => navigate(`./../../../${selectedProjectId}/templates`)}>
+                                <LayoutTemplateIcon /> From Template
                             </DropdownMenuItem>
+                        )}
 
-                            {ff_1041 && (
-                                <DropdownMenuItem onClick={() => navigate(`./../../templates`)}>
-                                    <LayoutTemplateIcon /> From Template
-                                </DropdownMenuItem>
-                            )}
-
-                            <DropdownMenuItem
-                                onClick={() => {
-                                    if (hiddenFileInputRef.current) {
-                                        hiddenFileInputRef.current.click();
-                                    }
-                                }}
-                            >
-                                <UploadIcon /> Import Workflow
-                            </DropdownMenuItem>
-                        </DropdownMenuContent>
-                    </DropdownMenu>
-                )}
+                        <DropdownMenuItem
+                            onClick={() => {
+                                if (workflowHiddenFileInputRef.current) {
+                                    workflowHiddenFileInputRef.current.click();
+                                }
+                            }}
+                        >
+                            <UploadIcon /> Import Workflow
+                        </DropdownMenuItem>
+                    </DropdownMenuContent>
+                </DropdownMenu>
             </div>
 
             <ScrollArea className="mb-3 h-screen w-full overflow-y-auto px-4">
@@ -209,20 +297,24 @@ const ProjectsLeftSidebar = ({
                 {!isLoading && (
                     <ul className="flex flex-col items-center gap-4">
                         {selectedProjectId === 0 &&
-                            (projects || []).map((project) => (
-                                <ProjectWorkflowsList
-                                    calculateTimeDifference={calculateTimeDifference}
-                                    currentWorkflowId={currentWorkflowId}
-                                    filteredWorkflowsList={filteredWorkflowsList}
-                                    findProjectIdByWorkflow={findProjectIdByWorkflow}
-                                    key={project.id}
-                                    onProjectClick={onProjectClick}
-                                    project={project}
-                                    setSelectedProjectId={setSelectedProjectId}
-                                />
+                            (projects ? (
+                                projects.map((project) => (
+                                    <ProjectWorkflowsList
+                                        calculateTimeDifference={calculateTimeDifference}
+                                        currentWorkflowId={currentWorkflowId}
+                                        filteredWorkflowsList={filteredWorkflowsList}
+                                        findProjectIdByWorkflow={findProjectIdByWorkflow}
+                                        key={project.id}
+                                        onProjectClick={onProjectClick}
+                                        project={project}
+                                        setSelectedProjectId={setSelectedProjectId}
+                                    />
+                                ))
+                            ) : (
+                                <span className="text-sm text-muted-foreground">Now workflows found</span>
                             ))}
 
-                        {selectedProjectId !== 0 &&
+                        {selectedProjectId !== 0 && filteredWorkflowsList.length > 0 ? (
                             filteredWorkflowsList.map((workflow) => (
                                 <WorkflowsListItem
                                     calculateTimeDifference={calculateTimeDifference}
@@ -233,7 +325,10 @@ const ProjectsLeftSidebar = ({
                                     setSelectedProjectId={setSelectedProjectId}
                                     workflow={workflow}
                                 />
-                            ))}
+                            ))
+                        ) : (
+                            <span className="text-sm text-muted-foreground">Now workflows found</span>
+                        )}
                     </ul>
                 )}
             </ScrollArea>
@@ -242,13 +337,27 @@ const ProjectsLeftSidebar = ({
                 <WorkflowDialog
                     createWorkflowMutation={createProjectWorkflowMutation}
                     onClose={() => setShowWorkflowDialog(false)}
-                    projectId={projectId}
-                    updateWorkflowMutation={updateWorkflowMutation}
+                    projectId={selectedProjectId}
                     useGetWorkflowQuery={useGetWorkflowQuery}
                 />
             )}
 
-            <input alt="file" className="hidden" onChange={handleFileChange} ref={hiddenFileInputRef} type="file" />
+            <input
+                accept=".json,.yaml,.yml"
+                alt="file"
+                className="hidden"
+                onChange={handleImportWorkflow}
+                ref={workflowHiddenFileInputRef}
+                type="file"
+            />
+
+            <input
+                accept=".zip"
+                className="hidden"
+                onChange={handleImportProject}
+                ref={projectHiddenFileInputRef}
+                type="file"
+            />
         </aside>
     );
 };
