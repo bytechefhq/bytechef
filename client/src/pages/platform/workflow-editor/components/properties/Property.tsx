d6ff7903fd021d200d2b5b762a3a14d62573d399
Ivica Cardic <ivica.cardic@gmail.com>
2025-10-28 18:46:53 +0100

    2536 client- Simplify

--- client/src/pages/platform/workflow-editor/components/properties/Property.tsx
+++ client/src/pages/platform/workflow-editor/components/properties/Property.tsx
@@ -1245,7 +1245,7 @@ const Property = ({
                             name={name!}
                             onChange={(value) => handleJsonSchemaBuilderChange(value)}
                             schema={inputValue ? JSON.parse(inputValue) : undefined}
-                            title={`${label || name}`}
+                            title={label || name}
                         />
                     )}
 
