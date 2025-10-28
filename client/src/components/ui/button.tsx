aab850bbf04d80d4b8978c507df9dedc7ffb1398
Ivica Cardic <ivica.cardic@gmail.com>
2025-10-28 23:58:08 +0100

    0 client - shadcn - Remove custom success variant

--- client/src/components/ui/button.tsx
+++ client/src/components/ui/button.tsx
@@ -19,7 +19,6 @@ const buttonVariants = cva(
           "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
-        success: "bg-success text-success-foreground shadow-sm hover:bg-success",
       },
       size: {
         default: "h-9 px-4 py-2",
