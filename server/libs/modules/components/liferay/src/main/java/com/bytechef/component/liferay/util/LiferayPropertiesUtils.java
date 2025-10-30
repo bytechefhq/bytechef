913efe0de69b5f6bfdffd46f2a9be0290d48505a
igorbeslic <igor.beslic@bitnaprednost.hr>
2025-10-31 00:11:29 +0100

    3428 liferay-component - clean code - better names

--- server/libs/modules/components/liferay/src/main/java/com/bytechef/component/liferay/util/LiferayPropertiesUtils.java
+++ server/libs/modules/components/liferay/src/main/java/com/bytechef/component/liferay/util/LiferayPropertiesUtils.java
@@ -54,10 +54,11 @@ public class LiferayPropertiesUtils {
     public static PropertiesContainer createPropertiesForParameters(
         String application, String endpoint, ActionContext context) {
 
-        String url = "/o/" + application + "/openapi.json";
-        String cacheKey = "/o/" + application + "/" + endpoint;
+        String applicationOpenApiUrl = "/o/" + application + "/openapi.json";
+        String applicationEndpointCacheKey = application + "/" + endpoint;
 
-        return PROPERTIES_CONTAINER_CACHE.get(cacheKey, key -> getPropertiesContainer(url, endpoint, context));
+        return PROPERTIES_CONTAINER_CACHE.get(
+            applicationEndpointCacheKey, key -> getPropertiesContainer(applicationOpenApiUrl, endpoint, context));
     }
 
     private static List<Map<String, Object>> extractPropertiesFromSchema(
