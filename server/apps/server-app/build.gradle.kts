d7351b6ac7f18286dd36f2743a2a4c458e3f92f0
Ivica Cardic <ivica.cardic@gmail.com>
2025-11-16 07:53:33 +0100

    3529 Comment out `spring-cloud-aws-starter-secrets-manager`, we don't need it?

--- server/apps/server-app/build.gradle.kts
+++ server/apps/server-app/build.gradle.kts
@@ -7,7 +7,7 @@ springBoot {
 }
 
 dependencies {
-    implementation("io.awspring.cloud:spring-cloud-aws-starter-secrets-manager")
+//    implementation("io.awspring.cloud:spring-cloud-aws-starter-secrets-manager")
     implementation("io.awspring.cloud:spring-cloud-aws-starter-sqs")
     implementation(libs.org.springdoc.springdoc.openapi.starter.common)
     implementation(libs.org.springdoc.springdoc.openapi.starter.webmvc.ui)
