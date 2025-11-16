ef24473902fcd789f1a5464426ef91bf17045544
Ivica Cardic <ivica.cardic@gmail.com>
2025-11-16 07:57:15 +0100

    3529 Update gRPC `grpc-netty` dependency to version 1.76.0

--- server/libs/modules/components/ai/vectorstore/pinecone/build.gradle.kts
+++ server/libs/modules/components/ai/vectorstore/pinecone/build.gradle.kts
@@ -2,6 +2,6 @@ version="1.0"
 
 dependencies {
     implementation("com.github.ben-manes.caffeine:caffeine")
-    implementation("io.grpc:grpc-netty:1.69.0")
+    implementation("io.grpc:grpc-netty:1.76.0")
     implementation("org.springframework.ai:spring-ai-pinecone-store")
 }
--- server/libs/modules/components/ai/vectorstore/qdrant/build.gradle.kts
+++ server/libs/modules/components/ai/vectorstore/qdrant/build.gradle.kts
@@ -1,6 +1,6 @@
 version="1.0"
 
 dependencies {
-    implementation("io.grpc:grpc-netty:1.69.0")
+    implementation("io.grpc:grpc-netty:1.76.0")
     implementation("org.springframework.ai:spring-ai-qdrant-store")
 }
