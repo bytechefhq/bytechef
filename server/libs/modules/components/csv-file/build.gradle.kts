147920d296a3737f6d8ddf166e2a6eb409ebc870
Ivica Cardic <ivica.cardic@gmail.com>
2025-11-16 19:54:53 +0100

    3288 Don’t use directly ommons-util

--- server/libs/modules/components/csv-file/build.gradle.kts
+++ server/libs/modules/components/csv-file/build.gradle.kts
@@ -4,7 +4,6 @@ dependencies {
     implementation("org.apache.commons:commons-lang3")
     implementation("org.apache.commons:commons-csv:1.14.1")
     implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-csv")
-    implementation(project(":server:libs:core:commons:commons-util"))
 
     testImplementation(project(":server:libs:atlas:atlas-execution:atlas-execution-api"))
     testImplementation(project(":server:libs:atlas:atlas-file-storage:atlas-file-storage-api"))
