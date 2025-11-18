cf60e1332aa6ba61cf7d20df6e7956f048d22627
Ivica Cardic <ivica.cardic@gmail.com>
2025-11-18 23:35:54 +0100

    3529 Introduce `KafkaMessageBrokerEnvironmentPostProcessor` to conditionally exclude Kafka auto-configuration based on the message broker provider.

---
+++ server/libs/core/message/message-broker/message-broker-kafka/src/main/java/com/bytechef/message/broker/kafka/config/KafkaMessageBrokerEnvironmentPostProcessor.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2025 ByteChef
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef.message.broker.kafka.config;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.env.EnvironmentPostProcessor;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.MapPropertySource;
+import org.springframework.core.env.MutablePropertySources;
+
+/**
+ * @author Ivica Cardic
+ */
+public class KafkaMessageBrokerEnvironmentPostProcessor implements EnvironmentPostProcessor {
+
+    @Override
+    public void postProcessEnvironment(ConfigurableEnvironment environment, SpringApplication application) {
+        Map<String, Object> source = new HashMap<>();
+
+        if (!Objects.equals(environment.getProperty("bytechef.message.broker.provider", String.class), "kafka")) {
+            source.put(
+                "spring.autoconfigure.exclude",
+                environment.getProperty("spring.autoconfigure.exclude") +
+                    ",org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration");
+        }
+
+        MapPropertySource mapPropertySource = new MapPropertySource("Custom Management Kafka Config", source);
+
+        MutablePropertySources mutablePropertySources = environment.getPropertySources();
+
+        mutablePropertySources.addFirst(mapPropertySource);
+    }
+}
