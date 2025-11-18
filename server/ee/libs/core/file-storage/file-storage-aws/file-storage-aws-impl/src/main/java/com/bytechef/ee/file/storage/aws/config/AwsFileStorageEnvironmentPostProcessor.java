30c45525be5b8f9f947a09d23a2aa587a3d69e97
Ivica Cardic <ivica.cardic@gmail.com>
2025-11-18 23:29:01 +0100

    3529 Introduce `AwsFileStorageEnvironmentPostProcessor` to conditionally enable AWS S3 based on `bytechef.file.storage.provider`.

---
+++ server/ee/libs/core/file-storage/file-storage-aws/file-storage-aws-impl/src/main/java/com/bytechef/ee/file/storage/aws/config/AwsFileStorageEnvironmentPostProcessor.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2025 ByteChef
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef.ee.file.storage.aws.config;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.env.EnvironmentPostProcessor;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.MapPropertySource;
+import org.springframework.core.env.MutablePropertySources;
+
+/**
+ * @author Ivica Cardic
+ */
+public class AwsFileStorageEnvironmentPostProcessor implements EnvironmentPostProcessor {
+
+    @Override
+    public void postProcessEnvironment(ConfigurableEnvironment environment, SpringApplication application) {
+        Map<String, Object> source = new HashMap<>();
+
+        if (Objects.equals(environment.getProperty("bytechef.file.storage.provider", String.class), "aws")) {
+            source.put("spring.cloud.aws.s3.enabled", true);
+        }
+
+        MapPropertySource mapPropertySource = new MapPropertySource("Custom Management AWS S3 Config", source);
+
+        MutablePropertySources mutablePropertySources = environment.getPropertySources();
+
+        mutablePropertySources.addFirst(mapPropertySource);
+    }
+}
