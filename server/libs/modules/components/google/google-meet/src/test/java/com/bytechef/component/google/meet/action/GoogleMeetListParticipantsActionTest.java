2c2caaf3468280b5d95446ce85704dbf21cd92f0
Monika Kušter <domitermonika@gmail.com>
2025-11-25 08:44:41 +0100

    2291 - fix tests

--- server/libs/modules/components/google/google-meet/src/test/java/com/bytechef/component/google/meet/action/GoogleMeetListParticipantsActionTest.java
+++ server/libs/modules/components/google/google-meet/src/test/java/com/bytechef/component/google/meet/action/GoogleMeetListParticipantsActionTest.java
@@ -18,44 +18,103 @@ package com.bytechef.component.google.meet.action;
 
 import static com.bytechef.component.google.meet.constant.GoogleMeetConstants.CONFERENCE_RECORDS;
 import static com.bytechef.component.google.meet.constant.GoogleMeetConstants.NAME;
+import static com.bytechef.component.google.meet.constant.GoogleMeetConstants.NEXT_PAGE_TOKEN;
+import static com.bytechef.component.google.meet.constant.GoogleMeetConstants.PAGE_SIZE;
+import static com.bytechef.component.google.meet.constant.GoogleMeetConstants.PAGE_TOKEN;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentCaptor.forClass;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import com.bytechef.component.definition.Context;
+import com.bytechef.component.definition.Context.ContextFunction;
 import com.bytechef.component.definition.Context.Http;
+import com.bytechef.component.definition.Context.Http.Configuration;
 import com.bytechef.component.definition.Parameters;
+import com.bytechef.component.definition.TypeReference;
 import com.bytechef.component.test.definition.MockParametersFactory;
+import java.util.List;
 import java.util.Map;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 
 /**
  * @author Marija Horvat
+ * @author Monika Kušter
  */
 class GoogleMeetListParticipantsActionTest {
 
+    private final ArgumentCaptor<Configuration.ConfigurationBuilder> configurationBuilderArgumentCaptor =
+        forClass(Configuration.ConfigurationBuilder.class);
+    @SuppressWarnings("unchecked")
+    private final ArgumentCaptor<ContextFunction<Http, Http.Executor>> httpFunctionArgumentCaptor =
+        forClass(ContextFunction.class);
     private final Context mockedContext = mock(Context.class);
     private final Http.Executor mockedExecutor = mock(Http.Executor.class);
+    private final Http mockedHttp = mock(Http.class);
     private final Parameters mockedParameters = MockParametersFactory.create(Map.of(CONFERENCE_RECORDS, "test"));
     private final Http.Response mockedResponse = mock(Http.Response.class);
-    private final Map<String, Object> responseMap = Map.of(
-        NAME, "test", "user", "test",
-        "earliestStartTime", "2025-10-04T10:00:00", "latestEndTime", "2025-10-04T12:00:00");
+    private final ArgumentCaptor<Object[]> objectsArgumentCaptor = forClass(Object[].class);
+    private final ArgumentCaptor<String> stringArgumentCaptor = forClass(String.class);
 
     @Test
     void testPerform() {
-        when(mockedContext.http(any()))
+        when(mockedContext.http(httpFunctionArgumentCaptor.capture()))
+            .thenAnswer(inv -> {
+                ContextFunction<Http, Http.Executor> value = httpFunctionArgumentCaptor.getValue();
+
+                return value.apply(mockedHttp);
+            });
+        when(mockedHttp.get(stringArgumentCaptor.capture()))
             .thenReturn(mockedExecutor);
-        when(mockedExecutor.configuration(any()))
+        when(mockedExecutor.queryParameters(objectsArgumentCaptor.capture()))
+            .thenReturn(mockedExecutor);
+        when(mockedExecutor.configuration(configurationBuilderArgumentCaptor.capture()))
             .thenReturn(mockedExecutor);
         when(mockedExecutor.execute())
             .thenReturn(mockedResponse);
-        when(mockedResponse.getBody())
-            .thenReturn(responseMap);
+        when(mockedResponse.getBody(any(TypeReference.class)))
+            .thenReturn(
+                Map.of("participants", List.of(Map.of(NAME, "first")), NEXT_PAGE_TOKEN, "t1"),
+                Map.of("participants", List.of(Map.of(NAME, "second"))));
 
         Object result = GoogleMeetListParticipantsAction.perform(mockedParameters, mockedParameters, mockedContext);
 
-        assertEquals(responseMap, result);
+        assertEquals(List.of(Map.of(NAME, "first"), Map.of(NAME, "second")), result);
+
+        ContextFunction<Http, Http.Executor> capturedFunction = httpFunctionArgumentCaptor.getValue();
+
+        assertNotNull(capturedFunction);
+
+        Configuration.ConfigurationBuilder configurationBuilder = configurationBuilderArgumentCaptor.getValue();
+
+        Configuration configuration = configurationBuilder.build();
+
+        Http.ResponseType responseType = configuration.getResponseType();
+
+        assertEquals(Http.ResponseType.Type.JSON, responseType.getType());
+        assertEquals(
+            List.of(
+                "https://meet.googleapis.com/v2/test/participants", "https://meet.googleapis.com/v2/test/participants"),
+            stringArgumentCaptor.getAllValues());
+
+        List<Object[]> allQueryParams = objectsArgumentCaptor.getAllValues();
+
+        assertEquals(2, allQueryParams.size());
+
+        Object[] queryParameters1 = {
+            PAGE_SIZE, 250,
+            PAGE_TOKEN, null
+        };
+        Object[] queryParameters2 = {
+            PAGE_SIZE, 250,
+            PAGE_TOKEN, "t1"
+        };
+
+        assertArrayEquals(queryParameters1, allQueryParams.get(0));
+        assertArrayEquals(queryParameters2, allQueryParams.get(1));
     }
 }
