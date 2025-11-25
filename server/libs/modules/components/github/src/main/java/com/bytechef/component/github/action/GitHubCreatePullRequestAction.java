e8a99f66809f4369aeb4014c6261fab49fcb27c4
Monika Kušter <domitermonika@gmail.com>
2025-11-25 09:54:24 +0100

    3196 improve createPullRequest action: add OWNER property, update output schema, use OWNER in request path, update field descriptions

--- server/libs/modules/components/github/src/main/java/com/bytechef/component/github/action/GitHubCreatePullRequestAction.java
+++ server/libs/modules/components/github/src/main/java/com/bytechef/component/github/action/GitHubCreatePullRequestAction.java
@@ -16,61 +16,154 @@
 
 package com.bytechef.component.github.action;
 
-import com.bytechef.component.definition.*;
-import com.bytechef.component.github.util.GithubUtils;
-
-import java.util.Map;
-
-import static com.bytechef.component.definition.ComponentDsl.*;
+import static com.bytechef.component.definition.ComponentDsl.ModifiableActionDefinition;
+import static com.bytechef.component.definition.ComponentDsl.action;
+import static com.bytechef.component.definition.ComponentDsl.array;
+import static com.bytechef.component.definition.ComponentDsl.bool;
+import static com.bytechef.component.definition.ComponentDsl.integer;
+import static com.bytechef.component.definition.ComponentDsl.object;
+import static com.bytechef.component.definition.ComponentDsl.outputSchema;
+import static com.bytechef.component.definition.ComponentDsl.string;
 import static com.bytechef.component.definition.Context.Http.responseType;
-import static com.bytechef.component.github.constant.GithubConstants.*;
-import static com.bytechef.component.github.util.GithubUtils.getOwnerName;
+import static com.bytechef.component.github.constant.GithubConstants.BASE;
+import static com.bytechef.component.github.constant.GithubConstants.BODY;
+import static com.bytechef.component.github.constant.GithubConstants.DRAFT;
+import static com.bytechef.component.github.constant.GithubConstants.HEAD;
+import static com.bytechef.component.github.constant.GithubConstants.HEAD_REPO;
+import static com.bytechef.component.github.constant.GithubConstants.ID;
+import static com.bytechef.component.github.constant.GithubConstants.ISSUE;
+import static com.bytechef.component.github.constant.GithubConstants.NAME;
+import static com.bytechef.component.github.constant.GithubConstants.OWNER;
+import static com.bytechef.component.github.constant.GithubConstants.OWNER_PROPERTY;
+import static com.bytechef.component.github.constant.GithubConstants.REPOSITORY;
+import static com.bytechef.component.github.constant.GithubConstants.TITLE;
+
+import com.bytechef.component.definition.Context;
+import com.bytechef.component.definition.Context.Http;
+import com.bytechef.component.definition.Parameters;
+import com.bytechef.component.definition.Property.ControlType;
+import com.bytechef.component.definition.TypeReference;
+import java.util.Map;
 
 /**
  * @author Anas Elgarhy (@0x61nas)
+ * @author Monika Kušter
  */
 public class GitHubCreatePullRequestAction {
-    public static final ComponentDsl.ModifiableActionDefinition ACTION_DEFINITION = action("createPullRequest")
+
+    public static final ModifiableActionDefinition ACTION_DEFINITION = action("createPullRequest")
         .title("Create Pull Request")
-        .description("Create Pull Request in GitHub Repository")
+        .description("Creates a new pull request.")
         .properties(
+            OWNER_PROPERTY,
             string(REPOSITORY)
                 .label("Repository")
                 .description("Repository where new pull request will be created.")
-                .options((OptionsDataSource.ActionOptionsFunction<String>) GithubUtils::getRepositoryOptions)
                 .required(true),
             string(TITLE)
                 .label("Title")
-                .description("Title of the pull request.")
+                .description("Title of the new pull request.")
                 .maxLength(100)
                 .required(false),
             string(BODY)
-                .label("Description")
-                .description("The description of the pull request.")
+                .label("Body")
+                .description("The contents of the pull request.")
+                .controlType(ControlType.TEXT_AREA)
                 .required(false),
             string(HEAD)
                 .label("Head")
-                .description("The name of the branch where your changes are implemented.")
+                .description(
+                    "The name of the branch where your changes are implemented. For cross-repository pull requests " +
+                        "in the same network, namespace head with a user like this: username:branch.")
                 .required(true),
             string(HEAD_REPO)
                 .label("Head Repo")
-                .description("The name of the repository where the changes in the pull request were made.")
+                .description(
+                    "The name of the repository where the changes in the pull request were made. This field is " +
+                        "required for cross-repository pull requests if both repositories are owned by the same " +
+                        "organization.")
                 .required(false),
             string(BASE)
                 .label("Base")
-                .description("The name of the branch you want the changes pulled into.")
+                .description(
+                    "The name of the branch you want the changes pulled into. This should be an existing branch " +
+                        "on the current repository. You cannot submit a pull request to one repository that " +
+                        "requests a merge to a base of another repository.")
                 .required(true),
             integer(ISSUE)
                 .label("Issue")
-                .description("An issue in the repository to convert to a pull request.")
+                .description(
+                    "An issue in the repository to convert to a pull request. The issue title, body, " +
+                        "and comments will become the title, body, and comments on the new pull request. " +
+                        "Required unless title is specified.")
                 .required(false),
             bool(DRAFT)
                 .label("Draft")
                 .description("Indicates whether the pull request is a draft.")
                 .defaultValue(false)
-                .required(true)
-        )
-        .output(outputSchema(PULL_REQUEST_OUTPUT_PROBERTY))
+                .required(true))
+        .output(
+            outputSchema(
+                object()
+                    .properties(
+                        string("url")
+                            .description("The URL of the created pull request."),
+                        integer(ID)
+                            .description("ID of the created pull request."),
+                        string("node_id"),
+                        string("html_url"),
+                        string("diff_url"),
+                        string("patch_url"),
+                        string("issue_url"),
+                        string("commits_url"),
+                        string("review_comment_url"),
+                        string("comments_url"),
+                        string("statuses_url"),
+                        integer("number")
+                            .description("Number uniquely identifying the pull request within its repository."),
+                        string("state")
+                            .description("The state of the pull request. Either open or closed."),
+                        bool("locked")
+                            .description(""),
+                        string("title")
+                            .description("The title of the pull request."),
+                        object("user")
+                            .description("A GitHub user.")
+                            .properties(
+                                string("login")
+                                    .description("The username of the assignee."),
+                                string(ID)
+                                    .description("ID of the assignee."),
+                                string("html_url")
+                                    .description("The URL to the assignee's profile page."),
+                                string("type")
+                                    .description("The type of user, e.g., User or Organization.")),
+                        string("body")
+                            .description("The contents of the pull request."),
+                        array("labels")
+                            .items(
+                                object()
+                                    .properties(
+                                        string(ID)
+                                            .description("ID of the label."),
+                                        string(NAME)
+                                            .description("Name of the label"),
+                                        string("description")
+                                            .description("A brief description of the label's purpose."))),
+                        integer("comments")
+                            .description("The number of comments on the pull request."),
+                        integer("review_comments")
+                            .description("The number of comments for review on the pull request."),
+                        bool("maintainer_can_modify")
+                            .description("Indicates whether maintainers can modify the pull request."),
+                        integer("commits")
+                            .description("The number of commits in the pull request."),
+                        integer("additions")
+                            .description("The number of additions in the pull request."),
+                        integer("deletions")
+                            .description("The number of deletions in the pull request."),
+                        integer("changed_files")
+                            .description("The number of changed files in the pull request."))))
         .perform(GitHubCreatePullRequestAction::perform);
 
     private GitHubCreatePullRequestAction() {
@@ -81,9 +174,10 @@ public class GitHubCreatePullRequestAction {
 
         return context
             .http(http -> http.post(
-                "/repos/" + getOwnerName(context) + "/" + inputParameters.getRequiredString(REPOSITORY) + "/pulls"))
+                "/repos/%s/%s/pulls".formatted(
+                    inputParameters.getRequiredString(OWNER), inputParameters.getRequiredString(REPOSITORY))))
             .body(
-                Context.Http.Body.of(
+                Http.Body.of(
                     TITLE, inputParameters.getString(TITLE),
                     HEAD, inputParameters.getRequiredString(HEAD),
                     HEAD_REPO, inputParameters.getString(HEAD_REPO),
@@ -91,9 +185,8 @@ public class GitHubCreatePullRequestAction {
                     BODY, inputParameters.getString(BODY),
                     DRAFT, inputParameters.getBoolean(DRAFT),
                     ISSUE, inputParameters.getInteger(ISSUE)))
-            .configuration(responseType(Context.Http.ResponseType.JSON))
+            .configuration(responseType(Http.ResponseType.JSON))
             .execute()
-            .getBody(new TypeReference<>() {
-            });
+            .getBody(new TypeReference<>() {});
     }
 }
