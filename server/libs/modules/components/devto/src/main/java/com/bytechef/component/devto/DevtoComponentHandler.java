8bf6796313b2bb28c1e4ffbde64abeb1424fa3a3
Monika Kušter <domitermonika@gmail.com>
2025-12-04 08:04:17 +0100

    3612 add icon, article id options, fix connection

--- server/libs/modules/components/devto/src/main/java/com/bytechef/component/devto/DevtoComponentHandler.java
+++ server/libs/modules/components/devto/src/main/java/com/bytechef/component/devto/DevtoComponentHandler.java
@@ -16,12 +16,52 @@
 
 package com.bytechef.component.devto;
 
+import static com.bytechef.component.definition.Authorization.VALUE;
+
 import com.bytechef.component.OpenApiComponentHandler;
+import com.bytechef.component.definition.Authorization;
+import com.bytechef.component.definition.Authorization.ApplyResponse;
+import com.bytechef.component.definition.ComponentCategory;
+import com.bytechef.component.definition.ComponentDsl.ModifiableAuthorization;
+import com.bytechef.component.definition.ComponentDsl.ModifiableComponentDefinition;
+import com.bytechef.component.definition.ComponentDsl.ModifiableConnectionDefinition;
 import com.google.auto.service.AutoService;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 /**
- * This class will not be overwritten on the subsequent calls of the generator.
+ * @author Monika Kušter
  */
 @AutoService(OpenApiComponentHandler.class)
 public class DevtoComponentHandler extends AbstractDevtoComponentHandler {
+
+    @Override
+    public ModifiableComponentDefinition modifyComponent(ModifiableComponentDefinition modifiableComponentDefinition) {
+        return modifiableComponentDefinition
+            .customAction(true)
+            .icon("path:assets/devto.svg")
+            .categories(ComponentCategory.PRODUCTIVITY_AND_COLLABORATION);
+    }
+
+    @Override
+    public ModifiableConnectionDefinition modifyConnection(
+        ModifiableConnectionDefinition modifiableConnectionDefinition) {
+
+        Optional<List<? extends Authorization>> optionalAuthorizations =
+            modifiableConnectionDefinition.getAuthorizations();
+
+        if (optionalAuthorizations.isPresent()) {
+            List<? extends Authorization> authorizations = optionalAuthorizations.get();
+            ModifiableAuthorization modifiableAuthorization = (ModifiableAuthorization) authorizations.getFirst();
+
+            modifiableAuthorization.apply((connectionParameters, context) -> ApplyResponse.ofHeaders(
+                Map.of(
+                    "accept", List.of("application/vnd.forem.api-v1+json"),
+                    "api-key", List.of(connectionParameters.getRequiredString(VALUE)),
+                    "User-Agent", List.of("ByteChef"))));
+        }
+
+        return modifiableConnectionDefinition;
+    }
 }
