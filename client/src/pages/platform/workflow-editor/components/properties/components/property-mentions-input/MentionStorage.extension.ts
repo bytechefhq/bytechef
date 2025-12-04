eb24cf5453f6c47d6c20e26d5df2ce210361adfc
Ivica Cardic <ivica.cardic@gmail.com>
2025-12-04 21:39:21 +0100

    3662 client - Refactor @tiptap usage and storage extensions for improved compatibility and clarity

--- client/src/pages/platform/workflow-editor/components/properties/components/property-mentions-input/MentionStorage.extension.ts
+++ client/src/pages/platform/workflow-editor/components/properties/components/property-mentions-input/MentionStorage.extension.ts
@@ -1,5 +1,16 @@
 import {Extension} from '@tiptap/core';
 
+import type {DataPillType} from '@/shared/types';
+
+declare module '@tiptap/core' {
+    // eslint-disable-next-line  @typescript-eslint/naming-convention
+    interface Storage {
+        MentionStorage: {
+            dataPills: DataPillType[];
+        };
+    }
+}
+
 export const MentionStorage = Extension.create({
     addStorage() {
         return {
