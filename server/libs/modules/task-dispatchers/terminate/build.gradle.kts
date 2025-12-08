dea2a07ce2745f57bb9d2bb15559e15b1a68a6f6
Matija Petanjek <matija.petanjek7@gmail.com>
2025-12-08 10:09:18 +0100

    515 - test build.gradle

--- server/libs/modules/task-dispatchers/terminate/build.gradle.kts
+++ server/libs/modules/task-dispatchers/terminate/build.gradle.kts
@@ -1,1 +1,8 @@
 version="1.0"
+
+dependencies {
+    testImplementation(project(":server:libs:atlas:atlas-coordinator:atlas-coordinator-impl"))
+    testImplementation(project(":server:libs:modules:task-dispatchers:condition"))
+    testImplementation(project(":server:libs:modules:task-dispatchers:loop"))
+    testImplementation(project(":server:libs:modules:task-dispatchers:on-error"))
+}
