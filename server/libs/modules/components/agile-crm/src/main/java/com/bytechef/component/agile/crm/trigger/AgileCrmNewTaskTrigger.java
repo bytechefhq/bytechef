9a4868c89b1185bc47adafc80a327aabf46abf25
Nikolina Spehar <speharnikolina87@gmail.com>
2025-12-09 16:12:07 +0100

    2352 - added NewTaskTrigger

---
+++ server/libs/modules/components/agile-crm/src/main/java/com/bytechef/component/agile/crm/trigger/AgileCrmNewTaskTrigger.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright 2025 ByteChef
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef.component.agile.crm.trigger;
+
+import static com.bytechef.component.agile.crm.constant.AgileCrmConstants.CREATED_TIME;
+import static com.bytechef.component.agile.crm.constant.AgileCrmConstants.LAST_TIME_CHECKED;
+import static com.bytechef.component.definition.ComponentDsl.object;
+import static com.bytechef.component.definition.ComponentDsl.outputSchema;
+import static com.bytechef.component.definition.ComponentDsl.trigger;
+import static com.bytechef.component.definition.Context.Http.responseType;
+
+import com.bytechef.component.definition.ComponentDsl.ModifiableTriggerDefinition;
+import com.bytechef.component.definition.Context.Http.ResponseType;
+import com.bytechef.component.definition.Parameters;
+import com.bytechef.component.definition.TriggerContext;
+import com.bytechef.component.definition.TriggerDefinition.PollOutput;
+import com.bytechef.component.definition.TriggerDefinition.TriggerType;
+import com.bytechef.component.definition.TypeReference;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author Nikolina Spehar
+ */
+public class AgileCrmNewTaskTrigger {
+    public static final ModifiableTriggerDefinition TRIGGER_DEFINITION = trigger("newTask")
+        .title("New Task")
+        .description("Triggers when a new task is added.")
+        .type(TriggerType.POLLING)
+        .properties()
+        .output(
+            outputSchema(object()))
+        .poll(AgileCrmNewTaskTrigger::poll);
+
+    private AgileCrmNewTaskTrigger() {
+    }
+
+    protected static PollOutput poll(
+        Parameters inputParameters, Parameters connectionParameters, Parameters closureParameters,
+        TriggerContext triggerContext) {
+
+        ZoneId zoneId = ZoneId.systemDefault();
+
+        LocalDateTime now = LocalDateTime.now(zoneId);
+
+        LocalDateTime lastTimeChecked = closureParameters.getLocalDateTime(
+            LAST_TIME_CHECKED, triggerContext.isEditorEnvironment() ? now.minusHours(3) : now);
+
+        long lastTimeCheckedEpoch = lastTimeChecked.atZone(zoneId)
+            .toEpochSecond();
+
+        List<Map<String, Object>> tasks = triggerContext.http(http -> http.get("/tasks/based"))
+            .configuration(responseType(ResponseType.JSON))
+            .execute()
+            .getBody(new TypeReference<>() {});
+
+        List<Map<String, Object>> newTasks = new ArrayList<>();
+
+        for (Map<String, Object> task : tasks) {
+            if ((Integer) task.get(CREATED_TIME) >= lastTimeCheckedEpoch) {
+                newTasks.add(task);
+            }
+        }
+
+        return new PollOutput(newTasks, Map.of(LAST_TIME_CHECKED, now), false);
+    }
+}
