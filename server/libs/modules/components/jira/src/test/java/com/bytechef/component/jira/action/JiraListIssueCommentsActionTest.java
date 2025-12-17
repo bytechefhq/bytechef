ce20b33887dcdb21f4d463569458c1f1b2795dae
ivonapavela <92436197+ivonapavela@users.noreply.github.com>
2025-12-17 16:51:51 +0100

    3182 - adding test

---
+++ server/libs/modules/components/jira/src/test/java/com/bytechef/component/jira/action/JiraListIssueCommentsActionTest.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright 2025 ByteChef
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef.component.jira.action;
+
+import static com.bytechef.component.jira.constant.JiraConstants.ISSUE_ID;
+import static com.bytechef.component.jira.constant.JiraConstants.MAX_RESULTS;
+import static com.bytechef.component.jira.constant.JiraConstants.ORDER_BY;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentCaptor.forClass;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import com.bytechef.component.definition.ActionContext;
+import com.bytechef.component.definition.Context;
+import com.bytechef.component.definition.Context.ContextFunction;
+import com.bytechef.component.definition.Context.Http;
+import com.bytechef.component.definition.Context.Http.Configuration.ConfigurationBuilder;
+import com.bytechef.component.definition.Parameters;
+import com.bytechef.component.definition.TypeReference;
+import com.bytechef.component.test.definition.MockParametersFactory;
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+
+/**
+ * @author Ivona Pavela
+ */
+class JiraListIssueCommentsActionTest {
+
+    private final ArgumentCaptor<ConfigurationBuilder> configurationBuilderArgumentCaptor =
+        forClass(ConfigurationBuilder.class);
+    @SuppressWarnings("unchecked")
+    private final ArgumentCaptor<ContextFunction<Http, Http.Executor>> httpFunctionArgumentCaptor =
+        forClass(ContextFunction.class);
+    private final ActionContext mockedActionContext = mock(ActionContext.class);
+    private final Http.Executor mockedExecutor = mock(Http.Executor.class);
+    private final Http mockedHttp = mock(Http.class);
+    private final Parameters mockedParameters = MockParametersFactory.create(
+        Map.of(
+            ISSUE_ID, "xy",
+            ORDER_BY, "+created",
+            MAX_RESULTS, 50));
+    private final Http.Response mockedResponse = mock(Http.Response.class);
+    private final Map<String, Object> responseMap = Map.of("comments", "example");
+    private final ArgumentCaptor<String> stringArgumentCaptor = forClass(String.class);
+
+    @Test
+    void testPerform() {
+        when(mockedActionContext.http(httpFunctionArgumentCaptor.capture()))
+            .thenAnswer(_ -> httpFunctionArgumentCaptor.getValue()
+                .apply(mockedHttp));
+        when(mockedHttp.get(stringArgumentCaptor.capture()))
+            .thenReturn(mockedExecutor);
+        when(mockedExecutor.configuration(configurationBuilderArgumentCaptor.capture()))
+            .thenReturn(mockedExecutor);
+        when(mockedExecutor.queryParameters(ORDER_BY, "+created", MAX_RESULTS, 50))
+            .thenReturn(mockedExecutor);
+        when(mockedExecutor.execute())
+            .thenReturn(mockedResponse);
+        when(mockedResponse.getBody(any(TypeReference.class)))
+            .thenReturn(responseMap);
+
+        Object result = JiraListIssueCommentsAction.perform(mockedParameters, null, mockedActionContext);
+        assertEquals(responseMap, result);
+
+        ContextFunction<Context.Http, Http.Executor> capturedFunction = httpFunctionArgumentCaptor.getValue();
+
+        assertNotNull(capturedFunction);
+
+        ConfigurationBuilder configurationBuilder = configurationBuilderArgumentCaptor.getValue();
+
+        Http.Configuration configuration = configurationBuilder.build();
+
+        Http.ResponseType responseType = configuration.getResponseType();
+
+        assertEquals(Http.ResponseType.Type.JSON, responseType.getType());
+
+        assertEquals("/issue/xy/comment", stringArgumentCaptor.getValue());
+
+    }
+
+}
