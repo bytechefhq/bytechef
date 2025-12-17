3a1480a1744287df5965834e9e8cb817467c73b1
ivonapavela <92436197+ivonapavela@users.noreply.github.com>
2025-12-17 16:51:30 +0100

    3182 - adding List Issue Comments action

---
+++ server/libs/modules/components/jira/src/main/java/com/bytechef/component/jira/action/JiraListIssueCommentsAction.java
@@ -0,0 +1,105 @@
+/*
+ * Copyright 2025 ByteChef
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef.component.jira.action;
+
+import static com.bytechef.component.definition.ComponentDsl.action;
+import static com.bytechef.component.definition.ComponentDsl.array;
+import static com.bytechef.component.definition.ComponentDsl.integer;
+import static com.bytechef.component.definition.ComponentDsl.object;
+import static com.bytechef.component.definition.ComponentDsl.option;
+import static com.bytechef.component.definition.ComponentDsl.outputSchema;
+import static com.bytechef.component.definition.ComponentDsl.string;
+import static com.bytechef.component.jira.constant.JiraConstants.ISSUE_ID;
+import static com.bytechef.component.jira.constant.JiraConstants.MAX_RESULTS;
+import static com.bytechef.component.jira.constant.JiraConstants.ORDER_BY;
+import static com.bytechef.component.jira.constant.JiraConstants.PROJECT;
+
+import com.bytechef.component.definition.ActionDefinition;
+import com.bytechef.component.definition.ComponentDsl.ModifiableActionDefinition;
+import com.bytechef.component.definition.Context;
+import com.bytechef.component.definition.Context.Http;
+import com.bytechef.component.definition.Parameters;
+import com.bytechef.component.definition.TypeReference;
+import com.bytechef.component.jira.util.JiraOptionsUtils;
+
+/**
+ * @author Ivona Pavela
+ */
+public class JiraListIssueCommentsAction {
+
+    public static final ModifiableActionDefinition ACTION_DEFINITION = action("listIssueComments")
+        .title("List Issue Comments")
+        .description("Return all comments for an issue.")
+        .properties(
+            string(PROJECT)
+                .label("Project ID")
+                .description("ID of the project where the issue is located.")
+                .options((ActionDefinition.OptionsFunction<String>) JiraOptionsUtils::getProjectIdOptions)
+                .required(false),
+            string(ISSUE_ID)
+                .label("Issue ID")
+                .description("ID of the issue.")
+                .options((ActionDefinition.OptionsFunction<String>) JiraOptionsUtils::getIssueIdOptions)
+                .optionsLookupDependsOn(PROJECT)
+                .required(true),
+            string(ORDER_BY)
+                .label("Order By")
+                .description("Order the results by a field.")
+                .options(
+                    option("Created", "created", "Order by created date."),
+                    option("+Created", "+created", "Order ascending by created date."),
+                    option("-Created", "-created", "Order descending by created date."))
+                .required(false),
+            integer(MAX_RESULTS)
+                .label("Max Results")
+                .description("The maximum number of items to return per page.")
+                .required(false))
+        .output(
+            outputSchema(
+                object().properties(
+                    integer("maxResults"),
+                    integer("startAt"),
+                    integer("total"),
+                    array("comments")
+                        .label("Comments")
+                        .description("List of comments on the issue")
+                        .items(
+                            object().properties(
+                                string("id").label("Comment ID"),
+                                string("self").label("API URL for the comment"),
+                                object("body").properties(
+                                    array("content").items(
+                                        object().properties(
+                                            string("text")))),
+                                object("author").properties(
+                                    string("displayName")),
+                                string("created"),
+                                string("updated"))))))
+        .perform(JiraListIssueCommentsAction::perform);
+
+    public static Object perform(Parameters inputParameters, Parameters connectionParameters, Context context) {
+
+        return context
+            .http(http -> http.get("/issue/" + inputParameters.getRequiredString(ISSUE_ID) + "/comment"))
+            .configuration(Http.responseType(Http.ResponseType.JSON))
+            .queryParameters(
+                ORDER_BY, inputParameters.getString(ORDER_BY),
+                MAX_RESULTS, inputParameters.getInteger(MAX_RESULTS))
+            .execute()
+            .getBody(new TypeReference<>() {});
+    }
+}
