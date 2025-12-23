b40f1e9a98d348fc369b13ee424eb83d8f23b14a
mkriskovic <marko.kriskovic@protonmail.com>
2025-12-23 14:53:49 +0100

    2085 - added ScrapeGraphAI actions StartCrawl and GetCrawlStatus

--- server/libs/modules/components/scrape-graph-ai/src/main/java/com/bytechef/component/scrape/graph/ai/AbstractScrapeGraphAiComponentHandler.java
+++ server/libs/modules/components/scrape-graph-ai/src/main/java/com/bytechef/component/scrape/graph/ai/AbstractScrapeGraphAiComponentHandler.java
@@ -20,9 +20,11 @@ import static com.bytechef.component.definition.ComponentDsl.component;
 
 import com.bytechef.component.OpenApiComponentHandler;
 import com.bytechef.component.definition.ComponentDefinition;
+import com.bytechef.component.scrape.graph.ai.action.ScrapeGraphAiGetCrawlStatusAction;
 import com.bytechef.component.scrape.graph.ai.action.ScrapeGraphAiMarkdownifyAction;
 import com.bytechef.component.scrape.graph.ai.action.ScrapeGraphAiSearchScraperAction;
 import com.bytechef.component.scrape.graph.ai.action.ScrapeGraphAiSmartScraperAction;
+import com.bytechef.component.scrape.graph.ai.action.ScrapeGraphAiStartCrawlAction;
 import com.bytechef.component.scrape.graph.ai.connection.ScrapeGraphAiConnection;
 
 /**
@@ -38,7 +40,9 @@ public abstract class AbstractScrapeGraphAiComponentHandler implements OpenApiCo
                 "ScrapeGraphAI is a web scraping python library that uses LLM and direct graph logic to create scraping pipelines for websites and local documents."))
                     .actions(modifyActions(ScrapeGraphAiSearchScraperAction.ACTION_DEFINITION,
                         ScrapeGraphAiMarkdownifyAction.ACTION_DEFINITION,
-                        ScrapeGraphAiSmartScraperAction.ACTION_DEFINITION))
+                        ScrapeGraphAiSmartScraperAction.ACTION_DEFINITION,
+                        ScrapeGraphAiStartCrawlAction.ACTION_DEFINITION,
+                        ScrapeGraphAiGetCrawlStatusAction.ACTION_DEFINITION))
                     .connection(modifyConnection(ScrapeGraphAiConnection.CONNECTION_DEFINITION))
                     .triggers(getTriggers());
 
