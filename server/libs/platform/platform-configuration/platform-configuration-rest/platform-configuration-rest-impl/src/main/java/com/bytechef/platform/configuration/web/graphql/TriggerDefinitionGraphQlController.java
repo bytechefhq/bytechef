51133b3b2f38d669ccea8c5f200a5f680b29e5d1
Ivica Cardic <ivica.cardic@gmail.com>
2025-12-27 13:10:53 +0100

    3827 Add GraphQL schema and controller for TriggerDefinition

---
+++ server/libs/platform/platform-configuration/platform-configuration-rest/platform-configuration-rest-impl/src/main/java/com/bytechef/platform/configuration/web/graphql/TriggerDefinitionGraphQlController.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2025 ByteChef
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef.platform.configuration.web.graphql;
+
+import com.bytechef.atlas.coordinator.annotation.ConditionalOnCoordinator;
+import com.bytechef.platform.component.domain.TriggerDefinition;
+import com.bytechef.platform.component.service.TriggerDefinitionService;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
+
+/**
+ * Exposes TriggerDefinition over GraphQL.
+ *
+ * @author Ivica Cardic
+ */
+@Controller
+@ConditionalOnCoordinator
+public class TriggerDefinitionGraphQlController {
+
+    private final TriggerDefinitionService triggerDefinitionService;
+
+    public TriggerDefinitionGraphQlController(TriggerDefinitionService triggerDefinitionService) {
+        this.triggerDefinitionService = triggerDefinitionService;
+    }
+
+    @QueryMapping(name = "triggerDefinition")
+    public TriggerDefinition triggerDefinition(
+        @Argument String componentName,
+        @Argument Integer componentVersion,
+        @Argument String triggerName) {
+
+        return triggerDefinitionService.getTriggerDefinition(componentName, componentVersion, triggerName);
+    }
+}
