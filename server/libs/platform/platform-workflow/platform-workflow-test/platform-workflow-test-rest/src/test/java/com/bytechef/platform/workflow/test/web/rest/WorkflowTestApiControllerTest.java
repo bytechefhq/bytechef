9786b77a091498b5ad4076f58755e0fe06aff8c4
Ivica Cardic <ivica.cardic@gmail.com>
2025-12-29 19:46:07 +0100

    682 Fix potential race condition where the future is cancelled before 'awaitTestResult' completes, which could leave resources in an inconsistent state

--- server/libs/platform/platform-workflow/platform-workflow-test/platform-workflow-test-rest/src/test/java/com/bytechef/platform/workflow/test/web/rest/WorkflowTestApiControllerTest.java
+++ server/libs/platform/platform-workflow/platform-workflow-test/platform-workflow-test-rest/src/test/java/com/bytechef/platform/workflow/test/web/rest/WorkflowTestApiControllerTest.java
@@ -48,7 +48,6 @@ import java.util.List;
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentMap;
-import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicReference;
