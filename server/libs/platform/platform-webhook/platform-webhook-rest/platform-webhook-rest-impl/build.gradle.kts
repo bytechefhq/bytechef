d4b5739481739d4a4d5febf6db0cec20bc8ee70c
Ivica Cardic <ivica.cardic@gmail.com>
2026-01-08 22:13:14 +0100

    2311 Streaming - Update dependencies

--- server/libs/platform/platform-webhook/platform-webhook-rest/platform-webhook-rest-impl/build.gradle.kts
+++ server/libs/platform/platform-webhook/platform-webhook-rest/platform-webhook-rest-impl/build.gradle.kts
@@ -3,6 +3,7 @@ dependencies {
 
     implementation("org.slf4j:slf4j-api")
     implementation("org.springframework:spring-web")
+    implementation("org.springframework:spring-webmvc")
     implementation("org.springframework.boot:spring-boot-autoconfigure")
     implementation(project(":server:libs:atlas:atlas-coordinator:atlas-coordinator-api"))
     implementation(project(":server:libs:config:app-config"))
