d6f3770f6c1aa486fdb679c4ce99cd1cc29a3784
Ivica Cardic <ivica.cardic@gmail.com>
2026-01-08 23:28:35 +0100

    3847 SF

--- server/libs/platform/platform-configuration/platform-configuration-service/src/test/java/com/bytechef/platform/configuration/facade/WorkflowNodeParameterFacadeTest.java
+++ server/libs/platform/platform-configuration/platform-configuration-service/src/test/java/com/bytechef/platform/configuration/facade/WorkflowNodeParameterFacadeTest.java
@@ -58,7 +58,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 import org.apache.commons.lang3.RandomStringUtils;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
