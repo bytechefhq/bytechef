ad336f177bac3d6ba1e6ab209428153a8a68aa1a
katarina-cala <katarinacala@gmail.com>
2026-01-12 15:00:10 +0100

    3589-fixed focus colour for accordion items

--- client/src/shared/components/workflow-executions/WorkflowExecutionsTriggerAccordionItem.tsx
+++ client/src/shared/components/workflow-executions/WorkflowExecutionsTriggerAccordionItem.tsx
@@ -16,7 +16,7 @@ const WorkflowExecutionsTriggerAccordionItem = ({
         <AccordionItem className="border-b-0 pl-2" value={triggerExecution.id || ''}>
             <AccordionTrigger
                 className={twMerge(
-                    'group flex w-full items-center justify-between rounded-md border border-stroke-neutral-primary p-2 hover:border-stroke-brand-primary hover:no-underline [&[data-state=closed]>svg]:hidden [&[data-state=open]>svg]:hidden [&[data-state=open]]:border-stroke-brand-primary [&[data-state=open]]:hover:border-stroke-brand-secondary',
+                    'group flex w-full items-center justify-between rounded-md border border-stroke-neutral-primary p-2 hover:border-stroke-brand-primary hover:no-underline focus-visible:outline-stroke-brand-focus focus-visible:transition-colors [&[data-state=closed]>svg]:hidden [&[data-state=open]>svg]:hidden [&[data-state=open]]:border-stroke-brand-primary [&[data-state=open]]:hover:border-stroke-brand-secondary',
                     selectedItem?.id === triggerExecution.id &&
                         'border-stroke-brand-primary bg-surface-neutral-secondary hover:bg-surface-neutral-secondary [&[data-state=open]]:border-stroke-brand-primary'
                 )}
