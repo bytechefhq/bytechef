fe0c94753b262779806015aeacb6950f1721d0d4
Ivica Cardic <ivica.cardic@gmail.com>
2026-01-14 18:47:03 +0100

    2680 sdk - Update prettier configuration

--- sdks/frontend/embedded/library/react/vite.config.ts
+++ sdks/frontend/embedded/library/react/vite.config.ts
@@ -30,14 +30,14 @@ export default defineConfig({
                 globSync(['src/components/**/index.{ts,tsx}', 'src/main.ts'])
                     .filter((file) => !file.includes('.test.'))
                     .map((file) => {
-                    // This removes `src/` as well as the file extension from each
-                    // file, so e.g. src/nested/foo.js becomes nested/foo
-                    const entryName = path.relative('src', file.slice(0, file.length - path.extname(file).length));
-                    // This expands the relative paths to absolute paths, so e.g.
-                    // src/nested/foo becomes /project/src/nested/foo.js
-                    const entryUrl = fileURLToPath(new URL(file, import.meta.url));
-                    return [entryName, entryUrl];
-                })
+                        // This removes `src/` as well as the file extension from each
+                        // file, so e.g. src/nested/foo.js becomes nested/foo
+                        const entryName = path.relative('src', file.slice(0, file.length - path.extname(file).length));
+                        // This expands the relative paths to absolute paths, so e.g.
+                        // src/nested/foo becomes /project/src/nested/foo.js
+                        const entryUrl = fileURLToPath(new URL(file, import.meta.url));
+                        return [entryName, entryUrl];
+                    })
             ),
             output: {
                 entryFileNames: '[name].js',
