17df31da8fa4d1442cef50b9bbf2f7060bb068f8
Krešimir Čoko <kresimircoko@gmail.com>
2026-01-14 15:44:08 +0100

    3873 - add playwright utils

--- client/test/playwright/utils/constants.ts
+++ client/test/playwright/utils/constants.ts
@@ -8,21 +8,6 @@ export const TEST_USER = {
     password: 'admin',
 } as const;
 
-export const TEST_PROJECT = {
-    name: 'Test Project',
-} as const;
-
-export const TEST_WORKFLOW = {
-    name: 'Test Workflow',
-} as const;
-
-export const TIMEOUTS = {
-    dialog: 5000,
-    elementVisible: 10000,
-    navigation: 10000,
-    networkIdle: 10000,
-} as const;
-
 export const ROUTES = {
     login: '/login',
     projectWorkflows: (projectId: number, workflowId: number) =>
