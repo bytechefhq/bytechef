d17a1625ed99003e562db1d3bea9e0f41e863799
Krešimir Čoko <kresimircoko@gmail.com>
2026-01-17 16:01:02 +0100

    3439 - improve Table Pagination page navigation rendering

--- client/src/components/TablePagination.tsx
+++ client/src/components/TablePagination.tsx
@@ -68,29 +68,30 @@ const TablePagination = ({onClick, pageNumber, pageSize, totalElements, totalPag
                     </p>
                 </div>
 
-                <PaginationItem>
-                    <PaginationPrevious
-                        href="#"
-                        onClick={() => {
-                            if (pageNumber > 0) {
-                                onClick(pageNumber - 1);
-                            }
-                        }}
-                    />
-                </PaginationItem>
+                {totalPages > 1 && (
+                    <>
+                        {pageNumber > 0 && (
+                            <PaginationItem>
+                                <PaginationPrevious href="#" onClick={() => onClick(pageNumber - 1)} />
+                            </PaginationItem>
+                        )}
 
-                {renderPageLinks()}
+                        {renderPageLinks()}
 
-                <PaginationItem>
-                    <PaginationNext
-                        href="#"
-                        onClick={() => {
-                            if (pageNumber < totalPages - 1) {
-                                onClick(pageNumber + 1);
-                            }
-                        }}
-                    />
-                </PaginationItem>
+                        {pageNumber < totalPages - 1 && (
+                            <PaginationItem>
+                                <PaginationNext
+                                    href="#"
+                                    onClick={() => {
+                                        if (pageNumber < totalPages - 1) {
+                                            onClick(pageNumber + 1);
+                                        }
+                                    }}
+                                />
+                            </PaginationItem>
+                        )}
+                    </>
+                )}
             </PaginationContent>
         </Pagination>
     );
