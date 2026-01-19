8dfabaf40c87b7675433e987359a66d658db7d55
Monika Kušter <domitermonika@gmail.com>
2026-01-19 08:42:15 +0100

    2839 update tests

--- server/libs/modules/components/heygen/src/test/java/com/bytechef/component/heygen/action/HeyGenGenerateVideoFromTemplateActionTest.java
+++ server/libs/modules/components/heygen/src/test/java/com/bytechef/component/heygen/action/HeyGenGenerateVideoFromTemplateActionTest.java
@@ -21,12 +21,17 @@ import static com.bytechef.component.heygen.constant.HeyGenConstants.ENABLE_SHAR
 import static com.bytechef.component.heygen.constant.HeyGenConstants.FOLDER_ID;
 import static com.bytechef.component.heygen.constant.HeyGenConstants.TEMPLATE_ID;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentCaptor.forClass;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import com.bytechef.component.definition.Context;
+import com.bytechef.component.definition.Context.ContextFunction;
+import com.bytechef.component.definition.Context.Http;
 import com.bytechef.component.definition.Context.Http.Body;
+import com.bytechef.component.definition.Context.Http.Configuration.ConfigurationBuilder;
 import com.bytechef.component.definition.Context.Http.Executor;
 import com.bytechef.component.definition.Context.Http.Response;
 import com.bytechef.component.definition.Parameters;
@@ -41,35 +46,58 @@ import org.mockito.ArgumentCaptor;
  */
 class HeyGenGenerateVideoFromTemplateActionTest {
 
-    private final ArgumentCaptor<Body> bodyArgumentCaptor = ArgumentCaptor.forClass(Body.class);
+    private final ArgumentCaptor<Body> bodyArgumentCaptor = forClass(Body.class);
+    private final ArgumentCaptor<ConfigurationBuilder> configurationBuilderArgumentCaptor =
+        forClass(ConfigurationBuilder.class);
+    @SuppressWarnings("unchecked")
+    private final ArgumentCaptor<ContextFunction<Http, Executor>> httpFunctionArgumentCaptor =
+        forClass(ContextFunction.class);
     private final Context mockedContext = mock(Context.class);
     private final Executor mockedExecutor = mock(Executor.class);
+    private final Http mockedHttp = mock(Http.class);
     private final Object mockedObject = mock(Object.class);
     private final Response mockedResponse = mock(Response.class);
     private final Parameters mockedParameters = MockParametersFactory.create(
         Map.of(TEMPLATE_ID, "1", FOLDER_ID, "2", CAPTION, false, ENABLE_SHARING, false));
+    private final ArgumentCaptor<String> stringArgumentCaptor = forClass(String.class);
 
     @Test
     void testPerform() {
-        when(mockedContext.http(any()))
+        when(mockedContext.http(httpFunctionArgumentCaptor.capture()))
+            .thenAnswer(inv -> {
+                ContextFunction<Http, Http.Executor> value = httpFunctionArgumentCaptor.getValue();
+
+                return value.apply(mockedHttp);
+            });
+        when(mockedHttp.post(stringArgumentCaptor.capture()))
             .thenReturn(mockedExecutor);
         when(mockedExecutor.body(bodyArgumentCaptor.capture()))
             .thenReturn(mockedExecutor);
-        when(mockedExecutor.configuration(any()))
+        when(mockedExecutor.configuration(configurationBuilderArgumentCaptor.capture()))
             .thenReturn(mockedExecutor);
         when(mockedExecutor.execute())
             .thenReturn(mockedResponse);
         when(mockedResponse.getBody(any(TypeReference.class)))
             .thenReturn(Map.of("data", mockedObject));
 
-        Object result =
-            HeyGenGenerateVideoFromTemplateAction.perform(mockedParameters, null, mockedContext);
+        Object result = HeyGenGenerateVideoFromTemplateAction.perform(
+            mockedParameters, null, mockedContext);
 
         assertEquals(mockedObject, result);
 
-        Body body = bodyArgumentCaptor.getValue();
-        Map<String, Object> expectedBody = Map.of(FOLDER_ID, "2", CAPTION, false, ENABLE_SHARING, false);
+        ContextFunction<Http, Http.Executor> capturedFunction = httpFunctionArgumentCaptor.getValue();
 
-        assertEquals(expectedBody, body.getContent());
+        assertNotNull(capturedFunction);
+
+        ConfigurationBuilder configurationBuilder = configurationBuilderArgumentCaptor.getValue();
+        Http.Configuration configuration = configurationBuilder.build();
+        Http.ResponseType responseType = configuration.getResponseType();
+
+        assertEquals(Http.ResponseType.Type.JSON, responseType.getType());
+        assertEquals("https://api.heygen.com/v2/template/1/generate", stringArgumentCaptor.getValue());
+        assertEquals(
+            Http.Body.of(
+                Map.of(FOLDER_ID, "2", CAPTION, false, ENABLE_SHARING, false), Http.BodyContentType.JSON),
+            bodyArgumentCaptor.getValue());
     }
 }
