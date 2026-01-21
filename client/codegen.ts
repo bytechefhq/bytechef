b33c505a09d2d180a3aa7c53f91a9abdafe9fc95
Ivica Cardic <ivica.cardic@gmail.com>
2026-01-21 06:15:54 +0100

    3955 DataTables - client - Update GraphQL schema paths in codegen configuration

--- client/codegen.ts
+++ client/codegen.ts
@@ -3,14 +3,17 @@ import {getCookie} from './src/shared/util/cookie-utils';
 
 const config: CodegenConfig = {
     schema: [
-        {
-            'http://localhost:9555/graphql': {
-                headers: {
-                    Authorization: 'Basic YWRtaW5AbG9jYWxob3N0LmNvbTphZG1pbg==',
-                    'Content-Type': 'application/json',
-                },
-            },
-        },
+        '../server/libs/platform/platform-configuration/platform-configuration-graphql/src/main/resources/graphql/*.graphqls',
+        '../server/libs/automation/automation-data-table/**/src/main/resources/graphql/*.graphqls',
+        '../server/libs/platform/platform-mcp/platform-mcp-graphql/src/main/resources/graphql/*.graphqls',
+        '../server/libs/automation/automation-mcp/automation-mcp-graphql/src/main/resources/graphql/*.graphqls',
+        '../server/libs/automation/automation-configuration/automation-configuration-graphql/src/main/resources/graphql/*.graphqls',
+        '../server/libs/automation/automation-task/automation-task-graphql/src/main/resources/graphql/*.graphqls',
+        '../server/libs/platform/platform-security/platform-security-graphql/src/main/resources/graphql/*.graphqls',
+        '../server/libs/ai/mcp/mcp-server-configuration/mcp-server-configuration-graphql/src/main/resources/graphql/*.graphqls',
+        '../server/ee/libs/platform/platform-user/platform-user-graphql/src/main/resources/graphql/*.graphqls',
+        '../server/ee/libs/embedded/embedded-configuration/embedded-configuration-graphql/src/main/resources/graphql/*.graphqls',
+        '../server/ee/libs/embedded/embedded-connected-user/embedded-connected-user-graphql/src/main/resources/graphql/*.graphqls',
     ],
     documents: ['src/graphql/**/*.graphql'],
     generates: {
