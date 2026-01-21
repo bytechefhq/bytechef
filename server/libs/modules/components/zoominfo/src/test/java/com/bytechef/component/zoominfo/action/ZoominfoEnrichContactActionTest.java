94965fbbe221cfafad41968cab1d0cb84e23a17e
marijahorvat171 <marijahorvat171@gmail.com>
2026-01-21 12:34:58 +0100

    2923 - improve tests

--- server/libs/modules/components/zoominfo/src/test/java/com/bytechef/component/zoominfo/action/ZoominfoEnrichContactActionTest.java
+++ server/libs/modules/components/zoominfo/src/test/java/com/bytechef/component/zoominfo/action/ZoominfoEnrichContactActionTest.java
@@ -28,27 +28,40 @@ import static com.bytechef.component.zoominfo.constant.ZoominfoConstants.OUTPUT_
 import static com.bytechef.component.zoominfo.constant.ZoominfoConstants.PERSON_ID;
 import static com.bytechef.component.zoominfo.constant.ZoominfoConstants.PHONE;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentCaptor.forClass;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import com.bytechef.component.definition.Context;
+import com.bytechef.component.definition.Context.Http;
+import com.bytechef.component.definition.Context.Http.Body;
+import com.bytechef.component.definition.Context.Http.Configuration;
 import com.bytechef.component.definition.Context.Http.Executor;
 import com.bytechef.component.definition.Context.Http.Response;
+import com.bytechef.component.definition.Context.Http.ResponseType;
 import com.bytechef.component.definition.Parameters;
 import com.bytechef.component.test.definition.MockParametersFactory;
 import java.util.List;
 import java.util.Map;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 
 /**
  * @author Marija Horvat
  */
 class ZoominfoEnrichContactActionTest {
 
+    private final ArgumentCaptor<Body> bodyArgumentCaptor = forClass(Body.class);
+    private final ArgumentCaptor<Configuration.ConfigurationBuilder> configurationBuilderArgumentCaptor =
+        forClass(Configuration.ConfigurationBuilder.class);
+    @SuppressWarnings("unchecked")
+    private final ArgumentCaptor<Context.ContextFunction<Http, Executor>> httpFunctionArgumentCaptor =
+        forClass(Context.ContextFunction.class);
     private final Context mockedContext = mock(Context.class);
-    private final Object mockedObject = mock(Object.class);
     private final Executor mockedExecutor = mock(Executor.class);
+    private final Http mockedHttp = mock(Http.class);
+    private final Object mockedObject = mock(Object.class);
     private final Response mockedResponse = mock(Response.class);
     private final Parameters mockedParameters = MockParametersFactory.create(
         Map.ofEntries(
@@ -57,22 +70,54 @@ class ZoominfoEnrichContactActionTest {
             Map.entry(PHONE, "+123456789"), Map.entry(JOB_TITLE, "developer"),
             Map.entry(EXTERNAL_URL, "http://www.url.com"), Map.entry(COMPANY_ID, 2),
             Map.entry(COMPANY_NAME, "Company"), Map.entry(OUTPUT_FIELDS, List.of("id", "firstName", "email"))));
+    private final ArgumentCaptor<String> stringArgumentCaptor = forClass(String.class);
 
     @Test
     void testPerform() {
-        when(mockedContext.http(any()))
+        when(mockedContext.http(httpFunctionArgumentCaptor.capture()))
+            .thenAnswer(inv -> {
+                Context.ContextFunction<Http, Executor> value = httpFunctionArgumentCaptor.getValue();
+
+                return value.apply(mockedHttp);
+            });
+        when(mockedHttp.post(stringArgumentCaptor.capture()))
             .thenReturn(mockedExecutor);
-        when(mockedExecutor.body(any()))
+        when(mockedExecutor.body(bodyArgumentCaptor.capture()))
             .thenReturn(mockedExecutor);
-        when(mockedExecutor.configuration(any()))
+        when(mockedExecutor.configuration(configurationBuilderArgumentCaptor.capture()))
             .thenReturn(mockedExecutor);
         when(mockedExecutor.execute())
             .thenReturn(mockedResponse);
         when(mockedResponse.getBody())
             .thenReturn(mockedObject);
 
-        Object result = ZoominfoEnrichContactAction.perform(mockedParameters, mockedParameters, mockedContext);
+        Object result = ZoominfoEnrichContactAction.perform(
+            mockedParameters, null, mockedContext);
 
         assertEquals(mockedObject, result);
+
+        Context.ContextFunction<Http, Executor> capturedFunction = httpFunctionArgumentCaptor.getValue();
+
+        assertNotNull(capturedFunction);
+
+        Configuration.ConfigurationBuilder configurationBuilder = configurationBuilderArgumentCaptor.getValue();
+        Configuration configuration = configurationBuilder.build();
+        ResponseType responseType = configuration.getResponseType();
+
+        Map<String, Object> attributes = Map.of(
+            OUTPUT_FIELDS, List.of("id", "firstName", "email"),
+            "matchPersonInput", Map.of(PERSON_ID, 1, FULL_NAME, "test test", FIRST_NAME, "test",
+                LAST_NAME, "test", EMAIL, "http://www.example.com", PHONE, "+123456789",
+                JOB_TITLE, "developer", EXTERNAL_URL, "http://www.url.com", COMPANY_ID, 2,
+                COMPANY_NAME, "Company"));
+
+        assertEquals(ResponseType.Type.JSON, responseType.getType());
+        assertEquals("/contacts/enrich", stringArgumentCaptor.getValue());
+        assertEquals(
+            Body.of(
+                Map.of("data", Map.of("type", "ContactEnrich",
+                    "attributes", attributes)),
+                Http.BodyContentType.JSON),
+            bodyArgumentCaptor.getValue());
     }
 }
