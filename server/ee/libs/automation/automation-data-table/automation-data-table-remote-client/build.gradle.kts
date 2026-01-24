2cae92d512a1475e57f8f0b2f9ba6e85813e6927
Ivica Cardic <ivica.cardic@gmail.com>
2026-01-24 08:41:15 +0100

    3955 Consolidate automation-data-table module structure

---
+++ server/ee/libs/automation/automation-data-table/automation-data-table-remote-client/build.gradle.kts
@@ -0,0 +1,5 @@
+dependencies {
+    implementation(project(":server:libs:automation:automation-data-table:automation-data-table-api"))
+    implementation(project(":server:libs:platform:platform-api"))
+    implementation("org.springframework:spring-context")
+}
--- server/libs/modules/components/data-table/build.gradle.kts
+++ server/libs/modules/components/data-table/build.gradle.kts
@@ -3,7 +3,6 @@ version="1.0"
 dependencies {
     implementation("org.springframework:spring-context")
     implementation(project(":sdks:backend:java:component-api"))
-    implementation(project(":server:libs:automation:automation-data-table:automation-data-table-configuration:automation-data-table-configuration-api"))
-    implementation(project(":server:libs:automation:automation-data-table:automation-data-table-execution:automation-data-table-execution-api"))
+    implementation(project(":server:libs:automation:automation-data-table:automation-data-table-api"))
     implementation(project(":server:libs:core:commons:commons-util"))
 }
