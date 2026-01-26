44e5d43aafad8bdd47c45041cea996838b1327b5
ivonapavela <92436197+ivonapavela@users.noreply.github.com>
2026-01-26 11:34:56 +0100

    3184 - Jira Edit Issue Action Test - adding tests for Edit Issue and Utils(addDescription) - adding JSON

---
+++ server/libs/modules/components/jira/src/test/java/com/bytechef/component/jira/action/JiraEditIssueActionTest.java
@@ -0,0 +1,116 @@
+/*
+ * Copyright 2025 ByteChef
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef.component.jira.action;
+
+import static com.bytechef.component.jira.constant.JiraConstants.ADD_LABELS;
+import static com.bytechef.component.jira.constant.JiraConstants.DESCRIPTION;
+import static com.bytechef.component.jira.constant.JiraConstants.FIELDS;
+import static com.bytechef.component.jira.constant.JiraConstants.ISSUE_ID;
+import static com.bytechef.component.jira.constant.JiraConstants.PROJECT;
+import static com.bytechef.component.jira.constant.JiraConstants.REMOVE_LABELS;
+import static com.bytechef.component.jira.constant.JiraConstants.SUMMARY;
+import static com.bytechef.component.jira.constant.JiraConstants.UPDATE;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentCaptor.forClass;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import com.bytechef.component.definition.ActionContext;
+import com.bytechef.component.definition.Context;
+import com.bytechef.component.definition.Context.ContextFunction;
+import com.bytechef.component.definition.Context.Http;
+import com.bytechef.component.definition.Context.Http.Configuration.ConfigurationBuilder;
+import com.bytechef.component.definition.Parameters;
+import com.bytechef.component.jira.util.JiraUtils;
+import com.bytechef.component.test.definition.MockParametersFactory;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+
+class JiraEditIssueActionTest {
+
+    private final ArgumentCaptor<Context.Http.Configuration.ConfigurationBuilder> configurationBuilderArgumentCaptor =
+        forClass(Context.Http.Configuration.ConfigurationBuilder.class);
+    @SuppressWarnings("unchecked")
+    private final ArgumentCaptor<Context.ContextFunction<Context.Http, Context.Http.Executor>> httpFunctionArgumentCaptor =
+        forClass(Context.ContextFunction.class);
+    private final ArgumentCaptor<Context.Http.Body> bodyArgumentCaptor = forClass(Context.Http.Body.class);
+    private final ActionContext mockedActionContext = mock(ActionContext.class);
+    private final Context.Http.Executor mockedExecutor = mock(Context.Http.Executor.class);
+    private final Context.Http mockedHttp = mock(Context.Http.class);
+    private final Parameters mockedParameters = MockParametersFactory.create(
+        Map.of(
+            PROJECT, "1",
+            ISSUE_ID, "1",
+            SUMMARY, "summary",
+            DESCRIPTION, "description",
+            ADD_LABELS, List.of("test_add_labels"),
+            REMOVE_LABELS, List.of("test_remove_labels")));
+    private final Context.Http.Response mockedResponse = mock(Context.Http.Response.class);
+    private final ArgumentCaptor<String> stringArgumentCaptor = forClass(String.class);
+
+    @Test
+    void testPerform() {
+        when(mockedActionContext.http(httpFunctionArgumentCaptor.capture()))
+            .thenAnswer(inv -> httpFunctionArgumentCaptor.getValue()
+                .apply(mockedHttp));
+        when(mockedHttp.put(stringArgumentCaptor.capture()))
+            .thenReturn(mockedExecutor);
+        when(mockedExecutor.configuration(configurationBuilderArgumentCaptor.capture()))
+            .thenReturn(mockedExecutor);
+        when(mockedExecutor.body(bodyArgumentCaptor.capture()))
+            .thenReturn(mockedExecutor);
+        when(mockedExecutor.execute())
+            .thenReturn(mockedResponse);
+
+        Boolean result = JiraEditIssueAction.perform(mockedParameters, mockedParameters, mockedActionContext);
+
+        assertTrue(result);
+
+        ContextFunction<Http, Http.Executor> capturedFunction = httpFunctionArgumentCaptor.getValue();
+
+        assertNotNull(capturedFunction);
+
+        ConfigurationBuilder configurationBuilder = configurationBuilderArgumentCaptor.getValue();
+
+        Http.Configuration configuration = configurationBuilder.build();
+
+        Http.ResponseType responseType = configuration.getResponseType();
+
+        assertEquals(Context.Http.ResponseType.Type.JSON, responseType.getType());
+        assertEquals("/issue/1", stringArgumentCaptor.getValue());
+
+        Map<String, Object> expectedFields = new HashMap<>();
+        expectedFields.put(SUMMARY, "summary");
+        JiraUtils.addDescriptionField(expectedFields, "description");
+
+        List<Map<String, Object>> expectedLabels = List.of(
+            Map.of("add", "test_add_labels"),
+            Map.of("remove", "test_remove_labels"));
+
+        Map<String, Object> expectedBody = Map.of(
+            FIELDS, expectedFields,
+            UPDATE, Map.of("labels", expectedLabels));
+
+        assertEquals(Http.Body.of(expectedBody), bodyArgumentCaptor.getValue());
+    }
+
+}
