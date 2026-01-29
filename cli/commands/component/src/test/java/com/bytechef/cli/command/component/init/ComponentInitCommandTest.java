c4c086db2cd64225814796cbb684cc934c97e1f5
Ivica Cardic <ivica.cardic@gmail.com>
2026-01-29 20:19:05 +0100

    3823 cli - Add assertions to CLI component init tests

--- cli/commands/component/src/test/java/com/bytechef/cli/command/component/init/ComponentInitCommandTest.java
+++ cli/commands/component/src/test/java/com/bytechef/cli/command/component/init/ComponentInitCommandTest.java
@@ -16,35 +16,89 @@
 
 package com.bytechef.cli.command.component.init;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import com.bytechef.cli.CliApplication;
 import java.io.File;
+import java.io.IOException;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Comparator;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
 
 /**
  * @author Ivica Cardic
  */
-public class ComponentInitCommandTest {
+class ComponentInitCommandTest {
 
-    @Test
-    public void testComponentInit() {
-        URL url = ComponentInitCommandTest.class.getResource("/dependencies/petstore.yaml");
+    @TempDir
+    Path tempDir;
 
-        CliApplication.main(
-            "component", "init", "--open-api-path", url.getFile(), "--output-path",
-            new File("build/test/generated").getAbsolutePath(), "-n", "petstore");
+    @BeforeEach
+    void setUp() throws IOException {
+        Path outputPath = tempDir.resolve("petstore");
 
-        // TODO Add asserts
+        if (Files.exists(outputPath)) {
+            Files.walk(outputPath)
+                .sorted(Comparator.reverseOrder())
+                .map(Path::toFile)
+                .forEach(File::delete);
+        }
     }
 
     @Test
-    public void testComponentInit2() {
-        URL url = ComponentInitCommandTest.class.getResource("/dependencies/petstore2.yaml");
+    void testComponentInitGeneratesExpectedFiles() {
+        URL url = ComponentInitCommandTest.class.getResource("/dependencies/petstore.yaml");
+        String outputPath = tempDir.toAbsolutePath()
+            .toString();
 
         CliApplication.main(
-            "component", "init", "--open-api-path", url.getFile(), "--output-path",
-            new File("build/test/generated").getAbsolutePath(), "-n", "petstore");
+            "component", "init", "--open-api-path", url.getFile(), "--output-path", outputPath, "--name", "petstore");
 
-        // TODO Add asserts
+        Path componentDir = tempDir.resolve("petstore");
+
+        assertTrue(Files.exists(componentDir), "Component directory should be created");
+
+        Path mainJavaDir = componentDir.resolve("src/main/java/com/bytechef/component/petstore");
+
+        assertTrue(
+            Files.exists(mainJavaDir.resolve("PetstoreComponentHandler.java")),
+            "ComponentHandler should be generated");
+        assertTrue(
+            Files.exists(mainJavaDir.resolve("AbstractPetstoreComponentHandler.java")),
+            "AbstractComponentHandler should be generated");
+        assertTrue(
+            Files.exists(mainJavaDir.resolve("connection/PetstoreConnection.java")),
+            "Connection class should be generated");
+
+        Path actionDir = mainJavaDir.resolve("action");
+
+        assertTrue(Files.exists(actionDir), "Action directory should be created");
+        assertTrue(
+            Files.exists(actionDir.resolve("PetstoreListPetsAction.java")),
+            "ListPets action should be generated");
+    }
+
+    @Test
+    void testComponentInitWithDifferentOpenApiSpec() {
+        URL url = ComponentInitCommandTest.class.getResource("/dependencies/petstore2.yaml");
+        String outputPath = tempDir.toAbsolutePath()
+            .toString();
+
+        CliApplication.main(
+            "component", "init", "--open-api-path", url.getFile(), "--output-path", outputPath, "--name", "petstore");
+
+        Path componentDir = tempDir.resolve("petstore");
+
+        assertTrue(Files.exists(componentDir), "Component directory should be created");
+
+        Path mainJavaDir = componentDir.resolve("src/main/java/com/bytechef/component/petstore");
+
+        assertTrue(
+            Files.exists(mainJavaDir.resolve("PetstoreComponentHandler.java")),
+            "ComponentHandler should be generated");
     }
 }
