e393bc3de885be823c485c41c1955132988dc014
Ivica Cardic <ivica.cardic@gmail.com>
2026-01-29 20:19:47 +0100

    3823 sdk - Add copy gradle task for component and definition jars

--- sdks/backend/java/build.gradle.kts
+++ sdks/backend/java/build.gradle.kts
@@ -1,3 +1,40 @@
 subprojects {
     apply(plugin = "com.bytechef.java-library-conventions")
+
+    afterEvaluate {
+        if (name == "component-api" || name == "definition-api") {
+            tasks.named("compileJava") {
+                finalizedBy(":sdks:backend:java:copyApiJars")
+            }
+        }
+    }
+}
+
+tasks.register("copyApiJars") {
+    dependsOn(
+        project(":sdks:backend:java:component-api").tasks.named("jar"),
+        project(":sdks:backend:java:definition-api").tasks.named("jar")
+    )
+
+    doLast {
+        val jarFiles = listOf(
+            project(":sdks:backend:java:component-api").tasks.named("jar").get().outputs.files,
+            project(":sdks:backend:java:definition-api").tasks.named("jar").get().outputs.files
+        )
+
+        val destinations = listOf(
+            rootProject.file("cli/cli-app/libs"),
+            rootProject.file("server/ee/libs/platform/platform-api-connector/platform-api-connector-configuration/platform-api-connector-configuration-service/src/main/resources/libs")
+        )
+
+        destinations.forEach { destination ->
+            destination.mkdirs()
+
+            jarFiles.forEach { files ->
+                files.forEach { file ->
+                    file.copyTo(destination.resolve(file.name), overwrite = true)
+                }
+            }
+        }
+    }
 }
