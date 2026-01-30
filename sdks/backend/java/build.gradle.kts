94220f52fafd51f3acf0376da9948b421907b795
Ivica Cardic <ivica.cardic@gmail.com>
2026-01-30 05:42:09 +0100

    3823 Update `build.gradle.kts` to simplify and modularize API JAR copying tasks

--- sdks/backend/java/build.gradle.kts
+++ sdks/backend/java/build.gradle.kts
@@ -1,40 +1,39 @@
 subprojects {
     apply(plugin = "com.bytechef.java-library-conventions")
-
-    afterEvaluate {
-        if (name == "component-api" || name == "definition-api") {
-            tasks.named("compileJava") {
-                finalizedBy(":sdks:backend:java:copyApiJars")
-            }
-        }
-    }
 }
 
-tasks.register("copyApiJars") {
+tasks.register<Copy>("copyApiJarsToCliLibs") {
     dependsOn(
         project(":sdks:backend:java:component-api").tasks.named("jar"),
         project(":sdks:backend:java:definition-api").tasks.named("jar")
     )
 
-    doLast {
-        val jarFiles = listOf(
-            project(":sdks:backend:java:component-api").tasks.named("jar").get().outputs.files,
-            project(":sdks:backend:java:definition-api").tasks.named("jar").get().outputs.files
+    from(project(":sdks:backend:java:component-api").tasks.named("jar"))
+    from(project(":sdks:backend:java:definition-api").tasks.named("jar"))
+
+    into(rootProject.layout.projectDirectory.dir("cli/cli-app/libs"))
+}
+
+tasks.register<Copy>("copyApiJarsToServerLibs") {
+    dependsOn(
+        project(":sdks:backend:java:component-api").tasks.named("jar"),
+        project(":sdks:backend:java:definition-api").tasks.named("jar")
+    )
+
+    from(project(":sdks:backend:java:component-api").tasks.named("jar"))
+    from(project(":sdks:backend:java:definition-api").tasks.named("jar"))
+
+    into(
+        rootProject.layout.projectDirectory.dir(
+            "server/ee/libs/platform/platform-api-connector/platform-api-connector-configuration/" +
+                "platform-api-connector-configuration-service/src/main/resources/libs"
         )
+    )
+}
 
-        val destinations = listOf(
-            rootProject.file("cli/cli-app/libs"),
-            rootProject.file("server/ee/libs/platform/platform-api-connector/platform-api-connector-configuration/platform-api-connector-configuration-service/src/main/resources/libs")
-        )
-
-        destinations.forEach { destination ->
-            destination.mkdirs()
-
-            jarFiles.forEach { files ->
-                files.forEach { file ->
-                    file.copyTo(destination.resolve(file.name), overwrite = true)
-                }
-            }
-        }
-    }
+tasks.register("copyApiJars") {
+    dependsOn(
+        tasks.named("copyApiJarsToCliLibs"),
+        tasks.named("copyApiJarsToServerLibs")
+    )
 }
