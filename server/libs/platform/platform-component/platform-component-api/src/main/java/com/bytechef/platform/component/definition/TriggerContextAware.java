139944205aecca229e55a254eb067613cdfb5d50
Ivica Cardic <ivica.cardic@gmail.com>
2026-02-01 23:06:57 +0100

    797 Add Javadoc comments for `ActionContextAware`, `JobContextAware`, and `TriggerContextAware`

--- server/libs/platform/platform-component/platform-component-api/src/main/java/com/bytechef/platform/component/definition/TriggerContextAware.java
+++ server/libs/platform/platform-component/platform-component-api/src/main/java/com/bytechef/platform/component/definition/TriggerContextAware.java
@@ -21,19 +21,48 @@ import com.bytechef.platform.constant.PlatformType;
 import org.jspecify.annotations.Nullable;
 
 /**
- * @author Ivica Cardic
+ * Represents a specialized extension of the {@link TriggerContext} interface that provides additional context for
+ * triggers, including identifiers and metadata for environment, job principal, and workflow. <br>
+ * Implementations of this interface should provide access to context-specific details necessary for trigger execution
+ * within workflows or automation processes.
  */
 public interface TriggerContextAware extends TriggerContext {
 
+    /**
+     * Retrieves the unique identifier of the environment associated with the current trigger context.
+     *
+     * @return the environment ID if available, or {@code null} if the environment ID is not set or applicable.
+     */
     @Nullable
     Long getEnvironmentId();
 
+    /**
+     * Retrieves the unique identifier of the job principal associated with the current trigger context.
+     *
+     * @return the job principal ID if available, or {@code null} if the job principal ID is not set or applicable.
+     */
     @Nullable
     Long getJobPrincipalId();
 
+    /**
+     * Retrieves the name of the trigger associated with the current context.
+     *
+     * @return the trigger name, or {@code null} if no trigger name is assigned or applicable.
+     */
     String getTriggerName();
 
+    /**
+     * Retrieves the platform type associated with the current trigger context.
+     *
+     * @return the {@link PlatformType} representing the platform configuration, indicating whether the context is for
+     *         automation or embedded use cases.
+     */
     PlatformType getType();
 
+    /**
+     * Retrieves the unique identifier of the workflow associated with the current trigger context.
+     *
+     * @return the workflow UUID, or {@code null} if the workflow UUID is not set or applicable.
+     */
     String getWorkflowUuid();
 }
