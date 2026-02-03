db9a2418e05d5e16be43352e6d065a35d3473fe5
Monika Kušter <domitermonika@gmail.com>
2026-02-03 10:50:05 +0100

    3200 updated output schema

--- server/libs/modules/components/figma/src/main/java/com/bytechef/component/figma/trigger/FigmaNewCommentTrigger.java
+++ server/libs/modules/components/figma/src/main/java/com/bytechef/component/figma/trigger/FigmaNewCommentTrigger.java
@@ -16,6 +16,10 @@
 
 package com.bytechef.component.figma.trigger;
 
+import static com.bytechef.component.definition.ComponentDsl.array;
+import static com.bytechef.component.definition.ComponentDsl.integer;
+import static com.bytechef.component.definition.ComponentDsl.object;
+import static com.bytechef.component.definition.ComponentDsl.outputSchema;
 import static com.bytechef.component.definition.ComponentDsl.string;
 import static com.bytechef.component.definition.ComponentDsl.trigger;
 import static com.bytechef.component.figma.constant.FigmaConstants.ID;
@@ -49,7 +53,71 @@ public class FigmaNewCommentTrigger {
                 .label("Team ID")
                 .description("The ID of the team.")
                 .required(true))
-        .output()
+        .output(
+            outputSchema(
+                object()
+                    .properties(
+                        string("file_name")
+                            .description("The name of the file that was updated."),
+                        string("created_at")
+                            .description("The UTC ISO 8601 time at which the comment was left."),
+                        string("comment_id")
+                            .description("ID of the comment."),
+                        object("triggered_by")
+                            .description("The user that made the comment and triggered this event.")
+                            .properties(
+                                string("id")
+                                    .description("ID of the user who triggered the event."),
+                                string("handle")
+                                    .description("Name of the user who triggered the event."),
+                                string("email")
+                                    .description("Email associated with the user's account."),
+                                string("img_url")
+                                    .description("URL link to the user's profile image.")),
+                        string("file_key")
+                            .description("The key of the file that was updated."),
+                        integer("retries")
+                            .description("Number of times the event has been retried."),
+                        string("event_type")
+                            .description("Type of the event."),
+                        string("webhook_id")
+                            .description("The id of the webhook that caused the callback."),
+                        string("parent_id")
+                            .description("If present, the id of the comment to which this is the reply."),
+                        string("resolved_at")
+                            .description("If set, the UTC ISO 8601 time the comment was resolved."),
+                        array("mentions")
+                            .description("Users that were mentioned in the comment.")
+                            .items(
+                                object()
+                                    .properties(
+                                        string("id")
+                                            .description("ID of the user."),
+                                        string("handle")
+                                            .description("Name of the user."),
+                                        string("email")
+                                            .description("Email associated with the user's account."),
+                                        string("img_url")
+                                            .description("URL link to the user's profile image."))),
+                        array("comment")
+                            .description("Contents of the comment itself.")
+                            .items(
+                                object()
+                                    .properties(
+                                        string("text")
+                                            .description("Text of the comment."),
+                                        string("mention")
+                                            .description(
+                                                "User id that is set if a fragment refers to a user mention."))),
+                        string("order_id")
+                            .description(
+                                "Only set for top level comments. The number displayed with the comment in the UI."),
+                        string("passcode")
+                            .description(
+                                "The passcode specified when the webhook was created, should match what was " +
+                                    "initially provided."),
+                        string("timestamp")
+                            .description("UTC ISO 8601 timestamp of when the event was triggered."))))
         .webhookEnable(FigmaNewCommentTrigger::webhookEnable)
         .webhookDisable(FigmaNewCommentTrigger::webhookDisable)
         .webhookRequest(FigmaNewCommentTrigger::webhookRequest);
@@ -62,11 +130,12 @@ public class FigmaNewCommentTrigger {
         TriggerContext triggerContext) {
 
         Map<String, ?> body = triggerContext.http(http -> http.post("/v2/webhooks"))
-            .body(Http.Body.of(
-                "event_type", "FILE_COMMENT",
-                TEAM_ID, inputParameters.getRequiredString(TEAM_ID),
-                "endpoint", webhookUrl,
-                "passcode", UUID.randomUUID()))
+            .body(
+                Http.Body.of(
+                    "event_type", "FILE_COMMENT",
+                    TEAM_ID, inputParameters.getRequiredString(TEAM_ID),
+                    "endpoint", webhookUrl,
+                    "passcode", UUID.randomUUID()))
             .configuration(Http.responseType(Http.ResponseType.JSON))
             .execute()
             .getBody(new TypeReference<>() {});
