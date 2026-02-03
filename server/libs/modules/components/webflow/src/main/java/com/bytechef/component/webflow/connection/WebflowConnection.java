ff23deaf78edf8030bf79b61cbc0fb24481bae85
Monika Kušter <domitermonika@gmail.com>
2026-02-03 08:22:21 +0100

    3419 Add expanded OAuth2 scopes and require scopes for Webflow operations

--- server/libs/modules/components/webflow/src/main/java/com/bytechef/component/webflow/connection/WebflowConnection.java
+++ server/libs/modules/components/webflow/src/main/java/com/bytechef/component/webflow/connection/WebflowConnection.java
@@ -24,6 +24,7 @@ import static com.bytechef.component.definition.ComponentDsl.connection;
 import static com.bytechef.component.definition.ComponentDsl.string;
 
 import com.bytechef.component.definition.ComponentDsl;
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -44,8 +45,38 @@ public class WebflowConnection {
                     .label("Client Secret")
                     .required(true))
             .authorizationUrl((connectionParameters, context) -> "https://webflow.com/oauth/authorize")
-            .scopes((connectionParameters, context) -> Map.of("cms:read", false, "ecommerce:read", false,
-                "ecommerce:write", false, "sites:read", false))
+            .scopes((connectionParameters, context) -> {
+                Map<String, Boolean> scopeMap = new HashMap<>();
+
+                scopeMap.put("assets:read", false);
+                scopeMap.put("assets:write", false);
+                scopeMap.put("authorized_user:read", false);
+                scopeMap.put("cms:read", true);
+                scopeMap.put("cms:write", false);
+                scopeMap.put("comments:read", false);
+                scopeMap.put("comments:write", false);
+                scopeMap.put("components:read", false);
+                scopeMap.put("components:write", false);
+                scopeMap.put("custom_code:read", false);
+                scopeMap.put("custom_code:write", false);
+                scopeMap.put("ecommerce:read", true);
+                scopeMap.put("ecommerce:write", true);
+                scopeMap.put("forms:read", false);
+                scopeMap.put("forms:write", false);
+                scopeMap.put("pages:read", false);
+                scopeMap.put("pages:write", false);
+                scopeMap.put("sites:read", true);
+                scopeMap.put("sites:write", false);
+                scopeMap.put("site_activity:read", false);
+                scopeMap.put("site_config:read", false);
+                scopeMap.put("site_config:write", false);
+                scopeMap.put("users:read", false);
+                scopeMap.put("users:write", false);
+                scopeMap.put("workspace:read", false);
+                scopeMap.put("workspace:write", false);
+
+                return scopeMap;
+            })
             .tokenUrl((connectionParameters, context) -> "https://api.webflow.com/oauth/access_token")
             .refreshUrl((connectionParameters, context) -> "https://api.webflow.com/oauth/access_token"));
 
