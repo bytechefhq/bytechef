8f99ee0a9e939d5b92ed60b3e116014977c35dde
Nikolina Spehar <speharnikolina87@gmail.com>
2026-02-03 14:38:13 +0100

    3668 - generated

---
+++ server/libs/modules/components/dhl/src/main/java/com/bytechef/component/dhl/connection/DhlConnection.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2025 ByteChef
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef.component.dhl.connection;
+
+import static com.bytechef.component.definition.Authorization.AuthorizationType;
+import static com.bytechef.component.definition.Authorization.KEY;
+import static com.bytechef.component.definition.Authorization.VALUE;
+import static com.bytechef.component.definition.ComponentDsl.authorization;
+import static com.bytechef.component.definition.ComponentDsl.connection;
+import static com.bytechef.component.definition.ComponentDsl.string;
+
+import com.bytechef.component.definition.ComponentDsl;
+
+/**
+ * Provides the component connection definition.
+ *
+ * @generated
+ */
+public class DhlConnection {
+    public static final ComponentDsl.ModifiableConnectionDefinition CONNECTION_DEFINITION = connection()
+        .baseUri((connectionParameters, context) -> "https://api-eu.dhl.com")
+        .authorizations(authorization(AuthorizationType.API_KEY)
+            .title("API Key")
+            .properties(
+                string(KEY)
+                    .label("Key")
+                    .required(true)
+                    .defaultValue("DHL-API-Key")
+                    .hidden(true),
+                string(VALUE)
+                    .label("API Key")
+                    .required(true)
+
+            ));
+
+    private DhlConnection() {
+    }
+}
