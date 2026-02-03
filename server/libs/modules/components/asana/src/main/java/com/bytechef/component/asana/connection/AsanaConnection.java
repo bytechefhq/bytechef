74bd0f73e57803a30fa2faa0db71d2af8b587e37
Monika Kušter <domitermonika@gmail.com>
2026-02-03 07:35:34 +0100

    3419 Add detailed Asana OAuth2 scopes, update connection scopes map

--- server/libs/modules/components/asana/src/main/java/com/bytechef/component/asana/connection/AsanaConnection.java
+++ server/libs/modules/components/asana/src/main/java/com/bytechef/component/asana/connection/AsanaConnection.java
@@ -24,6 +24,7 @@ import static com.bytechef.component.definition.ComponentDsl.connection;
 import static com.bytechef.component.definition.ComponentDsl.string;
 
 import com.bytechef.component.definition.ComponentDsl;
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -44,8 +45,47 @@ public class AsanaConnection {
                     .label("Client Secret")
                     .required(true))
             .authorizationUrl((connectionParameters, context) -> "https://app.asana.com/-/oauth_authorize")
-            .scopes((connectionParameters, context) -> Map.of("default", false, "openid", false, "email", false,
-                "profile", false))
+            .scopes((connectionParameters, context) -> {
+                Map<String, Boolean> scopeMap = new HashMap<>();
+
+                scopeMap.put("attachments:delete", false);
+                scopeMap.put("attachments:read", false);
+                scopeMap.put("attachments:write", false);
+                scopeMap.put("custom_fields:read", false);
+                scopeMap.put("custom_fields:write", false);
+                scopeMap.put("goals:read", false);
+                scopeMap.put("portfolios:read", false);
+                scopeMap.put("portfolios:write", false);
+                scopeMap.put("project_templates:read", false);
+                scopeMap.put("projects:delete", false);
+                scopeMap.put("projects:read", true);
+                scopeMap.put("projects:write", true);
+                scopeMap.put("stories:read", false);
+                scopeMap.put("stories:write", false);
+                scopeMap.put("tags:read", true);
+                scopeMap.put("tags:write", false);
+                scopeMap.put("task_templates:read", false);
+                scopeMap.put("tasks:delete", false);
+                scopeMap.put("tasks:read", false);
+                scopeMap.put("tasks:write", true);
+                scopeMap.put("team_memberships:read", false);
+                scopeMap.put("teams:read", true);
+                scopeMap.put("time_tracking_entries:read", false);
+                scopeMap.put("timesheet_approval_statuses:read", false);
+                scopeMap.put("timesheet_approval_statuses:write", false);
+                scopeMap.put("workspace.typehead:read", false);
+                scopeMap.put("users:read", true);
+                scopeMap.put("webhooks:delete", false);
+                scopeMap.put("webhooks:read", false);
+                scopeMap.put("webhooks:write", false);
+                scopeMap.put("workspaces:read", true);
+                scopeMap.put("default", false);
+                scopeMap.put("openid", false);
+                scopeMap.put("email", false);
+                scopeMap.put("profile", false);
+
+                return scopeMap;
+            })
             .tokenUrl((connectionParameters, context) -> "https://app.asana.com/-/oauth_token")
             .refreshUrl((connectionParameters, context) -> "https://app.asana.com/-/oauth_token"));
 
