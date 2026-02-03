4aebd8fdbe9077759c07edb093f20c88d38ec1a4
Monika Kušter <domitermonika@gmail.com>
2026-02-03 13:42:32 +0100

    3909 Support injecting ArgumentCaptor<...> types in MockContextSetupExtension

--- sdks/backend/java/component-test/src/main/java/com/bytechef/component/test/definition/extension/MockContextSetupExtension.java
+++ sdks/backend/java/component-test/src/main/java/com/bytechef/component/test/definition/extension/MockContextSetupExtension.java
@@ -17,20 +17,22 @@
 package com.bytechef.component.test.definition.extension;
 
 import static org.mockito.ArgumentCaptor.forClass;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import com.bytechef.component.definition.Context;
 import com.bytechef.component.definition.Context.ContextFunction;
 import com.bytechef.component.definition.Context.Http;
+import com.bytechef.component.definition.Context.Http.Configuration.ConfigurationBuilder;
 import com.bytechef.component.definition.Context.Http.Executor;
 import com.bytechef.component.definition.Context.Http.Response;
 import java.lang.reflect.Parameter;
+import java.lang.reflect.ParameterizedType;
 import org.jspecify.annotations.Nullable;
 import org.junit.jupiter.api.extension.BeforeEachCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.api.extension.ExtensionContext.Namespace;
+import org.junit.jupiter.api.extension.ExtensionContext.Store;
 import org.junit.jupiter.api.extension.ParameterContext;
 import org.junit.jupiter.api.extension.ParameterResolutionException;
 import org.junit.jupiter.api.extension.ParameterResolver;
@@ -43,12 +45,12 @@ public class MockContextSetupExtension implements BeforeEachCallback, ParameterR
 
     @Override
     public void beforeEach(ExtensionContext extensionContext) throws Exception {
+        ArgumentCaptor<ConfigurationBuilder> configurationBuilderArgumentCaptor = forClass(ConfigurationBuilder.class);
         Context mockedContext = mock(Context.class);
         Http.Executor mockedExecutor = mock(Http.Executor.class);
         Http.Response mockedResponse = mock(Http.Response.class);
-
-        ArgumentCaptor<ContextFunction<Http, Executor>> httpFunctionArgumentCaptor =
-            forClass(ContextFunction.class);
+        @SuppressWarnings("unchecked")
+        ArgumentCaptor<ContextFunction<Http, Executor>> httpFunctionArgumentCaptor = forClass(ContextFunction.class);
         Http mockedHttp = mock(Http.class);
 
         when(mockedContext.http(httpFunctionArgumentCaptor.capture()))
@@ -57,21 +59,19 @@ public class MockContextSetupExtension implements BeforeEachCallback, ParameterR
 
                 return value.apply(mockedHttp);
             });
-
-        when(mockedExecutor.configuration(any()))
-            .thenReturn(mockedExecutor);
-        when(mockedExecutor.body(any()))
+        when(mockedExecutor.configuration(configurationBuilderArgumentCaptor.capture()))
             .thenReturn(mockedExecutor);
         when(mockedExecutor.execute())
             .thenReturn(mockedResponse);
 
-        ExtensionContext.Store extensionContextStore = extensionContext.getStore(
-            Namespace.create(MockContextSetupExtension.class));
+        Store extensionContextStore = extensionContext.getStore(Namespace.create(MockContextSetupExtension.class));
 
         extensionContextStore.put(Context.class, mockedContext);
         extensionContextStore.put(Response.class, mockedResponse);
         extensionContextStore.put(Executor.class, mockedExecutor);
         extensionContextStore.put(Http.class, mockedHttp);
+        extensionContextStore.put("httpFunctionArgumentCaptor", httpFunctionArgumentCaptor);
+        extensionContextStore.put("configurationBuilderArgumentCaptor", configurationBuilderArgumentCaptor);
     }
 
     @Override
@@ -79,21 +79,27 @@ public class MockContextSetupExtension implements BeforeEachCallback, ParameterR
         throws ParameterResolutionException {
 
         Parameter parameter = parameterContext.getParameter();
+        java.lang.reflect.Type type = parameter.getParameterizedType();
 
         return parameter.getType() == Context.class ||
             parameter.getType() == Http.Response.class ||
             parameter.getType() == Http.Executor.class ||
-            parameter.getType() == Http.class;
+            parameter.getType() == Http.class ||
+            (parameter.getType() == ArgumentCaptor.class && type instanceof ParameterizedType pt &&
+                (pt.getActualTypeArguments()[0].equals(ConfigurationBuilder.class) ||
+                    (pt.getActualTypeArguments()[0] instanceof ParameterizedType nestedPt &&
+                        nestedPt.getRawType()
+                            .equals(ContextFunction.class))));
     }
 
     @Override
     public @Nullable Object resolveParameter(ParameterContext parameterContext, ExtensionContext extensionContext)
         throws ParameterResolutionException {
 
-        ExtensionContext.Store extensionContextStore = extensionContext.getStore(
-            Namespace.create(MockContextSetupExtension.class));
+        Store extensionContextStore = extensionContext.getStore(Namespace.create(MockContextSetupExtension.class));
 
         Parameter parameter = parameterContext.getParameter();
+        java.lang.reflect.Type type = parameter.getParameterizedType();
 
         if (parameter.getType() == Context.class) {
             return extensionContextStore.get(Context.class);
@@ -109,6 +115,19 @@ public class MockContextSetupExtension implements BeforeEachCallback, ParameterR
 
         if (parameter.getType() == Http.class) {
             return extensionContextStore.get(Http.class);
+        }
+
+        if (parameter.getType() == ArgumentCaptor.class && type instanceof ParameterizedType pt) {
+            if (pt.getActualTypeArguments()[0].equals(ConfigurationBuilder.class)) {
+                return extensionContextStore.get("configurationBuilderArgumentCaptor");
+            }
+
+            if (pt.getActualTypeArguments()[0] instanceof ParameterizedType nestedPt &&
+                nestedPt.getRawType()
+                    .equals(ContextFunction.class)) {
+
+                return extensionContextStore.get("httpFunctionArgumentCaptor");
+            }
         }
 
         throw new ParameterResolutionException(
