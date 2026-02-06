4a9e03cbd90c62e12463e9be100e8fe97cfd6947
marijahorvat171 <marijahorvat171@gmail.com>
2026-02-06 12:43:41 +0100

    1316 - add help and version for actions and connection

--- server/libs/modules/components/gitlab/src/main/java/com/bytechef/component/gitlab/GitlabComponentHandler.java
+++ server/libs/modules/components/gitlab/src/main/java/com/bytechef/component/gitlab/GitlabComponentHandler.java
@@ -18,7 +18,9 @@ package com.bytechef.component.gitlab;
 
 import com.bytechef.component.OpenApiComponentHandler;
 import com.bytechef.component.definition.ComponentCategory;
+import com.bytechef.component.definition.ComponentDsl.ModifiableActionDefinition;
 import com.bytechef.component.definition.ComponentDsl.ModifiableComponentDefinition;
+import com.bytechef.component.definition.ComponentDsl.ModifiableConnectionDefinition;
 import com.bytechef.component.definition.ComponentDsl.ModifiableTriggerDefinition;
 import com.bytechef.component.gitlab.trigger.GitlabNewIssueTrigger;
 import com.google.auto.service.AutoService;
@@ -36,10 +38,40 @@ public class GitlabComponentHandler extends AbstractGitlabComponentHandler {
     }
 
     @Override
+    public List<ModifiableActionDefinition> modifyActions(ModifiableActionDefinition... actionDefinitions) {
+        for (ModifiableActionDefinition actionDefinition : actionDefinitions) {
+            String name = actionDefinition.getName();
+
+            switch (name) {
+                case "createCommentOnIssue" ->
+                    actionDefinition.help("",
+                        "https://docs.bytechef.io/reference/components/gitlab_v1#create-comment-on-issue");
+                case "createIssue" ->
+                    actionDefinition.help("",
+                        "https://docs.bytechef.io/reference/components/gitlab_v1#create-issue");
+                default -> {
+                }
+            }
+        }
+        return super.modifyActions(actionDefinitions);
+    }
+
+    @Override
     public ModifiableComponentDefinition modifyComponent(ModifiableComponentDefinition modifiableComponentDefinition) {
         return modifiableComponentDefinition
+            .customActionHelp("", "https://docs.gitlab.com/api/rest/")
             .customAction(true)
             .icon("path:assets/gitlab.svg")
-            .categories(ComponentCategory.DEVELOPER_TOOLS);
+            .categories(ComponentCategory.DEVELOPER_TOOLS)
+            .version(1);
+    }
+
+    @Override
+    public ModifiableConnectionDefinition modifyConnection(
+        ModifiableConnectionDefinition modifiableConnectionDefinition) {
+
+        return modifiableConnectionDefinition
+            .help("", "https://docs.bytechef.io/reference/components/gitlab_v1#connection-setup")
+            .version(1);
     }
 }
