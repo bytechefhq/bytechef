8be900b0a5c960fc2e4ed510ee2caa04766c73b0
Ivica Cardic <ivica.cardic@gmail.com>
2026-02-06 21:21:46 +0100

    4070 client - Add Copilot panel toggle with zustand store integration and feature flag support
    Signed-off-by: Ivica Cardic <ivica.cardic@gmail.com>

---
+++ client/src/pages/platform/workflow-editor/components/hooks/useClusterElementsCanvasDialog.ts
@@ -0,0 +1,54 @@
+import {useClusterElementsCanvasDialogStore} from '@/pages/platform/workflow-editor/components/stores/useClusterElementsCanvasDialogStore';
+import useWorkflowNodeDetailsPanelStore from '@/pages/platform/workflow-editor/stores/useWorkflowNodeDetailsPanelStore';
+import {MODE, Source, useCopilotStore} from '@/shared/components/copilot/stores/useCopilotStore';
+import {useApplicationInfoStore} from '@/shared/stores/useApplicationInfoStore';
+import {useFeatureFlagsStore} from '@/shared/stores/useFeatureFlagsStore';
+import {useCallback} from 'react';
+
+interface UseClusterElementsCanvasDialogProps {
+    onOpenChange: (open: boolean) => void;
+}
+
+export default function useClusterElementsCanvasDialog({onOpenChange}: UseClusterElementsCanvasDialogProps) {
+    const {setCopilotPanelOpen} = useClusterElementsCanvasDialogStore();
+
+    const ai = useApplicationInfoStore((state) => state.ai);
+    const setContext = useCopilotStore((state) => state.setContext);
+
+    const ff_1570 = useFeatureFlagsStore()('ff-1570');
+
+    const copilotEnabled = ai.copilot.enabled && ff_1570;
+
+    const handleCopilotClick = useCallback(() => {
+        const currentContext = useCopilotStore.getState().context;
+
+        setContext({
+            ...currentContext,
+            mode: MODE.ASK,
+            parameters: {},
+            source: Source.WORKFLOW_EDITOR,
+        });
+
+        setCopilotPanelOpen(true);
+    }, [setContext, setCopilotPanelOpen]);
+
+    const handleCopilotClose = useCallback(() => {
+        setCopilotPanelOpen(false);
+    }, [setCopilotPanelOpen]);
+
+    const handleOpenChange = (isOpen: boolean) => {
+        onOpenChange(isOpen);
+
+        if (!isOpen) {
+            useClusterElementsCanvasDialogStore.getState().reset();
+            useWorkflowNodeDetailsPanelStore.getState().reset();
+        }
+    };
+
+    return {
+        copilotEnabled,
+        handleCopilotClick,
+        handleCopilotClose,
+        handleOpenChange,
+    };
+}
