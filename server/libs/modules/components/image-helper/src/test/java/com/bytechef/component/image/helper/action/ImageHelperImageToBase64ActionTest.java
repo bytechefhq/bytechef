cce4dde6f74d6b4541cab846b1556d39838a734a
Monika Kušter <domitermonika@gmail.com>
2026-02-06 09:50:19 +0100

    1457 update tests

--- server/libs/modules/components/image-helper/src/test/java/com/bytechef/component/image/helper/action/ImageHelperImageToBase64ActionTest.java
+++ server/libs/modules/components/image-helper/src/test/java/com/bytechef/component/image/helper/action/ImageHelperImageToBase64ActionTest.java
@@ -16,41 +16,75 @@
 
 package com.bytechef.component.image.helper.action;
 
+import static com.bytechef.component.image.helper.constant.ImageHelperConstants.IMAGE;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentCaptor.forClass;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import com.bytechef.commons.util.EncodingUtils;
 import com.bytechef.component.definition.Context;
+import com.bytechef.component.definition.Context.ContextFunction;
+import com.bytechef.component.definition.FileEntry;
 import com.bytechef.component.definition.Parameters;
 import com.bytechef.component.test.definition.MockParametersFactory;
 import java.util.Map;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 
 /**
  * @author Monika Kušter
  */
 class ImageHelperImageToBase64ActionTest {
 
+    @SuppressWarnings("unchecked")
+    private final ArgumentCaptor<ContextFunction<Context.File, ?>> contextFunctionArgumentCaptor =
+        forClass(ContextFunction.class);
+    @SuppressWarnings("unchecked")
+    private final ArgumentCaptor<ContextFunction<Context.Encoder, ?>> encoderFunctionArgumentCaptor =
+        forClass(ContextFunction.class);
     private final Context mockedContext = mock(Context.class);
-    private final Parameters mockedParameters = MockParametersFactory.create(Map.of());
+    private final FileEntry mockedFileEntry = mock(FileEntry.class);
+    private final Parameters mockedParameters = MockParametersFactory.create(Map.of(IMAGE, mockedFileEntry));
+    private final Context.File mockedContextFile = mock(Context.File.class);
+    private final Context.Encoder mockedContextEncoder = mock(Context.Encoder.class);
 
     @Test
-    void testPerform() {
+    void testPerform() throws Exception {
         byte[] fileContent = new byte[] {
             1, 2, 3
         };
 
         String encodeToString = EncodingUtils.base64EncodeToString(fileContent);
 
-        when(mockedContext.file(any()))
+        when(mockedContext.file(contextFunctionArgumentCaptor.capture()))
+            .thenAnswer(invocation -> {
+                ContextFunction<Context.File, ?> function = invocation.getArgument(0);
+
+                return function.apply(mockedContextFile);
+            });
+        when(mockedContextFile.readAllBytes(mockedFileEntry))
             .thenReturn(fileContent);
-        when(mockedContext.encoder(any()))
+        when(mockedContext.encoder(encoderFunctionArgumentCaptor.capture()))
+            .thenAnswer(invocation -> {
+                ContextFunction<Context.Encoder, ?> function = invocation.getArgument(0);
+
+                return function.apply(mockedContextEncoder);
+            });
+        when(mockedContextEncoder.base64EncodeToString(fileContent))
             .thenReturn(encodeToString);
 
-        String result = ImageHelperImageToBase64Action.perform(mockedParameters, mockedParameters, mockedContext);
+        String result = ImageHelperImageToBase64Action.perform(mockedParameters, any(Parameters.class), mockedContext);
 
         assertEquals(encodeToString, result);
+
+        ContextFunction<Context.File, ?> contextFunction = contextFunctionArgumentCaptor.getValue();
+
+        assertEquals(fileContent, contextFunction.apply(mockedContextFile));
+
+        ContextFunction<Context.Encoder, ?> encoderFunction = encoderFunctionArgumentCaptor.getValue();
+
+        assertEquals(encodeToString, encoderFunction.apply(mockedContextEncoder));
     }
 }
