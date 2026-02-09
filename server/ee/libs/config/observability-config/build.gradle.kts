309dd9b4be78091484d1ab02a73ceaf2330d7a5b
Matija Petanjek <matija.petanjek7@gmail.com>
2026-02-09 12:03:43 +0100

    745 - update dependencies to Open Telemetry version

--- server/ee/libs/config/observability-config/build.gradle.kts
+++ server/ee/libs/config/observability-config/build.gradle.kts
@@ -1,9 +1,13 @@
 dependencies {
-    implementation("io.micrometer:micrometer-registry-otlp")
-    implementation(libs.loki.logback.appender)
-    implementation("io.micrometer:micrometer-tracing-bridge-otel")
+    implementation("ch.qos.logback:logback-core")
+    implementation("jakarta.servlet:jakarta.servlet-api")
+    implementation("io.micrometer:micrometer-core")
+    implementation("io.micrometer:micrometer-tracing")
+    implementation("io.opentelemetry:opentelemetry-api")
     implementation("io.opentelemetry:opentelemetry-exporter-otlp")
-    implementation("org.apache.commons:commons-lang3")
+    implementation("io.opentelemetry.instrumentation:opentelemetry-logback-appender-1.0:2.24.0-alpha")
+    implementation("org.slf4j:slf4j-api")
     implementation("org.springframework:spring-context")
+    implementation("org.springframework:spring-web")
     implementation("org.springframework.boot:spring-boot-autoconfigure")
 }
