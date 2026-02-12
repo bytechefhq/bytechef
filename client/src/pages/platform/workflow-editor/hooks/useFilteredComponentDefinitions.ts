6b7e273327aeee3cf2eb046495c8932784a466d5
Petra <petra.pazanin10@gmail.com>
2026-02-12 09:13:52 +0100

    4112 - sf

--- client/src/pages/platform/workflow-editor/hooks/useFilteredComponentDefinitions.ts
+++ client/src/pages/platform/workflow-editor/hooks/useFilteredComponentDefinitions.ts
@@ -3,22 +3,24 @@ import {
     ComponentDefinitionWithActionsProps,
     useGetComponentDefinitionsWithActionsQuery,
 } from '@/shared/queries/platform/componentDefinitionsGraphQL.queries';
-import {useMemo, useState} from 'react';
+import {Dispatch, SetStateAction, useMemo, useState} from 'react';
 import {useDebounce} from 'use-debounce';
 
-interface UseFilteredComponentDefinitionsProps {
+type UseFilteredComponentDefinitionsReturnType = {
     componentsWithActions: Array<ComponentDefinitionBasic | ComponentDefinitionWithActionsProps>;
     filter: string;
     isSearchLoading: boolean;
-    setFilter: React.Dispatch<React.SetStateAction<string>>;
+    setFilter: Dispatch<SetStateAction<string>>;
     trimmedFilter: string;
-}
+};
 
 export const useFilteredComponentDefinitions = (
     componentDefinitions: Array<ComponentDefinitionBasic | ComponentDefinitionWithActionsProps>
-): UseFilteredComponentDefinitionsProps => {
+): UseFilteredComponentDefinitionsReturnType => {
     const [filter, setFilter] = useState('');
+
     const [debouncedFilter] = useDebounce(filter, 300);
+
     const trimmedFilter = debouncedFilter.trim();
 
     const {data: searchedComponentDefinitions, isLoading: isSearchLoading} =
