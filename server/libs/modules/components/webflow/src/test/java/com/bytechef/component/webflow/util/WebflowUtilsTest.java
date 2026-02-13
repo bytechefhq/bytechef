f0e1ccac0eed39362273eaad3679e494ae066ec1
marijahorvat171 <marijahorvat171@gmail.com>
2026-02-13 11:20:58 +0100

    1764 - update tests

--- server/libs/modules/components/webflow/src/test/java/com/bytechef/component/webflow/util/WebflowUtilsTest.java
+++ server/libs/modules/components/webflow/src/test/java/com/bytechef/component/webflow/util/WebflowUtilsTest.java
@@ -17,10 +17,14 @@
 package com.bytechef.component.webflow.util;
 
 import static com.bytechef.component.definition.ComponentDsl.option;
+import static com.bytechef.component.webflow.constant.WebflowConstants.COLLECTION_ID;
 import static com.bytechef.component.webflow.constant.WebflowConstants.DISPLAY_NAME;
 import static com.bytechef.component.webflow.constant.WebflowConstants.ID;
 import static com.bytechef.component.webflow.constant.WebflowConstants.ORDER_ID;
+import static com.bytechef.component.webflow.constant.WebflowConstants.SITE_ID;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentCaptor.forClass;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -30,70 +34,141 @@ import com.bytechef.component.definition.Context.Http;
 import com.bytechef.component.definition.Option;
 import com.bytechef.component.definition.Parameters;
 import com.bytechef.component.definition.TypeReference;
+import com.bytechef.component.test.definition.MockParametersFactory;
+import com.bytechef.component.test.definition.extension.MockContextSetupExtension;
 import java.util.List;
 import java.util.Map;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 
 /**
  * @author Monika Kušter
  */
+@ExtendWith(MockContextSetupExtension.class)
 class WebflowUtilsTest {
 
     private final List<Option<String>> expectedOptions = List.of(option("abc", "123"));
-    private final Context mockedContext = mock(Context.class);
-    private final Http.Executor mockedExecutor = mock(Http.Executor.class);
-    private final Parameters mockedParameters = mock(Parameters.class);
-    private final Http.Response mockedResponse = mock(Http.Response.class);
-
-    @BeforeEach
-    void beforeEach() {
-        when(mockedContext.http(any()))
-            .thenReturn(mockedExecutor);
-        when(mockedExecutor.configuration(any()))
-            .thenReturn(mockedExecutor);
-        when(mockedExecutor.execute())
-            .thenReturn(mockedResponse);
-    }
+    private final ArgumentCaptor<String> stringArgumentCaptor = forClass(String.class);
+    private Parameters mockedParameters;
 
     @Test
-    void testGetItemIdOptions() {
+    void testGetItemIdOptions(
+        Context mockedContext, Http.Response mockedResponse, Http.Executor mockedExecutor, Http mockedHttp,
+        ArgumentCaptor<Context.ContextFunction<Http, Http.Executor>> httpFunctionArgumentCaptor,
+        ArgumentCaptor<Http.Configuration.ConfigurationBuilder> configurationBuilderArgumentCaptor) {
+
+        mockedParameters = MockParametersFactory.create(Map.of(COLLECTION_ID, "1"));
+
+        when(mockedHttp.get(stringArgumentCaptor.capture()))
+            .thenReturn(mockedExecutor);
         when(mockedResponse.getBody(any(TypeReference.class)))
             .thenReturn(Map.of("items", List.of(Map.of(ID, "123", "fieldData", Map.of("name", "abc")))));
 
         assertEquals(
             expectedOptions,
-            WebflowUtils.getItemIdOptions(mockedParameters, mockedParameters, Map.of(), "",
-                mockedContext));
+            WebflowUtils.getItemIdOptions(
+                mockedParameters, null, null, null, mockedContext));
+
+        Context.ContextFunction<Http, Http.Executor> capturedFunction = httpFunctionArgumentCaptor.getValue();
+
+        assertNotNull(capturedFunction);
+
+        Http.Configuration.ConfigurationBuilder configurationBuilder = configurationBuilderArgumentCaptor.getValue();
+        Http.Configuration configuration = configurationBuilder.build();
+
+        Http.ResponseType responseType = configuration.getResponseType();
+        assertEquals(Http.ResponseType.Type.JSON, responseType.getType());
+        assertEquals("/collections/1/items", stringArgumentCaptor.getValue());
     }
 
     @Test
-    void testGetCollectionIdOptions() {
+    void testGetCollectionIdOptions(
+        Context mockedContext, Http.Response mockedResponse, Http.Executor mockedExecutor, Http mockedHttp,
+        ArgumentCaptor<Context.ContextFunction<Http, Http.Executor>> httpFunctionArgumentCaptor,
+        ArgumentCaptor<Http.Configuration.ConfigurationBuilder> configurationBuilderArgumentCaptor) {
+
+        mockedParameters = MockParametersFactory.create(Map.of(SITE_ID, "1"));
+
+        when(mockedHttp.get(stringArgumentCaptor.capture()))
+            .thenReturn(mockedExecutor);
         when(mockedResponse.getBody(any(TypeReference.class)))
             .thenReturn(Map.of("collections", List.of(Map.of(ID, "123", DISPLAY_NAME, "abc"))));
 
         assertEquals(
             expectedOptions,
-            WebflowUtils.getCollectionIdOptions(mockedParameters, mockedParameters, Map.of(), "", mockedContext));
+            WebflowUtils.getCollectionIdOptions(
+                mockedParameters, null, null, null, mockedContext));
+
+        Context.ContextFunction<Http, Http.Executor> capturedFunction = httpFunctionArgumentCaptor.getValue();
+
+        assertNotNull(capturedFunction);
+
+        Http.Configuration.ConfigurationBuilder configurationBuilder = configurationBuilderArgumentCaptor.getValue();
+        Http.Configuration configuration = configurationBuilder.build();
+
+        Http.ResponseType responseType = configuration.getResponseType();
+        assertEquals(Http.ResponseType.Type.JSON, responseType.getType());
+        assertEquals("/sites/1/collections", stringArgumentCaptor.getValue());
     }
 
     @Test
-    void testGetOrderIdOptions() {
+    void testGetOrderIdOptions(
+        Context mockedContext, Http.Response mockedResponse, Http.Executor mockedExecutor, Http mockedHttp,
+        ArgumentCaptor<Context.ContextFunction<Http, Http.Executor>> httpFunctionArgumentCaptor,
+        ArgumentCaptor<Http.Configuration.ConfigurationBuilder> configurationBuilderArgumentCaptor) {
+
+        mockedParameters = MockParametersFactory.create(Map.of(SITE_ID, "1"));
+
+        when(mockedHttp.get(stringArgumentCaptor.capture()))
+            .thenReturn(mockedExecutor);
         when(mockedResponse.getBody(any(TypeReference.class)))
             .thenReturn(Map.of("orders", List.of(Map.of(ORDER_ID, "123"))));
 
         assertEquals(
             List.of(option("123", "123")),
-            WebflowUtils.getOrderIdOptions(mockedParameters, mockedParameters, Map.of(), "", mockedContext));
+            WebflowUtils.getOrderIdOptions(
+                mockedParameters, null, null, null, mockedContext));
+
+        Context.ContextFunction<Http, Http.Executor> capturedFunction = httpFunctionArgumentCaptor.getValue();
+
+        assertNotNull(capturedFunction);
+
+        Http.Configuration.ConfigurationBuilder configurationBuilder = configurationBuilderArgumentCaptor.getValue();
+        Http.Configuration configuration = configurationBuilder.build();
+
+        Http.ResponseType responseType = configuration.getResponseType();
+        assertEquals(Http.ResponseType.Type.JSON, responseType.getType());
+        assertEquals("/sites/1/orders", stringArgumentCaptor.getValue());
     }
 
     @Test
-    void testGetSiteIdOptions() {
+    void testGetSiteIdOptions(
+        Context mockedContext, Http.Response mockedResponse, Http.Executor mockedExecutor, Http mockedHttp,
+        ArgumentCaptor<Context.ContextFunction<Http, Http.Executor>> httpFunctionArgumentCaptor,
+        ArgumentCaptor<Http.Configuration.ConfigurationBuilder> configurationBuilderArgumentCaptor) {
+
+        mockedParameters = mock(Parameters.class);
+
+        when(mockedHttp.get(stringArgumentCaptor.capture()))
+            .thenReturn(mockedExecutor);
         when(mockedResponse.getBody(any(TypeReference.class)))
             .thenReturn(Map.of("sites", List.of(Map.of(ID, "123", DISPLAY_NAME, "abc"))));
 
         assertEquals(
             expectedOptions,
-            WebflowUtils.getSiteIdOptions(mockedParameters, mockedParameters, Map.of(), "", mockedContext));
+            WebflowUtils.getSiteIdOptions(
+                mockedParameters, null, null, null, mockedContext));
+
+        Context.ContextFunction<Http, Http.Executor> capturedFunction = httpFunctionArgumentCaptor.getValue();
+
+        assertNotNull(capturedFunction);
+
+        Http.Configuration.ConfigurationBuilder configurationBuilder = configurationBuilderArgumentCaptor.getValue();
+        Http.Configuration configuration = configurationBuilder.build();
+
+        Http.ResponseType responseType = configuration.getResponseType();
+        assertEquals(Http.ResponseType.Type.JSON, responseType.getType());
+        assertEquals("/sites", stringArgumentCaptor.getValue());
     }
 }
