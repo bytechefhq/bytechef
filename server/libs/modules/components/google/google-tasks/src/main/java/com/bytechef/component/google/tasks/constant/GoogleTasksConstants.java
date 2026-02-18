fb4453a6526bb95bfe7c2bb94037d107869f156c
Nikolina Spehar <speharnikolina87@gmail.com>
2026-02-18 11:52:05 +0100

    2039 - changed ActionCOntext to Context in getTaskIdOptions method and updated task output property

--- server/libs/modules/components/google/google-tasks/src/main/java/com/bytechef/component/google/tasks/constant/GoogleTasksConstants.java
+++ server/libs/modules/components/google/google-tasks/src/main/java/com/bytechef/component/google/tasks/constant/GoogleTasksConstants.java
@@ -16,6 +16,8 @@
 
 package com.bytechef.component.google.tasks.constant;
 
+import static com.bytechef.component.definition.ComponentDsl.array;
+import static com.bytechef.component.definition.ComponentDsl.bool;
 import static com.bytechef.component.definition.ComponentDsl.object;
 import static com.bytechef.component.definition.ComponentDsl.string;
 
@@ -41,14 +43,64 @@ public class GoogleTasksConstants {
     public static final ModifiableObjectProperty TASK_OUTPUT_PROPERTY =
         object()
             .properties(
+                string("kind")
+                    .description("Type of the resource."),
                 string("id")
                     .description("Task identifier."),
+                string("etag")
+                    .description("ETag of the resource."),
                 string(TITLE)
-                    .description("Title of the task."),
+                    .description("Title of the task. Maximum length allowed: 1024 characters."),
+                string("updated")
+                    .description("Last modification time of the task (as a RFC 3339 timestamp)."),
+                string("selfLink")
+                    .description("URL pointing to this task. Used to retrieve, update, or delete this task."),
+                string("parent")
+                    .description("Parent task identifier."),
+                string("position")
+                    .description(
+                        "String indicating the position of the task among its sibling tasks under the same parent " +
+                            "task or at the top level."),
                 string(NOTES)
                     .description("Notes describing the task."),
                 string(STATUS)
-                    .description("Status of the task."));
+                    .description("Status of the task."),
+                string("due")
+                    .description("Scheduled date for the task (as an RFC 3339 timestamp)."),
+                string("completed")
+                    .description("Completion date of the task (as a RFC 3339 timestamp)."),
+                bool("deleted")
+                    .description("Flag indicating whether the task has been deleted."),
+                bool("hidden")
+                    .description("Flag indicating whether the task is hidden."),
+                array("links")
+                    .description("Collection of links.")
+                    .items(
+                        object()
+                            .properties(
+                                string("type")
+                                    .description(
+                                        "Type of the link, e.g. \"email\", \"generic\", \"chat_message\", " +
+                                            "\"keep_note\"."),
+                                string("description")
+                                    .description("The description (might be empty)."),
+                                string("link")
+                                    .description("The URL."))),
+                string("webViewLink")
+                    .description("An absolute link to the task in the Google Tasks Web UI."),
+                object("assignmentInfo")
+                    .description("Context information for assigned tasks.")
+                    .properties(
+                        string("linkToTask")
+                            .description(
+                                "An absolute link to the original task in the surface of assignment (Docs, Chat " +
+                                    "spaces, etc.)."),
+                        string("surfaceType")
+                            .description("The type of surface this assigned task originates from."),
+                        object("DriveResourceInfo")
+                            .description("Information about the Drive file where this task originates from."),
+                        object("spaceInfo")
+                            .description("Information about the Chat Space where this task originates from.")));
 
     private GoogleTasksConstants() {
     }
