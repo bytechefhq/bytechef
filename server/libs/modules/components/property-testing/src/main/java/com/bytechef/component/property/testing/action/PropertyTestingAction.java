e04032d3762356355974c03902e96e48fa6647b0
Nikolina Spehar <speharnikolina87@gmail.com>
2026-02-20 14:01:06 +0100

    multi selct ne radi

--- server/libs/modules/components/property-testing/src/main/java/com/bytechef/component/property/testing/action/PropertyTestingAction.java
+++ server/libs/modules/components/property-testing/src/main/java/com/bytechef/component/property/testing/action/PropertyTestingAction.java
@@ -18,13 +18,28 @@ package com.bytechef.component.property.testing.action;
 
 import static com.bytechef.component.definition.ComponentDsl.action;
 import static com.bytechef.component.definition.ComponentDsl.array;
+import static com.bytechef.component.definition.ComponentDsl.bool;
+import static com.bytechef.component.definition.ComponentDsl.date;
+import static com.bytechef.component.definition.ComponentDsl.dateTime;
+import static com.bytechef.component.definition.ComponentDsl.dynamicProperties;
+import static com.bytechef.component.definition.ComponentDsl.fileEntry;
 import static com.bytechef.component.definition.ComponentDsl.integer;
 import static com.bytechef.component.definition.ComponentDsl.number;
 import static com.bytechef.component.definition.ComponentDsl.object;
+import static com.bytechef.component.definition.ComponentDsl.option;
 import static com.bytechef.component.definition.ComponentDsl.outputSchema;
 import static com.bytechef.component.definition.ComponentDsl.string;
+import static com.bytechef.component.property.testing.constant.PropertyTestingConstants.ARRAY_DEFAULT_VALUES;
 import static com.bytechef.component.property.testing.constant.PropertyTestingConstants.ARRAY_MAX_ITEMS;
 import static com.bytechef.component.property.testing.constant.PropertyTestingConstants.ARRAY_MIN_ITEMS;
+import static com.bytechef.component.property.testing.constant.PropertyTestingConstants.ARRAY_NO_DEFAULT_VALUES;
+import static com.bytechef.component.property.testing.constant.PropertyTestingConstants.ARRAY_NO_PREDEFINED_PROPERTIES;
+import static com.bytechef.component.property.testing.constant.PropertyTestingConstants.ARRAY_PREDEFINED_PROPERTIES;
+import static com.bytechef.component.property.testing.constant.PropertyTestingConstants.BOOL;
+import static com.bytechef.component.property.testing.constant.PropertyTestingConstants.DATE;
+import static com.bytechef.component.property.testing.constant.PropertyTestingConstants.DATE_TIME;
+import static com.bytechef.component.property.testing.constant.PropertyTestingConstants.DYNAMIC;
+import static com.bytechef.component.property.testing.constant.PropertyTestingConstants.FILE_ENTRY;
 import static com.bytechef.component.property.testing.constant.PropertyTestingConstants.INTEGER_MAX_VALUE;
 import static com.bytechef.component.property.testing.constant.PropertyTestingConstants.INTEGER_MIN_VALUE;
 import static com.bytechef.component.property.testing.constant.PropertyTestingConstants.NUMBER_MAX_PRECISION;
@@ -32,13 +47,25 @@ import static com.bytechef.component.property.testing.constant.PropertyTestingCo
 import static com.bytechef.component.property.testing.constant.PropertyTestingConstants.NUMBER_MIN_PRECISION;
 import static com.bytechef.component.property.testing.constant.PropertyTestingConstants.NUMBER_MIN_VALUE;
 import static com.bytechef.component.property.testing.constant.PropertyTestingConstants.NUMBER_PRECISION;
+import static com.bytechef.component.property.testing.constant.PropertyTestingConstants.OBJECT_DEFAULT_VALUES;
+import static com.bytechef.component.property.testing.constant.PropertyTestingConstants.OBJECT_NO_DEFAULT_VALUES;
+import static com.bytechef.component.property.testing.constant.PropertyTestingConstants.OBJECT_NO_PREDEFINED_PROPERTIES;
+import static com.bytechef.component.property.testing.constant.PropertyTestingConstants.OBJECT_PREDEFINED_PROPERTIES;
+import static com.bytechef.component.property.testing.constant.PropertyTestingConstants.OPTION_MULTISELECT;
+import static com.bytechef.component.property.testing.constant.PropertyTestingConstants.OPTION_NO_MULTISELECT;
 import static com.bytechef.component.property.testing.constant.PropertyTestingConstants.STRING_MAX_LENGTH;
 import static com.bytechef.component.property.testing.constant.PropertyTestingConstants.STRING_MIN_LENGTH;
 import static com.bytechef.component.property.testing.constant.PropertyTestingConstants.STRING_REG_EX;
 
+import com.bytechef.component.definition.ActionContext;
+import com.bytechef.component.definition.ActionDefinition.OptionsFunction;
 import com.bytechef.component.definition.ComponentDsl.ModifiableActionDefinition;
 import com.bytechef.component.definition.Context;
+import com.bytechef.component.definition.Option;
 import com.bytechef.component.definition.Parameters;
+
+import java.util.List;
+import java.util.Map;
 
 public class PropertyTestingAction {
 
@@ -46,6 +73,10 @@ public class PropertyTestingAction {
         .title("Testing")
         .description("Description")
         .properties(
+            array(ARRAY_DEFAULT_VALUES)
+                .label("Array With Default Values")
+                .description("Default value is [element1, element2, element3]")
+                .defaultValue("element1", "element2", "element3"),
             array(ARRAY_MAX_ITEMS)
                 .label("Array Max Items")
                 .maxItems(3)
@@ -56,6 +87,21 @@ public class PropertyTestingAction {
                 .minItems(3)
                 .description("Min items set to 3")
                 .items(string()),
+            array(ARRAY_NO_DEFAULT_VALUES)
+                .label("Array With No Default Values"),
+            array(ARRAY_NO_PREDEFINED_PROPERTIES)
+                .label("Array No Predefined Properties"),
+            array(ARRAY_PREDEFINED_PROPERTIES)
+                .label("Array Predefined Properties")
+                .items(string("element")),
+            bool(BOOL)
+                .label("Boolean Property"),
+            date(DATE)
+                .label("Date Property"),
+            dateTime(DATE_TIME)
+                .label("Date Time Property"),
+            fileEntry(FILE_ENTRY)
+                .label("FileEntry Property"),
             integer(INTEGER_MAX_VALUE)
                 .maxValue(10)
                 .label("Integer Max Value")
@@ -84,6 +130,30 @@ public class PropertyTestingAction {
                 .label("Number Precision")
                 .description("Number precision set to 3")
                 .numberPrecision(3),
+            object(OBJECT_DEFAULT_VALUES)
+                .label("Object With Default Values")
+                .description("Default value is { key1 : value1, key2: value2 }")
+                .defaultValue(Map.of("key1", "value1", "key2", "value2")),
+            object(OBJECT_NO_DEFAULT_VALUES)
+                .label("Object With No Default Values"),
+            object(OBJECT_NO_PREDEFINED_PROPERTIES)
+                .label("Object No Predefined Properties"),
+            object(OBJECT_PREDEFINED_PROPERTIES)
+                .label("Object Predefined Properties")
+                .properties(
+                    string("key"),
+                    string("value")),
+            array(OPTION_MULTISELECT)
+                .label("Options Multiselect")
+                .items(string())
+                .options((OptionsFunction<String>) PropertyTestingAction::getMultiSelectOptions),
+//            string(OPTION_NO_MULTISELECT)
+//                .label("Options No Multiselect")
+//                .options(
+//                    option("option1", "1"),
+//                    option("option2", "2"),
+//                    option("option3", "3"),
+//                    option("option4", "4")),
             string(STRING_MAX_LENGTH)
                 .label("String Max Length")
                 .maxLength(5)
@@ -99,6 +169,17 @@ public class PropertyTestingAction {
                 .regex("[^A-Za-z]"))
         .output(outputSchema(object()))
         .perform(PropertyTestingAction::perform);
+
+    private static List<Option<String>> getMultiSelectOptions(
+        Parameters inputParameters, Parameters connectionParameters, Map<String, String> lookupDependsOnPaths,
+        String searchText, ActionContext context) {
+
+        return List.of(
+            option("option1", "1"),
+            option("option2", "2"),
+            option("option3", "3"),
+            option("option4", "4"));
+    }
 
     private PropertyTestingAction() {
     }
--- server/libs/modules/components/property-testing/src/main/java/com/bytechef/component/property/testing/constant/PropertyTestingConstants.java
+++ server/libs/modules/components/property-testing/src/main/java/com/bytechef/component/property/testing/constant/PropertyTestingConstants.java
@@ -18,8 +18,17 @@ package com.bytechef.component.property.testing.constant;
 
 public class PropertyTestingConstants {
 
+    public static final String ARRAY_DEFAULT_VALUES = "arrayDefaultValues";
     public static final String ARRAY_MAX_ITEMS = "arrayMaxItems";
     public static final String ARRAY_MIN_ITEMS = "arrayMinItems";
+    public static final String ARRAY_NO_DEFAULT_VALUES = "arrayNoDefaultValues";
+    public static final String ARRAY_NO_PREDEFINED_PROPERTIES = "arrayNoPredefinedProperties";
+    public static final String ARRAY_PREDEFINED_PROPERTIES = "arrayPredefinedProperties";
+    public static final String BOOL = "bool";
+    public static final String DATE = "date";
+    public static final String DATE_TIME = "dateTime";
+    public static final String DYNAMIC = "dynamic";
+    public static final String FILE_ENTRY = "fileEntry";
     public static final String INTEGER_MAX_VALUE = "integerMaxValue";
     public static final String INTEGER_MIN_VALUE = "integerMinValue";
     public static final String NUMBER_MAX_PRECISION = "numberMaxNumPrecision";
@@ -27,6 +36,12 @@ public class PropertyTestingConstants {
     public static final String NUMBER_MAX_VALUE = "numberMaxValue";
     public static final String NUMBER_MIN_VALUE = "numberMinValue";
     public static final String NUMBER_PRECISION = "numberPrecision";
+    public static final String OBJECT_DEFAULT_VALUES = "objectDefaultValues";
+    public static final String OBJECT_NO_DEFAULT_VALUES = "objectNoDefaultValues";
+    public static final String OBJECT_NO_PREDEFINED_PROPERTIES = "objectNoPredefinedProperties";
+    public static final String OBJECT_PREDEFINED_PROPERTIES = "objectPredefinedProperties";
+    public static final String OPTION_MULTISELECT = "optionsMultiselect";
+    public static final String OPTION_NO_MULTISELECT = "optionsNoMultiselect";
     public static final String STRING_MAX_LENGTH = "stringMaxLength";
     public static final String STRING_MIN_LENGTH = "stringMinLength";
     public static final String STRING_REG_EX = "stringRegEx";
