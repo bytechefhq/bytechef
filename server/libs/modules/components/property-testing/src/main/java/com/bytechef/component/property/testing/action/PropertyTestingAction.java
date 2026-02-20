67e94733a8e7fae0c9dc25cee10702774c012e8a
Nikolina Spehar <speharnikolina87@gmail.com>
2026-02-20 14:01:06 +0100

    4157 - added additional types of properties to the Property Testing component

--- server/libs/modules/components/property-testing/src/main/java/com/bytechef/component/property/testing/action/PropertyTestingAction.java
+++ server/libs/modules/components/property-testing/src/main/java/com/bytechef/component/property/testing/action/PropertyTestingAction.java
@@ -18,13 +18,30 @@ package com.bytechef.component.property.testing.action;
 
 import static com.bytechef.component.definition.ComponentDsl.action;
 import static com.bytechef.component.definition.ComponentDsl.array;
+import static com.bytechef.component.definition.ComponentDsl.bool;
+import static com.bytechef.component.definition.ComponentDsl.date;
+import static com.bytechef.component.definition.ComponentDsl.dateTime;
+import static com.bytechef.component.definition.ComponentDsl.dynamicProperties;
+import static com.bytechef.component.definition.ComponentDsl.fileEntry;
 import static com.bytechef.component.definition.ComponentDsl.integer;
 import static com.bytechef.component.definition.ComponentDsl.number;
 import static com.bytechef.component.definition.ComponentDsl.object;
+import static com.bytechef.component.definition.ComponentDsl.option;
 import static com.bytechef.component.definition.ComponentDsl.outputSchema;
 import static com.bytechef.component.definition.ComponentDsl.string;
+import static com.bytechef.component.property.testing.constant.PropertyTestingConstants.ARRAY_DEFAULT_VALUES;
 import static com.bytechef.component.property.testing.constant.PropertyTestingConstants.ARRAY_MAX_ITEMS;
 import static com.bytechef.component.property.testing.constant.PropertyTestingConstants.ARRAY_MIN_ITEMS;
+import static com.bytechef.component.property.testing.constant.PropertyTestingConstants.ARRAY_NO_DEFAULT_VALUES;
+import static com.bytechef.component.property.testing.constant.PropertyTestingConstants.ARRAY_NO_PREDEFINED_PROPERTIES;
+import static com.bytechef.component.property.testing.constant.PropertyTestingConstants.ARRAY_PREDEFINED_PROPERTIES;
+import static com.bytechef.component.property.testing.constant.PropertyTestingConstants.BOOL;
+import static com.bytechef.component.property.testing.constant.PropertyTestingConstants.DATE;
+import static com.bytechef.component.property.testing.constant.PropertyTestingConstants.DATE_TIME;
+import static com.bytechef.component.property.testing.constant.PropertyTestingConstants.DISPLAY_CONDITION;
+import static com.bytechef.component.property.testing.constant.PropertyTestingConstants.DYNAMIC_PROPERTIES_LOOKUP;
+import static com.bytechef.component.property.testing.constant.PropertyTestingConstants.DYNAMIC_PROPERTY;
+import static com.bytechef.component.property.testing.constant.PropertyTestingConstants.FILE_ENTRY;
 import static com.bytechef.component.property.testing.constant.PropertyTestingConstants.INTEGER_MAX_VALUE;
 import static com.bytechef.component.property.testing.constant.PropertyTestingConstants.INTEGER_MIN_VALUE;
 import static com.bytechef.component.property.testing.constant.PropertyTestingConstants.NUMBER_MAX_PRECISION;
@@ -32,13 +49,27 @@ import static com.bytechef.component.property.testing.constant.PropertyTestingCo
 import static com.bytechef.component.property.testing.constant.PropertyTestingConstants.NUMBER_MIN_PRECISION;
 import static com.bytechef.component.property.testing.constant.PropertyTestingConstants.NUMBER_MIN_VALUE;
 import static com.bytechef.component.property.testing.constant.PropertyTestingConstants.NUMBER_PRECISION;
+import static com.bytechef.component.property.testing.constant.PropertyTestingConstants.OBJECT_DEFAULT_VALUES;
+import static com.bytechef.component.property.testing.constant.PropertyTestingConstants.OBJECT_NO_DEFAULT_VALUES;
+import static com.bytechef.component.property.testing.constant.PropertyTestingConstants.OBJECT_NO_PREDEFINED_PROPERTIES;
+import static com.bytechef.component.property.testing.constant.PropertyTestingConstants.OBJECT_PREDEFINED_PROPERTIES;
+import static com.bytechef.component.property.testing.constant.PropertyTestingConstants.OPTION_LOOKUP_DEPENDS_ON;
+import static com.bytechef.component.property.testing.constant.PropertyTestingConstants.OPTION_MULTISELECT;
+import static com.bytechef.component.property.testing.constant.PropertyTestingConstants.OPTION_NO_MULTISELECT;
+import static com.bytechef.component.property.testing.constant.PropertyTestingConstants.SET_FOR_OPTIONS_LOOKUP;
 import static com.bytechef.component.property.testing.constant.PropertyTestingConstants.STRING_MAX_LENGTH;
 import static com.bytechef.component.property.testing.constant.PropertyTestingConstants.STRING_MIN_LENGTH;
 import static com.bytechef.component.property.testing.constant.PropertyTestingConstants.STRING_REG_EX;
 
+import com.bytechef.component.definition.ActionDefinition.OptionsFunction;
+import com.bytechef.component.definition.ActionDefinition.PropertiesFunction;
 import com.bytechef.component.definition.ComponentDsl.ModifiableActionDefinition;
 import com.bytechef.component.definition.Context;
+import com.bytechef.component.definition.Option;
 import com.bytechef.component.definition.Parameters;
+import com.bytechef.component.definition.Property.ValueProperty;
+import java.util.List;
+import java.util.Map;
 
 public class PropertyTestingAction {
 
@@ -46,6 +77,10 @@ public class PropertyTestingAction {
         .title("Testing")
         .description("Description")
         .properties(
+            array(ARRAY_DEFAULT_VALUES)
+                .label("Array With Default Values")
+                .description("Default value is [element1, element2, element3]")
+                .defaultValue("element1", "element2", "element3"),
             array(ARRAY_MAX_ITEMS)
                 .label("Array Max Items")
                 .maxItems(3)
@@ -56,6 +91,38 @@ public class PropertyTestingAction {
                 .minItems(3)
                 .description("Min items set to 3")
                 .items(string()),
+            array(ARRAY_NO_DEFAULT_VALUES)
+                .label("Array With No Default Values"),
+            array(ARRAY_NO_PREDEFINED_PROPERTIES)
+                .label("Array No Predefined Properties"),
+            array(ARRAY_PREDEFINED_PROPERTIES)
+                .label("Array Predefined Properties")
+                .items(
+                    object()
+                        .properties(
+                            string("predefined1")
+                                .label("Predefined String 1"),
+                            string("predefined2")
+                                .label("Predefined String 2"))),
+            bool(BOOL)
+                .label("Boolean Property and Display Condition")
+                .description("Display condition string will be shown when this value is set to True."),
+            date(DATE)
+                .label("Date Property"),
+            dateTime(DATE_TIME)
+                .label("Date Time Property"),
+            string(DISPLAY_CONDITION)
+                .label("Display Condition")
+                .description("Will be displayed only if the boolean property is true.")
+                .displayCondition("%s == true".formatted(BOOL)),
+            string(DYNAMIC_PROPERTIES_LOOKUP)
+                .label("Dynamic Properties Lookup")
+                .description("Dynamic property will be shown when this value is set."),
+            dynamicProperties(DYNAMIC_PROPERTY)
+                .propertiesLookupDependsOn(DYNAMIC_PROPERTIES_LOOKUP)
+                .properties((PropertiesFunction) PropertyTestingAction::createDynamicParameters),
+            fileEntry(FILE_ENTRY)
+                .label("FileEntry Property"),
             integer(INTEGER_MAX_VALUE)
                 .maxValue(10)
                 .label("Integer Max Value")
@@ -84,6 +151,47 @@ public class PropertyTestingAction {
                 .label("Number Precision")
                 .description("Number precision set to 3")
                 .numberPrecision(3),
+            object(OBJECT_DEFAULT_VALUES)
+                .label("Object With Default Values")
+                .description("Default value is { key1 : value1, key2: value2 }")
+                .defaultValue(Map.of("key1", "value1", "key2", "value2")),
+            object(OBJECT_NO_DEFAULT_VALUES)
+                .label("Object With No Default Values"),
+            object(OBJECT_NO_PREDEFINED_PROPERTIES)
+                .label("Object No Predefined Properties"),
+            object(OBJECT_PREDEFINED_PROPERTIES)
+                .label("Object Predefined Properties")
+                .properties(
+                    array("array")
+                        .items(
+                            object()
+                                .properties(
+                                    string("key"),
+                                    string("value")))),
+            string(SET_FOR_OPTIONS_LOOKUP)
+                .label("Set For Options Lookup")
+                .description("This has to be set so you can se the options below."),
+            string(OPTION_LOOKUP_DEPENDS_ON)
+                .label("Option Lookup Depends On")
+                .description("This options depend on \"Set For Options Lookup\" property.")
+                .optionsLookupDependsOn(SET_FOR_OPTIONS_LOOKUP)
+                .options((OptionsFunction<String>) PropertyTestingAction::getOptionsLookup),
+            array(OPTION_MULTISELECT)
+                .label("Options Multiselect")
+                .items(string())
+                .options(
+                    List.of(
+                        option("option1", "1"),
+                        option("option2", "2"),
+                        option("option3", "3"),
+                        option("option4", "4"))),
+            string(OPTION_NO_MULTISELECT)
+                .label("Options No Multiselect")
+                .options(
+                    option("option1", "1"),
+                    option("option2", "2"),
+                    option("option3", "3"),
+                    option("option4", "4")),
             string(STRING_MAX_LENGTH)
                 .label("String Max Length")
                 .maxLength(5)
@@ -99,6 +207,28 @@ public class PropertyTestingAction {
                 .regex("[^A-Za-z]"))
         .output(outputSchema(object()))
         .perform(PropertyTestingAction::perform);
+
+    private static List<ValueProperty<?>> createDynamicParameters(
+        Parameters inputParameters, Parameters connectionParameters, Map<String, String> lookupDependsOnPaths,
+        Context context) {
+
+        return List.of(
+            string("dynamicProperty1")
+                .label("Dynamic Property 1"),
+            string("dynamicProperty2")
+                .label("Dynamic Property 2"));
+    }
+
+    private static List<? extends Option<String>> getOptionsLookup(
+        Parameters inputParameters, Parameters connectionParameters, Map<String, String> lookupDependsOnPaths,
+        String searchText, Context context) {
+
+        return List.of(
+            option("option1", "1"),
+            option("option2", "2"),
+            option("option3", "3"),
+            option("option4", "4"));
+    }
 
     private PropertyTestingAction() {
     }
