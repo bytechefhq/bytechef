3a3548d1d2aba5e1aca10c9125afea63a9ac61c0
Matija Petanjek <matija.petanjek7@gmail.com>
2026-02-24 12:47:22 +0100

    1664 add TenantId to MDC for logging

--- server/libs/core/tenant/tenant-api/build.gradle.kts
+++ server/libs/core/tenant/tenant-api/build.gradle.kts
@@ -1,6 +1,7 @@
 dependencies {
     implementation("io.micrometer:context-propagation")
     implementation("org.apache.commons:commons-lang3")
+    implementation("org.slf4j:slf4j-api")
     implementation("org.springframework.boot:spring-boot-autoconfigure")
     implementation(project(":server:libs:core:commons:commons-util"))
 }
