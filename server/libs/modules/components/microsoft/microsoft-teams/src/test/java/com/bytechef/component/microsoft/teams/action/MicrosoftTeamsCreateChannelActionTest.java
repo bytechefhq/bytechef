144b8b98d2b269025c98434252faea1714164355
Nikolina Spehar <speharnikolina87@gmail.com>
2026-03-04 16:02:17 +0100

    909 - fixed tests

--- server/libs/modules/components/microsoft/microsoft-teams/src/test/java/com/bytechef/component/microsoft/teams/action/MicrosoftTeamsCreateChannelActionTest.java
+++ server/libs/modules/components/microsoft/microsoft-teams/src/test/java/com/bytechef/component/microsoft/teams/action/MicrosoftTeamsCreateChannelActionTest.java
@@ -18,32 +18,68 @@ package com.bytechef.component.microsoft.teams.action;
 
 import static com.bytechef.component.microsoft.teams.constant.MicrosoftTeamsConstants.DESCRIPTION;
 import static com.bytechef.component.microsoft.teams.constant.MicrosoftTeamsConstants.DISPLAY_NAME;
+import static com.bytechef.component.microsoft.teams.constant.MicrosoftTeamsConstants.TEAM_ID;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentCaptor.forClass;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
+import com.bytechef.component.definition.Context;
+import com.bytechef.component.definition.Context.ContextFunction;
 import com.bytechef.component.definition.Context.Http;
+import com.bytechef.component.definition.Context.Http.Configuration;
+import com.bytechef.component.definition.Context.Http.Configuration.ConfigurationBuilder;
+import com.bytechef.component.definition.Context.Http.Executor;
+import com.bytechef.component.definition.Context.Http.Response;
+import com.bytechef.component.definition.Context.Http.ResponseType;
+import com.bytechef.component.definition.Parameters;
+import com.bytechef.component.definition.TypeReference;
+import com.bytechef.component.test.definition.MockParametersFactory;
+import com.bytechef.component.test.definition.extension.MockContextSetupExtension;
 import java.util.Map;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 
 /**
  * @author Monika Domiter
  */
-class MicrosoftTeamsCreateChannelActionTest extends AbstractMicrosoftTeamsActionTest {
+@ExtendWith(MockContextSetupExtension.class)
+class MicrosoftTeamsCreateChannelActionTest {
+
+    private final ArgumentCaptor<Http.Body> bodyArgumentCaptor = forClass(Http.Body.class);
+    private final Parameters mockedParameters = MockParametersFactory.create(
+        Map.of(DISPLAY_NAME, "new channel", DESCRIPTION, "channel description", TEAM_ID, "teamId"));
+    private final Map<String, Object> responseMap = Map.of("key", "value");
+    private final ArgumentCaptor<String> stringArgumentCaptor = forClass(String.class);
 
     @Test
-    void testPerform() {
-        when(mockedParameters.getRequiredString(DISPLAY_NAME))
-            .thenReturn("new channel");
-        when(mockedParameters.getString(DESCRIPTION))
-            .thenReturn("channel description");
+    void testPerform(
+        Context context, Response mockedResponse, Executor mockedExecutor, Http mockedHttp,
+        ArgumentCaptor<ContextFunction<Http, Executor>> httpFunctionArgumentCaptor,
+        ArgumentCaptor<ConfigurationBuilder> configurationBuilderArgumentCaptor) {
 
-        Object result = MicrosoftTeamsCreateChannelAction.perform(mockedParameters, mockedParameters, mockedContext);
+        when(mockedHttp.post(stringArgumentCaptor.capture()))
+            .thenReturn(mockedExecutor);
+        when(mockedExecutor.body(bodyArgumentCaptor.capture()))
+            .thenReturn(mockedExecutor);
+        when(mockedResponse.getBody(any(TypeReference.class)))
+            .thenReturn(responseMap);
+
+        Object result = MicrosoftTeamsCreateChannelAction.perform(mockedParameters, mockedParameters, context);
 
         assertEquals(responseMap, result);
 
         Http.Body body = bodyArgumentCaptor.getValue();
 
         assertEquals(Map.of(DISPLAY_NAME, "new channel", DESCRIPTION, "channel description"), body.getContent());
-    }
+        assertNotNull(httpFunctionArgumentCaptor.getValue());
 
+        ConfigurationBuilder configurationBuilder = configurationBuilderArgumentCaptor.getValue();
+        Configuration configuration = configurationBuilder.build();
+
+        assertEquals(ResponseType.JSON, configuration.getResponseType());
+        assertEquals("/teams/teamId/channels", stringArgumentCaptor.getValue());
+    }
 }
