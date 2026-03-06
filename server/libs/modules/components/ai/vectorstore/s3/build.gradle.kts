d53986da5c059ae3f8a19d396b73d6e8407e4fee
mkriskovic <marko.kriskovic@protonmail.com>
2026-03-06 16:16:45 +0100

    1344 - s3
    (cherry picked from commit f31518d2644791c31cf42afbc16f66931e47bdce)

---
+++ server/libs/modules/components/ai/vectorstore/s3/build.gradle.kts
@@ -0,0 +1,6 @@
+version="1.0"
+
+dependencies {
+    implementation("org.springframework.ai:spring-ai-vector-store")
+    implementation("software.amazon.awssdk:s3")
+}
---
+++ server/libs/modules/components/ai/vectorstore/s3/src/main/java/com/bytechef/component/ai/vectorstore/s3/S3VectorStoreComponentHandler.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2025 ByteChef
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef.component.ai.vectorstore.s3;
+
+import static com.bytechef.component.ai.vectorstore.s3.constant.S3VectorStoreConstants.S3_VECTOR_STORE;
+import static com.bytechef.component.definition.ComponentDsl.component;
+
+import com.bytechef.component.ComponentHandler;
+import com.bytechef.component.ai.vectorstore.s3.action.S3VectorStoreLoadAction;
+import com.bytechef.component.ai.vectorstore.s3.action.S3VectorStoreSearchAction;
+import com.bytechef.component.ai.vectorstore.s3.cluster.S3VectorStoreClusterElement;
+import com.bytechef.component.ai.vectorstore.s3.connection.S3VectorStoreConnection;
+import com.bytechef.component.definition.ComponentCategory;
+import com.bytechef.component.definition.ComponentDefinition;
+import com.bytechef.platform.component.definition.AbstractComponentDefinitionWrapper;
+import com.bytechef.platform.component.definition.VectorStoreComponentDefinition;
+import com.bytechef.platform.component.service.ClusterElementDefinitionService;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Marko Krišković
+ */
+@Component(S3_VECTOR_STORE + "_v1_ComponentHandler")
+public class S3VectorStoreComponentHandler implements ComponentHandler {
+
+    private final VectorStoreComponentDefinition componentDefinition;
+
+    public S3VectorStoreComponentHandler(ClusterElementDefinitionService clusterElementDefinitionService) {
+        this.componentDefinition = new S3VectorStoreComponentDefinitionImpl(
+            component(S3_VECTOR_STORE)
+                .title("S3 Vector Store")
+                .description(
+                    "Amazon S3 Vector Store uses AWS S3 as a persistent storage backend for vector embeddings, " +
+                        "enabling scalable and durable similarity search.")
+                .icon("path:assets/s3-vector-store.svg")
+                .connection(S3VectorStoreConnection.CONNECTION_DEFINITION)
+                .categories(ComponentCategory.ARTIFICIAL_INTELLIGENCE)
+                .actions(
+                    S3VectorStoreLoadAction.of(clusterElementDefinitionService),
+                    S3VectorStoreSearchAction.of(clusterElementDefinitionService))
+                .clusterElements(
+                    S3VectorStoreClusterElement.of(clusterElementDefinitionService)));
+    }
+
+    @Override
+    public ComponentDefinition getDefinition() {
+        return componentDefinition;
+    }
+
+    private static class S3VectorStoreComponentDefinitionImpl extends AbstractComponentDefinitionWrapper
+        implements VectorStoreComponentDefinition {
+
+        public S3VectorStoreComponentDefinitionImpl(ComponentDefinition componentDefinition) {
+            super(componentDefinition);
+        }
+    }
+}
