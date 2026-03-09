265a155f5c17ea0d6e7d65276b36e7782933a685
mkriskovic <marko.kriskovic@protonmail.com>
2026-03-09 16:44:20 +0100

    4397 - moved files to other modules

---
+++ server/libs/platform/platform-workflow/platform-workflow-validator/platform-workflow-validator-service/build.gradle.kts
@@ -0,0 +1,11 @@
+dependencies {
+    api(project(":server:libs:platform:platform-workflow:platform-workflow-validator:platform-workflow-validator-api"))
+    api(project(":server:libs:platform:platform-api"))
+    api("tools.jackson.core:jackson-databind")
+
+    implementation("org.apache.commons:commons-lang3")
+    implementation("org.slf4j:slf4j-api")
+    implementation("org.springframework:spring-core")
+    implementation(project(":server:libs:core:commons:commons-util"))
+    implementation(project(":server:libs:core:evaluator:evaluator-impl"))
+}
