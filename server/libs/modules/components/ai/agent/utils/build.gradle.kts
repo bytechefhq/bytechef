e37a5561da4181d47403afe26fda7f626844da61
Ivica Cardic <ivica.cardic@gmail.com>
2026-03-09 22:55:51 +0100

    4482 AI Agent Utils

---
+++ server/libs/modules/components/ai/agent/utils/build.gradle.kts
@@ -0,0 +1,5 @@
+dependencies {
+    implementation(libs.org.springaicommunity.spring.ai.agent.utils)
+    implementation(project(":server:libs:core:commons:commons-util"))
+    implementation(project(":server:libs:platform:platform-component:platform-component-api"))
+}
