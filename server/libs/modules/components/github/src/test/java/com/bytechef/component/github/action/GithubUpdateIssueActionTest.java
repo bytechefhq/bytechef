7b9e4b1de8c8c568d89f0506746cb2efba83350d
ivonapavela <ipavel00@fesb.hr>
2026-03-10 11:01:07 +0100

    3123 - added SearchCodeAction and updateIssueAction

---
+++ server/libs/modules/components/github/src/test/java/com/bytechef/component/github/action/GithubUpdateIssueActionTest.java
@@ -0,0 +1,101 @@
+/*
+ * Copyright 2025 ByteChef
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef.component.github.action;
+
+import static com.bytechef.component.definition.HttpStatus.OK;
+import static com.bytechef.component.github.constant.GithubConstants.ASSIGNEES;
+import static com.bytechef.component.github.constant.GithubConstants.BODY;
+import static com.bytechef.component.github.constant.GithubConstants.ISSUE;
+import static com.bytechef.component.github.constant.GithubConstants.LABELS;
+import static com.bytechef.component.github.constant.GithubConstants.MILESTONE;
+import static com.bytechef.component.github.constant.GithubConstants.OWNER;
+import static com.bytechef.component.github.constant.GithubConstants.REPOSITORY;
+import static com.bytechef.component.github.constant.GithubConstants.STATE;
+import static com.bytechef.component.github.constant.GithubConstants.TITLE;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentCaptor.forClass;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+import com.bytechef.component.definition.Context;
+import com.bytechef.component.definition.Context.ContextFunction;
+import com.bytechef.component.definition.Context.Http;
+import com.bytechef.component.definition.Context.Http.Body;
+import com.bytechef.component.definition.Context.Http.Configuration.ConfigurationBuilder;
+import com.bytechef.component.definition.Context.Http.Executor;
+import com.bytechef.component.definition.Context.Http.Response;
+import com.bytechef.component.definition.Parameters;
+import com.bytechef.component.definition.TypeReference;
+import com.bytechef.component.test.definition.MockParametersFactory;
+import com.bytechef.component.test.definition.extension.MockContextSetupExtension;
+import java.util.List;
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+
+/**
+ * @author Ivona Pavela
+ */
+@ExtendWith(MockContextSetupExtension.class)
+class GithubUpdateIssueActionTest {
+
+    private final ArgumentCaptor<Body> bodyArgumentCaptor = forClass(Http.Body.class);
+    private final Parameters mockedParameters = MockParametersFactory.create(
+        Map.of(OWNER, "testOwner", REPOSITORY, "testRepo", ISSUE, "testIssue", TITLE, "testTitle",
+            BODY, "testBody", STATE, "open", MILESTONE, "1", LABELS, List.of("test", "bug"),
+            ASSIGNEES, List.of("testAssignee")));
+    private final ArgumentCaptor<String> stringArgumentCaptor = forClass(String.class);
+
+    @Test
+    void testPerform(
+        Context mockedContext, Response mockedResponse, Executor mockedExecutor, Http mockedHttp,
+        ArgumentCaptor<ContextFunction<Http, Executor>> httpFunctionArgumentCaptor,
+        ArgumentCaptor<ConfigurationBuilder> configurationBuilderArgumentCaptor) {
+
+        String json =
+            "{\"title\":\"testTitle\",\"body\":\"testBody\",\"state\":\"open\",\"labels\":[\"test\",\"bug\"],\"assignees\":[\"testAssignee\"],\"milestone\":1}";
+
+        when(mockedHttp.patch(stringArgumentCaptor.capture()))
+            .thenReturn(mockedExecutor);
+        when(mockedExecutor.body(bodyArgumentCaptor.capture()))
+            .thenReturn(mockedExecutor);
+        when(mockedResponse.getBody(any(TypeReference.class)))
+            .thenReturn(Map.of(OK, true));
+        when(mockedContext.json(any()))
+            .thenReturn(json);
+
+        Object result = GithubUpdateIssueAction.perform(mockedParameters, null, mockedContext);
+
+        assertEquals(Map.of(OK, true), result);
+
+        ContextFunction<Http, Http.Executor> capturedFunction = httpFunctionArgumentCaptor.getValue();
+
+        assertNotNull(capturedFunction);
+
+        Http.Configuration.ConfigurationBuilder configurationBuilder = configurationBuilderArgumentCaptor.getValue();
+        Http.Configuration configuration = configurationBuilder.build();
+        Http.ResponseType responseType = configuration.getResponseType();
+
+        assertEquals(Http.ResponseType.Type.JSON, responseType.getType());
+        assertEquals("/repos/testOwner/testRepo/issues/testIssue", stringArgumentCaptor.getValue());
+
+        assertEquals(Http.Body.of(json), bodyArgumentCaptor.getValue());
+    }
+
+}
