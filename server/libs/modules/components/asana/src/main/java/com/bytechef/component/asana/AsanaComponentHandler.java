26ecd0138d1ebe1ebfba63e6849e0373a43d8b89
Nikolina Spehar <speharnikolina87@gmail.com>
2026-03-12 15:58:13 +0100

    884 - added custom action, version, help links and display conditions

--- server/libs/modules/components/asana/src/main/java/com/bytechef/component/asana/AsanaComponentHandler.java
+++ server/libs/modules/components/asana/src/main/java/com/bytechef/component/asana/AsanaComponentHandler.java
@@ -17,9 +17,23 @@
 package com.bytechef.component.asana;
 
 import com.bytechef.component.OpenApiComponentHandler;
+import com.bytechef.component.definition.ActionDefinition;
 import com.bytechef.component.definition.ComponentCategory;
+import com.bytechef.component.definition.ComponentDsl.ModifiableActionDefinition;
+import com.bytechef.component.definition.ComponentDsl.ModifiableArrayProperty;
 import com.bytechef.component.definition.ComponentDsl.ModifiableComponentDefinition;
+import com.bytechef.component.definition.ComponentDsl.ModifiableConnectionDefinition;
+import com.bytechef.component.definition.ComponentDsl.ModifiableIntegerProperty;
+import com.bytechef.component.definition.ComponentDsl.ModifiableNumberProperty;
+import com.bytechef.component.definition.ComponentDsl.ModifiableObjectProperty;
+import com.bytechef.component.definition.ComponentDsl.ModifiableProperty;
+import com.bytechef.component.definition.ComponentDsl.ModifiableStringProperty;
+import com.bytechef.component.definition.Property.ValueProperty;
+import com.bytechef.definition.BaseProperty;
 import com.google.auto.service.AutoService;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 /**
  * @author Monika Domiter
@@ -32,6 +46,103 @@ public class AsanaComponentHandler extends AbstractAsanaComponentHandler {
         return modifiableComponentDefinition
             .customAction(true)
             .icon("path:assets/asana.svg")
-            .categories(ComponentCategory.PROJECT_MANAGEMENT);
+            .categories(ComponentCategory.PROJECT_MANAGEMENT)
+            .customActionHelp(
+                "Asana Web API documentation", "https://developers.asana.com/docs/api-explorer")
+            .version(1);
+    }
+
+    @Override
+    public List<ModifiableActionDefinition> modifyActions(ModifiableActionDefinition... actionDefinitions) {
+        for (ModifiableActionDefinition actionDefinition : actionDefinitions) {
+            String name = actionDefinition.getName();
+
+            switch (name) {
+                case "createCustomField" ->
+                    actionDefinition.help(
+                        "",
+                        "https://docs.bytechef.io/reference/components/asana_v1#create-custom-field");
+                case "createProject" ->
+                    actionDefinition.help(
+                        "",
+                        "https://docs.bytechef.io/reference/components/asana_v1#create-project");
+                case "createSubtask" ->
+                    actionDefinition.help(
+                        "",
+                        "https://docs.bytechef.io/reference/components/asana_v1#create-subtask");
+                case "createTask" ->
+                    actionDefinition.help(
+                        "",
+                        "https://docs.bytechef.io/reference/components/asana_v1#create-task");
+                default -> {
+                }
+            }
+        }
+
+        return super.modifyActions(actionDefinitions);
+    }
+
+    @Override
+    public ModifiableConnectionDefinition modifyConnection(
+        ModifiableConnectionDefinition modifiableConnectionDefinition) {
+
+        return modifiableConnectionDefinition
+            .help("", "https://docs.bytechef.io/reference/components/asana_v1#connection-setup")
+            .version(1);
+    }
+
+    @Override
+    public ModifiableProperty<?> modifyProperty(
+        ActionDefinition actionDefinition, ModifiableProperty<?> modifiableProperty) {
+
+        if (actionDefinition.getName()
+            .equals("createCustomField")
+            && Objects.equals(modifiableProperty.getName(), "data")) {
+
+            Optional<List<? extends ValueProperty<?>>> propertiesOptional =
+                ((ModifiableObjectProperty) modifiableProperty).getProperties();
+
+            for (BaseProperty baseProperty : propertiesOptional.get()) {
+
+                if (Objects.equals(baseProperty.getName(), "text_value")) {
+                    ((ModifiableStringProperty) baseProperty)
+                        .displayCondition("%s == '%s'".formatted("data.resource_subtype", "text"));
+                }
+
+                else if (Objects.equals(baseProperty.getName(), "enum_options")) {
+                    ((ModifiableArrayProperty) baseProperty)
+                        .displayCondition("%s == '%s' || %s == '%s'"
+                            .formatted("data.resource_subtype", "enum",
+                                "data.resource_subtype", "multi_enum"));
+                }
+
+                else if (Objects.equals(baseProperty.getName(), "number_value")) {
+                    ((ModifiableNumberProperty) baseProperty)
+                        .displayCondition("%s == '%s'".formatted("data.resource_subtype", "number"));
+                }
+
+                else if (Objects.equals(baseProperty.getName(), "precision")) {
+                    ((ModifiableIntegerProperty) baseProperty)
+                        .displayCondition("%s == '%s'".formatted("data.resource_subtype", "number"));
+                }
+
+                else if (Objects.equals(baseProperty.getName(), "date_value")) {
+                    ((ModifiableObjectProperty) baseProperty)
+                        .displayCondition("%s == '%s'".formatted("data.resource_subtype", "date"));
+                }
+
+                else if (Objects.equals(baseProperty.getName(), "people_value")) {
+                    ((ModifiableArrayProperty) baseProperty)
+                        .displayCondition("%s == '%s'".formatted("data.resource_subtype", "people"));
+                }
+
+                else if (Objects.equals(baseProperty.getName(), "reference_value")) {
+                    ((ModifiableArrayProperty) baseProperty)
+                        .displayCondition("%s == '%s'".formatted("data.resource_subtype", "reference"));
+                }
+            }
+        }
+
+        return modifiableProperty;
     }
 }
