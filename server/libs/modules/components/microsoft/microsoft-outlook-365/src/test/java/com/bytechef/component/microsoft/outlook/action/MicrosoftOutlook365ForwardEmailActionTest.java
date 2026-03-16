84908bb6c6dda914b16af5ee2055ae9f380c5c03
Monika Kušter <domitermonika@gmail.com>
2026-03-16 17:16:43 +0100

    4565 add tests

---
+++ server/libs/modules/components/microsoft/microsoft-outlook-365/src/test/java/com/bytechef/component/microsoft/outlook/action/MicrosoftOutlook365ForwardEmailActionTest.java
@@ -0,0 +1,95 @@
+/*
+ * Copyright 2025 ByteChef
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef.component.microsoft.outlook.action;
+
+import static com.bytechef.component.microsoft.outlook.constant.MicrosoftOutlook365Constants.ADDRESS;
+import static com.bytechef.component.microsoft.outlook.constant.MicrosoftOutlook365Constants.COMMENT;
+import static com.bytechef.component.microsoft.outlook.constant.MicrosoftOutlook365Constants.CONTENT;
+import static com.bytechef.component.microsoft.outlook.constant.MicrosoftOutlook365Constants.CONTENT_TYPE;
+import static com.bytechef.component.microsoft.outlook.constant.MicrosoftOutlook365Constants.EMAIL_ADDRESS;
+import static com.bytechef.component.microsoft.outlook.constant.MicrosoftOutlook365Constants.ID;
+import static com.bytechef.component.microsoft.outlook.constant.MicrosoftOutlook365Constants.TO_RECIPIENTS;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.ArgumentCaptor.forClass;
+import static org.mockito.Mockito.mockStatic;
+import static org.mockito.Mockito.when;
+
+import com.bytechef.component.definition.Context;
+import com.bytechef.component.definition.Context.ContextFunction;
+import com.bytechef.component.definition.Context.Http;
+import com.bytechef.component.definition.Context.Http.Body;
+import com.bytechef.component.definition.Context.Http.BodyContentType;
+import com.bytechef.component.definition.Context.Http.Executor;
+import com.bytechef.component.definition.Parameters;
+import com.bytechef.component.microsoft.outlook.constant.ContentType;
+import com.bytechef.component.microsoft.outlook.util.MicrosoftOutlook365Utils;
+import com.bytechef.component.test.definition.MockParametersFactory;
+import com.bytechef.component.test.definition.extension.MockContextSetupExtension;
+import java.util.List;
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.MockedStatic;
+
+/**
+ * @author Monika Kušter
+ */
+@ExtendWith(MockContextSetupExtension.class)
+class MicrosoftOutlook365ForwardEmailActionTest {
+
+    private final ArgumentCaptor<Body> bodyArgumentCaptor = forClass(Body.class);
+    @SuppressWarnings("rawtypes")
+    private final ArgumentCaptor<List> listArgumentCaptor = forClass(List.class);
+    private final Parameters mockedParameters = MockParametersFactory.create(
+        Map.of(ID, "xy", CONTENT_TYPE, ContentType.TEXT.name(), CONTENT, "test", TO_RECIPIENTS, List.of("address1")));
+    private final ArgumentCaptor<String> stringArgumentCaptor = forClass(String.class);
+
+    @Test
+    @SuppressWarnings("unchecked")
+    void testPerform(
+        Context mockedContext, Executor mockedExecutor, Http mockedHttp,
+        ArgumentCaptor<ContextFunction<Http, Executor>> httpFunctionArgumentCaptor) {
+
+        try (MockedStatic<MicrosoftOutlook365Utils> microsoftOutlook365UtilsMockedStatic =
+            mockStatic(MicrosoftOutlook365Utils.class)) {
+
+            microsoftOutlook365UtilsMockedStatic.when(
+                () -> MicrosoftOutlook365Utils.createRecipientList(listArgumentCaptor.capture()))
+                .thenReturn(List.of(Map.of(EMAIL_ADDRESS, Map.of(ADDRESS, "address1"))));
+
+            when(mockedHttp.post(stringArgumentCaptor.capture()))
+                .thenReturn(mockedExecutor);
+            when(mockedExecutor.body(bodyArgumentCaptor.capture()))
+                .thenReturn(mockedExecutor);
+
+            Object result = MicrosoftOutlook365ForwardEmailAction.perform(mockedParameters, null, mockedContext);
+
+            assertNull(result);
+            assertNotNull(httpFunctionArgumentCaptor.getValue());
+            assertEquals("/me/messages/xy/forward", stringArgumentCaptor.getValue());
+            assertEquals(List.of("address1"), listArgumentCaptor.getValue());
+
+            Map<String, Object> expectedBody = Map.of(
+                TO_RECIPIENTS, List.of(Map.of(EMAIL_ADDRESS, Map.of(ADDRESS, "address1"))), COMMENT, "test");
+
+            assertEquals(Body.of(expectedBody, BodyContentType.JSON), bodyArgumentCaptor.getValue());
+        }
+    }
+}
