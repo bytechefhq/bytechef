1c38fc4faae6dcd45e8048ec9611d7fe679320c9
marijahorvat171 <marijahorvat171@gmail.com>
2026-03-17 14:10:03 +0100

    996 - update tests

--- server/libs/modules/components/microsoft/microsoft-excel/src/test/java/com/bytechef/component/microsoft/excel/action/MicrosoftExcelDeleteRowActionTest.java
+++ server/libs/modules/components/microsoft/microsoft-excel/src/test/java/com/bytechef/component/microsoft/excel/action/MicrosoftExcelDeleteRowActionTest.java
@@ -17,32 +17,50 @@
 package com.bytechef.component.microsoft.excel.action;
 
 import static com.bytechef.component.microsoft.excel.constant.MicrosoftExcelConstants.ROW_NUMBER;
+import static com.bytechef.component.microsoft.excel.constant.MicrosoftExcelConstants.WORKBOOK_ID;
+import static com.bytechef.component.microsoft.excel.constant.MicrosoftExcelConstants.WORKSHEET_NAME;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentCaptor.forClass;
 import static org.mockito.Mockito.when;
 
+import com.bytechef.component.definition.Context;
+import com.bytechef.component.definition.Context.ContextFunction;
 import com.bytechef.component.definition.Context.Http;
+import com.bytechef.component.definition.Context.Http.Body;
+import com.bytechef.component.definition.Context.Http.Configuration;
+import com.bytechef.component.definition.Context.Http.Configuration.ConfigurationBuilder;
+import com.bytechef.component.definition.Context.Http.Executor;
+import com.bytechef.component.definition.Context.Http.ResponseType;
 import com.bytechef.component.definition.Parameters;
 import com.bytechef.component.microsoft.excel.util.MicrosoftExcelUtils;
 import com.bytechef.component.test.definition.MockParametersFactory;
+import com.bytechef.component.test.definition.extension.MockContextSetupExtension;
 import java.util.Map;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
 
 /**
  * @author Monika Kušter
  */
+@ExtendWith(MockContextSetupExtension.class)
 class MicrosoftExcelDeleteRowActionTest extends AbstractMicrosoftExcelActionTest {
 
-    private final ArgumentCaptor<Http.Body> bodyArgumentCaptor = ArgumentCaptor.forClass(Http.Body.class);
-    private final Parameters mockedParameters = MockParametersFactory.create(Map.of(ROW_NUMBER, 2));
+    private final ArgumentCaptor<Body> bodyArgumentCaptor = forClass(Body.class);
+    private final Parameters mockedParameters =
+        MockParametersFactory.create(
+            Map.of(WORKBOOK_ID, 1, WORKSHEET_NAME, "test", ROW_NUMBER, 2));
+    private final ArgumentCaptor<String> stringArgumentCaptor = forClass(String.class);
 
     @Test
-    void testPerform() {
-        when(mockedActionContext.http(any()))
-            .thenReturn(mockedExecutor);
-        when(mockedExecutor.configuration(any()))
+    void testPerform(
+        Context mockedContext, Executor mockedExecutor, Http mockedHttp,
+        ArgumentCaptor<ContextFunction<Http, Executor>> httpFunctionArgumentCaptor,
+        ArgumentCaptor<ConfigurationBuilder> configurationBuilderArgumentCaptor) {
+
+        when(mockedHttp.post(stringArgumentCaptor.capture()))
             .thenReturn(mockedExecutor);
         when(mockedExecutor.body(bodyArgumentCaptor.capture()))
             .thenReturn(mockedExecutor);
@@ -52,15 +70,23 @@ class MicrosoftExcelDeleteRowActionTest extends AbstractMicrosoftExcelActionTest
                 parametersArgumentCaptor.capture(), actionContextArgumentCaptor.capture()))
             .thenReturn("C");
 
-        Object result = MicrosoftExcelDeleteRowAction.perform(mockedParameters, mockedParameters, mockedActionContext);
+        Object result = MicrosoftExcelDeleteRowAction.perform(mockedParameters, mockedParameters, mockedContext);
 
         assertNull(result);
+        assertNotNull(httpFunctionArgumentCaptor.getValue());
 
-        Http.Body body = bodyArgumentCaptor.getValue();
+        ConfigurationBuilder configurationBuilder = configurationBuilderArgumentCaptor.getValue();
+        Configuration configuration = configurationBuilder.build();
+
+        assertEquals(ResponseType.JSON, configuration.getResponseType());
+        assertEquals(
+            "/me/drive/items/1/workbook/worksheets/test/range(address='A2:C2')/delete",
+            stringArgumentCaptor.getValue());
+
+        Body body = bodyArgumentCaptor.getValue();
 
         assertEquals(Map.of("shift", "Up"), body.getContent());
         assertEquals(mockedParameters, parametersArgumentCaptor.getValue());
-        assertEquals(mockedActionContext, actionContextArgumentCaptor.getValue());
+        assertEquals(mockedContext, actionContextArgumentCaptor.getValue());
     }
-
 }
