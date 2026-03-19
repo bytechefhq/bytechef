9d8cad0922603d871e8398abfe34ef553d411097
katarina-cala <katarinacala@gmail.com>
2026-03-19 17:24:19 +0100

    4328-switched to custom Button component, adjusted styling accordingly, removed wrappers and added indentation for nested items to AccordionContent

--- client/src/shared/components/workflow-executions/WorkflowExecutionsTriggerAccordionItem.tsx
+++ client/src/shared/components/workflow-executions/WorkflowExecutionsTriggerAccordionItem.tsx
@@ -1,3 +1,4 @@
+import Button from '@/components/Button/Button';
 import WorkflowTriggerExecutionItem from '@/shared/components/workflow-executions/WorkflowTriggerExecutionItem';
 import {TaskExecution, TriggerExecution} from '@/shared/middleware/automation/workflow/execution';
 import {twMerge} from 'tailwind-merge';
@@ -14,22 +15,22 @@ const WorkflowExecutionsTriggerAccordionItem = ({
     const isSelected = selectedItem?.id === triggerExecution.id;
 
     return (
-        <div className="pl-2">
-            <button
-                className={twMerge(
-                    'group flex w-full items-center justify-between rounded-md border border-stroke-neutral-primary p-2 text-left hover:border-stroke-brand-primary focus-visible:outline-stroke-brand-focus focus-visible:transition-colors',
-                    isSelected &&
-                        'border-stroke-brand-primary bg-surface-neutral-secondary hover:bg-surface-neutral-secondary'
-                )}
-                onClick={() => {
-                    if (!isSelected) {
-                        onTaskClick(triggerExecution);
-                    }
-                }}
-            >
-                <WorkflowTriggerExecutionItem triggerExecution={triggerExecution} />
-            </button>
-        </div>
+        <Button
+            className={twMerge(
+                'active:text-content-primary h-auto w-full justify-between rounded-md border border-stroke-neutral-primary p-2 text-left transition-colors hover:border-stroke-brand-primary hover:bg-transparent focus-visible:outline focus-visible:outline-2 focus-visible:-outline-offset-2 focus-visible:outline-stroke-brand-focus focus-visible:ring-0 focus-visible:transition-colors active:bg-transparent [&_svg]:size-5',
+                isSelected &&
+                    'border-stroke-brand-primary bg-surface-neutral-secondary hover:bg-surface-neutral-secondary active:bg-surface-neutral-secondary'
+            )}
+            onClick={() => {
+                if (!isSelected) {
+                    onTaskClick(triggerExecution);
+                }
+            }}
+            type="button"
+            variant="ghost"
+        >
+            <WorkflowTriggerExecutionItem triggerExecution={triggerExecution} />
+        </Button>
     );
 };
 
