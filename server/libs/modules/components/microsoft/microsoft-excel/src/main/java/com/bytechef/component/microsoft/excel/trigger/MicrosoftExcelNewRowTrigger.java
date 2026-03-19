a07401e138b1046e86cae3c88942ba337ed0d1e9
marijahorvat171 <marijahorvat171@gmail.com>
2026-03-19 10:24:30 +0100

    996 - add new row trigger

---
+++ server/libs/modules/components/microsoft/microsoft-excel/src/main/java/com/bytechef/component/microsoft/excel/trigger/MicrosoftExcelNewRowTrigger.java
@@ -0,0 +1,95 @@
+/*
+ * Copyright 2025 ByteChef
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef.component.microsoft.excel.trigger;
+
+import static com.bytechef.component.definition.ComponentDsl.string;
+import static com.bytechef.component.definition.ComponentDsl.trigger;
+import static com.bytechef.component.microsoft.excel.constant.MicrosoftExcelConstants.IS_THE_FIRST_ROW_HEADER_PROPERTY;
+import static com.bytechef.component.microsoft.excel.constant.MicrosoftExcelConstants.WORKBOOK_ID;
+import static com.bytechef.component.microsoft.excel.constant.MicrosoftExcelConstants.WORKSHEET_NAME;
+
+import com.bytechef.component.definition.ComponentDsl.ModifiableTriggerDefinition;
+import com.bytechef.component.definition.Parameters;
+import com.bytechef.component.definition.TriggerContext;
+import com.bytechef.component.definition.TriggerDefinition.OptionsFunction;
+import com.bytechef.component.definition.TriggerDefinition.PollOutput;
+import com.bytechef.component.definition.TriggerDefinition.TriggerType;
+import com.bytechef.component.microsoft.excel.util.MicrosoftExcelRowUtils;
+import com.bytechef.component.microsoft.excel.util.MicrosoftExcelUtils;
+import com.bytechef.microsoft.commons.MicrosoftUtils;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author Marija Horvat
+ */
+public class MicrosoftExcelNewRowTrigger {
+
+    public static final ModifiableTriggerDefinition TRIGGER_DEFINITION = trigger("newRow")
+        .title("New Row")
+        .description("Triggers when a new row is added.")
+        .help("", "https://docs.bytechef.io/reference/components/microsoft-excel_v1#new-row")
+        .type(TriggerType.POLLING)
+        .properties(
+            string(WORKBOOK_ID)
+                .label("Workbook ID")
+                .description("The ID of the workbook.")
+                .options((OptionsFunction<String>) MicrosoftExcelUtils::getWorkbookIdOptions)
+                .required(true),
+            string(WORKSHEET_NAME)
+                .label("Worksheet")
+                .description("The name of the worksheet.")
+                .options((OptionsFunction<String>) MicrosoftExcelUtils::getWorksheetNameOptions)
+                .optionsLookupDependsOn(WORKBOOK_ID)
+                .required(true),
+            IS_THE_FIRST_ROW_HEADER_PROPERTY)
+        .output()
+        .poll(MicrosoftExcelNewRowTrigger::poll)
+        .processErrorResponse(MicrosoftUtils::processErrorResponse);
+
+    private MicrosoftExcelNewRowTrigger() {
+    }
+
+    protected static PollOutput poll(
+        Parameters inputParameters, Parameters connectionParameters, Parameters closureParameters,
+        TriggerContext context) {
+
+        int lastRowIndex = closureParameters.get("lastRowIndex") != null
+            ? (Integer) closureParameters.get("lastRowIndex") : 0;
+
+        int currentRowCount = MicrosoftExcelUtils.getLastUsedRowIndex(inputParameters, context);
+
+        if (currentRowCount <= lastRowIndex) {
+            return new PollOutput(List.of(), Map.of("lastRowIndex", lastRowIndex), false);
+        }
+
+        List<Map<String, Object>> newRows = new ArrayList<>();
+
+        for (int rowNum = lastRowIndex + 1; rowNum <= currentRowCount; rowNum++) {
+            List<Object> row = MicrosoftExcelRowUtils.getRowFromWorksheet(inputParameters, context, rowNum);
+
+            if (!row.isEmpty()) {
+                Map<String, Object> mappedRow =
+                    MicrosoftExcelUtils.getMapOfValuesForRow(inputParameters, context, row);
+                newRows.add(mappedRow);
+            }
+        }
+
+        return new PollOutput(newRows, Map.of("lastRowIndex", currentRowCount), false);
+    }
+}
---
+++ server/libs/modules/components/microsoft/microsoft-excel/src/test/java/com/bytechef/component/microsoft/excel/trigger/MicrosoftExcelNewRowTriggerTest.java
@@ -0,0 +1,93 @@
+/*
+ * Copyright 2025 ByteChef
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef.component.microsoft.excel.trigger;
+
+import static com.bytechef.component.microsoft.excel.constant.MicrosoftExcelConstants.IS_THE_FIRST_ROW_HEADER;
+import static com.bytechef.component.microsoft.excel.constant.MicrosoftExcelConstants.WORKBOOK_ID;
+import static com.bytechef.component.microsoft.excel.constant.MicrosoftExcelConstants.WORKSHEET_NAME;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentCaptor.forClass;
+import static org.mockito.Mockito.mockStatic;
+
+import com.bytechef.component.definition.Parameters;
+import com.bytechef.component.definition.TriggerContext;
+import com.bytechef.component.definition.TriggerDefinition.PollOutput;
+import com.bytechef.component.microsoft.excel.util.MicrosoftExcelRowUtils;
+import com.bytechef.component.microsoft.excel.util.MicrosoftExcelUtils;
+import com.bytechef.component.test.definition.MockParametersFactory;
+import com.bytechef.component.test.definition.extension.MockContextSetupExtension;
+import java.util.List;
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.MockedStatic;
+
+/**
+ * @author Marija Horvat
+ */
+@ExtendWith(MockContextSetupExtension.class)
+class MicrosoftExcelNewRowTriggerTest {
+
+    private final Parameters mockedClosureParameters = MockParametersFactory.create(Map.of("lastRowIndex", 1));
+    private final Parameters mockedInputParameters =
+        MockParametersFactory.create(
+            Map.of(WORKBOOK_ID, 1, WORKSHEET_NAME, "test", IS_THE_FIRST_ROW_HEADER, true));
+    private final ArgumentCaptor<Integer> integerArgumentCaptor = forClass(Integer.class);
+    @SuppressWarnings("rawtypes")
+    private final ArgumentCaptor<List> listArgumentCaptor = forClass(List.class);
+    private final ArgumentCaptor<Parameters> parametersArgumentCaptor = forClass(Parameters.class);
+    private final ArgumentCaptor<TriggerContext> triggerContextArgumentCaptor = forClass(TriggerContext.class);
+
+    @Test
+    void testPoll(TriggerContext mockedTriggerContext) {
+
+        List<Object> row = List.of("abc", "sheetName", false);
+        Map<String, Object> map = Map.of("key", "value");
+
+        try (MockedStatic<MicrosoftExcelUtils> microsoftExcelUtilsMockedStatic = mockStatic(MicrosoftExcelUtils.class);
+            MockedStatic<MicrosoftExcelRowUtils> microsoftExcelRowUtilsMockedStatic =
+                mockStatic(MicrosoftExcelRowUtils.class)) {
+
+            microsoftExcelUtilsMockedStatic
+                .when(() -> MicrosoftExcelUtils.getLastUsedRowIndex(parametersArgumentCaptor.capture(),
+                    triggerContextArgumentCaptor.capture()))
+                .thenReturn(2);
+            microsoftExcelRowUtilsMockedStatic
+                .when(() -> MicrosoftExcelRowUtils.getRowFromWorksheet(
+                    parametersArgumentCaptor.capture(), triggerContextArgumentCaptor.capture(),
+                    integerArgumentCaptor.capture()))
+                .thenReturn(row);
+            microsoftExcelUtilsMockedStatic
+                .when(() -> MicrosoftExcelUtils.getMapOfValuesForRow(
+                    parametersArgumentCaptor.capture(), triggerContextArgumentCaptor.capture(),
+                    listArgumentCaptor.capture()))
+                .thenReturn(map);
+
+            PollOutput result = MicrosoftExcelNewRowTrigger.poll(
+                mockedInputParameters, null, mockedClosureParameters, mockedTriggerContext);
+
+            assertEquals(new PollOutput(List.of(map), Map.of("lastRowIndex", 2), false), result);
+            assertEquals(List.of(mockedInputParameters, mockedInputParameters, mockedInputParameters),
+                parametersArgumentCaptor.getAllValues());
+            assertEquals(List.of(mockedTriggerContext, mockedTriggerContext, mockedTriggerContext),
+                triggerContextArgumentCaptor.getAllValues());
+            assertEquals(2, integerArgumentCaptor.getValue());
+            assertEquals(row, listArgumentCaptor.getValue());
+        }
+    }
+}
