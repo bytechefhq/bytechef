2665b1e1b6a452f80949e3d7b82e10817195cae5
Ivica Cardic <ivica.cardic@gmail.com>
2026-03-23 22:04:22 +0100

    0 client - Upgrade dependencies and refactor MDX components

--- docs/components/code-block.tsx
+++ docs/components/code-block.tsx
@@ -1,7 +1,6 @@
 import * as Base from 'fumadocs-ui/components/codeblock';
-import { getHighlighter, hastToJsx } from 'fumadocs-core/highlight';
+import { highlight } from 'fumadocs-core/highlight';
 import { cn } from '@/lib/cn';
-import type { BundledLanguage } from 'shiki';
 
 export interface CodeBlockProps {
   code: string;
@@ -9,23 +8,14 @@ export interface CodeBlockProps {
   lang: string;
 }
 
-const highlighter = await getHighlighter('js', {
-  langs: ['js', 'ts', 'jsx', 'tsx'],
-  themes: ['vesper', 'github-light'],
-});
-
 export async function CodeBlock({ code, lang, wrapper }: CodeBlockProps) {
-  await highlighter.loadLanguage(lang as BundledLanguage);
-  const hast = highlighter.codeToHast(code, {
+  const rendered = await highlight(code, {
     lang,
     defaultColor: false,
     themes: {
       light: 'github-light',
       dark: 'vesper',
     },
-  });
-
-  const rendered = hastToJsx(hast, {
     components: {
       pre: Base.Pre,
     },
