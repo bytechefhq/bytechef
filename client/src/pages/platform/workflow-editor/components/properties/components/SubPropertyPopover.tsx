3b49520d1c37f3a07315c7b2b87f5ee4acd1b706
Krešimir Čoko <kresimircoko@gmail.com>
2026-03-24 10:01:29 +0100

    4150 - ArrayProperty improvements, bugfixes and code organization

--- client/src/pages/platform/workflow-editor/components/properties/components/SubPropertyPopover.tsx
+++ client/src/pages/platform/workflow-editor/components/properties/components/SubPropertyPopover.tsx
@@ -80,7 +80,10 @@ const SubPropertyPopover = ({
                 />
             </PopoverTrigger>
 
-            <PopoverContent className="min-w-sub-property-popover-width space-y-4 p-4">
+            <PopoverContent
+                aria-label={`${array ? 'Array' : 'Object'} property popover`}
+                className="min-w-sub-property-popover-width space-y-4 p-4"
+            >
                 <header className="flex items-center justify-between">
                     <span className="font-semibold">Add {array ? 'array item' : 'object property'}</span>
 
