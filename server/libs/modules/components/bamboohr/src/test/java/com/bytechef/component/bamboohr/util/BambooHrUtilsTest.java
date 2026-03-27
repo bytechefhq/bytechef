fbb4763daa089465d2d8c906d07f0b66b6086ff5
marijahorvat171 <marijahorvat171@gmail.com>
2026-03-27 09:56:06 +0100

    2285 - fix tests

--- server/libs/modules/components/bamboohr/src/test/java/com/bytechef/component/bamboohr/util/BambooHrUtilsTest.java
+++ server/libs/modules/components/bamboohr/src/test/java/com/bytechef/component/bamboohr/util/BambooHrUtilsTest.java
@@ -16,44 +16,149 @@
 
 package com.bytechef.component.bamboohr.util;
 
+import static com.bytechef.component.bamboohr.constant.BambooHrConstants.ID;
 import static com.bytechef.component.bamboohr.constant.BambooHrConstants.NAME;
 import static com.bytechef.component.definition.ComponentDsl.option;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentCaptor.forClass;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import com.bytechef.component.definition.ActionContext;
+import com.bytechef.component.definition.Context.ContextFunction;
 import com.bytechef.component.definition.Context.Http;
+import com.bytechef.component.definition.Context.Http.Configuration;
+import com.bytechef.component.definition.Context.Http.Configuration.ConfigurationBuilder;
+import com.bytechef.component.definition.Context.Http.Executor;
+import com.bytechef.component.definition.Context.Http.Response;
+import com.bytechef.component.definition.Context.Http.ResponseType;
 import com.bytechef.component.definition.Option;
 import com.bytechef.component.definition.Parameters;
 import com.bytechef.component.definition.TypeReference;
+import com.bytechef.component.test.definition.MockParametersFactory;
+import com.bytechef.component.test.definition.extension.MockContextSetupExtension;
 import java.util.List;
 import java.util.Map;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
 
 /**
  * @author Marija Horvat
  */
+@ExtendWith(MockContextSetupExtension.class)
 class BambooHrUtilsTest {
 
-    private final ActionContext mockedActionContext = mock(ActionContext.class);
-    private final Http.Executor mockedExecutor = mock(Http.Executor.class);
-    private final Http.Response mockedResponse = mock(Http.Response.class);
-    private final Parameters mockedParameters = mock(Parameters.class);
-    private final ArgumentCaptor<String> stringArgumentCaptor = ArgumentCaptor.forClass(String.class);
+    private final ArgumentCaptor<String> stringArgumentCaptor = forClass(String.class);
 
     @Test
-    void testGetOptions() throws Exception {
-        when(mockedActionContext.http(any()))
+    void testGetEmployeeIdOptions(
+        ActionContext mockedContext, Response mockedResponse, Executor mockedExecutor, Http mockedHttp,
+        ArgumentCaptor<ContextFunction<Http, Executor>> httpFunctionArgumentCaptor,
+        ArgumentCaptor<ConfigurationBuilder> configurationBuilderArgumentCaptor) {
+
+        when(mockedHttp.get(stringArgumentCaptor.capture()))
             .thenReturn(mockedExecutor);
         when(mockedExecutor.header(stringArgumentCaptor.capture(), stringArgumentCaptor.capture()))
             .thenReturn(mockedExecutor);
-        when(mockedExecutor.configuration(any()))
+        when(mockedResponse.getBody(any(TypeReference.class)))
+            .thenReturn(Map.of("employees", List.of(Map.of(ID, "1", "displayName", "name"))));
+
+        List<? extends Option<String>> result = BambooHrUtils.getEmployeeIdOptions(
+            null, null, null, null, mockedContext);
+
+        assertEquals(List.of(option("name - 1", "1")), result);
+        assertEquals(List.of("/employees/directory", "accept", "application/json"),
+            stringArgumentCaptor.getAllValues());
+        ContextFunction<Http, Executor> capturedFunction = httpFunctionArgumentCaptor.getValue();
+
+        assertNotNull(capturedFunction);
+
+        ConfigurationBuilder configurationBuilder = configurationBuilderArgumentCaptor.getValue();
+
+        Configuration configuration = configurationBuilder.build();
+
+        ResponseType responseType = configuration.getResponseType();
+
+        assertEquals(ResponseType.Type.JSON, responseType.getType());
+    }
+
+    @Test
+    void testGetEmployeeFilesIdOptions(
+        ActionContext mockedContext, Response mockedResponse, Executor mockedExecutor, Http mockedHttp,
+        ArgumentCaptor<ContextFunction<Http, Executor>> httpFunctionArgumentCaptor,
+        ArgumentCaptor<ConfigurationBuilder> configurationBuilderArgumentCaptor) {
+
+        Parameters mockedParameters = MockParametersFactory.create(Map.of(ID, "1"));
+
+        when(mockedHttp.get(stringArgumentCaptor.capture()))
             .thenReturn(mockedExecutor);
-        when(mockedExecutor.execute())
-            .thenReturn(mockedResponse);
+        when(mockedExecutor.header(stringArgumentCaptor.capture(), stringArgumentCaptor.capture()))
+            .thenReturn(mockedExecutor);
+        when(mockedResponse.getBody(any(TypeReference.class)))
+            .thenReturn(Map.of("category", List.of(Map.of(ID, "1", NAME, "name"))));
+
+        List<? extends Option<String>> result = BambooHrUtils.getEmployeeFilesIdOptions(
+            mockedParameters, null, null, null, mockedContext);
+
+        assertEquals(List.of(option("name", "1")), result);
+        assertEquals(List.of("/employees/1/files/view", "accept", "application/xml"),
+            stringArgumentCaptor.getAllValues());
+        ContextFunction<Http, Executor> capturedFunction = httpFunctionArgumentCaptor.getValue();
+
+        assertNotNull(capturedFunction);
+
+        ConfigurationBuilder configurationBuilder = configurationBuilderArgumentCaptor.getValue();
+
+        Configuration configuration = configurationBuilder.build();
+
+        ResponseType responseType = configuration.getResponseType();
+
+        assertEquals(ResponseType.Type.XML, responseType.getType());
+    }
+
+    @Test
+    void testGetFieldOptions(
+        ActionContext mockedContext, Response mockedResponse, Executor mockedExecutor, Http mockedHttp,
+        ArgumentCaptor<ContextFunction<Http, Executor>> httpFunctionArgumentCaptor,
+        ArgumentCaptor<ConfigurationBuilder> configurationBuilderArgumentCaptor) {
+
+        when(mockedHttp.get(stringArgumentCaptor.capture()))
+            .thenReturn(mockedExecutor);
+        when(mockedExecutor.header(stringArgumentCaptor.capture(), stringArgumentCaptor.capture()))
+            .thenReturn(mockedExecutor);
+        when(mockedResponse.getBody(any(TypeReference.class)))
+            .thenReturn(List.of(Map.of(NAME, "name", "alias", "alias")));
+
+        List<? extends Option<String>> result = BambooHrUtils.getFieldOptions(
+            null, null, null, null, mockedContext);
+
+        assertEquals(List.of(option("name", "alias")), result);
+        assertEquals(List.of("/meta/fields", "accept", "application/json"), stringArgumentCaptor.getAllValues());
+        ContextFunction<Http, Executor> capturedFunction = httpFunctionArgumentCaptor.getValue();
+
+        assertNotNull(capturedFunction);
+
+        ConfigurationBuilder configurationBuilder = configurationBuilderArgumentCaptor.getValue();
+
+        Configuration configuration = configurationBuilder.build();
+
+        ResponseType responseType = configuration.getResponseType();
+
+        assertEquals(ResponseType.Type.JSON, responseType.getType());
+    }
+
+    @Test
+    void testGetOptions(
+        ActionContext mockedContext, Response mockedResponse, Executor mockedExecutor, Http mockedHttp,
+        ArgumentCaptor<ContextFunction<Http, Executor>> httpFunctionArgumentCaptor,
+        ArgumentCaptor<ConfigurationBuilder> configurationBuilderArgumentCaptor) throws Exception {
+
+        when(mockedHttp.get(stringArgumentCaptor.capture()))
+            .thenReturn(mockedExecutor);
+        when(mockedExecutor.header(stringArgumentCaptor.capture(), stringArgumentCaptor.capture()))
+            .thenReturn(mockedExecutor);
         when(mockedResponse.getBody(any(TypeReference.class)))
             .thenReturn(List.of(
                 Map.of(
@@ -63,9 +168,20 @@ class BambooHrUtilsTest {
 
         List<? extends Option<String>> locations = BambooHrUtils
             .getOptions("Location")
-            .apply(mockedParameters, mockedParameters, Map.of(), "", mockedActionContext);
+            .apply(null, null, null, null, mockedContext);
 
         assertEquals(List.of(option("London, UK", "London, UK")), locations);
-        assertEquals(List.of("accept", "application/json"), stringArgumentCaptor.getAllValues());
+        assertEquals(List.of("/meta/lists", "accept", "application/json"), stringArgumentCaptor.getAllValues());
+        ContextFunction<Http, Executor> capturedFunction = httpFunctionArgumentCaptor.getValue();
+
+        assertNotNull(capturedFunction);
+
+        ConfigurationBuilder configurationBuilder = configurationBuilderArgumentCaptor.getValue();
+
+        Configuration configuration = configurationBuilder.build();
+
+        ResponseType responseType = configuration.getResponseType();
+
+        assertEquals(ResponseType.Type.JSON, responseType.getType());
     }
 }
