ab7957e9a83eb64931329892fc45c03e3d7ae6f5
Monika Kušter <domitermonika@gmail.com>
2026-04-01 12:39:38 +0200

    471 update tests

--- server/libs/modules/components/resend/src/test/java/com/bytechef/component/resend/action/ResendSendEmailActionTest.java
+++ server/libs/modules/components/resend/src/test/java/com/bytechef/component/resend/action/ResendSendEmailActionTest.java
@@ -28,109 +28,105 @@ import static com.bytechef.component.resend.constant.ResendConstants.TAGS;
 import static com.bytechef.component.resend.constant.ResendConstants.TEXT;
 import static com.bytechef.component.resend.constant.ResendConstants.TO;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentCaptor.forClass;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.mockStatic;
 import static org.mockito.Mockito.when;
 
 import com.bytechef.component.definition.ActionContext;
+import com.bytechef.component.definition.Context.ContextFunction;
 import com.bytechef.component.definition.Context.Http;
+import com.bytechef.component.definition.Context.Http.Body;
+import com.bytechef.component.definition.Context.Http.BodyContentType;
+import com.bytechef.component.definition.Context.Http.Configuration;
+import com.bytechef.component.definition.Context.Http.Configuration.ConfigurationBuilder;
+import com.bytechef.component.definition.Context.Http.Executor;
+import com.bytechef.component.definition.Context.Http.Response;
+import com.bytechef.component.definition.Context.Http.ResponseType;
+import com.bytechef.component.definition.FileEntry;
 import com.bytechef.component.definition.Parameters;
 import com.bytechef.component.resend.util.ResendUtils;
+import com.bytechef.component.test.definition.MockParametersFactory;
+import com.bytechef.component.test.definition.extension.MockContextSetupExtension;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.MockedStatic;
 
 /**
- * @author Monika Domiter
+ * @author Monika Kušter
  */
+@ExtendWith(MockContextSetupExtension.class)
 class ResendSendEmailActionTest {
 
-    private final ArgumentCaptor<Http.Body> bodyArgumentCaptor = ArgumentCaptor.forClass(Http.Body.class);
-    private final ActionContext mockedContext = mock(ActionContext.class);
-    private final Http.Executor mockedExecutor = mock(Http.Executor.class);
+    private final ArgumentCaptor<Body> bodyArgumentCaptor = forClass(Body.class);
     private final Object mockedObject = mock(Object.class);
-    private final Parameters mockedParameters = mock(Parameters.class);
-    private final Http.Response mockedResponse = mock(Http.Response.class);
+    private final FileEntry mockedFileEntry = mock(FileEntry.class);
+    private final Parameters mockedParameters = MockParametersFactory.create(createParametersMap());
     private final List<Map<String, String>> attachments = List.of(Map.of("filename", "fileName"));
+    private final ArgumentCaptor<String> stringArgumentCaptor = forClass(String.class);
+    private final ArgumentCaptor<ActionContext> actionContextArgumentCaptor = forClass(ActionContext.class);
+    @SuppressWarnings("unchecked")
+    private final ArgumentCaptor<List<FileEntry>> listArgumentCaptor = forClass(List.class);
 
     @Test
-    void testPerform() {
-        Map<String, Object> propertyStubsMap = createPropertyStubsMap();
+    void testPerform(
+        ActionContext mockedContext, Response mockedResponse, Executor mockedExecutor, Http mockedHttp,
+        ArgumentCaptor<ContextFunction<Http, Executor>> httpFunctionArgumentCaptor,
+        ArgumentCaptor<ConfigurationBuilder> configurationBuilderArgumentCaptor) {
 
-        when(mockedParameters.getRequiredString(FROM))
-            .thenReturn((String) propertyStubsMap.get(FROM));
-        when(mockedParameters.getRequiredList(TO, String.class))
-            .thenReturn((List<String>) propertyStubsMap.get(TO));
-        when(mockedParameters.getRequiredString(SUBJECT))
-            .thenReturn((String) propertyStubsMap.get(SUBJECT));
-        when(mockedParameters.getList(BCC, String.class))
-            .thenReturn((List<String>) propertyStubsMap.get(BCC));
-        when(mockedParameters.getList(CC, String.class))
-            .thenReturn((List<String>) propertyStubsMap.get(CC));
-        when(mockedParameters.getList(REPLY_TO, String.class))
-            .thenReturn((List<String>) propertyStubsMap.get(REPLY_TO));
-        when(mockedParameters.getString(HTML))
-            .thenReturn((String) propertyStubsMap.get(HTML));
-        when(mockedParameters.getString(TEXT))
-            .thenReturn((String) propertyStubsMap.get(TEXT));
-        when(mockedParameters.getMap(HEADERS, String.class))
-            .thenReturn((Map<String, String>) propertyStubsMap.get(HEADERS));
-        when(mockedParameters.getFileEntries(ATTACHMENTS, List.of()))
-            .thenReturn(List.of());
-        when((List<Map<String, String>>) mockedParameters.getList(TAGS))
-            .thenReturn((List<Map<String, String>>) propertyStubsMap.get(TAGS));
-
-        when(mockedContext.http(any()))
+        when(mockedHttp.post(stringArgumentCaptor.capture()))
             .thenReturn(mockedExecutor);
         when(mockedExecutor.body(bodyArgumentCaptor.capture()))
             .thenReturn(mockedExecutor);
-        when(mockedExecutor.configuration(any()))
-            .thenReturn(mockedExecutor);
-        when(mockedExecutor.execute())
-            .thenReturn(mockedResponse);
         when(mockedResponse.getBody())
             .thenReturn(mockedObject);
 
         try (MockedStatic<ResendUtils> resendUtilsMockedStatic = mockStatic(ResendUtils.class)) {
             resendUtilsMockedStatic
-                .when(() -> ResendUtils.getAttachments(List.of(), mockedContext))
+                .when(() -> ResendUtils.getAttachments(
+                    listArgumentCaptor.capture(), actionContextArgumentCaptor.capture()))
                 .thenReturn(attachments);
 
             Object result = ResendSendEmailAction.perform(mockedParameters, mockedParameters, mockedContext);
 
             assertEquals(mockedObject, result);
+            assertEquals(List.of(mockedFileEntry), listArgumentCaptor.getValue());
+            assertEquals(mockedContext, actionContextArgumentCaptor.getValue());
+            assertNotNull(httpFunctionArgumentCaptor.getValue());
+            assertEquals("/emails", stringArgumentCaptor.getValue());
 
-            Http.Body bodyValue = bodyArgumentCaptor.getValue();
+            Map<String, Object> bodyMap = createParametersMap();
+            bodyMap.put("attachments", attachments);
 
-            assertEquals(propertyStubsMap, bodyValue.getContent());
+            assertEquals(Body.of(bodyMap, BodyContentType.JSON), bodyArgumentCaptor.getValue());
+
+            ConfigurationBuilder configurationBuilder = configurationBuilderArgumentCaptor.getValue();
+            Configuration configuration = configurationBuilder.build();
+
+            assertEquals(ResponseType.JSON, configuration.getResponseType());
         }
     }
 
-    private Map<String, Object> createPropertyStubsMap() {
-        Map<String, Object> propertyStubsMap = new HashMap<>();
+    private Map<String, Object> createParametersMap() {
+        Map<String, Object> map = new HashMap<>();
 
-        List<Map<String, String>> tags = List.of(Map.of(
-            "name", "category",
-            "value", "confirm_email"));
+        map.put(FROM, "from@mail.com");
+        map.put(TO, List.of("to@mail.com"));
+        map.put(SUBJECT, "subject");
+        map.put(BCC, List.of("bcc@mail.com"));
+        map.put(CC, List.of("cc@mail.com"));
+        map.put(REPLY_TO, List.of("reply@mail.com"));
+        map.put(HTML, "html");
+        map.put(TEXT, "text");
+        map.put(HEADERS, Map.of("X-Entity-Ref-ID", "123456789"));
+        map.put(ATTACHMENTS, List.of(mockedFileEntry));
+        map.put(TAGS, List.of(Map.of("name", "category", "value", "confirm_email")));
 
-        Map<String, String> headerMap = Map.of("X-Entity-Ref-ID", "123456789");
-
-        propertyStubsMap.put(FROM, "from@mail.com");
-        propertyStubsMap.put(TO, List.of("to@mail.com"));
-        propertyStubsMap.put(SUBJECT, "subject");
-        propertyStubsMap.put(BCC, List.of("bcc@mail.com"));
-        propertyStubsMap.put(CC, List.of("cc@mail.com"));
-        propertyStubsMap.put(REPLY_TO, List.of("reply@mail.com"));
-        propertyStubsMap.put(HTML, "html");
-        propertyStubsMap.put(TEXT, "text");
-        propertyStubsMap.put(HEADERS, headerMap);
-        propertyStubsMap.put(ATTACHMENTS, attachments);
-        propertyStubsMap.put(TAGS, tags);
-
-        return propertyStubsMap;
+        return map;
     }
 }
