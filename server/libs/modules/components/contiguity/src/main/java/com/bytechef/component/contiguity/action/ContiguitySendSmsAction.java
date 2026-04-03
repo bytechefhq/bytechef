19211df7499ca3d15945ddcb6cd303a7e564ea22
Nikolina Spehar <speharnikolina87@gmail.com>
2026-04-03 11:18:14 +0200

    2086 - generated

--- server/libs/modules/components/contiguity/src/main/java/com/bytechef/component/contiguity/action/ContiguitySendSmsAction.java
+++ server/libs/modules/components/contiguity/src/main/java/com/bytechef/component/contiguity/action/ContiguitySendSmsAction.java
@@ -74,7 +74,8 @@ public class ContiguitySendSmsAction {
                 .required(false))
             .metadata(
                 Map.of(
-                    "responseType", ResponseType.JSON))));
+                    "responseType", ResponseType.JSON))))
+        .help("", "https://docs.bytechef.io/reference/components/contiguity_v1#send-sms");
 
     private ContiguitySendSmsAction() {
     }
