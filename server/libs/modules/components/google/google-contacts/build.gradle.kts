6558dd9e027831d5e051f9765bbc80aa5f58bc49
Monika Kušter <domitermonika@gmail.com>
2026-04-03 14:00:43 +0200

    804 remove unused dependency

--- server/libs/modules/components/google/google-contacts/build.gradle.kts
+++ server/libs/modules/components/google/google-contacts/build.gradle.kts
@@ -1,6 +1,5 @@
 version="1.0"
 
 dependencies {
-    implementation("com.google.apis:google-api-services-people:v1-rev20250513-2.0.0")
     api(project(":server:libs:modules:components:google:google-commons"))
 }
