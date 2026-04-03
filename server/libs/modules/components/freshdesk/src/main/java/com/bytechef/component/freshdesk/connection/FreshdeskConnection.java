0921c802ca3a19673918265abe63f70dd79f9d95
Monika Kušter <domitermonika@gmail.com>
2026-04-03 13:15:13 +0200

    2591 components - generated

--- server/libs/modules/components/freshdesk/src/main/java/com/bytechef/component/freshdesk/connection/FreshdeskConnection.java
+++ server/libs/modules/components/freshdesk/src/main/java/com/bytechef/component/freshdesk/connection/FreshdeskConnection.java
@@ -24,6 +24,7 @@ import static com.bytechef.component.definition.ComponentDsl.connection;
 import static com.bytechef.component.definition.ComponentDsl.string;
 
 import com.bytechef.component.definition.ComponentDsl;
+import com.bytechef.component.definition.Property;
 
 /**
  * Provides the component connection definition.
@@ -41,6 +42,7 @@ public class FreshdeskConnection {
                     .required(true),
                 string(PASSWORD)
                     .label("Password")
+                    .controlType(Property.ControlType.PASSWORD)
                     .required(true)));
 
     private FreshdeskConnection() {
