9826f79687be591a507c1a5dd2e541a9ef2821a8
Monika Kušter <domitermonika@gmail.com>
2026-04-03 13:15:13 +0200

    2591 components - generated

--- server/libs/modules/components/pipeliner/src/main/java/com/bytechef/component/pipeliner/connection/PipelinerConnection.java
+++ server/libs/modules/components/pipeliner/src/main/java/com/bytechef/component/pipeliner/connection/PipelinerConnection.java
@@ -24,6 +24,7 @@ import static com.bytechef.component.definition.ComponentDsl.connection;
 import static com.bytechef.component.definition.ComponentDsl.string;
 
 import com.bytechef.component.definition.ComponentDsl;
+import com.bytechef.component.definition.Property;
 
 /**
  * Provides the component connection definition.
@@ -43,6 +44,7 @@ public class PipelinerConnection {
                     .required(true),
                 string(PASSWORD)
                     .label("Password")
+                    .controlType(Property.ControlType.PASSWORD)
                     .required(true)));
 
     private PipelinerConnection() {
