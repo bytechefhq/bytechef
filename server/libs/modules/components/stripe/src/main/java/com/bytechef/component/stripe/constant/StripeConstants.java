48ff0a03ae85f354fc4a8b2836a3905652a1afd9
marijahorvat171 <marijahorvat171@gmail.com>
2026-04-08 14:39:25 +0200

    1390 - add new actions

--- server/libs/modules/components/stripe/src/main/java/com/bytechef/component/stripe/constant/StripeConstants.java
+++ server/libs/modules/components/stripe/src/main/java/com/bytechef/component/stripe/constant/StripeConstants.java
@@ -16,12 +16,81 @@
 
 package com.bytechef.component.stripe.constant;
 
+import static com.bytechef.component.definition.ComponentDsl.array;
+import static com.bytechef.component.definition.ComponentDsl.bool;
+import static com.bytechef.component.definition.ComponentDsl.integer;
+import static com.bytechef.component.definition.ComponentDsl.object;
+import static com.bytechef.component.definition.ComponentDsl.string;
+
+import com.bytechef.component.definition.ComponentDsl.ModifiableObjectProperty;
+
 /**
  * @author Monika Kušter
  */
 public class StripeConstants {
 
+    public static final String COLLECTION_METHOD = "collection_method";
+    public static final String COUPON_ID = "coupon";
+    public static final String CUSTOMER_ID = "customer";
+    public static final String DAYS_UNTIL_DUE = "days_until_due";
+    public static final String DEFAULT_PAYMENT_METHOD = "default_payment_method";
     public static final String ID = "id";
+    public static final String ITEMS = "items";
+    public static final String METADATA = "metadata";
+    public static final String PRICE_ID = "price";
+    public static final String QUANTITY = "quantity";
+    public static final String SUBSCRIPTION_ID = "subscription";
+
+    public static final ModifiableObjectProperty SUBSCRIPTION_OUTPUT = object()
+        .properties(
+            object("automatic_tax")
+                .description("Tax rates that apply to automatic tax calculations."),
+            integer("billing_cycle_anchor")
+                .description("The reference point that aligns future billing cycle dates."),
+            object("billing_mode")
+                .description("Billing mode of the subscription."),
+            bool("cancel_at_period_end")
+                .description("Whether this subscription is cancel at the end of the current billing period."),
+            string("collection_method")
+                .description("When charging automatically, Stripe will attempt to pay this subscription at the end of" +
+                    " the cycle using the default source attached to the customer. When sending an invoice, Stripe " +
+                    "will email your customer an invoice with payment instructions and mark the subscription as `active`."),
+            integer("created")
+                .description("Time at which the object was created."),
+            string("currency")
+                .description("Three-letter ISO currency code in lowercase."),
+            string("customer")
+                .description("ID of the customer who owns the subscription."),
+            array("discounts")
+                .description("The discounts applied to the subscription.")
+                .items(string()),
+            string("id")
+                .description("Unique identifier for the object."),
+            object("invoice_settings")
+                .description("Invoice settings for the subscription."),
+            object("items")
+                .description("List of subscription items, each with an attached price.")
+                .properties(
+                    array("data")
+                        .description("Details about each object.")
+                        .items(object()),
+                    bool("has_more")
+                        .description("True if this list has another page of items after this one that can be fetched."),
+                    string("object")
+                        .description("String representing the object's type."),
+                    string("url")
+                        .description("The URL where this list can be accessed.")),
+            bool("livemode")
+                .description(
+                    "If the object exists in live mode, the value is `true`. If the object exists in test mode, the value is `false`."),
+            object("metadata")
+                .description("Set of key-value pairs that you can attach to an object."),
+            string("object")
+                .description("String representing the object's type. Objects of the same type share the same value."),
+            integer("start_date")
+                .description("Date when the subscription was first created."),
+            string("status")
+                .description("Status of the subscription."));
 
     private StripeConstants() {
     }
---
+++ server/libs/modules/components/stripe/src/test/java/com/bytechef/component/stripe/action/StripeUpdateSubscriptionActionTest.java
@@ -0,0 +1,107 @@
+/*
+ * Copyright 2025 ByteChef
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef.component.stripe.action;
+
+import static com.bytechef.component.stripe.constant.StripeConstants.COLLECTION_METHOD;
+import static com.bytechef.component.stripe.constant.StripeConstants.COUPON_ID;
+import static com.bytechef.component.stripe.constant.StripeConstants.CUSTOMER_ID;
+import static com.bytechef.component.stripe.constant.StripeConstants.DAYS_UNTIL_DUE;
+import static com.bytechef.component.stripe.constant.StripeConstants.DEFAULT_PAYMENT_METHOD;
+import static com.bytechef.component.stripe.constant.StripeConstants.ITEMS;
+import static com.bytechef.component.stripe.constant.StripeConstants.METADATA;
+import static com.bytechef.component.stripe.constant.StripeConstants.PRICE_ID;
+import static com.bytechef.component.stripe.constant.StripeConstants.QUANTITY;
+import static com.bytechef.component.stripe.constant.StripeConstants.SUBSCRIPTION_ID;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentCaptor.forClass;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+import com.bytechef.component.definition.Context;
+import com.bytechef.component.definition.Context.ContextFunction;
+import com.bytechef.component.definition.Context.Http;
+import com.bytechef.component.definition.Context.Http.Body;
+import com.bytechef.component.definition.Context.Http.BodyContentType;
+import com.bytechef.component.definition.Context.Http.Configuration;
+import com.bytechef.component.definition.Context.Http.Configuration.ConfigurationBuilder;
+import com.bytechef.component.definition.Context.Http.Executor;
+import com.bytechef.component.definition.Context.Http.Response;
+import com.bytechef.component.definition.Context.Http.ResponseType;
+import com.bytechef.component.definition.Parameters;
+import com.bytechef.component.definition.TypeReference;
+import com.bytechef.component.test.definition.MockParametersFactory;
+import com.bytechef.component.test.definition.extension.MockContextSetupExtension;
+import java.util.List;
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+
+/**
+ * @author Marija Horvat
+ */
+@ExtendWith(MockContextSetupExtension.class)
+class StripeUpdateSubscriptionActionTest {
+
+    private final ArgumentCaptor<Body> bodyArgumentCaptor = forClass(Body.class);
+    private final Parameters mockedParameters =
+        MockParametersFactory.create(
+            Map.of(CUSTOMER_ID, "xy", SUBSCRIPTION_ID, "ab", ITEMS,
+                List.of(Map.of(PRICE_ID, "1", COUPON_ID, "2", QUANTITY, 3)),
+                COLLECTION_METHOD, "send_invoice", DAYS_UNTIL_DUE, 2, DEFAULT_PAYMENT_METHOD, "abc",
+                METADATA, Map.of("key", "value")));
+    private final ArgumentCaptor<String> stringArgumentCaptor = forClass(String.class);
+    private final Map<String, Object> responseMap = Map.of("key", "value");
+
+    @Test
+    void testPerform(
+        Context mockedContext, Response mockedResponse, Executor mockedExecutor, Http mockedHttp,
+        ArgumentCaptor<ContextFunction<Http, Executor>> httpFunctionArgumentCaptor,
+        ArgumentCaptor<ConfigurationBuilder> configurationBuilderArgumentCaptor) {
+
+        when(mockedHttp.post(stringArgumentCaptor.capture()))
+            .thenReturn(mockedExecutor);
+        when(mockedExecutor.body(bodyArgumentCaptor.capture()))
+            .thenReturn(mockedExecutor);
+        when(mockedResponse.getBody(any(TypeReference.class)))
+            .thenReturn(responseMap);
+
+        Object result = StripeUpdateSubscriptionAction.perform(mockedParameters, null, mockedContext);
+
+        assertEquals(responseMap, result);
+
+        assertNotNull(httpFunctionArgumentCaptor.getValue());
+
+        ConfigurationBuilder configurationBuilder =
+            configurationBuilderArgumentCaptor.getValue();
+
+        Configuration configuration = configurationBuilder.build();
+
+        assertEquals(ResponseType.JSON, configuration.getResponseType());
+        assertEquals("/subscriptions/ab", stringArgumentCaptor.getValue());
+
+        assertEquals(
+            Body.of(
+                Map.of(COLLECTION_METHOD, "send_invoice", DAYS_UNTIL_DUE, 2,
+                    DEFAULT_PAYMENT_METHOD, "abc", "items[0][discounts][0][coupon]", "2",
+                    "items[0][price]", "1", "items[0][quantity]", 3,
+                    "metadata[key]", "value"),
+                BodyContentType.FORM_URL_ENCODED),
+            bodyArgumentCaptor.getValue());
+    }
+}
