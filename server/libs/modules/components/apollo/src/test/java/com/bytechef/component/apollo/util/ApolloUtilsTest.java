444ee031f8889928a4ce33f3ea1d861941d39b41
Monika Kušter <domitermonika@gmail.com>
2026-04-08 14:26:30 +0200

    2119 update tests

--- server/libs/modules/components/apollo/src/test/java/com/bytechef/component/apollo/util/ApolloUtilsTest.java
+++ server/libs/modules/components/apollo/src/test/java/com/bytechef/component/apollo/util/ApolloUtilsTest.java
@@ -17,44 +17,53 @@
 package com.bytechef.component.apollo.util;
 
 import static com.bytechef.component.definition.ComponentDsl.option;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentCaptor.forClass;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import com.bytechef.component.definition.Context;
+import com.bytechef.component.definition.Context.ContextFunction;
 import com.bytechef.component.definition.Context.Http;
+import com.bytechef.component.definition.Context.Http.Configuration;
+import com.bytechef.component.definition.Context.Http.Configuration.ConfigurationBuilder;
+import com.bytechef.component.definition.Context.Http.Executor;
+import com.bytechef.component.definition.Context.Http.Response;
+import com.bytechef.component.definition.Context.Http.ResponseType;
 import com.bytechef.component.definition.Option;
 import com.bytechef.component.definition.Parameters;
 import com.bytechef.component.definition.TypeReference;
+import com.bytechef.component.test.definition.extension.MockContextSetupExtension;
 import java.util.List;
 import java.util.Map;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 
 /**
  * @author Monika Kušter
  */
+@ExtendWith(MockContextSetupExtension.class)
 class ApolloUtilsTest {
 
     private final List<Option<String>> expectedOptions = List.of(option("ime", "123"));
-    private final Http.Executor mockedExecutor = mock(Http.Executor.class);
     private final Parameters mockedParameters = mock(Parameters.class);
-    private final Http.Response mockedResponse = mock(Http.Response.class);
-    private final Context mockedContext = mock(Context.class);
-
-    @BeforeEach
-    void beforeEach() {
-        when(mockedContext.http(any()))
-            .thenReturn(mockedExecutor);
-        when(mockedExecutor.configuration(any()))
-            .thenReturn(mockedExecutor);
-        when(mockedExecutor.execute())
-            .thenReturn(mockedResponse);
-    }
+    private final ArgumentCaptor<String> stringArgumentCaptor = forClass(String.class);
+    private final ArgumentCaptor<Object[]> objectsArgumentCaptor = forClass(Object[].class);
 
     @Test
-    void testGetAccountIdOptions() {
+    void testGetAccountIdOptions(
+        Context mockedContext, Response mockedResponse, Executor mockedExecutor, Http mockedHttp,
+        ArgumentCaptor<ContextFunction<Http, Executor>> httpFunctionArgumentCaptor,
+        ArgumentCaptor<ConfigurationBuilder> configurationBuilderArgumentCaptor) {
+
+        when(mockedHttp.get(stringArgumentCaptor.capture()))
+            .thenReturn(mockedExecutor);
+        when(mockedExecutor.queryParameters(objectsArgumentCaptor.capture()))
+            .thenReturn(mockedExecutor);
         when(mockedResponse.getBody(any(TypeReference.class)))
             .thenReturn(Map.of("organizations", List.of(Map.of("id", "123", "name", "ime"))));
 
@@ -62,21 +71,76 @@ class ApolloUtilsTest {
             mockedParameters, mockedParameters, null, "", mockedContext);
 
         assertEquals(expectedOptions, result);
+        assertNotNull(httpFunctionArgumentCaptor.getValue());
+
+        ConfigurationBuilder configurationBuilder = configurationBuilderArgumentCaptor.getValue();
+        Configuration configuration = configurationBuilder.build();
+
+        assertEquals(ResponseType.JSON, configuration.getResponseType());
+        assertEquals("/mixed_companies/search", stringArgumentCaptor.getValue());
+
+        Object[] queryParameters = {
+            "page", 1, "per_page", 100
+        };
+
+        assertArrayEquals(queryParameters, objectsArgumentCaptor.getValue());
     }
 
     @Test
-    void testGetOOwnerIdOptions() {
+    void testGetOwnerIdOptions(
+        Context mockedContext, Response mockedResponse, Executor mockedExecutor, Http mockedHttp,
+        ArgumentCaptor<ContextFunction<Http, Executor>> httpFunctionArgumentCaptor,
+        ArgumentCaptor<ConfigurationBuilder> configurationBuilderArgumentCaptor) {
+
+        when(mockedHttp.get(stringArgumentCaptor.capture()))
+            .thenReturn(mockedExecutor);
+        when(mockedExecutor.queryParameters(objectsArgumentCaptor.capture()))
+            .thenReturn(mockedExecutor);
         when(mockedResponse.getBody(any(TypeReference.class)))
-            .thenReturn(Map.of("users", List.of(Map.of("id", "123", "name", "ime"))));
+            .thenReturn(
+                Map.of("users", List.of(Map.of("id", "123", "name", "ime")),
+                    "pagination", Map.of("total_pages", 2)))
+            .thenReturn(
+                Map.of("users", List.of(Map.of("id", "234", "name", "abc")),
+                    "pagination", Map.of("total_pages", 2)));
 
         List<Option<String>> result = ApolloUtils.getOwnerIdOptions(
             mockedParameters, mockedParameters, null, "", mockedContext);
 
-        assertEquals(expectedOptions, result);
+        assertEquals(List.of(option("ime", "123"), option("abc", "234")), result);
+        assertNotNull(httpFunctionArgumentCaptor.getValue());
+
+        ConfigurationBuilder configurationBuilder = configurationBuilderArgumentCaptor.getValue();
+        Configuration configuration = configurationBuilder.build();
+
+        assertEquals(ResponseType.JSON, configuration.getResponseType());
+        assertEquals("/users/search", stringArgumentCaptor.getValue());
+
+        Object[] queryParameters1 = {
+            "page", 1, "per_page", 100
+        };
+
+        Object[] queryParameters2 = {
+            "page", 2, "per_page", 100
+        };
+
+        List<Object[]> allValues = objectsArgumentCaptor.getAllValues();
+
+        assertEquals(2, allValues.size());
+        assertArrayEquals(queryParameters1, allValues.getFirst());
+        assertArrayEquals(queryParameters2, allValues.getLast());
     }
 
     @Test
-    void testGetOpportunityIdOptions() {
+    void testGetOpportunityIdOptions(
+        Context mockedContext, Response mockedResponse, Executor mockedExecutor, Http mockedHttp,
+        ArgumentCaptor<ContextFunction<Http, Executor>> httpFunctionArgumentCaptor,
+        ArgumentCaptor<ConfigurationBuilder> configurationBuilderArgumentCaptor) {
+
+        when(mockedHttp.get(stringArgumentCaptor.capture()))
+            .thenReturn(mockedExecutor);
+        when(mockedExecutor.queryParameters(objectsArgumentCaptor.capture()))
+            .thenReturn(mockedExecutor);
         when(mockedResponse.getBody(any(TypeReference.class)))
             .thenReturn(Map.of("opportunities", List.of(Map.of("id", "123", "name", "ime"))));
 
@@ -84,5 +148,18 @@ class ApolloUtilsTest {
             mockedParameters, mockedParameters, null, "", mockedContext);
 
         assertEquals(expectedOptions, result);
+        assertNotNull(httpFunctionArgumentCaptor.getValue());
+
+        ConfigurationBuilder configurationBuilder = configurationBuilderArgumentCaptor.getValue();
+        Configuration configuration = configurationBuilder.build();
+
+        assertEquals(ResponseType.JSON, configuration.getResponseType());
+        assertEquals("/opportunities/search", stringArgumentCaptor.getValue());
+
+        Object[] queryParameters = {
+            "page", 1, "per_page", 100
+        };
+
+        assertArrayEquals(queryParameters, objectsArgumentCaptor.getValue());
     }
 }
