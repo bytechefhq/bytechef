48a7471b65b78555760f21b0daf8429efc6e320f
marijahorvat171 <marijahorvat171@gmail.com>
2026-04-09 14:53:37 +0200

    2295 - update test

--- server/libs/modules/components/mixpanel/src/test/java/com/bytechef/component/mixpanel/action/MixpanelTrackEventsActionTest.java
+++ server/libs/modules/components/mixpanel/src/test/java/com/bytechef/component/mixpanel/action/MixpanelTrackEventsActionTest.java
@@ -22,45 +22,52 @@ import static com.bytechef.component.mixpanel.constant.MixpanelConstants.EVENTS;
 import static com.bytechef.component.mixpanel.constant.MixpanelConstants.INSERT_ID;
 import static com.bytechef.component.mixpanel.constant.MixpanelConstants.TIME;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.mock;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentCaptor.forClass;
 import static org.mockito.Mockito.when;
 
 import com.bytechef.component.definition.Context;
+import com.bytechef.component.definition.Context.ContextFunction;
 import com.bytechef.component.definition.Context.Http;
+import com.bytechef.component.definition.Context.Http.Body;
+import com.bytechef.component.definition.Context.Http.Configuration;
+import com.bytechef.component.definition.Context.Http.Configuration.ConfigurationBuilder;
+import com.bytechef.component.definition.Context.Http.Executor;
+import com.bytechef.component.definition.Context.Http.Response;
+import com.bytechef.component.definition.Context.Http.ResponseType;
 import com.bytechef.component.definition.Parameters;
 import com.bytechef.component.test.definition.MockParametersFactory;
+import com.bytechef.component.test.definition.extension.MockContextSetupExtension;
 import java.util.List;
 import java.util.Map;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
 
 /**
  * @author Marija Horvat
  */
+@ExtendWith(MockContextSetupExtension.class)
 class MixpanelTrackEventsActionTest {
 
-    private final Context mockedContext = mock(Context.class);
-    private final Http.Executor mockedExecutor = mock(Http.Executor.class);
-    private final Http.Response mockedResponse = mock(Http.Response.class);
     private final Map<String, Object> responseMap = Map.of("key", "value");
-    private final ArgumentCaptor<Http.Body> bodyArgumentCaptor = ArgumentCaptor.forClass(Http.Body.class);
-    private final ArgumentCaptor<String> stringArgumentCaptor = ArgumentCaptor.forClass(String.class);
+    private final ArgumentCaptor<Body> bodyArgumentCaptor = forClass(Body.class);
+    private final ArgumentCaptor<String> stringArgumentCaptor = forClass(String.class);
     private final Parameters mockedParameters = MockParametersFactory.create(
         Map.of(EVENTS, List.of(Map.of(EVENT, "test1", TIME, "2025-04-15T16:44:12", DISTINCT_ID, 1, INSERT_ID, 1))));
 
     @Test
-    void testPerform() {
-        when(mockedContext.http(any()))
+    void testPerform(
+        Context mockedContext, Response mockedResponse, Executor mockedExecutor, Http mockedHttp,
+        ArgumentCaptor<ContextFunction<Http, Executor>> httpFunctionArgumentCaptor,
+        ArgumentCaptor<ConfigurationBuilder> configurationBuilderArgumentCaptor) {
+
+        when(mockedHttp.post(stringArgumentCaptor.capture()))
             .thenReturn(mockedExecutor);
         when(mockedExecutor.queryParameter(stringArgumentCaptor.capture(), stringArgumentCaptor.capture()))
             .thenReturn(mockedExecutor);
         when(mockedExecutor.body(bodyArgumentCaptor.capture()))
             .thenReturn(mockedExecutor);
-        when(mockedExecutor.configuration(any()))
-            .thenReturn(mockedExecutor);
-        when(mockedExecutor.execute())
-            .thenReturn(mockedResponse);
         when(mockedResponse.getBody())
             .thenReturn(responseMap);
 
@@ -68,12 +75,18 @@ class MixpanelTrackEventsActionTest {
 
         assertEquals(responseMap, result);
 
-        Http.Body body = bodyArgumentCaptor.getValue();
+        Body body = bodyArgumentCaptor.getValue();
         List<Map<String, Object>> expected = List.of(Map.of(
             EVENT, "test1",
             "properties", Map.of(TIME, 1744735452L, DISTINCT_ID, 1, INSERT_ID, 1)));
 
         assertEquals(expected, body.getContent());
-        assertEquals(List.of("strict", "1"), stringArgumentCaptor.getAllValues());
+        assertEquals(List.of("/import", "strict", "1"), stringArgumentCaptor.getAllValues());
+
+        assertNotNull(httpFunctionArgumentCaptor.getValue());
+
+        ConfigurationBuilder configurationBuilder = configurationBuilderArgumentCaptor.getValue();
+        Configuration configuration = configurationBuilder.build();
+        assertEquals(ResponseType.JSON, configuration.getResponseType());
     }
 }
