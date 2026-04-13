e0eb1ebe73550c2a9e61c437d645f2f9feda626f
mkriskovic <marko.kriskovic@protonmail.com>
2026-04-13 12:13:35 +0200

    4542 - trimming invisible characters

--- server/libs/core/encryption/encryption-impl/src/main/java/com/bytechef/encryption/EncryptionImpl.java
+++ server/libs/core/encryption/encryption-impl/src/main/java/com/bytechef/encryption/EncryptionImpl.java
@@ -151,7 +151,7 @@ public class EncryptionImpl implements Encryption {
     }
 
     private Key getSecretKey() {
-        byte[] decodedKey = EncodingUtils.base64Decode(encryptionKey.getKey());
+        byte[] decodedKey = EncodingUtils.base64Decode(encryptionKey.getKey().trim());
 
         return new SecretKeySpec(Arrays.copyOf(decodedKey, 16), "AES");
     }
