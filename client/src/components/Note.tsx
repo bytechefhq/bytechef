6ba875059f8aeecef8a3bbb7052af3d8a2843a92
Krešimir Čoko <kresimircoko@gmail.com>
2026-04-18 12:24:33 +0200

    2729 - improve the Note component

--- client/src/components/Note.tsx
+++ client/src/components/Note.tsx
@@ -1,8 +1,20 @@
 import {ReactNode} from 'react';
 import {twMerge} from 'tailwind-merge';
 
-export const Note = ({content, icon}: {content: string; icon?: ReactNode}) => (
-    <div className={twMerge('relative flex items-center rounded-md bg-surface-warning-secondary p-4', icon && 'gap-2')}>
+interface NoteProps {
+    className?: string;
+    content: string;
+    icon?: ReactNode;
+}
+
+export const Note = ({className, content, icon}: NoteProps) => (
+    <div
+        className={twMerge(
+            'relative flex items-center rounded-md border border-stroke-warning-secondary bg-surface-warning-secondary p-4',
+            icon && 'gap-2',
+            className
+        )}
+    >
         {icon && icon}
 
         <p className="text-sm font-medium">{content}</p>
