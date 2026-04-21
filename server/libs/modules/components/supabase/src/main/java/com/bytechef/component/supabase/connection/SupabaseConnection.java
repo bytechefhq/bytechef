d9fdd553d27f64a8f36b483064fd05d47f4bc0a3
Nikolina Spehar <speharnikolina87@gmail.com>
2026-04-21 15:04:29 +0200

    2320 - added 'apikey' to header, updated baseURL and fixed URL encoding of space from '+' to '%20'

--- server/libs/modules/components/supabase/src/main/java/com/bytechef/component/supabase/connection/SupabaseConnection.java
+++ server/libs/modules/components/supabase/src/main/java/com/bytechef/component/supabase/connection/SupabaseConnection.java
@@ -16,14 +16,18 @@
 
 package com.bytechef.component.supabase.connection;
 
+import static com.bytechef.component.definition.Authorization.AUTHORIZATION;
 import static com.bytechef.component.definition.Authorization.TOKEN;
 import static com.bytechef.component.definition.ComponentDsl.authorization;
 import static com.bytechef.component.definition.ComponentDsl.connection;
 import static com.bytechef.component.definition.ComponentDsl.string;
 import static com.bytechef.component.supabase.constant.SupabaseConstants.PROJECT_URL;
 
+import com.bytechef.component.definition.Authorization.ApplyResponse;
 import com.bytechef.component.definition.Authorization.AuthorizationType;
 import com.bytechef.component.definition.ComponentDsl.ModifiableConnectionDefinition;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author Nikolina Spehar
@@ -31,7 +35,9 @@ import com.bytechef.component.definition.ComponentDsl.ModifiableConnectionDefini
 public class SupabaseConnection {
 
     public static final ModifiableConnectionDefinition CONNECTION_DEFINITION = connection()
-        .baseUri((connectionParameters, context) -> connectionParameters.getRequiredString(PROJECT_URL))
+        .baseUri(
+            (connectionParameters, context) -> connectionParameters.getRequiredString(PROJECT_URL)
+                .split("/rest/")[0])
         .authorizations(
             authorization(AuthorizationType.BEARER_TOKEN)
                 .properties(
@@ -42,8 +48,12 @@ public class SupabaseConnection {
                     string(TOKEN)
                         .label("Project API Key")
                         .description(
-                            "Can be found in Project Settings -> Data API. Choose service_role/secret API key.")
-                        .required(true)))
+                            "Can be found in Project Settings -> API Keys. Copy key under Secret keys.")
+                        .required(true))
+                .apply((connectionParameters, context) -> ApplyResponse.ofHeaders(
+                    Map.of(
+                        AUTHORIZATION, List.of("Bearer " + connectionParameters.getRequiredString(TOKEN)),
+                        "apikey", List.of(connectionParameters.getRequiredString(TOKEN))))))
         .version(1)
         .help("", "https://docs.bytechef.io/reference/components/supabase_v1#connection-setup");
 
