583a356db2c882d47ca3e7b022c4cf68d104b5ed
marijahorvat171 <marijahorvat171@gmail.com>
2026-04-30 10:00:38 +0200

    2385 - update tests

--- server/libs/modules/components/coda/src/test/java/com/bytechef/component/coda/action/CodaInsertRowActionTest.java
+++ server/libs/modules/components/coda/src/test/java/com/bytechef/component/coda/action/CodaInsertRowActionTest.java
@@ -20,50 +20,58 @@ import static com.bytechef.component.coda.constant.CodaConstants.DOC_ID;
 import static com.bytechef.component.coda.constant.CodaConstants.ROW_VALUES;
 import static com.bytechef.component.coda.constant.CodaConstants.TABLE_ID;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentCaptor.forClass;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import com.bytechef.component.definition.Context;
+import com.bytechef.component.definition.Context.ContextFunction;
 import com.bytechef.component.definition.Context.Http;
 import com.bytechef.component.definition.Context.Http.Body;
+import com.bytechef.component.definition.Context.Http.Configuration;
+import com.bytechef.component.definition.Context.Http.Configuration.ConfigurationBuilder;
 import com.bytechef.component.definition.Context.Http.Executor;
 import com.bytechef.component.definition.Context.Http.Response;
+import com.bytechef.component.definition.Context.Http.ResponseType;
 import com.bytechef.component.definition.Parameters;
 import com.bytechef.component.test.definition.MockParametersFactory;
+import com.bytechef.component.test.definition.extension.MockContextSetupExtension;
 import java.util.List;
 import java.util.Map;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
 
 /**
  * @author Marija Horvat
  */
+@ExtendWith(MockContextSetupExtension.class)
 class CodaInsertRowActionTest {
 
-    private final ArgumentCaptor<Http.Body> bodyArgumentCaptor = ArgumentCaptor.forClass(Http.Body.class);
-    private final Context mockedContext = mock(Context.class);
-    private final Executor mockedExecutor = mock(Executor.class);
-    private final Response mockedResponse = mock(Response.class);
+    private final ArgumentCaptor<Body> bodyArgumentCaptor = forClass(Body.class);
+    private final ArgumentCaptor<String> stringArgumentCaptor = forClass(String.class);
     private final Object mockedObject = mock(Object.class);
 
     @Test
-    void testPerform() {
+    void testPerform(
+        Context mockedContext, Response mockedResponse, Executor mockedExecutor, Http mockedHttp,
+        ArgumentCaptor<ContextFunction<Http, Executor>> httpFunctionArgumentCaptor,
+        ArgumentCaptor<ConfigurationBuilder> configurationBuilderArgumentCaptor) {
+
         Parameters parameters = MockParametersFactory.create(
             Map.of(DOC_ID, "1", TABLE_ID, "2", ROW_VALUES, Map.of("3", "test")));
 
-        when(mockedContext.http(any()))
+        when(mockedHttp.post(stringArgumentCaptor.capture()))
             .thenReturn(mockedExecutor);
         when(mockedExecutor.body(bodyArgumentCaptor.capture()))
             .thenReturn(mockedExecutor);
-        when(mockedExecutor.configuration(any()))
-            .thenReturn(mockedExecutor);
-        when(mockedExecutor.execute())
-            .thenReturn(mockedResponse);
         when(mockedResponse.getBody())
             .thenReturn(mockedObject);
 
-        Object result = CodaInsertRowAction.perform(parameters, parameters, mockedContext);
+        Object result = CodaInsertRowAction.perform(parameters, null, mockedContext);
+
+        assertEquals(mockedObject, result);
 
         Body body = bodyArgumentCaptor.getValue();
 
@@ -71,6 +79,14 @@ class CodaInsertRowActionTest {
             "cells", List.of(Map.of("column", "3", "value", "test")))));
 
         assertEquals(expectedBody, body.getContent());
-        assertEquals(mockedObject, result);
+
+        assertEquals("/docs/1/tables/2/rows", stringArgumentCaptor.getValue());
+
+        assertNotNull(httpFunctionArgumentCaptor.getValue());
+
+        ConfigurationBuilder configurationBuilder = configurationBuilderArgumentCaptor.getValue();
+        Configuration configuration = configurationBuilder.build();
+        assertEquals(ResponseType.JSON, configuration.getResponseType());
+
     }
 }
