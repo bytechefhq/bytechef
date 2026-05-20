d2fe8e4e42aa84b402f859af20e71a56e092cb09
Ivica Cardic <ivica.cardic@gmail.com>
2026-05-20 09:58:58 +0200

    4000 Move pgvector loader bean to knowledge base configuration

--- server/ee/libs/config/tenant-multi-pgvector-config/src/main/java/com/bytechef/ee/tenant/multi/config/MultiTenantPgVectorDataSourceConfiguration.java
+++ server/ee/libs/config/tenant-multi-pgvector-config/src/main/java/com/bytechef/ee/tenant/multi/config/MultiTenantPgVectorDataSourceConfiguration.java
@@ -8,22 +8,18 @@
 package com.bytechef.ee.tenant.multi.config;
 
 import com.bytechef.config.ApplicationProperties;
-import com.bytechef.ee.tenant.multi.pgvector.MultiTenantPgVectorLoader;
 import com.bytechef.ee.tenant.multi.sql.MultiTenantPgVectorDataSource;
 import com.bytechef.platform.annotation.ConditionalOnEEVersion;
 import com.bytechef.tenant.annotation.ConditionalOnMultiTenant;
-import com.bytechef.tenant.service.TenantService;
 import com.zaxxer.hikari.HikariDataSource;
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import javax.sql.DataSource;
 import org.springframework.ai.vectorstore.pgvector.autoconfigure.PgVectorStoreProperties;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.core.JdbcTemplate;
 
 /**
  * @version ee
@@ -57,13 +53,5 @@ class MultiTenantPgVectorDataSourceConfiguration {
             .build();
 
         return new MultiTenantPgVectorDataSource(dataSource);
-    }
-
-    @Bean
-    MultiTenantPgVectorLoader multiTenantPgVectorLoader(
-        @Qualifier("pgVectorJdbcTemplate") JdbcTemplate pgVectorJdbcTemplate,
-        PgVectorStoreProperties properties, TenantService tenantService) {
-
-        return new MultiTenantPgVectorLoader(pgVectorJdbcTemplate, properties, tenantService);
     }
 }
