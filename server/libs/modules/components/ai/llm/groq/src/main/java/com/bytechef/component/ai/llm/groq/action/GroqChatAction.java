0901eb80b034a4713f55c40a785616e3ae4f3a42
Nikolina Spehar <speharnikolina87@gmail.com>
2026-05-26 15:11:25 +0200

    1018 - api key should be set in both client and options

--- server/libs/modules/components/ai/llm/groq/src/main/java/com/bytechef/component/ai/llm/groq/action/GroqChatAction.java
+++ server/libs/modules/components/ai/llm/groq/src/main/java/com/bytechef/component/ai/llm/groq/action/GroqChatAction.java
@@ -115,6 +115,7 @@ public class GroqChatAction {
                     .build())
             .options(
                 OpenAiChatOptions.builder()
+                    .apiKey(connectionParameters.getRequiredString(TOKEN))
                     .model(inputParameters.getRequiredString(MODEL))
                     .frequencyPenalty(inputParameters.getDouble(FREQUENCY_PENALTY))
                     .logitBias(inputParameters.getMap(LOGIT_BIAS, new TypeReference<>() {}))
