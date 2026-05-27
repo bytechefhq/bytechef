c3578a46739b95daedacb34fa1015af81b4c95a8
igorbeslic <igor.beslic@bitnaprednost.hr>
2026-05-27 22:51:13 +0200

    4446 ftp-component - test dependency - add dependency to support real file support

--- server/libs/modules/components/ftp/build.gradle.kts
+++ server/libs/modules/components/ftp/build.gradle.kts
@@ -4,6 +4,8 @@ dependencies {
     implementation("commons-net:commons-net:3.11.1")
     implementation("com.hierynomus:sshj:0.40.0")
 
+    testImplementation(project(":server:libs:platform:platform-file-storage:platform-file-storage-api"))
+    testImplementation(project(":server:libs:platform:platform-file-storage:platform-file-storage-impl"))
     testImplementation("org.mockito:mockito-core:5.20.0")
     testImplementation("org.testcontainers:junit-jupiter")
 }
