fde6d28d7a245a44d1d676806149e1dd38c7efaf
Monika Kušter <domitermonika@gmail.com>
2026-05-28 08:58:48 +0200

    727 generated

--- server/libs/modules/components/encharge/src/main/java/com/bytechef/component/encharge/action/EnchargeCreateEmailTemplateAction.java
+++ server/libs/modules/components/encharge/src/main/java/com/bytechef/component/encharge/action/EnchargeCreateEmailTemplateAction.java
@@ -18,6 +18,7 @@ package com.bytechef.component.encharge.action;
 
 import static com.bytechef.component.OpenApiComponentHandler.PropertyType;
 import static com.bytechef.component.definition.ComponentDsl.action;
+import static com.bytechef.component.definition.ComponentDsl.bool;
 import static com.bytechef.component.definition.ComponentDsl.integer;
 import static com.bytechef.component.definition.ComponentDsl.object;
 import static com.bytechef.component.definition.ComponentDsl.outputSchema;
@@ -33,10 +34,10 @@ import java.util.Map;
  *
  * @generated
  */
-public class EnchargeCreateEmailAction {
-    public static final ComponentDsl.ModifiableActionDefinition ACTION_DEFINITION = action("createEmail")
+public class EnchargeCreateEmailTemplateAction {
+    public static final ComponentDsl.ModifiableActionDefinition ACTION_DEFINITION = action("createEmailTemplate")
         .title("Create Email Template")
-        .description("Create email template")
+        .description("Creates email template.")
         .metadata(
             Map.of(
                 "method", "POST",
@@ -47,42 +48,63 @@ public class EnchargeCreateEmailAction {
             Map.of(
                 "type", PropertyType.BODY))
             .label("Name")
-            .description("Name of the email template")
+            .description("Name of the email template.")
             .required(true),
             string("subject").metadata(
                 Map.of(
                     "type", PropertyType.BODY))
                 .label("Subject")
-                .description("Subject of the email")
+                .description("Subject of the email.")
                 .required(true),
             string("fromEmail").metadata(
                 Map.of(
                     "type", PropertyType.BODY))
                 .label("From Email")
-                .description("From address to send the email from")
+                .description("From address to send the email from.")
                 .required(true),
             string("replyEmail").metadata(
                 Map.of(
                     "type", PropertyType.BODY))
                 .label("Reply Email")
                 .description("Address that recipients will reply to by default.")
-                .required(false))
-        .output(outputSchema(object()
-            .properties(object("email").properties(integer("id").description("ID of the email template.")
                 .required(false),
-                string("name").description("Name of the email template.")
-                    .required(false),
-                string("subject").description("Subject of the email.")
-                    .required(false),
-                string("fromEmail").description("From address to send the email from.")
-                    .required(false),
-                string("replyEmail").description("Address that recipients will reply to by default.")
-                    .required(false))
+            string("html").metadata(
+                Map.of(
+                    "type", PropertyType.BODY))
+                .label("HTML Content")
+                .description("HTML content of the email template.")
                 .required(false))
+        .output(outputSchema(object().properties(object("email").properties(
+            integer("id").description("ID of the email template.")
+                .required(false),
+            bool("isStandalone").description("Whether this email template is standalone or part of a broadcast.")
+                .required(false),
+            integer("accountId").description("ID of the account associated with the email template.")
+                .required(false),
+            string("name").description("Name of the email template.")
+                .required(false),
+            string("subject").description("Subject of the email.")
+                .required(false),
+            string("fromEmail").description("From address to send the email from.")
+                .required(false),
+            string("replyEmail").description("Address that recipients will reply to by default.")
+                .required(false),
+            string("type").description("Type of the email. HTML or plain-text. Currently only HTML is supported.")
+                .required(false),
+            bool("archived").description("Indicates if the email template is archived.")
+                .required(false),
+            bool("canSpamCompliance").description("Indicates if the email template complies with CAN-SPAM regulations.")
+                .required(false),
+            integer("communicationCategoryId").description("ID of the communication category of this email template.")
+                .required(false),
+            bool("isDefaultTemplate")
+                .description("Indicates if the email template is the default template for the account.")
+                .required(false))
+            .required(false))
             .metadata(
                 Map.of(
                     "responseType", ResponseType.JSON))));
 
-    private EnchargeCreateEmailAction() {
+    private EnchargeCreateEmailTemplateAction() {
     }
 }
