aef1929d515ce6197a86b13eac609bacd1d4cce8
Monika Kušter <domitermonika@gmail.com>
2026-06-02 10:04:56 +0200

    2120 update tests

--- server/libs/modules/components/hunter/src/test/java/com/bytechef/component/hunter/util/HunterUtilsTest.java
+++ server/libs/modules/components/hunter/src/test/java/com/bytechef/component/hunter/util/HunterUtilsTest.java
@@ -17,45 +17,87 @@
 package com.bytechef.component.hunter.util;
 
 import static com.bytechef.component.definition.ComponentDsl.option;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentCaptor.forClass;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-import com.bytechef.component.definition.Context;
+import com.bytechef.component.definition.ActionContext;
+import com.bytechef.component.definition.Context.ContextFunction;
 import com.bytechef.component.definition.Context.Http;
+import com.bytechef.component.definition.Context.Http.Configuration;
+import com.bytechef.component.definition.Context.Http.Configuration.ConfigurationBuilder;
+import com.bytechef.component.definition.Context.Http.Executor;
+import com.bytechef.component.definition.Context.Http.Response;
+import com.bytechef.component.definition.Context.Http.ResponseType;
 import com.bytechef.component.definition.Option;
 import com.bytechef.component.definition.Parameters;
 import com.bytechef.component.definition.TypeReference;
+import com.bytechef.component.test.definition.extension.MockContextSetupExtension;
 import java.util.List;
 import java.util.Map;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 
 /**
  * @author Monika Kušter
  */
+@ExtendWith(MockContextSetupExtension.class)
 class HunterUtilsTest {
 
-    private final Context mockedContext = mock(Context.class);
-    private final Http.Executor mockedExecutor = mock(Http.Executor.class);
+    private final ArgumentCaptor<String> stringArgumentCaptor = forClass(String.class);
+    private final ArgumentCaptor<Object[]> objectsArgumentCaptor = forClass(Object[].class);
     private final Parameters mockedParameters = mock(Parameters.class);
-    private final Http.Response mockedResponse = mock(Http.Response.class);
 
     @Test
-    void testGetLeadListIdOptions() {
-        when(mockedContext.http(any()))
+    void testGetLeadListIdOptions(
+        ActionContext mockedContext, Response mockedResponse, Executor mockedExecutor, Http mockedHttp,
+        ArgumentCaptor<ContextFunction<Http, Executor>> httpFunctionArgumentCaptor,
+        ArgumentCaptor<ConfigurationBuilder> configurationBuilderArgumentCaptor) {
+
+        when(mockedHttp.get(stringArgumentCaptor.capture()))
             .thenReturn(mockedExecutor);
-        when(mockedExecutor.configuration(any()))
+        when(mockedExecutor.queryParameters(objectsArgumentCaptor.capture()))
             .thenReturn(mockedExecutor);
-        when(mockedExecutor.execute())
-            .thenReturn(mockedResponse);
         when(mockedResponse.getBody(any(TypeReference.class)))
-            .thenReturn(Map.of("data", Map.of("leads_lists", List.of(Map.of("id", 123, "name", "some name")))));
+            .thenReturn(
+                Map.of(
+                    "data", Map.of("leads_lists", List.of(Map.of("id", 123, "name", "abc"))),
+                    "meta", Map.of("total", 2)))
+            .thenReturn(Map.of(
+                "data", Map.of("leads_lists", List.of(Map.of("id", 345, "name", "xy"))),
+                "meta", Map.of("total", 2)));
 
-        List<Option<Long>> result =
-            HunterUtils.getLeadListIdOptions(mockedParameters, mockedParameters, Map.of(), "", mockedContext);
-        List<Option<Long>> expectedOptions = List.of(option("some name", 123));
+        List<Option<Long>> result = HunterUtils.getLeadListIdOptions(
+            mockedParameters, mockedParameters, Map.of(), "", mockedContext);
 
-        assertEquals(expectedOptions, result);
+        assertEquals(List.of(option("abc", 123), option("xy", 345)), result);
+        assertNotNull(httpFunctionArgumentCaptor.getValue());
+
+        ConfigurationBuilder configurationBuilder = configurationBuilderArgumentCaptor.getValue();
+        Configuration configuration = configurationBuilder.build();
+
+        assertEquals(ResponseType.JSON, configuration.getResponseType());
+        assertEquals("/leads_lists", stringArgumentCaptor.getValue());
+
+        Object[] queryParameters1 = {
+            "limit", 100,
+            "offset", 0
+        };
+
+        Object[] queryParameters2 = {
+            "limit", 100,
+            "offset", 1
+        };
+
+        List<Object[]> argumentCaptorAllValues = objectsArgumentCaptor.getAllValues();
+
+        assertEquals(2, argumentCaptorAllValues.size());
+        assertArrayEquals(queryParameters1, argumentCaptorAllValues.get(0));
+        assertArrayEquals(queryParameters2, argumentCaptorAllValues.get(1));
     }
 }
