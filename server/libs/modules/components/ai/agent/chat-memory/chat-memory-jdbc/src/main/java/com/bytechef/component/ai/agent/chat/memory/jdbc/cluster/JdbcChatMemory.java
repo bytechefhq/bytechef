c96b40fd550b8cad980f1b3d48c9628d4a254ba1
mkriskovic <marko.kriskovic@protonmail.com>
2026-06-05 12:20:46 +0200

    Revert "1652 - using filter in Chat memory directly"
    This reverts commit 77f054e8a9bb29827e67673c71f80632d4906069.

--- server/libs/modules/components/ai/agent/chat-memory/chat-memory-jdbc/src/main/java/com/bytechef/component/ai/agent/chat/memory/jdbc/cluster/JdbcChatMemory.java
+++ server/libs/modules/components/ai/agent/chat-memory/chat-memory-jdbc/src/main/java/com/bytechef/component/ai/agent/chat/memory/jdbc/cluster/JdbcChatMemory.java
@@ -20,7 +20,6 @@ import static com.bytechef.component.ai.agent.chat.memory.jdbc.constant.JdbcChat
 import static com.bytechef.component.definition.ComponentDsl.string;
 import static com.bytechef.platform.component.definition.ai.agent.ChatMemoryFunction.CHAT_MEMORY;
 
-import com.bytechef.component.ai.agent.chat.memory.jdbc.util.FilteringWindowChatMemory;
 import com.bytechef.component.ai.agent.chat.memory.jdbc.util.JdbcChatMemoryUtils;
 import com.bytechef.component.definition.ClusterElementDefinition;
 import com.bytechef.component.definition.ComponentDsl;
@@ -74,7 +73,7 @@ public class JdbcChatMemory {
             .build();
 
         return new ChatMemoryFunction.Result(
-            MessageChatMemoryAdvisor.builder(new FilteringWindowChatMemory(chatMemory))
+            MessageChatMemoryAdvisor.builder(chatMemory)
                 .build(),
             chatMemory);
     }
