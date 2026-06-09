08526685d50156984a5f7321d938700621763848
Monika Kušter <domitermonika@gmail.com>
2026-06-09 11:58:22 +0200

    3352 update tests

--- server/libs/modules/components/linkedin/src/test/java/com/bytechef/component/linkedin/util/LinkedInUtilsTest.java
+++ server/libs/modules/components/linkedin/src/test/java/com/bytechef/component/linkedin/util/LinkedInUtilsTest.java
@@ -19,6 +19,8 @@ package com.bytechef.component.linkedin.util;
 import static com.bytechef.component.linkedin.constant.LinkedInConstants.IMAGE;
 import static com.bytechef.component.linkedin.constant.LinkedInConstants.IMAGES;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentCaptor.forClass;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -26,34 +28,37 @@ import static org.mockito.Mockito.when;
 import com.bytechef.component.definition.Context;
 import com.bytechef.component.definition.Context.ContextFunction;
 import com.bytechef.component.definition.Context.Http;
+import com.bytechef.component.definition.Context.Http.Body;
+import com.bytechef.component.definition.Context.Http.Configuration;
+import com.bytechef.component.definition.Context.Http.Configuration.ConfigurationBuilder;
+import com.bytechef.component.definition.Context.Http.Executor;
+import com.bytechef.component.definition.Context.Http.Response;
+import com.bytechef.component.definition.Context.Http.ResponseType;
 import com.bytechef.component.definition.FileEntry;
 import com.bytechef.component.definition.TypeReference;
+import com.bytechef.component.test.definition.extension.MockContextSetupExtension;
 import java.util.List;
 import java.util.Map;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
 
 /**
  * @author Monika Kušter
  */
+@ExtendWith(MockContextSetupExtension.class)
 class LinkedInUtilsTest {
 
-    private final ArgumentCaptor<Http.Body> bodyArgumentCaptor = ArgumentCaptor.forClass(Http.Body.class);
-    @SuppressWarnings("unchecked")
-    private final ArgumentCaptor<ContextFunction<Http, Http.Executor>> contextFunctionArgumentCaptor =
-        ArgumentCaptor.forClass(ContextFunction.class);
-    private final Context mockedContext = mock(Context.class);
-    private final Http.Executor mockedExecutor = mock(Http.Executor.class);
+    private final ArgumentCaptor<Body> bodyArgumentCaptor = forClass(Body.class);
     private final FileEntry mockedFileEntry = mock(FileEntry.class);
-    private final Http mockedHttp = mock(Http.class);
-    private final Http.Response mockedResponse = mock(Http.Response.class);
-    private final ArgumentCaptor<String> stringArgumentCaptor = ArgumentCaptor.forClass(String.class);
+    private final ArgumentCaptor<String> stringArgumentCaptor = forClass(String.class);
 
     @Test
-    void testUploadContent() {
-        when(mockedContext.http(contextFunctionArgumentCaptor.capture()))
-            .thenAnswer(inv -> contextFunctionArgumentCaptor.getValue()
-                .apply(mockedHttp));
+    void testUploadContent(
+        Context mockedContext, Response mockedResponse, Executor mockedExecutor, Http mockedHttp,
+        ArgumentCaptor<ContextFunction<Http, Executor>> httpFunctionArgumentCaptor,
+        ArgumentCaptor<ConfigurationBuilder> configurationBuilderArgumentCaptor) {
+
         when(mockedHttp.post(stringArgumentCaptor.capture()))
             .thenReturn(mockedExecutor);
         when(mockedHttp.put(stringArgumentCaptor.capture()))
@@ -62,23 +67,30 @@ class LinkedInUtilsTest {
             .thenReturn(mockedExecutor);
         when(mockedExecutor.body(bodyArgumentCaptor.capture()))
             .thenReturn(mockedExecutor);
-        when(mockedExecutor.configuration(any()))
-            .thenReturn(mockedExecutor);
-        when(mockedExecutor.execute())
-            .thenReturn(mockedResponse);
         when(mockedResponse.getBody(any(TypeReference.class)))
             .thenReturn(Map.of("value", Map.of("uploadUrl", "uploadUrl", IMAGE, "imageUrn")));
 
         String result = LinkedInUtils.uploadContent(mockedContext, mockedFileEntry, "urn", IMAGES);
 
         assertEquals("imageUrn", result);
+
+        for (ContextFunction<Http, Executor> function : httpFunctionArgumentCaptor.getAllValues()) {
+            assertNotNull(function);
+        }
+
+        for (ConfigurationBuilder configurationBuilder : configurationBuilderArgumentCaptor.getAllValues()) {
+            Configuration configuration = configurationBuilder.build();
+
+            assertEquals(ResponseType.JSON, configuration.getResponseType());
+        }
+
         assertEquals(
             List.of("/v2/images", "action", "initializeUpload", "uploadUrl"),
             stringArgumentCaptor.getAllValues());
         assertEquals(
             List.of(
-                Http.Body.of("initializeUploadRequest", Map.of("owner", "urn")),
-                Http.Body.of(Map.of("file", mockedFileEntry), Http.BodyContentType.FORM_DATA)),
+                Body.of("initializeUploadRequest", Map.of("owner", "urn")),
+                Body.of(Map.of("file", mockedFileEntry), Http.BodyContentType.FORM_DATA)),
             bodyArgumentCaptor.getAllValues());
     }
 }
