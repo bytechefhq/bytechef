cff88a42f5f060c7df8e8706c7cadf90bc76accd
Nikolina Spehar <speharnikolina87@gmail.com>
2026-06-10 11:42:31 +0200

    5168 - added ReplyToChannelMessage action

---
+++ server/libs/modules/components/microsoft/microsoft-teams/src/main/java/com/bytechef/component/microsoft/teams/action/MicrosoftTeamsSendReplyToChannelMessageAction.java
@@ -0,0 +1,106 @@
+/*
+ * Copyright 2025 ByteChef
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef.component.microsoft.teams.action;
+
+import static com.bytechef.component.definition.ComponentDsl.action;
+import static com.bytechef.component.definition.ComponentDsl.outputSchema;
+import static com.bytechef.component.definition.ComponentDsl.string;
+import static com.bytechef.component.microsoft.teams.constant.MicrosoftTeamsConstants.ATTACHMENTS;
+import static com.bytechef.component.microsoft.teams.constant.MicrosoftTeamsConstants.ATTACHMENTS_PROPERTY;
+import static com.bytechef.component.microsoft.teams.constant.MicrosoftTeamsConstants.BODY;
+import static com.bytechef.component.microsoft.teams.constant.MicrosoftTeamsConstants.CHANNEL_ID;
+import static com.bytechef.component.microsoft.teams.constant.MicrosoftTeamsConstants.CONTENT;
+import static com.bytechef.component.microsoft.teams.constant.MicrosoftTeamsConstants.CONTENT_PROPERTY;
+import static com.bytechef.component.microsoft.teams.constant.MicrosoftTeamsConstants.CONTENT_TYPE;
+import static com.bytechef.component.microsoft.teams.constant.MicrosoftTeamsConstants.CONTENT_TYPE_PROPERTY;
+import static com.bytechef.component.microsoft.teams.constant.MicrosoftTeamsConstants.MESSAGE_ID;
+import static com.bytechef.component.microsoft.teams.constant.MicrosoftTeamsConstants.MESSAGE_OUTPUT_PROPERTY;
+import static com.bytechef.component.microsoft.teams.constant.MicrosoftTeamsConstants.TEAM_ID;
+
+import com.bytechef.component.definition.ActionDefinition.OptionsFunction;
+import com.bytechef.component.definition.ComponentDsl.ModifiableActionDefinition;
+import com.bytechef.component.definition.Context;
+import com.bytechef.component.definition.Context.Http;
+import com.bytechef.component.definition.Parameters;
+import com.bytechef.component.definition.TypeReference;
+import com.bytechef.component.microsoft.teams.util.MicrosoftTeamsUtils;
+import com.bytechef.microsoft.commons.MicrosoftUtils;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author Nikolina Spehar
+ */
+public class MicrosoftTeamsSendReplyToChannelMessageAction {
+
+    public static final ModifiableActionDefinition ACTION_DEFINITION = action("sendReplyToChannelMessage")
+        .title("Send Reply to Channel Message")
+        .description(
+            "Sends a reply to a channel message. Sending attachments is supported with Message Text Format is set to " +
+                "\"html\".")
+        .properties(
+            string(TEAM_ID)
+                .label("Team ID")
+                .description("ID of the team where the channel is located.")
+                .options((OptionsFunction<String>) MicrosoftTeamsUtils::getTeamIdOptions)
+                .required(true),
+            string(CHANNEL_ID)
+                .label("Channel ID")
+                .description("Channel of the message that will get a reply.")
+                .optionsLookupDependsOn(TEAM_ID)
+                .options((OptionsFunction<String>) MicrosoftTeamsUtils::getChannelIdOptions)
+                .required(true),
+            string(MESSAGE_ID)
+                .label("Message ID")
+                .description("ID of the message that will get a reply.")
+                .required(true),
+            CONTENT_TYPE_PROPERTY,
+            CONTENT_PROPERTY,
+            ATTACHMENTS_PROPERTY)
+        .output(outputSchema(MESSAGE_OUTPUT_PROPERTY))
+        .perform(MicrosoftTeamsSendReplyToChannelMessageAction::perform)
+        .help(
+            "",
+            "https://docs.bytechef.io/reference/components/microsoft-teams_v1#send-reply-to-message")
+        .processErrorResponse(MicrosoftUtils::processErrorResponse);
+
+    private MicrosoftTeamsSendReplyToChannelMessageAction() {
+    }
+
+    public static Object perform(Parameters inputParameters, Parameters connectionParameters, Context context) {
+        List<String> fileIds = inputParameters.getList(ATTACHMENTS, String.class, List.of());
+        List<Map<String, String>> attachments = MicrosoftTeamsUtils.getAttachmentsList(fileIds, context);
+
+        String htmlAttachmentsTag = MicrosoftTeamsUtils.getHtmlAttachmentsTag(attachments);
+
+        return context
+            .http(http -> http.post(
+                "/teams/%s/channels/%s/messages/%s/replies".formatted(
+                    inputParameters.getRequiredString(TEAM_ID),
+                    inputParameters.getRequiredString(CHANNEL_ID),
+                    inputParameters.getRequiredString(MESSAGE_ID))))
+            .configuration(Http.responseType(Http.ResponseType.JSON))
+            .body(
+                Http.Body.of(
+                    BODY, Map.of(
+                        CONTENT, inputParameters.getRequiredString(CONTENT) + htmlAttachmentsTag,
+                        CONTENT_TYPE, inputParameters.getRequiredString(CONTENT_TYPE)),
+                    ATTACHMENTS, attachments))
+            .execute()
+            .getBody(new TypeReference<>() {});
+    }
+}
---
+++ server/libs/modules/components/microsoft/microsoft-teams/src/test/java/com/bytechef/component/microsoft/teams/action/MicrosoftTeamsSendReplyToChannelMessageActionTest.java
@@ -0,0 +1,114 @@
+/*
+ * Copyright 2025 ByteChef
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef.component.microsoft.teams.action;
+
+import static com.bytechef.component.microsoft.teams.constant.MicrosoftTeamsConstants.ATTACHMENTS;
+import static com.bytechef.component.microsoft.teams.constant.MicrosoftTeamsConstants.BODY;
+import static com.bytechef.component.microsoft.teams.constant.MicrosoftTeamsConstants.CHANNEL_ID;
+import static com.bytechef.component.microsoft.teams.constant.MicrosoftTeamsConstants.CONTENT;
+import static com.bytechef.component.microsoft.teams.constant.MicrosoftTeamsConstants.CONTENT_TYPE;
+import static com.bytechef.component.microsoft.teams.constant.MicrosoftTeamsConstants.MESSAGE_ID;
+import static com.bytechef.component.microsoft.teams.constant.MicrosoftTeamsConstants.TEAM_ID;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentCaptor.forClass;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.CALLS_REAL_METHODS;
+import static org.mockito.Mockito.mockStatic;
+import static org.mockito.Mockito.when;
+
+import com.bytechef.component.definition.Context;
+import com.bytechef.component.definition.Context.ContextFunction;
+import com.bytechef.component.definition.Context.Http;
+import com.bytechef.component.definition.Context.Http.Body;
+import com.bytechef.component.definition.Context.Http.BodyContentType;
+import com.bytechef.component.definition.Context.Http.Configuration;
+import com.bytechef.component.definition.Context.Http.Configuration.ConfigurationBuilder;
+import com.bytechef.component.definition.Context.Http.Executor;
+import com.bytechef.component.definition.Context.Http.Response;
+import com.bytechef.component.definition.Context.Http.ResponseType;
+import com.bytechef.component.definition.Parameters;
+import com.bytechef.component.definition.TypeReference;
+import com.bytechef.component.microsoft.teams.util.MicrosoftTeamsUtils;
+import com.bytechef.component.test.definition.MockParametersFactory;
+import com.bytechef.component.test.definition.extension.MockContextSetupExtension;
+import java.util.List;
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.MockedStatic;
+
+/**
+ * @author Nikolina Spehar
+ */
+@ExtendWith(MockContextSetupExtension.class)
+class MicrosoftTeamsSendReplyToChannelMessageActionTest {
+
+    private final ArgumentCaptor<Body> bodyArgumentCaptor = forClass(Http.Body.class);
+    private final ArgumentCaptor<Context> contextArgumentCaptor = forClass(Context.class);
+    private final ArgumentCaptor<Object> listArgumentCaptor = forClass(Object.class);
+    private final Parameters mockedParameters = MockParametersFactory.create(
+        Map.of(ATTACHMENTS, List.of("fileId"), TEAM_ID, "teamId", CHANNEL_ID, "channelId",
+            CONTENT, "content", MESSAGE_ID, "messageId", CONTENT_TYPE, "contentType"));
+    private final Map<String, Object> responseMap = Map.of("key", "value");
+    private final ArgumentCaptor<String> stringArgumentCaptor = forClass(String.class);
+
+    @Test
+    void testPerform(
+        Context context, Response mockedResponse, Executor mockedExecutor, Http mockedHttp,
+        ArgumentCaptor<ContextFunction<Http, Executor>> httpFunctionArgumentCaptor,
+        ArgumentCaptor<ConfigurationBuilder> configurationBuilderArgumentCaptor) {
+
+        try (
+            MockedStatic<MicrosoftTeamsUtils> microsoftTeamsUtilsMockedStatic =
+                mockStatic(MicrosoftTeamsUtils.class, CALLS_REAL_METHODS)) {
+
+            microsoftTeamsUtilsMockedStatic.when(
+                () -> MicrosoftTeamsUtils.getAttachmentsList(
+                    (List<String>) listArgumentCaptor.capture(), contextArgumentCaptor.capture()))
+                .thenReturn(List.of());
+
+            when(mockedHttp.post(stringArgumentCaptor.capture()))
+                .thenReturn(mockedExecutor);
+            when(mockedExecutor.body(bodyArgumentCaptor.capture()))
+                .thenReturn(mockedExecutor);
+            when(mockedResponse.getBody(any(TypeReference.class)))
+                .thenReturn(responseMap);
+
+            Object result = MicrosoftTeamsSendReplyToChannelMessageAction.perform(
+                mockedParameters, mockedParameters, context);
+
+            assertEquals(responseMap, result);
+
+            Map<String, Object> expectedBody = Map.of(
+                BODY, Map.of(CONTENT_TYPE, "contentType", CONTENT, "content"), ATTACHMENTS, List.of());
+
+            assertEquals(Body.of(expectedBody, BodyContentType.JSON), bodyArgumentCaptor.getValue());
+            assertNotNull(httpFunctionArgumentCaptor.getValue());
+
+            ConfigurationBuilder configurationBuilder = configurationBuilderArgumentCaptor.getValue();
+            Configuration configuration = configurationBuilder.build();
+
+            assertEquals(List.of("fileId"), listArgumentCaptor.getValue());
+            assertEquals(ResponseType.JSON, configuration.getResponseType());
+            assertEquals(context, contextArgumentCaptor.getValue());
+            assertEquals(
+                "/teams/teamId/channels/channelId/messages/messageId/replies", stringArgumentCaptor.getValue());
+        }
+    }
+}
