99974a02dc98a28334e34347d667d52dbbdc3394
Nikolina Spehar <speharnikolina87@gmail.com>
2026-06-30 10:00:07 +0200

    2131 - added tests

--- server/libs/modules/components/wordpress/src/test/java/com/bytechef/component/wordpress/util/WordpressUtilsTest.java
+++ server/libs/modules/components/wordpress/src/test/java/com/bytechef/component/wordpress/util/WordpressUtilsTest.java
@@ -18,38 +18,47 @@ package com.bytechef.component.wordpress.util;
 
 import static com.bytechef.component.definition.ComponentDsl.option;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentCaptor.forClass;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import com.bytechef.component.definition.Context;
+import com.bytechef.component.definition.Context.ContextFunction;
+import com.bytechef.component.definition.Context.Http;
+import com.bytechef.component.definition.Context.Http.Configuration;
+import com.bytechef.component.definition.Context.Http.Configuration.ConfigurationBuilder;
 import com.bytechef.component.definition.Context.Http.Executor;
 import com.bytechef.component.definition.Context.Http.Response;
+import com.bytechef.component.definition.Context.Http.ResponseType;
 import com.bytechef.component.definition.Option;
 import com.bytechef.component.definition.Parameters;
 import com.bytechef.component.definition.TypeReference;
+import com.bytechef.component.test.definition.extension.MockContextSetupExtension;
 import java.util.List;
 import java.util.Map;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 
 /**
  * @author Nikolina Spehar
  */
+@ExtendWith(MockContextSetupExtension.class)
 class WordpressUtilsTest {
 
-    private static final Context mockedContext = mock(Context.class);
-    private static final Executor mockedExecutor = mock(Executor.class);
     private static final Parameters mockedParameters = mock(Parameters.class);
-    private static final Response mockedResponse = mock(Response.class);
+    private static final ArgumentCaptor<String> stringArgumentCaptor = forClass(String.class);
 
     @Test
-    void testGetCategoriesOptions() {
-        when(mockedContext.http(any()))
+    void testGetCategoriesOptions(
+        Context mockedContext, Response mockedResponse, Executor mockedExecutor, Http mockedHttp,
+        ArgumentCaptor<ContextFunction<Http, Executor>> httpFunctionArgumentCaptor,
+        ArgumentCaptor<ConfigurationBuilder> configurationBuilderArgumentCaptor) {
+
+        when(mockedHttp.get(stringArgumentCaptor.capture()))
             .thenReturn(mockedExecutor);
-        when(mockedExecutor.configuration(any()))
-            .thenReturn(mockedExecutor);
-        when(mockedExecutor.execute())
-            .thenReturn(mockedResponse);
         when(mockedResponse.getBody(any(TypeReference.class)))
             .thenReturn(List.of(Map.of("name", "category1", "id", 1),
                 Map.of("name", "category2", "id", 2)));
@@ -62,5 +71,12 @@ class WordpressUtilsTest {
             option("category2", 2));
 
         assertEquals(expected, result);
+        assertNotNull(httpFunctionArgumentCaptor.getValue());
+        assertEquals("/wp/v2/categories", stringArgumentCaptor.getValue());
+
+        ConfigurationBuilder configurationBuilder = configurationBuilderArgumentCaptor.getValue();
+        Configuration configuration = configurationBuilder.build();
+
+        assertEquals(ResponseType.JSON, configuration.getResponseType());
     }
 }
