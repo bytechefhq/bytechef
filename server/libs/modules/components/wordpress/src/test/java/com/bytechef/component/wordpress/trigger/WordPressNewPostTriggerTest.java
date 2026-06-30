01203d0ba0890e756603a595ea4671c6ddaa059d
Nikolina Spehar <speharnikolina87@gmail.com>
2026-06-30 10:00:07 +0200

    2131 - added tests

--- server/libs/modules/components/wordpress/src/test/java/com/bytechef/component/wordpress/trigger/WordPressNewPostTriggerTest.java
+++ server/libs/modules/components/wordpress/src/test/java/com/bytechef/component/wordpress/trigger/WordPressNewPostTriggerTest.java
@@ -20,58 +20,70 @@ import static com.bytechef.component.wordpress.constant.WordpressConstants.LAST_
 import static com.bytechef.component.wordpress.constant.WordpressConstants.WEBSITE;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import com.bytechef.component.definition.Context.ContextFunction;
 import com.bytechef.component.definition.Context.Http;
+import com.bytechef.component.definition.Context.Http.Configuration;
+import com.bytechef.component.definition.Context.Http.Configuration.ConfigurationBuilder;
+import com.bytechef.component.definition.Context.Http.Executor;
+import com.bytechef.component.definition.Context.Http.Response;
+import com.bytechef.component.definition.Context.Http.ResponseType;
 import com.bytechef.component.definition.Parameters;
 import com.bytechef.component.definition.TriggerContext;
 import com.bytechef.component.definition.TriggerDefinition.PollOutput;
 import com.bytechef.component.definition.TypeReference;
 import com.bytechef.component.test.definition.MockParametersFactory;
+import com.bytechef.component.test.definition.extension.MockContextSetupExtension;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Map;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
 
 /**
  * @author Nikolina Spehar
  */
+@ExtendWith(MockContextSetupExtension.class)
 class WordPressNewPostTriggerTest {
 
-    private final Http.Executor mockedExecutor = mock(Http.Executor.class);
     private final LocalDateTime mockedLocalDateTime = LocalDateTime.of(2025, 6, 17, 9, 0);
     private final Parameters mockedParameters =
         MockParametersFactory.create(Map.of(WEBSITE, "mockWebsite", LAST_TIME_CHECKED, mockedLocalDateTime));
-    private final Http.Response mockedResponse = mock(Http.Response.class);
-    private final TriggerContext mockedTriggerContext = mock(TriggerContext.class);
     private final ArgumentCaptor<String> stringArgumentCaptor = ArgumentCaptor.forClass(String.class);
 
     @Test
-    void testPoll() {
-        when(mockedTriggerContext.http(any()))
-            .thenReturn(mockedExecutor);
-        when(mockedExecutor.configuration(any()))
+    void testPoll(
+        TriggerContext mockedContext, Response mockedResponse, Executor mockedExecutor, Http mockedHttp,
+        ArgumentCaptor<ContextFunction<Http, Executor>> httpFunctionArgumentCaptor,
+        ArgumentCaptor<ConfigurationBuilder> configurationBuilderArgumentCaptor) {
+
+        when(mockedHttp.get(stringArgumentCaptor.capture()))
             .thenReturn(mockedExecutor);
         when(mockedExecutor.queryParameter(stringArgumentCaptor.capture(), stringArgumentCaptor.capture()))
             .thenReturn(mockedExecutor);
-        when(mockedExecutor.execute())
-            .thenReturn(mockedResponse);
         when(mockedResponse.getBody(any(TypeReference.class)))
             .thenReturn(List.of(Map.of("id", "1")));
 
         PollOutput pollOutput = WordPressNewPostTrigger.poll(
-            mockedParameters, mockedParameters, mockedParameters, mockedTriggerContext);
+            mockedParameters, mockedParameters, mockedParameters, mockedContext);
 
         assertEquals(List.of(Map.of("id", "1")), pollOutput.records());
         assertFalse(pollOutput.pollImmediately());
 
-        List<String> expectedQueryArguments = List.of(
-            "after", mockedLocalDateTime.format(DateTimeFormatter.ISO_DATE_TIME));
+        List<String> expectedStrings = List.of(
+            "/wp/v2/posts", "after", mockedLocalDateTime.format(DateTimeFormatter.ISO_DATE_TIME));
 
-        assertEquals(expectedQueryArguments, stringArgumentCaptor.getAllValues());
+        assertEquals(expectedStrings, stringArgumentCaptor.getAllValues());
+        assertNotNull(httpFunctionArgumentCaptor.getValue());
+
+        ConfigurationBuilder configurationBuilder = configurationBuilderArgumentCaptor.getValue();
+        Configuration configuration = configurationBuilder.build();
+
+        assertEquals(ResponseType.JSON, configuration.getResponseType());
     }
 }
