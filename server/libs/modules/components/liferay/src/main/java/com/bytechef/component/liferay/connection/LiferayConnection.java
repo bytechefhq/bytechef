f369d5f3d892673b795ecd1ef5ae8790601eaca2
Nikolina Spehar <speharnikolina87@gmail.com>
2026-07-07 14:25:57 +0200

    5128 - added OAuth connection option to Liferay

--- server/libs/modules/components/liferay/src/main/java/com/bytechef/component/liferay/connection/LiferayConnection.java
+++ server/libs/modules/components/liferay/src/main/java/com/bytechef/component/liferay/connection/LiferayConnection.java
@@ -17,6 +17,8 @@
 package com.bytechef.component.liferay.connection;
 
 import static com.bytechef.component.definition.Authorization.ADD_TO;
+import static com.bytechef.component.definition.Authorization.CLIENT_ID;
+import static com.bytechef.component.definition.Authorization.CLIENT_SECRET;
 import static com.bytechef.component.definition.Authorization.KEY;
 import static com.bytechef.component.definition.Authorization.PASSWORD;
 import static com.bytechef.component.definition.Authorization.TOKEN;
@@ -33,6 +35,9 @@ import com.bytechef.component.definition.Authorization.ApiTokenLocation;
 import com.bytechef.component.definition.Authorization.AuthorizationType;
 import com.bytechef.component.definition.ComponentDsl.ModifiableConnectionDefinition;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 /**
  * @author Igor Beslic
  */
@@ -42,9 +47,157 @@ public class LiferayConnection {
         .properties(
             string(BASE_URI)
                 .label("Base URI")
+                .required(true)
                 .description("If set, it will be combined Liferay API Endpoint attribute value."))
         .authorizationRequired(true)
         .authorizations(
+            authorization(AuthorizationType.OAUTH2_AUTHORIZATION_CODE)
+                .title("OAuth2")
+                    .properties(
+                        string(CLIENT_ID)
+                            .label("Client ID")
+                            .required(true),
+                        string(CLIENT_SECRET)
+                            .label("Client Secret")
+                            .required(true))
+                .authorizationUrl((connectionParameters, context) ->
+                    connectionParameters.getRequiredString(BASE_URI) + "o/oauth2/authorize")
+                .tokenUrl(((connectionParameters, context) ->
+                    connectionParameters.getRequiredString(BASE_URI) + "o/oauth2/token"))
+                .scopes((connectionParameters, context) -> {
+                    Map<String, Boolean> map = new LinkedHashMap<>();
+
+                    map.put("Liferay.Headless.Discovery.OpenAPI.everything.read", true);
+                    map.put("Liferay.Analytics.Reports.REST.everything.read", false);
+                    map.put("Liferay.Analytyics.Settings.REST.everything.read", false);
+                    map.put("Liferay.Analytyics.Settings.REST.everything.write", false);
+                    map.put("Liferay.Analytyics.Settings.REST.everything", false);
+                    map.put("Liferay.Batch.Planner.REST.everything.read", false);
+                    map.put("Liferay.Batch.Planner.REST.everything", false);
+                    map.put("Liferay.Batch.Planner.REST.everything.write", false);
+                    map.put("Liferay.Bulk.REST.everything.write", false);
+                    map.put("Liferay.Bulk.REST.everything.read", false);
+                    map.put("Liferay.Bulk.REST.everything", false);
+                    map.put("Liferay.Captcha.REST.everything.read", false);
+                    map.put("Liferay.Captcha.REST.everything.write", false);
+                    map.put("Liferay.Captcha.REST.everything", false);
+                    map.put("Liferay.Change.Tracking.REST.everything.write", false);
+                    map.put("Liferay.Change.Tracking.REST.everything.read", false);
+                    map.put("Liferay.Change.Tracking.REST.everything", false);
+                    map.put("Liferay.Commerce.COMMERCE_DEFAULT", false);
+                    map.put("Liferay.Data.Engine.REST.everything.write", false);
+                    map.put("Liferay.Data.Engine.REST.everything.read", false);
+                    map.put("Liferay.Data.Engine.REST.everything", false);
+                    map.put("Liferay.Digital.Signature.REST.everything.write", false);
+                    map.put("Liferay.Digital.Signature.REST.everything.read", false);
+                    map.put("Liferay.Digital.Signature.REST.everything", false);
+                    map.put("Liferay.Dispatch.REST.everything.read", false);
+                    map.put("Liferay.Dispatch.REST.everything.write", false);
+                    map.put("Liferay.Dispatch.REST.everything", false);
+                    map.put("Liferay.Headless.Admin.Address.everything.write", false);
+                    map.put("Liferay.Headless.Admin.Address.everything.read", false);
+                    map.put("Liferay.Headless.Admin.Address.everything", false);
+                    map.put("Liferay.Headless.Admin.Content.everything", false);
+                    map.put("Liferay.Headless.Admin.Content.everything.read", false);
+                    map.put("Liferay.Headless.Admin.Content.everything.write", false);
+                    map.put("Liferay.Headless.Admin.List.Type.everything", false);
+                    map.put("Liferay.Headless.Admin.List.Type.everything.read", false);
+                    map.put("Liferay.Headless.Admin.List.Type.everything.write", false);
+                    map.put("Liferay.Headless.Admin.Site.everything", false);
+                    map.put("Liferay.Headless.Admin.Site.everything.read", false);
+                    map.put("Liferay.Headless.Admin.Site.everything.write", false);
+                    map.put("Liferay.Headless.Admin.Taxonomy.everything.write", false);
+                    map.put("Liferay.Headless.Admin.Taxonomy.everything.read", false);
+                    map.put("Liferay.Headless.Admin.Taxonomy.everything", false);
+                    map.put("Liferay.Headless.Admin.User.everything.read", false);
+                    map.put("Liferay.Headless.Admin.User.everything.write", false);
+                    map.put("Liferay.Headless.Admin.User.everything", false);
+                    map.put("Liferay.Headless.Admin.Workflow.everything.read", false);
+                    map.put("Liferay.Headless.Admin.Workflow.everything.write", false);
+                    map.put("Liferay.Headless.Admin.Workflow.everything", false);
+                    map.put("Liferay.Headless.Batch.Engine.everything.read", false);
+                    map.put("Liferay.Headless.Batch.Engine.everything.write", false);
+                    map.put("Liferay.Headless.Batch.Engine.everything", false);
+                    map.put("Liferay.Headless.Commerce.Admin.Account.everything.read", false);
+                    map.put("Liferay.Headless.Commerce.Admin.Account.everything.write", false);
+                    map.put("Liferay.Headless.Commerce.Admin.Account.everything", false);
+                    map.put("Liferay.Headless.Commerce.Admin.Catalog.everything.read", false);
+                    map.put("Liferay.Headless.Commerce.Admin.Catalog.everything.write", false);
+                    map.put("Liferay.Headless.Commerce.Admin.Catalog.everything", false);
+                    map.put("Liferay.Headless.Commerce.Admin.Channel.everything", false);
+                    map.put("Liferay.Headless.Commerce.Admin.Channel.everything.write", false);
+                    map.put("Liferay.Headless.Commerce.Admin.Channel.everything.read", false);
+                    map.put("Liferay.Headless.Commerce.Admin.Inventory.everything.write", false);
+                    map.put("Liferay.Headless.Commerce.Admin.Inventory.everything.read", false);
+                    map.put("Liferay.Headless.Commerce.Admin.Inventory.everything", false);
+                    map.put("Liferay.Headless.Commerce.Admin.Order.everything", false);
+                    map.put("Liferay.Headless.Commerce.Admin.Order.everything.read", false);
+                    map.put("Liferay.Headless.Commerce.Admin.Order.everything.write", false);
+                    map.put("Liferay.Headless.Commerce.Admin.Payment.everything", false);
+                    map.put("Liferay.Headless.Commerce.Admin.Payment.everything.write", false);
+                    map.put("Liferay.Headless.Commerce.Admin.Payment.everything.read", false);
+                    map.put("Liferay.Headless.Commerce.Admin.Pricing.everything", false);
+                    map.put("Liferay.Headless.Commerce.Admin.Pricing.everything.write", false);
+                    map.put("Liferay.Headless.Commerce.Admin.Pricing.everything.read", false);
+                    map.put("Liferay.Headless.Commerce.Admin.Shipment.everything.read", false);
+                    map.put("Liferay.Headless.Commerce.Admin.Shipment.everything.write", false);
+                    map.put("Liferay.Headless.Commerce.Admin.Shipment.everything", false);
+                    map.put("Liferay.Headless.Commerce.Admin.Site.Setting.everything", false);
+                    map.put("Liferay.Headless.Commerce.Admin.Site.Setting.everything.read", false);
+                    map.put("Liferay.Headless.Commerce.Admin.Site.Setting.everything.write", false);
+                    map.put("Liferay.Headless.Commerce.Delivery.Catalog.everything.read", false);
+                    map.put("Liferay.Headless.Commerce.Delivery.Catalog.everything.write", false);
+                    map.put("Liferay.Headless.Commerce.Delivery.Catalog.everything", false);
+                    map.put("Liferay.Headless.Commerce.Delivery.Order.everything.write", false);
+                    map.put("Liferay.Headless.Commerce.Delivery.Order.everything.read", false);
+                    map.put("Liferay.Headless.Commerce.Delivery.Order.everything", false);
+                    map.put("Liferay.Headless.Commerce.Machine.Learning.everything.read", false);
+                    map.put("Liferay.Headless.Delivery.everything.read", false);
+                    map.put("Liferay.Headless.Delivery.everything.write", false);
+                    map.put("Liferay.Headless.Delivery.everything", false);
+                    map.put("Liferay.Headless.Discovery.API.everything.read", false);
+                    map.put("Liferay.Headless.Form.everything.read", false);
+                    map.put("Liferay.Headless.Form.everything.write", false);
+                    map.put("Liferay.Headless.Form.everything", false);
+                    map.put("Liferay.Headless.Portal.Instances.everything.write", false);
+                    map.put("Liferay.Headless.Portal.Instances.everything.read", false);
+                    map.put("Liferay.Headless.Portal.Instances.everything", false);
+                    map.put("Liferay.Headless.Site.everything.read", false);
+                    map.put("Liferay.Headless.Site.everything.write", false);
+                    map.put("Liferay.Headless.Site.everything", false);
+                    map.put("Liferay.Headless.User.Notification.everything", false);
+                    map.put("Liferay.Headless.User.Notification.everything.read", false);
+                    map.put("Liferay.Headless.User.Notification.everything.write", false);
+                    map.put("Liferay.Notification.REST.everything.read", false);
+                    map.put("Liferay.Notification.REST.everything.write", false);
+                    map.put("Liferay.Notification.REST.everything", false);
+                    map.put("Liferay.Object.Admin.REST.everything.read", false);
+                    map.put("Liferay.Object.Admin.REST.everything.write", false);
+                    map.put("Liferay.Object.Admin.REST.everything", false);
+                    map.put("Liferay.Portal.Language.REST.everything", false);
+                    map.put("Liferay.Portal.Language.REST.everything.write", false);
+                    map.put("Liferay.Portal.Language.REST.everything.read", false);
+                    map.put("Liferay.Portal.Search.REST.everything.read", false);
+                    map.put("Liferay.Portal.Search.REST.everything.write", false);
+                    map.put("Liferay.Portal.Search.REST.everything", false);
+                    map.put("Liferay.Portal.Workflow.Metrics.REST.everything.write", false);
+                    map.put("Liferay.Portal.Workflow.Metrics.REST.everything.read", false);
+                    map.put("Liferay.Portal.Workflow.Metrics.REST.everything", false);
+                    map.put("Liferay.Saml.Admin.REST.everything.write", false);
+                    map.put("Liferay.Saml.Admin.REST.everything.read", false);
+                    map.put("Liferay.Saml.Admin.REST.everything", false);
+                    map.put("Liferay.Scim.REST.everything.read", false);
+                    map.put("Liferay.Scim.REST.everything.write", false);
+                    map.put("Liferay.Scim.REST.everything", false);
+                    map.put("Liferay.Search.Experiences.REST.everything.read", false);
+                    map.put("Liferay.Search.Experiences.REST.everything.write", false);
+                    map.put("Liferay.Search.Experiences.REST.everything", false);
+                    map.put("Liferay.Segments.Asah.REST.everything", false);
+                    map.put("Liferay.Segments.Asah.REST.everything.read", false);
+                    map.put("Liferay.Segments.Asah.REST.everything.write", false);
+
+                    return map;
+                }),
             authorization(AuthorizationType.API_KEY)
                 .title("API Key")
                 .properties(
