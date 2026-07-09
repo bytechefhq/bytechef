e033dedb7812285b5c0c579033e9579d5eeee8ab
Matija Petanjek <matija.petanjek7@gmail.com>
2026-07-09 11:41:32 +0200

    513 - server - Quartz scheduler for Stripe usage reports

---
+++ server/libs/platform/platform-billing/platform-billing-service/src/main/java/com/bytechef/platform/billing/config/BillingSchedulingConfiguration.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2025 ByteChef
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytechef.platform.billing.config;
+
+import com.bytechef.platform.billing.job.StripeUsageReportingJob;
+import org.quartz.CronScheduleBuilder;
+import org.quartz.CronTrigger;
+import org.quartz.JobBuilder;
+import org.quartz.JobDetail;
+import org.quartz.TriggerBuilder;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Matija Petanjek
+ */
+@Configuration
+@ConditionalOnProperty(
+    prefix = "bytechef", name = "coordinator.trigger.scheduler.provider",
+    havingValue = "quartz", matchIfMissing = true)
+public class BillingSchedulingConfiguration {
+
+    @Bean
+    public JobDetail stripeUsageReportingJobDetail() {
+        return JobBuilder.newJob(StripeUsageReportingJob.class)
+            .withIdentity("stripeUsageReportingJob")
+            .storeDurably()
+            .build();
+    }
+
+    @Bean
+    public CronTrigger stripeUsageReportingTrigger(JobDetail stripeUsageReportingJobDetail) {
+        return TriggerBuilder.newTrigger()
+            .forJob(stripeUsageReportingJobDetail)
+            .withIdentity("stripeUsageReportingTrigger")
+            .withSchedule(CronScheduleBuilder.cronSchedule("0 0/1 * * * ?"))
+            .build();
+    }
+}
