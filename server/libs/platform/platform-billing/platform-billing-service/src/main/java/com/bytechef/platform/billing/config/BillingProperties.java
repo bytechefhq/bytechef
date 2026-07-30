6e6797cb43c6f28ef91d8a4a203b044c6824e420
Matija Petanjek <matija.petanjek7@gmail.com>
2026-07-30 13:01:53 +0200

    513 server - extract billing properties out of bytechef branch

--- server/libs/platform/platform-billing/platform-billing-service/src/main/java/com/bytechef/platform/billing/config/BillingProperties.java
+++ server/libs/platform/platform-billing/platform-billing-service/src/main/java/com/bytechef/platform/billing/config/BillingProperties.java
@@ -21,7 +21,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 /**
  * @author Matija Petanjek
  */
-@ConfigurationProperties("bytechef.billing")
+@ConfigurationProperties("billing")
 public record BillingProperties(Stripe stripe) {
 
     public record Stripe(
