a71ccfd0d07076833a655e2b2304748bc8329b0a
marijahorvat171 <marijahorvat171@gmail.com>
2026-07-31 17:39:26 +0200

    2038 - update tests

--- server/libs/modules/components/beamer/src/test/java/com/bytechef/component/beamer/action/BeamerNewCommentActionTest.java
+++ server/libs/modules/components/beamer/src/test/java/com/bytechef/component/beamer/action/BeamerNewCommentActionTest.java
@@ -23,58 +23,71 @@ import static com.bytechef.component.beamer.constant.BeamerConstants.USER_FIRST_
 import static com.bytechef.component.beamer.constant.BeamerConstants.USER_ID;
 import static com.bytechef.component.beamer.constant.BeamerConstants.USER_LAST_NAME;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentCaptor.forClass;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import com.bytechef.component.definition.ActionContext;
+import com.bytechef.component.definition.Context.ContextFunction;
 import com.bytechef.component.definition.Context.Http;
 import com.bytechef.component.definition.Context.Http.Body;
+import com.bytechef.component.definition.Context.Http.Configuration;
+import com.bytechef.component.definition.Context.Http.Configuration.ConfigurationBuilder;
+import com.bytechef.component.definition.Context.Http.Executor;
+import com.bytechef.component.definition.Context.Http.Response;
+import com.bytechef.component.definition.Context.Http.ResponseType;
 import com.bytechef.component.definition.Parameters;
 import com.bytechef.component.definition.TypeReference;
 import com.bytechef.component.test.definition.MockParametersFactory;
+import com.bytechef.component.test.definition.extension.MockContextSetupExtension;
 import java.util.Map;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
 
 /**
  * @author Nikolina Spehar
  */
+@ExtendWith(MockContextSetupExtension.class)
 class BeamerNewCommentActionTest {
 
-    private final ArgumentCaptor<Body> bodyArgumentCaptor = ArgumentCaptor.forClass(Http.Body.class);
-    private final ActionContext mockedActionContext = mock(ActionContext.class);
-    private final Http.Executor mockedExecutor = mock(Http.Executor.class);
+    private final ArgumentCaptor<Body> bodyArgumentCaptor = forClass(Http.Body.class);
     private final Parameters mockedParameters = MockParametersFactory.create(Map.of(
         POST_ID, 2, TEXT, "testText", USER_ID, 1010, USER_EMAIL, "testUserEmail",
         USER_FIRST_NAME, "Jane", USER_LAST_NAME, "Doe"));
-    private final Http.Response mockedResponse = mock(Http.Response.class);
     private final Map<String, Object> responseMap = Map.of();
+    private final ArgumentCaptor<String> stringArgumentCaptor = forClass(String.class);
 
     @Test
-    void perform() {
-        when(mockedActionContext.http(any()))
-            .thenReturn(mockedExecutor);
-        when(mockedExecutor.configuration(any()))
+    void perform(
+        ActionContext mockedContext, Response mockedResponse, Executor mockedExecutor, Http mockedHttp,
+        ArgumentCaptor<ContextFunction<Http, Executor>> httpFunctionArgumentCaptor,
+        ArgumentCaptor<ConfigurationBuilder> configurationBuilderArgumentCaptor) {
+
+        when(mockedHttp.post(stringArgumentCaptor.capture()))
             .thenReturn(mockedExecutor);
         when(mockedExecutor.body(bodyArgumentCaptor.capture()))
             .thenReturn(mockedExecutor);
-        when(mockedExecutor.execute())
-            .thenReturn(mockedResponse);
         when(mockedResponse.getBody(any(TypeReference.class)))
             .thenReturn(responseMap);
 
         Map<String, Object> result = BeamerNewCommentAction.perform(
-            mockedParameters, mockedParameters, mockedActionContext);
+            mockedParameters, mockedParameters, mockedContext);
 
         assertEquals(responseMap, result);
+        assertNotNull(httpFunctionArgumentCaptor.getValue());
+        assertEquals("/posts/2/comments", stringArgumentCaptor.getValue());
 
-        Body body = bodyArgumentCaptor.getValue();
+        ConfigurationBuilder configurationBuilder = configurationBuilderArgumentCaptor.getValue();
+        Configuration configuration = configurationBuilder.build();
 
-        assertEquals(
-            Map.of(
-                TEXT, "testText", USER_ID, "1010", USER_EMAIL, "testUserEmail", USER_FIRST_NAME, "Jane",
-                USER_LAST_NAME, "Doe"),
-            body.getContent());
+        assertEquals(ResponseType.JSON, configuration.getResponseType());
+
+        Map<String, Object> expectedBody = Map.of(
+            TEXT, "testText", USER_ID, "1010", USER_EMAIL, "testUserEmail", USER_FIRST_NAME, "Jane",
+            USER_LAST_NAME, "Doe");
+
+        assertEquals(Body.of(expectedBody, Http.BodyContentType.JSON), bodyArgumentCaptor.getValue());
     }
 }
