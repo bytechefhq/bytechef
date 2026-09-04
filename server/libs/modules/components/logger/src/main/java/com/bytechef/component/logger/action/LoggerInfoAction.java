8449f931716050eb62debea21a0c7d0773153ee4
Ivica Cardic <ivica.cardic@gmail.com>
2026-09-04 17:47:26 +0200

    5638 Log an empty message when the Logger text is not set

--- server/libs/modules/components/logger/src/main/java/com/bytechef/component/logger/action/LoggerInfoAction.java
+++ server/libs/modules/components/logger/src/main/java/com/bytechef/component/logger/action/LoggerInfoAction.java
@@ -40,7 +40,7 @@ public class LoggerInfoAction {
 
         Object text = inputParameters.get(TEXT);
 
-        context.log(log -> log.info(String.valueOf(text)));
+        context.log(log -> log.info(text == null ? "" : String.valueOf(text)));
 
         return null;
     }
