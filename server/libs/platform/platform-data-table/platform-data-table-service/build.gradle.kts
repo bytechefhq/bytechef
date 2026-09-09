8cffb12d9f5b29d69c425e52740f570c6ca1364a
Ivica Cardic <ivica.cardic@gmail.com>
2026-09-09 11:42:17 +0200

    5685 Resolve data table and knowledge base references

--- server/libs/platform/platform-data-table/platform-data-table-service/build.gradle.kts
+++ server/libs/platform/platform-data-table/platform-data-table-service/build.gradle.kts
@@ -12,6 +12,7 @@ dependencies {
     implementation(project(":server:libs:core:commons:commons-util"))
     implementation(project(":server:libs:platform:platform-configuration:platform-configuration-api"))
     implementation(project(":server:libs:platform:platform-tag:platform-tag-api"))
+    implementation(project(":server:libs:platform:platform-workflow:platform-workflow-validator:platform-workflow-validator-api"))
 
     testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation("org.mockito:mockito-core")
