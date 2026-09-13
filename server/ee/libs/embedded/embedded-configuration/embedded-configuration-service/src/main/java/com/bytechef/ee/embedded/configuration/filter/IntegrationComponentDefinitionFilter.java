22575c9ec41f60b0db5dc73810e70f10b578a39e
Ivica Cardic <ivica.cardic@gmail.com>
2026-09-13 20:38:50 +0200

    5747 Hide Data Table and Knowledge Base components in the embedded workflow editor

--- server/ee/libs/embedded/embedded-configuration/embedded-configuration-service/src/main/java/com/bytechef/ee/embedded/configuration/filter/IntegrationComponentDefinitionFilter.java
+++ server/ee/libs/embedded/embedded-configuration/embedded-configuration-service/src/main/java/com/bytechef/ee/embedded/configuration/filter/IntegrationComponentDefinitionFilter.java
@@ -23,7 +23,8 @@ import org.springframework.stereotype.Component;
 @ConditionalOnEEVersion
 public class IntegrationComponentDefinitionFilter implements ComponentDefinitionFilter {
 
-    private static final List<String> COMPONENT_NAMES = List.of("apiPlatform", "codeWorkflow", "webhook");
+    private static final List<String> COMPONENT_NAMES = List.of(
+        "apiPlatform", "codeWorkflow", "dataTable", "knowledgeBase", "webhook");
 
     @Override
     public boolean filter(ComponentDefinition componentDefinition) {
