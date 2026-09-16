82b7aaa39f5db07e52c3d663fd6f894a9e40ff45
Krešimir Čoko <kresimircoko@gmail.com>
2026-09-16 15:31:12 +0200

    3029 - Add the Dialog barrel

---
+++ client/src/components/Dialog/index.ts
@@ -0,0 +1,35 @@
+export {Dialog, DialogClose, DialogContent, type DialogContentProps, DialogTrigger} from './Dialog';
+
+export {
+    DialogCancelButton,
+    type DialogCancelButtonProps,
+    DialogNextButton,
+    type DialogNextButtonProps,
+} from './DialogButtons';
+
+export {
+    DialogBody,
+    type DialogBodyProps,
+    DialogFooter,
+    type DialogFooterProps,
+    DialogHeader,
+    type DialogHeaderProps,
+    DialogMain,
+    type DialogMainProps,
+} from './DialogMain';
+
+export {DialogSidebar, type DialogSidebarProps} from './DialogSidebar';
+
+export {DialogStepIndicator, type DialogStepIndicatorProps} from './DialogStepIndicator';
+
+export {DialogSteps, type DialogStepsProps} from './DialogSteps';
+
+export {
+    type DialogStepI,
+    type DialogStepsContextI,
+    type DialogStepStatusI,
+    DialogStepsProvider,
+    type DialogStepsProviderProps,
+} from './DialogStepsProvider';
+
+export {useDialogSteps} from './hooks/useDialogSteps';
